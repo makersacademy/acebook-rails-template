9aef0392f3c84e5d4b04601393123fd4262296d9
Kay Lovelace <kay@makersacademy.com>
2017-05-26 16:10:11 +0100

    Complete creation & display of Posts

---
+++ spec/features/user_can_submit_posts_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "Timeline", type: :feature do
+  scenario "Can submit posts and view them" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world!")
+  end
+end
