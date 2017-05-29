ff383f5c1726e7351787323c7510eff3c1052b0d
ddosque <dpbergquist@gmail.com>
2017-05-29 20:11:05 +0100

    switch to pg database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170526114520) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
