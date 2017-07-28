1297e8a29b0e328fb6037d4d2b50ffccfe542227
Jasper King-Harman <j.kingharman@googlemail.com>
2017-07-28 10:34:59 +0100

    Changing test so that 302 is appropriate response

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe UsersController, type: :controller do
   describe "GET /show " do
     it "responds with 200" do
       get :show
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
     it "adds user to database" do
       expect{ User.create(name: "test", email: "test@test.com", password: "Password123",
