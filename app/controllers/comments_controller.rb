157ea1d92d9bec375577a2778dfb810915a6b062
Thomas Duffy <tomjamesduffy@gmail.com>
2018-04-10 16:02:11 +0100

    Can delete posts and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     redirect_to posts_url
   end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to posts_url
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:body)
