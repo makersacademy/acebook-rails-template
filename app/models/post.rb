31b318410335677cd192e48209955a133e606326
Charlene Bastians <charlenebastians@hotmail.com>
2018-04-10 15:50:04 +0100

    sytax fixed to fix arity bug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
-  has_many :likes, :comments
+  has_many :likes
+  has_many :comments
   validates :message, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,19 +15,6 @@ ActiveRecord::Schema.define(version: 20180410105607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.bigint "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
-
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
@@ -35,7 +22,13 @@ ActiveRecord::Schema.define(version: 20180410105607) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -44,8 +37,6 @@ ActiveRecord::Schema.define(version: 20180410105607) do
     t.datetime "updated_at", null: false
   end
 
-
-  add_foreign_key "likes", "posts"
   add_foreign_key "comments", "posts"
-
+  add_foreign_key "likes", "posts"
 end
