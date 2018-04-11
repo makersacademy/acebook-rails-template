644ba462e64c618d9823bc44beb0c5bf228f093d
paosch <paola_schiaffino@yahoo.co.uk>
2018-04-11 17:49:08 +0100

    Initial signup set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
-  resources :posts
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+  resources :users
 end
