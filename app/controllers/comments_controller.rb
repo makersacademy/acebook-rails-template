4c964b8e9e21b09173bf80c015aaa2fa531be96e
Alfie Darko <me@alfiedarko.co.uk>
2018-04-11 17:33:13 +0100

    (spiked) implemented edit comment feature, fixing rspec tests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,9 +12,25 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to('/posts/' + comment_params[:post_id])
+    else
+      render 'commentsform'
+    end
+  end
+
   private
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.permit(:commenter, :body, :post_id)
   end
 end
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -1,14 +1,14 @@
 
 def add_new_post
-  visit "/posts"
-  click_link "New post"
-  fill_in "Post a new message", with: "Hello, world!"
-  click_button "Submit"
+  visit '/posts'
+  click_link 'New post'
+  fill_in 'Post a message', with: 'Hello, world!'
+  click_button 'Submit'
 end
 
 def add_comment
-  click_link "Show"
-  fill_in "comment[commenter]", with: "Charlene"
-  fill_in "comment[body]", with: "Rails is awesome!"
-  click_button "Create Comment"
+  click_link 'Show'
+  fill_in 'comment[commenter]', with: 'Charlene'
+  fill_in 'comment[body]', with: 'Rails is awesome!'
+  click_button 'Create Comment'
 end
