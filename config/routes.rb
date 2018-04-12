8e709f27bf1613a4fafc83cfbb9915c38213da8b
paosch <paola_schiaffino@yahoo.co.uk>
2018-04-12 11:56:19 +0100

    "Deleting previous changes to switch to Devise"
    This reverts commit 644ba462e64c618d9823bc44beb0c5bf228f093d.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
-  resources :users
+  # get 'welcome/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  resources :posts
 end
