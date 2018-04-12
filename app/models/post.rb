3c8178d120186c295402cb1a08bc4de7d67fcefc
Charlene Bastians <charlenebastians@hotmail.com>
2018-04-12 16:28:14 +0100

    added belongs to and has many to all models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  belongs_to :users
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   validates :message, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :likes
+  has_many :comments
+  has_many :posts       
 end
