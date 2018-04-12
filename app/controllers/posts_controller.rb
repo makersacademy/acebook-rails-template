c5ef8c19e6226f47e52db12838bc688e4c5a0eb4
George Drayson <drayson.g@mac.com>
2018-04-12 11:54:22 +0100

    Edit function red to green

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to '/'
+    redirect_to posts_path
   end
 
   private
