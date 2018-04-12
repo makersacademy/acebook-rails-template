c7c6d3670e8026f45fdbd1f71f56d85da8d783a2
Christopher Ly <demedial@gmail.com>
2018-04-12 16:23:23 +0100

    Fix capybara tests

--- spec/features/newest_post_are_listed_first_spec.rb
+++ spec/features/newest_post_are_listed_first_spec.rb
@@ -2,16 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "User can submit a post and posts are sorted in a DESC order" do
-    visit "/posts"
+    user_sign_up
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
 
-    visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Bye, world!"
     click_button "Submit"
 
-    expect(page.first('p').text).to include("Bye, world!")
+    expect(page.all('.post').first.text).to include("Bye, world!")
   end
 end
