8c8534ce5f8c3f6718971916ed38c47e77786ca1
Thomas Duffy <tomjamesduffy@gmail.com>
2018-04-12 23:06:35 +0100

    Rubocop errors amended

--- spec/features/user_can_edit_their_post_spec.rb
+++ spec/features/user_can_edit_their_post_spec.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require_relative '../helpers/custom_helper'
 
-
-RSpec.describe "Timeline", type: :feature do
-  scenario "User wants to delete a post" do
+RSpec.describe 'Timeline', type: :feature do
+  scenario 'User wants to delete a post' do
     sign_up
     create_post
     click_link 'Edit'
