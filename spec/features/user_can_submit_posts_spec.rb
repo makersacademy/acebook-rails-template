02bb1be995c5bd55ab8854b17565f936713dfaab
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-12 14:27:17 +0100

    added passing feature test for post order

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -6,9 +6,7 @@ RSpec.feature "Timeline", type: :feature do
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
-    expect(page).to have_content("Hello, world!")
-  end
 
-  scenario "Posts are listed in descending order" do
+    expect(page).to have_content("Hello, world!")
   end
 end
