13deeadc47911f949f76d773c135eafb520a4e55
Vivian Allen <mrvivianallen@gmail.com>
2018-04-12 13:59:05 +0100

    posts in ascending age order, red

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -8,4 +8,22 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+  scenario "posts are viewed in ascending age" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "I am post 1"
+    click_button "Submit"
+    click_link "New post"
+    fill_in "Message", with: "I am post 2"
+    click_button "Submit"
+    click_link "New post"
+    fill_in "Message", with: "I am post 3"
+    click_button "Submit"
+    alltext = page.body
+    i1 = alltext.index('I am post 1')
+    i2 = alltext.index('I am post 2')
+    i3 = alltext.index('I am post 3')
+    expect(i3 < i2).to eq true
+    expect(i2 < i1).to eq true
+  end
 end
