246889b87b18a7a6a5c08a9d133dd2fdef2da2ba
Thomas Duffy <tomjamesduffy@gmail.com>
2018-04-12 21:10:27 +0100

    Amended test so that it incorporates hidden delete button

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,7 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @post = current_user.posts.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
     redirect_to posts_url
--- spec/features/users_cannot_delete_others_posts_spec.rb
+++ spec/features/users_cannot_delete_others_posts_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 require_relative '../helpers/custom_helper'
 
 RSpec.describe "Timeline", type: :feature do
-  scenario "User wants to delete a post" do
+  scenario "User wants to delete a post that is not theirs" do
     sign_up
     create_post
     click_link "Logout"
     sign_up_user_2
-    click_link 'Delete Post'
-    expect(page).to have_content("Hello, world!")
+    expect(page).to_not have_link("Delete Post")
   end
 end
