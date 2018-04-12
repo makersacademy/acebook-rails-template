90ab271fb76de1eda436b7dd683e8fc4b0cfee0c
Raefe <raefe.nj@gmail.com>
2018-04-12 12:11:37 +0100

    passing tests, 100% coverage

--- spec/helpers/custom_helper.rb
+++ spec/helpers/custom_helper.rb
@@ -12,3 +12,24 @@ def create_comment
   fill_in "comment[body]", with: "Hello, Mars!"
   click_button "Comment"
 end
+
+def sign_up
+  visit "/users/sign_up"
+  fill_in "user[email]", with: "example@email.com"
+  fill_in "user[password]", with: "password"
+  fill_in "user[password_confirmation]", with: "password"
+  click_button "Sign up"
+end
+
+def sign_up_user_2
+  visit "/users/sign_up"
+  fill_in "user[email]", with: "example2@email.com"
+  fill_in "user[password]", with: "password123"
+  fill_in "user[password_confirmation]", with: "password123"
+  click_button "Sign up"
+end
+
+def create_liked_post
+  create_post
+  click_link "Like"
+end
