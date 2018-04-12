ba401fc5c3f0c99a30e8fbae42d83c045e220317
julesnuggy <julesnuggy@gmail.com>
2018-04-12 11:46:31 +0100

    Fixed comment editing - created edit_form and amended controller and class code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -23,20 +23,17 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
-    if @comment.update(update_comment_params)
-      redirect_to('/posts/' + update_comment_params[:post_id])
+    if @comment.update(comment_params)
+      redirect_to @post
     else
-      render 'commentsform'
+      render 'edit'
     end
   end
+
 
   private
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body, :post_id)
-  end
-
-  def update_comment_params
-    params.permit(:commenter, :body, :post_id)
+    params.require(:comment).permit(:commenter, :body)
   end
 end
