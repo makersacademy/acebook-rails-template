bad66dfa6c502b4988239dadc1db5b5b05e9454b
danielcampos <ddregalo@gmail.com>
2018-04-12 22:43:06 +0100

    Update feature tests for username input into helpers - tests passed 100%

--- spec/helpers/custom_helper.rb
+++ spec/helpers/custom_helper.rb
@@ -17,6 +17,7 @@ end
 
 def sign_up
   visit "/users/sign_up"
+  fill_in "user[username]", with: "testUser"
   fill_in "user[email]", with: "example@email.com"
   fill_in "user[password]", with: "password"
   fill_in "user[password_confirmation]", with: "password"
@@ -25,6 +26,7 @@ end
 
 def sign_up_user_2
   visit "/users/sign_up"
+  fill_in "user[username]", with: "testUserTwo"
   fill_in "user[email]", with: "example2@email.com"
   fill_in "user[password]", with: "password123"
   fill_in "user[password_confirmation]", with: "password123"
