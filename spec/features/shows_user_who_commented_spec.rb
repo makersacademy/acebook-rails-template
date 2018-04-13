70efc2056ab73c46108dbfc052a42e9cd897716c
George Drayson <drayson.g@mac.com>
2018-04-13 15:26:20 +0100

    Added the feature to show the user email of the commenter and poster (in future will need usernames)

---
+++ spec/features/shows_user_who_commented_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "Commenting", type: :feature do
+
+  before(:each) do
+    sign_up
+    add_post
+    click_link "View Comments"
+    fill_in "comment[content]", with: "I am comment#1"
+    click_button "Create Comment"
+  end
+
+  scenario "Can create comment and view immediately" do
+    expect(page).to have_content("I am comment#1 - test@test.com")
+  end
+  
+end
