99ae20b2059d1cd781ea253b099618a61401295a
Cindy <jl7e12@gmail.com>
2018-04-13 10:20:33 +0100

    fixed refresh to show comment issue

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,8 @@ class CommentsController < ApplicationController
   def create
     @comment = @post.comments.create(params[:comment].permit(:content))
     @comment.user_id = current_user.id
-    if @comment.save
+    @comment.save
       redirect_to post_path(@post)
-    end
-
   end
 
   private
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,4 @@
 class Comment < ApplicationRecord
-  # validates :content, presence: true
-
   belongs_to :post
   belongs_to :user
 end
