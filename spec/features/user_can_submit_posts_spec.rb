cffde1771d12539f916aa94472eb2c58844f2665
Gia1987 <giacomo.n27@gmail.com>
2018-04-13 16:55:14 +0100

    Fixed test

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
+require_relative '../helpers/helper'
 
-RSpec.feature "Timeline", type: :feature do
+describe "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
+    sign_up
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
