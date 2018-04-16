023aa783b3469f4d513324650cdd5016f87a770e
danielcampos <ddregalo@gmail.com>
2018-04-16 12:19:29 +0100

    Implement comment text-area slide down on button click

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,2 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+
+$('#comment_form').hide(); //Initially form wil be hidden.
+
+ $('#new_comment').click(function() {
+  $('#comment_form').slideToggle('slow');//Form shows on button click
+
+  });
+});
