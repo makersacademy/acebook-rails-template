b0769f0c2204a6afaf091fa9695add1c580abbec
Charlene Bastians <charlenebastians@hotmail.com>
2018-04-16 17:48:58 +0100

    tests to check you cant edit or delete someone elses comment

--- spec/features/editing_own_content_spec.rb
+++ spec/features/editing_own_content_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.feature 'users can only delete own comment', type: :feature do
+RSpec.feature 'users can only delete own post', type: :feature do
   scenario 'user signs in, posts, signs out, another user cant delete post' do
     sign_up
     add_new_post
@@ -10,5 +10,19 @@ RSpec.feature 'users can only delete own comment', type: :feature do
     sign_up_second_user
     expect(page).to have_content 'Hello, world!'
     expect(page).not_to have_selector "input[type=submit][value='Delete']"
+  end
+end
+
+RSpec.feature 'users can only delete own comment', type: :feature do
+  scenario 'user makes a comment, another user cant delete or edit that comment' do
+    sign_up
+    add_new_post
+    add_comment
+    sign_out
+    sign_up_second_user
+    click_button 'Show'
+    expect(page).to have_content 'Rails is awesome!'
+    expect(page).not_to have_link 'Delete Comment'
+    expect(page).not_to have_link 'Edit Comment'
   end
 end
