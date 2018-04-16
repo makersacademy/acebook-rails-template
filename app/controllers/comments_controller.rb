54b844ccdf0992997696ba4f43b902402a3f4d09
Tom Betts <tom.betts@live.co.uk>
2018-04-16 15:23:57 +0100

    Switch to using 'merge' in controllers rather than hidden tags in html files

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.create(comment_params.merge(commenter: current_user.username))
     redirect_to post_path(@post)
   end
 
@@ -33,6 +33,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:body)
   end
 end
