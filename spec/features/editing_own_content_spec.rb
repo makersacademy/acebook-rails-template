6901b52aef3e7fd4fe76115aa77401b5bbb28082
Charlene Bastians <charlenebastians@hotmail.com>
2018-04-16 16:08:54 +0100

    test checking user can only delete own post

---
+++ spec/features/editing_own_content_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'users can only delete own comment', type: :feature do
+  scenario 'user signs in, posts, signs out, another user cant delete post' do
+    sign_up
+    add_new_post
+    sign_out
+    sign_up_second_user
+    expect(page).to have_content 'Hello, world!'
+    expect(page).not_to have_selector "input[type=submit][value='Delete']"
+  end
+end
