fdacb4271f73fef3900462cf5d2d1088db56fcf0
Vivian Allen <mrvivianallen@gmail.com>
2018-04-17 11:24:19 +0100

    post timesort class method, red

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,9 @@ class Post < ApplicationRecord
   def owner?(user)
     self.user_id.to_i == user.id.to_i
   end
+
+  # class methods
+  # def self.time_sort
+  #   self.all.sort_by { |x| x.created_at }.reverse.each_with_index
+  # end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   let(:dbl_user) { double :user, id: 1 }
-  describe "#owner" do
+  describe "#owner?" do
     it "returns true when called with its owner" do
       post = Post.create(message: "test", user_id: 1)
       expect(post.owner?(dbl_user)).to be_truthy
@@ -10,6 +10,13 @@ RSpec.describe Post, type: :model do
     it "return false when called with a different owner" do
       post = Post.create(message: "test", user_id: 2)
       expect(post.owner?(dbl_user)).to be_falsey
+    end
+  end
+  describe "#self.timesort" do
+    it "returns a time-sorted list of posts" do
+      post1 = Post.create(message: "test", user_id: 1)
+      post2 = Post.create(message: "test2", user_id: 2)
+      expect(Post.timesort[0]).to equal(post2)
     end
   end
 end
