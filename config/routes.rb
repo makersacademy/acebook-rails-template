1214c65e84e164764408f934d2db5fd214ca39b5
Gia1987 <giacomo.n27@gmail.com>
2018-04-17 11:09:18 +0100

    Implemented post and  comment functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :comments
+  resources :posts
   devise_for :users
   # get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
-  resources :posts
+  root 'posts#index'
 end
