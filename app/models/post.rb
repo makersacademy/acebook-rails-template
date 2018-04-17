5aba82339cd78ec4fb6d4f6ae40f828cdf12e214
Vivian Allen <mrvivianallen@gmail.com>
2018-04-17 13:18:00 +0100

    post comment_count_string method, red->green

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,14 @@ class Post < ApplicationRecord
     self.user_id.to_i == user.id.to_i
   end
 
+  def like_count_string
+    "#{self.likes.length} #{self.likes.length ==1 ? "Like" : "Likes" }"
+  end
+
+  def comment_count_string
+    "#{self.comments.length} #{self.comments.length ==1 ? "Comment" : "Comments" }"
+  end
+
   # class methods
   def self.time_sort_all
     self.all.sort_by { |post| post.created_at }.reverse
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -12,6 +12,44 @@ RSpec.describe Post, type: :model do
       expect(post.owner?(dbl_user)).to be_falsey
     end
   end
+
+  describe "#like_count_string" do
+    it "returns singular string when given a single like" do
+      post = Post.create(message: "test", user_id: 1)
+      post.likes.create(user_id: 1)
+      expect(post.like_count_string).to eq('1 Like')
+    end
+    it "returns plural string when no likes" do
+      post = Post.create(message: "test", user_id: 1)
+      expect(post.like_count_string).to eq('0 Likes')
+    end
+    it "returns plural string when given multiple likes" do
+      post = Post.create(message: "test", user_id: 1)
+      post.likes.create(user_id: 1)
+      post.likes.create(user_id: 2)
+      expect(post.like_count_string).to eq('2 Likes')
+    end
+  end
+
+  describe "#comment_count_string" do
+    it "returns singular string when given a single like" do
+      post = Post.create(message: "test", user_id: 1)
+      post.comments.create(content: "test", post_id: 1, user_id: 1)
+      expect(post.comment_count_string).to eq('1 Comment')
+    end
+    it "returns plural string when no likes" do
+      post = Post.create(message: "test", user_id: 1)
+      expect(post.comment_count_string).to eq('0 Comments')
+    end
+    it "returns plural string when given multiple likes" do
+      post = Post.create(message: "test", user_id: 1)
+      post.comments.create(content: "test", post_id: 1, user_id: 1)
+      post.comments.create(content: "test2", post_id: 1, user_id: 1)
+      expect(post.comment_count_string).to eq('2 Comments')
+    end
+  end
+
+
   describe "#self.timesort" do
     it "returns a time-sorted list of posts" do
       post1 = Post.create(message: "test", user_id: 1)
