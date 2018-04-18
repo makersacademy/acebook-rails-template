c53b41ef2fc330e8618323db2841b120dc97335f
Gia1987 <giacomo.n27@gmail.com>
2018-04-18 10:53:07 +0100

    Removed title area from posts

--- db/migrate/20180418093831_create_posts.rb
+++ db/migrate/20180418093831_create_posts.rb
@@ -1,8 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :body
+      t.text :message
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417092143) do
+ActiveRecord::Schema.define(version: 20180418093831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,7 @@ ActiveRecord::Schema.define(version: 20180417092143) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
