6efc4b00e088747b24fcf5e01cbce5008c3d9ab2
Charlene Bastians <charlenebastians@hotmail.com>
2018-04-18 17:17:19 +0100

    profile page shows only users posts

--- spec/features/profile_page_spec.rb
+++ spec/features/profile_page_spec.rb
@@ -7,3 +7,18 @@ RSpec.feature 'can see profile link', type: :feature do
     expect(page).to have_content "Tom's Profile"
   end
 end
+
+RSpec.feature 'can see posts', type: :feature do
+  scenario 'only the users posts are shown on a profile page' do
+    sign_up
+    add_new_post
+    sign_out
+    sign_up_second_user
+    second_user_add_post
+    sign_out
+    sign_in
+    click_link 'Profile'
+    expect(page).to have_content 'Hello, world!'
+    expect(page).not_to have_content 'Makers Academy is awesome'
+  end
+end
