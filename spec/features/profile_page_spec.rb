202d964a3850df922f8eb0db0357c748379b22b2
Charlene Bastians <charlenebastians@hotmail.com>
2018-04-18 15:52:34 +0100

    link for profile page and redirected to users/show

--- spec/features/profile_page_spec.rb
+++ spec/features/profile_page_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature 'can see profile link', type: :feature do
-  scenario 'link takes you to a users profile'
-  sign_up
-  click_link 'Profile'
-  expect(page).to have_content "Tom's Profile"
+  scenario 'link takes you to a users profile' do
+    sign_up
+    click_link 'Profile'
+    expect(page).to have_content "Tom's Profile"
+  end
 end
