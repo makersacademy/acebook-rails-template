27abe66c36745b7bff521d14cb17613d681c3d5b
Gia1987 <giacomo.n27@gmail.com>
2018-04-18 13:58:05 +0100

    fixed bug : User can't edit/destroy other user's comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418104940) do
+ActiveRecord::Schema.define(version: 20180418124547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180418104940) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
