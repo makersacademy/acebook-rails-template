2d80c84a247ff49235e78da3219b17103699d3b0
Gia1987 <giacomo.n27@gmail.com>
2018-04-18 21:51:58 +0100

    fixed routes 'comments link'

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  get 'comments#index'
+  get 'comments/index'
 end
 #forheroku
