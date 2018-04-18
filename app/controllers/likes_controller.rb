09bb48b97aaa0f57e0b8ea529e6e387a3dd7cb35
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-18 15:08:44 +0100

    refactored code

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
   before_action :set_like
+
   def create
     if @like
       @like.update_attribute(:liked, true)
@@ -10,35 +11,31 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    if @like
-      @like.update_attribute(:liked, false)
-    end
+    @like.update_attribute(:liked, false)
     redirect_to posts_url
   end
 
   private
 
-    def post_or_comment(params)
-      if params[:comment_id]
-        return Comment.find_by(id: params[:comment_id])
-      else
-        return Post.find_by(id: params[:post_id])
-      end
+  def post_or_comment(params)
+    if params[:comment_id]
+      return Comment.find_by(id: params[:comment_id])
+    else
+      return Post.find_by(id: params[:post_id])
     end
+  end
 
-    def create_like(params)
-      @parent_object = post_or_comment(params)
-      @like = @parent_object.likes.build(user_id: current_user.id, liked: true)
-      @like.save!
+  def create_like(params)
+    @parent_object = post_or_comment(params)
+    @like = @parent_object.likes.build(user_id: current_user.id, liked: true)
+    @like.save!
+  end
+
+  def set_like
+    if params[:post_id]
+      @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    else
+      @like = Like.find_by(user_id: current_user.id, comment_id: params[:comment_id])
     end
-
-    def set_like
-      if params[:post_id]
-        @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
-      else
-        @like = Like.find_by(user_id: current_user.id, comment_id: params[:comment_id])
-      end
-    end
-
-
+  end
 end
