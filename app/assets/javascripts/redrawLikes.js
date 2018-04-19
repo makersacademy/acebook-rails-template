3bb8e0e27d726e80e90b23b3f723b76ae43b78d3
Alexandra McCarroll <34590557+AlexMcCarroll@users.noreply.github.com>
2018-04-19 11:58:56 +0100

    Update redrawLikes.js

--- app/assets/javascripts/redrawLikes.js
+++ app/assets/javascripts/redrawLikes.js
@@ -1,9 +1,7 @@
 function redrawLikes(wrapperID, url, linkID) {
-  console.log('function called')
   $('#' + linkID).ajaxSuccess(redraw(wrapperID, url));
   function redraw(wrapperID, url) {
     setTimeout(function(){
-      console.log('callback called')
       $('#' + wrapperID).load(url + ' #' + wrapperID);
     }, 100);
   };
