4cc0b86ab583878ad062def337b5a79a7c0d9662
Vivian Allen <mrvivianallen@gmail.com>
2018-04-19 11:54:03 +0100

    functional using timeout and javascrip ajax

--- app/assets/javascripts/redrawLikes.js
+++ app/assets/javascripts/redrawLikes.js
@@ -1,3 +1,10 @@
-function redrawLikes(divID, url) {
-  $('#' + divID).load(url + ' #' + divID.replace("_wrapper", ""));
+function redrawLikes(wrapperID, url, linkID) {
+  console.log('function called')
+  $('#' + linkID).ajaxSuccess(redraw(wrapperID, url));
+  function redraw(wrapperID, url) {
+    setTimeout(function(){
+      console.log('callback called')
+      $('#' + wrapperID).load(url + ' #' + wrapperID);
+    }, 100);
+  };
 };
