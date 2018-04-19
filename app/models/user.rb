ac46d3947e29b8e4229bd18418532e74cf1fbe25
Charlene Bastians <charlenebastians@hotmail.com>
2018-04-19 12:52:46 +0100

    resizing fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: "avatar_:style.png"
+  has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#"}, default_url: "avatar_:style.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
