43723efd274ac43ccd85b6ff19f0592c651a120c
Reenakoudi <kreena92@gmail.com>
2018-04-19 11:49:10 +0100

    Refactored code with right identation

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -18,27 +18,25 @@ class LikesController < ApplicationController
 
   private
 
-    def post_or_comment(params)
-      if params[:comment_id]
-        return Comment.find_by(id: params[:comment_id])
-      else
-        return Post.find_by(id: params[:post_id])
-      end
+  def post_or_comment(params)
+    if params[:comment_id]
+      return Comment.find_by(id: params[:comment_id])
+    else
+      return Post.find_by(id: params[:post_id])
     end
+  end
 
-    def create_like(params)
-      @parent_object = post_or_comment(params)
-      @like = @parent_object.likes.build(user_id: current_user.id, liked: true)
-      @like.save!
+  def create_like(params)
+    @parent_object = post_or_comment(params)
+    @like = @parent_object.likes.build(user_id: current_user.id, liked: true)
+    @like.save!
+  end
+
+  def set_like
+    if params[:post_id]
+      @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    else
+      @like = Like.find_by(user_id: current_user.id, comment_id: params[:comment_id])
     end
-
-    def set_like
-      if params[:post_id]
-        @like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
-      else
-        @like = Like.find_by(user_id: current_user.id, comment_id: params[:comment_id])
-      end
-    end
-
-
+  end
 end
