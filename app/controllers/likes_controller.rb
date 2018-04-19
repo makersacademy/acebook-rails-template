c78523953afe6cac61cbd282d73d94ca70fa81b1
Reenakoudi <kreena92@gmail.com>
2018-04-19 14:40:23 +0100

    Refactoring

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -10,7 +10,9 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    @like&.@like.update_attribute(:liked, false)
+    if @like 
+      @like.update_attribute(:liked, false)
+    end
     redirect_to posts_url
   end
 
