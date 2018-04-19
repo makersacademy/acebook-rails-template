c39878ec98cd32b2bf8536b2ac3a0d7e127fc2b7
Cindy <jl7e12@gmail.com>
2018-04-19 14:57:38 +0100

    Added asw gem and aws.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,11 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :bucket => "elasticbeanstalk-eu-west-2-862365935758"
+}
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
