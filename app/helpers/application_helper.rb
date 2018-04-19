415540e24ba50ba733ba9d94bfa52c10538e089e
Alfie Darko <me@alfiedarko.co.uk>
2018-04-19 17:11:06 +0100

    added emoji support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def emojify(content)
+    if content.present?
+      h(content).to_str.gsub(/:([\w+-]+):/) do |match|
+        if emoji = Emoji.find_by_alias(Regexp.last_match(1))
+          %(<img alt="#{Regexp.last_match(1)}" src="#{image_path("emoji/#{emoji.image_filename}")}" style="vertical-align:middle" width="20" height="20" />)
+        else
+          match
+        end
+      end.html_safe
+    end
+end
 end
