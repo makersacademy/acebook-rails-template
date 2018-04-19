a0297310a779fea7648bee685a98e2d5bf352ad1
Alfie Darko <me@alfiedarko.co.uk>
2018-04-19 16:20:20 +0100

    created new personal message view, added turbolinks gemfile

--- app/controllers/personal_messages_controller.rb
+++ app/controllers/personal_messages_controller.rb
@@ -3,14 +3,21 @@
 class PersonalMessagesController < ApplicationController
   before_action :find_conversation!
 
+  def new
+    redirect_to(conversation_path(@conversation)) && return if @conversation
+    @personal_message = current_user.personal_messages.build
+  end
+
   def create
+    @conversation ||= Conversation.create(author_id: current_user.id,
+                                          receiver_id: @receiver.id)
     @personal_message = current_user.personal_messages.build(personal_message_params)
     @personal_message.conversation_id = @conversation.id
     @personal_message.save!
 
     flash[:success] = 'Your message was sent!'
     redirect_to conversation_path(@conversation)
-  end
+end
 
   private
 
@@ -19,7 +26,13 @@ class PersonalMessagesController < ApplicationController
   end
 
   def find_conversation!
-    @conversation = Conversation.find_by(id: params[:conversation_id])
-    redirect_to(root_path) && return unless @conversation&.participates?(current_user)
+    if params[:receiver_id]
+      @receiver = User.find_by(id: params[:receiver_id])
+      redirect_to(root_path) && return unless @receiver
+      @conversation = Conversation.between(current_user.id, @receiver.id)[0]
+    else
+      @conversation = Conversation.find_by(id: params[:conversation_id])
+      redirect_to(root_path) && return unless @conversation&.participates?(current_user)
+    end
   end
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -9,6 +9,10 @@ class Conversation < ApplicationRecord
     where('(conversations.author_id = ? OR conversations.reciever_id = )', user.id, user.id)
   end
 
+  scope :between, ->(sender_id, receiver_id) do
+                    where(author_id: sender_id, receiver_id: receiver_id).or(where(author_id: receiver_id, receiver_id: sender_id)).limit(1)
+                  end
+
   def with(current_user)
     author == current_user ? receiver : author
   end
