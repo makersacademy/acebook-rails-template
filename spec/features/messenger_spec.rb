8e92bd06f59b1e3edad54fd0e63354d64fe117e6
Alfie Darko <me@alfiedarko.co.uk>
2018-04-19 14:52:13 +0100

    passing rspec test for Messenger > Users can check whether a specific friend is online

--- spec/features/messenger_spec.rb
+++ spec/features/messenger_spec.rb
@@ -8,4 +8,12 @@ RSpec.feature 'Messenger', type: :feature do
     click_button 'Messenger'
     expect(page).to have_content 'Users Online'
   end
+
+  scenario 'User can check whether a specific friend is online' do
+    sign_up
+    sign_out
+    sign_up2
+    click_button 'Messenger'
+    expect(page).to have_content 'Tom | send a message'
+  end
 end
