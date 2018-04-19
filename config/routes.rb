f51bf5e34867141fbd998172d927fa9d17ebe8fc
paosch <paola_schiaffino@yahoo.co.uk>
2018-04-19 11:36:13 +0100

    Added tests and changed some routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   devise_for :users
-  # get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'posts#index'
   get 'comments/index'
 end
