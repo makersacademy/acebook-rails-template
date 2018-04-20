35601db193d5c5d5eb6b0446d92e0f2047cce700
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-20 13:09:09 +0100

    fixed aws bucket

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   storage: :s3,
+  s3_region: ENV.fetch('AWS_REGION'),
   s3_credentials: {
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
