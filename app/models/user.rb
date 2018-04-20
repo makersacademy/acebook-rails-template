6143b86dfcb55163e208e000a7c7e44e52c2d6d7
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-20 15:26:03 +0100

    changing default picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" } , default_url: "capybara.jpg"
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" } , default_url: "pinkfish.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
   devise :database_authenticatable, :registerable,
