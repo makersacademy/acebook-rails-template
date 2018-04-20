8b80b344a46ffc634d62a5ce712150ebd90f258e
Alfie Darko <me@alfiedarko.co.uk>
2018-04-20 07:41:38 +0100

    added appearance channel serverside implementation

---
+++ app/channels/appearance_channel.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class AppearancesChannel < ApplicationCable::Channel
+  def subscribed
+    redis.set("user_#{current_user.id}_online", '1')
+    stream_from('appearances_channel')
+    ActionCable.server.broadcast 'appearances_channel',
+                                 user_id: current_user.id,
+                                 online: true
+  end
+
+  def unsubscribed
+    redis.del("user_#{current_user.id}_online")
+    ActionCable.server.broadcast 'appearances_channel',
+                                 user_id: current_user.id,
+                                 online: false
+  end
+
+  private
+
+  def redis
+    Redis.new
+  end
+end
