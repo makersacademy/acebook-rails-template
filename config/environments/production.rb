caca8428f0d5655ebf0ea65ac3282f20a66d4e9f
Vivian Allen <mrvivianallen@gmail.com>
2018-04-21 08:09:47 +0100

    square brackets in paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,10 +33,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV('S3_BUCKET_NAME'),
-      access_key_id: ENV('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV('AWS_REGION'),
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      s3_region: ENV['AWS_REGION'],
       # endpoint: ENV.fetch('AWS_ENDPOINT')
     },
     s3_host_name: "s3-eu-west-2.amazonaws.com",
