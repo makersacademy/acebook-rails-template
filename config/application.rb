0291e7bb2cbd97e3bd2925b3e56aa3a68480dce3
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-21 18:14:23 +0100

    refactoring code

--- config/application.rb
+++ config/application.rb
@@ -23,14 +23,13 @@ module Acebook
     end
 
     ActionMailer::Base.smtp_settings = {
-      :address => 'smtp.gmail.com',
-      :domain => 'mail.google.com',
-      :port => 587,
-      :user_name => ENV['GMAIL_USERNAME'],
-      :password => ENV['GMAIL_PASSWORD'],
-      :authentication => 'login',
-      :enable_starttls_auto => true
+      address: 'smtp.gmail.com',
+      domain: 'mail.google.com',
+      port: 587,
+      user_name: ENV['GMAIL_USERNAME'],
+      password: ENV['GMAIL_PASSWORD'],
+      authentication: 'login',
+      enable_starttls_auto: true
     }
-
   end
 end
