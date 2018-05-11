e8a100ef63175d7f9b3aa5fc3223ee717fa9d26e
Daniel Miller <danielmiller5791@gmail.com>
2018-05-11 14:34:13 +0100

    updated database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
-DatabaseCleaner.strategy = :truncation
+DatabaseCleaner[:active_record].strategy = :truncation
 
 RSpec.configure do |config|
   
