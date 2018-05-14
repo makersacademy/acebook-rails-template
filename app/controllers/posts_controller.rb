2d291201064b9cf082b254c14372345d4d35f6ad
Jihin Ip <jim_ip20@hotmail.com>
2018-05-14 17:01:41 +0100

    Adding Edit routes, db manipulation. Adding links to edit at index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,16 @@ class PostsController < ApplicationController
     redirect_to posts_url
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post = Post.update(post_params)
+    redirect_to posts_url
+  end
+
   def index
     @posts = Post.all
   end
