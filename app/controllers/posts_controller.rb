8afbec33c39366c1a0fd1282ab8353f33b582b90
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-14 12:16:35 +0100

    post before action authenticate user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, :except => [:index, :show]
+  before_action :authenticate_user!
 
   def new
     @post = current_user.posts.new
