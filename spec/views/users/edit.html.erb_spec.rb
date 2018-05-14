fb44dac41a9c1b3a58931a796d785d45838ac1d8
Daniel Tooke <daniel.tooke@gmail.com>
2018-05-14 15:38:07 +0100

    Added users MVC and password encryption

---
+++ spec/views/users/edit.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "users/edit", type: :view do
+  before(:each) do
+    @user = assign(:user, User.create!(
+      :name => "MyString",
+      :email => "MyString",
+      :password_digest => "MyString"
+    ))
+  end
+
+  it "renders the edit user form" do
+    render
+
+    assert_select "form[action=?][method=?]", user_path(@user), "post" do
+
+      assert_select "input[name=?]", "user[name]"
+
+      assert_select "input[name=?]", "user[email]"
+
+      assert_select "input[name=?]", "user[password_digest]"
+    end
+  end
+end
---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "users/new", type: :view do
+  before(:each) do
+    assign(:user, User.new(
+      :name => "MyString",
+      :email => "MyString",
+      :password_digest => "MyString"
+    ))
+  end
+
+  it "renders new user form" do
+    render
+
+    assert_select "form[action=?][method=?]", users_path, "post" do
+
+      assert_select "input[name=?]", "user[name]"
+
+      assert_select "input[name=?]", "user[email]"
+
+      assert_select "input[name=?]", "user[password_digest]"
+    end
+  end
+end
