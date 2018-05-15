5af733ee7aa553b97a167d8fa75b8e22786fc941
Shih Han <swang05@mail.bbk.ac.uk>
2018-05-15 11:35:17 +0100

    link comment to user and post and routing

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
 
   validates :user_id, presence: true
   belongs_to :user
-
+  has_many :comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root(to: "posts#index")
-  resources(:posts)
+  
+  resources :posts do
+    resources :comments
+  end
+
 end
