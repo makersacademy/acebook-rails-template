f15ebd8a9be79dcc9162dc147b4a5a52b9e27e03
Daniel Tooke <daniel.tooke@gmail.com>
2018-05-15 12:15:01 +0100

    Added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence:true,
+  length: {minimum: 6}
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
