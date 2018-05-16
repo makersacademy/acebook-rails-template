640b8ed27b377cb5b68ce4abb5e2417689c0e1fc
marydomashneva <mary.domashneva@gmail.com>
2018-05-16 11:00:04 +0100

    all present and worked on minor css and other changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @user_posts = @user.posts
+    @user_posts = @user.posts.order('updated_at DESC')
   end
 end
