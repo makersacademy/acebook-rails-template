692b8bd0ab34323dee1b851b842210fe97434b0b
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-05-17 17:38:05 +0100

    logging in redirects to users wall

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
     @user = User.find(params[:id])
     @posts = @user.wall.posts
+  end
+
+  def index
+    redirect_to user_path(current_user)
   end
 end
