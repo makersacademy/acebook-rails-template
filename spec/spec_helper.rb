39c76ef94dff88aa501c915a0b5c99e74836b2bd
Patryk Pilecki <ppilecki@icloud.com>
2018-05-18 11:48:24 +0100

    filter for irrelevant files in rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,12 @@ SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
   add_filter '/spec/' # for rspec
+  add_filter '/app/helpers/'
+  add_filter '/app/channels/application_cable/'
+  add_filter '/app/jobs/'
+  add_filter '/app/mailers/application_mailer.rb'
+  add_filter '/app/models/application_record.rb'
+  add_filter '/app/models/user.rb'
 end
 
 
