ce7c22249c3bb25882a5a27f67f6a89e114982b2
Patryk Pilecki <ppilecki@icloud.com>
2018-05-18 11:15:18 +0100

    Tests for, number of likes below the post, unlike feature

--- spec/features/user_can_like_posts_spec.rb
+++ spec/features/user_can_like_posts_spec.rb
@@ -7,4 +7,26 @@ RSpec.feature "Likes", type: :feature do
     click_link "Like"
     expect(page).to have_content("Unlike")
   end
+  scenario "Number of likes showed below the post" do
+    signup_and_login
+    create_dummy_post
+    click_link "Like"
+    visit '/'
+    click_link "username-dropdown"
+    click_link "Logout"
+    signup_and_login_alt_user
+    click_link "View Posts"
+    click_link "Like"
+    expect(page).to have_content("2 people like this")
+  end
+  scenario "User can unlike post" do
+    signup_and_login
+    create_dummy_post
+    click_link "Like"
+    visit '/'
+    click_link "View Posts"
+    click_link "Unlike"
+    expect(page).to have_content("Like")
+    expect(page).not_to have_content("1 person likes this")
+  end
 end
