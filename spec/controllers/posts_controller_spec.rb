5f4b71c059354917191e421999487c51f9c4b13f
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-07 10:48:16 +0100

    testing homepage redirects to sign in page

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,35 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  context 'User is signed in' do
 
-  before(:each) do
-    user = double(:user)
-    allow(request.env['warden']).to receive(:authenticate!).and_return(user)
-  end
+    before(:each) do
+      user = double(:user)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+    end
 
-  describe "GET /new " do
-    it "responds with 200" do
-      get :new
-      expect(response).to have_http_status(200)
+    describe "GET /new " do
+      it "responds with 200" do
+        get :new
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "POST /" do
+      it "responds with 200" do
+        post :create, params: { post: { message: "Hello, world!" } }
+        expect(response).to redirect_to(posts_url)
+      end
+
+      it "creates a post" do
+        post :create, params: { post: { message: "Hello, world!" } }
+        expect(Post.find_by(message: "Hello, world!")).to be
+      end
+    end
+
+    describe "GET /" do
+      it "responds with 200" do
+        get :index
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
-  describe "POST /" do
-    it "responds with 200" do
-      post :create, params: { post: { message: "Hello, world!" } }
-      expect(response).to redirect_to(posts_url)
-    end
-
-    it "creates a post" do
-      post :create, params: { post: { message: "Hello, world!" } }
-      expect(Post.find_by(message: "Hello, world!")).to be
-    end
-  end
-
-  describe "GET /" do
-    it "responds with 200" do
-      get :index
-      expect(response).to have_http_status(200)
+  context 'User is not signed in' do
+    describe 'GET /' do
+      it "redirects page" do
+        get :index
+        expect(response).to have_http_status(302)
+      end
+      it "prompts user to sign in" do
+        get :index
+        expect(response).to redirect_to(new_user_session_url)
+      end
     end
   end
 end
