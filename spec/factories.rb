094d5cba7a4f8cacd9458630840f1d86f45410ca
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-08 15:49:22 +0100

    Update tests with name and username

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :user do
     email 'example@hotmail.com'
+    first_name 'Clint'
+    last_name 'Pondsworth'
+    username 'GoldFish'
     password '123456'
   end
   factory :post do
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,9 @@
 def sign_up_and_sign_in
   visit "/posts"
   click_link "Sign up"
+  fill_in "First name", with: 'Clint'
+  fill_in "Last name", with: 'Pondsworth'
+  fill_in "Username", with: 'GoldFish'
   fill_in "Email", with: "example@hotmail.com"
   fill_in "Password", with: "123456"
   fill_in "Password confirmation", with: "123456"
