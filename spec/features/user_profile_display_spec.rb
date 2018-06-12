2eea47158eb0e69195f7d50367ebfbddf9ebf522
Robert <fishwickrob@live.com>
2018-06-12 14:17:16 +0100

    whatever I failed to push before lunch

--- spec/features/user_profile_display_spec.rb
+++ spec/features/user_profile_display_spec.rb
@@ -1,8 +1,9 @@
-# require 'rails_helper'
-#
-# RSpec.feature "Display username", type: :feature do
-#   scenario "User can see their username" do
-#     sign_up_and_sign_in
-#     click
-#   end
-# end
+require 'rails_helper'
+
+RSpec.feature "Display username", type: :feature do
+  scenario "User can see their username" do
+    sign_up_and_sign_in
+    click_link "Profile"
+    expect(page).to have_content('Clint Pondsworth')
+  end
+end
