fca26ffadf85455ccb86de3994ce8ccce6d796eb
Revilo1882 <oliverrharris@gmail.com>
2018-06-12 15:02:36 +0100

    updated tests

--- spec/features/user_edit_profile_spec.rb
+++ spec/features/user_edit_profile_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can go to edit profile page" do
     sign_up_and_sign_in
-    click_link "Edit Profile"
+    click_link "Settings"
     expect(page).to have_content('Edit User')
   end
 end
