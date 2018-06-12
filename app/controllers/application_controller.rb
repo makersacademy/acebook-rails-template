29b6b049cc0ab02572e3d4cc8611f63c0a4fb386
Revilo1882 <oliverrharris@gmail.com>
2018-06-12 16:04:51 +0100

    adds phot to user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :sign_up,
-      keys: [:first_name, :last_name, :username, :email,
+      keys: [:first_name, :last_name, :username, :email, :image,
             :password, :confirm_password])
     devise_parameter_sanitizer.permit(:sign_in,
       keys: [:email, :password, :remember_me])
     devise_parameter_sanitizer.permit(:account_update,
-      keys: [:first_name, :last_name, :username, :email,
+      keys: [:first_name, :last_name, :username, :email, :image,
             :password, :confirm_password, :remember_me])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   validates :username, :presence => true
 
   has_many :posts
+
+  has_attached_file :image,
+  styles: { large: "600x600>", medium: "300x300>", thumb: "150x150" },
+  :default_url => 'fishbook-logo.png'
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
 end
