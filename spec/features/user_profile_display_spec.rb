6648381fbe0b8905711182a9e815e676e47ef1f4
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-13 11:49:31 +0100

    Fix failing test

--- spec/features/user_profile_display_spec.rb
+++ spec/features/user_profile_display_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Display username", type: :feature do
   scenario "User can see their username" do
     sign_up_and_sign_in
     click_link "Profile"
-    expect(page).to have_content('Clint GoldFish Pondsworth')
+    expect(page).to have_content('Clint Pondsworth')
   end
 
   scenario "User can click on antoher user's name to see their profile" do
