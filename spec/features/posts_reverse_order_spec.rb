5e0b0653e56ee82d3dff83fe8e4a73d132122882
luciagore <lucy.gore@hotmail.co.uk>
2018-07-02 17:36:50 +0100

    Refactored

--- spec/features/posts_reverse_order_spec.rb
+++ spec/features/posts_reverse_order_spec.rb
@@ -1,17 +1,9 @@
 require 'rails_helper'
+require 'create_three_posts'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Posts appear in reverse order" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "First post"
-    click_button "Submit"
-    click_link "New post"
-    fill_in "Message", with: "Second post"
-    click_button "Submit"
-    click_link "New post"
-    fill_in "Message", with: "Third post"
-    click_button "Submit"
+    create_three_posts
     expect(page).to have_selector("ul li:nth-child(1)", text: "Third post")
     expect(page).to have_selector("ul li:nth-child(3)", text: "First post")
   end
--- spec/features/posts_show_date_created_spec.rb
+++ spec/features/posts_show_date_created_spec.rb
@@ -1,16 +1,13 @@
 require 'rails_helper'
+require 'create_post'
 
 RSpec.feature "Timeline", type: :feature do
-    before do
-        now = Time.zone.parse("Apr 17, 2018, 12:00PM")
-        Timecop.freeze(now)
-    end
+  before do
+    now = Time.zone.parse("Apr 17, 2018, 12:00PM")
+    Timecop.freeze(now)
+  end
   scenario "Posts can have multiple lines" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!\n This is a post with multiple \n lines"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!\n This is a post with multiple \n lines")
+    create_post
     expect(Time.zone.now).to have_content("2018-04-17 12:00:00 UTC")
   end
 end
