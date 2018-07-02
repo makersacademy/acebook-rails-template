d5168ddadf5c44d422bf6d17a7bb2d6f0234f841
Simone <simonecaplin@gmail.com>
2018-07-02 16:49:22 +0100

    Validate email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  validates :name, :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name, presence: true, length: { maximum: 50}
+  validates :email, presence: true, length: { maximum: 50}, format: { with: VALID_EMAIL_REGEX }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,4 +15,34 @@ RSpec.describe User, type: :model do
     user = User.new(name: "Example user", email: "")
     expect(user).not_to be_valid
   end
+
+  it 'name should not be too long' do
+    user = User.new(name: "Example user", email: "user@email.com")
+    user.name = "a" * 51
+    expect(user).not_to be_valid
+  end
+
+  it 'email should not be too long' do
+    user = User.new(name: "Example user", email: "user@email.com")
+    user.email = "user" * 51 + "@email.com"
+    expect(user).not_to be_valid
+  end
+
+  it 'email validation should accept valid addresses' do
+    user = User.new(name: "Example user", email: "user@email.com")
+    valid_addresses = %w[hello@email.com GOODBYE@example.com test_1234@mail.com]
+    valid_addresses.each do |valid_address|
+      user.email = valid_address
+      expect(user).to be_valid
+    end
+  end
+
+  it 'email validation should not accept invalid addresses' do
+    user = User.new(name: "Example user", email: "user@email.com")
+    valid_addresses = %w[helloemail.com GOODBYE&example.com test_1234@mail user@example,com]
+    valid_addresses.each do |valid_address|
+      user.email = valid_address
+      expect(user).not_to be_valid
+    end
+  end
 end
