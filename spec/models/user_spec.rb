23a0eac08b0d7c0860f5451aff522d24b0f725fe
Simone <simonecaplin@gmail.com>
2018-07-02 17:25:28 +0100

    Refactor user_spec to create new user in a before block

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before(:each) do
+    @user = User.new(name: "Example user", email: "user@email.com")
+  end
+
+  let(:user) {@user}
+
   it 'user input should be valid' do
-    user = User.new(name: "Example user", email: "user@email.com")
     expect(user).to be_valid
   end
 
   it 'name should be present' do
-    user = User.new(name: "", email: "user@email.com")
+    user.name = ""
     expect(user).not_to be_valid
   end
 
   it 'email should be present' do
-    user = User.new(name: "Example user", email: "")
+    user.email = ""
     expect(user).not_to be_valid
   end
 
   it 'name should not be too long' do
-    user = User.new(name: "Example user", email: "user@email.com")
     user.name = "a" * 51
     expect(user).not_to be_valid
   end
 
   it 'email should not be too long' do
-    user = User.new(name: "Example user", email: "user@email.com")
     user.email = "user" * 51 + "@email.com"
     expect(user).not_to be_valid
   end
 
   it 'email validation should accept valid addresses' do
-    user = User.new(name: "Example user", email: "user@email.com")
     valid_addresses = %w[hello@email.com GOODBYE@example.com test_1234@mail.com]
     valid_addresses.each do |valid_address|
       user.email = valid_address
@@ -38,7 +40,6 @@ RSpec.describe User, type: :model do
   end
 
   it 'email validation should not accept invalid addresses' do
-    user = User.new(name: "Example user", email: "user@email.com")
     valid_addresses = %w[helloemail.com GOODBYE&example.com test_1234@mail user@example,com]
     valid_addresses.each do |valid_address|
       user.email = valid_address
@@ -47,7 +48,6 @@ RSpec.describe User, type: :model do
   end
 
   it 'email addresses should be unique' do
-    user = User.new(name: "Example user", email: "user@email.com")
     duplicate_user = user.dup
     user.save
     duplicate_user.email = user.email.upcase
