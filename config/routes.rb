ae588836273f95c65f5be1a6f80eab04a78c4521
Robert Faldo <robertfaldo@gmail.com>
2018-07-03 14:54:16 +0100

    User registration and login working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#index'
-  resources :users
+  # resources :users
   resources :posts
 end
