c05bb66b7e50451e7e0e19cec3d91b8c9ad4f9f4
Simone <simonecaplin@gmail.com>
2018-07-03 16:32:52 +0100

    Implement user log-out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,11 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
