6b5245a4b9130edaf8e350c1123b8b704cb55344
Nick <nicholasjohnrichardson@gmail.com>
2018-07-03 10:50:42 +0100

    added signup form and error messages partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :users
   resources :posts
 end
