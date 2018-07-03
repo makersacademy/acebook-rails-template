8672a091c210eb2ce1b193ce397d2f9253d3d05f
luciagore <lucy.gore@hotmail.co.uk>
2018-07-03 15:00:31 +0100

    Add signin form and changed homepage link

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   get '/', to: 'homepage#index'
   get '/signup', to: 'users#new'
 
