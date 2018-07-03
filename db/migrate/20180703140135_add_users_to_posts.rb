291f7d1da922552272327388163a6a6dc44904ae
Nick <nicholasjohnrichardson@gmail.com>
2018-07-03 16:10:47 +0100

    added travis

---
+++ db/migrate/20180703140135_add_users_to_posts.rb
@@ -0,0 +1,5 @@
+class AddUsersToPosts < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :posts, :users, foreign_key: true
+  end
+end
---
+++ db/migrate/20180703140655_remove_column.rb
@@ -0,0 +1,5 @@
+class RemoveColumn < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :posts, :users_id
+  end
+end
