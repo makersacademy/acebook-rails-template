ab45f4382ddf1e71b321044dc8d61b92e2b39d1f
luciagore <lucy.gore@hotmail.co.uk>
2018-07-04 10:56:40 +0100

    Added tests for posts and refactored

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "sessions/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  # it 'finds the title of the page which is Sign up' do
+  #   assert_select 'title', 'Sign up'
+  # end
 end
