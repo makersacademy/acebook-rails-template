9521b848fe25316c1d1acf4c7830388584cdcd8e
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-07-04 14:40:41 +0100

    remove user column from post database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,6 @@ class PostsController < ApplicationController
   def index
     @post = Post.new
     @posts = Post.all
-    @user = User.new
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180704113214) do
+ActiveRecord::Schema.define(version: 20180704133737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20180704113214) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user"
   end
 
   create_table "users", force: :cascade do |t|
