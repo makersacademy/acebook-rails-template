5a01246ac7ec9a1365dc068b3603c71e99ff5a31
Matthew <mdwareing@gmail.com>
2018-07-05 15:35:54 +0100

    Prevent user from accessing content without logging in

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,6 @@
 class HomepageController < ApplicationController
+    skip_before_action :require_login
+    
     def index
       
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  skip_before_action :require_login
   def new
     @user = User.new
   end
