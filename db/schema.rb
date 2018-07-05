b4f6e16c5b4b19ccb0d6b8efe80bda5f135d04a3
Simone <simonecaplin@gmail.com>
2018-07-05 16:24:52 +0100

    Fix tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180703140655) do
+ActiveRecord::Schema.define(version: 20180704143122) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180703140655) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "posts", "users"
