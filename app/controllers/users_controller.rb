ede050a876ad0d3f8e27c58d9cd5b259de7c95c1
Robert Faldo <robertfaldo@gmail.com>
2018-07-05 14:22:00 +0100

    uploading image to the public folder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    # @user = User.find(params[:id])
+    p "params[:user][:avatar]"
+    p params[:file]
+    p "before current_user"
+    p current_user
+    p "after current_user"
+    current_user.avatar = params[:user][:avatar]
+    p "current_user after assigning"
+    p current_user.avatar
+    p "end"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:avatar)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # The way to tell CarrierWave to associate the image
+  # with a model is to use the mount_uploader method, 
+  # which takes as arguments a symbol representing the
+  # attribute and the class name of the generated uploader:
+  mount_uploader :avatar, AvatarUploader
 end
