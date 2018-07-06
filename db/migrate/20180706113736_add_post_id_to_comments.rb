1555af7d11e57b542dd1b947914cc8f1eb07af91
Simone <simonecaplin@gmail.com>
2018-07-06 15:01:08 +0100

    Edit migrate file

--- db/migrate/20180706113736_add_post_id_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddPostIdToComments < ActiveRecord::Migration[5.1]
-  def change
-    add_column :comments, :post_id, :bigint
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -42,5 +42,19 @@ ActiveRecord::Schema.define(version: 20180706130207) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  create_table "votes", id: :serial, force: :cascade do |t|
+    t.string "votable_type"
+    t.integer "votable_id"
+    t.string "voter_type"
+    t.integer "voter_id"
+    t.boolean "vote_flag"
+    t.string "vote_scope"
+    t.integer "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+  end
+
   add_foreign_key "posts", "users"
 end
