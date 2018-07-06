a349f0a0ee8787d8556570e0669c934df6ab96ca
Nick <nicholasjohnrichardson@gmail.com>
2018-07-06 10:46:03 +0100

    added migration file to delete posts with no user id

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,15 @@
 class LikesController < ApplicationController
 
-  # before_action :find_likeable
-
   def create
-    puts current_user
-    puts 'reached here'
-    puts params
     @post = Post.find(params[:post_id])
     @post.liked_by @post.user # find at some point how to find the post for the current user
     redirect_to user_posts_path(@post.user)
   end
-  #
-  # def destroy
-  #   @likeable.disliked_by current_user
-  # end
-  #
-  # private
-  # def find_likeable
-  #   @likeable_type = params[:likeable_type].classify
-  #   @likeable = @likeable_type.constantize.find(params[:likeable_id])
-  # end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @post.disliked_by @post.user # find at some point how to find the post for the current user
+    redirect_to user_posts_path(@post.user)
+  end
 
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,7 @@ module SessionsHelper
 
   def log_in(user)
     session[:user_id] = user.id
+    @current_user = session[:user_id]
   end
 
   def log_out
