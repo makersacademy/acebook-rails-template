a1b7d3845a38892924c7a6976fc8db0bba0ec11d
Robert Faldo <robertfaldo@gmail.com>
2018-07-06 13:00:43 +0100

    Add test for adding profile pic

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  # resources :users, :only => [:show, :update]
   resources :users
   resources :posts
 end
---
+++ spec/features/user_can_upload_profile_picture_spec.rb
@@ -0,0 +1,12 @@
+RSpec.feature "User can view their profile", type: :feature do
+  scenario 'successfuly' do
+    sign_up_successfully
+    click_link 'View profile'
+    attach_file('user[avatar]', 'pic_for_test.30.03.png')
+    click_button 'Update'
+    visit current_path
+    save_and_open_page
+    expect(page).to have_content("Type of Animal:")
+    expect(page).to have_css("img[src*='/uploads/profile_pic_for_test.30.03.png']")
+  end
+end
