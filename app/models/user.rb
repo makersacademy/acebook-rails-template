676cf74cd5a47971a7f8ceb777d443f8bdb96669
Robert Faldo <robertfaldo@gmail.com>
2018-07-06 14:56:57 +0100

    refactor image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # The way to tell CarrierWave to associate the image
-  # with a model is to use the mount_uploader method, 
-  # which takes as arguments a symbol representing the
-  # attribute and the class name of the generated uploader:
   mount_uploader :avatar, AvatarUploader
 end
--- spec/features/user_can_upload_profile_picture_spec.rb
+++ spec/features/user_can_upload_profile_picture_spec.rb
@@ -4,8 +4,6 @@ RSpec.feature "User can view their profile", type: :feature do
     click_link 'View profile'
     attach_file('user[avatar]', 'pic_for_test.30.03.png')
     click_button 'Update'
-    save_and_open_page
-    expect(page).to have_content("Type of Animal:")
-    expect(page).to have_css("img[src*='/uploads/profile_pic_for_test.30.03.png']")
+    expect(page.find('.profile_pic')['src']).to have_content 'profile_pic_for_test.30.03.png'
   end
 end
