0f71e27f4c711b8c4d991b7bea42deb188ef76df
Robert Faldo <robertfaldo@gmail.com>
2018-07-06 10:24:43 +0100

    Add profile pic to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,8 @@ class UsersController < ApplicationController
   end
 
   def update
-    # @user = User.find(params[:id])
-    p "params[:user][:avatar]"
-    p params[:file]
-    p "before current_user"
-    p current_user
-    p "after current_user"
     current_user.avatar = params[:user][:avatar]
-    p "current_user after assigning"
-    p current_user.avatar
-    p "end"
+    current_user.save!
   end
 
   private
