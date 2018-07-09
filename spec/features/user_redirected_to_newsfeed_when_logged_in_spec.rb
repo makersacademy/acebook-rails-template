f4cba5dc2fc149513ec369b79cf54eb37249ec51
anguspettifer <angus.pettifer@gmail.com>
2018-07-09 17:52:39 +0100

    Refactored tests

--- spec/features/user_redirected_to_newsfeed_when_logged_in_spec.rb
+++ spec/features/user_redirected_to_newsfeed_when_logged_in_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature "User redirected to newsfeed on login", type: :feature do
   scenario "successfuly" do
     sign_up_successfully
-    expect(page).to have_content("Message")
+    expect(page).to have_content("What's on your mind?")
   end
 end
