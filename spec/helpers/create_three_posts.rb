43d821492c21d687eadd4b41ebeb7dade530a540
Matthew <mdwareing@gmail.com>
2018-07-09 15:23:26 +0100

    Fixed five unloggedin tests

--- spec/helpers/create_three_posts.rb
+++ spec/helpers/create_three_posts.rb
@@ -1,5 +1,11 @@
+require 'rails_helper'
+require 'helpers/create_user'
+require 'helpers/user_log_in'
+
 def create_three_posts
   user = User.create(name: "simone", email: "mail@simone.com", password: "123456")
+  create_user
+  user_log_in
   visit user_posts_path(user)
   click_link "New post"
   fill_in "Message", with: "First post"
