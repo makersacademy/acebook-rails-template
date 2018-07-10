28649c18eede1c561a67b3882f86b6e9730c0e43
Robert Faldo <robertfaldo@gmail.com>
2018-07-10 16:02:21 +0100

    Comments appear most recent first

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
-  belongs_to :user_id
-  belongs_to :post_id
+  belongs_to :user
+  belongs_to :post
+
+  # this line makes most recent comments appear first
+  default_scope -> { order('created_at DESC') }
+  
+  # refactor note - I think we can delete the validates
+  # post_id and user_id because it already requires them
+  validates :post_id, presence: true
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :likes
+  has_many :comments
 
   mount_uploader :avatar, AvatarUploader
 end
