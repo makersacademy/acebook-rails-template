67fb5f541ede5abafbb30c1f7f4d8d6b042b2f62
Robert Faldo <robertfaldo@gmail.com>
2018-07-10 20:03:02 +0100

    Add comment to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,22 @@
 class CommentsController < ApplicationController
+  def create
+    # post = params[:comment][:post]
+    # p "HEREEEEE"
+    # p comment_params
+    post = Post.find(comment_params[:post_id])
 
+    # p post
+    # p "AFTER"
+    content = params[:comment][:content]
+    @comment = post.comments.build(content: content, user: current_user)
+    @comment.save
+    redirect_to root_path
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :content)
+  end
 
 end
