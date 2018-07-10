496a2748588e293c7a0b4c897463e520b3117a64
Robert Faldo <robertfaldo@gmail.com>
2018-07-10 10:24:49 +0100

    Refactor

--- spec/features/user_can_see_their_profile_spec.rb
+++ spec/features/user_can_see_their_profile_spec.rb
@@ -2,14 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "User can view their profile", type: :feature do
   scenario 'successfuly' do
-    visit root_path
-    click_link "Sign up"
-    fill_in "Email", with: 'millie@gmail.com'
-    fill_in "Firstname", with: "Jen"
-    fill_in "Lastname", with: "Smith"
-    fill_in "Password", with: "Secure123"
-    fill_in "Password confirmation", with: "Secure123"
-    click_button 'Sign up'
+    sign_up_successfully
+    
     click_link 'View profile'
     expect(page).to have_content("Type of Animal:")
     expect(page).to have_content("Age:")
