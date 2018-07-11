43f1297d21a3f87729be6b0692444f9dd62c0cce
Jen Strong <35671443+JenStrong@users.noreply.github.com>
2018-07-11 16:51:13 +0100

    Update comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,17 +8,4 @@ RSpec.describe Comment, type: :model do
     expect(comment).not_to be_valid
   end
 
-  it 'doesnt raise error when valid user_id and post_id' do
-
-    # post = Post.new
-    # comment = Comment.new(content: "Hello" )
-    #
-    # expect(comment).to be_valid
-  end
-
-  # it 'should have a maximum length of 250 chars' do
-  #   user = User.new
-  #   comment = Comment.new
-  # end
-
 end
