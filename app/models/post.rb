7459a27894eb6486feae6433f6352289c5fd3a46
Chris Russell <chris.john.russell@gmail.com>
2018-07-11 14:08:59 +0100

    reverse posts via the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
+  default_scope {order(created_at: :desc)}
   belongs_to :user
   has_many :likes
-  
 end
--- spec/features/user_can_logout_from_navbar_spec.rb
+++ spec/features/user_can_logout_from_navbar_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "User can log out of account from their profile page", type: :feature do
-  scenario 'successfuly' do
+  scenario 'successfully' do
     sign_up_successfully
 
     click_link 'Sign Out'
