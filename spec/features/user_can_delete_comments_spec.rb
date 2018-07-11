bfee827acd1479d1c1c27bc94bd3387f2bbefd23
Jen Strong <jenniferastrong@gmail.com>
2018-07-11 16:09:10 +0100

    Remove unneeded avatar photos

--- spec/features/user_can_delete_comments_spec.rb
+++ spec/features/user_can_delete_comments_spec.rb
@@ -12,17 +12,3 @@ RSpec.feature "User can delete their own comment", type: :feature do
     expect(page).not_to have_content("This is a text comment on the post")
   end
 end
-
-# RSpec.feature "User cannot like post twice", type: :feature do
-#   scenario 'successfully' do
-#     sign_up_successfully
-#
-#     fill_in "post_message", with: "test text"
-#     click_button 'Post'
-#     click_button 'Like'
-#     click_button 'Like'
-#
-#     expect(page).to have_content("Likes: 1")
-#     expect(page).not_to have_content("Likes: 2")
-#   end
-# end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,1 @@
-require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  it { is_expected.to be }
-end
