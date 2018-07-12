0b1e0611bf15f478566ea15d415e11dce873ce0e
Jen <35671443+JenStrong@users.noreply.github.com>
2018-07-12 11:56:47 +0100

    Update comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,3 @@
-
-<<<<<<< HEAD
 RSpec.describe Comment, type: :model do
 
   it 'raises error when no user_id or post_id' do
@@ -7,7 +5,5 @@ RSpec.describe Comment, type: :model do
 
     expect(comment).not_to be_valid
   end
-
 end
-=======
->>>>>>> 7f34566fb797bd738d1449ada90cc66a2f9fd11f
+
