d1d722d1283b127746a8484e1ea85bc31dbe7376
diego fontecilla <mail@diegofontecilla.com>
2018-07-12 14:16:23 +0100

    still working in editing and deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+before_action :find_comment, only: [:edit, :update, :show, :destroy]
+
   def index
     @comments = Comment.all
   end
@@ -14,6 +17,35 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    post = Post.find(params[:post_id])
+    user = User.find(params[:user_id])
+    if @comment.update_attributes(comment_params)
+      flash[:notice] = "Successfully updated post"
+      redirect_to user_posts_path(user)
+    else
+      flash[:alert] = "Error updating post!"
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    post = Post.find(params[:post_id])
+    user = User.find(params[:user_id])
+    if @comment.destroy
+      flash[:notice] = "Successfully deleted comment"
+      redirect_to user_posts_path(user)
+    else
+      flash[:alert] = "Error deleting comment!"
+    end
+  end
+
   def new
     @comment = Comment.new
   end
@@ -23,5 +55,8 @@ class CommentsController < ApplicationController
     def comment_params
     params.require(:comment).permit(:comment)
     end
-end
 
+    def find_comment
+      @comment = Comment.find(params[:id])
+    end
+end
