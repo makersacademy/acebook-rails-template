78ea6a21686173c00f872fd89d9988ad340f1d55
Jen Strong <jenniferastrong@gmail.com>
2018-07-13 09:35:27 +0100

    Remove link to logout and newsfeed from profile page

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -5,9 +5,8 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     sign_up_successfully
-
-    fill_in "post_message", with: "Hello, world!"
-    click_button "Post"
+    submit_post_successfully
+    
     expect(page).to have_content("Hello, world!")
   end
 end
