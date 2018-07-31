2a8ba76317cf69f3300cb4bb1d1d9765aeec779e
Jay Khan <jkhan992@gmail.com>
2018-07-31 18:39:21 +0100

    OTHER removed github merge syntax

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-# require 'rails_helper'
-#
-# RSpec.describe SessionController, type: :controller do
-#   describe "Get /session/new" do
-#     it "responds with 200" do
-#       get :new
-#       expect(response).to have_http_status(200)
-#     end
-#   end
-#
-#   describe "POST /session" do
-#     it "responds with 200" do
-#       session :create, params: { session: { email: "jay@gmail.com", password: "jay123" } }
-#       expect(response).to redirect_to(user_url)
-#     end
-#
-#     it "create a session" do
-#       session :create, params: { session: { email: "jay@gmail.com", password: "jay123" } }
-#       expect(Session.find_by(id: 1)).to be
-#     end
-#   end
-# end
-=======
 require 'rails_helper'
 
 RSpec.describe SessionController, type: :controller do
@@ -44,4 +20,3 @@ RSpec.describe SessionController, type: :controller do
     end
   end
 end
->>>>>>> origin/sign-in-model
