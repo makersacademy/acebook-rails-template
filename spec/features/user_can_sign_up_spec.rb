e0bc82fe0400b4038331358e03b5cad728dbd431
Elishka Flint <elishkaf@gmail.com>
2018-07-31 17:26:17 +0100

    FEATURE Add view with sign up input boxes to pass original feature test for sign up

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.feature "SignUp", type: :feature do
 
     scenario "A user can sign up with valid credentials" do
-      visit "/"
-      fill_in :first_name, with: "Elishka"
-      fill_in :last_name, with: "Flint"
-      fill_in :email_address, with: "elishka@keepingitrails.com"
-      fill_in :password, with: "pa55w0rd"
+      visit "/users/new"
+      fill_in 'First name', with: "Elishka"
+      fill_in 'Last name', with: "Flint"
+      fill_in 'Email', with: "elishka@keepingitrails.com"
+      fill_in 'Password', with: "pa55w0rd"
       click_button "Sign Up"
       expect(page).to have_content("Welcome Elishka!")
     end
