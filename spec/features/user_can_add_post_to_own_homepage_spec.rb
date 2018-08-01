52e281700bd5331ad970bced831aff78b8a7abf1
Elishka Flint <elishkaf@gmail.com>
2018-08-01 11:56:28 +0100

    TEST Update first feature test to include requirement for reverse chronological order in displaying posts

--- spec/features/user_can_add_post_to_own_homepage_spec.rb
+++ spec/features/user_can_add_post_to_own_homepage_spec.rb
@@ -4,16 +4,19 @@ RSpec.feature "Timeline", type: :feature do
 
   before(:each) do
     successful_sign_up
-    @time = Timecop.freeze('2018-01-01 01:00:00 +0100')
+    @time1 = Timecop.freeze('2018-01-01 01:00:00 +0100')
+    @time2 = Timecop.freeze('2018-01-01 02:00:00 +0100')
   end
 
   scenario "A user can add a post to their own homepage" do
     successful_log_in
     fill_in :post, with: "My first post"
     click_button "Post"
+    fill_in :post, with: "My second post"
+    click_button "Post"
     # syntax below should allow us to check that a few different strings
     # are displayed in the correct order
-    expect(page).to have_text(/'test_first_name' .+ 'My first post' .+ 'created at #{@time}'/)
+    expect(page).to have_text(/'test_first_name' .+ 'My second post' .+ 'created at #{@time2}' .+ 'test_first_name' .+ 'My first post' .+ 'created at #{@time1}'/)
   end
 
 end
