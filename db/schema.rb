c3484117827f25885b7938747f53742456d7d756
Elishka Flint <elishkaf@gmail.com>
2018-08-01 15:32:28 +0100

    SET UP Add user_id foreign key to the posts database

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180801010332) do
+ActiveRecord::Schema.define(version: 20180801142405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +31,5 @@ ActiveRecord::Schema.define(version: 20180801010332) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
