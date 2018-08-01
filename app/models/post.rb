469d3e7dc785b39ae789d10978c719e6ac3d66cc
Elishka Flint <elishkaf@gmail.com>
2018-08-01 15:54:39 +0100

    FEATURE Show posts, user first_name and timestamp on user homepage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+
+  belongs_to :users, :class_name => User, :foreign_key => "user_id"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   # This is a module, part of ActiveModel library
   # full details here: https://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html
   has_secure_password
+
+  has_many :posts
+
 end
