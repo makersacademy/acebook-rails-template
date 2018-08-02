d205d0b8ec942e8f14581d1bb65b7a19ead74adc
Jay Khan <jkhan992@gmail.com>
2018-08-02 12:31:01 +0100

    TEST: Controller test for non-logged user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,4 +24,12 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe "POST /users/id" do
+
+    it "redirects a non-logged in user to the homepage" do
+      expect(response).to redirect_to("/")
+    end
+
+  end
+
 end
