ee9bb60e8749f4ed0e343386b4e64a23b444b2b2
Elishka Flint <elishkaf@gmail.com>
2018-08-02 17:05:15 +0100

    TEST Fix final tests before code review

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -13,22 +13,9 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to redirect_to("/users/#{@user.id}")
     end
 
-    it "creates a user" do
+    it "creates a post" do
       expect(Post.find_by(message: "Test message")).to be
     end
-  end
-
-  describe "GET /posts/:id" do
-
-    before(:each) do
-      @user = User.create(first_name: "test first name", last_name: "test last name", email: "test@test.com", password: "pa55w0rd")
-    end
-
-    it 'responds with 200' do
-      get :show, params: { user_id: @user.id }
-      expect(response).to redirect_to post_path(@user)
-    end
-
   end
 
 end
