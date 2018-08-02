25292c06b3f47d7c7598360bcd0265c9b6c2a8e8
Elishka Flint <elishkaf@gmail.com>
2018-08-02 11:47:17 +0100

    TEST Add test for posts#show get route

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
+  describe "GET /posts/:id" do
+
+    before(:each) do
+      @user = User.create(first_name: "test first name", last_name: "test last name", email: "test@test.com", password: "pa55w0rd")
+    end
+
+    it 'responds with 200' do
+      get :show, params: { user_id: @user.id }
+      expect(response).to redirect_to("/posts/#{@user.id}")
+    end
+
+  end
+
 end
