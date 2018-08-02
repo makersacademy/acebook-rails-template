b162498e7f8d466e57090755b3391f5439358089
Jay Khan <jkhan992@gmail.com>
2018-08-02 13:42:01 +0100

    TEST updated feature test

--- spec/features/user_cannot_view_another_users_index_spec.rb
+++ spec/features/user_cannot_view_another_users_index_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.feature "User's index page", type: :feature do
 
   before(:each) do
-    @user = User.create(first_name: "Elishka", last_name: "Flint", email: "elishka@keepingitrails.com", password: "pa55w0rd")
+    @user = User.create(first_name: "test_first_name", last_name: "test_last_name", email: "test_email@keepingitrails.com", password: "pa55w0rd")
   end
 
   describe "A User visits another user's index page" do
     describe "Redirect user to index page" do
       visit("/users/#{@user.id}")
-      expect(page).to have_content("First name, Last name")
+      expect(response).to redirect_to("/")
     end
   end
 end
