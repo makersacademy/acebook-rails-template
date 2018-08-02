a6346415f9e7fa4671c0e08881f884ed261fb900
Jay Khan <jkhan992@gmail.com>
2018-08-02 16:48:38 +0100

    REFACTOR renamed test

--- spec/features/redirect_user_to_sign_page_when_not_logged_in_spec.rb
+++ spec/features/redirect_user_to_sign_page_when_not_logged_in_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "User's index page", type: :feature do
+RSpec.feature "User homepage", type: :feature do
 
   context "User not signed in" do
     scenario "Redirect user to sign-in page" do
