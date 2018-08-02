a2c1ecb54785eee07e3808e3253be240f5e35d20
Cornelis Heuperman <cheuperman@gmail.com>
2018-08-02 11:24:37 +0100

    Sing out redirects to index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+
+
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    p 'got here'
+    new_user_session_path
+  end
 end
