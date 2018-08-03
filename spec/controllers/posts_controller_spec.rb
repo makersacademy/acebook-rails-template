158fd92317a98b5fc2559daf329082c13babce61
danieljonesdmj <danieljonesdmj@gmail.com>
2018-08-03 16:56:42 +0100

    refactored tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,38 +2,32 @@ require 'rails_helper'
 require 'helpers/users_helper_spec'
 
 RSpec.describe PostsController, type: :controller do
-  describe "GET /new " do
-    it "responds with 200" do
-      user = User.create!(email: 'example@example.com', password: '1234567')
-      sign_in(user)
+  before(:each) do
+    user = User.create!(email: 'example@example.com', password: '1234567')
+    sign_in(user)
+  end
+
+  describe 'GET /new' do
+    it 'responds with 200' do
       get :new
       expect(response).to have_http_status(200)
     end
   end
-# we think that our new re_directs are changing the request status.
-# Is this an issue or do we need to just update tests to fit.
 
-
-  describe "POST /" do
-    it "responds with 200" do
-      user = User.create!(email: 'example@example.com', password: '1234567')
-      sign_in(user)
-      post :create, params: { post: { message: "Hello, world!" } }
+  describe 'POST /' do
+    it 'responds with 200' do
+      post :create, params: { post: { message: 'Hello, world!' } }
       expect(response).to redirect_to(posts_url)
     end
 
-    it "creates a post" do
-      user = User.create!(email: 'example@example.com', password: '1234567')
-      sign_in(user)
-      post :create, params: { post: { message: "Hello, world!" } }
-      expect(Post.find_by(message: "Hello, world!")).to be
+    it 'creates a post' do
+      post :create, params: { post: { message: 'Hello, world!' } }
+      expect(Post.find_by(message: 'Hello, world!')).to be
     end
   end
 
-  describe "GET /" do
-    it "responds with 200" do
-      user = User.create!(email: 'example@example.com', password: '1234567')
-      sign_in(user)
+  describe 'GET /' do
+    it 'responds with 200' do
       get :index
       expect(response).to have_http_status(200)
     end
