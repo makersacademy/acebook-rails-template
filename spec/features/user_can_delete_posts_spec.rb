61800fe542b35138f065ad497ef7f9b2a282f479
Jay Khan <jkhan992@gmail.com>
2018-08-06 12:48:11 +0100

    FEATURE Add delete post function

--- spec/features/user_can_delete_posts_spec.rb
+++ spec/features/user_can_delete_posts_spec.rb
@@ -5,18 +5,12 @@ RSpec.feature "Delete Posts", type: :feature do
   before(:each) do
     successful_sign_up
     first_user_post
+
   end
 
   scenario "A user can delete post from the user homepage" do
-    click_link("Delete", :match => :first)
-    click_button("OK")
+    click_link("Delete")
     expect(page).not_to have_text('test_first_name My first post less than a minute')
   end
-
-  # scenario "Posts display in reverse chronological order" do
-  #   successful_sign_up_second_user
-  #   second_user_post
-  #   expect(page).to have_text('test_first_name2 Second user post less than a minute test_first_name My first post less than a minute')
-  # end
 
 end
