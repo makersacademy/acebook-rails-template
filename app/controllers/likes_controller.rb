dc47cef9f6d274f1b03d7b9b53abed63b8a1dec3
Jay Khan <jkhan992@gmail.com>
2018-08-07 22:59:05 +0100

    OTHER Remove commented out redundant code

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,15 +5,8 @@ class LikesController < ApplicationController
     @post = Post.find_by(id: params[:post_id])
     @like = Like.create(like_params)
     @like.save
-
-    # @like_count = Like.count(:conditions => "post_id" == 2)
-    # p @like_count
     redirect_to user_path(@user)
   end
-
-  # def addLike
-  #   @post.increment_counter(:post_count, @post.id)
-  # end
 
   private
 
