3571d0df2bc7d0d6b586530d9efc6c2ca91898c1
Jay Khan <jkhan992@gmail.com>
2018-08-07 17:45:40 +0100

    OTHER Fix feature tests

--- spec/features/user_can_like_a_post_spec.rb
+++ spec/features/user_can_like_a_post_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "Like a post", type: :feature do
 
   scenario "A user can like a post" do
     click_button "Like"
-    expect(page).to have_content('test_first_name My first post less than a minute Like 1')
+    expect(page).to have_content('test_first_name My first post less than a minute Delete Update Likes: 1')
   end
 
 end
