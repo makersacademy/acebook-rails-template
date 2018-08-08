f107e3d778eef914c6bb31a3e11cf6bb93eae4c0
jeff1108 <jeffhongkong2007@gmail.com>
2018-08-08 12:19:41 +0100

    TEST Add controller test for testing the route to the post like page

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -8,10 +8,15 @@ RSpec.describe LikesController, type: :controller do
       @user = User.create(first_name: "test_first_name", last_name: "test_last_name", email: "test_email@keepingitrails.com", password: "pa55w0rd")
       @post = Post.create(user_id: @user.id, message: "Test message")
       post :create, params: { user_id: @user.id, post_id: @post.id }
+      @like = Like.where(user_id: @user.id, post_id: @post.id)
     end
 
-    it "responds with 200" do
+    it "responds with 200 user homepage" do
       expect(response).to redirect_to("/users/#{@user.id}")
+    end
+
+    it "responds with 200 post like page" do
+      expect(response).to redirect_to("/users/#{@user.id}/posts/#{@post.id}/likes/#{@like.id}")
     end
 
   end
