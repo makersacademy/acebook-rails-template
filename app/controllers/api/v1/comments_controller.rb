73c6ab8bc23014f5d81081c41accdd97f24b3d41
Bibi Collins <bibicollins94@googlemail.com>
2018-08-08 12:18:52 +0100

    Made an API controller for comments

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::CommentsController < ApplicationController
+
+    def create
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.create(comment_params)
+      render json: post
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:message, :user_id)
+    end
+
+end
