b087dadfdc7964d7c8af702ffc2a86ec01e4a91c
danieljonesdmj <danieljonesdmj@gmail.com>
2018-08-08 12:37:17 +0100

    reverted routing to non-api

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
-      resources :posts do
-        resources :comments
-      end
-    end
+
+  resources :posts do
+    resources :comments
   end
 
   root to: 'posts#index'
