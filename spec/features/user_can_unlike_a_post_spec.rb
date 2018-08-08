87b645bbeae66c968e9631bdf8a509a58d95e8d2
Jay Khan <jkhan992@gmail.com>
2018-08-08 16:07:37 +0100

    TESTS Add unlike feature test

---
+++ spec/features/user_can_unlike_a_post_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "Unlike a post", type: :feature do
+
+  before(:each) do
+    successful_sign_up
+    click_button 'Log Out'
+    first_user_post
+  end
+
+  scenario "A user can like a post" do
+    click_button "Like"
+    click_button "Unlike"
+    expect(page).to have_content('test_first_name My first post less than a minute Delete Update Likes: 0')
+    expect(page).to_not have_content('test_first_name My first post less than a minute Delete Update Likes: 1')
+
+  end
+
+end
