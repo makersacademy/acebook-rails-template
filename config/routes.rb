2b3fccb375630c58947b27bd083f89d1a04a2042
Cornelis Heuperman <cheuperman@gmail.com>
2018-08-10 12:47:04 +0100

    Remove Rails default comments from config and db

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :posts do
     resources :comments
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
 user1 = User.create(email: 'hello1@gmail.com', password: 'hello1', username: 'Gru')
 post1 = user1.posts.create(message: 'I love minions')
 user2 = User.create(email: 'hello2@gmail.com', password: 'hello2', username: 'The underminer')
