eb920ffcee7168b824f78c6b17034b8bbf800f2f
Marcus <marcus.kerr94@gmail.com>
2018-08-29 17:54:40 +0100

    Validation, uniqueness, passwords - GREEN

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,5 +19,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  # has_secure_password
+  has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
