6d209afbfcdd9e20bb5c6afa0115560fdceebc91
dani <daniboo83@gmail.com>
2018-08-29 14:09:05 +0100

    User signup redirects to homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to :home, notice: 'Account created successfully'
+      redirect_to '/home'
+      # :home, notice: 'Account created successfully'
     else
       redirect_to '/signup'
     end
