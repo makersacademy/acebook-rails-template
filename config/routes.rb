be65ffa704ad714d0096938958125d78d16651aa
Angela Wolff <warriorwomenblog@gmail.com>
2018-08-29 12:21:21 +0100

    Ignore this commit - nothing interesting here

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/login' => 'session#new'
+  
+  get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   
