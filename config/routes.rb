f1f4b9ee4e3b6df779de883497561bbe1d22f986
Marek <MarekSchmidt@outlook.com>
2018-08-30 11:45:29 +0100

    Trying to get root route working on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'posts#index'
+  
   resources :posts
 
-  root 'posts#index'
+
 end
