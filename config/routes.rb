15d81c0fcc74d3d7dd8bf3319cd983f412ab0d61
dani <daniboo83@gmail.com>
2018-08-30 15:34:10 +0100

    Fix signup route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'users#new'
 
+  get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
@@ -13,3 +14,5 @@ Rails.application.routes.draw do
   post '/users' => 'users#create'
   resources :posts
 end
+
+
