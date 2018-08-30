5b35980c717e8d76d1944509b8ec7a5095246076
Farah Jabri <farahjabri@hotmail.com>
2018-08-30 14:30:02 +0100

    authentication, html.erb files to reflect authentication and the forms, 2 db's created to hold users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
+
+
+  root 'posts#index'
+   get '/posts' => 'posts#index'
+   get '/posts/new' => 'posts#new'
+   post '/posts/new' => 'posts#create'
+
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login' => 'sessions#new'
+   post '/login' => 'sessions#create'
+   get '/logout' => 'sessions#destroy'
 
   resources :posts
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
--- db/migrate/20170526114520_create_posts.rb
+++ db/migrate/20170526114520_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :message
+      t.string :user_name
 
       t.timestamps
     end
