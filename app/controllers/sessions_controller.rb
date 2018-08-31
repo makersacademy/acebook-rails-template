e140da26a92d2ec4414d96dc1cf681e469a5e3b5
Marek <MarekSchmidt@outlook.com>
2018-08-31 11:46:23 +0100

    Marek's code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+  end
 
   def create
     user = User.find_by_email(params[:email])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to 'posts#new'
+      redirect_to '/posts/new'
     else
       redirect_to '/login'
     end
