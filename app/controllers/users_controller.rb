d7187c20473b32abf67de7f51b830f4a36729ce0
Angela Wolff <warriorwomenblog@gmail.com>
2018-08-31 12:18:11 +0100

    Added bootstrap for flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,11 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      flash[:success] = 'Successfully signed up'
       redirect_to '/home'
-      # :home, notice: 'Account created successfully'
     else
       redirect_to '/signup'
+      flash[:warning] = 'Your user account has not been created - please try again!'
     end
   end
 
