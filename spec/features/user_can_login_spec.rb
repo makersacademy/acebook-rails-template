4f291556974be27bd733700a9aa34edab3042368
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-03 11:26:26 +0100

    Added Dani's feature test with devise corrections

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -5,15 +5,16 @@ require 'features/web_helper'
 
 RSpec.feature 'User login', type: :feature do
   before(:each) { 
-    visit('/')
-    sign_in_flaubert
+    visit '/users/sign_up'
+    sign_up_jackbert
   }
-
-  xit scenario 'user can login to existing account' do
-    visit '/login'
-    fill_in 'user_email', with: 'js@test.com'
-    fill_in 'user_password', with: 'foobar'
-    click_button 'Submit'
-    expect(page).to have_content('Signed in as Flaubert')
+  
+  scenario 'user can login to existing account' do
+    sign_out :user
+    visit '/users/sign_in'
+    fill_in 'Email', with: 'js@test.com'
+    fill_in 'Password', with: 'foobar'
+    click_button 'Log in'
+    expect(page).to have_content('Logged in as Jack')
   end
 end
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -1,9 +1,9 @@
-def sign_in_flaubert
-  visit('/')
-  fill_in 'user_first_name', with: 'Jack'
-  fill_in 'user_last_name', with: 'Sparrow'
-  fill_in 'user_email', with: 'js@test.com'
+def sign_up_jackbert
+  visit '/users/sign_up'
+  fill_in 'First name', with: 'Jack'
+  fill_in 'Last name', with: 'Sparrow'
+  fill_in 'Email', with: 'js@test.com'
   fill_in 'user_password', with: 'foobar'
   fill_in 'user_password_confirmation', with: 'foobar'
-  click_button 'Submit'
+  click_button 'Sign up'
 end
