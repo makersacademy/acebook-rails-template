eb5e3ea46be7396857e536e055a71d5dd5df1aa9
limcuili <limcuili@hotmail.com>
2018-09-03 14:48:17 +0100

    CRUD merged with collapsable comments, and enforced uniqueness of usernames

--- db/migrate/20180830103256_create_users.rb
+++ db/migrate/20180830103256_create_users.rb
@@ -7,5 +7,6 @@ class CreateUsers < ActiveRecord::Migration[5.1]
 
       t.timestamps
     end
+      add_index :users, :name, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180831132524) do
+ActiveRecord::Schema.define(version: 2018_08_31_132524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180831132524) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name", unique: true
   end
 
   add_foreign_key "comments", "posts"
