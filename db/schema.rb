4d3121f5642eb71f0842515e16861b6620603c8b
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-03 15:25:38 +0100

    Post images added using CarrierWave

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180902194913) do
+ActiveRecord::Schema.define(version: 20180903131653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180902194913) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "postimage"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
