058d09203fe78179f89bc118c0fe43b2bd4441a2
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-04 14:02:19 +0100

    Adding host name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,13 @@ CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
+      :s3_host_name           => ENV['S3_HOST_NAME'],
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region                 => 'eu-west-2',
-      :host                   => 'rofbucket.s3.amazonaws.com'
+      :region                 => 'eu-west-2'
     }
-    
+    config.fog_directory = ENV['S3_BUCKET_NAME']
     config.storage = :fog
   else
     config.storage = :file
