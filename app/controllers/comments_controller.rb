a6417f9bc394b77eda96f2cbca5a1bdd638bb7b4
Marcus <marcus.kerr94@gmail.com>
2018-09-05 11:50:17 +0100

    Comments show names of user who posted

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = Comment.new(comment_params)
+    @comment = Comment.new(comment_params.merge(user_id: current_user.id))
 
     respond_to do |format|
       if @comment.save
@@ -69,6 +69,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:user_name, :body, :post_id)
+      params.require(:comment).permit(:user_name, :body, :post_id, :user_id)
     end
 end
