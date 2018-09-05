9c0a998761d2ac9f5d0b10ebada80bc4b0b97b36
Marcus <marcus.kerr94@gmail.com>
2018-09-05 15:42:03 +0100

    Removed username form comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180905103246) do
+ActiveRecord::Schema.define(version: 20180905144030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "user_name"
     t.text "body"
     t.integer "post_id"
     t.datetime "created_at", null: false
