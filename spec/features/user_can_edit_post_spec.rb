7ec79c45691208e0bfa2defe548a8e2a5ca973df
dani <daniboo83@gmail.com>
2018-09-06 18:14:09 +0100

    editing posts feature tests passing

--- spec/features/user_can_edit_post_spec.rb
+++ spec/features/user_can_edit_post_spec.rb
@@ -6,26 +6,33 @@ require 'cancan/matchers'
 RSpec.feature 'Timeline', type: :feature do
 
   scenario 'Standard User can edit own post' do
-    standard_user = create(:user)
-    login_as standard_user
-    newpost = Post.create(message: 'Console test', user_id: standard_user.id)
+    standard_user_posts
     visit ('/posts')  
     click_link 'Edit Post'  
-    expect(page).to have_content('Editing Post')
-    
+    expect(page).to have_content('Editing Post')   
+  end
+
+  scenario 'New Standard User cannot edit someone else post' do
+    sign_up_jackbert
+    visit ('/posts')   
+    expect(page).not_to have_content('Edit Post')
   end
 
   scenario 'Standard User cannot edit someone else post' do
-    # login_as create( :user ), scope: :user
-    # sign_out :user
-    # visit new_post_path
-    # expect(page).to have_content("You need to sign in or sign up before continuing.")
+    standard_user_posts
+    visit ('/posts')  
+    click_link 'Logout'
+    sign_up_jackbert
+    visit ('/posts')    
+    expect(page).not_to have_content('Edit Post')
   end
 
   scenario 'Superuser can edit someone else post' do
-    # login_as create( :user ), scope: :user
-    # sign_out :user
-    # visit new_post_path
-    # expect(page).to have_content("You need to sign in or sign up before continuing.")
+    standard_user_posts
+    visit ('/posts')  
+    click_link 'Logout'
+    super_user_logs_in
+    visit ('/posts')  
+    expect(page).to have_content('Edit Post')
   end
 end
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -15,3 +15,16 @@ def make_a_coffee
   fill_in 'post[message]', with: 'Hello, world!'
   click_button 'Create Post'
 end
+
+def standard_user_posts
+  standard_user = create(:user)
+  login_as standard_user
+  newpost = Post.create(message: 'Console test', user_id: standard_user.id)
+end
+
+def super_user_logs_in
+  superadmin_user = create(:user)
+  superadmin_user.superadmin_role = true
+  login_as superadmin_user
+  p superadmin_user
+end
