5032d9fc36131da261870bbc6fd2dbdb3a055425
Angela Wolff <warriorwomenblog@gmail.com>
2018-09-06 15:19:23 +0100

    Testing superadmin and standard user detroy powers

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,12 @@ class Ability
  
     user ||= User.new # guest user (not logged in)
     if user.superadmin_role?
-          can :manage, :all
-          can :access, :rails_admin       # only allow admin users to access Rails Admin
-          can :dashboard                  # allow access to dashboard
+      can :manage, :all
+      can :access, :rails_admin       # only allow admin users to access Rails Admin
+      can :dashboard                  # allow access to dashboard
     end
     if user.supervisor_role?
-          can :manage, User
+      can :manage, User
     end
     if user.user_role?
       can :manage, :all, user_id: user.id
