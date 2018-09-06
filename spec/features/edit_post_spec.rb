64405d306f2ef2d4c9994896e41f24bf658474b6
limcuili <38138998+limcuili@users.noreply.github.com>
2018-09-06 11:43:01 +0100

    Update edit_post_spec.rb

--- spec/features/edit_post_spec.rb
+++ spec/features/edit_post_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.feature 'Edit a post', type: :feature do
   scenario 'User can edit a post they created' do
     sign_up
-    sign_in
     create_post
     click_link('Edit')
     fill_in 'post[message]', with: 'Hello, kirt!'
