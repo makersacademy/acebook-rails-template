5b485661d5588198a8be043dee4598ce41600851
limcuili <38138998+limcuili@users.noreply.github.com>
2018-09-06 11:41:19 +0100

    Update user_can_add_multiple_posts_spec.rb

--- spec/features/user_can_add_multiple_posts_spec.rb
+++ spec/features/user_can_add_multiple_posts_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.feature 'User can create multiple posts', type: :feature do
   scenario 'User creates a post, then another' do
     sign_up
-    sign_in
     create_post
     expect(page).to have_content('Hello, world!')
     click_link('New post')
