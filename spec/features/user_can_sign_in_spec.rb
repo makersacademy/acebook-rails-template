82423c6ff1eec5d0bdadb343ae1c4579f98a42b6
limcuili <38138998+limcuili@users.noreply.github.com>
2018-09-06 11:42:03 +0100

    Update user_can_sign_in_spec.rb

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Main page sign in', type: :feature do
   scenario 'Users can sign in' do
     sign_up
+    click_link('Logout')
     sign_in
     expect(page).to have_content('Welcome, Sassy')
   end
