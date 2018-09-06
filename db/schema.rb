4b205e1b037fdfe165e09232c24a628f9c869ec7
limcuili <limcuili@hotmail.com>
2018-09-06 10:49:42 +0100

    got likes and profile page working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_132524) do
+ActiveRecord::Schema.define(version: 2018_09_05_165532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,15 +22,14 @@ ActiveRecord::Schema.define(version: 2018_08_31_132524) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
-
-    create_table "likes", force: :cascade do |t|
-      t.string "user_name"
-      t.bigint "post_id"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.index ["post_id"], name: "index_likes_on_post_id"
-
+  create_table "likes", force: :cascade do |t|
+    t.string "user_name"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
