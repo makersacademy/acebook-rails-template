9fb7c4d5c1ab5acafafa0e2f3f3a6fabc84312c5
limcuili <38138998+limcuili@users.noreply.github.com>
2018-09-06 11:40:49 +0100

    Update user_can_logout_spec.rb

--- spec/features/user_can_logout_spec.rb
+++ spec/features/user_can_logout_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.feature 'Logged in user can logout', type: :feature do
   scenario 'A user can login and then logout' do
     sign_up
-    sign_in
     click_link('Logout')
     expect(page).to have_content('Sign up')
   end
