6cbe0c21c6e9e16d752b7c5cf552f8da8027a879
Harry <harryjp1995@gmail.com>
2018-09-25 10:32:50 +0100

    Adds Users controller & /new view

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180924144914) do
+ActiveRecord::Schema.define(version: 20170526114520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "user_email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
