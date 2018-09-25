c3f8a81861f8033d7f67916ab3a9fa70c98e6133
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-25 12:16:23 +0100

    chore: adds db create_comments table migration

--- db/migrate/20180925105802_create_comments.rb
+++ db/migrate/20180925105802_create_comments.rb
@@ -3,6 +3,9 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.references :user, foreign_key: true
       t.references :post, foreign_key: true
+      t.string :message
+
+      t.timestamps
     end
   end
 end
