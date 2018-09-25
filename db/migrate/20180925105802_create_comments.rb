e86b5ee33430d7143cf301f72ad5ddca60ade7ff
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-25 11:58:53 +0100

    add new migration for create_comments table using two foriegn keys with command: generate migration create_comments user:references post:references

---
+++ db/migrate/20180925105802_create_comments.rb
@@ -0,0 +1,8 @@
+class CreateComments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
+    end
+  end
+end
