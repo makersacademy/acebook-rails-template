cf423cd75a00e0fbb9757f59711544d2044ee9ff
Samir Soormally <samirsoormally@gmail.com>
2018-09-25 11:43:56 +0100

    chore: adds user foreign key to posts
    *WE DO NOT UPDATE SCHEMA* we update migrations, which update the schema. User "rails generate create_post user:references" creates a foreign key of user on 'posts' table.
    
    `$ rake db:drop db:create db:migrate` is one command to drop old table, create new table, and use migrations to update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526114520) do
+ActiveRecord::Schema.define(version: 20180925103153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "password", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
     t.string "message"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.interger "post_id"
-    t.string "message"
-    t.integer "user_id"
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.interger "post_id"
-    t.integer "user_id"
   end
 
   add_foreign_key "posts", "users"
-  add_foreign_key "comments", "posts"
 end
