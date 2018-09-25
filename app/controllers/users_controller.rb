c730f5e6aa13ab0abc7b4d025be21b87f3a38dc8
Harry <harryjp1995@gmail.com>
2018-09-25 10:58:50 +0100

    Sign up form now complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:user].inspect
+    @user = User.new(user_params)
+    @user.save
+    redirect_to @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
