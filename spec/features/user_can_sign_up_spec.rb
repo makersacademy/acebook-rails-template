400a320224b12430caa771bd3f433930df864c18
Harry <37018462+hjpugh@users.noreply.github.com>
2018-09-25 12:28:46 +0100

    Update user_can_sign_up_spec.rb
    Removes superfluous test used at the top to refer to structure

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
-RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and view them" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
-  end
-end
-
 feature 'User can sign up' do
   scenario 'blank email gives an error' do
     visit 'users/new'
