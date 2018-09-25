689c102bc7deec2cfd4a822cbb49b20ee6e6396d
Samir Soormally <samirsoormally@gmail.com>
2018-09-25 14:55:30 +0100

    chore: adds columns 'handle' and 'full_name' to users table

--- db/migrate/20180925100351_create_users.rb
+++ db/migrate/20180925100351_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :email
       t.string :password
+      t.string :handle
+      t.string :full_name
 
       t.timestamps
     end
