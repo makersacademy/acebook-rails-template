9a58ef64d47efb87759552e34a385a09a2e9e11a
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-09-26 15:05:55 +0100

    feat: sign-up form now redirects to /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.create
+    redirect_to users_url
+  end
+
+  def index
+    
+  end
+
+  
 end
+
+
+# private
+
+# def user_params
+#   params.require(:post).permit(:message)
+# end
