2aa767ae73b09d582b7844bfcc0218a8d185bc53
Aneel Marshall <aneel.marshall@outlook.com>
2018-09-26 14:19:55 +0100

    refactor: change root route to user_signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions'
       }
+  devise_scope :user do
+    root to: 'devise/registrations#new'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
 
-  root 'posts#index'
 
 end
