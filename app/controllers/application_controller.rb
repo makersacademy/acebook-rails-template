2d853d25d72b187b7ba7a0c26b4393a9c35566b0
Andrew Wood <andrew.wood.dev@outlook.com>
2018-09-26 16:04:19 +0100

    Fix: rerouting after sign in to posts page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,6 @@ class ApplicationController < ActionController::Base
 
   protected
     def after_sign_in_path_for(resource)
-      sign_in_url = new_user_session_url
-      if request.referer == sign_in_url
-        super
-      else
-        stored_location_for(resource) || posts_path
-      end
+      posts_path
     end
 end
