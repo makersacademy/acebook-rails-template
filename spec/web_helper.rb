7065f854c17b98fa3abcbfee6ea8b78cbbe9dc7f
Andrew Wood <andrew.wood.dev@outlook.com>
2018-09-26 17:37:02 +0100

    Feat: Test for nav bar buttons and then nav bar buttons

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -3,3 +3,12 @@ def submit_test_post
   fill_in :post_content, with: "test post"
   click_on "Create Post"
 end
+
+def login
+  visit new_user_registration_path
+  fill_in :user_name, with: "Bob"
+  fill_in :user_email, with: "Bob@bob.com"
+  fill_in :user_password, with: "123456"
+  fill_in :user_password_confirmation, with: "123456"
+  click_on "Sign up"
+end
