59e717e5863c41f2be3ab33242eabd454d4253bf
jdm79 <james.malvern@gmail.com>
2018-09-26 15:15:01 +0100

    add character maximumof 10 for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password, presence: true, length: {minimum:6, maximum:10}
 end
