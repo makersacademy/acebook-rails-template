cb56ff6f192254b31d1a1429ace23589b682f004
James Sutherland <jrsutherland78@googlemail.com>
2018-09-27 11:46:36 +0100

    feat: emails uniqueness check is case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   validates :full_name, presence: true, length: { maximum: 50 }
   validates :handle, presence: true, length: { maximum: 50 }, uniqueness: true
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6, maximum: 10 }
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -58,7 +58,7 @@ RSpec.describe User, type: :model do
       end
 
       it "will be unique" do
-        duplicate_user = User.new(full_name: "My Other Name", handle: "My Other Handle", email: "ab@cd.com", password:"567890")
+        duplicate_user = User.new(full_name: "My Other Name", handle: "My Other Handle", email: "AB@CD.COM", password:"567890")
         @user.save
         expect(duplicate_user.valid?).not_to eq(true)
       end
