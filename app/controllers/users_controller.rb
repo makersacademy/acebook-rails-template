a87f21dd1ec1fa293b0ab0742e144ee1da350caf
josh nickson <jnickson1506@gmail.com>
2018-09-27 16:15:41 +0100

    feat: add user profile picture - option to update via edit route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,16 @@ class UsersController < ApplicationController
     @posts = Post.where(user_id: params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(params.require(:user).permit(:profilepic))
+    redirect_to @user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
 end
+
+
