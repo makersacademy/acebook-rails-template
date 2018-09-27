a31560572e22bf60b0a1a12581516dd1474302a1
Samir Soormally <samirsoormally@gmail.com>
2018-09-27 14:14:09 +0100

    tests: refactored tests: handle removed

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -4,12 +4,11 @@ RSpec.feature "Sign Up", type: :feature do
   scenario "User can sign up" do
     visit "/users/new"
     fill_in "Full name", with: "My Name"
-    fill_in "Handle", with: "My Handle"
     fill_in "Email", with: "ab@cd.com"
     fill_in "Password", with: "123456"
 
     click_button "Submit"
 
-    expect(page).to have_content("You are now signed up")
+    expect(page).to have_content("You have signed up successfully")
   end
 end
