107300752a061cf1cdcce187959817f75a7633c3
Andrew Wood <andrew.wood.dev@outlook.com>
2018-09-27 11:51:25 +0100

    Feat: Installed acts_as_votable gem and started creating a routes option for likes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts
-
+  resources :posts do
+    user do
+      put "like", to: "posts#add_like"
+    end
+  end
 end
