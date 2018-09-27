248548846b51d479bbae6653a0d796653e6c1d5c
Samir Soormally <samirsoormally@gmail.com>
2018-09-27 15:18:58 +0100

    feat: implementing devise: fixed views and routes associated with sign_up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/new' => 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts, :users
+  resources :posts
 
   root 'posts#index'
 
