c5823421aebfa14fdfccfd140ba6c38b4e604617
Harry <harryjp1995@gmail.com>
2018-09-27 13:53:30 +0100

    Chore: Refactors & adds tests for siging up; adds logout link to homepage

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -1,23 +1,28 @@
 require 'rails_helper'
 
 feature 'User can sign up' do
-  scenario 'blank email gives an error' do
-    # visit 'users/new'
+  before(:each) do
     visit 'users/sign_up'
-    # save_and_open_page
+  end
 
-    click_button 'Sign up'
-    fill_in 'email', with: ''
-    fill_in 'user_password', with: 'test'
+  scenario 'blank email gives an error' do
+    fill_in 'user[password]', with: 'tester'
     click_on 'Sign up'
     expect(page).to have_content('Email can\'t be blank')
   end
 
   scenario 'blank password gives an error' do
-    visit 'users/sign_up'
-    save_and_open_page
-    fill_in 'email', with: 'test@test.com'
+    fill_in 'user[email]', with: 'test@test.com'
     click_on 'Sign up'
     expect(page).to have_content('Password can\'t be blank')
+  end
+
+  scenario 'confirms successful sign up' do
+    save_and_open_page
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: 'tester'
+    fill_in 'user[password_confirmation]', with: 'tester'
+    click_on 'Sign up'
+    expect(page).to have_content('Welcome! You have signed up successfully.')
   end
 end
