835e4c77e9062948728ee0d599d064f92bdf4eea
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-09-27 19:34:10 +0100

    refactors post and timeline tests to use Customer instead of User

--- spec/features/timeline_spec.rb
+++ spec/features/timeline_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
 
   before do
-    user = User.create(first_name: 'Dave', last_name: 'Whoever', email: 'nojokes@email.com', password: '1234')
-    Post.create(message: 'Hi Mathilde!', user_id: user.id)
-    Post.create(message: 'Another post', user_id: user.id)
+    customer = Customer.create(email: 'nojokes@email.com', password: '123456')
+    Post.create(message: 'Hi Mathilde!', customer_id: customer.id)
+    Post.create(message: 'Another post', customer_id: customer.id)
   end
 
   scenario "User can see all posts" do
