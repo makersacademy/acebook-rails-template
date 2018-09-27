dcd0ede138b8f6db099cee149eaac84ef3775c86
jdm79 <james.malvern@gmail.com>
2018-09-27 12:31:20 +0100

    fixes some of the tests regarding logging in and signup, changed id's and targeted values for another test

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -2,18 +2,22 @@ require 'rails_helper'
 
 feature 'User can sign up' do
   scenario 'blank email gives an error' do
-    visit 'users/new'
+    # visit 'users/new'
+    visit 'users/sign_up'
+    # save_and_open_page
+
+    click_button 'Sign up'
     fill_in 'email', with: ''
-    fill_in 'password', with: 'test'
-    click_button 'submit'
+    fill_in 'user_password', with: 'test'
+    click_on 'Sign up'
     expect(page).to have_content('Email can\'t be blank')
   end
 
   scenario 'blank password gives an error' do
-    visit 'users/new'
+    visit 'users/sign_up'
+    save_and_open_page
     fill_in 'email', with: 'test@test.com'
-    fill_in 'password', with: ''
-    click_button 'submit'
+    click_on 'Sign up'
     expect(page).to have_content('Password can\'t be blank')
   end
 end
