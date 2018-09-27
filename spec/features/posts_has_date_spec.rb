2451785f1711cd192d1e93ad02aebfa498879bda
leekarho <k-h_lee@hotmail.co.uk>
2018-09-27 12:03:52 +0100

    added Timecop gem to stub date in tests

--- spec/features/posts_has_date_spec.rb
+++ spec/features/posts_has_date_spec.rb
@@ -2,12 +2,21 @@ require 'rails_helper'
 require 'web_helper'
 
 RSpec.feature "Post has dates", type: :feature do
+  before do
+     Timecop.freeze(Time.local(1990))
+   end
+
+   after do
+     Timecop.return
+   end
+
   scenario "Each post has a date" do
+
     user_sign_up
     submit_test_post
-    expect(page).to have_content("September 27")
+    expect(page).to have_content("January 01")
     visit("/posts")
-    expect(page).to have_content("September 27")
+    expect(page).to have_content("January 01")
   end
 
 end
