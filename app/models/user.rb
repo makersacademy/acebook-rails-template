2b4bb3b884db40d7bb98fe11e1329633ceb2cb2f
James Sutherland <jrsutherland78@googlemail.com>
2018-09-27 10:13:36 +0100

    feat: checks for full_name presence on creation of User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  
-  
+
+  validates :full_name, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+
   before(:each) do
     @user = User.new(full_name: "My Name", handle: "My Handle", email: "ab@cd.com", password:"123456")
   end
@@ -9,6 +9,11 @@ RSpec.describe User, type: :model do
   describe "validity of sign-up details" do
     it "is valid" do
       expect(@user.valid?).to eq(true)
+    end
+
+    it "full name should not be blank" do
+      @user.full_name = " "
+      expect(@user.valid?).not_to eq(true)
     end
   end
 
