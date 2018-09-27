06d76dec6f67f8270c557f807d993ac61f2e4a0d
davidlawes <davidlawes@hotmail.co.uk>
2018-09-27 15:03:22 +0100

    test: updated unit tests for model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it { is_expected.to be }
-end
 
-# has foreign key: post.user_id = user_id
+  before do
+    @user = User.create(first_name: 'Dave', last_name: 'Whatever', email: 'dave@123.com', password: 123)
+    @post = Post.create(message: 'hello world', user_id: @user.id)
+  end
+
+  it 'Create a new Post object' do
+    expect(subject).to be_a Post
+  end
+
+  it 'Is linked to user_id' do
+    expect(@post.user_id).to eq @user.id
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,25 @@
 require 'rails_helper'
 
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe User, type: :model do
 
-# test uniquness
-# test presence
+  before do
+    subject = User.create(first_name: 'Dave', last_name: 'Whoever', email: 'dave@123.com', password: 123)
+  end
+  
+  it 'Creates a new User object' do
+    expect(subject).to be_a User
+  end
+
+  context 'For testing validation helpers' do
+    it 'Does not add data with a non-unique email address' do
+      user = User.create(first_name: 'Mathilde', last_name: 'Giggly', email: 'dave@123.com', password: 123)
+      expect(user.id).to eq nil
+    end
+
+    it 'Does not add data wefuses entries without a first name' do
+      user = User.create(first_name: '', last_name: 'Giggle', email: 'anotheremail@email.com', password: 123)
+      expect(user.id).to eq nil
+    end
+  end
+
+end
