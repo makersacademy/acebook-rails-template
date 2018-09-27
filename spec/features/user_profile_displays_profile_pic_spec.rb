fd5d10aec5e0cbad5d65966242f9706df5dbf13f
josh nickson <jnickson1506@gmail.com>
2018-09-27 16:47:51 +0100

    test: add test for presence of default profile pic

---
+++ spec/features/user_profile_displays_profile_pic_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.feature "Profile picture", type: :feature do
+  scenario "user profile has default image" do
+    user_sign_up
+    submit_test_post
+    click_link "bob@bob.com"
+    expect(page).to have_css("img[src*='https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg']")
+  end
+  scenario "user profile picture can be changed" do
+    user_sign_up
+    submit_test_post
+    click_link "bob@bob.com"
+    expect(page).to have_css("img[src*='https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg']")
+  end
+
+end
