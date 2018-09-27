f824e6f6e215813481349a9599b7375d0249ad31
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-09-27 12:14:20 +0100

    Adds method to like posts, redirect to stay on page after clicking like

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts do
-    user do
+    member do
       put "like", to: "posts#add_like"
     end
   end
