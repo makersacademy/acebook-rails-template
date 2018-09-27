68213ff99acbec3361e2a22213b386ee96d51913
leekarho <k-h_lee@hotmail.co.uk>
2018-09-27 11:07:14 +0100

    posts can show dates

--- spec/features/posts_has_date_spec.rb
+++ spec/features/posts_has_date_spec.rb
@@ -5,8 +5,9 @@ RSpec.feature "Post has dates", type: :feature do
   scenario "Each post has a date" do
     user_sign_up
     submit_test_post
+    expect(page).to have_content("September 27")
     visit("/posts")
-    expect(page).not_to have_content("September 27")
+    expect(page).to have_content("September 27")
   end
 
 end
