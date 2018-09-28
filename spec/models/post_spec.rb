c3968dc187fd04621cb32816f7ac0cf355c1fc6e
jdm79 <james.malvern@gmail.com>
2018-09-28 13:56:51 +0100

    test: adds test to check a post message is not empty

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,29 +9,9 @@ describe '#create' do
     @post = Post.create({ message: 'test'})
     expect(Post.all.include?(@post)).to be(true)
   end
+
+  it "doesn\'t add empty message" do
+    @post = Post.create({message: ''})
+    expect(Post.all.include?(@post)).to be(false)
+  end
 end
-
-
-
-
-
-
-# example test fom user_spec
-# describe '#create' do
-#   it 'adds a user to the database' do
-#     @user = User.create({email: 'test@test.com', password: 'test'})
-#     expect(User.all.include?(@user)).to be(true)
-#   end
-
-#   it 'doesn\'t add empty details' do
-#     @user = User.create({email: '', password: ''})
-#     expect(User.all.include?(@user)).to be(false)
-#   end
-
-#   it 'doesn\'t add the same email twice' do
-#     @user1 = User.create({email: 'test@test.com', password: 'test'})
-#     @user2 = User.create({email: 'test@test.com', password: 'test'})
-#     expect(User.all.include?(@user1)).to be(true)
-#     expect(User.all.include?(@user2)).to be(false)
-#   end
-# end
