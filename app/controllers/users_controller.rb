6533ef9e48b2a9a03b2310060f348d2040d10a8b
josh nickson <jnickson1506@gmail.com>
2018-09-28 11:34:25 +0100

    refactor: DRY controller with find_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,23 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
+    find_user
     @posts = Post.where(user_id: params[:id]).order(created_at: :desc)
   end
 
   def update
-    @user = User.find(params[:id])
+    find_user
     @user.update(params.require(:user).permit(:profilepic))
     redirect_to @user
   end
 
   def edit
+    find_user
+  end
+
+  private
+
+  def find_user
     @user = User.find(params[:id])
   end
 
