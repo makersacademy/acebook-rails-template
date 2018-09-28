49f64f8cca20f7e60cb3baa9df4d472f7970cb55
Lucien Najev <luciennajev@hotmail.com>
2018-09-28 15:40:58 +0100

    refactored feature tests to pass

--- spec/features/timeline_spec.rb
+++ spec/features/timeline_spec.rb
@@ -6,6 +6,11 @@ RSpec.feature "Timeline", type: :feature do
     customer = Customer.create(email: 'nojokes@email.com', password: '123456')
     Post.create(message: 'Hi Mathilde!', customer_id: customer.id)
     Post.create(message: 'Another post', customer_id: customer.id)
+    visit '/customers/sign_up'
+    fill_in 'customer_email', with: 'test@test.com'
+    fill_in 'customer_password', with: 'test123'
+    fill_in 'customer_password_confirmation', with: 'test123'
+    click_button 'Sign up'
   end
 
   scenario "User can see all posts" do
