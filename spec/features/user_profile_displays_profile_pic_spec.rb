b6cf65d8a238fa420c0bd3c275cef4a35bd45782
josh nickson <jnickson1506@gmail.com>
2018-09-28 11:32:18 +0100

    test: add tet for changing profile picture

--- spec/features/user_profile_displays_profile_pic_spec.rb
+++ spec/features/user_profile_displays_profile_pic_spec.rb
@@ -11,7 +11,10 @@ RSpec.feature "Profile picture", type: :feature do
     user_sign_up
     submit_test_post
     click_link "bob@bob.com"
-    expect(page).to have_css("img[src*='https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg']")
+    click_link "profile-pic"
+    fill_in :user_profilepic, with: "test"
+    click_on "Update User"
+    expect(page).to have_css("img[src*='test']")
   end
 
 end
