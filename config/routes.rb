0af112e2a9b12beea5fb5aac604bb50af62243d1
Lucien Najev <luciennajev@hotmail.com>
2018-09-28 12:05:59 +0100

    users can now sign up with first and last name

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   get 'pages/home'
 
-  devise_for :customers
+  devise_for :customers, :controllers => { registrations: 'registrations' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
