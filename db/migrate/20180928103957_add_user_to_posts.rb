2c852a9ecbcdc7ca38cee3a65046af776101c7a5
jdm79 <james.malvern@gmail.com>
2018-09-28 14:32:48 +0100

    feature: adds user id to posts, tests for this - still a fail on one test for posts going to db which did not fail before - explore this

--- db/migrate/20180928103957_add_user_to_posts.rb
+++ db/migrate/20180928103957_add_user_to_posts.rb
@@ -1,5 +1,5 @@
 class AddUserToPosts < ActiveRecord::Migration[5.1]
   def change
-    add_reference :posts, :user, foreign_key: true
+    add_reference :posts, :user, index: true, foreign_key: true
   end
 end
