b1b32cb461ba6ebec0f3c4d4835bc8ee20e98132
josh nickson <jnickson1506@gmail.com>
2018-09-28 12:14:44 +0100

    fix: fix failing profile picture tests

--- spec/features/user_profile_displays_profile_pic_spec.rb
+++ spec/features/user_profile_displays_profile_pic_spec.rb
@@ -5,12 +5,14 @@ RSpec.feature "Profile picture", type: :feature do
     user_sign_up
     submit_test_post
     click_link "Bob"
-    expect(page).to have_css("img[src*='https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg']")
+    puts page.body
+    expect(page).to have_css("img[src*='http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png']")
   end
   scenario "user profile picture can be changed" do
     user_sign_up
     submit_test_post
     click_link "Bob"
+    puts page.body
     click_link "profile-pic"
     fill_in :user_profilepic, with: "test"
     click_on "Update User"
