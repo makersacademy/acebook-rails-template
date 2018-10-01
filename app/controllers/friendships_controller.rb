a33ddd5509bddc8d05069999c83ca5335fc93b5e
leekarho <k-h_lee@hotmail.co.uk>
2018-10-01 16:10:38 +0100

    feat: user can add and remove friend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,24 @@
 class FriendshipsController < ApplicationController
+
+  def create
+    @friendship = current_user.friendships.new(:friend_id => params[:friend_id])
+    @user = User.find(params[:friend_id])
+
+    if @friendship.save
+      redirect_to user_path(@user)
+      flash[:notice] = "Added friend."
+    else
+      redirect_to user_path(@user)
+      flash[:notice] = "Unable to add friend."
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:friend_id])
+    @friendship = current_user.friendships.find(params[:id])
+    @friendship.destroy
+    flash[:notice] = "Removed friendship."
+    redirect_to user_path(@user)
+  end
+
 end
