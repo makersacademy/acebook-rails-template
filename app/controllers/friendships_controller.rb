f751a39ffd942887e858ff783509fb991fe2e803
leekarho <k-h_lee@hotmail.co.uk>
2018-10-01 17:47:28 +0100

    user can now remove friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -14,10 +14,9 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    # @user = User.find(params[:friend_id])
     @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
-    # redirect_to user_path(@user)
+    redirect_to user_path(current_user)
     flash[:notice] = "Removed friendship."
   end
 
