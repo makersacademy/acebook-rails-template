54328e5c5adcc77fd7841e8b6227186e9d83b3a5
Lucien Najev <luciennajev@hotmail.com>
2018-10-01 11:09:39 +0100

    feature testing - migrating post entry over to the posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-  def new
-    @post = Post.new
-  end
+  # def new
+  #   @post = Post.new
+  # end
 
   def create
     @post = Post.create(post_params)
--- spec/features/single_posts_view_spec.rb
+++ spec/features/single_posts_view_spec.rb
@@ -12,9 +12,14 @@ RSpec.feature 'Single page post', type: :feature do
     scenario 'A user can make a post submission directly on the posts page' do
 
       click_link 'get :posts'
+      expect(URI.parse(current_url).path).to eq '/posts'
+      fill_in 'post_message', with: 'test'
       expect(page).to have_button "Submit"
-
-
+      click_button 'Submit'
+      expect(URI.parse(current_url).path).to eq '/posts'
+      expect(page).to have_content 'post.message = test'
 
   end
+
+
 end
