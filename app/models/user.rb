0abb82a1e528efb0fc7e17a991a28efc54c5f4f5
Rashika Patel <rashika_patel@hotmail.com>
2018-10-01 10:54:52 +0100

    feat: updates user model with name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+  attr_accessible :first_name, :surname
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # validates :email, presence: true, uniqueness: true
   validates :password, length: {maximum:10}
 end
