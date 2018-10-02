808a8ce588eb3393bc93a9155ac3520bf0bbcf2f
Rashika Patel <rashika_patel@hotmail.com>
2018-10-02 12:32:11 +0100

    fix: typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessible :first_name, :surname
+  attr_accessor :first_name, :surname
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
