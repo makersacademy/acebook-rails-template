bca89084874818c344d5cd0b69150327286ba810
Harry <harryjp1995@gmail.com>
2018-10-03 12:53:56 +0100

    User authentication now seems to be working okay

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,6 +5,7 @@ module Api
       def create
         @user = User.where(email: params[:email]).first
         if @user&.valid_password?(params[:password])
+          @user.save
           render json: @user.as_json(only: [:email, :authentication_token]), status: :created
         else
           head(:unauthorized)
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181003105330) do
+ActiveRecord::Schema.define(version: 20181003091505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "jwt_blacklists", force: :cascade do |t|
-    t.string "jti", null: false
-    t.index ["jti"], name: "index_jwt_blacklists_on_jti"
-  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
