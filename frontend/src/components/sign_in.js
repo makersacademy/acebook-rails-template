a83e952bc33c2f443e7399d9ecda8b0054bdcf36
Harry <harryjp1995@gmail.com>
2018-10-03 14:58:27 +0100

    User auth now mostly working, still some work re. wrong details & errors etc

--- frontend/src/components/sign_in.js
+++ frontend/src/components/sign_in.js
@@ -1,57 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class SignIn extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      email: '',
-      password: ''
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+      email: "",
+      password: "",
+      token: ""
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleInputChange(event){
-    const target = event.target
+  handleInputChange(event) {
+    const target = event.target;
     this.setState({
       [target.name]: target.value
-    })
+    });
   }
 
-  handleSubmit(event){
-    const url = 'localhost:2000/api/v1/sessions'
+  handleSubmit(event) {
+    const url = "http://localhost:3000/api/v1/sessions";
     fetch(url, {
-      method: 'POST',
-      body: { 
-        "email": this.state.email, 
-        "password": this.state.password
-      }
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password
+      })
     })
-    .then( res => res.json)
-    .then( result => console.log(result))
-    event.preventDefault()
+      .then(res => res.json())
+      .then(result =>
+        this.setState({
+          token: result.authentication_token
+        })
+      );
+    console.log(this.state.token);
+    event.preventDefault();
   }
 
-
-
-  render(){
-    return(
+  render() {
+    return (
       <form onSubmit={this.handleSubmit}>
-        <br/>
+        <br />
         <label>
           Email:
-          <br/>
-          <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange} />
+          <br />
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleInputChange}
+          />
         </label>
         <label>
           Password:
-          <br/>
-          <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange} />
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+          />
         </label>
-        <input type="submit" value="Submit"/>
+        <input type="submit" value="Sign In" />
       </form>
-    )
+    );
   }
 }
 
-export default SignIn
+export default SignIn;
--- frontend/src/components/sign_up.js
+++ frontend/src/components/sign_up.js
@@ -1,64 +1,81 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class SignUp extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      first_name: '',
-      surname: '',
-      email: '',
-      password: ''
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+      first_name: "",
+      surname: "",
+      email: "",
+      password: ""
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleInputChange(event) {
-    const target = event.target
+    const target = event.target;
     this.setState({
       [target.name]: target.value
-    })
+    });
   }
 
   handleSubmit(event) {
-    const url = "localhost:2000/api/v1/users"
+    const url = "http://localhost:2000/api/v1/users";
     fetch(url, {
-      method: 'POST',
-      body: { "user": { "email": this.state.email, "password": this.state.password }}
-    })
-    .then( res => console.log(res))
-    event.preventDefault()
+      method: "POST",
+      body: { user: { email: this.state.email, password: this.state.password } }
+    }).then(res => console.log(res));
+    event.preventDefault();
   }
 
-
   render() {
-    return(
+    return (
       <form onSubmit={this.handleSubmit}>
         <label>
           First Name
-          <br/>
-          <input type="text" name="first_name" value={this.state.first_name} onChange={this.handleInputChange}/>
+          <br />
+          <input
+            type="text"
+            name="first_name"
+            value={this.state.first_name}
+            onChange={this.handleInputChange}
+          />
         </label>
         <label>
           Surname
-          <br/>
-          <input type="text" name="surname" value={this.state.surname} onChange={this.handleInputChange}/>
+          <br />
+          <input
+            type="text"
+            name="surname"
+            value={this.state.surname}
+            onChange={this.handleInputChange}
+          />
         </label>
         <label>
           Email
-          <br/>
-          <input type="email" name="email" value={this.state.email} onChange={this.handleInputChange}/>
+          <br />
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleInputChange}
+          />
         </label>
         <label>
           Password
-          <br/>
-          <input type="password" name="password" value={this.state.password} onChange={this.handleInputChange}/>
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+          />
         </label>
         <input type="submit" value="Sign Up" />
       </form>
-    )
+    );
   }
-
 }
 
-export default SignUp
+export default SignUp;
