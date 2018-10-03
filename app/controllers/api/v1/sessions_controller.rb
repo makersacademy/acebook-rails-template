aab7742cd4c4a464c13c08f45e2ad0bc24ff144c
Harry <harryjp1995@gmail.com>
2018-10-03 10:53:32 +0100

    Not working just yet, uploaded to allow rest of team to work on

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ module Api
       def create
         @user = User.where(email: params[:email]).first
         if @user&.valid_password?(params[:password])
-          render json: @user.as_json(only: [:id, :email]), status: :created
+          render json: @user.as_json(only: [:email, :authentication_token]), status: :created
         else
           head(:unauthorized)
         end
