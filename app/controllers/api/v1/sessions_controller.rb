06e5f7e0d318d4fb67437b9f9208dfebe0dafab1
Harry <harryjp1995@gmail.com>
2018-10-03 10:13:54 +0100

    Adds the simple token gem

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ module Api
     class SessionsController < ApplicationController
       def create
         @user = User.where(email: params[:email]).first
-        if @user.valid_password?(params[:password])
+        if @user&.valid_password?(params[:password])
           render json: @user.as_json(only: [:id, :email]), status: :created
         else
           head(:unauthorized)
