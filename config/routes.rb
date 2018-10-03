40b1eccdfa24ccf0be4f6342939740e845267628
Harry <harryjp1995@gmail.com>
2018-10-03 09:34:26 +0100

    Adds session controller and configures routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      devise_for :users
+      # Commented out as to allow us to do our own authentication:
+      # devise_for :users
       get 'home/index'
 
       # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
       resources :posts
       resources :users
+      resource :sessions, only: [:create, :destroy]
       # resources :posts
 
       root 'home#index'
