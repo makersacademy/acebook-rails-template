6579e30636fe9c690d028aa6608fb3fb4c7d3605
Darcie <darciewalsh@hotmail.com>
2018-10-22 17:42:48 +0100

    simplecov formatting

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   gem 'rubocop', require: false
   gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,15 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
 require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
 SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
