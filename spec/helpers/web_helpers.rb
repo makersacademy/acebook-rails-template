8e78ce1af878ecc143cdf657b62e25cae33ea844
Cameron Whitehead <camjw119@gmail.com>
2018-10-23 11:22:24 +0100

    Added ability to update posts, no offenses 100% coverage

--- spec/helpers/web_helpers.rb
+++ spec/helpers/web_helpers.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 def create_hello_world_post
   visit '/posts'
   click_link 'New post'
   fill_in 'Message', with: 'Hello, world!'
   click_button 'Submit'
-end 
+end
