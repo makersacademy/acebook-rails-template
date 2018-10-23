d72419292c458b1477f5406b2f92cd7a543b0d30
Andres Faraone <andresfaraone@outlook.com>
2018-10-23 14:13:15 +0100

    changed test

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     visit "/posts"
-    click_link "New post"
+    click_link "Create post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
