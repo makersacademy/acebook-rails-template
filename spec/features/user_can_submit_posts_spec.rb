f2ee04a84577c1b48fce4fbd06a7438e24dfbdb2
Andres Faraone <andresfaraone@outlook.com>
2018-10-23 17:15:53 +0100

    posts appear reverse chrono on the feed and posts all have formatted timestamp

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -4,14 +4,22 @@ require 'timecop'
 RSpec.feature "Timeline", type: :feature do
   context "Creating a new post" do
     scenario "Can submit posts and view them" do
-      create_post
+      create_post("Hello, world!")
       expect(page).to have_content("Hello, world!")
     end
 
     scenario "Posts have a timestamp" do
       Timecop.freeze(Time.zone.parse('13:00 3 October 2018'))
-      create_post
+      create_post("Hello, world!")
       expect(page).to have_content("Wednesday, 3 Oct 2018 at 1:00 PM")
+    end
+
+    scenario "Posts appear in reverse chronological order" do
+      Timecop.freeze(Time.zone.parse('13:00 3 October 2018'))
+      create_post("First Post")
+      Timecop.freeze(Time.zone.parse('13:00 4 October 2018'))
+      create_post("Second Post")
+      expect(first('p.post-message')).to have_content("Second Post")
     end
   end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,6 @@
-def create_post
+def create_post(message)
   visit "/posts"
   click_link "Create post"
-  fill_in "Message", with: "Hello, world!"
+  fill_in "Message", with: message
   click_button "Submit"
 end
