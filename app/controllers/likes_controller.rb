22020d24dd3e3145213908b799dc49770ba787b1
Alex Kharouk <akharoukpp@gmail.com>
2018-10-24 15:42:10 +0100

    complete like feature so users can like and unlikes posts (once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,38 @@
 
 class LikesController < ApplicationController
   before_action :find_posts
+  before_action :find_like, only: [:destroy]
 
   def create
-    @post.likes.create(user_id: current_user.id)
+    if already_liked?
+      flash[:notice] = "can't like more than once"
+    else
+     @post.likes.create(user_id: current_user.id)
+    end
+    redirect_to posts_url
+  end
+
+  def destroy
+    if !(already_liked?)
+      flash[:notice] = 'cant unlike'
+    else
+      @like.destroy
+    end
     redirect_to posts_url
   end
 
   private
 
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
+  end
+
+  def find_like
+    @like = @post.likes.find(params[:id])
+  end
+
   def find_posts
     @post = Post.find(params[:post_id])
   end
+  
 end
