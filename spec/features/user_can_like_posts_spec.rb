14cd98ebf67664744dfe7b71135abef75f371d43
Alex Kharouk <akharoukpp@gmail.com>
2018-10-24 17:04:18 +0100

    fix test to pass travis

--- spec/features/user_can_like_posts_spec.rb
+++ spec/features/user_can_like_posts_spec.rb
@@ -4,10 +4,14 @@ require 'rails_helper'
 
 RSpec.feature 'Like posts', type: :feature do
   scenario 'Like post counter' do
+    create_hello_world_post
+    click_button 'Like'
+    expect(page).to have_content('1 Like')
+    click_button 'Unlike'
+    expect(page).to have_content('0 Likes')
   end
 
   scenario 'Can unlike post' do
-    signup
     create_hello_world_post
     click_button 'Like'
     expect(page).to have_content('1 Like')
