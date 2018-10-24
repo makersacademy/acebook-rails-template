e5cf1626110a38b1057570d0e4a330408c993af1
Cameron Whitehead <camjw119@gmail.com>
2018-10-24 13:57:06 +0100

    Only two tests failing now, updated webhelpers

--- spec/helpers/web_helpers.rb
+++ spec/helpers/web_helpers.rb
@@ -19,13 +19,8 @@ end
 def signup
   visit '/'
   click_link 'Sign Up'
-  fill_in 'user_email', with: 'test@mail.com'
+  fill_in 'user_email', with: 'tests@mail.com'
   fill_in 'user_password', with: 'test123'
   fill_in 'user_password_confirmation', with: 'test123'
   click_button 'Sign up'
-end
-
-def logout
-  signup
-  click_link 'Logout'
 end
