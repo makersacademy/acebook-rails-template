a2e38a3317b7bda5955413ad20d1efd2eefa9725
djstephenson7 <dan_0502@hotmail.co.uk>
2018-10-25 11:41:59 +0100

    Passing test

--- spec/helpers/web_helpers.rb
+++ spec/helpers/web_helpers.rb
@@ -8,13 +8,13 @@ def create_hello_world_post
   click_button 'Submit'
 end
 
-def login
-  visit '/'
-  click_link 'Login'
-  fill_in 'user_email', with: 'tests@mail.com'
-  fill_in 'user_password', with: 'test123'
-  click_button 'Log in'
-end
+# def login
+#   visit '/'
+#   click_link 'Login'
+#   fill_in 'user_email', with: 'tests@mail.com'
+#   fill_in 'user_password', with: 'test123'
+#   click_button 'Log in'
+# end
 
 def signup
   visit '/'
