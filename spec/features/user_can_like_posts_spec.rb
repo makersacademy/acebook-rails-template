1e4e99fdc52495f37cca950fba4111426480dce1
Alex <39955296+Kharouk@users.noreply.github.com>
2018-10-25 05:43:39 +0100

    update test + fix codeclimate

--- spec/features/user_can_like_posts_spec.rb
+++ spec/features/user_can_like_posts_spec.rb
@@ -5,10 +5,9 @@ require 'rails_helper'
 RSpec.feature 'Like posts', type: :feature do
   scenario 'Like post counter' do
     create_hello_world_post
+    expect(page).not_to have_content('1 Like')
     click_button 'Like'
     expect(page).to have_content('1 Like')
-    click_button 'Unlike'
-    expect(page).to have_content('0 Likes')
   end
 
   scenario 'Can unlike post' do
