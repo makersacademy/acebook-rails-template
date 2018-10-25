baaddbf4ff2a5218525a5344a1535636b5c1f9ea
Andres Faraone <andresfaraone@outlook.com>
2018-10-25 11:33:41 +0100

    finished tests for user and post features

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -7,4 +7,46 @@ RSpec.feature 'Sign up', type: :feature do
     expect(page).to have_selector('#user_email')
     expect(page).to have_selector('#user_password')
   end
+
+  scenario 'not logged in user redirected to login page' do
+    visit('/posts')
+    expect(page).to have_current_path('/users/sign_in')
+    expect(page).to have_selector('#user_email')
+    expect(page).to have_selector('#user_password')
+  end
+
+  scenario 'user can sign up' do
+    sign_up_helper
+    expect(find('.notice')).to have_content('Welcome! You have signed up successfully.')
+  end
+
+  context 'when User signs up' do
+    before { sign_up_helper }
+
+    scenario 'shows logout button' do
+      expect(page).to have_selector('#log-out')
+    end
+
+    scenario 'user can log out' do
+      click_on('Logout')
+      expect(find('.notice')).to have_content('Signed out successfully.')
+    end
+  end
+
+  context 'existing user' do
+    before do
+      sign_up_helper
+      click_on('Logout')
+    end
+
+    scenario 'can sign in' do
+      log_in( 'test@email.com', 'Testing123')
+      expect(find('.notice')).to have_content('Signed in successfully.')
+    end
+
+    scenario 'cannot sign in if password wrong' do
+      log_in( 'test@email.com', 'password')
+      expect(find('.alert')).to have_content('Invalid Email or password.')
+    end
+  end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -5,10 +5,17 @@ def create_post(message)
   click_button 'Submit'
 end
 
-def sign_in_helper
+def sign_up_helper
   visit('/users/sign_up')
   fill_in('user_email', with: 'test@email.com')
+  fill_in('user_name', with: 'Test')
   fill_in('user_password', with: 'Testing123')
   fill_in('user_password_confirmation', with: 'Testing123')
   click_button('Sign up')
+end
+
+def log_in(email, password)
+  fill_in('user_email', with: email)
+  fill_in('user_password', with: password)
+  click_button('Log in')
 end
