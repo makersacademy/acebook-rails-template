9e6757ae7168e3bc9ce8f6f2667ddca1807a6565
Alex Kharouk <akharoukpp@gmail.com>
2018-10-25 13:13:30 +0100

    lint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome_page#timeline'
-  get 'welcome', to: "welcome_page#welcome"
+  get 'welcome', to: 'welcome_page#welcome'
 
   resources :welcome_page
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
