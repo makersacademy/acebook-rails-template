2cf7326b26b60f11a18d26edca8e8c091a806260
tom miller <thomas.j.c.miller@outlook.com>
2018-10-26 12:35:22 +0100

    added time stamp to posts

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -3,16 +3,23 @@ require 'rails_helper'
 RSpec.feature "Post", type: :feature do
   scenario "Can submit posts and view them" do
     postAndClick
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
+    fillPostAndSubmit("Hello, world!")
     expect(page).to have_content("Hello, world!")
   end
 
   scenario "Can add posts with newlines" do
     postAndClick
-    fill_in "Message", with: "Hello\nNewLine here"
-    click_button "Submit"
+    fillPostAndSubmit("Hello\nNewLine here")
     expect(page).to have_content("Hello\nNewLine here")
+  end
+
+  scenario "Post has a time stamp" do
+    t = Time.now.utc
+    time = t.strftime("%H:%M %Y-%m-%d")
+    postAndClick
+    fillPostAndSubmit("time stamp")
+    expect(page).to have_content("time stamp")
+    expect(page).to have_content(time)
   end
 
 end
