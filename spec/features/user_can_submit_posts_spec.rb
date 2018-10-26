1c0be00042f14a24aa48d8d29a967e1ed18d2f7b
alakijayo <alakijaayo@gmail.com>
2018-10-26 16:24:01 +0100

    Updating tests

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 RSpec.feature "Post", type: :feature do
+  before(:each) do
+    successfulSignUp
+  end
   scenario "Can submit posts and view them" do
-    postAndClick
-    fillPostAndSubmit("Hello, world!")
+    newPostAndSubmit("Hello, world!")
     expect(page).to have_content("Hello, world!")
   end
 
   scenario "Can add posts with newlines" do
-    postAndClick
-    fillPostAndSubmit("Hello\nNewLine here")
+    newPostAndSubmit("Hello\nNewLine here")
     expect(page).to have_content("Hello\nNewLine here")
   end
 
   scenario "Post has a time stamp" do
     t = Time.now.utc
     time = t.strftime("%H:%M %Y-%m-%d")
-    postAndClick
-    fillPostAndSubmit("time stamp")
+    newPostAndSubmit("time stamp")
     expect(page).to have_content("time stamp")
     expect(page).to have_content(time)
   end
