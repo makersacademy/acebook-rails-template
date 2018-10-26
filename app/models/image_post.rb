136723822f63aff70d2aaf3075bd31645a470ac2
Alex Kharouk <akharoukpp@gmail.com>
2018-10-26 13:46:32 +0100

    create pic boundaries

--- app/models/image_post.rb
+++ app/models/image_post.rb
@@ -2,4 +2,11 @@
 
 class ImagePost < ApplicationRecord
   mount_uploader :picture, PictureUploader
+  validates_processing_of :image
+  validate :image_size_validation
+
+  private
+    def image_size_validation
+      errors[:picture] << "should be less than 1MB" if image.size > 1.megabytes
+    end
 end
