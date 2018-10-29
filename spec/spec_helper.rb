77842a2ad1f9eba37be4d2f5d5c70b51b879997d
Cameron Whitehead <camjw119@gmail.com>
2018-10-29 09:27:53 +0000

    Fixed bug and no rubocop offenses

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -33,6 +33,7 @@ end
 
 require_relative 'helpers/web_helpers'
 RSpec.configure do |config|
+  config.raise_errors_for_deprecations!
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
