e63e1b6fe6101b7d7a002e0f2ff4da7c30a12ead
Cameron Whitehead <camjw119@gmail.com>
2018-10-29 14:33:45 +0000

    Should be good coverage now

--- app/helpers/image_posts_helper.rb
+++ app/helpers/image_posts_helper.rb
@@ -2,10 +2,6 @@
 
 # Helper functions for the ImagePosts controller
 module ImagePostsHelper
-  def find_image_post_email(post)
-    User.find(post.user_id).email
-  end
-
   def get_images_by_user(user_id)
     ImagePost.where(user_id: user_id)
   end
