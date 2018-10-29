937d8e4de7fdffaefa8f10e903265aa4df9363ac
Cameron Whitehead <camjw119@gmail.com>
2018-10-29 11:58:58 +0000

    Added more top level comments

--- db/migrate/20181026134728_add_user_id_to_image_table.rb
+++ db/migrate/20181026134728_add_user_id_to_image_table.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Adds the user id to the image table
 class AddUserIdToImageTable < ActiveRecord::Migration[5.1]
   def change
     add_column :image_posts, :user_id, :integer
