608ca3219d99e8787c0d24b1b0bc57724be92bea
Andres Faraone <andresfaraone@outlook.com>
2018-10-29 12:15:39 +0000

    Refactored posts into a collection partial

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET /id" do
+    context "param id given" do
+      it "responds with 200" do
+        get :show, params: {id: 1}
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "no param id given" do
+      it "responds with 200" do
+        get :show
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
