b0428aeb674c1df9ce950d18b4808899cd000495
olliebaum <oliver@baums.org.uk>
2018-10-31 14:04:21 +0000

    User's name no longer needs to be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,6 @@ class User < ApplicationRecord
     ]
   end
 
-  validates :name, presence: true, uniqueness: true,
+  validates :name, presence: true,
     format: { with: /\A[^0-9]*\z/, message: "cannot contain numbers" }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     describe 'username' do
-      it 'is invalid if not unique' do
-        User.create(name: "Bill", email: "bill@bill.com", password: "billbill")
-        user = User.new(name: "Bill", email: "bill2@bill.com", password: "billbill")
-        expect(user.valid?).to be false
-      end
-      fit 'invalid if contains numbers' do
+      it 'invalid if contains numbers' do
         user = User.new(name: "Bill Ben7", email: "bill2@bill.com", password: "billbill")
         expect(user.valid?).to be false
       end
