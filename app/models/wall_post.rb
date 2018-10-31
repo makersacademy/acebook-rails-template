18daf75336eb59b48141d0688762d255dabb77ee
Darcie <darciewalsh@hotmail.com>
2018-10-31 13:05:52 +0000

    Can see the sender's name on a wall post

--- app/models/wall_post.rb
+++ app/models/wall_post.rb
@@ -1,3 +1,9 @@
 class WallPost < ApplicationRecord
   belongs_to :user
+  belongs_to :sender, :class_name => 'User'
+
+  def format_date
+    full_time = created_at
+    full_time.strftime('%A, %-d %b %Y at %-I:%M %p')
+  end
 end
--- spec/features/user_can_post_on_wall_spec.rb
+++ spec/features/user_can_post_on_wall_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'timecop'
 
 RSpec.feature 'Profile page', type: :feature do
   before(:each) do
@@ -13,12 +14,23 @@ RSpec.feature 'Profile page', type: :feature do
 
     scenario 'Can click on link and view new wall post form' do
       click_on('Post to this wall')
-      expect(page).to have_content('Write your post here')
+      expect(find('h1.wall-post-title')).to have_content('Write your wall post here')
     end
 
-    scenario 'Can post to someones wall and is redirected back to the profile wall' do
+    scenario 'Can post to someones wall and is redirected back to that profile wall' do
       create_wall_post('A message to my friend')
-      expect(page).to have_content('A message to my friend')
+      expect(first('p.wall-post-message')).to have_content('A message to my friend')
+    end
+
+    scenario 'Can see the time when a wall post was posted' do
+        Timecop.freeze(Time.zone.parse('13:00 3 October 2018'))
+        create_wall_post('A message to my friend')
+        expect(find('p#1.wallpost-timestamp')).to have_content("Wednesday, 3 Oct 2018 at 1:00 PM")
+    end
+
+    scenario 'Can see the sender\'s name on a wall post' do
+      create_wall_post('A message to my friend')
+      expect(find('div.wall-post-wrapper')).to have_selector('a#1.wall-post-author')
     end
   end
 end
