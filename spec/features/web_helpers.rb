2dafe328c83efb2538fe52d28e4d549696b20bed
Andres Faraone-Pirie <andresfaraone@outlook.com>
2018-11-01 10:21:53 +0000

    Update web_helpers.rb

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -33,6 +33,7 @@ def create_comment(message)
   click_on('Comment')
   fill_in('comment[message]', with: message)
   click_button("Submit")
+end
 
 def create_wall_post(message)
   visit '/testname'
