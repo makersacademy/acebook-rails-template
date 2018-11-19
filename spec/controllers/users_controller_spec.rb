64d85cd8094472617b2591c47cee130b01054c8f
shannongamby <shannongamby@hotmail.com>
2018-11-19 16:53:37 +0000

    user registration

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,4 +9,23 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  describe "POST /" do
+    it "responds with 200" do
+      post :create, params: { user: { name: "Jeff", email: "jeff@hotmail.com", password: "password1" } }
+      expect(response).to redirect_to(users_registration_confirmation_url)
+    end
+
+  it "creates a post" do
+    post :create, params: { user: { name: "Jeff", email: "jeff@hotmail.com", password: "password1" } }
+    expect(User.find_by(name: "Jeff")).to be
+  end
+end
+
+describe "GET /" do
+  it "responds with 200" do
+    get :index
+    expect(response).to have_http_status(200)
+  end
+end
+
 end
