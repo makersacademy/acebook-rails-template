d7b21ae844d78511b25a6d255970ca961cf34a3b
rbbri <robin.s.johnson@gmail.com>
2018-11-19 17:50:15 +0000

    Add routes for user model
    - Add a sign up page

--- Gemfile
+++ Gemfile
@@ -7,10 +7,11 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'bcrypt'
+
 gem 'coveralls', require: false
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.1'
+gem 'bcrypt'
 # Use postgresql as the database for Active Record
 gem 'pg'
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,29 @@
 
 # user controller
 class UsersController < ApplicationController
-  def new; end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+
+      else
+        render 'new'
+      end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+    :password_confirmation)
+  end
+
 end
