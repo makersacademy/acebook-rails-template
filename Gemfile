f0d46c0ac388076d50b9d02b5d9a3a6eb8f09ae2
rbbri <robin.s.johnson@gmail.com>
2018-11-19 15:34:28 +0000

    Add user model and controller
    Adds tests for name and email validation

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ end
 gem 'rails', '~> 5.1.1'
 # Use postgresql as the database for Active Record
 gem 'pg'
+
+gem 'pry'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+
+RSpec.describe User, type: :model do
+  before do
+    @user = User.new(name:'Example', email:'user@example.com')
+  end
+  it 'should be valid' do
+    expect(@user.valid?).to eq(true)
+  end
+  it 'name should be present' do
+    @user.name = ""
+    expect(@user.valid?).to eq(false)
+  end
+  it 'name should not be too long' do
+    @user.name = "n" * 51
+    expect(@user.valid?).to eq(false)
+  end
+  it 'email should be present' do
+    @user.email = ""
+    expect(@user.valid?).to eq(false)
+  end
+  it 'email should not be too long' do
+    @user.email = "n" * 244 + '@example.com'
+    expect(@user.valid?).to eq(false)
+  end
+end
