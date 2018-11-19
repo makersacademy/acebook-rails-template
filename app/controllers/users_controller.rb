8a0cfb5e99e0223688e2cbe7be8ae624f6e45f14
Todd Langford-Archer <toddpla@gmail.com>
2018-11-19 16:20:19 +0000

    user can now register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to users_url
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :users
   resources :posts
 end
