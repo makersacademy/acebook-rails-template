36fcdd39671f1b45d0c76fd899c7af78aad734dc
felixjtdb <felixjtdb@gmail.com>
2018-11-20 16:04:27 +0000

    test passing

--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -6,11 +6,10 @@ RSpec.feature "Timeline", type: :feature do
     click_link 'Sign Up'
     expect(page).to have_current_path("/users/sign_up")
     # fill_in "username_textbox", with: "username123"
-    fill_in "field[:email]", with: "user@user.com"
-    fill_in "password", with: "pword123"
-    fill_in "password_confirmation", with: "pword123"
+    fill_in "user[email]", with: "user@user.com"
+    fill_in "user[password]", with: "pword123"
+    fill_in "user[password_confirmation]", with: "pword123"
     click_button "Sign up"
-    expect(page).to have_content("Logged in as username123")
+    expect(page).to have_content("Log Out from user@user.com")
   end
-
 end
