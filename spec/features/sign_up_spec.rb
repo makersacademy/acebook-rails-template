6a6840720ac527ee631543a2e0d0104e29853e4f
jebax <joel.eliot.baxter@gmail.com>
2018-11-20 12:43:26 +0000

    users can sign up

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Sign up", type: :feature do
+  let(:user) { build(:user) }
+
+  before do
+    visit root_path
+    find('a', text: 'Sign up').click
+  end
+
+  scenario 'user can navigate to sign up page' do
+    expect(page).to have_current_path '/users/sign_up'
+  end
+
+  scenario 'user can sign up successfully' do
+    save_and_open_page
+    fill_in 'user[name]', with: user.name
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: user.password
+    click_button 'Sign up'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+end
