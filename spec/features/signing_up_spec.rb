8349a0973edc8aabe8741410be68c9a49233723d
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-20 15:31:19 +0000

    on sign up, password must be longer than 6 chars
    file was also renamed

---
+++ spec/features/signing_up_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Signing Up", type: :feature do
+  scenario "successful sign up" do
+    visit('/')
+    click_link('Register')
+    fill_in(:user_name, with: "user1")
+    fill_in(:user_email, with: "user1@test.com")
+    fill_in(:user_password, with: "password1")
+    fill_in(:user_password_confirmation, with: "password1")
+    click_button 'Sign up'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario "password must be longer than 6 characters" do
+    visit('/')
+    click_link('Register')
+    fill_in(:user_name, with: "user1")
+    fill_in(:user_email, with: "user1@test.com")
+    fill_in(:user_password, with: "1")
+    fill_in(:user_password_confirmation, with: "1")
+    click_button 'Sign up'
+    expect(page).to have_content 'Password is too short (minimum is 6 characters)'
+  end
+
+end
