32e49fc45c8c8efc928f944fb0aecdba78781fde
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-20 14:40:29 +0000

    Can press enter and not submit form and then view format on index

---
+++ spec/features/new_lines_in_posts_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+ #js: true add to rspec if needed
+
+RSpec.feature "New Post", type: :feature do
+#   scenario "Can create posts with new lines" do
+#     visit "/posts"
+#     click_link "New post"
+#     find('#post_message').set("hello\n")
+#     # fill_in "Message", with: "Hello, world!"
+#     # find('#post_message').send_keys(:enter)
+#     # fill_in "Message", with: "Bye world!"
+#     expect(current_path).to eq '/posts/new'
+#   end
+    scenario "Can create posts with new lines" do
+      visit "/posts"
+      click_link "New post"
+      fill_in "Message", with: "Hello, world\nBye world"
+      click_button "Submit"
+      expect(page).to have_content "Hello, world\nBye world"
+    end
+end
