a710d6225676f19885f4830c14b4934f642e4855
jebax <joel.eliot.baxter@gmail.com>
2018-11-20 16:28:03 +0000

    permitted name parameters for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
-  # def method_name
-  #
-  # end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
