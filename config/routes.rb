99b5deee2aa450fd68d5b0a3cb32b2070e5c502f
Chris Cooksley <christopher.cooksley@gmail.com>
2018-11-20 14:49:05 +0000

    signup with name

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "users/registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
+  get '/pages/test', to: 'pages#index'
   resources :posts
 end
