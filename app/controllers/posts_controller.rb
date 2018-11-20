f4cc462831fe016f1a20f78af7f927b7604e3fd7
Chloe Verity <chloeverity@hotmail.co.uk>
2018-11-20 13:46:12 +0000

    fixing rubocop infractions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,6 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-
   def create
     @post = Post.create(post_params)
 
@@ -36,7 +35,6 @@ class PostsController < ApplicationController
       render 'edit'
     end
   end
-
 
   def destroy
     @post = Post.find(params[:id])
--- spec/features/user_can_edit_posts_spec.rb
+++ spec/features/user_can_edit_posts_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and edit them" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "post[message]", with: "This is a test post!"
-    click_button "Create Post"
-    visit "/posts"
-    click_link "Edit"
-    fill_in "post[message]", with: "This is an edited post!"
-    click_button "Update Post"
-    expect(page).to have_content("This is an edited post!")
+RSpec.feature 'Timeline', type: :feature do
+  scenario 'Can submit posts and edit them' do
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'post[message]', with: 'This is a test post!'
+    click_button 'Create Post'
+    visit '/posts'
+    click_link 'Edit'
+    fill_in 'post[message]', with: 'This is an edited post!'
+    click_button 'Update Post'
+    expect(page).to have_content('This is an edited post!')
   end
 end
