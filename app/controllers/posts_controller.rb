185b2bda10f122e64887797af3c59d759d68299c
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-21 14:31:19 +0000

    fix indent

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,7 @@ class PostsController < ApplicationController
   end
 
   def show
-  p  @post = Post.find(params[:id])
-
+    @post = Post.find(params[:id])
   end
 
   def new
--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.feature 'Sign out', type: :feature do
 
-   before do
-     user = create(:user)
-     login_as(user, scope: :user)
-     visit root_path
-    end
-    scenario 'user can sign out successfully' do
-      click_link 'Sign out'
-      expect(page).to have_content 'Log in'
-    end
+  before do
+    user = create(:user)
+    login_as(user, scope: :user)
+    visit root_path
+  end
+
+  scenario 'user can sign out successfully' do
+    click_link 'Sign out'
+    expect(page).to have_content 'Log in'
+  end
 end
