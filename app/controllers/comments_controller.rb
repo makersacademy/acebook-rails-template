64966e77c5a927f27e209ccdad584884bbe86e2a
Alfie Freeman <runticle@gmail.com>
2018-11-21 12:03:53 +0000

    adds edit and destroy methods to comment controller and adds edit route to config file
    Co-Authored-By: Imogen Misso <imogenmisso@users.noreply.github.com>
    Co-Authored-By: Ray Smith <rsmith88@users.noreply.github.com>
    Co-Authored-By: Shannon Gamby <shannongamby@users.noreply.github.com>
    Co-Authored-By: Todd Langford-Archer <toddpla@gmail.com>

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,10 +9,30 @@ class CommentsController < ApplicationController
     redirect_to posts_url
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.editable?
+      @comment.update(comment: params[:post][:comment])
+    else
+      flash[:danger] = "fuck off, this is not yours!"
+    end
+    redirect_to posts_url
+  end
+
   private
 
   def post_comment_params
     params.require(:comment).permit(:message)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,12 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
 
   get 'posts/:id/edit', to: 'posts#edit'
+  
+  get 'comments/:id/edit', to: 'comments#edit'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
   resources :posts do
     resources :comments
-  end 
+  end
 end
