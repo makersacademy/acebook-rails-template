e9d4d232968c7a0d766099aac9a360e1bf05ed10
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-21 11:44:22 +0000

    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "users/registrations"}
+  devise_for :users, :controllers => {
+    :registrations => "users/registrations",
+    :sessions => "users/sessions"
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
   resources :posts
