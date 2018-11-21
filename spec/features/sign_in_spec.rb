a9e304009a933c8efb531ecb57d7824cc7305816
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-21 15:22:17 +0000

    refactored

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -4,30 +4,21 @@ require 'rails_helper'
 
 RSpec.feature 'Signing in', type: :feature do
   scenario 'successful sign in' do
-    visit('/')
-    click_link('Login')
-    fill_in(:user_email, with: 'test@user.com')
-    fill_in(:user_password, with: 'qwerty')
-    click_button 'Log in'
+    login(email: 'test@user.com', password: 'qwerty')
+
     expect(page).to have_content 'Signed in successfully.'
     expect(current_path).to eq '/posts'
   end
 
   scenario 'incorrect email' do
-    visit('/')
-    click_link('Login')
-    fill_in(:user_email, with: 'test@uer.com')
-    fill_in(:user_password, with: 'qwerty')
-    click_button 'Log in'
+    login(email: 'test@uer.com', password: 'qwerty')
+
     expect(page).to have_content 'Invalid Email or password'
   end
 
   scenario 'incorrect password' do
-    visit('/')
-    click_link('Login')
-    fill_in(:user_email, with: 'test@user.com')
-    fill_in(:user_password, with: 'qwrty')
-    click_button 'Log in'
+    login(email: 'test@user.com', password: 'qwrty')
+    
     expect(page).to have_content 'Invalid Email or password'
   end
 end
