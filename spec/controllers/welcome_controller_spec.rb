356634500bb49c75ca135546ff99b8eba36e02c4
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-21 14:44:44 +0000

    update welcomecontroller spec

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
+    it "returns http success if logged in" do
       user = create(:user)
-      login_as(user, scope: :user)
+      sign_in user
       get :index
       expect(response).to have_http_status(:success)
     end
