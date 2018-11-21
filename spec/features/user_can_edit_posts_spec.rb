1b341a74bbe71c980bd639593982838789e95363
Chloe Verity <chloeverity@hotmail.co.uk>
2018-11-21 14:05:57 +0000

    adding commenting capability

--- spec/features/user_can_edit_posts_spec.rb
+++ spec/features/user_can_edit_posts_spec.rb
@@ -14,4 +14,17 @@ RSpec.feature 'Timeline', type: :feature do
     click_button 'Update Post'
     expect(page).to have_content('This is an edited post!')
   end
+  scenario 'Cannot edit others posts' do
+    user = create(:user)
+    login_as(user, scope: :user)
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'post[message]', with: 'This is a test post!'
+    click_button 'Create Post'
+    second_user = create(:user, email: 'joel@test.com', name: 'Joel', password: 'password123')
+    login_as(second_user, scope: :user)
+    visit '/posts'
+    click_link 'Show'
+    expect(page).not_to have_content('Edit')
+  end
 end
