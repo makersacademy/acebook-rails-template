c3d54b88e64dc635e7617d78ae9c250668871512
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-21 11:24:05 +0000

    added test for incorrect email

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,11 +3,19 @@ require 'rails_helper'
 RSpec.feature "Signing in", type: :feature do
   scenario "successful sign in" do
     visit('/')
-    save_and_open_page
     click_link("Login")
     fill_in(:user_email, with: "test@user.com")
     fill_in(:user_password, with: "qwerty")
     click_button 'Log in'
     expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario "incorrect email" do
+    visit('/')
+    click_link("Login")
+    fill_in(:user_email, with: "test@uer.com")
+    fill_in(:user_password, with: "qwerty")
+    click_button 'Log in'
+    expect(page).to have_content 'Invalid Email or password'
   end
 end
