6d9629df4de393a1de0561f00485e2c14b6841a6
Caitlin Cooling <ccooling97@gmail.com>
2018-11-21 14:25:12 +0000

    Feature test: - Logged in user can view posts

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -2,6 +2,11 @@ feature 'Authentication' do
   scenario 'a logged out user tries to visit the posts page' do
     visit '/posts'
     expect(page).to have_content "You must be logged in to access this section"
+  end
 
+  scenario 'a logged in user tried to visit the posts page' do
+    sign_up
+    click_link 'Timeline'
+    expect(page).to have_content "New post"
   end
 end
