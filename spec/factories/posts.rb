d2ada85b7d6efa5b714c028ad94dab75ba501377
Raylene <smith.raylene@gmail.com>
2018-11-21 11:18:22 +0000

    Integrated with factory girl and model spec
    Co-Authored-By: Alfie <runticle@gmail.com>
    Co-Authored-By: Imogen <imogenmisso@hotmail.co.uk>
    Co-Authored-By: Shannon <shannongamby@hotmail.com>
    Co-Authored-By: Todd <toddpla@gmail.com>

---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :post do
+    user nil
+    message "Test post"
+  end
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # User.new(name: 'Jeff', email: 'jeff@hotmail.com', password: 'password1')
+  # Post.new(message: 'This is a post', user_id: 1)
+  #
+
+  subject do
+    user = FactoryGirl.create(:user)
+    post = FactoryGirl.create(:post, user: user)
+    described_class.new(comment: "This is a test", user_id: 1, post_id: 1)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a message' do
+    subject.comment = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a user_id' do
+    subject.user_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a post_id' do
+    subject.post_id = nil
+    expect(subject).to_not be_valid
+  end
+
+
 end
