a379d3bfb493122057f537af80e48e2d79755a26
jebax <joel.eliot.baxter@gmail.com>
2018-11-21 11:52:33 +0000

    added more tests for comment model

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
     body { "MyText" }
-    user { nil }
-    post { nil }
+    user
+    post
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Comment, type: :model do
     association = described_class.reflect_on_association(:post).macro
     expect(association).to eq :belongs_to
   end
+
+  it 'is valid when created' do
+    comment = build(:comment)
+    expect(comment).to be_valid
+  end
+
+  it 'is invalid if it does not have a body' do
+    expect(Comment.new(user_id: 1, post_id: 1, body: '')).not_to be_valid
+  end
 end
