53b7497d3a9e8daa8ef792e930dde6c63c2bcc2c
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-22 17:03:30 +0000

    unliking halfway done

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  $('.like-button').click(function(event) {
+    console.log(event)
+    event.preventDefault()
+  });
+  //
+  // function incrementLikes() {
+  //   var newLikes = parseInt(document.getElementById('likes-number').innerHTML) + 1;
+  //   document.getElementById('likes-number').innerHTML = newLikes;
+  // }
+
+});
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,13 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @like = @post.likes.create(like_params)
+    @like = @post.likes.create(user_id: current_user.id)
+    redirect_to posts_path
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @like.destroy
 
     redirect_to posts_path
   end
