4f6be011365f1104da21b3dcbd4247bf1e3ce855
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-22 15:54:44 +0000

    test

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -21,5 +21,5 @@ def login(email:, password:)
   click_link('Login')
   fill_in(:user_email, with: email)
   fill_in(:user_password, with: password)
-  click_button 'Log in'
+  click_button "Log in"
 end
