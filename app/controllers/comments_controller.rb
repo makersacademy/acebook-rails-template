9f3d48b11f4305786dd06c6e4e9b4f1ad22f1494
Chris Cooksley <christopher.cooksley@gmail.com>
2018-11-22 16:22:37 +0000

    need to finalise creation of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,4 +12,10 @@ class CommentsController < ApplicationController
     @comment = @post.comments.create(comment_params)
     redirect_to '/'
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
