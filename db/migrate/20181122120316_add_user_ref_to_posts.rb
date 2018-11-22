8800ab593caa5a4ef77685284ceea282ac9c3cac
muge <muge.hasilci@hotmail.com>
2018-11-22 12:14:36 +0000

    link database

---
+++ db/migrate/20181122120316_add_user_ref_to_posts.rb
@@ -0,0 +1,7 @@
+class AddUserRefToPosts < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :posts, :user, foreign_key: true
+  end
+end
+
+#rails g migration AddUserRefToPosts user:references
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181121153023) do
+ActiveRecord::Schema.define(version: 20181122120316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20181121153023) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 20181121153023) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
