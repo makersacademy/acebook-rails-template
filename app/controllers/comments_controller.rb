ab8bd0b6ff74d98a6275eb9318290f1994cafcd1
Chloe Verity <chloeverity@hotmail.co.uk>
2018-11-22 16:11:39 +0000

    set up edit and delete *INCOMPLETE*

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,23 +4,38 @@ class CommentsController < ApplicationController
     @comment = Comment.new
   end
 
+  def edit
+      @post = Post.find(params[:id])
+      @comment = Comment.find(params[:id])
+  end
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
 
-    redirect_to post_path(@post)
-  end
-  def edit
-      @post = Post.find(params[:id])
-      @comment = Comment.find(params[:post_id])
-  end
-  def update
-    @comment = current_user.comment.create(post_params)
-
     if @comment.save
-      redirect_to posts_url
+      redirect_to post_path(@post)
     else
       render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      redirect_to posts_url
+    else
+      render 'edit'
+    end
+
+    def destroy
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.find(params[:id])
+      @comment.destroy
+
+      redirect_to posts_path(@post)
     end
 
   end
