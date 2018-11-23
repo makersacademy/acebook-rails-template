532d8eee69edc84f1f2d7672ad32f430337f9f4d
Chloe Verity <chloeverity@hotmail.co.uk>
2018-11-23 16:24:42 +0000

    upping test coverage

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "" do
+    it "GET /" do
+      user = create(:user)
+      sign_in user
+      get :show, params: { id: user.id }
+      expect(response).to have_http_status(200)
+    end
+  end
 end
