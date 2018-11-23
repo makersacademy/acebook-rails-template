5c16ea4763fd2296e46e7653fa55d7ca0d3e44e2
Rupert Adams <RupertNCAdams@aol.com>
2018-11-23 12:04:45 +0000

    added flash for invalid user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to posts_url, notice: "Logged in!"
     else
-      flash.now.alert = "Email or password is invalid."
+      flash[:alert] = "Email or password is invalid."
+      render action: 'new'
     end
   end
 
