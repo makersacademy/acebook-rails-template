dfccfdc128898768ddb9d1a274c11e5bef855aa0
Chloe Verity <chloeverity@hotmail.co.uk>
2018-11-23 15:26:33 +0000

    feature test for editing comment

---
+++ spec/features/edit_comment_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature 'Editing Comment', type: :feature do
+  scenario 'making a comment' do
+    sign_in_and_create_post
+    click_link 'Comment'
+    fill_in 'comment[body]', with: 'A comment'
+    click_button('Comment')
+    click_link 'Edit comment'
+    fill_in 'comment[body]', with: 'An edited comment'
+    click_button('Update Comment')
+    expect(page).to have_content('An edited comment')
+  end
+end
