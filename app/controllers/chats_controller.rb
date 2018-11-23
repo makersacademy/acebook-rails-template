10dbf73a5b1d3177bb9d5cb84a518933970cd49c
Todd Langford-Archer <toddpla@gmail.com>
2018-11-23 18:03:34 +0000

    chat box styling
    pulled to the bottom
    classes added
    
    Co-Authored-By: Alfie <runticle@gmail.com>
    Co-Authored-By: Imogen Misso <imogenmisso@users.noreply.github.com>
    Co-Authored-By: Ray Smith <rsmith88@users.noreply.github.com>
    Co-Authored-By: Shannon Gamby <shannongamby@users.noreply.github.com>

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,16 +1,14 @@
 class ChatsController < ApplicationController
 
   def create
-    Chat.create(chat_params)
+    chat = Chat.create(chat_params)
     room = "Group Chat"
     ActionCable.server.broadcast(
       "chat_#{room}",
-      sent_by: current_user.name,
-      body: chat_params[:message]
+      sent_by: chat.user.name,
+      body: chat.message
     )
   end
-
-  
 
   def chat_params
     params.require(:chat).permit(:message).merge(user_id: current_user.id)
