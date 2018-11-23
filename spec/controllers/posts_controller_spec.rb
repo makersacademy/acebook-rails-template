595845705316bbc29f0b0f818afc4a55d694be4c
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-23 16:26:07 +0000

    add test for posts controller add remove likes

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe "POST /" do
-    it "responds with 200" do
+    it "redirects to posts url when post is created" do
       user = create(:user)
       sign_in user
       post :create, params: { post: { message: "Hello, world!" } }
@@ -20,7 +20,8 @@ RSpec.describe PostsController, type: :controller do
       user = create(:user)
       sign_in user
       post :create, params: { post: { message: "Hello, world!" } }
-      get :show
+      post_id = Post.find_by(message: "Hello, world!").id
+      get :show, params: { id: post_id }
       expect(response).to have_http_status(200)
     end
   end
@@ -43,7 +44,13 @@ RSpec.describe PostsController, type: :controller do
     end
 
     it "removes a like" do
-
+      user = create(:user)
+      sign_in user
+      post :create, params: { post: { message: "Hello, world!" } }
+      post_id = Post.find_by(message: "Hello, world!").id
+      post :like, params: { like: true, id: post_id }
+      post :like, params: { like: true, id: post_id }
+      expect(Post.find_by(message: "Hello, world!").likes.size).to be 0
     end
   end
 end
