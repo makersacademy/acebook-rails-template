b4aeb979c8a731435ae3ee5f1aaf976b186c3579
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-11-23 17:06:29 +0000

    user_id is now saved to post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      p session[:user_id]
       redirect_to posts_url, notice: "Logged in!"
     else
       flash[:alert] = "Email or password is invalid."
@@ -14,7 +13,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    p session[:user_id]
     redirect_to '/', notice: 'Logged out!'
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(post_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to posts_url, notice: 'User was successfully created'
     else
       # Later, we should try to expand this to make the flash alerts unique
