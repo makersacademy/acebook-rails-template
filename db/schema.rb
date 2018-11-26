94e9ad78d7e81376ec5dcf30db8bd2dc6bfe6d46
rbbri <robin.s.johnson@gmail.com>
2018-11-26 16:25:42 +0000

    Style User Index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181126102539) do
+ActiveRecord::Schema.define(version: 20181126103030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
