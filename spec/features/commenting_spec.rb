b6723fcd67482b039980e425bbf64e2a2831c022
NaomiBid <naomi0bid@gmail.com>
2018-11-26 10:55:23 +0000

    added comments to post index

--- spec/features/commenting_spec.rb
+++ spec/features/commenting_spec.rb
@@ -18,4 +18,13 @@ RSpec.feature 'Commenting', type: :feature do
   #   click_button 'Comment'
   #   expect(page).to have_current_path new_post_comment_path(post)
   # end
+
+  scenario "viewing comments on the index page" do
+    sign_in_and_create_post
+    click_link 'Comment'
+    fill_in 'comment[body]', with: 'A comment'
+    click_button('Comment')
+    click_link('Back')
+    expect(page).to have_content('A comment')
+  end
 end
