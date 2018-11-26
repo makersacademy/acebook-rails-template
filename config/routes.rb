ba374b39760e8a6a468021ef1b2d2b5eb4b9fcf7
becc-mu <becc.mulugeta@gmail.com>
2018-11-26 16:07:10 +0000

    added user.id page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     get '/login', to: 'sessions#new'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
+    get '/:id', to: 'users#show'
     resources :users
     resources :posts do
       resources :comments
