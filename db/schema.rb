3fd7d87feef7477e1e3a2dc2612013e91890f56f
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-11-26 11:13:00 +0000

    added foreign key to comments post_id and user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181121122525) do
+ActiveRecord::Schema.define(version: 20181126110448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
@@ -36,5 +44,7 @@ ActiveRecord::Schema.define(version: 20181121122525) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
