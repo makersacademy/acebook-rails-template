f40acff805d7d176b100af1fec95256039e4ff0e
Becka <rebecca.lelew@gmail.com>
2018-11-26 13:47:58 +0000

    comments added to database

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,10 @@
 class CommentsController < ApplicationController
 
   def new
-    puts 1
     @comment = Comment.new
   end
 
   def create
-    puts 2
     @comment = Comment.create(comment_params.merge({user_id: current_user.id}))
     redirect_to posts_url
   end
@@ -14,10 +12,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    puts 3
-    puts params
-    puts params.comment
-    params.require(:comment).permit(:comment_text, :post_id)
+    params.permit(:text, :post_id)
   end
 
 end
