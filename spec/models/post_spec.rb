1306e4e7c17c975b92a37642830c647c5643a779
Caitlin Cooling <ccooling97@gmail.com>
2018-11-26 15:43:00 +0000

    Add wall_id column to post: - unit rtest for model - Create new migration to add column - ran db:migrste which updated schema

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,4 +7,9 @@ describe 'Post' do
     post = Post.new(message: 'Test message 1')
     expect(post.message).to eq 'Test message 1'
   end
+
+  it 'has a wall id' do
+    post = Post.new(message: 'Test message 1', wall_id: 1)
+    expect(post.wall_id).to eq 1
+  end
 end
