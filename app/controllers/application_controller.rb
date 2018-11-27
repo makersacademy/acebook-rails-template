c5b1428946ddc4d51f11c39f8c600c9a53dd0908
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-11-27 11:58:14 +0000

    Redirect to timeline after sign in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
+  def after_sign_in_path_for(user)
+    user_page_path(current_user.id)
+  end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,15 +2,13 @@ require_relative '../models/post.rb'
 class UserController < ApplicationController
 
   def user
-    p params
     @posts = get_posts_by_id(params[:id])
-    p "here are our posts:  "
-    p @posts
     render :file => '../views/timeline.html.erb'
   end
 
   def get_posts_by_id(id)
-    Post.where(user_id: id)
+    @user = User.find(id)
+    @user.posts
   end
 
 end
