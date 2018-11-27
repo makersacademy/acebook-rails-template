5e5be0525323f03036046fc971f753950592ae79
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-27 11:06:04 +0000

    update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -15,9 +15,6 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "production" }
 
-bind "unix:///var/run/puma/my_app.sock"
-pidfile "/var/run/puma/my_app.sock"
-
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
