1820beba6477e62b98ba25930ddb1fcd489bc613
Chloe Verity <chloeverity@hotmail.co.uk>
2018-11-27 12:22:25 +0000

    Testing for chat room routes

--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -1,7 +1,12 @@
 module FeatureHelpers
-  def sign_in_and_create_post
+  def sign_in_as_user
     user = create(:user)
     login_as(user, scope: :user)
+    visit '/'
+  end
+
+  def sign_in_and_create_post
+    sign_in_as_user
     visit '/posts'
     click_link 'New post'
     fill_in 'post[message]', with: 'This is a test post!'
