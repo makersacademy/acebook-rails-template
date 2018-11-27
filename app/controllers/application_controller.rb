d4b88153e29a3b097fb4d7dbba64aaf5280b091e
muge <muge.hasilci@hotmail.com>
2018-11-27 12:20:41 +0000

    Finally sign out button is working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-     session[:user_id] ? @current_user ||= User.find(session[:user_id]) : nil
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 end
