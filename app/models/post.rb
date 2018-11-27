05e9db09d14163ed3403359603cdc7c2d611f725
Reka <rekapap28@gmail.com>
2018-11-27 14:34:16 +0000

    adds sanitize

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,11 @@
 require 'time'
 
 class Post < ApplicationRecord
+  require 'sanitize'
+  before_save :sanitize_message
   belongs_to :user
 
+  def sanitize_message
+    Sanitize.fragment(message, Sanitize::Config::RELAXED)
+  end
 end
