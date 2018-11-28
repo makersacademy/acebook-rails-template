a051209e77e0b2c4d026c469dd0733e6d42c41ef
felixjtdb <felixjtdb@gmail.com>
2018-11-28 16:44:12 +0000

    username can be used to find wall, redirect to custom error page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,12 +11,15 @@ class UserController < ApplicationController
 
   def get_posts_by_id(id)
     begin
-    @user = User.find(id)
+      @user = User.find(id)
+    rescue
+      begin
+        @user = User.find_by(username: id)
+      rescue
+        redirect_to '/errors/invalid-user-id'
+      end
+    end
     @posts = @user.posts
     render :file => '../views/timeline.html.erb'
-    rescue
-      redirect_to '/errors/invalid-user-id'
-    end
   end
-
 end
