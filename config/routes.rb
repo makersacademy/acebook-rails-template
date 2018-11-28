4598cbde687d687b57c554eeeaa5f18d837fa6ce
Felix Dalby Bowler <felixjtdb@gmail.com>
2018-11-28 14:29:15 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { sign_up: 'registrations' }
   root to: 'posts#index'
   resources :posts, :shallow => true do
-    resources :comments do
-      member do
-        post 'like'
-      end
-    end
+    resources :comments
+    resources :likes
   end
   get '/:id', to: 'user#user', as: :user_page
 end
