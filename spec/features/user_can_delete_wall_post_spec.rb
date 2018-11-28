97b053ae6096d94ac6cef64a858041129aa89a43
Caitlin Cooling <ccooling97@gmail.com>
2018-11-28 15:05:17 +0000

    Ensuring that you cannot delete someone else's post

--- spec/features/user_can_delete_wall_post_spec.rb
+++ spec/features/user_can_delete_wall_post_spec.rb
@@ -8,11 +8,18 @@ feature 'wall posts', type: :feature do
 
   feature 'delete wall post' do
     scenario 'user can delete wall post' do
-      # save_and_open_page
       click_link 'Delete'
       expect(page).not_to have_content 'Post on my wall'
       expect(page).to have_content 'Amy Smith - Wall'
     end
-  end
 
+    scenario 'user can only delete their own post' do
+      log_out
+      sign_up_as_second_user
+      click_link 'Users'
+      click_link 'Amy Smith'
+      click_link 'Delete'
+      expect(page).to have_content "You cannot delete someone else's post"
+    end
+  end
 end
