d02bd9ed83ae2e30f53e0018eb5d1669b1957521
Jordan Bailey <jordanbailey@Jordans-MBP.home>
2018-11-28 11:29:15 +0000

    added full user authnetication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,76 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /tweets
+  # GET /tweets.json
+  def index
+    @post = Post.all.order("created_at DESC")
+    @post = Post.new
+  end
+
+  # GET /tweets/1
+  # GET /tweets/1.json
+  def show
+  end
+
+  # GET /tweets/new
   def new
     @post = Post.new
   end
 
-  def create
-    @post = Post.create(post_params)
-    redirect_to posts_url
+  # GET /tweets/1/edit
+  def edit
   end
 
-  def index
-    @posts = Post.all
+  # POST /tweets
+  # POST /tweets.json
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to root_path, notice: 'post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tweets/1
+  # PATCH/PUT /tweets/1.json
+  def update
+    respond_to do |format|
+      if @post.update(tweet_params)
+        format.html { redirect_to @post, notice: 'post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tweets/1
+  # DELETE /tweets/1.json
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to tweets_url, notice: 'post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
 
-  def post_params
-    params.require(:post).permit(:message)
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:post)
+    end
 end
