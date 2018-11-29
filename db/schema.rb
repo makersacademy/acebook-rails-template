690df5a516fc9af29485b5672d6d803ff06d0a46
Chloe Verity <chloeverity@hotmail.co.uk>
2018-11-29 17:02:25 +0000

    rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_129_160_843) do
+ActiveRecord::Schema.define(version: 20181129160843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
