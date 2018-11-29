2fa95680b48b9f8a39896142f0ff0e92311a044b
jebax <joel.eliot.baxter@gmail.com>
2018-11-29 16:57:18 +0000

    cleaning up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_181_129_160_843) do
+ActiveRecord::Schema.define(version: 2018_11_29_154657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,11 +80,6 @@ ActiveRecord::Schema.define(version: 20_181_129_160_843) do
     t.datetime "updated_at", null: false
     t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
-  end
-
-  create_table "photos", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
