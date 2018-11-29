d02c52f4787e68c78d9d42c112e3d56f6bb4a9e9
rbbri <robin.s.johnson@gmail.com>
2018-11-29 11:43:26 +0000

    Works for timeline

--- spec/wall_spec.rb
+++ spec/wall_spec.rb
@@ -4,13 +4,16 @@ describe 'Wall' do
 
   let(:wall) {Wall.new(1)}
 
-  describe 'User Walls' do
-    it 'has a user id' do
-      expect(wall.id).to eq(1)
-    end
+  it 'has an empty array of posts at initialize' do
+    expect(wall.posts).to eq []
+  end
 
-    it 'has an empty array of posts at initialize' do
-      expect(wall.posts).to eq []
+  describe 'User Walls' do
+
+  let(:wall) {Wall.new(1)}
+
+    it 'has a wall id' do
+      expect(wall.id).to eq(1)
     end
 
     it 'loads a post made on the wall' do
@@ -26,6 +29,31 @@ describe 'Wall' do
       post2 = create(:post)
       wall.add_posts
       expect(wall.posts.length).to eq 2
+    end
+  end
+
+  describe 'Timeline' do
+
+  let(:timeline) {Wall.new("NULL")}
+
+    it "has an empty wall id" do
+      expect(timeline.id).to eq("NULL")
+    end
+
+
+    it 'loads a post made on the timeline' do
+      user = create(:user)
+      post = create(:post, wall_id: "NULL")
+      timeline.add_posts
+      expect(timeline.posts).to eq [post.message]
+    end
+
+    it 'loads posts made on a timeline' do
+      user = create(:user)
+      post1 = create(:post, wall_id: "NULL")
+      post2 = create(:post, wall_id: "NULL")
+      timeline.add_posts
+      expect(timeline.posts.length).to eq 2
     end
   end
 end
