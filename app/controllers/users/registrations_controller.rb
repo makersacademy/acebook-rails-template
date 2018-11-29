e7d314a28f82c3f6aa0e026caa6f75e333487481
Reka <rekapap28@gmail.com>
2018-11-29 16:11:41 +0000

    fixes devise model/controller isssue

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,4 @@
 require('devise')
 
 class RegistrationsController < Devise::RegistrationsController
-  devise :database_authenticatable, :registerable,
-       :recoverable, :rememberable, :trackable,
-       :validatable, authentication_keys: [:login, :sign_up]
 end
