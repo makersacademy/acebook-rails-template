3a1781cb05306bec85c09e7b0d0ac8768b89f763
Jordan Bailey <jordanbailey@Jordans-MacBook-Pro.local>
2018-11-30 10:33:18 +0000

    navbar stuff

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,7 +58,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to tweets_url, notice: 'post was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: 'post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
