15b4f554526ccd8c0c295017d6c2530feee73c3b
joshharris1 <josh.harris@cgi.com>
2018-12-04 14:45:45 +0000

    added post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 140 } #only allows user to add content maximum of 140 characters
+  default_scope -> { order(created_at: :desc) } #newest tweets / posts first
 end
