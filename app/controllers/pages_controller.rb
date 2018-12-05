a69db86be0c3cf241101d4e7ba8b8a302b9099c6
Jordan Bailey <jordanbailey@Jordans-MacBook-Pro.local>
2018-12-05 15:16:14 +0000

    add personalised user profiles depedning on who is logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,13 +3,23 @@ class PagesController < ApplicationController
   end
 
   def home
+    @posts = Post.all
+
   end
 
   def profile
     #grab the username from the url as # ID;
-    @username = params[:id]
-  end
+        if (User.find_by_username(params[:id]))
+          @username = params[:id]
+        else
+     # redirect to 404 (root for now)
+           redirect_to root_path, :notice=> "User not found!"
+        end
+
+   end
 
   def explore
+    @posts = Post.all
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ root 'pages#index'
 
   get 'home' => 'pages#home'
   get '/users/home' => 'pages#home'
-  get 'profile' => 'pages#profile'
-  get '/users/profile' => 'pages#home'
+  get '/user/:id' => 'pages#profile'
   get 'explore' => 'pages#explore'
   get '/users/explore' => 'pages#home'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
