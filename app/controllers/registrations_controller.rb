e1027f93202513fcd056ea2d5549f9aa3ab72825
luisatheodoro <theodoro.luisa@gmail.com>
2018-12-18 13:11:58 +0000

    Adding to sign up page first name and last name input field

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,14 +3,16 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit( :name,
+    params.require(:user).permit( :first_name,
+                                  :last_name,
                                   :email,
                                   :password,
                                   :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit( :name,
+    params.require(:user).permit( :first_name,
+                                  :last_name,
                                   :email,
                                   :password,
                                   :password_confirmation,
--- db/migrate/20181218093633_devise_create_users.rb
+++ db/migrate/20181218093633_devise_create_users.rb
@@ -4,9 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false, default: ""
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :first_name,               null: false, default: ""
+      t.string :last_name,                null: false, default: ""
+      t.string :email,                    null: false, default: ""
+      t.string :encrypted_password,       null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -37,9 +38,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :name,                 unique: true
-    add_index :users, :reset_password_token, unique: true
+    add_index :users,      :email,                 unique: true
+    add_index :users,      :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
