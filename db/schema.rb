63408267bdeb1147ca654a189f11d43154f6cf94
elly <elly.chan.mda@gmail.com>
2018-12-18 23:49:15 +0000

    alter user database with user profile details, add userprofile page, add form to receive params and store to users table, display user info in profile page

--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,13 @@ ActiveRecord::Schema.define(version: 20181217210944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string "message"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "user_name", default: ""
+    t.date "birthday"
+    t.string "avatar", default: ""
+    t.string "occupation", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
