870b6fcd7dd58e74aa886ae9ddc716787f18f257
luisatheodoro <theodoro.luisa@gmail.com>
2018-12-18 11:38:08 +0000

    fixing bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
+  # devise_for :users,
+  #            :skip => [:registrations, :sessions]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to: 'pages#index'
   devise_scope :user do
--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20181218093633) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
