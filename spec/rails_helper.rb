91b2c9e752b527ab95c6c5ab9f7c7ece16bb5e9a
leoncross <leon@lscross.co.uk>
2018-12-18 16:02:37 +0000

    getting travis to work

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,12 +28,6 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
 
-  config.before(:each) do
-      connection = PG.connect
-      connection.exec("CREATE DATABASE pgapp_test")
-  end
-
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
