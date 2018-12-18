8f693f217ce6a1d48054d7567d8afde19d313c0f
Cristopher Palacios <criszelaya24@hotmail.com>
2018-12-18 15:53:45 +0000

    Setting up index as signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  	def index
+  		redirect_to "/login"
+  	end
 
     def after_sign_in_path_for(resource)
        "/posts"
