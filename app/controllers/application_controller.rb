7b4d0055e73dff4fa1ab5a80caacbe00ea3f6f79
Cristopher Palacios <criszelaya24@hotmail.com>
2018-12-18 15:11:42 +0000

    redirect to post when user signin and signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+    def after_sign_in_path_for(resource)
+       "/posts"
+    end
+
+    def after_sign_up_path_for(resource)
+       "/posts"
+    end
 end
