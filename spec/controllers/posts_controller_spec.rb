848fced8cd179c6d9d981f799e48ca7de1018dee
leoncross <leon@lscross.co.uk>
2018-12-18 16:32:43 +0000

    testing capabilities of Travis re failing tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -26,4 +26,11 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "FAILING TEST - TRAVIS CHECKER" do
+    it "responds with 200" do
+      get :index
+      expect(response).to have_http_status(235)
+    end
+  end
 end
