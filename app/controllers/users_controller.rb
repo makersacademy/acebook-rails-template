c1c57a425b80de51fe5f5bf3794ec6c6bf4596d3
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-12-18 13:40:50 +0000

    skipping github cleanup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,3 @@
 class UsersController < ApplicationController
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @users = User.create(post_params)
-    redirect_to posts_url
-  end
-
-  def index
-    @users = User.all
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:message)
-  end
-
 end
