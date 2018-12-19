0c3552d9d815ce016e176471ec0e0dd0f0057c26
Tomas Doherty <tomasdoh1@gmail.com>
2018-12-19 15:11:29 +0000

    Feature test for updating a post
    Currently failing, feature not yet implemented

--- spec/features/update_post_spec.rb
+++ spec/features/update_post_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 require_relative '../support/feature_web_helpers'
 require_relative '../support/capybara_selectors'
 
-RSpec.feature "Delete posts", type: :feature do
-  scenario "User can delete their own post" do
+RSpec.feature "Update posts", type: :feature do
+  scenario "User can update their own post" do
     signup
     post_hello_world_message
-    click_on('Delete')
+    click_on('Update')
+    fill_in 'post-message', with: "Goodbye, world!"
+    expect(page).to_have_content("Goodbye, world!")
     expect(page).not_to have_content("Hello, world!")
   end
 end
