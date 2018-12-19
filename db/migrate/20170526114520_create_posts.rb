70eee8e03951eb948e9cd83ae0f1ce723e9aa1f4
topickering <topickering@gmail.com>
2018-12-19 12:14:39 +0000

    ID field removed

--- db/migrate/20170526114520_create_posts.rb
+++ db/migrate/20170526114520_create_posts.rb
@@ -1,7 +1,6 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.integer :id
       t.string :text
       t.integer :poster_id
       t.integer :recipient_id
