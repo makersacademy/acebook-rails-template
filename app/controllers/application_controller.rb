6995af6faec3e93affa15affb886830ec4a53571
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-12-19 17:51:00 +0000

    working name requirements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  	def index
-  		redirect_to "/login"
-  	end
+	def index
+		redirect_to "/login"
+	end
 
-    def after_sign_in_path_for(resource)
-       "/posts"
-    end
+  def after_sign_in_path_for(resource)
+     "/posts"
+  end
 
-    def after_sign_up_path_for(resource)
-       "/posts"
-    end
+  def after_sign_up_path_for(resource)
+     "/posts"
+  end
+
 end
