8d57742b609505d36b408a35a6dfbed938bc4dca
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-19 13:57:42 +0000

    web helpers added for signup and posting a message. feature tests for user interactions added

---
+++ spec/support/feature_web_helpers.rb
@@ -0,0 +1,16 @@
+def signup
+  visit "/"
+  click_link "Signup"
+  fill_in "user_first_name", with: 'first name'
+  fill_in "user_last_name", with: 'last name'
+  fill_in "user_email", with: 'test@email.com'
+  fill_in "user_password", with: 'password'
+  fill_in "user_password_confirmation", with: 'password'
+  click_button "Sign up"
+end
+
+def post_hello_world_message
+  click_link "New post"
+  fill_in "Message", with: "Hello, world!"
+  click_button "Submit"
+end
