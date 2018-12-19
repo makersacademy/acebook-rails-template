41e76f9d5efd1537dfdfb0263903e20f459c9bf7
James de Winton <james.dewinton@gmail.com>
2018-12-19 12:13:09 +0000

    edit bio views, without id

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181218183119) do
+ActiveRecord::Schema.define(version: 20181219103127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bios", force: :cascade do |t|
+    t.integer "age"
+    t.text "bio"
+    t.text "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
