eafd90580df786ddb0c379451c971b951299ecf7
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-19 15:55:53 +0000

    rails helper and spec helper files updated to include coverage filter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,8 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+require 'simplecov'
+SimpleCov.start 'rails'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
 
