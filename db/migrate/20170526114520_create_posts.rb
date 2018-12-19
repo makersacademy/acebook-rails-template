16ebb6f46f6c75c4bf51dc975b1f9b2603b5e18b
topickering <topickering@gmail.com>
2018-12-19 10:47:56 +0000

    Refinements to post model

--- db/migrate/20170526114520_create_posts.rb
+++ db/migrate/20170526114520_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.string :message
+      t.integer :id
+      t.string :text
+      t.integer :poster_id
+      t.integer :recipient_id
 
       t.timestamps
     end
