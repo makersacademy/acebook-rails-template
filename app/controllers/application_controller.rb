f8f80def2ab6fef04ba84fab014d72431b77c648
cesare camurani <cesare.camurani@gmail.com>
2018-12-19 17:23:46 +0000

    Show user profile - Show all users added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :surname, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :username, :email, :password, :current_password)}
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:firstname, :lastname, :surname, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:firstname, :lastname, :username, :email, :password, :current_password)}
   end
 
 end
