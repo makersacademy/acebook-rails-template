b61a83eadb28edef7983d3480c7378242ef44358
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-19 12:34:36 +0000

    user and post model tests unit tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,21 +4,41 @@ RSpec.describe PostsController, type: :controller do
 
   let(:user) { FactoryBot.create(:user) }
 
-  before :each do
-    login_as(user, scope: :user)
-  end
+  describe "When user NOT Logged IN" do
 
-  describe "GET / " do
-    it "responds with 200" do
-      get :index
-      expect(response).to have_http_status(200)
+    describe "GET /new " do
+      it "responds with 200" do
+        get :new
+        expect(response).to have_http_status(302)
+      end
     end
   end
 
-  describe "POST /" do
-    it "creates a post" do
-      post :create, params: { post: { title: 'Hello, world!' } }
-      expect(response).to redirect_to(posts_url)
+  describe "When Logged IN" do
+
+    before :each do
+      login_as(user, scope: :user)
+    end
+
+    describe "GET / " do
+      it "responds with 200" do
+        get :index
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "GET /new " do
+      it "responds with 200" do
+        get :new
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe "POST /" do
+      it "creates a post" do
+        post :create, params: { post: { title: 'Hello, world!' } }
+        expect(response).to redirect_to(posts_url)
+      end
     end
   end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, :type => :model do
+  subject { described_class.new }
+
+  it "is valid with valid attributes" do
+    subject.id = 1
+    subject.first_name = "Anything"
+    subject.last_name = "Anything"
+    subject.email = "anyEmail@anyDomain.com"
+    subject.password = "Any password"
+    subject.created_at = Time.now
+    subject.updated_at = Time.now
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a name, email and password" do
+    expect(subject).to_not be_valid
+  end
+
 end
