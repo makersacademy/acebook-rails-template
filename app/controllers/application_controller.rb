c39a0a97c7a975ebb16a86f160c110d34105d023
Cristopher Palacios <criszelaya24@hotmail.com>
2018-12-19 14:50:47 +0000

    Editing routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  	def index
-  		redirect_to "/login"
-  	end
-
     def after_sign_in_path_for(resource)
        "/posts"
     end
