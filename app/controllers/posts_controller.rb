22491927283f2deb0fa67a724b5a1a2496c22438
Luca <eto.lucafrancesco@gmail.com>
2018-12-20 14:39:23 +0000

    Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,23 @@
 class PostsController < ApplicationController
 
+
+  def show
+    @post = Post.find(params[:id])
+  end
   def new
-    
+
   end
 
   def create
-    render plain: params[:post].inspect
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:description)
+  end
+
 end
