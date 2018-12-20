c2aefefac04b525ff1bb52f9cd44e920928e1ac8
elly <elly.chan.mda@gmail.com>
2018-12-20 22:10:13 +0000

    pass current_user id to db table comments, perform in models/comment.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,13 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.create(comment_params.merge(commenter_id: current_user.id))
     redirect_to post_path(@post)
   end
  
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:body)
     end
 
 end
