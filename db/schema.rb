fc44843dab753b5eabf0d711d84218383d39a83c
James de Winton <james.dewinton@gmail.com>
2018-12-20 15:38:53 +0000

    adding database cleaning gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181219122703) do
+ActiveRecord::Schema.define(version: 20181220142244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20181219122703) do
     t.string "text"
     t.integer "poster_id"
     t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
