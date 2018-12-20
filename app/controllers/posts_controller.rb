e1477b4426673274b91a028f1e303a823d2813ba
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-12-20 11:25:19 +0000

    create method for show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,11 @@ class PostsController < ApplicationController
     redirect_to @post
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
 private
   def post_params
     params.require(:post).permit(:description)
