469c4d6c1423ce1826dd0b99a6c02798f3338419
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-20 16:51:20 +0000

    users controller show action test bug fixed and view formatting changed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe PostsController, type: :controller do
       login_as(user, scope: :user)
     end
 
-    describe "GET / " do
+    describe "GET /index " do
       it "responds with 200" do
         get :index
         expect(response).to have_http_status(200)
