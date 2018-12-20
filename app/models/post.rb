87aa9892428ccbb7bc3ef277678cdb74bd5a8ec2
Cristopher Palacios <criszelaya24@hotmail.com>
2018-12-20 16:44:22 +0000

    Fixing failing test on model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-
+	belongs_to :user
   has_many :likes, dependent: :destroy
 
 end
--- spec/support/feature_web_helpers.rb
+++ spec/support/feature_web_helpers.rb
@@ -1,18 +1,4 @@
 def fill_in_signup_form_and_submit
-<<<<<<< HEAD
-=======
-  fill_in "user_first_name", with: 'first name'
-  fill_in "user_last_name", with: 'last name'
-  fill_in "user_email", with: 'test@email.com'
-  fill_in "user_password", with: 'password'
-  fill_in "user_password_confirmation", with: 'password'
-  click_button "Sign up"
-end
-
-def signup
-  visit "/"
-  click_link "Signup"
->>>>>>> 3e92e935bdbf0d8b2b45858494cb00203afaf672
   fill_in "user_first_name", with: 'first name'
   fill_in "user_last_name", with: 'last name'
   fill_in "user_email", with: 'test@email.com'
