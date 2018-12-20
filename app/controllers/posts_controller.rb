75572e434bab2745e1eadb90b353edb4d27e042d
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-12-20 11:21:57 +0000

    created private method for post params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,14 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params[:post])
+    @post = Post.new(post_params)
 
     @post.save
     redirect_to @post
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:description)
   end
 end
