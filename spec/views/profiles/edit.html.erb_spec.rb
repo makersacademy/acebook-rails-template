17d850051f891e13ccc21a3c2a46dd36fcd15d76
leoncross <leon@lscross.co.uk>
2018-12-20 14:27:33 +0000

    created user

---
+++ spec/views/profiles/edit.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "profiles/edit", type: :view do
+  before(:each) do
+    @profile = assign(:profile, Profile.create!())
+  end
+
+  it "renders the edit profile form" do
+    render
+
+    assert_select "form[action=?][method=?]", profile_path(@profile), "post" do
+    end
+  end
+end
