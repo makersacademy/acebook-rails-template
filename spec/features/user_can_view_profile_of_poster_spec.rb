4db4807d85c8e8339230c8eee112c278538d3da8
Tomas Doherty <tomasdoh1@gmail.com>
2018-12-21 12:04:32 +0000

    Updated feature test

--- spec/features/user_can_view_profile_of_poster_spec.rb
+++ spec/features/user_can_view_profile_of_poster_spec.rb
@@ -7,8 +7,7 @@ RSpec.feature "User can view profile of poster", type: :feature do
     fill_in_signup_form_and_submit
     post_hello_world_message
     click_link("First name")
-    expect(page).to have_content("First name's profile")
-    expect(page).to have_content("First name's posts")
+    expect(page).to have_content("First name Last name")
     expect(page).to have_content("Hello, world!")
     expect(current_path).to eq("/users/1")
   end
