d6d271e5168cdbd70c854750aa628fd44e282078
elly <elly.chan.mda@gmail.com>
2018-12-30 23:31:16 +0000

    add 2 more tests on posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 
 RSpec.describe PostsController, type: :controller do
 
-  # let(:post) { create(:post) }
-
   it 'should show post' do
     post = Post.new
     post = Post.create!(description: "content")
@@ -12,6 +10,18 @@ RSpec.describe PostsController, type: :controller do
     expect(response.status).to eq(200)
   end
 
+  it "assigns all posts as @posts" do
+    post = Post.create!(description: "content")
+    get :index
+    assigns(:posts).should eq([post])
+  end
 
+  it "assigns the requested post as @post" do
+    post = Post.create!(description: "content")
+    get :edit, params: { id: post.id }
+    assigns(:post).should eq(post)
+  end
 
 end
+
+
