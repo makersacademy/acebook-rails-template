94c44bbe62a967da28b96e002dc2d9a13045b47d
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-07 17:06:19 +0000

    updating test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -18,9 +18,9 @@ RSpec.describe PostsController, type: :controller do
     end
 
     describe "GET #index " do
-      it "responds with 200" do
+      it "responds with 302" do
         get :index
-        expect(response).to have_http_status(200)
+        expect(response).to have_http_status(302)
       end
     end
 
