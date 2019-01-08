00270a62796f9429490c439c315c40dce9f9b9fd
Cristopher Palacios <criszelaya24@hotmail.com>
2019-01-08 12:35:22 +0000

    Getting all users to print it in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
+    @allUsers = User.all.where("id != ?", current_user.id)
+    @allUsers = @allUsers.order(first_name: :asc)
   end
 end
