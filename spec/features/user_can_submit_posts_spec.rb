87c3b3412a11e25a25e8e64c4e183113ddee2c29
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2019-01-08 11:53:45 +0000

    adds tests for post edit and delete

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -8,4 +8,25 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+
+  scenario "Can edit a post and see it" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_link "Edit"
+    fill_in "Message", with: "goodbuy"
+    click_button "Submit"
+    expect(page).to have_content("goodbuy")
+  end
+
+  scenario "Can add a post and delete it" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_link "Delete"
+    expect(page).not_to have_content("Hello, world!")
+  end
+
 end
