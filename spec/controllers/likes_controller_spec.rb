54e13072923289df795e6420e4b49f6b572bedff
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-08 12:19:36 +0000

    likes controller tests for create and delete added

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -1,4 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe LikesController, type: :controller do
+
+  let(:test_post) { FactoryBot.create(:post) }
+  let(:user) { FactoryBot.create(:user) }
+
+  describe "POST #create " do
+    it "responds with 302 if post created" do
+      allow(controller).to receive(:current_user).and_return(user)
+      post :create, params: { post_id: test_post.id }
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "DELETE #destroy " do
+
+    let!(:like) { FactoryBot.create(:like) }
+
+    it "responds with 302 if post deleted" do
+      allow(controller).to receive(:current_user).and_return(user)
+      delete :destroy, params: { post_id: like.post.id, id: like.id }
+      expect(response).to have_http_status(302)
+    end
+  end
+
 end
