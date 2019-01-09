47b771d98c5aa3da37d3984156b32781dac9569a
elly <elly.chan.mda@gmail.com>
2019-01-09 22:54:15 +0000

    feature test for application controller, sign up with name

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -4,9 +4,12 @@ RSpec.feature "Sign up", type: :feature do
   scenario "Can sign up with name email and pw" do
     visit "/users/sign_up"
     # click_link "New post"
-    fill_in "Name", with: "tester"
-    # fill_in "Name", with: "tester"
-    # click_button "Submit"
-    # expect(page).to have_content("Hello, world!")
+    fill_in "user_user_name", with: "tester"
+    fill_in "user_email", with: "tester@tester.com"
+    fill_in "user_password", with: "1234567"
+    fill_in "user_password_confirmation", with: "1234567"
+    click_button "Sign up"
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+    # expect(page).to have_content("Signed in successfully.")
   end
 end
