aaebbad3f34173adaec4c9d673bf2fcca4b59ca5
David Gisbey <davidgisbey@Davids-MacBook-Pro.local>
2019-01-09 11:24:35 +0000

    references to comments created and migrated to db

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,37 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @user = current_user
+    @comment = @post.comments.create(comment_params)
+    redirect_to comments_url
+  end
+
+  def index
+    @comments = Comment.all
+  end
+
+  def edit
+     @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to comments_url
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to comments_url
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190109105343) do
+ActiveRecord::Schema.define(version: 20190109112048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "userid"
-    t.integer "postid"
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -48,5 +50,7 @@ ActiveRecord::Schema.define(version: 20190109105343) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
