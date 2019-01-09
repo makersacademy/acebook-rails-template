5e05834aa038e02d8daea7116a4c7a18e31dd1c5
David Gisbey <davidgisbey@Davids-MacBook-Pro.local>
2019-01-09 11:00:58 +0000

    added comment model and migrated it to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190108142859) do
+ActiveRecord::Schema.define(version: 20190109105343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "userid"
+    t.integer "postid"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
