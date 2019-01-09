0a21d071005720dd2a83b2369d90f04c37be17de
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-09 13:40:18 +0000

    user show view refactored to get rid of find method and replace by post.user throughout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = Post.where("timeline_id = #{@user.timeline.id}")
-    @allUsers = User.all.where("id != ?", current_user.id)
-    @allUsers = @allUsers.order(first_name: :asc)
+    @users = User.all.where("id != ?", current_user.id).order(first_name: :asc)
   end
 end
