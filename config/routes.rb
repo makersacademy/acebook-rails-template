84f363730de99bd31e268b3687b72c2fc3c0e20d
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2019-01-09 12:49:06 +0000

    adds a profile page which shows username for current user

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index', as: 'home'
   resources :posts
-
+  match 'users/:id' => 'users#show', via: :get, as: 'profile_page'
 end
