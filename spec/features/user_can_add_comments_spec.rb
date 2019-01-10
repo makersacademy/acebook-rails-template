6edf99f493a2d98142af7ac931fe8df3d7bbc6ae
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2019-01-10 14:32:04 +0000

    adds a test for deleting a comment

--- spec/features/user_can_add_comments_spec.rb
+++ spec/features/user_can_add_comments_spec.rb
@@ -23,4 +23,12 @@ RSpec.feature "Comments", type: :feature do
     click_button "Submit"
     expect(page).to have_content "Changed comment"
   end
+
+  scenario "Can delete a comment" do
+    click_link "Comment"
+    fill_in "comment_content", with: "No comment"
+    click_button "Submit"
+    all('a', :text => 'Delete')[1].click
+    expect(page).not_to have_content "No comment"
+  end
 end
