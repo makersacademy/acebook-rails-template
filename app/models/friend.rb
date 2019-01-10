1dc1b4be5291b4fe1d2a8e09fa4f6466e311a22f
Obsiye <mrabdirizak@gmail.com>
2019-01-10 15:36:57 +0000

    Friends Model is set up to accept two users

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,4 @@
 class Friend < ApplicationRecord
-  belongs_to :user
-  belongs_to :friend1, :foreign_key => :friend1_id, class_name => "User"
-  # belongs_to :friend2, :foreign_key => :friend2_id, class_name => "User"
+  belongs_to :friend1, :class_name => "User", :foreign_key => "friend1_id"
+  belongs_to :friend2, :class_name => "User", :foreign_key => "friend2_id"
 end
