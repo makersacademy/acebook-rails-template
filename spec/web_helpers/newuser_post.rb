2784d1afc3fea52e6d254ddea098b605db798902
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2019-01-10 12:21:20 +0000

    adds helper methods for login and posting

--- spec/web_helpers/newuser_post.rb
+++ spec/web_helpers/newuser_post.rb
@@ -4,7 +4,8 @@ def createUser(
   lastname: "Ably",
   birthday: "10/10/1980",
   email: "maybe@test.com"
-)
+  )
+
   User.create({
     email: email,
     password: "hello2",
@@ -12,5 +13,20 @@ def createUser(
     firstname: firstname,
     lastname: lastname,
     birthday: birthday
-  })
+    })
+
+end
+
+def login(email: "maybe@test.com", password: "hello2")
+  visit "/users/sign_in"
+  fill_in "Email", with: email
+  fill_in "Password", with: password
+  click_button "Log in"
+end
+
+def postMessage(content: "This is a post")
+  visit "/posts"
+  click_link "New post"
+  fill_in "Message", with: content
+  click_button "Submit"
 end
