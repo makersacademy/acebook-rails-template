e9a56b2336150d73da4d6cbc1d491a672d1e2b00
Obsiye <mrabdirizak@gmail.com>
2019-01-10 12:01:09 +0000

    Start adding web_helpers for comments testing

---
+++ spec/web_helpers/newuser_post.rb
@@ -0,0 +1,9 @@
+def createUser(username: "test", firstname: "Wayne", lastname: "Ably", birthday: "10/10/1980", email: "test@test.com")
+    User.create({email: "hello@test.com",
+    password: "hello2",
+    })
+end
+username | firstname | lastname |  birthday | email
+
+def createPost
+end
