425ef4cad230687a1450dc9b075bb6f242276cae
willmcb <williamjamesmcbrien@gmail.com>
2019-01-11 16:35:04 +0000

    Display a users friends on their profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @friend = Struct.new(:firstname, :lastname)
-    @friends = [@friend.new('Jim', 'Smith'), @friend.new('Anna', 'Blake'), @friend.new('Tom', 'Thumb'), @friend.new('Mark', 'Bolan'),
-                @friend.new('Darth', 'Vader'), @friend.new('Bill', 'Tomson'),
-                @friend.new('Greg', 'Jones')]
+    @friends = @user.friendships.map do |row|
+      User.find(row.friend_id)
+    end
   end
 
   def remove
