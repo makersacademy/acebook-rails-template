042826f0e8f5c412748e3fc819cdc960071399d7
luisatheodoro <theodoro.luisa@gmail.com>
2019-01-11 11:24:18 +0000

    fixing tests

--- spec/features/user_can_view_profile_of_poster_spec.rb
+++ spec/features/user_can_view_profile_of_poster_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "User can view profile of poster", type: :feature do
     visit "/"
     fill_in_signup_form_and_submit
     post_hello_world_message
-    click_link("First name")
+    click_link("First name", match: :prefer_exact)
     expect(page).to have_content("First name Last name")
     expect(page).to_not have_content("Hello, world!")
     expect(current_path).to eq("/users/1")
