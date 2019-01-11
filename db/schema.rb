8ddb46bc826115f7aeb53452686821820b14343a
David Gisbey <davidgisbey@Davids-MacBook-Pro.local>
2019-01-11 11:58:28 +0000

    bug fuxed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190111103457) do
+ActiveRecord::Schema.define(version: 20190111115804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,16 +23,6 @@ ActiveRecord::Schema.define(version: 20190111103457) do
     t.bigint "post_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "friends", force: :cascade do |t|
-    t.integer "friend1_id"
-    t.integer "friend2_id"
-    t.string "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "friendships", force: :cascade do |t|
@@ -61,12 +51,13 @@ ActiveRecord::Schema.define(version: 20190111103457) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "user_id"
+    t.integer "friend_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "friends", "users"
   add_foreign_key "posts", "users"
 end
