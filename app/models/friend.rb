7e083f7d27a28b17a43cf1a515b687a01367a86c
Obsiye <mrabdirizak@gmail.com>
2019-01-11 13:23:04 +0000

    Remove commented out code

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,4 @@
 class Friend < ApplicationRecord
   belongs_to :user
   belongs_to :friend1, :foreign_key => :friend1_id, :class_name => "User"
-  # belongs_to :friend2, :foreign_key => :friend2_id, class_name => "User"
 end
