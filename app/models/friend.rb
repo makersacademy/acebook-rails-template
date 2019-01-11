c8b8b39240177f2c4cba51fbc21fb4f78170da1c
willmcb <williamjamesmcbrien@gmail.com>
2019-01-11 15:23:10 +0000

    Delete friend

--- app/models/friend.rb
+++
@@ -1,4 +0,0 @@
-class Friend < ApplicationRecord
-  belongs_to :friend1, :class_name => "User", :foreign_key => "friend1_id"
-  belongs_to :friend2, :class_name => "User", :foreign_key => "friend2_id"
-end
