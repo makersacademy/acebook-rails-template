f3d141b58525a01773f65f5fa52c001a0a512734
Aga <a.dufrat@gmail.com>
2019-01-14 14:17:48 +0000

    Show a default profile image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.serve_static_assests = true
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190114133608) do
+ActiveRecord::Schema.define(version: 20190114134634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,10 @@ ActiveRecord::Schema.define(version: 20190114133608) do
   end
 
   create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "friend_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -53,8 +53,6 @@ ActiveRecord::Schema.define(version: 20190114133608) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "user_id"
-    t.integer "friend_id"
     t.string "avatar_file_name"
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
