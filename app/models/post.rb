0ffcd99264b2882bc93609010a219475dd727810
Aga <a.dufrat@gmail.com>
2019-01-15 16:26:40 +0000

    Bug fix: allow to delete posts and comments that have likes/comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :message, presence: true
-  has_many :post_likes
+  has_many :post_likes, dependent: :destroy
 end
