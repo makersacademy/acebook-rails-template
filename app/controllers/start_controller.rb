7c00ced054f35da8d820779696af5bddd1c4178e
Rodrigue  Kayembe <rodriguekayembe@gmail.com>
2019-01-28 15:25:30 +0000

    playing with heroku

--- app/controllers/start_controller.rb
+++
@@ -1,6 +0,0 @@
-class StartController < ActionController::Base
-
-  def hello 
-    p 'hi there'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
 
-  get '/', to: 'start#hello'
+  root to: 'home#index'
 end
