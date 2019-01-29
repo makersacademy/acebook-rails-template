5ab85fd79cef1a0ca7d4aec63e7a30da90792bc3
Carol Gardiner <carolan.gardiner@gmail.com>
2019-01-29 10:59:38 +0000

    Added form to signup page and included routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  get '/homepage', to: 'welcome#index'
+  get '/signup', to: 'users#new'
 
-  resources :posts
+ 
+
+  
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+
+  # resources :posts
 end
