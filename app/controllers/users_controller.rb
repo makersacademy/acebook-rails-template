a069253dcfce913f377eb7d809186302c9feed5b
OSSahota <osstechunlimited@gmail.com>
2019-01-29 11:57:48 +0000

    Saved user details to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,15 @@ class UsersController < ApplicationController
   def new
   end
 
-  def create 
-    render plain: params[:users].inspect
+  def create
+    # render plain: params[:users].inspect
+
+    @user = User.new(user_params)
+    @user.save
   end
 
+  private
+  def user_params
+    params.require(:users).permit(:username, :email, :password)
+  end
 end
