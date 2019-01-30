516f0c3e033c2a91f39df314c446b02800e582b7
stonefarmer9 <g.p.levaillant@gmail.com>
2019-01-30 11:39:28 +0000

    Test for validations now passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe "#validate" do
-    it "email is present" do
-      get :create, params: { users: { username: 'oss', email: '', password: '123' } }
-      expect(User.valid?).to eq false
+    it "doesn't let you save a user without an email" do
+      cont = UsersController.new
+      user = User.new({:username => 'test0001',:email => "",:password => "12345"})
+      expect(user.save).to eq false
     end
   end
 end
