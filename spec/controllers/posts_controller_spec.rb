4f0b532452e347115d0b4b82f6b09851118a11b6
Alina Goaga <goaga.alina@gmail.com>
2019-01-30 22:38:58 +0000

    Solved previous conflict in index and added unit test for delete

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
 
-# create a user before each test is run
-
   before do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(User.new(id: 1))
   end
@@ -31,6 +29,16 @@ RSpec.describe PostsController, type: :controller do
     it "responds with 200" do
       get :index
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it "deletes a post" do
+      @user = User.create(email: "a@a.com", password: "abc123", id: 2)
+      @post = Post.create(message: "Hello, world!", id: 100, user_id: @user.id)
+      expect {
+        delete :destroy, params: { id: @post.id }
+      }.to change(Post, :count).by(-1)
     end
   end
 
