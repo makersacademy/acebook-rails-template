780003c8c6b7b5ca8a057396dddfed684742ef6a
Carol Gardiner <carolan.gardiner@gmail.com>
2019-01-30 13:37:20 +0000

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
 
   post '/signup', to: 'users#create'
 
+
+
   get '/posts', to: 'posts#new'
+
+  post '/posts', to: 'posts#create'
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
