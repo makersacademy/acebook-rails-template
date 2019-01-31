0692db4b238945c538b0b7171f594979241f15fc
Jethro Whaley <jethro.whaley@gmail.com>
2019-01-31 15:18:25 +0000

    test for a non - recent message passing

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,13 +3,16 @@ require 'timecop'
 
 RSpec.describe Post, type: :model do
 
-  Time.freeze
-
   subject(:post) { described_class.create(:created_at => Time.now)}
+  subject(:early_post) { described_class.create(:created_at => (Time.now - 601))}
 
   describe '#edit_timer?' do
     it 'returns true if less than 10 minutes have passed' do
       expect(post.edit_timer?).to eq(true)
+    end
+
+    it 'returns false if more than 10 minutes have passed' do
+      expect(early_post.edit_timer?).to eq(false)
     end
   end
 end
