ea54d36c70706886f41ac3ff312c939ad29eb2e3
Shaneil Clarke <shaneilske@yahoo.com>
2019-01-31 22:37:29 +0000

    removed relations

--- db/migrate/20170526114520_create_posts.rb
+++ db/migrate/20170526114520_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :message
-      t.references :user
+      # t.references :user
 
       t.timestamps
     end
