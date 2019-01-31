8756ca41f60674dfc828f62af17ef79d3762d5e4
Rodrigue  Kayembe <rodriguekayembe@gmail.com>
2019-01-31 15:29:01 +0000

    Failing Post model tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it { is_expected.to be }
+  it "is a valid with  valid attributes" do
+    expect(Post.new).to be_valid
+  end 
+  it "is not valid without a message" do
+    post = Post.new(message: nil)
+    expect(post).to_not be_valid
+  end
+  it "is not valid without a created_at" do 
+    expect(Post.new).to be_valid
+  end
+  it "is not valid without a updated_at" do 
+    expect(Post.new).to be_valid
+  end
 end
