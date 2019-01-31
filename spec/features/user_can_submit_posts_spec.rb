e622fdcfdb34daa41043a4ae9a06401308446c82
Tom Russell <TR115251@hotmail.co.uk>
2019-01-31 16:07:28 +0000

    tests written, WIP

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,21 +1,21 @@
 require 'rails_helper'
 
-RSpec.feature "Timeline", type: :feature do
+# RSpec.feature "Timeline", type: :feature do
+#   scenario "empty posts raise error" do
+#     visit "/posts"
+#     click_link "New post"
+#     fill_in "Message", with: ""
+#     click_button "Submit"
+#     expect(page).to_not have_content("")
+#   end
+# end
+
+feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
-  end
-end
-
-feature "Timeline", type: :feature do
-  scenario "empty posts raise error" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: ""
-    click_button "Submit"
-    expect(page).to raise_error("mystery-error")
   end
 end
