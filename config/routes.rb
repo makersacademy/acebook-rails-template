ffbb2f49b901629dea02cc7056b0d4539cf84f6e
Shaneil Clarke <shaneilske@yahoo.com>
2019-01-31 22:14:00 +0000

    updated views files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'users/index' => 'users#index' 
+  get 'users/index' => 'users#index'
   post 'users' => 'users#create'
   get 'posts' => 'posts#index'
-  get 'users/profile'
+  
   root 'users#index'
 
   resources :users
