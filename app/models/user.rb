979c83c01da5b686962b1fabbb6718dc94cb97a8
CKKH <chriskkhassan@gmail.com>
2019-01-31 12:22:59 +0000

    Feature: user signs up successfully, redirected to profile
    Co-authored-by: matharotheelf <44533664+matharotheelf@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
-  attr_accessor :email, :first_name, :last_name, :birthday, :gender, :password
   
-  validates_presence_of :email, :first_name, :last_name, :birthday, :gender, :password
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :birthday
+  validates_presence_of :password
+  validates_presence_of :gender
+  validates_presence_of :email
   validates_uniqueness_of :email
+
 end
