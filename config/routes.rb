7acce24ee37e0aa6b56c3f7d45732ab1bc023451
Emanuele <emanuelegorga@gmail.com>
2019-01-31 12:24:54 +0000

    Sign Up form displaying on the index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root 'home#index'
+
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
 
-  root 'home#index'
+
 end
