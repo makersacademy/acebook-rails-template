6ca5cb74e6fb53d9acb4ba193e682ffeda5d6128
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-01 13:15:02 +0000

    have a working delete button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,17 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
+
+  def show
+    redirect_to posts_url
+  end
+
+  def destroy 
+     post = Post.find(params[:id])
+     post.destroy
+     redirect_to posts_path
+  end
+
 
   private
 
