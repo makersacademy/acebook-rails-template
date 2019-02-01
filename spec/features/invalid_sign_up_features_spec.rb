ad6910522e58ad4349a5fec22f9dbb9ca22b10c8
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-01 12:37:30 +0000

    Tests: Added feature test for not allowing same email twice

---
+++ spec/features/invalid_sign_up_features_spec.rb
@@ -0,0 +1,22 @@
+RSpec.feature 'Signup', type: :feature do
+  scenario 'User successfully signs up' do
+    visit '/'
+    fill_in :first_name, with: 'Dave'
+    fill_in :last_name, with: 'Katze'
+    fill_in :email, with: 'davethecat@katze.com'
+    fill_in :password, with: 'Gato123'
+    fill_in :birthday, with: '2000/10/10'
+    fill_in :gender, with: 'Male'
+    click_button 'Sign Up'
+    visit '/'
+    fill_in :first_name, with: 'Email'
+    fill_in :last_name, with: 'Thief'
+    fill_in :email, with: 'davethecat@katze.com'
+    fill_in :password, with: 'Evil'
+    fill_in :birthday, with: '2001/10/10'
+    fill_in :gender, with: 'Female'
+    click_button 'Sign Up'
+    expect(page.current_path).to eq('/users')
+    expect(page).to have_content('has already been taken')
+  end
+end
