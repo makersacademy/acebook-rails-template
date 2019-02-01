90c7a4686704a08f96565fdf3ac26d2180df7ea4
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-01 12:29:20 +0000

    test: user cannot create an empty post

--- db/schema.rb
+++ db/schema.rb
@@ -23,10 +23,8 @@ ActiveRecord::Schema.define(version: 20190131121452) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "description"
   end
 
 end
