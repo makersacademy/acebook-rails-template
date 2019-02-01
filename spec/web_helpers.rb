552f50debf1cf8e077587900b607202078fb5923
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-01 12:41:41 +0000

    simplecov and simplecov console added - coverage currently at 98.13%

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -6,12 +6,12 @@ def sign_up_steps
   click_button('Sign up')
 end
 
-def sign_in_steps
-  click_link('Sign In')
-  fill_in "user_email", with: 'test@gmail.com'
-  fill_in "user_password", with: 'test123'
-  click_button('Log in')
-end
+# def sign_in_steps
+#   click_link('Sign In')
+#   fill_in "user_email", with: 'test@gmail.com'
+#   fill_in "user_password", with: 'test123'
+#   click_button('Log in')
+# end
 
 def submit_post
   visit "/posts"
