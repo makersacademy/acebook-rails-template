10e774193aef73471e5dee12573ad43cc9da8b3b
Adam Lodowski <adamlodowski@gmail.com>
2019-02-04 14:04:40 +0000

    Minor refactoring

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:sign_in_password])
       session[:user_id] = user.id
       redirect_to posts_url, notice: "Logged in as #{user.email}"
-      # redirect_to root_url, notice: "Logged in!"
     else
-      # flash.now.alert = "Email or password is invalid"
       redirect_to root_url, notice: "Email or password is invalid"
     end
   end
