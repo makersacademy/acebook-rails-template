d38bfb53da88d1ee6977f2170bf4ce4f6a0395e9
Tom Russell <TR115251@hotmail.co.uk>
2019-02-04 15:03:48 +0000

    refactor: css added, sign up, sign in links separated for clarity

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -11,12 +11,19 @@ describe "sign up", type: :feature do
   scenario "user can sign up" do
     visit '/'
     click_link "Sign up"
-    click_link "Sign up"
     fill_in "user[email]", with: "test@test.com"
     fill_in "user[password]", with: "testpassword"
     fill_in "user[password_confirmation]", with: "testpassword"
-    click_on 'Sign up'
+    click_button 'Sign up'
     # expect(current_url).to eq('/')
     expect(page).to have_content('Currently logged in as test@test.com')
+  end
+end
+
+describe "Authentication" do
+  scenario "User must sign up/in to access posts" do
+    visit '/'
+    expect(page).to have_content('Sign in', 'Sign up')
+
   end
 end
