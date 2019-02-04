e34a1a64c3e38fbb1d3307270d0dda4ba483a6b1
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-04 18:04:35 +0000

    navbar switch3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/home', to: 'posts#home'
