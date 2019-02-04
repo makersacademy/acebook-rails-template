afacaea33dd75351cbbebeb716a1d5c952006a5b
Adam Lodowski <adamlodowski@gmail.com>
2019-02-04 12:15:36 +0000

    Added functional sign in and feature test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:sign_in_email])
+    if user && user.authenticate(params[:sign_in_password])
+      session[:user_id] = user.id
+      redirect_to posts_url
+      # redirect_to root_url, notice: "Logged in!"
+    else
+      # flash.now.alert = "Email or password is invalid"
+      redirect_to root_url
+    end
   end
+
 end
