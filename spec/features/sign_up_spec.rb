d33cf43af04a706f28d08a8f66909287e587c07a
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-04 12:23:40 +0000

    test: failing test for user sign up

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -7,17 +7,16 @@ describe "sign up", type: :feature do
   end
 end
 
-# describe "sign up", type: :feature do
-#   scenario "user can sign up" do
-#     visit '/'
-#     click_link "signup"
-#     fill_in "email", with: "test@test.com"
-#     fill_in "password", with: "testpassword"
-#     fill_in "password_confirmation", with: "testpassword"
-#     click_button 'Sign Up'
-#     expect(current_url).to eq('/profile')
-#     expect(page).to have_content('Welcome test@test.com')
-#   end
-# end
-
-
+describe "sign up", type: :feature do
+  scenario "user can sign up" do
+    visit '/'
+    click_link "Sign up"
+    click_link "Sign up"
+    fill_in "email", with: "test@test.com"
+    fill_in "password", with: "testpassword"
+    fill_in "password_confirmation", with: "testpassword"
+    click_button 'Sign Up'
+    expect(current_url).to eq('/')
+    expect(page).to have_content('Currently logged in as test@test.com')
+  end
+end
