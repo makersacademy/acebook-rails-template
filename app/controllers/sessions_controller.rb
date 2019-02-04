10401cbb744cb7ee8a62df73b54f5034ca5b6a39
stonefarmer9 <g.p.levaillant@gmail.com>
2019-02-04 12:28:32 +0000

    Commiting before experimentation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Log user in to profile
+    else
+    flash.now[:danger] = "Invalid details you cretin"
+    render 'new'
+    end
+  end
+
 end
--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -2,20 +2,11 @@ require 'rails_helper'
 RSpec.feature 'Signin', type: :feature do
   scenario "A user signs in and see's their account details" do
     signup_and_click
-    visit '/signup'
-    fill_in 'users_username', with: 'Test 002'
-    fill_in 'users_email', with: 'Test200@test.com'
-    fill_in 'users_password', with: 'Password2'
+
+    visit '/login'
+    fill_in 'session_email', with: 'Test100@test.com'
+    fill_in 'session_password', with: 'Password001'
     find('input[name="commit"]').click
-    visit '/signup'
-    fill_in 'users_username', with: 'Test 003'
-    fill_in 'users_email', with: 'Test300@test.com'
-    fill_in 'users_password', with: 'Password3'
-    find('input[name="commit"]').click
-    visit '/signin'
-    fill_in 'users_email', with: 'Test200@test.com'
-    fill_in 'users_password', with: 'Password002'
-    find('input[name="commit"]').click
-    expect(page).to have_content('Test 002')
+    expect(page).to have_content('Test 001')
   end
 end
