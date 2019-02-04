ee129d170e72de6e9e3391893e8fed6c25580581
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-04 16:04:08 +0000

    User can edit posts

--- spec/features/user_can_sumbit_and_view_posts_spec.rb
+++ spec/features/user_can_sumbit_and_view_posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'Post display', type: :feature do
   scenario 'User sumbits a post' do
-    visit '/posts'
+    sign_up
     click_link 'New post'
     fill_in 'Message', with: 'Hello, world!'
     click_button 'Submit'
@@ -10,7 +10,7 @@ RSpec.feature 'Post display', type: :feature do
   end
 
   scenario 'User submits multiline post' do
-    visit '/posts'
+    sign_up
     click_link 'New post'
     fill_in 'Message', with: "Hello, world! \nGoodbye, world!"
     click_button 'Submit'
@@ -18,20 +18,19 @@ RSpec.feature 'Post display', type: :feature do
   end
 
   scenario 'User views newest posts first' do
-    fill_in_sign_up_details
-    click_button 'Sign Up'
-    click_link "New post"
-    fill_in "Message", with: 'First message'
+    sign_up
+    click_link 'New post'
+    fill_in 'Message', with: 'First message'
     click_button 'Submit'
-    click_link "New post"
-    fill_in "Message", with: 'Second message'
+    click_link 'New post'
+    fill_in 'Message', with: 'Second message'
     click_button 'Submit'
     expect('Second message').to appear_before('First message')
   end
 
   scenario 'User can see the time a post was submitted' do
     post = Post.create('message' => 'Time')
-    visit '/posts'
+    sign_up
     expect(page).to have_content(post.created_at)
   end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -10,6 +10,17 @@ def fill_in_sign_up_details
   fill_in :gender, with: 'Male'
 end
 
+def sign_up
+  visit '/'
+  fill_in :first_name, with: 'Dave'
+  fill_in :last_name, with: 'Katze'
+  fill_in :email, with: 'davethecat@katze.com'
+  fill_in :password, with: 'Gato123'
+  fill_in :birthday, with: '2000/10/10'
+  fill_in :gender, with: 'Male'
+  click_button 'Sign Up'
+end
+
 # Specs in this file have access to a helper object that includes
 # the UsersHelper. For example:
 #
