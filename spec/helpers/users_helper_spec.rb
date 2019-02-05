69aa014f9352876ef10aab681f76508e2837eb0f
Adam Lodowski <adamlodowski@gmail.com>
2019-02-05 12:21:26 +0000

    Code linting

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -10,7 +10,6 @@ def fill_in_sign_up_details
   fill_in :gender, with: 'Male'
 end
 
-
 def create_user
   tom = User.create(first_name: 'Tom', last_name: 'Lawrence', birthday: '2000-01-01', password: 'nerds123', gender: 'Male', email: 'tom@hotmail.com') 
   session[:user_id] = tom.id
