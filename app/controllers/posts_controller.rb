71b3556e318dfa0418c4375fa8e004fa21ab8bee
Rodrigue  Kayembe <rodriguekayembe@gmail.com>
2019-02-05 16:55:45 +0000

    Update and edit posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,17 @@ class PostsController < ApplicationController
     redirect_to posts_url
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+  end
+
+
   def destroy 
      post = Post.find(params[:id])
      post.destroy
