8a271e250dec77c1f296f58cf90c3769e6307618
CKKH <chriskkhassan@gmail.com>
2019-02-05 15:32:21 +0000

    Feature: user can only delete their own posts

--- spec/features/user_can_delete_post_spec.rb
+++ spec/features/user_can_delete_post_spec.rb
@@ -1,3 +1,6 @@
+require 'rails_helper'
+require_relative '../helpers/users_helper_spec'
+
 RSpec.feature 'Delete post', type: :feature do
   scenario 'User can delete a post' do
     sign_up
@@ -9,9 +12,14 @@ RSpec.feature 'Delete post', type: :feature do
     expect(page).not_to have_content('Hello, world!')
   end
 
-  # scenario 'User cannot delete other user posts' do
-  #   post = Post.create("message" => "Delete me")
-  #   sign_up
-  #   expect(page).not_to have_link("You can't delete me")
-  # end
+  scenario 'User cannot delete other user posts' do
+    sign_up
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, universe!'
+    click_button 'Submit'
+    click_link 'Sign Out'
+    second_user_sign_up
+    expect(page).not_to have_link("Hello, universe!")
+    expect(page).to have_content ("Hello, universe!")
+  end
 end
