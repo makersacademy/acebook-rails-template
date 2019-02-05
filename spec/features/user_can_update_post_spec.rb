75c8c2c2a5162714a9237766ec8939a5ebc7b554
Shaneil Clarke <shaneilske@yahoo.com>
2019-02-05 15:26:08 +0000

    added spec feature to only allow user to edit posts they created

--- spec/features/user_can_update_post_spec.rb
+++ spec/features/user_can_update_post_spec.rb
@@ -1,15 +1,28 @@
 require 'rails_helper'
+require_relative '../helpers/users_helper_spec'
 
 RSpec.feature 'Edit Post', type: :feature do
   scenario 'User can edit a post' do
     sign_up
     click_link 'New post'
-    fill_in 'Message', with: 'Hello, world!'
+    fill_in 'Message', with: 'Hello, world'
     click_button 'Submit'
-    click_link 'Hello, world!'
+    click_link 'Hello, world'
     fill_in 'Message', with: 'Goodbye, world!'
     click_button 'Edit'
     expect(page).to have_content('Goodbye, world!')
-    expect(page).not_to have_content('Hello, world!')
+    expect(page).not_to have_content('Hello, world')
   end
+
+  scenario 'User cannot edit other user posts' do
+    sign_up
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+    click_link 'Sign Out'
+    second_user_sign_up
+    expect(page).not_to have_link("Hello, world!")
+    expect(page).to have_content ("Hello, world!")
+  end
+
 end
