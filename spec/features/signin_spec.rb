72cac406eba0af0bafead75fe0019ec8e8db2efe
stonefarmer9 <g.p.levaillant@gmail.com>
2019-02-05 11:00:45 +0000

    Broken feature test fixed and passing, added feature test for case sensitive login

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -1,18 +1,31 @@
 require 'rails_helper'
 RSpec.feature 'Signin', type: :feature do
-  xscenario "A user signs in and see's their account details" do
+  scenario "A user signs in and see's their account details" do
 
     visit '/signup'
-    fill_in 'users_username', with: 'Test 001'
+    fill_in 'users_username', with: '1'
     fill_in 'users_email', with: 'Test100@test.com'
-    fill_in 'users_password', with: 'Password001'
+    fill_in 'users_password', with: '12345'
     find('input[name="commit"]').click
-
-    visit '/login'
-
+    click_on "Log Out"
+    click_link "Log In!"
     fill_in 'session_email', with: 'Test100@test.com'
-    fill_in 'session_password', with: 'Password001'
+    fill_in 'session_password', with: '12345'
     find('input[name="commit"]').click
-    expect(page).to have_content('Test 001')
+    expect(page).to have_content('1')
+  end
+
+  scenario "Signin is case sensitive" do
+    visit '/signup'
+    fill_in 'users_username', with: '1'
+    fill_in 'users_email', with: 'Test100@test.com'
+    fill_in 'users_password', with: '12345'
+    find('input[name="commit"]').click
+    click_on "Log Out"
+    click_link "Log In!"
+    fill_in 'session_email', with: 'test100@test.com'
+    fill_in 'session_password', with: '12345'
+    find('input[name="commit"]').click
+    expect(page).to have_content('you cretin')
   end
 end
