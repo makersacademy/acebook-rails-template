70132dd05daa2c29516875fb44c797595daa612d
beatricecourage <bea_courage@hotmail.co.uk>
2019-02-05 12:15:23 +0000

    In process of fixing model posts spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,23 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
 
-  let(:post) { Post.new(message: nil, created_at: nil, updated_at: nil) }
-  let(:valid_post) { Post.new( message: "valid post")}
+  let(:post) { Post.new(message: nil) }
+  let(:valid_post) { Post.new( message: "valid post", created_at: nil, updated_at: nil, user_id: 1 )}
+  let(:user) { User.new( email: "bea_courage@hotmail.com", password: "waaa", user_id: 1 )}
+
+  # before {
+  #   sign_up(email, password)
+  # }
+
 
   it "is valid with valid attributes" do
     expect(valid_post).to be_valid
-  end 
+  end
 
   it "is not valid without a message" do
     expect(post).to_not be_valid
   end
 
-  it "is not valid without a timestamp" do 
+  it "is not valid without a timestamp" do
     expect(post).to_not be_valid
   end
 
-  it "is not valid without a updated_at" do 
+  it "is not valid without a updated_at" do
     expect(post).to_not be_valid
   end
-  
+
 end
