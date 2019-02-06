97ff30bdb98ad4c0eaad520dbed7abd5db8ab659
OSSahota <osstechunlimited@gmail.com>
2019-02-06 12:58:52 +0000

    Feature test for logged in user to view another users wall - passing

--- spec/features/users_list_spec.rb
+++ spec/features/users_list_spec.rb
@@ -3,12 +3,22 @@
 require 'rails_helper'
 
 RSpec.feature 'View all users', type: :feature do
-  scenario "Logged in user can see other acebook users" do
+  scenario "Logged in user can see a list of other acers" do
     signup_and_click
     logout
     second_user_signup_and_click
     click_on 'View All Acers'
 
     expect(page).to have_content('1')
+  end
+
+  scenario "Logged in user can view the wall of another acer" do
+    signup_and_click
+    logout
+    second_user_signup_and_click
+    click_on 'View All Acers'
+    click_on '1'
+
+    expect(page.current_path).to eql '/users/1'
   end
 end
