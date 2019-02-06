4f906da316e0a60b6df61d4dbe822f2332c2c5ef
Rodrigue  Kayembe <rodriguekayembe@gmail.com>
2019-02-06 12:20:52 +0000

    Added username to sign up and User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :password_confirmation])
+ end
 
   def after_sign_in_path_for(resource)
     posts_path
@@ -9,7 +14,6 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource)
     posts_path
   end
-
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
   has_many :posts, dependent: :destroy
+
 end
