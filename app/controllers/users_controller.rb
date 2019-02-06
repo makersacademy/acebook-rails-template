2ba90c175505f4cc01ebaa9c9614879050e9b81b
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-02-06 16:01:18 +0000

    Linted with errorpage functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,20 +19,8 @@ class UsersController < ApplicationController
     unless id.include? "@"
       @user = User.find(id)
       redirect_to user_path(@user.email)
-      # if User.find(id)
-      #   @user = User.find(id)
-      #   redirect_to user_path(@user.email)
-      # else
-      #   redirect_to '/errors#not_found'
-      # end
     end
-    if current_user
-      session[:current_wall] = request.original_url.split("/").pop
-      @post = current_user.posts.build
-      @posts = Post.all
-    else
-      redirect_to root_url, notice: "Please sign in"
-    end
+    check_current_wall
   end
 
   private
@@ -46,6 +34,17 @@ class UsersController < ApplicationController
       :gender,
       :email
     )
+  end
+
+  def check_current_wall
+    if current_user
+      session[:current_wall] = request.original_url.split("/").pop
+      @post = current_user.posts.build
+      @posts = Post.all
+    else
+      redirect_to root_url, notice: "Please sign in"
+    end
+
   end
 
 end
--- spec/features/user_has_wall_spec.rb
+++ spec/features/user_has_wall_spec.rb
@@ -16,7 +16,7 @@ RSpec.feature 'Visit own wall', type: :feature do
 
   scenario 'User redirected to wall if enters user_id as param' do
     sign_up
-    tom = User.create(first_name: 'Tom', last_name: 'Lawrence', birthday: '2000-01-01', password: 'nerds123', gender: 'Male', email: 'tom@hotmail.com', id: 9_999_999)
+    User.create(first_name: 'Tom', last_name: 'Lawrence', birthday: '2000-01-01', password: 'nerds123', gender: 'Male', email: 'tom@hotmail.com', id: 9_999_999)
     visit "/users/9999999"
     expect(page.current_path).to eq('/users/tom@hotmail.com')
   end
