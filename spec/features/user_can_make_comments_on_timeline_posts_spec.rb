0eafefb4529c98db0c923dbede58db038bd81d19
Adam Lodowski <adamlodowski@gmail.com>
2019-02-07 11:28:43 +0000

    Improved Testing

--- spec/features/user_can_make_comments_on_timeline_posts_spec.rb
+++ spec/features/user_can_make_comments_on_timeline_posts_spec.rb
@@ -1,9 +1,11 @@
 RSpec.feature 'Timeline post comments', type: :feature do
   scenario 'User submits a comment' do
     sign_up
+    visit '/posts'
     click_link 'New post'
     fill_in 'Message', with: 'Hello, world!'
     click_button 'Submit'
+    visit '/posts'
     fill_in 'message', with: 'Comment, world!'
     click_button 'Submit'
     expect(page).to have_content('Comment, world!')
@@ -11,9 +13,11 @@ RSpec.feature 'Timeline post comments', type: :feature do
 
   scenario 'User cannot submit empty comment' do
     sign_up
+    visit '/posts'
     click_link 'New post'
     fill_in 'Message', with: 'Hello, world!'
     click_button 'Submit'
+    visit '/posts'
     fill_in 'message', with: ''
     click_button 'Submit'
     expect(page).to have_content("Your new post couldn't be created!")
