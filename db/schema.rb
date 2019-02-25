2dfa5701909a9221695261db5b831e16d50e8b15
David Mears <44967334+david-mears@users.noreply.github.com>
2019-02-25 17:05:02 +0000

    Move rubocop disable statement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_526_114_520) do # rubocop:disable Style/NumericLiterals
+# rubocop:disable Style/NumericLiterals
+ActiveRecord::Schema.define(version: 20_170_526_114_520) do 
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
