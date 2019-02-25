662fad6f4d80f8003b9b7968d5a3c2ba9ddd79e7
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-02-25 16:04:20 +0000

    Setup Rubocop & SimpleCov
    Co-authored-by: Adrien Fabre  <adrien.fabre.1@gmail.com>
    Co-authored-by: Luca Fruzza  <lfruzza@hotmail.co.uk>
    Co-authored-by: Elliot  Jennings <elliot.r.jennings@outlook.com>
    Co-authored-by: Vaith Schmitz <vaithschmitz@googlemail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,19 @@
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
-#
+
+require 'simplecov'
+require 'simplecov-console'
+require 'rubocop'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
