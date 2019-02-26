deeb90ba589dd3fa0653febbad6c5b951fd586df
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-26 12:19:09 +0000

    Add users migration and reorder migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_526_114_520) do 
+ActiveRecord::Schema.define(version: 20190226115920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20_170_526_114_520) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "pronoun"
+    t.string "password"
+    t.string "profile_pic_link"
   end
 
 end
