51fe07b323b592dee581f178d972409054b739bf
debora <debora.piu92@gmail.com>
2019-02-26 10:52:50 +0000

    users table linked to posts table (db)

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "users/index", type: :view do
+  before(:each) do
+    assign(:users, [
+      User.create!(
+        :email => "Email",
+        :password => "Password",
+        :first_name => "First Name",
+        :last_name => "Last Name"
+      ),
+      User.create!(
+        :email => "Email",
+        :password => "Password",
+        :first_name => "First Name",
+        :last_name => "Last Name"
+      )
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    assert_select "tr>td", :text => "Email".to_s, :count => 2
+    assert_select "tr>td", :text => "Password".to_s, :count => 2
+    assert_select "tr>td", :text => "First Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
+  end
+end
---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "users/new", type: :view do
+  before(:each) do
+    assign(:user, User.new(
+      :email => "MyString",
+      :password => "MyString",
+      :first_name => "MyString",
+      :last_name => "MyString"
+    ))
+  end
+
+  it "renders new user form" do
+    render
+
+    assert_select "form[action=?][method=?]", users_path, "post" do
+
+      assert_select "input[name=?]", "user[email]"
+
+      assert_select "input[name=?]", "user[password]"
+
+      assert_select "input[name=?]", "user[first_name]"
+
+      assert_select "input[name=?]", "user[last_name]"
+    end
+  end
+end
