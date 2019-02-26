bb3e87ea832c19b9502c22030dbcf5c7d9e92110
Diptis Halder <diptis.halder@gmail.com>
2019-02-26 16:59:29 +0000

    :boom: feat: Enabled Sign-Out and adapted Sign-Up and Sign-In

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,11 @@ ActiveRecord::Schema.define(version: 20190226114553) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
