d5774e6313acc116e2a616ff434043c5311539ed
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-02-27 14:47:30 +0000

    :white_check_mark: updated tests to expect not allowing behaviour when not logged in

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and view them" do
+  scenario "Cannot submit posts and view them if not logged in" do
     visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
+    # click_link "New post"
+    # fill_in "Message", with: "Hello, world!"
+    # click_button "Submit"
+    expect(page).to have_content("You need to sign in or sign up")
   end
 end
