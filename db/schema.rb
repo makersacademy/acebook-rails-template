d86765d2861f352775f6ab9015b594233385f9c4
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-02-27 10:13:44 +0000

    Rubocop rule updates (and 2 files changed) to get rubocop all green

--- db/schema.rb
+++ db/schema.rb
@@ -9,9 +9,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20170526114520) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,5 +18,4 @@ ActiveRecord::Schema.define(version: 20170526114520) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
