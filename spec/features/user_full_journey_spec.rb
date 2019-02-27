687d1efd6feaff54fabf8908c709f6127f47f6e6
debora <debora.piu92@gmail.com>
2019-02-27 16:31:14 +0000

    full journey feature test

--- spec/features/user_full_journey_spec.rb
+++ spec/features/user_full_journey_spec.rb
@@ -11,5 +11,16 @@ RSpec.feature "Journey", type: :feature do
     fill_in 'user_password_confirmation', with: "featurepwd"
     click_on "Submit"
     expect(page).to have_content('Logged in')
+    click_link("New post")
+    expect(page).to have_content('Message')
+    fill_in "post_message", with: "test feature message"
+    click_on "Submit"
+    expect(page).to have_content("test feature message")
+    click_link "Logout"
+    expect(page).to have_content("Log in")
+    fill_in 'user_email', with: 'featuretest@email.com'
+    fill_in 'user_password', with: 'featurepwd'
+    click_on "Log in"
+    expect(page).to have_content("test feature message")
   end
 end
