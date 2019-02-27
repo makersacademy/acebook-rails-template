f445cdbfbb17991988b1629088a7fb39280e65aa
david-mears <kieukmail@gmail.com>
2019-02-27 15:45:58 +0000

    obey rubocop at all times

--- spec/features/authenticate_spec.rb
+++ spec/features/authenticate_spec.rb
@@ -1,5 +1,5 @@
-RSpec.context "Authentication: ", type: :feature do
-  scenario "When a user has not signed up if they visit another URL, they are redirected to the sign in page" do
+RSpec.context "When a user has not signed up", type: :feature do
+  scenario "if they visit another URL, they are redirected to the sign in page" do
     visit '/posts/index'
     expect(current_path).to eq("/users/sign_in")
   end
