6eaa008f88f31ef265902f6dba6d3575fbeaafc7
david-mears <kieukmail@gmail.com>
2019-02-27 15:40:49 +0000

    :white_check_mark: added test for redirection

--- spec/features/authenticate_spec.rb
+++ spec/features/authenticate_spec.rb
@@ -1,11 +1,8 @@
-RSpec.context "When a user has not signed up", type: :feature do
-  scenario "if they visit another URL, they are redirected to the sign in page" do
-    visit '/posts/new'
+require_relative '../helpers/web_helper.rb'
+
+RSpec.context "Authentication: ", type: :feature do
+  scenario "When a user has not signed up if they visit another URL, they are redirected to the sign in page" do
+    visit '/posts/index'
     expect(current_path).to eq("/users/sign_in")
-  end
-  scenario "when they do sign up, they are not redirected to the sign in page" do
-    perform_valid_sign_up
-    visit '/posts/new'
-    expect(current_path).to eq("/posts/new")
   end
 end
