c8fc24b6c69fb6b5b46dc5816003739ef7d73231
David Mears <44967334+david-mears@users.noreply.github.com>
2019-02-27 15:57:06 +0000

    solve rubocop - please check before pushing

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
 
-    private
+  private
 
-    def sign_up_params
-        params.require(:user).permit(:first_name, :last_name, :email, :pronoun, :profile_pic, :password, :password_confirmation)
-    end
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :pronoun, :profile_pic, :password, :password_confirmation)
+  end
 
-    def account_update_params
-        params.require(:user).permit(:first_name, :last_name, :email, :pronoun, :profile_pic, :password, :password_confirmation, :current_password)
-    end
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :pronoun, :profile_pic, :password, :password_confirmation, :current_password)
+  end
 end
 
