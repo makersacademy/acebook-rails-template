fde2218248f8b8f67038d3a31876409923524219
David Mears <44967334+david-mears@users.noreply.github.com>
2019-02-27 16:02:23 +0000

    solve rubocop complaints again

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,23 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :pronoun, :profile_pic, :password, :password_confirmation)
+    params.require(:user).permit(:first_name,
+      :last_name,
+      :email,
+      :pronoun,
+      :profile_pic,
+      :password,
+      :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :pronoun, :profile_pic, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name,
+      :last_name,
+      :email,
+      :pronoun,
+      :profile_pic,
+      :password,
+      :password_confirmation,
+      :current_password)
   end
 end
-
