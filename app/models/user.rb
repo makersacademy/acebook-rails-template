c2ce3328e5ce1c1d893e0b54e4040d8c3f3578ec
Sherif Shendidy <sherif@shendidy.com>
2019-02-28 11:40:09 +0000

    :construction_worker: feat: Can't leave user name blank.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates_presence_of :user_name # Ensures user name can't be blank.
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
