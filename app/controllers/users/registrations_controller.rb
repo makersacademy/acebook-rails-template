53f1d4bef83c857c9ea04d12685e52044a10e963
Simon Green <simon.green@thielsen.co.uk>
2019-02-28 12:17:03 +0000

    removed rubocop violation

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -41,7 +41,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   end
