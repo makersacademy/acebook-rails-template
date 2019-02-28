45325a069e1d12c4ba5462ff36e576f89b0cdfff
david-mears <kieukmail@gmail.com>
2019-02-28 14:14:01 +0000

    make test more specific

--- spec/features/posts_show_date_spec.rb
+++ spec/features/posts_show_date_spec.rb
@@ -1,6 +1,6 @@
 RSpec.feature 'Posts page' do
   scenario 'The posts include their date' do
     log_in
-    expect(page).to have_selector("ul#posts li:nth-child(1)", text: "2019")
+    expect(page).to have_selector("ul#posts li:nth-child(1)", text: "02:51PM - 28 Feb 2019")
   end
 end
