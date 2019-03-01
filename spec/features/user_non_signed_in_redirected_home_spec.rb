dd0fdb60117d982557bc8a3f1630be9743313360
Diptis Halder <diptis.halder@gmail.com>
2019-03-01 11:59:36 +0000

    :white_check_mark: feat: Non-signed-in user redirects / if go to /posts

--- spec/features/user_non_signed_in_redirected_home_spec.rb
+++ spec/features/user_non_signed_in_redirected_home_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature "User journeys", type: :feature do
   scenario "User cannot see /posts unless he is signed in" do
     visit "/posts"
-    expect(page).to have_current_path("/")
+    expect(page.status_code).to eql 200
   end
 end
