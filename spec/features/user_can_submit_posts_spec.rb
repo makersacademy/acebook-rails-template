b9f35f4acfb1cd1450a012f5e5a84cedc484a670
Sherif Shendidy <sherif@shendidy.com>
2019-03-01 17:22:26 +0000

    :white_check_mark: test: passed all tests

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
+    visit "/users/sign_in"
+    fill_in "user[email]", :with => 'sherif@shendidy.com'
+    fill_in "user[password]", :with => '123456'
+    click_button "Sign In"
+    expect(page).to have_content("Add a new post")
+    fill_in "exampleTextarea", :with => 'test message'
+    click_button "Add post"
+    expect(page).to have_content("test message")
   end
 end
