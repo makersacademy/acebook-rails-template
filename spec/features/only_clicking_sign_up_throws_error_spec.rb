460c14eddeb8f0b82fdc5eab2290ffccda1e4796
Diptis Halder <diptis.halder@gmail.com>
2019-03-01 14:38:04 +0000

    :white_check_mark: feat: Only clicking on Sign up gives an error

--- spec/features/only_clicking_sign_up_throws_error_spec.rb
+++ spec/features/only_clicking_sign_up_throws_error_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Sign up", type: :feature do
-  scenario "only clicking on Sign Up throws an error" do
+  scenario "only clicking on Sign Up gives an error" do
     visit "/users/sign_up"
     fill_in "user[email]", with: ""
     fill_in "user[password]", with: ""
     fill_in "user[password_confirmation]", with: ""
     click_button ("Sign up")
-    expect { click_button ("Sign up") }.to raise_error("Missing field data")
+    expect(page).to have_content("2 errors prohibited this user from being saved:")
   end
 end
