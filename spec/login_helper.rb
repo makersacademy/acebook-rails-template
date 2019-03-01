21a2e5e1b0ad5d8bd942cd2320676bc55e5a9762
debora <debora.piu92@gmail.com>
2019-03-01 11:27:51 +0000

    Likes features test

--- spec/login_helper.rb
+++ spec/login_helper.rb
@@ -3,6 +3,7 @@ def create_test_user_1
 end
 
 def log_in_test_user_1
+    create_test_user_1
     visit("/users/sign_in")
     fill_in :user_email, with: 'testuser@makers.com'
     fill_in :user_password, with: 'abc123'
