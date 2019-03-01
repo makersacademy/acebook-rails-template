ff7242d4fe7db4d526726705ac60e9258c24959d
Sherif Shendidy <sherif@shendidy.com>
2019-03-01 17:37:11 +0000

    :white_check_mark: test: Fixed capybara test for travis environment

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit "/users/sign_in"
-    fill_in "user[email]", :with => 'sherif@shendidy.com'
+    visit "/users/sign_up"
+    fill_in "user[user_name]", :with => 'testUser'
+    fill_in "user[email]", :with => 'test@test.com'
     fill_in "user[password]", :with => '123456'
-    click_button "Sign In"
+    fill_in "user[password_confirmation]", :with => '123456'
+    click_button "Sign up"
     expect(page).to have_content("Add a new post")
     fill_in "exampleTextarea", :with => 'test message'
     click_button "Add post"
