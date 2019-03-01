81d83fc4df3ece18d24a3a3397fe9a9f7accf344
Sherif Shendidy <sherif@shendidy.com>
2019-03-01 15:54:41 +0000

    :white_check_mark: test: passed first tests in posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,9 +2,21 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
   describe "GET /new " do
-    it "responds with 200" do
+    it "responds with 302 if not signed in" do
+      get :new
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe "GET /new " do
+    it "responds with 200 if signed in" do
+      user = User.create(user_name: 'testUser', email: 'test@test.com', password: "password", password_confirmation: "password")
+      sign_in user
+
       get :new
       expect(response).to have_http_status(200)
+
+      sign_out user
     end
   end
 
