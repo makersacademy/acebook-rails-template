d666545e7a632f8237d902022480b0d6baf2366e
Brooke Woolley <brooke.woolley@gmail.com>
2019-03-04 16:03:48 +0000

    Deleted image columns from Posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190304155519) do
+ActiveRecord::Schema.define(version: 20190304155950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,10 +49,6 @@ ActiveRecord::Schema.define(version: 20190304155519) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.integer "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
