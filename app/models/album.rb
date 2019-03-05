b3ce751c2603c7d248d9766bfc3451af55606177
debora <debora.piu92@gmail.com>
2019-03-05 16:12:22 +0000

    album_exists? boolean method to find if current_user has an album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,7 @@
 class Album < ApplicationRecord
+
+  def self.album_exists?(current_user)
+    find_by(user_id: current_user.id) != nil
+  end
+
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "#album_exists?" do
+    it "returns true if album exists" do
+      sophiealbum = User.create!(email: "Sophie.Album@gmail.com", first_name: "Sophie", last_name: "Album", password: "sa1234")
+      current_user = double("current_user")
+      allow(current_user).to receive(:id).and_return(1)
+      Album.create(title: "sophies_photos", user_id: 1)
+      expect(Album.album_exists?(current_user)).to eq true
+    end
+  end
+
 end
