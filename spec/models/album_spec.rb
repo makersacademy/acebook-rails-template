5c114cf56fc19fa23d97003a9cd1171e9a3985de
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-03-05 16:44:20 +0000

    tests fail like a boss

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -23,4 +23,23 @@ RSpec.describe Album, type: :model do
       expect(Album.users_first_album(current_user).id).to eq(1)
     end
   end
+
+  context "a new user, creating a post with an image, creates a new album" do
+    it "creates a new album the first time a user uploads an image" do
+      sophiealbum = User.create!(email: "Sophie.Album@gmail.com", first_name: "Sophie", last_name: "Album", password: "sa1234")
+      current_user = double("current_user")
+      allow(current_user).to receive(:id).and_return(1)
+      testpost = Post.create!(user_id: current_user.id, message: "Testing picture upload")
+      phototest = Photo.create(image_file_name: "test.jpg", image_content_type: "image/jpg", user_id: current_user.id, post_id: testpost.id)
+      expect(Album.album_exists?(current_user)).to eq true
+    end
+    it "assigns the album ID to the newly-uploaded picture" do
+      sophiealbum = User.create!(email: "Sophie.Album@gmail.com", first_name: "Sophie", last_name: "Album", password: "sa1234")
+      current_user = double("current_user")
+      allow(current_user).to receive(:id).and_return(1)
+      testpost = Post.create!(user_id: current_user.id, message: "Testing picture upload")
+      phototest = Photo.create(image_file_name: "test.jpg", image_content_type: "image/jpg", user_id: current_user.id, post_id: testpost.id)
+      expect(phototest.album_id).to eq(Album.users_first_album(current_user).id)
+    end
+  end
 end
