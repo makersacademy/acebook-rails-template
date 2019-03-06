b104fead9b8932b245ecf2b1bc17af7fcfad4a28
Sherif Shendidy <sherif@shendidy.com>
2019-03-06 21:36:20 +0000

    :sparkles: feat: Can see new lines in posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  def can_edit(post_id)
+  def can_edit(post_id, current_user_id)
     @post = Post.find(post_id)
     posted_at = @post.created_at
     editable = (Time.now - posted_at) > 600 ? false : true
-    editable ? @post.id == user.id ? true : false : false
+    editable ? @post.user_id == current_user_id ? true : false : false
   end
 end
