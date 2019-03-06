8a14304f9bee8959754d7f89bf745f8667337bc1
Diptis Halder <diptis.halder@gmail.com>
2019-03-06 17:56:12 +0000

    :white_check_mark: feat: Created sign_up_helper and post test

--- spec/features/post_timestamp_verbose_rough_time_spec.rb
+++ spec/features/post_timestamp_verbose_rough_time_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
+require 'sign_up_helper.rb'
 require 'login_helper'
 
 RSpec.feature "Post timestamp", type: :feature do
-  scenario "initially do a new test login" do
-    new_login("madname", "mademail@nutter.com", "abc123")
+  
+  scenario "initially do a new test sign up" do
+    new_sign_up("easyname", "easyemail@easy.com", "abc123")
     expect(page.status_code).to eql 200
     expect(page).to have_content("Add a new post")
   end
+
+  scenario "test that post shows '1 minute ago' if current time is 1 minute ahead of post time" do
+    new_sign_up("easyname", "easyemail@easy.com", "abc123")
+    expect(page.status_code).to eql 200
+    expect(page).to have_content("Add a new post")
+    fill_in "post[message]", :with => 'lots of shizzle me nizzle test message'
+    click_button "Add post"
+    expect(page).to have_content("shizzle me nizzle")
+    sleep (5)
+    # expect(page).to have_content("1 minute ago")
+  end
+
 end
--- spec/login_helper.rb
+++ spec/login_helper.rb
@@ -1,11 +1,9 @@
 require_relative 'rails_helper'
 
-def new_login(testusername, testemail, testpassword)
-  visit "/users/sign_up"
-  fill_in "user[user_name]", with: testusername
+def new_login(testemail, testpassword)
+  visit "/users/sign_in"
   fill_in "user[email]", with: testemail
   fill_in "user[password]", with: testpassword
-  fill_in "user[password_confirmation]", with: testpassword
-  click_button ("Sign up")
+  click_button ("Sign In")
 end
 
