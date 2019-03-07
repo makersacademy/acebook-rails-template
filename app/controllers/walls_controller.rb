1936504123caf9d36845be9478b3ff5a35027a50
Sherif Shendidy <sherif@shendidy.com>
2019-03-07 14:29:27 +0000

    pulled walls from chris

---
+++ app/controllers/walls_controller.rb
@@ -0,0 +1,11 @@
+class WallsController < ApplicationController
+  def index
+    @walls = User.all
+  end
+
+  def show
+    @posts = Post.all.where(wall_user_id: params[:id]).order("updated_at DESC")
+    session[:wall_user_id] = params[:id]
+    @post = Post.new
+  end
+end
