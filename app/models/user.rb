dc99e1ae5691dd653a72c8a6b3d2ad10b62ce068
Brooke Woolley <brooke.woolley@gmail.com>
2019-03-07 12:05:04 +0000

    stash work on profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :photos, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
--- spec/features/user_can_have_profile_picture_spec.rb
+++ spec/features/user_can_have_profile_picture_spec.rb
@@ -1,14 +1,15 @@
-# require 'rails_helper'
-# require 'login_signup_helper'
-#
-# RSpec.feature "Profile picture", type: :feature do
-#   xscenario 'user can upload profile picture on sign up' do
-#     sign_up_wendy_withpicture
-#     expect(page.html).to include("imagename.jpg")
-#   end
-#
-#   scenario 'user can choose not to upload profile picture on sign up and instead use app default' do
-#     sign_up_nellie_nopicture
-#     expect(page.html).to include(".jpg")
-#   end
-# end
+require 'rails_helper'
+require 'login_signup_helper'
+
+RSpec.feature "Profile picture", type: :feature do
+  scenario 'user can upload profile picture on sign up' do
+    sign_up_wendy_withpicture
+    click_on("Edit profile")
+    expect(page).to have_content("Edit User")
+    page.attach_file("post_image", 'spec/fixtures/imagename.jpg')
+    click_on("Update")
+    click_on("wendy.withpicture@gmail.com")
+    expect(page.html).to include("imagename.jpg")
+  end
+
+end
