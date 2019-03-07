3a8a9255abdeef7d3de0fe8517b3108d3c7bd681
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-03-07 15:14:46 +0000

    :white_check_mark: test: user can delete a post

--- spec/features/user_can_delete_a_post_spec.rb
+++ spec/features/user_can_delete_a_post_spec.rb
@@ -3,9 +3,17 @@ require 'rails_helper'
 
 RSpec.feature 'delete', type: :feature do
 
-  scenario 'user can delete a post' do
+  scenario 'user can see a delete button' do
     add_a_post
     expect(page).to have_content("Delete")
   end
+
+  scenario 'user can see a delete button' do
+    add_a_post
+    click_link "Delete"
+    expect(page).not_to have_content('test message')
+  end
+
+
 
 end
