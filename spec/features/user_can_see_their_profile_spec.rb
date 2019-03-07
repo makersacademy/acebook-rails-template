17c4e6e5dbdf1606d18e9b12c45e30fdf86600cb
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-03-07 16:41:55 +0000

    Added test for clicking to profile; fixed older tests to make sure they click the right link

--- spec/features/user_can_see_their_profile_spec.rb
+++ spec/features/user_can_see_their_profile_spec.rb
@@ -4,11 +4,19 @@ RSpec.feature "Profile page", type: :feature do
   scenario "can see their profile page with posts" do
     login_george_manyposts
     expect(page).to have_content('Logged in')
-    click_link('George Manyposts')
+    click_link('George Manyposts', match: :first)
     expect(page).to have_content("George Manyposts")
     expect(page).to have_content("A medium length post of a single sentence of an average length. Number 2")
     visit "/users/2"
     expect(page).to have_content("Percy Onepost")
     expect(page).to have_content("A medium length post of a single sentence of an average length")
   end
+
+  scenario "can click on a different user's profile" do
+    login_george_manyposts
+    expect(page).to have_content('Logged in')
+    click_link('Percy Onepost')
+    expect(page).to have_content("Percy Onepost's Profile")
+  end
+
 end
