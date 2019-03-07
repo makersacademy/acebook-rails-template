a21aab65cee6615eff0e111ca3ce0eb96a0f7278
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-03-07 16:30:37 +0000

    :white_check_mark: test: user can delete content on his own wall, posted by others

--- spec/features/add_a_post_helper.rb
+++ spec/features/add_a_post_helper.rb
@@ -8,3 +8,14 @@ def add_a_post
   fill_in "exampleTextarea", :with => 'test message'
   click_button "Add post"
 end
+
+def add_a_post_two
+  visit "/users/sign_up"
+  fill_in "user[user_name]", :with => 'testUserTwo'
+  fill_in "user[email]", :with => 'testTwo@test.com'
+  fill_in "user[password]", :with => '1234567'
+  fill_in "user[password_confirmation]", :with => '1234567'
+  click_button "Sign up"
+  fill_in "exampleTextarea", :with => 'test message two'
+  click_button "Add post"
+end
--- spec/features/user_can_delete_a_post_spec.rb
+++ spec/features/user_can_delete_a_post_spec.rb
@@ -8,12 +8,42 @@ RSpec.feature 'delete', type: :feature do
     expect(page).to have_content("Delete")
   end
 
-  scenario 'user can see a delete button' do
+  scenario 'user can delete a post' do
     add_a_post
     click_link "Delete"
     expect(page).not_to have_content('test message')
   end
 
+  scenario 'user is not allowed to delete other users posts' do
+    add_a_post
+    click_link "Sign out"
+    add_a_post_two
+    click_link "See other walls"
+    click_link "testUser"
+    expect(page).not_to have_content('Delete')
+  end
+
+  scenario 'user can delete content on his own wall, posted by others' do
+    visit "/users/sign_up"
+    fill_in "user[user_name]", :with => 'testUser'
+    fill_in "user[email]", :with => 'test@test.com'
+    fill_in "user[password]", :with => '123456'
+    fill_in "user[password_confirmation]", :with => '123456'
+    click_button "Sign up"
+    click_link "Sign out"
+    add_a_post_two
+    click_link "See other walls"
+    click_link "testUser"
+    fill_in "exampleTextarea", :with => 'test message three'
+    click_button "Add post"
+    click_link "Sign out"
+    click_link "Sign In"
+    fill_in "user[email]", :with => 'test@test.com'
+    fill_in "user[password]", :with => '123456'
+    click_button "Sign In"
+    expect(page).to have_content('Delete')
+
+  end
 
 
 end
