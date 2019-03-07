563927cd677efaf0d01f561ce7634728ded09628
debora <debora.piu92@gmail.com>
2019-03-07 15:46:37 +0000

    link to profile changed to firstname and lastname in test

--- spec/features/user_can_see_their_profile_spec.rb
+++ spec/features/user_can_see_their_profile_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Profile page", type: :feature do
   scenario "can see their profile page with posts" do
     login_george_manyposts
     expect(page).to have_content('Logged in')
-    click_link('george.manyposts@gmail.com')
+    click_link('George Manyposts')
     expect(page).to have_content("George Manyposts")
     expect(page).to have_content("A medium length post of a single sentence of an average length. Number 2")
     visit "/users/2"
