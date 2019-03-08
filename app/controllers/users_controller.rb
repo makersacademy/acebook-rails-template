ea57bee54f03c85e3b318d6df9f80a62bf15c321
Simon Green <simon.green@thielsen.co.uk>
2019-03-08 11:31:13 +0000

    Remove unused private funcction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,4 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    def user_params
-      params.require(:user).permit(:role, :user_name)
-    end
 end
