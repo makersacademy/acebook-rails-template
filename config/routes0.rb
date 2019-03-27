9d360ac22e0355c18ba640aa18589bfa68b34542
Steve Kahn <44611057+steve-f-kahn@users.noreply.github.com>
2019-03-27 14:50:22 +0000

    Cleaning branch

--- config/routes0.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  get 'welcome/signin'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#signin'
-  resources :posts
-end
