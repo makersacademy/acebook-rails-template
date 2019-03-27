4b3ac3280a3fb2f7e53e5c693dec2eb1dc2a8607
anu <anu_iv2002@hotmail.com>
2019-03-27 18:07:17 +0000

    feature test for date and time

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature "Timeline", type: :feature do
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
+
   end
   scenario "Can see the newest post first" do
     visit "/posts"
@@ -17,6 +18,17 @@ RSpec.feature "Timeline", type: :feature do
     fill_in "Message", with: "Second"
     click_button "Submit"
     first = page.first(".msg").text
-    expect(first).to eq("Second")
+    expect(first).to have_content("Second")
+  end
+
+  scenario "Can see the post with date and time" do
+    post1 = Post.create(message: "Hello")
+    time = post1[:created_at].strftime("%B %d %Y, %l:%M%P")
+    # time = timeFrame.strftime("%B %d %Y, %l:%M%P")
+    visit "/posts"
+    first1 = page.first(".msg").text
+    expect(first1).to have_content("#{time}")
+
+
   end
 end
