fc24ec46c5a60dd1c8f3f9324e9aa9dad4c863f0
Dora Kemeny <dory1318@gmail.com>
2019-03-27 13:57:49 +0000

    feature test for newest post

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -8,4 +8,15 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+  scenario "Can see the newest post first" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "First"
+    click_button "Submit"
+    click_link "New post"
+    fill_in "Message", with: "Second"
+    click_button "Submit"
+    first = page.first(".msg").text
+    expect(first).to eq("Second")
+  end
 end
