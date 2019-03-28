4ff9394a943d0c1809ea268f239fbc44b5e0766a
anu <anu_iv2002@hotmail.com>
2019-03-28 16:11:11 +0000

    refactored the test_helper.rb

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe PostsController, type: :controller do
 
   describe "POST /" do
     it "responds with 200" do
-      fill_in_database
+      fill_in_database("Hello, world!")
       expect(response).to redirect_to(posts_url)
     end
 
     it "creates a post" do
-      fill_in_database
+      fill_in_database("Hello, world!")
       expect(Post.find_by(message: "Hello, world!")).to be
     end
   end
@@ -27,8 +27,8 @@ RSpec.describe PostsController, type: :controller do
     end
 
     it "orders posts in reverse order" do
-      fill_in_database
-      fill_in_database_again
+      fill_in_database("Hello Today")
+      fill_in_database("Second post")
       expect(Post.all.order("created_at DESC")[0][:message]).to eq("Second post")
     end
   end
--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -3,14 +3,13 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     visit "/posts"
-    fill_in_form
+    fill_in_form("Hello world!")
     expect(page).to have_content("Hello world!")
   end
 
   scenario "Can see the newest post first" do
     visit "/posts"
-    fill_in_form
-    fill_in_form_again
+    fill_in_form("Good morning")
     expect(page_content).to have_content("Good morning")
   end
 
