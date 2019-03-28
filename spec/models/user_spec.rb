a74430bc621e3745eb34acc8397b8016fa0a4af7
David Thai <dafuloth@users.noreply.github.com>
2019-03-28 16:27:23 +0000

    fixed the test... create needs a password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   it " User is added to the database" do
-    User.create(email: "a@a.com")
+    # User.create(email: "a@a.com")
+    expect(User.where(email: "a@a.com").present?).to be false
+    User.create(email: "a@a.com", password: "testing")
     expect(User.all[0].email).to eq "a@a.com"
+    # expect(User.where(email: "a@a.com").present?).to be true
   end
-
 end
