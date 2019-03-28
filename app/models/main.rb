4ac32b13ba8fc71a7dee46e1bd5dae804da28222
David Thai <dafuloth@users.noreply.github.com>
2019-03-28 13:44:59 +0000

    main model not actually needed

--- app/models/main.rb
+++
@@ -1,2 +0,0 @@
-class Main < ApplicationRecord
-end
--- db/migrate/20190328122308_create_mains.rb
+++
@@ -1,8 +0,0 @@
-class CreateMains < ActiveRecord::Migration[5.1]
-  def change
-    create_table :mains do |t|
-
-      t.timestamps
-    end
-  end
-end
