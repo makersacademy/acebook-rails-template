58c34442cf7a9d0017cd8cf266273cd9c1bc4b71
Lima Vadakara <lima.vadakara@gmail.com>
2019-03-28 12:34:21 +0000

    Allow multi-line posts to be entered by users. This feature has been developed by adding the simple-send-keys gem to the repo. This gem allows to send key strokes to Capybara to simulate enter and other keys typed in the text area. The change of the input from a text box to text area allows for entering multiple lines on a post. The use of simple_format before displaying the posts on the page, allows for auto creation of <br> tags with \n to show the lines on the posts separately. Feature test for multi-line posts that uses simple-send-keys requires js: true to be set. Selienium webdriver is being used for the feature tests.

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
+require 'simple_send_keys'
 
-RSpec.feature "Timeline", type: :feature do
+RSpec.feature "Timeline", type: :feature, js: true do
   scenario "Can submit posts and view them" do
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
+  end
+
+  scenario "Can submit a multi-line post and view it" do
+    visit "/posts"
+    click_link "New post"
+    find_field('post_message').send_keys('[This, enter, is, enter, a, enter, multi-line, enter, post]')
+    click_button "Submit"
+    expect(page.html).to include("<p>This\n<br>is\n<br>a\n<br>multi-line\n<br>post")
   end
 end
