cc0cc2f5a81c09292987618632dfe951b5d24a34
limavadakara <40064182+limavadakara@users.noreply.github.com>
2019-03-28 16:23:05 +0000

    Remove commented code

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -1,25 +1,12 @@
-#for unit test:
 def fill_in_database(message)
   post :create, params: { post: { message: message } }
 end
 
-# def fill_in_database_again
-#   post :create, params: { post: { message: "Second post" } }
-# end
-
-
-#for feature test:
 def fill_in_form(post)
   click_link "New post"
   fill_in "Message", with: post
   click_button "Submit"
 end
-
-# def fill_in_form_again
-#   click_link "New post"
-#   fill_in "Message", with: "Good morning"
-#   click_button "Submit"
-# end
 
 def page_content
   content = page.first(".msg").text
