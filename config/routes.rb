31281d8afea66595df0c24eee89a67700182db73
Lima Vadakara <lima.vadakara@gmail.com>
2019-03-29 16:16:14 +0000

    Add user sign up/sign in functionality. Redirect user to sign in page if not signed in This implementation uses Devise to create models/views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts, only: [:new, :create, :index]
--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -2,7 +2,7 @@ def fill_in_database(message)
   post :create, params: { post: { message: message } }
 end
 
-def fill_in_form(post)
+def create_new_post(post)
   click_link "New post"
   fill_in "Message", with: post
   click_button "Submit"
@@ -10,4 +10,14 @@ end
 
 def page_content
   page.first(".msg").text
+end
+
+def user_sign_up
+  visit "/posts"
+  expect(page.current_path).to eq '/users/sign_in'
+  click_link('Sign up')
+  fill_in('user_email', :with => 'test@test.com')
+  fill_in('user_password', :with => 'testing123')
+  fill_in('user_password_confirmation', :with => 'testing123')
+  click_button 'Sign up'
 end
