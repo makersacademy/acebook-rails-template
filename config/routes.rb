10a928579f326c365910bf07a22513fa7e57001e
Steve Kahn <steve@kahn.org.uk>
2019-03-29 14:57:41 +0000

    User can delete own posts only

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/update' , to: "posts#update"
   post 'posts/update', to: "posts#edit"
+  get 'posts/delete', to: 'posts#delete'
 
 
   get '/signup', to: 'signup#index'
   post '/signup', to: 'signup#create'
   resources :signup, :posts
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
 end
