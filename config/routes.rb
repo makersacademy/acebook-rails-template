ca30408de13e1b99378362c4c847709cf51798b4
Amina Jusic <amina_jusic@yahoo.com>
2019-03-29 15:06:53 +0000

    Changed the root to signup

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   resources :signup, :posts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index'
+  root 'signup#index'
 end
