0213e590a4c0b68858cd933fdf807c8738a14eba
Jimjule <roadknightj@gmail.com>
2019-03-29 12:35:22 +0000

    remove rails application from module in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,22 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Acebook
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+class Application < Rails::Application
+  # Initialize configuration defaults for originally generated Rails version.
+  config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-  end
+  # Settings in config/environments/* take precedence over those specified here.
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory are automatically loaded.
 end
+
+# module Acebook
+#   class Application < Rails::Application
+#     # Initialize configuration defaults for originally generated Rails version.
+#     config.load_defaults 5.1
+#
+#     # Settings in config/environments/* take precedence over those specified here.
+#     # Application configuration should go into files in config/initializers
+#     # -- all .rb files in that directory are automatically loaded.
+#   end
+# end
