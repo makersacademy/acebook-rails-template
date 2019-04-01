bc1831303e487e75d8d66466aefcae4d5a0e0ef6
Steve Kahn <steve@kahn.org.uk>
2019-04-01 14:42:07 +0100

    Posts css struggles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( signup.css )
+Rails.application.config.assets.precompile += %w( posts.css )
