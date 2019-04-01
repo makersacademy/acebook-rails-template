93206525ed0ac89f887f107fcd29ba6ed79d147e
kate102 <kate.morris.new@gmail.com>
2019-04-01 16:28:35 +0100

    Added timestamp to posts

--- spec/features/user_can_sign_out_spec.rb
+++ spec/features/user_can_sign_out_spec.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature "Sign out and log in", type: :feature do
-  scenario "Can sign out and log in" do
-    visit "/"
-    click_link("Sign up", match: :first)
-    fill_in "Email", with: "emailstupid@duh.com!"
-    fill_in "Password", with: "notapassword"
-    fill_in "Password confirmation", with: "notapassword"
-    click_button "Sign up"
-    click_link "Logout"
-    fill_in "Email", with: "emailstupid@duh.com!"
-    fill_in "Password", with: "notapassword"
-    click_button "Log in"
-    expect(page).to have_content("Logout")
+RSpec.feature 'A user can sign out and log in', type: :feature do
+  scenario 'Can sign out and log in' do
+    visit '/'
+    click_link('Sign up', match: :first)
+    fill_in 'Email', with: 'emailstupid@duh.com!'
+    fill_in 'Password', with: 'notapassword'
+    fill_in 'Password confirmation', with: 'notapassword'
+    click_button 'Sign up'
+    click_link 'Logout'
+    fill_in 'Email', with: 'emailstupid@duh.com!'
+    fill_in 'Password', with: 'notapassword'
+    click_button 'Log in'
+    expect(page).to have_content('Logout')
   end
 end
