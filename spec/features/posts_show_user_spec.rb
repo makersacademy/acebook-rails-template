1697853bad91bbe689a67097d22081843c1e2588
kate102 <kate.morris.new@gmail.com>
2019-04-02 13:17:57 +0100

    Added user email to posts

---
+++ spec/features/posts_show_user_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Posts to show user name', type: :feature do
+  scenario 'all posts will show user name ' do
+    create_user
+    click_link 'New post'
+    fill_in 'Message', with: 'Testing User name'
+    click_button 'Post'
+    test_username = "email@mail.com"
+
+    expect(page).to have_content(test_username)
+  end
+end
