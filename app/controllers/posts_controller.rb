0fe0c1b809d84a13fce7a4235557d3ace2d35821
Dora Kemeny <dory1318@gmail.com>
2019-04-02 11:05:27 +0100

    delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,11 @@ class PostsController < ApplicationController
     @posts = Post.all.order('created_at DESC')
   end
 
+  def destroy
+    @post = Post.find_by(id: params[:id]).destroy
+    redirect_to posts_url
+  end
+
   private
 
   def post_params
