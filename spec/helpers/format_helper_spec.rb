379b30bde4bf3154cf8e96344f70f7b77e0b03a7
Luke Phyall <lukeprospect@yahoo.com>
2019-04-02 10:15:55 +0100

    Adds passing test for break helper

--- spec/helpers/format_helper_spec.rb
+++ spec/helpers/format_helper_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe FormatHelper, type: :helper do
  describe "#linebreak" do
    it "substitutes a <br> for a newline" do
      message = "Here is a message \n with a new line in it"
-     expect().to eq()
+     breaks = "Here is a message <br/> with a new line in it"
+     expect(helper.linebreak(message)).to eq(breaks)
    end
  end
 end
