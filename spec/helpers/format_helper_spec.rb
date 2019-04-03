153f0023da90dade45bf9863156176b6773b528a
Luke Phyall <lukeprospect@yahoo.com>
2019-04-03 14:43:55 +0100

    Reimplements text area in posts with capacity for new lines.

--- spec/helpers/format_helper_spec.rb
+++ spec/helpers/format_helper_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe FormatHelper, type: :helper do
  describe "#linebreak" do
    it "substitutes a <br> for a newline" do
      message = "Here is a message \n with a new line in it"
-     breaks = "Here is a message <br/> with a new line in it"
+     breaks = "Here is a message <br/><br/> with a new line in it"
      expect(helper.linebreak(message)).to eq(breaks)
    end
  end
