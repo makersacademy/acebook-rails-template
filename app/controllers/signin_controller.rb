d070ec618cd185f742494e7e5f2f86ed1bff2908
Oliver <ofcharrison@gmail.com>
2019-04-04 14:05:16 +0100

    hopefully website bug fixed

--- app/controllers/signin_controller.rb
+++ app/controllers/signin_controller.rb
@@ -1,16 +1,23 @@
 class SigninController < ApplicationController
   def index
     # @user = Signup.find_by(username: params[:username])
-end
+  end
 
   def new
   end
 
   def create
-    session[:user_id] = nil
+    unless Signup.find_by(id: session[:user_id]) == nil
+       # Signup.find(
+       session[:user_id] = nil
+       session[:user] = nil
+
+    end
+
     @user = Signup.find_by(username: params[:signin][:username])
     if @user && @user.authenticate(params[:signin][:password])
-      session[:user] == @user.id
+      session[:user] = @user
+      session[:user_id] = @user.id
       session[:current_user] = @user
       redirect_to posts_url
     else
@@ -20,6 +27,9 @@ end
   end
 
   def destroy
+    Signup.find_by(id: session[:user_id])
+    # @delete = Signup.find(session[:user_id])
+    session[:user] = nil
     session[:user_id] = nil
     redirect_to "/signin"
   end
