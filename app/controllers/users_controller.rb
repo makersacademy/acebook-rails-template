600ddb5782b835e3cf148fddd5562e2090a78e3f
justcallmegoyo <justcallmegoyo@gmail.com>
2019-04-24 16:56:16 +0100

    post_params changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).require(:email).require(:password)
+    params.require(:user).permit(:email).permit(:password)
   end
 end
