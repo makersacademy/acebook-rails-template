d5b74ed57eb3d708adb0672e5c89655a1b5e2ca3
justcallmegoyo <justcallmegoyo@gmail.com>
2019-04-24 16:39:34 +0100

    deleted routes file problem

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-  resources :users
-  resources :posts
-=======
   resources :posts, :users
->>>>>>> d1a152c5621af5bdab6d7c4f854e17bb4fd9c7dc
-=======
-  resources :posts, :users
->>>>>>> 8b2bd6f9973a976f4042053c7c04f0c009a8cdb8
 end
