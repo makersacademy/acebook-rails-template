a3e43d32c25e023806138dda24a62cc4daab6138
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-04-24 13:53:45 +0100

    ADD: Submitting signup form takes you to /posts
    test created, and code written to pass test which navigates to posts path
    after entering email and password and click button.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    redirect_to posts_path
+  end
 end
--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -6,4 +6,14 @@ RSpec.feature 'Sign up', type: :feature do
     expect(page).to have_field("user[email_address]")
     expect(page).to have_field("user[password]")
   end
+
+
+  # maybe need to return to this later as am unsure if posts path is limited.
+  scenario 'Completing the signup form navigates to posts route' do
+    visit '/'
+    fill_in 'user[email_address]', with: 'myemail@gmail.com'
+    fill_in 'user[password]', with: 'mypassword'
+    click_button 'Sign up'
+    expect(page).to have_current_path("/posts")
+  end
 end
