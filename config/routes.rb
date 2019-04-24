18fbf53d5964026a8ee5eecc484daa66b23e1eb2
Riya Dattani <riya_dattani@hotmail.com>
2019-04-24 11:35:25 +0100

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'posts/index'
 
   resources :posts
+
+  root 'posts#index'
 end
