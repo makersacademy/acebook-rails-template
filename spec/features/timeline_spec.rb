05874c10f212afdf749a9479568d105fa6432985
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-24 11:26:14 +0100

    Feature: Posts appear with newest post first
    [Link to trello](https://trello.com/c/4e1TZC2k/9-posts-appear-with-newest-post-first-low)
    
    Displays posts in reverse order (newest first)

--- spec/features/timeline_spec.rb
+++ spec/features/timeline_spec.rb
@@ -17,4 +17,15 @@ RSpec.feature "Timeline", type: :feature do
     created_at = Post.all[0].created_at.strftime('%H:%M, %-d %B')
     expect(page).to have_content(created_at)
   end
+
+  scenario "Can see date and time posts submitted" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world again!"
+    click_button "Submit"
+    expect("Hello, world again!").to appear_before "Hello, world!"
+  end
 end
