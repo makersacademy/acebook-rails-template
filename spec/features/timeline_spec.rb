afbd4de1fa16c39a88d6c00b045c29fdf4c7c482
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-24 11:00:54 +0100

    Refactors previous commit to add formatted time to post display

--- spec/features/timeline_spec.rb
+++ spec/features/timeline_spec.rb
@@ -14,7 +14,7 @@ RSpec.feature "Timeline", type: :feature do
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
-    created_at = Post.all[0].created_at
+    created_at = Post.all[0].created_at.strftime('%H:%M, %-d %B')
     expect(page).to have_content(created_at)
   end
 end
