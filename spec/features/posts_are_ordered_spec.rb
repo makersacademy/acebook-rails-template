8736e6b3c930d8e6aa53f07e212a04fbee52d161
Paul Martin <communty.hivemind@gmail.com>
2019-04-24 16:21:00 +0100

    Update posts_are_ordered_spec.rb

--- spec/features/posts_are_ordered_spec.rb
+++ spec/features/posts_are_ordered_spec.rb
@@ -5,14 +5,14 @@ RSpec.feature 'Posts are ordered', type: :feature do
 
     visit "/posts"
     click_link "New post"
-    fill_in "Message", with: "Last"
+    fill_in "Message", with: "First post"
     click_button "Submit"
 
     click_link "New post"
-    fill_in "Message", with: "First"
+    fill_in "Message", with: "Second post"
     click_button "Submit"
     visit "/posts"
 
-    expect(page.body.index("First")).to be < page.body.index("Last")
+    expect(page.body.index("Second post")).to be < page.body.index("First post")
   end
 end
