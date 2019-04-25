5a935a4ba02c9b4c392ed90368d834078c9bea15
Riya Dattani <riya_dattani@hotmail.com>
2019-04-25 17:24:23 +0100

    added a fake user to pass post controller tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,32 +1,35 @@
-# frozen_string_literal: true
+require 'rails_helper'
 
-# require 'rails_helper'
-#
-# RSpec.describe PostsController, type: :controller do
-#   describe 'GET /posts/new ' do
-#     it 'redirects to home page' do
-#       get :posts
-#       get :new
-#       expect(response).to redirect_to("/users/sign_in")
-#     end
-#   end
+RSpec.describe PostsController, type: :controller do
+  let(:user) { FactoryBot.create(:user) }
 
-  # describe 'POST /' do
-  #   it 'responds with 200' do
-  #     post :create, params: { post: { message: 'Hello, world!' } }
-  #     expect(response).to redirect_to(posts_url)
-  #   end
-  #
-  #   it 'creates a post' do
-  #     post :create, params: { post: { message: 'Hello, world!' } }
-  #     expect(Post.find_by(message: 'Hello, world!')).to be
-  #   end
-  # end
-  #
-  # describe 'GET /' do
-  #   it 'responds with 200' do
-  #     get :index
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
-# end
+  before do
+    sign_in user
+  end
+
+  describe "GET /new " do
+    it "responds with 200" do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "POST /" do
+    it "responds with 200" do
+      post :create, params: { post: { message: "Hello, world!" } }
+      expect(response).to redirect_to(posts_url)
+    end
+
+    it "creates a post" do
+      post :create, params: { post: { message: "Hello, world!" } }
+      expect(Post.find_by(message: "Hello, world!")).to be
+    end
+  end
+
+  describe "GET /" do
+    it "responds with 200" do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
+end
