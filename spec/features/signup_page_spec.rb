c551a5b599e6aacc167d9b68d41292a62c4236e0
James Palmer <jamesdcpalmer1@gmail.com>
2019-04-25 10:41:53 +0100

    Test for too long password

--- spec/features/signup_page_spec.rb
+++ spec/features/signup_page_spec.rb
@@ -16,4 +16,14 @@ RSpec.feature 'Sign up', type: :feature do
     click_button 'Sign up'
     expect(page).to have_content("Password is too short (minimum is 6 characters)")
   end
+
+  scenario 'User can only enter a password of 10 or less characters' do
+    visit 'users/sign_up'
+    fill_in 'user_email', with: 'james@test.com'
+    fill_in 'user_password', with: '12345678910'
+    fill_in 'user_password_confirmation', with: '12345678910'
+    click_button 'Sign up'
+    expect(page).to have_content("Password is too long (maximum is 10 characters)")
+  end
+
 end
