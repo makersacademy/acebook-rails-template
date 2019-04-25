3d481f270db06a4894d75a0e76b6395b4964adce
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-04-25 13:20:48 +0100

    ADD: Enabled feature tests for sign up notification.
    The feature tests for sign up notifications were pending until the work on the sign up form was complete. After merging in the sign up form from master we are able to enable the feature tests which are now passing

--- spec/features/user_signup_shows_confirmation_message_spec.rb
+++ spec/features/user_signup_shows_confirmation_message_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.feature "Sign up message", type: :feature do
 
-  xscenario "After sign up, user sees message saying sign up complete" do
+  scenario "After sign up, user sees message saying sign up complete" do
     visit "/users/new"
-    fill_in 'email', with: 'test@gmail.com'
-    fill_in 'password', with: '123'
-    click_button "Sign up"
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: '123'
+    click_button "Create my account"
     expect(page).to have_content("Congratulations, you are signed up!")
   end
-  
-  xscenario "After sign up and refresh of posts page, no message is displayed" do
+
+  scenario "After sign up and refresh of posts page, no message is displayed" do
     visit "/users/new"
-    fill_in 'email', with: 'test@gmail.com'
-    fill_in 'password', with: '123'
-    click_button "Sign up"
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: '123'
+    click_button "Create my account"
     visit '/posts'
 
     expect(page).to_not have_content("Congratulations, you are signed up!")
