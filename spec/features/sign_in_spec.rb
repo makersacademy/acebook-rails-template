9cb9955d0d555f8dbfde1c8d92996573fe366b46
henrygambles <gambles.henry2@gmail.com>
2019-04-25 16:56:48 +0100

    WIP - redirect to index/beautify url names

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 require 'sign_up_helper'
 
 RSpec.feature "Sign in", type: :feature do
-  scenario "User redirected if not signed in & visits posts" do 
+  scenario "User redirected if not signed in & visits posts, then prompted to sign in" do 
     visit '/posts'
     expect(page).to have_content("Log in")
     expect(page).to have_button("Log in")
+  end
+
+  scenario "User redirected if not signed in & visits posts" do 
+    visit '/posts'
+    expect(page).to have_current_path("/users/sign_in")
   end
 
 end
