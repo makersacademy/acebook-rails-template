9ca12c030891c0e6faa653c67308bf0903b6befc
saypop <tome.m.dj@gmail.com>
2019-04-26 14:19:31 +0100

    passed unit test for post, verify id

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+
+  def self.verify_user(session_user, user_post)
+    session_user.id == user_post.user_id
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,17 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it { is_expected.to be }
+
+  describe '.verify_user' do
+
+
+    it 'verifies that post creator is current user' do
+      user_dbl = double("user", :id => 1)
+      user_post_dbl = double("post", :user_id => 1)
+      other_post_dbl = double("post", :user_id => 2)
+      expect(Post.verify_user(user_dbl, user_post_dbl)).to be(true)
+      expect(Post.verify_user(user_dbl, other_post_dbl)).to be(false)
+    end
+
+  end
 end
