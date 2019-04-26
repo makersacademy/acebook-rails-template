747053e9aa9e40922cd0265ff9d67fd495d4e08b
Paul Martin <communty.hivemind@gmail.com>
2019-04-26 14:39:26 +0100

    REFACTOR: posts tests use `create_post` helper

---
+++ spec/features/posts_can_be_modified_by_their_owners_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.feature "Editing posts", type: :feature do
+  scenario "An 'edit' link should appear on a user's post" do
+    sign_up
+    create_post
+    expect(page).to have_link('Edit')
+  end
+
+  scenario "An 'edit' link should not appear on a different user's post" do
+    sign_up email: 'user1@gmail.com'
+    create_post
+    sign_up email: 'user2@gmail.com'
+    visit '/posts'
+    expect(page).not_to have_link('Edit')
+  end
+end
