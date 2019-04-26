31082b5ab0cd6a5c6c703fc35b5d559b173d8837
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-26 12:10:53 +0100

    Prevents user updating another user's post
    User story 2: Update own posts
    ------------------------------
    As a user,
    so that the integrity of shared posts is maintained,
    I want to be sure that only I can update my posts.
    ```
    
    2. Update link is not available for posts where `user_id` of post != `current_user.id`

--- spec/sign_up_helper.rb
+++ spec/sign_up_helper.rb
@@ -1,8 +1,15 @@
 def create_user_and_sign_up
   visit "/"
-
   fill_in("user_email", with: "georgie@com")
   fill_in("user_password", with: "password1")
   fill_in("user_password_confirmation", with: "password1")
+  click_button("Sign up")
+end
+
+def create_second_user_and_sign_in
+  visit "/"
+  fill_in("user_email", with: "evelyn@com")
+  fill_in("user_password", with: "password2")
+  fill_in("user_password_confirmation", with: "password2")
   click_button("Sign up")
 end
