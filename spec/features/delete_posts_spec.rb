a6cc6f3697070199919301409097fe397df48d2e
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-04-26 15:14:18 +0100

    Gets back to passing tests with selenium

--- spec/features/delete_posts_spec.rb
+++ spec/features/delete_posts_spec.rb
@@ -1,27 +1,16 @@
 require 'rails_helper'
 require 'sign_up_helper'
 
-RSpec.feature "Update posts", type: :feature do
-  scenario "user can update an existing post" do
+ RSpec.feature "Delete posts", type: :feature do
+  Capybara.current_driver = :selenium
+  scenario "user can delete an existing post" do
     create_user_and_sign_up
     visit "/posts"
     click_link "New post"
-    fill_in "Message", with: "This message is going to be updated!"
+    fill_in "Message", with: "This message is going to be deleted!"
     click_button "Submit"
-    click_link "Update"
-    fill_in "Message", with: "This message has been updated!"
-    click_button "Update"
-    expect(page).to have_content "This message has been updated!"
-  end
-
-  scenario "user cannot update other user's post" do
-    create_user_and_sign_up
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "User 1 message"
-    click_button "Submit"
-    click_button "Sign out"
-    create_second_user_and_sign_in
-    expect(page).to have_no_link "Update"
+    click_button "Delete"
+    page.driver.browser.switch_to.alert.accept
+    expect(page).not_to have_content "This message is going to be deleted!"
   end
 end
