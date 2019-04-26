e590b337d44e1c1b9cc9354d52118c797cd13652
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-04-26 14:28:00 +0100

    Still trying to sort out default user and posts user_id

--- db/migrate/20190426130643_add_default_user_to_users.rb
+++ db/migrate/20190426130643_add_default_user_to_users.rb
@@ -2,6 +2,7 @@ class AddDefaultUserToUsers < ActiveRecord::Migration[5.1]
   def change
     if User.find_by(id: -1)
     else
-      User.create({ email: 'notarealemail@makers.com', password: 'admin' })
+      User.create({ email: 'notarealemail@makers.com', password: 'admin', id: -1 })
+    end
   end
 end
