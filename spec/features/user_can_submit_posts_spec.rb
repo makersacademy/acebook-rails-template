c434ae3116b94b051be9c93380dedca67ad8ffdd
Cosmin <mirceacosmin@gmail.com>
2019-04-26 15:57:04 +0100

    Updated tests to take account of authentication

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -3,11 +3,20 @@
 require 'rails_helper'
 
 RSpec.feature 'Timeline', type: :feature do
+  include Warden::Test::Helpers
+
+  let(:user) {user = User.create(email: 'test@test.com', password: "password", password_confirmation: "password") }
+  
   scenario 'Can submit posts and view them' do
+    login_as(user, :scope => :user)
+
     visit '/posts'
+
     click_link 'New post'
+    save_and_open_page
     fill_in 'Message', with: 'Hello, world!'
     click_button 'Post!'
+
     expect(page).to have_content('Hello, world!')
   end
 end
--- spec/features/users_can_delete_posts_spec.rb
+++ spec/features/users_can_delete_posts_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature 'Timeline', type: :feature do
+  include Warden::Test::Helpers
+
+  let(:user) {user = User.create(email: 'test@test.com', password: "password", password_confirmation: "password") }
+  
   scenario 'Can delete pre-made posts' do
+    login_as(user, :scope => :user)
+
     visit '/posts'
     click_link 'New post'
     fill_in 'Message', with: 'Hello, world!'
