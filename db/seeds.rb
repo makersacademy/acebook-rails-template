db204206f4745f58b47adb37e1fe2f1e14d2333c
alexanderchen6 <alexbaggio315@hotmail.com>
2019-04-26 14:36:44 +0100

    modified seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = {}
+user['password'] = 'asdf'
+
+ActiveRecord::Base.transaction do
+  10.times do 
+    user['first_name']= Faker::Name.first_name
+    user['last_name']= Faker::Name.last_name
+    user['email'] = Faker::Internet.email
+    User.create(user)
+  end 
+end 
+
+post = {}
+uids = []
+User.all.each {|u| uids << u.id}
+
+ActiveRecord::Base.transaction do 
+  20.times do 
+    post['message']= Faker::Hipster.sentence
+    post['user_id']= uids.sample
+    Post.create(post)
+  end 
+end 
