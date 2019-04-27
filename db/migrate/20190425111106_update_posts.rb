2492c7acd819e5c377fa1bdd7223e2f150fce986
alexanderchen6 <alexbaggio315@hotmail.com>
2019-04-27 12:31:43 +0100

    testing database fix for aws

--- db/migrate/20190425111106_update_posts.rb
+++ db/migrate/20190425111106_update_posts.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class UpdatePosts < ActiveRecord::Migration[5.1]
-  def change; end
+  def change
+  end
 end
--- db/migrate/20190426084412_add_user_id_to_posts.rb
+++ db/migrate/20190426084412_add_user_id_to_posts.rb
@@ -1,5 +1,6 @@
 class AddUserIdToPosts < ActiveRecord::Migration[5.1]
   def change
-    add_column :posts, :user_id, :integer
+    # add_column :posts, :user_id, :integer
+    add_reference :posts, :user, foreign_key: true
   end
 end
