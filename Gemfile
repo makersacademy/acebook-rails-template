a443434527b71a77c8f6f0c6170e28502e5bf1f1
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-04-27 18:18:00 +0100

    ADD: Tidied up test & coverage
    Configured coverall so test coverage is displayed in the readme on the git repo
    Removed test files that weren't being used
    Configured simplecov to ignore certain directories that do not require testing (This was skewing coverage results)

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 3.5'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'coveralls', require: false
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,17 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'coveralls'
+Coveralls.wear!('rails')
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  ])
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
