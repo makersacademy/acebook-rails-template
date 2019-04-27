219af5d237ebdc3f043121d4faa271500c377d32
Arthur Ashman <arthurashman@gmail.com>
2019-04-27 10:07:49 +0100

    REFACTOR: Add further flash messages to sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,12 +22,18 @@ class UsersController < ApplicationController
     else
       if user_with_same_email
         flash[:danger] = "An account with this email already exists"
+      elsif user.errors.details[:email].include? ({:error=>:invalid, :value=>""})
+        flash[:danger] = "Email field cannot be empty"
       elsif user.errors[:email].any? 
         flash[:danger] = "Invalid email address"
-      elsif user.errors[:password].any?
-        flash[:danger] = "Invalid password"
-      end
-      redirect_to root_path
+      elsif user.errors.details[:password] == [{:error=>:too_short, :count=>6}]
+        flash[:danger] = "Password too short - must be 6-10 characters in length"
+      elsif user.errors.details[:password] == [{:error=>:too_long, :count=>10}]
+        flash[:danger] = "Password too long - must be 6-10 characters in length"
+      elsif user.errors.details[:password].include? ({:error=>:blank})
+        flash[:danger] = "Password field cannot be empty"
+    end
+    redirect_to root_path
     end
   end
 end
