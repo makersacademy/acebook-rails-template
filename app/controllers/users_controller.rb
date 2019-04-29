7fcb923e7b47e4b6735e939ba7781a1faeeef410
Arthur Ashman <arthurashman@gmail.com>
2019-04-29 13:20:21 +0100

    Refactor sign-up error messages
    We used `user.errors.full_messages` which gives you an array of
    human-readable error messages.
    
    Plus we refactored the posts index view to get the posts's author's
    email address directly and deleted the helper method. Neater, and we
    delegated the `.email` method from the post through to the post's
    author in accordance with the Law of Demeter.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
     email = params[:user][:email_address].downcase
     password = params[:user][:password]
 
-    user_with_same_email = User.find_by(email: email)
 
     user = User.new(email: email, password: password)
 
@@ -18,22 +17,11 @@ class UsersController < ApplicationController
       user.save
       session[:user_id] = user.id
       flash[:success] = "New account created"
-      redirect_to posts_path
+      redirect_to posts_path and return
     else
-      if user_with_same_email
-        flash[:danger] = "An account with this email already exists"
-      elsif user.errors.details[:email].include? ({:error=>:blank})
-        flash[:danger] = "Email field cannot be empty"
-      elsif user.errors[:email].any? 
-        flash[:danger] = "Invalid email address"
-      elsif user.errors.details[:password] == [{:error=>:too_short, :count=>6}]
-        flash[:danger] = "Password too short - must be 6-10 characters in length"
-      elsif user.errors.details[:password] == [{:error=>:too_long, :count=>10}]
-        flash[:danger] = "Password too long - must be 6-10 characters in length"
-      elsif user.errors.details[:password].include? ({:error=>:blank})
-        flash[:danger] = "Password field cannot be empty"
+      flash[:danger] = user.errors.full_messages.join("<br>")
     end
+
     redirect_to root_path
-    end
   end
 end
