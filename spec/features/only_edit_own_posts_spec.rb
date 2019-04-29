88e84fba9c76a516b91dfa647daa5d7e35efb502
kimdiep <kim.diep.techno@gmail.com>
2019-04-29 14:36:03 +0100

    Feature: Cannot edit after 10 minutes from post creation

--- spec/features/only_edit_own_posts_spec.rb
+++ spec/features/only_edit_own_posts_spec.rb
@@ -13,4 +13,13 @@ RSpec.feature 'Edit button', type: :feature do
     expect(page).to have_content("You can't edit somebody elses post!")
     expect(current_path).to eq("/posts")
   end
+
+  scenario 'Cannot edit 10 minutes after post creation' do
+    sign_up
+    post_message
+    allow(Time).to receive(:now).and_return(@time_now + 11*60)
+    click_link('Edit')
+    expect(page).to have_content("You can't edit after 10 minutes!")
+  end
+
 end
