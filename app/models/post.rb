cd780d4383dd697873bfb3a7c94e52276c17333c
Paul Martin <communty.hivemind@gmail.com>
2019-04-29 16:46:28 +0100

    Create 'isEditable' method on Post model
    Returns true if post is less than 10 minutes old, false otherwise

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,14 @@
 class Post < ApplicationRecord
   belongs_to :user
   delegate :email, :to => :user
+  
+  def isEditable
+    isLessThan10MinutesOld
+  end
+
+  private
+
+  def isLessThan10MinutesOld
+    created_at > 10.minutes.ago
+  end
 end
