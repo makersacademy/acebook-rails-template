148f51ef5189e774f33515c95244c13c3cf4b4ef
James Palmer <jamesdcpalmer1@gmail.com>
2019-04-30 11:09:43 +0100

    TDD index and show pages for users

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
 
   end
   resources :posts
+  resources :users, only: [:show, :index]
 
 end
---
+++ spec/features/view_user_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'feature_test_helpers'
+
+RSpec.feature 'View user', type: :feature do
+  scenario 'After clicking user link' do
+    sign_up
+    click_link('Logout')
+    sign_up_2
+    click_link('All Users')
+    click_link('james@test.com')
+    expect(current_path).to include("/users/")
+    expect(page).to have_content("james@test.com")
+  end
+end
