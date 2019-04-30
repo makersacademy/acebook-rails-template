b320e23146ed485c458c76901f9d94e00d0e7cc0
Paul Martin <communty.hivemind@gmail.com>
2019-04-30 10:24:29 +0100

    Restrict automatically generated routes
    We were automatically generating all routes for our users and posts
    resources, some of which were unnecessary. So I've restricted the number
    of routes generated to what we are actually using. This is in line with
    Rails best practices, and cuts down on memory usage, speeding up the
    routing process.
    
    https://rails-bestpractices.com/posts/2011/08/19/restrict-auto-generated-routes/

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   delete "/logout", to: "sessions#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :posts
+  resources :users, only: [:create]
+  resources :posts, only: [:index, :new, :edit, :create, :update, :destroy]
 
   root "users#new"
 end
