97cb1334443d184ef973198a1e0ba509dd36fa13
Wardah <wlt_93@hotmail.com>
2019-04-30 10:58:38 +0100

    tests working for deleting any post on the post page, using post id

--- spec/features/deleting_post_spec.rb
+++ spec/features/deleting_post_spec.rb
@@ -13,18 +13,19 @@ RSpec.feature "Delete post", type: :feature do
     expect(page).to_not have_content("Please delete me!!")
   end
 
-  # scenario "Testing to delete the second post" do
-  #   sign_up_helper('test@test.com','123456')
-  #   log_in_helper('test@test.com','123456')
-  #   click_link "New post"
-  #   fill_in "Message", with: "Please delete me!!"
-  #   click_button "Submit"
-  #   expect(page).to have_content("Please delete me!!")
-  #   click_link "New post"
-  #   fill_in "Message", with: "Please delete me toooo!!"
-  #   click_button "Submit"
-  #   expect(page).to have_content("Please delete me toooo!!")
-  #   page.all(:xpath, '//*[@id="2"]').click
-  #   expect(page).to_not have_content("Please delete me toooo!!")
-  # end
+  scenario "Testing to delete the second post" do
+    sign_up_helper('test@test.com','123456')
+    log_in_helper('test@test.com','123456')
+    click_link "New post"
+    fill_in "Message", with: "Please delete me!!"
+    click_button "Submit"
+    expect(page).to have_content("Please delete me!!")
+    click_link "New post"
+    fill_in "Message", with: "Please delete me toooo!!"
+    click_button "Submit"
+    expect(page).to have_content("Please delete me toooo!!")
+    deleteID = Post.find_by(message: "Please delete me toooo!!").id
+    find_button(deleteID).click
+    expect(page).to_not have_content("Please delete me toooo!!")
+  end
 end
