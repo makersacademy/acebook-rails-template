8a5b2d18327b83ebc95d4b00cef2397929a33aa0
hybridbad <hybridbad90@gmail.com>
2019-04-30 16:04:51 +0100

    create recipient id in posts table using user_id as FK

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190430132944) do
+ActiveRecord::Schema.define(version: 20190430145716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 20190430132944) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
+    t.bigint "recipient_id"
     t.index ["author_id"], name: "index_posts_on_author_id"
+    t.index ["recipient_id"], name: "index_posts_on_recipient_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 20190430132944) do
   end
 
   add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "recipient_id"
 end
