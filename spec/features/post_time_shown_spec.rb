12beece1a3b3aad6907976bdba77b81125c966ed
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-04-30 16:05:08 +0100

    REFACTOR: Restructured posts page with divs

--- spec/features/post_time_shown_spec.rb
+++ spec/features/post_time_shown_spec.rb
@@ -7,6 +7,6 @@ RSpec.feature "Posts time shown", type: :feature do
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
-    expect(page.first(:css, "span").text).to match(/^([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/\d\d\d\d\s([0-1]?[0-9]|2?[0-3]):([0-5]\d)$/)
+    expect(first(:css, 'div.timestamp').text).to match(/^([1-9]|([012][0-9])|(3[01]))\/([0]{0,1}[1-9]|1[012])\/\d\d\d\d\s([0-1]?[0-9]|2?[0-3]):([0-5]\d)$/)
   end
 end
