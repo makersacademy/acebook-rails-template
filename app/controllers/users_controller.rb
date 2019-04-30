28687d67a29e4f1ec813df067b97efd1081e6d07
saypop <tome.m.dj@gmail.com>
2019-04-30 17:03:26 +0100

    TDD'ed requests page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,22 @@
 class UsersController < ApplicationController
 
   def show
-   @user = User.find(params[:id])
- end
+    @user = User.find(params[:id])
+  end
 
- def index
-   @users = User.all
- end
+  def index
+    @users = User.all
+  end
 
- def addfriend
+  def addfriend
     @user = User.find(params[:user_id])
     current_user.friend_request(@user)
     flash[:success] = "You have sent #{@user.email} a friend request!"
     redirect_to users_url
+  end
+
+  def friend_requests
+    @users = current_user.requested_friends
   end
 
 end
