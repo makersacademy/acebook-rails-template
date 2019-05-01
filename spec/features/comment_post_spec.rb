675fa34ddb8487462f5155c2eba8adf886a32d14
Amy Jordan <amy_jordan@live.co.uk>
2019-05-01 10:02:22 +0100

    Adds tests for edge cases User does not see button to edit or delete comments that they didn't post

--- spec/features/comment_post_spec.rb
+++ spec/features/comment_post_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require 'sign_up_helper'
+require 'sign_in_helper'
 require 'create_post_helper'
 
 RSpec.feature "Comment posts", type: :feature do
@@ -14,42 +15,60 @@ RSpec.feature "Comment posts", type: :feature do
     click_button 'Add comment'
   end
 
-  scenario "User can comment on any post" do
-    expect(page).to have_content 'Test adding comment to post'
-  end
+  feature "Add comment" do
+    scenario "User can comment on any post" do
+      expect(page).to have_content 'Test adding comment to post'
+    end
 
-  scenario "User sees success message on adding comment" do
-    expect(page).to have_content 'Salmon had to say it!'
-  end
-
-  scenario "User can edit their comment" do
-    click_link 'Edit comment'
-    fill_in 'comment_body', with: 'Updated comment'
-    click_button 'Update'
-    expect(page).to have_content 'Updated comment'
-  end
-
-  scenario "User cannot edit their comment after 10 minutes" do
-    Timecop.travel(Time.now + 11.minutes) do
-      visit '/posts'
-      expect(page).to have_no_link 'Edit comment'
+    scenario "User sees success message on adding comment" do
+      expect(page).to have_content 'Salmon had to say it!'
     end
   end
 
-  scenario "User sees success message on editing their comment" do
-    click_link 'Edit comment'
-    fill_in 'comment_body', with: 'I want to see a success message'
-    click_button 'Update'
-    expect(page).to have_content 'You krill me!'
+  feature "Edit comment" do
+    scenario "User can edit their comment" do
+      click_link 'Edit comment'
+      fill_in 'comment_body', with: 'Updated comment'
+      click_button 'Update'
+      expect(page).to have_content 'Updated comment'
+    end
+
+    scenario "User cannot edit their comment after 10 minutes" do
+      Timecop.travel(Time.now + 11.minutes) do
+        visit '/posts'
+        expect(page).to have_no_link 'Edit comment'
+      end
+    end
+
+    scenario "User cannot edit a different user's comment" do
+      click_button 'Sign out'
+      first_user_sign_in
+      expect(page).to have_no_link 'Edit comment'
+    end
+
+    scenario "User sees success message on editing their comment" do
+      click_link 'Edit comment'
+      fill_in 'comment_body', with: 'I want to see a success message'
+      click_button 'Update'
+      expect(page).to have_content 'You krill me!'
+    end
   end
 
-  scenario "User can delete their comment" do
-    click_button 'Delete comment'
-    expect(page).not_to have_content 'Test adding comment to post'
-  end
+  feature "Delete comment" do
+    scenario "User can delete their comment" do
+      click_button 'Delete comment'
+      expect(page).not_to have_content 'Test adding comment to post'
+    end
 
-  scenario "User sees success message on deleting their comment" do
-    click_button 'Delete comment'
-    expect(page).to have_content 'That comment is fish-tory'
+    scenario "User cannot delete a different user's comment" do
+      click_button 'Sign out'
+      first_user_sign_in
+      expect(page).to have_no_button 'Delete comment'
+    end
+
+    scenario "User sees success message on deleting their comment" do
+      click_button 'Delete comment'
+      expect(page).to have_content 'That comment is fish-tory'
+    end
   end
 end
