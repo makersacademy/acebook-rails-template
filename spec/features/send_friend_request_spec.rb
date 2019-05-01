4d92ca4f48aab587a24addff27b6b7305308d599
kimdiep <kim.diep.techno@gmail.com>
2019-05-01 16:35:19 +0100

    manage requests from user index

--- spec/features/send_friend_request_spec.rb
+++ spec/features/send_friend_request_spec.rb
@@ -19,4 +19,32 @@ RSpec.feature 'Send friend request', type: :feature do
     click_link('My friends')
     expect(page).to_not have_content('Add friend')
   end
+
+  scenario "cannot do two requests" do
+    sign_up
+    click_link('Logout')
+    sign_up_2
+    click_link('All Users')
+    click_link('Add friend')
+    expect(current_path).to include('/users')
+    expect(page).not_to have_content("james@test.com Add friend")
+    expect(page).to have_content("james@test.com Request pending...")
+  end
+
+  scenario "cannot request someone who requested you" do
+    sign_up
+    click_link('Logout')
+    sign_up_2
+    click_link('All Users')
+    click_link('Add friend')
+    click_link('Logout')
+    sign_in
+    click_link('All Users')
+    expect(page).to have_content("tome@test.com View friend request.")
+    click_link('View friend request.')
+    expect(current_path).to include("/friend_requests")
+  end
 end
+
+
+
