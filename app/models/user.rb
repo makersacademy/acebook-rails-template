0cef327cd4e3fa8c056f1e40dd88f4bab1fe61a0
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-05-01 14:53:17 +0100

    ADD: Posting users email now shows against their posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_reader :email
   has_secure_password
   # validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 end
