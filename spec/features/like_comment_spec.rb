168d25b7a32f4066121727faa0450b1f3dfdf676
Amy Jordan <amy_jordan@live.co.uk>
2019-05-02 14:37:45 +0100

    User can unlike a comment they have previously liked
    As a user,
    so that I can show my appreciation for comments people have added to posts,
    I want to be able to like comments.

--- spec/features/like_comment_spec.rb
+++ spec/features/like_comment_spec.rb
@@ -16,4 +16,18 @@ RSpec.feature "Like comments", type: :feature do
     click_on(class: 'like-comment-button')
     expect(page).to have_css("#comment_number_of_likes", text: "1")
   end
+
+  scenario "User can unlike a comment they have previously liked" do
+    create_user_and_sign_up
+    create_post
+    visit '/posts'
+    fill_in 'comment_body', with: 'Adding a comment'
+    click_button 'Add comment'
+    click_button 'Sign out'
+    create_second_user_and_sign_in
+    visit '/posts'
+    click_on(class: 'like-comment-button')
+    click_on(class: 'unlike-comment-button')
+    expect(page).to have_css("#comment_number_of_likes", text: "0")
+  end
 end
