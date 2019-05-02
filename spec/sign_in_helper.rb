6a6f2a4391b65d627890f17df7454c2174ae2c1c
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-05-02 16:41:17 +0100

    Adds styling to login, sign-up and header
    Starts styles for posts page

--- spec/sign_in_helper.rb
+++ spec/sign_in_helper.rb
@@ -1,6 +1,6 @@
 def first_user_sign_in
-  click_link("Log in")
+  click_link("Login")
   fill_in("user_email", with: "georgie@com")
   fill_in("user_password", with: "password1")
-  click_button("Log in")
+  click_button("Login")
 end
