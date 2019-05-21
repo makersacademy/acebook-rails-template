a791d6bc6654e0d2a3ec8c1d30c2bbc1f5083d2b
guilhe0756 <guiheurich@gmail.com>
2019-05-21 13:56:38 +0100

    Rubocop set-up, solved a number of offenses

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -11,7 +13,8 @@ module Acebook
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/*
+    # take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
