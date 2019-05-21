06de0cbf56fd2952b6c98528da1d83c1a5884b11
Hannah Frost <h.7frost@gmail.com>
2019-05-21 10:44:01 +0100

    Purposely making travis fail

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -6,6 +6,6 @@ RSpec.feature "Timeline", type: :feature do
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
-    expect(page).to have_content("Hello, world!")
+    expect(page).to have_content("Helo, world!")
   end
 end
