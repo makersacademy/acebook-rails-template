7ce8c46b73bdc3edb37b66dde4daf78bda11c67b
domw30 <domw30@gmail.com>
2019-05-22 16:55:55 +0100

    add update/edit/show (not complete)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,43 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
 
   def create
     @post = Post.create(post_params)
     redirect_to posts_url
   end
 
-  def index
-    @posts = Post.all
-  end
+
 
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_url
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    #
+    # if @post.update(post_params)
+    # else
+    render 'edit'
+    # end
   end
 
   private
