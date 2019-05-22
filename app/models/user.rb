70a11f362fca94245fce4d521511bb10479c1288
Jake Napper <napperjlg@gmail.com>
2019-05-22 11:20:10 +0100

    first name presence validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+
+  it 'should be a valid user' do
+    @user = User.new(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "john123456")
+    assert @user.valid?
+  end
+
+  it 'should have a valid first name' do
+    @user = User.new(first_name: "", last_name: "Doe", email: "john@doe.com", password: "john123456")
+    assert !(@user.valid?)
+  end
+
 end
