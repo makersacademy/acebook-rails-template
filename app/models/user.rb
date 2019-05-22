a1008e6ae229dbd764a1c3f80a1b5396c31630f0
Jake Napper <napperjlg@gmail.com>
2019-05-22 11:51:39 +0100

    all sign up fields need to be filled in for user to be valid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,15 +3,36 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
 
-
   it 'should be a valid user' do
     @user = User.new(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "john123456")
     assert @user.valid?
   end
 
-  it 'should have a valid first name' do
-    @user = User.new(first_name: "", last_name: "Doe", email: "john@doe.com", password: "john123456")
-    assert !(@user.valid?)
+  context 'fields should be filled in' do
+
+    it 'first name should be present' do
+      @user = User.new(first_name: "", last_name: "Doe", email: "john@doe.com", password: "john123456")
+      assert !@user.valid?
+    end
+
+    it 'last name should be present' do
+      @user = User.new(first_name: "John", last_name: "", email: "john@doe.com", password: "john123456")
+      assert !@user.valid?
+    end
+
+    it 'email address should be present' do
+      @user = User.new(first_name: "John", last_name: "Doe", email: "", password: "john123456")
+      assert !@user.valid?
+    end
+
+    it 'password should be present' do
+      @user = User.new(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "")
+      assert !@user.valid?
+    end
   end
+
+
+
+
 
 end
