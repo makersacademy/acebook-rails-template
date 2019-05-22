8fcc79306de2a7566b83d67253e0b56ff74f0c3a
Calum Darroch <calum_darroch@hotmail.com>
2019-05-22 15:00:39 +0100

    Password formatting implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6, maximum: 10 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-
   it 'should be a valid user' do
     @user = User.new(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "john123456")
     assert @user.valid?
@@ -31,7 +30,23 @@ RSpec.describe User, type: :model do
     end
   end
 
+  context 'password conforms to valid format' do
 
+    it 'should be between six and ten characters' do
+      @user = User.new(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "john12")
+      assert @user.valid?
+      @user2 = User.new(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "john123456")
+      assert @user2.valid?
+    end
+
+    it 'should not be less than six or more than ten characters' do
+      @user = User.new(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "john")
+      assert !@user.valid?
+      @user2 = User.new(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "john12456789")
+      assert !@user2.valid?
+    end
+
+  end
 
 
 
