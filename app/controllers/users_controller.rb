79e932fdcd8f21c93c9bd7d2ed6f125705f595fa
Jake Napper <napperjlg@gmail.com>
2019-05-22 14:36:20 +0100

    'create' and 'new' user routes created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(post_params)
+    redirect_to posts_url
+end
+
+private
+
+def post_params
+  params.require(:user).permit(:first_name, :last_name, :email, :password)
+end
 end
