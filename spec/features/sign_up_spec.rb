e7853348041d770e7d9590dfa0d4679d60c62594
Xa1n <xhs893@gmail.com>
2019-05-22 10:34:43 +0100

    Refactored test to visit index before running expects
    Co-authored-by: Ayelisha Amoah <ayelisha.amoah@gmail.com>
    Co-authored-by: Max Roberts-Dear <maxhroberts@gmail.com>

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "Sign Up", type: :feature do
-    scenario "Can see Sign Up form" do
-      visit "/"
-      expect(page).to have_content("Sign Up")
-    end
 
-    scenario "Can fill out Sign Up form" do
-      visit("/")
-      fill_in "Email", with: "test@email.com"
-      fill_in "Password", with: "testpassword"
-      click_button "Submit"
-      expect(page).to have_content("Sign Up Successful!")
-    end
+  before(:each) do
+    visit("/")
+  end
+
+  scenario "Can see Sign Up form" do
+    expect(page).to have_content("Sign Up")
+  end
+
+  scenario "Can fill out Sign Up form" do
+    fill_in "Email", with: "test@email.com"
+    fill_in "Password", with: "testpassword"
+    click_button "Submit"
+    expect(page).to have_content("Sign Up Successful!")
+  end
 end
