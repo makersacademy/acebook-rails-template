3f0d897a9396f02fb7d789a6654d3b60bc0d2a0b
Oliver Cripps <ollycripps@gmail.com>
2019-05-22 15:20:33 +0100

    updated table schema and invalid login flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log in the user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -6,3 +6,15 @@ RSpec.feature "Login", type: :feature do
     expect(page).to have_content("Log in")
   end
 end
+
+RSpec.feature "UsersLogins", type: :feature do
+  scenario "login with invlaid information" do
+    visit '/sessions/new'
+    expect(page).to have_content 'New bear?'
+    click_on 'Log in'
+    expect(page).to have_content 'Invalid email/password combination'
+    visit '/users/new'
+    visit '/sessions/new'
+    expect(page).not_to have_content 'Invalid email/password combination'
+  end
+end
