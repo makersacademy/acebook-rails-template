3fcb547b88b6cc4f2658ca97239280a41e555e8d
Kiah Hewitt <hewittkiah@gmail.com>
2019-05-22 15:00:42 +0100

    added log in feature and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   resources :users
 
   root "posts#new"
   get "/signup", to: 'users#new'
-  
-
-
+  get "/login",  to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
