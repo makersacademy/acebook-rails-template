a0ec54351a9e71722434275bfd0b65170ab86d21
guilhe0756 <guiheurich@gmail.com>
2019-05-22 12:14:28 +0100

    Posts can now be deleted

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,32 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  # GET request
   def new
     @post = Post.new
   end
 
+  # POST request
   def create
     @post = Post.create(post_params)
     redirect_to posts_url
   end
 
+  # GET request
   def index
     @posts = Post.all.order(created_at: :desc)
   end
+
+  # GET request
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # DELETE request
+  def destroy
+    @posts = Post.destroy(params[:id])
+  end
+
 
   private
 
