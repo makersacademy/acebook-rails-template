5661845c10e213140c5f2efdf4cbd9f9e45ca2fd
Calum Darroch <calum_darroch@hotmail.com>
2019-05-23 11:14:11 +0100

    Update foratting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.create(post_params)
+    @user = User.create(user_params)
     redirect_to posts_url
-end
+  end
 
-private
+  private
 
-def post_params
-  params.require(:user).permit(:first_name, :last_name, :email, :password)
-end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
