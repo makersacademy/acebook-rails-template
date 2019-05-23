a5e7750e85518b4c14e3fb879ede7a7b4c0c35d5
Hannah Frost <h.7frost@gmail.com>
2019-05-23 16:22:43 +0100

    Added foreign key
    Co-authored-by: Marcus Clairmont <marcusclairmont@gmail.com>
    Co-authored-by: Xain Storey <xhs893@gmail.com>
    Co-authored-by: Ayelisha Amoah <ayelisha.amoah@gmail.com>
    Co-authored-by: Max Roberts-Dear <maxhroberts@gmail.com>
    Co-authored-by: Jose Quinones <josemanuelqn.88@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190523145237) do
+ActiveRecord::Schema.define(version: 20190523150715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,8 @@ ActiveRecord::Schema.define(version: 20190523145237) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +31,5 @@ ActiveRecord::Schema.define(version: 20190523145237) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
