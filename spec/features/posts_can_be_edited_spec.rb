5f19e0600b4cb413bbf09a31ca19a848a1ec73ea
Stuart <stuart.pethurst@gmail.com>
2019-05-24 13:59:54 +0100

    simplecove fix

--- spec/features/posts_can_be_edited_spec.rb
+++ spec/features/posts_can_be_edited_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-# require 'rails_helper'
-#
-# RSpec.feature 'Updating Posts', type: :feature do
-#   scenario 'user can edit posts' do
-#     visit '/posts'
-#     click_link 'New post'
-#     fill_in 'Message', with: 'Hello, world!'
-#     click_button 'Submit'
-#     click_button 'Edit'
-#     fill_in 'Message', with: 'Hello, planet!'
-#     click_button 'Submit'
-#     expect(page).to have_content("Hello, planet")
-#   end
-# end
+require 'rails_helper'
+
+RSpec.feature 'Updating Posts', type: :feature do
+  scenario 'user can edit posts' do
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+    click_button 'Edit'
+    fill_in 'Message', with: 'Hello, planet!'
+    click_button 'Submit'
+    expect(page).to have_content("Hello, planet")
+  end
+end
