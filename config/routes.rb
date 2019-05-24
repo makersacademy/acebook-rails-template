4d74bffb8c7d9ddb698eeec536be7664deb0e24a
Jake Napper <napperjlg@gmail.com>
2019-05-24 14:53:03 +0100

    set homepage to login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, :users
 end
