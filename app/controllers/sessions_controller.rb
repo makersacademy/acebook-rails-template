bbbb11bffa730919536d4a8f0db47ce93382e506
Jake Napper <napperjlg@gmail.com>
2019-05-24 10:26:48 +0100

    logged in user displayed in view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,14 @@
 class SessionsController < ApplicationController
+
+  include SessionsHelper
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
+    log_in user
     redirect_to '/posts'
   else
     flash.now[:error] = 'Incorrect email/password'
