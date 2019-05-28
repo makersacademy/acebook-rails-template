a2e3957e3ad86f86ced813c3cf44a32399b34a8c
Stuart <stuart.pethurst@gmail.com>
2019-05-28 14:26:58 +0100

    devise login working

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -3,12 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe 'GET /new ' do
-    it 'responds with 200' do
-      get :new
-      expect(response).to have_http_status(200)
-    end
-  end
+  # describe 'GET /new ' do
+  #   it 'responds with 200' do
+  #     get :new
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
 
   describe 'POST /' do
     xit 'responds with 200' do
