e80c5853fd6ce2ec4e8e8cc94559557b43c6e379
Stuart <stuart.pethurst@gmail.com>
2019-05-28 16:10:04 +0100

    added prodcution file config for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # devise
+  config.action_mailer.default_url_options = { :host => 'steam-acebook.herokuapp.com' }
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger =
