196a4b5bdbf0d09da62bede5c77d8376431a02c0
Hannah Frost <h.7frost@gmail.com>
2019-05-28 12:17:53 +0100

    Added devise login and sign up forms
    Co-authored-by: Marcus Clairmont <marcusclairmont@gmail.com>
    Co-authored-by: Xain Storey <xhs893@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'homepage#index'
+  root 'posts#index'
   get 'homepage/login'
   resources :posts
 end
