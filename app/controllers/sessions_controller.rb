91e2b465d09acd264fb524cf824da0800a5beec2
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-05-28 09:56:01 +0100

    updated font to roboto condensed, refactored for rubocops

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,21 +3,23 @@ class SessionsController < ApplicationController
   include SessionsHelper
 
   def new
+
   end
 
   def create
     user = User.find_by(email: params[:session][:email])
-      if user && user.authenticate(params[:session][:password])
-        log_in user
-        redirect_to '/posts'
-      else
-        flash.now[:error] = 'Incorrect email/password'
-        render 'new'
-      end
-   end
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to '/posts'
+    else
+      flash.now[:error] = 'Incorrect email/password'
+      render 'new'
+    end
+  end
 
-   def destroy
-     log_out
-     redirect_to login_url
-   end
+  def destroy
+    log_out
+    redirect_to login_url
+  end
+
 end
