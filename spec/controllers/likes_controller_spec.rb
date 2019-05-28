3b893f1e88171a96d80fb69b5f7e9abc83841c19
Calum Darroch <calum_darroch@hotmail.com>
2019-05-28 13:54:08 +0100

    Likes model and controller implemented, user can only like a post once

---
+++ spec/controllers/likes_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe LikesController, type: :controller do
+
+  # To test:
+  # That user id is stored on the like?
+  # Number of likes changes?
+  # Mock the user in the unit test...
+
+end
