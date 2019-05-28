123a02272114990eaa9c3de31324a3042d621400
Kiah Hewitt <hewittkiah@gmail.com>
2019-05-28 16:47:33 +0100

    added search bar function

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   devise_for :users
+  get 'search/index'
+  get 'search' => 'search#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
   resources :posts
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20_190_528_104_715) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
   create_table 'posts', force: :cascade do |t|
     t.string 'message'
