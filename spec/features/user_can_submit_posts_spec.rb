75144818184a07cfc9b48928a8df0e97767c2c31
shektor <44544977+shektor@users.noreply.github.com>
2019-05-28 12:19:25 +0100

    Add feature tests sign up and submit post
    co-authored-by: Carly <carly.jenkinson65@googlemail.com>

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,11 +1,25 @@
 require 'rails_helper'
 
-# RSpec.feature "Timeline", type: :feature do
-#   scenario "Can submit posts and view them" do
-#     visit "/posts"
-#     click_link "New post"
-#     fill_in "post[message]", with: "Hello, world!"
-#     click_button "Submit"
-#     expect(page).to have_content("Hello, world!")
-#   end
-# end
+RSpec.feature "Timeline", type: :feature do
+  scenario "Can submit posts and view them" do
+    first_name = 'Bob'
+    last_name = 'Geldof'
+    email = 'its.me@bobgeldof.com'
+    password = 'bob123'
+
+    visit "/users/new"
+    fill_in('user[first_name]', with: first_name)
+    fill_in('user[last_name]', with: last_name)
+    fill_in('user[email]', with: email)
+    fill_in('user[password]', with: password)
+    click_button('Signup')
+    expect(page).to have_selector("input[type=submit][value='Log in']")
+    fill_in('session[email]', with: email)
+    fill_in('session[password]', with: password)
+    click_button('Log in')
+    click_link "New post"
+    fill_in "post[message]", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world!")
+  end
+end
