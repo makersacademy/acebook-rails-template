c5a6aa954d1af6218bf206fdaa0ecdda2a07baea
Hannah Frost <h.7frost@gmail.com>
2019-05-28 15:32:11 +0100

    Added username params to sign up
    Co-authored-by: Marcus Clairmont <marcusclairmont@gmail.com>
    Co-authored-by: Xain Storey <xhs893@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
