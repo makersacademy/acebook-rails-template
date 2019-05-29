d69281bf1de75e787fb9c69c649cf1fc61d195ad
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-05-29 14:20:14 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190528135441) do
+ActiveRecord::Schema.define(version: 20190528102102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20190528135441) do
     t.string "password_digest"
   end
 
-  add_foreign_key "posts", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
