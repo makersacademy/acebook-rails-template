750802b4d216f015a1e86865955d12cc94f32603
Hannah Frost <h.7frost@gmail.com>
2019-05-29 10:11:10 +0100

    Altered application_controller.rb, null_session -> exception
    Co-authored-by: Marcus Clairmont <marcusclairmont@gmail.com>
    Co-authored-by: Xain Storey <xhs893@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
   before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
