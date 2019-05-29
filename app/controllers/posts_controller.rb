298f41f731c84b496fc987a43586539517b891e1
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-05-29 15:16:29 +0100

    Add stub for current user in post controller tests
    Co-authored-by: Hannah Frost <h.7frost@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,8 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.create(post_params)
+    # @post.user_id = current_user.id
+    # @post.save
     redirect_to posts_url
   end
 
