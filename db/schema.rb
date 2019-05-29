fba2402e20fa3533281cf83fcf5c355c62a8e8ac
guilhe0756 <guiheurich@gmail.com>
2019-05-29 11:07:18 +0100

    Updated DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20_190_528_104_715) do
     t.string 'message'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table 'users', force: :cascade do |t|
@@ -35,4 +37,6 @@ ActiveRecord::Schema.define(version: 20_190_528_104_715) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
