5ef410aebaf55655036ec6cc05c11a1059475da6
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-05-29 10:45:19 +0100

    add username to post model and update posts index view

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :message, presence: true
+
+  def username
+    user = User.find(user_id)
+    "#{user.first_name} #{user.last_name}"
+  end
+
+
+
+
 end
