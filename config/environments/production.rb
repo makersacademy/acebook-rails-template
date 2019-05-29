f0f248197df6ee58338a9aa66571d382dc04b036
Stuart <stuart.pethurst@gmail.com>
2019-05-29 11:01:00 +0100

    fix for images on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version`
   # have moved to config/initializers/assets.rb
