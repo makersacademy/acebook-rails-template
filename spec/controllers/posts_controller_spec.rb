7a22d50f66d1afc23f4cdc86e140ec0adbc113b8
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-05-29 17:41:35 +0100

    remove automatically generated pending tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,44 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  describe "GET /new" do
-    it "responds with 302" do
-      get :new
-      expect(response).to have_http_status(302)
-    end
-  end
-
-  xdescribe "POST /" do
-    it "responds with 200" do
-      post :create, params: { post: { message: "Hello, world!" } }
-      expect(response).to redirect_to(posts_url)
-    end
-
-    it "creates a post" do
-      post :create, params: { post: { message: "Hello, world!" } }
-      post = Post.find_by(message: "Hello, world!")
-      expect(post.message).to eq "Hello, world!"
-    end
-  end
-
-  describe "DELETE /" do
-    xit 'deletes a post' do
-      post :create, params: { post: { message: "Hello, world!" } }
-      post = Post.find_by(message: "Hello, world!")
-      post.destroy
-      expect(Post.exists?(post.id)).to be false
-    end
-  end
-
-  describe "PATCH /" do
-    xit 'updates a post' do
-      post :create, params: { post: { message: "Hello, world!" } }
-      post = Post.find_by(message: "Hello, world!")
-      post.update(message: "Sup, dudes!")
-      expect(post.message).to eq "Sup, dudes!"
-    end
-  end
-
+  # describe "GET /new" do
+  #   it "responds with 302" do
+  #     get :new
+  #     expect(response).to have_http_status(302)
+  #   end
+  # end
+  #
+  # xdescribe "POST /" do
+  #   it "responds with 200" do
+  #     post :create, params: { post: { message: "Hello, world!" } }
+  #     expect(response).to redirect_to(posts_url)
+  #   end
+  #
+  #   it "creates a post" do
+  #     post :create, params: { post: { message: "Hello, world!" } }
+  #     post = Post.find_by(message: "Hello, world!")
+  #     expect(post.message).to eq "Hello, world!"
+  #   end
+  # end
+  #
+  # describe "DELETE /" do
+  #   xit 'deletes a post' do
+  #     post :create, params: { post: { message: "Hello, world!" } }
+  #     post = Post.find_by(message: "Hello, world!")
+  #     post.destroy
+  #     expect(Post.exists?(post.id)).to be false
+  #   end
+  # end
+  #
+  # describe "PATCH /" do
+  #   xit 'updates a post' do
+  #     post :create, params: { post: { message: "Hello, world!" } }
+  #     post = Post.find_by(message: "Hello, world!")
+  #     post.update(message: "Sup, dudes!")
+  #     expect(post.message).to eq "Sup, dudes!"
+  #   end
+  # end
+  
   describe "GET /" do
     it "responds with 200" do
       get :index
