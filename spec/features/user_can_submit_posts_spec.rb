ff7635a71e44903edfc6e67685008455eb6339ee
Kintaro-Oe <marcusclairmont@gmail.com>
2019-05-30 18:09:22 +0100

    add "newest post first" feature test, dry up tests

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -8,10 +8,15 @@ RSpec.feature "Timeline", type: :feature do
   end
 
   scenario "Can submit posts and view them" do
-    visit "/posts"
+    new_post_hello_world
+    expect(page).to have_content("hello world")
+  end
+
+  scenario "Newest posts are shown first" do
+    new_post_hello_world
     click_link "New post"
-    fill_in "What's on your mind?", with: "Hello, world!"
+    fill_in "What's on your mind?", with: "test 2"
     click_button "Submit"
-    expect(page).to have_content("Hello, world!")
+    expect(page.body.index('test 2')).to be < page.body.index('hello world')
   end
 end
