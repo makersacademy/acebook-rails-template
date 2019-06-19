19b3061842ce879f28cb22c2e225ab002a000c12
Sam <samkitchen94@gmail.com>
2019-06-19 15:18:36 +0100

    updated db migration

--- db/migrate/20190618152229_create_users.rb
+++ db/migrate/20190618152229_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |g|
-      g.string :name
-      g.string :email
-      g.string :password
-      g.string :confirm_password
+    create_table :users do |t|
+      # g.string :name
+      # g.string :email
+      # g.string :password
+      # g.string :confirm_password
 
-      g.timestamps
+      t.timestamps
     end
   end
 end
