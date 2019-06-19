bb2ef6539720337a02b3eb902dd0ff20fed7abad
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-06-19 16:45:10 +0100

    Raise errors for invalid signup user details
    Co-authored-by: VladC24 <vlad.chiriac24@gmail.com>
    Co-authored-by: EdinaBMakers <bolkeny.edina@gmail.com>
    Co-authored-by: Cosmin Croitoriu <cosmin.c.croitoriu@gmail.com>

--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -16,6 +16,63 @@ RSpec.feature "Signup", type: :feature do
     end
   end
 
+  context "User must fill in a valid password" do
+    scenario "user left password field empty" do
+      sign_up(email: "test@test.com", password: "")
+
+      expect(page).to have_content("Password can't be blank")
+      expect(page).to have_content("Password is too short (minimum is 6 characters)")
+    end
+
+    scenario "user provides a password less than 6 characters" do
+      sign_up(email: "test@test.com", password: "123")
+
+      expect(page).to have_content("Password is too short (minimum is 6 characters)")
+    end
+
+    scenario "user provides a password longer than 10 characters" do
+      sign_up(email: "test@test.com", password: "12345678910")
+
+      expect(page).to have_content("Password is too long (maximum is 10 characters)")
+    end
+  end
+
+  context "User must provide a valid email address" do
+    scenario "user must provide an email address" do 
+      sign_up(email: "", password: "123456789")
+
+      expect(page).to have_content("Email can't be blank")
+    end
+
+    scenario "user provides an email address that has already been taken" do
+      sign_up(email: "test@test.com", password: "123456789")
+
+      expect(page).to have_current_path("/posts")
+
+      sign_up(email: "test@test.com", password: "12345")
+
+      expect(page).to have_content("Email has already been taken")
+    end
+
+    scenario "user provides an email address that doesn't have 'at sign'" do
+      sign_up(email: "testtest.com", password: "123456789")
+
+      expect(page).to have_content("Email is invalid")
+    end
+
+    scenario "user provides an email address that doesn't have unique user name" do
+      sign_up(email: "@test.com", password: "123456789")
+
+      expect(page).to have_content("Email is invalid")
+    end
+
+    scenario "user provides an email address that doesn't have domain name" do
+      sign_up(email: "test@", password: "123456789")
+
+      expect(page).to have_content("Email is invalid")
+    end
+  end
+
   scenario "after completing the signup form it navigates to posts page" do
     sign_up(email: "test@test.com", password: "password")
 
