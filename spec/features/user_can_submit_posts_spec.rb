05fc00fe108ea79a3051e4641612d7276ff6050f
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-06-20 08:35:12 +0100

    Update tests

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
+    sign_up(email: "test@test.com", password: "password")
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
