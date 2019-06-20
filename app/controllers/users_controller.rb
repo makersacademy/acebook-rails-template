4e135863e440bec4cfff19934ccce6f30d3191c5
Yemi <yemiadebayo60@gmail.com>
2019-06-20 16:45:21 +0100

    Add Sign-up
    co-authored-by: LisaMcCormack <lisamccormack85@gmail.com>
    co-authored-by: fetc90 <francescachater@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  #
+
+  def show
+    @user = User.find(params[:id])
+    redirect_to users_path
+  end
+
+
   def create
     # render plain: params[:user].inspect
-    @user = User.new(user_params)
+    @user = User.new(user_params) 
 
     @user.save
     redirect_to @user
--- spec/features/users_can_signup_spec.rb
+++ spec/features/users_can_signup_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Signup", type: :feature do
     fill_in 'user_name', with: "Lisa"
     fill_in "user_email", with: "lisa@email.com"
     fill_in "user_password", with: "Password"
-    click_button "Sign up"
-    expect(page).to have_content("Sign in")
+    click_button "Sign Up"
+    expect(page).to have_content("Sign In")
   end
 end
