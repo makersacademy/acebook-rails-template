91e7468287e0547845bf67c5a4d6f734389af8f4
Nicholas Barnes <n3ttl3t@gmail.com>
2019-06-20 14:27:16 +0100

    retry
    co-authored-by: LisaMcCormack <lisamccormack85@gmail.com>
    co-authored-by: fetc90 <francescachater@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,14 @@ class UsersController < ApplicationController
   end
   #
   def create
-    render plain: params[:user].inspect
+    # render plain: params[:user].inspect
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to @user
+  end
+
+  private def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { is_expected.to be }
+  it "is valid with valid attributes" do
+    expect(User.new(name: 'Lisa', email: 'lisa@email.com', password: 'password')).to be_valid
+  end
+  it "is not valid without a name" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+  it "is not valid without a password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
 end
