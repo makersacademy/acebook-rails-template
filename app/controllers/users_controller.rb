5c6249f32ac39886ca318eb68d2d688f1a46d3f3
Chris <chris.daf@gmail.com>
2019-06-21 12:01:04 +0100

    added flash notice for reusing email address

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     session[:user] = @user
-    redirect_to posts_url
+    if @user.save
+      redirect_to posts_url
+    else
+      flash[:error] = "Email address already taken"
+      redirect_to users_url
+    end
   end
 
   private
