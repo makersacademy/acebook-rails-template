1f82bbdb919079fc803a4234975864c7157ed8cc
Yemi <yemiadebayo60@gmail.com>
2019-06-21 11:11:30 +0100

    Amend User table migration

--- spec/features/users_can_signup_spec.rb
+++ spec/features/users_can_signup_spec.rb
@@ -3,7 +3,6 @@
 RSpec.feature "Signup", type: :feature do
   scenario "sign up page has name, email and password fields" do
     visit '/users/new'
-    save_and_open_page
     fill_in 'user_name', with: "Lisa"
     fill_in "user_email", with: "lisa@email.com"
     fill_in "user_password", with: "Password"
