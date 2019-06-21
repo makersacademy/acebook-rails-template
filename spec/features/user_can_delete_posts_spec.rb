d2a02f8dccb2a94421746d6148bfbaf4180e4b2c
Student <comsin.c.croitoriu@gmail.com>
2019-06-21 11:16:37 +0100

    renamed the delete feature test

--- spec/features/user_can_delete_posts_spec.rb
+++ spec/features/user_can_delete_posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Posts", type: :feature do
-  scenario "Views posts in reverse chronological  order" do
+  scenario "Users are able to delete thier own posts" do
     sign_up(email: "test@test.com", password: "password")
     visit "/posts/new"
     fill_in "Message", with: "Post number one"
