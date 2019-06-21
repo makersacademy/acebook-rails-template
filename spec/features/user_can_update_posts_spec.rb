f68c7264e0dc86adc8d55d2fa5c52dce23c23e34
Student <comsin.c.croitoriu@gmail.com>
2019-06-21 11:26:19 +0100

    renamed the update test

--- spec/features/user_can_update_posts_spec.rb
+++ spec/features/user_can_update_posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Posts", type: :feature do
-  scenario "user can update his posts" do
+  scenario "user can update posts" do
     sign_up(email: "test@test.com", password: "password")
     visit "/posts/new"
     fill_in "Message", with: "Post number one"
