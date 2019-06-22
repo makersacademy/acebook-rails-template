77603112580642a55ec68d6d8eb7f39842c7a3ab
VladC <vlad.chiriac24@gmail.com>
2019-06-22 22:26:42 +0100

    Posts can't be edited after 10 min - flash message showing on all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :can_edit, :only => [ :edit, :update, :destroy ]
-  
+  before_action :can_edit, only: [ :edit, :update, :destroy ]
+  before_action :check_time!, only: [:edit, :update]
 
   def index
     @posts = Post.order("created_at DESC")
@@ -45,6 +45,13 @@ class PostsController < ApplicationController
   def can_edit
     @post = Post.find(params[:id])
     unless @post and current_user and current_user.can_edit? @post
+      redirect_to posts_url
+    end
+  end
+
+  def check_time!
+    if Time.now() > @post.created_at + 10.minutes
+      flash[:created_at] = "Post can only be edited 10 min after it has been created"
       redirect_to posts_url
     end
   end
