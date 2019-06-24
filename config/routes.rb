6552c35e2f7d10e46ff9d9397835e85f54fb1b06
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-06-24 11:39:55 +0100

    generated a new users table with Devise for signup
    Co-authored-by: Ben Reynolds-Carr <bjr.kitz@gmail.com>
    Co-authored-by: Sam Kitchen <samkitchen94@gmail.com>
    Co-authored-by: Ray Swilling <ray_swilling@yahoo.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'welcome' => 'users#index'
-  resources :posts, :users
 
-  root :to => 'users#index'
+  resources :posts
+
+  root :to => 'home#index'
 end
