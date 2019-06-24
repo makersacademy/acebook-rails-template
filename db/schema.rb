bbce37e55c5307f3601c869a2784f05220ac017a
Laurence <ltaylorapp@gmail.com>
2019-06-24 16:59:53 +0100

    Trying to get all tests to pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190621144402) do
+ActiveRecord::Schema.define(version: 20190624143645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20190621144402) do
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,17 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   subject { described_class.new }
-  
+
   it 'is not valid without a message' do
     expect(subject).to_not be_valid
   end
 
+  it 'is not valid without a userid' do
+    subject.message = 'Anything'
+    expect(subject).to_not be_valid
+  end
+
   it 'is valid with valid attributes' do
+    user = User.create('test', 'test@test.com', '123')
     subject.message = "Anything"
+    subject.user_id = user.id
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a userid'
   it 'has a timestamp'
 end
-
