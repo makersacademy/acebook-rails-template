b68ab8d3e68bde74260300787190e15e62c2804d
VladC <vlad.chiriac24@gmail.com>
2019-06-24 11:12:54 +0100

    Update tests to pass for reverese chronological order and posts controller spec
    Co-authored-by: lbarber729 <lbarber729@hotmail.co.uk>

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+
+
   describe "GET /new " do
     it "responds with 200" do
       session[:user_id] = 1
@@ -11,13 +13,15 @@ RSpec.describe PostsController, type: :controller do
 
   describe "POST /" do
     it "responds with 200" do
-      session[:user_id] = 1
+      @user = User.create(email: "test@test.com", password: "password")
+      session[:user_id] = @user.id
       post :create, params: { post: { message: "Hello, world!" } }
       expect(response).to redirect_to(posts_url)
     end
 
     it "creates a post" do
-      session[:user_id] = 1
+      @user = User.create(email: "test@test.com", password: "password")
+      session[:user_id] = @user.id
       post :create, params: { post: { message: "Hello, world!" } }
       expect(Post.find_by(message: "Hello, world!")).to be
     end
@@ -31,3 +35,4 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 end
+  
