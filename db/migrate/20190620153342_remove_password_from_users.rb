ef06a4d6d73c8ef93f4488ae7dbbb947ca63897a
Student <student@Makerss-MacBook-Pro-4.local>
2019-06-24 14:22:12 +0100

    Added devise feature

--- db/migrate/20190620153342_remove_password_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemovePasswordFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :password, :string
-    remove_column :users, :confirm_password, :string
-  end
-end
--- db/migrate/20190620153550_add_password_function_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPasswordFunctionToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :password_digest, :string
-  end
-end
