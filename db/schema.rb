cc1c26e9535c0c7cc3e8150615285baa91d39c76
Lucy Barber <lbarber729@hotmail.co.uk>
2019-06-24 16:49:35 +0100

    Adds user Name and password confirmation
    Co-authored-by:  VladC24 <vlad.chiriac24@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190622190841) do
+ActiveRecord::Schema.define(version: 20190624154038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20190622190841) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "name"
   end
 
   add_foreign_key "likes", "posts"
--- spec/test_helpers.rb
+++ spec/test_helpers.rb
@@ -4,10 +4,12 @@ def go_to_homepage
   visit '/'
 end
 
-def sign_up(email: 'test@test.com', password: 'password')
+def sign_up(name: 'test', email: 'test@test.com', password: 'password', password_confirmation: 'password')
   go_to_homepage
+  fill_in('user[name]', with: name)
   fill_in('user[email]', with: email)
   fill_in('user[password]', with: password)
+  fill_in('user[password_confirmation]', with: password)
   click_button('Signup')
 end
 
