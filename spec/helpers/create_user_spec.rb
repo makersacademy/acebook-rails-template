301e60121a76ef41263dd7bf6d48a04ffd315763
Michael Hughes <mhughes36@live.co.uk>
2019-06-25 15:37:35 +0100

    merge conflicts / rspec passes all tests

--- spec/helpers/create_user_spec.rb
+++ spec/helpers/create_user_spec.rb
@@ -1,3 +1,3 @@
 def create_user
-  user = User.create(name: 'Test', email: 'test@test.com', password: '123')
+  user = User.create(name: 'Test', email: 'test@test.com', password: '123456', password_confirmation: '123456')
 end
