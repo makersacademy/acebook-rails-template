6b764456ef28f7dc001161085e0800fae8be637c
carlton anand <carlton.anand@gmail.com>
2019-06-25 17:34:51 +0100

    added individual user walls and linking them from name in the hoome feed. also wrote the feature tests for this Co-authored-by: Max Van Aken <mtvanaken@gmail.com> Co-authored-by: Ray Swilling <ray_swilling@yahoo.com>

--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -11,15 +11,35 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe HomeHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+ pending "add some examples to (or delete) #{__FILE__}"
 end
 
-def signup 
-  visit('/')
-  click_link('Sign up')
-  fill_in 'Name', with: 'Amoeba'
-  fill_in 'Email', with: 'amoeba@boyz.com'
-  fill_in 'Password', with: 'amoeba123'
-  fill_in 'Password confirmation', with: 'amoeba123'
-  click_button 'Sign up'
-end 
+def signup
+ visit('/')
+ click_link('Sign up')
+ fill_in 'Name', with: 'Amoeba'
+ fill_in 'Email', with: 'amoeba@boyz.com'
+ fill_in 'Password', with: 'amoeba123'
+ fill_in 'Password confirmation', with: 'amoeba123'
+ click_button 'Sign up'
+end
+
+def signup2
+ visit('/')
+ click_link('Sign up')
+ fill_in 'Name', with: 'Amoeba2'
+ fill_in 'Email', with: 'amoeba2@boyz.com'
+ fill_in 'Password', with: 'amoeba123'
+ fill_in 'Password confirmation', with: 'amoeba123'
+ click_button 'Sign up'
+end
+
+def signup3
+ visit('/')
+ click_link('Sign up')
+ fill_in 'Name', with: 'Chris'
+ fill_in 'Email', with: 'calves@boyz.com'
+ fill_in 'Password', with: 'amoeba123'
+ fill_in 'Password confirmation', with: 'amoeba123'
+ click_button 'Sign up'
+end
