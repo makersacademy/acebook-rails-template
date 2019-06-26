94fb604a3a591ea919eb2ae823e8019e2287609b
Laurence <ltaylorapp@gmail.com>
2019-06-26 09:55:33 +0100

    Any users profile now viewable, no matter who is logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(session[:user_id])
+    @user = User.find(params[:id])
     @posts = Post.where("user_id = #{@user.id}")
   end
 
