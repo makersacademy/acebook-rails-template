a4b4d0a3a22f7dc36494b7a3460a8be1f1affb0d
Student <student@Makerss-MacBook-Pro-4.local>
2019-06-26 14:17:01 +0100

    Working on posting on other users walls (incomplete)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190625131041) do
+ActiveRecord::Schema.define(version: 20190626112502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190625131041) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "recipient_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -34,8 +35,10 @@ ActiveRecord::Schema.define(version: 20190625131041) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.serial "wall_id", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "posts", "users"
