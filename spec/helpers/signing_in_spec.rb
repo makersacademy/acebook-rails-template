4807354a51d8b6febe1cd3e941254184302acd23
Laurence <ltaylorapp@gmail.com>
2019-06-26 15:09:52 +0100

    amended the links to users walls to work consistently across feed and walls
    Co-authored-by: n3ttl3t <n3ttl3t@gmail.com>
    Co-authored-by: LaurenceTaylor <ltaylorapp@gmail.com>

--- spec/helpers/signing_in_spec.rb
+++ spec/helpers/signing_in_spec.rb
@@ -4,3 +4,11 @@ def sign_in_user
   fill_in "password", with: "Password"
   click_button "Sign In"
 end
+
+def sign_in_another_user
+  visit '/sessions/new'
+  click_button "Sign In"
+  fill_in "email", with: "test@test.com"
+  fill_in "password", with: "Password"
+  click_button "Sign In"
+end
--- spec/helpers/signing_up_spec.rb
+++ spec/helpers/signing_up_spec.rb
@@ -6,3 +6,12 @@ def sign_up_user
   fill_in "user_password_confirmation", with: "Password"
   click_button "Sign Up"
 end
+
+def sign_up_another_user
+  visit '/users/new'
+  fill_in 'user_name', with: "Test"
+  fill_in "user_email", with: "test@test.com"
+  fill_in "user_password", with: "Password"
+  fill_in "user_password_confirmation", with: "Password"
+  click_button "Sign Up"
+end
