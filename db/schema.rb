ce918287ee316bc98175a0fedc85dc8d0c931825
VladC <vlad.chiriac24@gmail.com>
2019-06-27 14:37:37 +0100

    Refactor views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190626112209) do
+ActiveRecord::Schema.define(version: 20190625132146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
