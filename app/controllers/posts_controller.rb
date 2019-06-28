2f9d516d954cd3769f0b2722b14780c2653c72f5
Student <student@Makerss-MacBook-Pro-7.local>
2019-06-28 09:38:34 +0100

    Added recipient name to show individual post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,8 +31,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user = current_user
-    user_id = User.all.ids.each{ |x| x.to_i }
-    user_id != @post.recipient_id ? p(User.all.name) : p('hi ben and ray')
+
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
