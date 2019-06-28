bcdad3d7898f00f164045324d0e70d747d0593b1
Chris <chris.daf@gmail.com>
2019-06-28 09:44:05 +0100

    Added 404 and 500 custom error pages with styling

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Acebook
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    config.exceptions_app = self.routes
+    # This is related to the custom 404 and 500 error pages, and the errors controller
   end
 end
