da6bb9f1d3086d45bbb3381d7f6cdfa112da5147
Ben Scott <bengscott2@gmail.com>
2019-07-17 15:22:17 +0100

    feat: add date to posts and display in order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(updated_at: :desc)
   end
 
   private
---
+++ spec/features/user_views_posts_date_in_reverse_order_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+require "date"
+
+RSpec.feature "Posts with dates in order", type: :feature do
+  scenario "Can view posts with dates, in order" do
+    date = DateTime.now
+    date = date.strftime("%d-%m-%Y")
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Message 1"
+    click_button "Submit"
+    click_link "New post"
+    fill_in "Message", with: "Message 2"
+    click_button "Submit"
+    expect(page).to have_content("Message 2 updated on #{date}\nMessage 1 updated on #{date}")
+  end
+end
