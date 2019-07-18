b2f85543f765090ef6fc1e16dba037ca6df9104c
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-07-18 14:49:24 +0100

    fix: resolve linting issues

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     '/posts'
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,3 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { "add some examples to (or delete) #{__FILE__}" }
 end
-
