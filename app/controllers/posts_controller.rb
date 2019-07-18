417271a363fb2366baa6702b3d82cd654fd09f8f
joseph townshend <joetownshend@mac.com>
2019-07-18 11:30:14 +0100

    posts: add functionality to edit post and show post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
 
   def show
-    @post = post.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
     @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
   end
 
   def create
@@ -16,9 +20,17 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
-
-
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
 
   def index
     @posts = Post.all
