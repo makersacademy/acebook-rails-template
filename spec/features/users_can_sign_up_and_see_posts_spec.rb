6c1d73eadde0d7ad5b799762b8c6219ac961bf46
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-07-19 12:01:48 +0100

    feat: redirect user to index if not signed up

--- spec/features/users_can_sign_up_and_see_posts_spec.rb
+++ spec/features/users_can_sign_up_and_see_posts_spec.rb
@@ -11,4 +11,9 @@ RSpec.feature "User signs up", type: :feature do
     expect(page).to have_current_path('/posts')
     expect(page).to have_content('Welcome! You have signed up successfully.')
   end
+
+  scenario "User unable to view posts until they sign up" do
+    visit '/posts'
+    expect(page).to have_current_path('/')
+  end
 end
