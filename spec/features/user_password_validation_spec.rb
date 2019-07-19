c93fea4c084971a981b3493bd8765d4424efa6ac
Ruth Baker <ruthmoog@gmail.com>
2019-07-19 14:58:56 +0100

    test: Include test for more than 10 character password

--- spec/features/user_password_validation_spec.rb
+++ spec/features/user_password_validation_spec.rb
@@ -10,4 +10,14 @@ RSpec.feature "User can only enter password between 6 - 10 characters inclusive"
     click_button "Sign up"
     expect(page).to have_content("Password is too short (minimum is 6 characters)")
   end
+
+  scenario "User enters password with 11 characters" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: "tesymcemail@email.com"
+    fill_in "Password", with: "passwordwor"
+    fill_in "Password confirmation", with: "passwordwor"
+    click_button "Sign up"
+    expect(page).to have_content("Password is too long (maximum is 10 characters)")
+  end
 end
