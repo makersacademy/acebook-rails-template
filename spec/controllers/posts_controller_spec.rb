89fa6a34d7b164e943cc6994127e225a6b3a2826
Ben Scott <bengscott2@gmail.com>
2019-07-19 15:34:39 +0100

    fix: add mock to authenticate user for posts_controller_spec

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,36 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  # before(:each) do
-  #   visit "/"
-  #   click_link "Sign up"
-  #   fill_in "Email", with: "tesymcemail@email.com"
-  #   fill_in "Password", with: "password"
-  #   fill_in "Password confirmation", with: "password"
-  #   click_button "Sign up"
-  # end
+  before(:each) do
+    allow(controller).to receive(:authenticate_user!).and_return(true)
+  end
 
   describe "GET /new " do
-    xit "responds with 200" do
+    it "responds with 200" do
       get :new
       expect(response).to have_http_status(200)
     end
   end
 
   describe "POST /" do
-    xit "responds with 200" do
+    it "responds with 200" do
       post :create, params: { post: { message: "Hello, world!" } }
       expect(response).to redirect_to(posts_url)
     end
 
-    xit "creates a post" do
+    it "creates a post" do
       post :create, params: { post: { message: "Hello, world!" } }
       expect(Post.find_by(message: "Hello, world!")).to be
     end
   end
 
   describe "GET /" do
-    xit "responds with 200" do
+    it "responds with 200" do
       get :index
       expect(response).to have_http_status(200)
     end
