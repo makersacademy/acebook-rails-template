6edba6734156bd4ed09046d4072e3c25de24940d
Student <kaczanowskamonika@gmail.com>
2019-07-19 16:58:22 +0100

    see all posts after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,7 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
+
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   root 'users#new'
 
 
-  resources :users do
+  resources :users 
     resources :posts
-  end
+
   resources :sessions, only: [:new, :create]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
