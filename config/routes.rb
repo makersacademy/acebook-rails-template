2db713282c8fd583851141571681d83bf6f7e0dc
Student <kaczanowskamonika@gmail.com>
2019-07-19 10:00:20 +0100

    routing in progress

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create]
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
+  # get 'posts', to: 'users#posts', as: 'posts'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 end
