180eed730ebae54eba2cc301460aad83291894c5
Jake Pilikian <ja.pilikian@gmail.com>
2019-07-22 15:43:18 +0100

    add: feature test for minimum password

--- spec/features/users_signup_spec.rb
+++ spec/features/users_signup_spec.rb
@@ -1,4 +1,4 @@
-# users can sign up
+
 # sign up minimum password
 # passwords don't match
 # user successfully updated if new passwords match
@@ -14,5 +14,14 @@ RSpec.feature "Sign up", type: :feature do
     fill_in "user_password_confirmation", with: "password1"
     click_button "Create User"
     expect(page).to have_content("User was successfully created.")
+  end
+
+  scenario "can raise an error if minimum password" do
+    visit "/"
+    fill_in "user_email", with: "email@email.com"
+    fill_in "user_password", with: "passw"
+    fill_in "user_password_confirmation", with: "passw"
+    click_button "Create User"
+    expect(page).to have_content("Password is too short (minimum is 6 characters)")
   end
 end
