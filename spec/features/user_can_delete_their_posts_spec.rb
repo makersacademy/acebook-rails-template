f3faaa5401c72246442a0a8667edc8cc4881099d
Ruth Baker <ruthmoog@gmail.com>
2019-07-22 12:14:25 +0100

    refactor: tests for absense of delete button and uses helpers

--- spec/features/user_can_delete_their_posts_spec.rb
+++ spec/features/user_can_delete_their_posts_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
+require 'seed_post_helper'
 
 RSpec.feature "Users can delete their own posts", type: :feature do
   scenario "delete option appears on own post by signed in user" do
-    visit "/"
-    click_link "Sign up"
-    fill_in "Email", with: "testemail@fakemail.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_button "Sign up"
+    seed_post
+    sign_up
     visit "/posts"
+    expect(page).not_to have_content("Delete")
     click_link "New post"
     fill_in "Message", with: "This is a message that can be removed"
     click_button "Submit"
