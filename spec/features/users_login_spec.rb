f3308c6ef6d3f2476eaf33cb5ecac32c8b1683b2
joseph townshend <joetownshend@mac.com>
2019-07-22 16:18:11 +0100

    add: feature test for successful login - failing

--- spec/features/users_login_spec.rb
+++ spec/features/users_login_spec.rb
@@ -13,12 +13,27 @@ RSpec.feature "Log in", type: :feature do
 
     visit "/login"
     fill_in "email", with: "email@email.com"
+    fill_in "password", with: "password1"
+    click_button "login"
+    expect(page).to redirect_to("/posts")
+  end
+
+  scenario "can raise an error if login details are incorrect" do
+    visit "/"
+    fill_in "user_email", with: "email@email.com"
+    fill_in "user_password", with: "password1"
+    fill_in "user_password_confirmation", with: "password1"
+    click_button "Create User"
+    expect(page).to have_content("User was successfully created.")
+
+    visit "/login"
+    fill_in "email", with: "email@email.com"
     fill_in "password", with: "password420"
     click_button "login"
     expect(page).to have_content("Email or password is invalid")
   end
 
-  # scenario "can let a user edit details" do
+  # scenario "can let a existing user edit details" do
   #   visit "/"
   #   fill_in "user_email", with: "email@email.com"
   #   fill_in "user_password", with: "password1"
