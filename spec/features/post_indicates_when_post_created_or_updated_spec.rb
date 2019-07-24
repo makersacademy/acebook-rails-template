a11c6c16395754251493b560e251069a379ddaad
Chris Groves <chris_groves87@hotmail.co.uk>
2019-07-24 12:11:29 +0100

    test: written and passed test for 'updated on' label on re-edited posts

--- spec/features/post_indicates_when_post_created_or_updated_spec.rb
+++ spec/features/post_indicates_when_post_created_or_updated_spec.rb
@@ -16,4 +16,23 @@ RSpec.feature "Display if post has been updated", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!!! updated on #{date}")
   end
+
+  scenario "Posts display date of update if they have been updated a second time" do
+    date = DateTime.now
+    date = date.strftime("%d-%m-%Y")
+    sign_up
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world! created on #{date}")
+    click_button "Edit"
+    fill_in "Message", with: "Hello, world!!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world!! updated on #{date}")
+    click_button "Edit"
+    fill_in "Message", with: "Hello, world!!!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world!!! updated on #{date}")
+  end
 end
