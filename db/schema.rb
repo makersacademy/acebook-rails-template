44409a8b3799b6ae29397a415f2452dca20fd8ea
Ben Scott <bengscott2@gmail.com>
2019-07-24 12:30:12 +0100

    feat: add ability to comment on post

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 20190723163810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "post_id"
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
   create_table "commontator_comments", id: :serial, force: :cascade do |t|
     t.string "creator_type"
     t.integer "creator_id"
@@ -84,7 +74,5 @@ ActiveRecord::Schema.define(version: 20190723163810) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
