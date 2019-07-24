088ac8699437363e18e0ca90da5caca3db300a24
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-07-24 14:18:03 +0100

    feature test for comment thread

--- spec/features/users_can_comment_on_posts_spec.rb
+++ spec/features/users_can_comment_on_posts_spec.rb
@@ -7,23 +7,6 @@ RSpec.feature "People can comment on posts", type: :feature do
     find_link("Comment").click
     expect(page).to have_content "Hello, giraffe!"
     expect(page).to have_css '.comment-thread'
+    expect(page).to have_link("Show Comments (0)", visible: false)
   end
-
-  scenario "go to a comment thread and post your comment" do
-    seed_post
-    sign_up
-    find_link("Comment").click
-    find(:xpath, "//a[@href='/commontator/threads/1']").click
-    find_link("Show Comments (0)").click
-    find_link("New Comment").click
-    fill_in "comment[body]", with: "actually it's zebra"
-    find_button("Post Comment").click
-    expect(page).to have_content "actually it's zebra"
-    expect(page).to have_content "Posted on"
-    expect(page).to have_link "New Comment"
-  end
-
-  xscenario "see existing non-empty comment thread"
-
-  xscenario "both original post and new comment have date created"
 end
