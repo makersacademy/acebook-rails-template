8bd9c6071eafa6242fc98cafdda5bb26d8ed4cd9
Chris Groves <chris_groves87@hotmail.co.uk>
2019-07-24 16:06:26 +0100

    test: posts appear in reverse date order

--- spec/features/user_views_posts_date_in_reverse_order_spec.rb
+++ spec/features/user_views_posts_date_in_reverse_order_spec.rb
@@ -1,18 +1,22 @@
 require "rails_helper"
 require "date"
+require "sign_up_helper"
+
+RSpec::Matchers.define :appear_before do |later_content|
+  match do |earlier_content|
+    page.body.index(earlier_content) < page.body.index(later_content)
+  end
+end
 
 RSpec.feature "Posts with dates in reverse order", type: :feature do
-  xscenario "Can view posts with dates, in order" do
+  scenario "Can view posts with dates, in order" do
     sign_up
-    date = DateTime.now
-    date = date.strftime("%d-%m-%Y")
-    visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Message 1"
     click_button "Submit"
     click_link "New post"
     fill_in "Message", with: "Message 2"
     click_button "Submit"
-    expect(page).to have_content("Message 2 created on #{date}\nMessage 1 created on #{date}")
+    expect("Message 2").to appear_before("Message 1")
   end
 end
