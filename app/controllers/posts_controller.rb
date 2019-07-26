41531f5f9604494306ee4651f4375831805cd8b2
petraartep <petra.pogacsas@gmail.com>
2019-07-26 05:38:51 +0100

    fix: current user method removed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,4 @@
 class PostsController < ApplicationController
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    else
-      @current_user = nil
-    end
-  end
 
   def index    
     @posts = Post.all.order('created_at DESC')
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,5 +20,4 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to root_url, notice: "Logged out!"
   end
-
 end
