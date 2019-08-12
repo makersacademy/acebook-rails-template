a02a9c9a256526ca667e423a28b0f66d559776e9
Rianne McCartney <riannemccartney@hotmail.com>
2019-08-12 17:13:47 +0100

    Sign up funcitonality working, started log_in function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/index'
+
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions
+  root "sessions#new"
+  root "sessions#create"
+  root "sessions#destroy"
+
+  resources :users
+  root "users#new"
+  root "users#index"
 
   resources :posts
   root "posts#index"
