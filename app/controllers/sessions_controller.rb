7fddc5fdce18125621a09535011fb9b4615bb903
Rianne McCartney <riannemccartney@hotmail.com>
2019-08-12 17:29:07 +0100

    Trying to implement log in function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    render 'new'
+    redirect_to posts_index_path
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
+  # post 'signin', to: "sessions#create", as "signin"
 
   get 'users/index'
 
