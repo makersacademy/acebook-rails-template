6e056feb8c342e51000ebfefcdc44d3291807ffb
Ben D <benjamin.d928@icloud.com>
2019-08-13 15:43:11 +0100

    Adding session id - incomplete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+
+require_relative '../models/sessions.rb'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  p session[:current_session]
+  #   before_action :require_login
+  #
+  #   private
+  #
+  #   current_session = session[:current_session]
+  #   def require_login
+  #     redirect_to login_url unless current_session
+  # end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-    def create
-        @user = params
-        p @user['Password']
-    end
+  # skip_before_action :require_login
+  def create
+    session[:current_session] = params[:authenticity_token]
+  end
 
-    def index
-
-    end
+  def index; end
 end
