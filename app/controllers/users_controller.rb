16713deb64c33b3ec5438cb60b1a84d106f0397e
Kareem <kareem335@yahoo.co.uk>
2019-08-13 16:16:46 +0100

    user sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    if @user.password.length < 6 || @user.password.length > 10
+    if @user.password.length <= 6 || @user.password.length >= 10
       flash[:danger] = "Password needs to be between 6 - 10 characters"
       render 'new'
     else
-      redirect_to posts_url
-
+      session[:user_id] = @user.id
+      redirect_to posts_url, notice: "You have successfully signed up, #{@user.username}!"
     end
   end
 end
