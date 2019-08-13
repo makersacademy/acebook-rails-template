e7cefe58763f97f905099eb976e2d73ab6370c68
Will Howes <williameldenhowes@gmail.com>
2019-08-13 12:38:47 +0100

    First test for sign up almost complete. Just need to sort the redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
   end
 
   def create
--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.feature "Sign Up", type: :feature do
   scenario "User can sign up" do
     visit "/users/new"
-    fill_in "First Name", with: "Test"
-    fill_in "Last Name", with: "Example"
+    fill_in "Firstname", with: "Test"
+    fill_in "Lastname", with: "Example"
     fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password123"
     click_button "Submit"
