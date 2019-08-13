bba900b40e48d50dd7f6ba24c45de7c6ec4ca8dd
Josh Hemsley <josh@breathehr.com>
2019-08-13 11:03:48 +0100

    working flash message and redirect after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,20 @@ class UsersController < ApplicationController
    @user = User.new
  end
 
+   def create
+       @user = User.new(user_params)
+       if @user.save
+         flash[:success] = "Welcome to MugManual!"
+         redirect_to "/" #needs changing to homepage
+       else
+         render 'new'
+       end
+   end
+
+private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password,
+                                 :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   root 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  # get  '/help',    to: 'static_pages#help'
+  # get  '/about',   to: 'static_pages#about'
+  # get  '/contact', to: 'static_pages#contact'
+  # get  '/signup',  to: 'users#new'
+  # post '/signup',  to: 'users#create'
   resources :posts
   resources :users
 end
