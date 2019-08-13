09a12eadbd5b36fe436ca0337d11c9f99374a59d
gennyallcroft <gennyallcroft@hotmail.co.uk>
2019-08-13 16:45:09 +0100

    can only edit your own posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,15 +21,20 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to root_path
+    @user = User.find(session[:user_id])
+    if @post.user_id != @user.id
+      flash[:error] = "You can only edit your own posts"
+      redirect_to posts_path
+    else
+      @post.update(post_params)
+      redirect_to posts_path
+    end
   end
 
   def destroy
     @post = Post.find(params[:id])
     @user = User.find(session[:user_id])
     if @post.user_id != @user.id
-      p "testing"
       flash[:error] = "You can only delete your own posts"
       redirect_to posts_path
     else
