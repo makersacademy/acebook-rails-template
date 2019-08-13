a410c1e441389cc390dfad5cfec1b13f80610595
Richie <richieganney@gmail.com>
2019-08-13 16:40:58 +0100

    user can login and logout and header changes. moving onto destroying sesssion

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user 
+      redirect_to posts_path
+      flash[:success] = "Welcome back, #{user.username}"
       # Log the user in and redirect to the user's show page.
     else
       #create an error message
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-
+  delete '/logout' => 'sessions#destroy'
   root 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get  '/help',    to: 'static_pages#help'
+  get  '/help',    to: 'static_pages#help'
   # get  '/about',   to: 'static_pages#about'
   # get  '/contact', to: 'static_pages#contact'
   # get  '/signup',  to: 'users#new'
