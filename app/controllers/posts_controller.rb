159f6964770d720de7db9975c7ed60623278b98d
Richie <richieganney@gmail.com>
2019-08-14 16:53:50 +0100

    user_id is stored as a foreign key when user adds a new post. moving onto updating and deleting posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.create(post_params)
+    @user = User.find(session[:user_id])
+    @post = @user.posts.create(post_params)
     redirect_to posts_url
   end
 
@@ -18,3 +20,21 @@ class PostsController < ApplicationController
     params.require(:post).permit(:message)
   end
 end
+
+# def create
+#     @user = User.find(session[:user_id])
+#     @posts = @user.posts.create(post_params)
+#     p "LOOK HERE"
+#     p @user
+#     redirect_to user_path(@user)
+#   end
+
+#   def index
+#     @posts = Post.all
+#   end
+
+#   private
+
+#   def post_params
+#     params.require(:post).permit(:message)
+#   end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
   # get  '/signup',  to: 'users#new'
   # post '/signup',  to: 'users#create'
   resources :posts
-  resources :users
+  resources :users 
   resources :sessions
 end
