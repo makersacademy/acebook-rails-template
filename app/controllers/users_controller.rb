7bbf92abf29aa01360021b15c4924139eb028092
Will Howes <williameldenhowes@gmail.com>
2019-08-14 11:05:57 +0100

    Show page (i.e. profile page) basics created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    # The below line starts a rails console which you can use for debugging
+    # debugger
   end
 
   def new
