0e3baa126fd166703cd28de8531aa391319be362
Student <ziwei.polaris@gmail.com>
2019-08-14 16:23:52 +0100

    is it fixed

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,6 @@ ActiveRecord::Schema.define(version: 20190814104643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
-
-  create_table "my_test_tab", id: false, force: :cascade do |t|
-    t.integer "a"
-  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
