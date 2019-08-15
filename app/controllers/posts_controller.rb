d2420746fafb29c23a072f20be465658c6971d89
Josh Hemsley <josh@breathehr.com>
2019-08-15 10:02:05 +0100

    correcting changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,21 +20,3 @@ class PostsController < ApplicationController
     params.require(:post).permit(:message)
   end
 end
-
-# def create
-#     @user = User.find(session[:user_id])
-#     @posts = @user.posts.create(post_params)
-#     p "LOOK HERE"
-#     p @user
-#     redirect_to user_path(@user)
-#   end
-
-#   def index
-#     @posts = Post.all
-#   end
-
-#   private
-
-#   def post_params
-#     params.require(:post).permit(:message)
-#   end
