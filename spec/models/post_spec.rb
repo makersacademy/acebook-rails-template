b159812872b0bd83e261a47144ff0aee059968bc
Richie <richieganney@gmail.com>
2019-08-15 09:45:42 +0100

    confirming working tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = User.create({username: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar"})
+    @post = Post.new(message: "Lorem ipsum", user_id: @user.id)
+    @user
+  end
+
+  it "user id should be present" do
+    @post.user_id = nil
+    assert !@post.valid?
+  end
+
+  it "content should be present" do
+    @post.message = "new post"
+    assert @post.valid?
+  end
+
 end
