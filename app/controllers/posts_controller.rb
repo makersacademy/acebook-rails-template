5bd8e98b043d309489b96082d4ba95ed129ba02a
TBrockster <tobybrock93@gmail.com>
2019-08-15 14:33:24 +0100

    completed deletion

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :correct_user,   only: :destroy
+
   def new
     @post = Post.new
   end
@@ -15,7 +17,19 @@ class PostsController < ApplicationController
     @posts = Post.order(created_at: :desc)
   end
 
+  def destroy
+    @post.destroy
+    flash[:success] = "Post deleted"
+    redirect_to request.referrer || root_url
+  end
+
   private
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    flash[:success] = "You can't delete another users post!"
+    redirect_to '/posts' if @post.nil?
+  end
 
   def post_params
     params.require(:post).permit(:message)
