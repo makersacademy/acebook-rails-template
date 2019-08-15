a0884b5640fa8af6584da3ea74bb93c1cfd58ab9
Kaja <catcalledkaja@gmail.com>
2019-08-15 11:04:05 +0100

    merging user story

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+
 RSpec.describe "users/edit", type: :view do
   before(:each) do
     @user = assign(:user, User.create!(
@@ -24,4 +25,5 @@ RSpec.describe "users/edit", type: :view do
       assert_select "input[name=?]", "user[password]"
     end
   end
+
 end
