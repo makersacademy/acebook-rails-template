1a67956587407c19f57a54dee5b7786e39dcdfdd
marvinmatovu@gmail.com <student@Makerss-MacBook-Pro-4.local>
2019-08-15 17:12:18 +0100

    added posts to our branch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,43 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
+  before_action :current_post, only: %i[show edit update destroy]
   def new
     @post = Post.new
   end
 
   def create
-    @post = Post.create(post_params)
+    @user = User.find(session[:user_id])
+    @post = @user.posts.create(post_params)
     redirect_to posts_url
   end
 
   def index
     @posts = Post.all
-  end
+   end
 
   def show
+    @post = Post.find_by(id: params[:id])
   end
 
   def update
+    @post.update(post_params)
+    redirect_to posts_url
   end
 
   def destroy
-  end
+    @user = User.find(session[:user_id])
+    @post.destroy if @user.id == @post.user_id
+    redirect_to posts_url
+ end
 
   private
 
   def post_params
     params.require(:post).permit(:message)
+  end
+
+  def current_post
+    @post = Post.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-   validates :name, :username, :email, :password, presence: true
-   validates :password, length: { in: 8..20 }
-   validates :email, uniqueness: true
-   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-   validates :email, presence: true, length: { maximum: 255 },
+  has_many :posts, dependent: :destroy
+  validates :name, :username, :email, :password, presence: true
+  validates :password, length: { in: 8..20 }
+  validates :email, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-   has_secure_password
+  has_secure_password
 end
