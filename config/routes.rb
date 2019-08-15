1192e6ae35b2c243a11452148178d220d07a2db6
marvinmatovu@gmail.com <student@Makerss-MacBook-Pro-4.local>
2019-08-15 12:30:19 +0100

    login branch fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
-  get  '/signup',  to: 'users#new'
+  get  '/signup', to: 'users#new'
   resources :posts
+
+  get '/login', to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
