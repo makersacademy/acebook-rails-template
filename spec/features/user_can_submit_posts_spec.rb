342688c32e220e58bada6b55d441687a1d22f71c
Will Howes <williameldenhowes@gmail.com>
2019-08-16 14:22:29 +0100

    Tests passing. Test coverage at 90.1%

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
+require_relative './sign_up_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
+    registration
     create_post
     expect(page).to have_content("Hi")
   end
 
   scenario "Can submit multi-line posts and view them" do
+    registration
     create_post_multi_line
     expect(page.html).to include("Line one\n<br />Line Two")
   end
--- spec/features/user_can_update_posts_spec.rb
+++ spec/features/user_can_update_posts_spec.rb
@@ -1,19 +1,33 @@
 require 'rails_helper'
 require 'timecop'
 require_relative './method_helpers'
+require_relative './sign_up_helper'
 
 RSpec.feature "Timeline", type: :feature do
+
   scenario "can update posts" do
+    registration
     create_post
     update_post
     expect(page).to have_content("Bye")
   end
-  xscenario "cannot update a post after ten minutes" do
+
+  scenario "cannot update a post after ten minutes" do
+    registration
     create_post
     t = Time.now
-    Timecop.freeze(t + 10) do
-      update_post
-      expect(page).to have_content("Error")
+    Timecop.freeze(t + 9) do
+      expect(page).to have_selector(:link_or_button, "Edit")
+    end
+  end
+
+  scenario "cannot update a post after ten minutes" do
+    registration
+    create_post
+    t = Time.now
+    Timecop.freeze(t + 601) do
+      visit current_path
+      expect(page).not_to have_selector(:link_or_button, "Edit")
     end
   end
 end
