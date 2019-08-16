b72de4e5548e528bbadacbdb7a4140275b8149cf
Kaja <catcalledkaja@gmail.com>
2019-08-16 11:05:23 +0100

    adding bootstrp and connecting user story

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
-  get  '/signup',  to: 'users#new'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   resources :posts
 end
