db732958df56267f55637cf255b4d7d41187333a
Rianne McCartney <riannemccartney@hotmail.com>
2019-08-16 17:00:46 +0100

    css changes to sign in and sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,21 @@
 Rails.application.routes.draw do
+  get 'sessions/index'
+
   resources :sessions, only: [:new, :create, :destroy]
 
-  get 'login', to: "sessions#new"
-  post 'login', to: "sessions#create"
+  # # get '/login', to: "sessions#new"
+  # post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
+
+  root 'sessions#new'
+
+  get '/update', to: "posts#update"
+  post '/update', to: "posts#update"
+  put '/update', to: "posts#update"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions
-  root "sessions#new"
-  root "sessions#create"
-  root "sessions#destroy"
-
   resources :users
-  root "users#new"
-  root "users#index"
-
   resources :posts
-  root "posts#new"
-  root "posts#create"
-  root "posts#index"
-  root "posts#edit"
-  root "posts#update"
-  root "posts#destroy"
 end
