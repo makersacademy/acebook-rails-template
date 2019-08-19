672141d048a60686e67df5a6210edfcaa07c02f3
Richie <richieganney@gmail.com>
2019-08-19 11:42:50 +0100

    refactor

--- spec/features/create_wall_posts.rb
+++ spec/features/create_wall_posts.rb
@@ -10,9 +10,20 @@ RSpec.feature 'Timeline', type: :feature do
     fill_in 'Password', with: 'testing123'
     fill_in 'Confirmation', with: 'testing123'
     click_button 'Create my account'
-    click_link 'New post'
+
+    visit '/'
+    fill_in 'Username', with: 'Example2'
+    fill_in 'Email', with: 'test2@example.com'
+    fill_in 'Password', with: 'testing123'
+    fill_in 'Confirmation', with: 'testing123'
+    click_button 'Create my account'
+    click_button 'Users'
+    click_link 'View Wall'
     fill_in 'Message', with: 'Hello, world!'
     click_button 'Submit'
     expect(page).to have_content('Hello, world!')
+
+    visit '/'
+    expect(page).not_to have_content('Hello, world!')
   end
 end
