d33b4b2300bad1f2f0b708d6c20fc25949c5ddb5
Kareem <kareem335@yahoo.co.uk>
2019-08-19 14:57:05 +0100

    all tests pass

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
+    User.create!(username: "yo man",email: "123@123.123", password: "1234567")
+    visit "/"
+
+    fill_in "email", with: "123@123.123"
+    fill_in "password", with: "1234567"
+    p click_button("Log in")
+    
+   
+    fill_in "post_area", with: "Hello, world!"
+    click_button("Post")
     expect(page).to have_content("Hello, world!")
   end
 end
