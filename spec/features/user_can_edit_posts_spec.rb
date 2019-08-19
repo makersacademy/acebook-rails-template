5a749a9715041a483d9f27bf7cbcfd6b40f29fbb
Kareem <kareem335@yahoo.co.uk>
2019-08-19 12:35:46 +0100

    test edit still not working

--- spec/features/user_can_edit_posts_spec.rb
+++ spec/features/user_can_edit_posts_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can edit posts and view updated post" do
-    User.create!(username: "yo man",email: "123@123.123", password: "1234567")
+    user = User.create!(username: "yo man",email: "123@123.123", password: "1234567")
+    post = Post.create!(message: "Hello, world!",user_id: "#{user.id}")
 
     visit "/"
 
@@ -10,6 +11,11 @@ RSpec.feature "Timeline", type: :feature do
     fill_in "password", with: "1234567"
     p click_button("Log in")
 
-    fill_in "post_area", with: "Hello, world!"
     first('.nav-link').first(:link, 'edit')
+
+    p fill_in "edit-post-inline-#{post.id}", with: "Edited post"
+    click_button('Submit')
+
+    expect(page).to have_content("Hello, world! edited post")
+  end
 end
