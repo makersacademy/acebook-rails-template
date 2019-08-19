0051b01e7b642c148e9408a0c5bd3a76ddf734ea
djvcom <djverrall@gmail.com>
2019-08-19 11:56:10 +0100

    added functionality to stop users editing posts older than 10mins, as well as funcitonality to delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,24 @@ class PostsController < ApplicationController
     @posts = Post.all.reverse
   end
 
+  def edit
+    @post = Post.find_by_id(params[:id])
+  end
+
+  def destroy
+    @post = Post.find_by_id(params[:id])
+    @post.destroy
+
+    redirect_to posts_url
+  end
+  
+  def update
+    @post = Post.find_by_id(params[:id])
+    permitted_columns = params.require(:post).permit(:message)
+    @post.update_attributes(permitted_columns)
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
