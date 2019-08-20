859ddaae1d9d3ec0216f25e00175bd5543f15c1e
Kaja <catcalledkaja@gmail.com>
2019-08-20 15:52:46 +0100

    Adding_scss_and_posts_on_the_wall

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -1,16 +1,21 @@
 class WallsController < ApplicationController
-  def index
+  def new
     @post = Post.new
   end
+
 
   def show
     @post = Post.find_by(id: session[:user_id])
   end
 
-  def create
-  end
 
-  def new
+    def create
+
+    end
+
+
+  def index
+    @posts = Post.all
   end
 
   def edit
@@ -20,5 +25,14 @@ class WallsController < ApplicationController
   end
 
   def destroy
+  end
+  private
+
+  def post_params
+    params.require(:post).permit(:message)
+  end
+
+  def current_post
+    @post = Post.find(params[:id])
   end
 end
