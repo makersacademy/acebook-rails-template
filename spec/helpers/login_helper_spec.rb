1db3d657f940130fe9298091c1f310ede6997ee2
Student <ziwei.polaris@gmail.com>
2019-08-20 16:41:06 +0100

    fixed tests

--- spec/helpers/login_helper_spec.rb
+++ spec/helpers/login_helper_spec.rb
@@ -1,21 +1,21 @@
 
 
 def sign_up_second_user
-  visit "users/sign_up"
+  visit "/users/sign_up"
   fill_in "user[email]", with: "test@test.com"
   fill_in "user[password]", with: "testing"
   fill_in "user[password_confirmation]", with: "testing"
   click_button "Sign up"
 end
 
-def sign_up
-  visit ("/")
-  click_link 'Sign up'
-  fill_in "user[email]", with: "ben@test.com"
-  fill_in "user[password]", with: "testing"
-  fill_in "user[password_confirmation]", with: "testing"
-  click_button "Sign up"
-end
+# def sign_up
+#   visit ("/")
+#   click_link 'Sign up'
+#   fill_in "user[email]", with: "ben@test.com"
+#   fill_in "user[password]", with: "testing"
+#   fill_in "user[password_confirmation]", with: "testing"
+#   click_button "Sign up"
+# end
 
 def log_in
   visit('/')
