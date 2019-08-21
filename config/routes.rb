48af2b32e2bfa05f08616099cabe0a64c86979c3
gennyallcroft <gennyallcroft@hotmail.co.uk>
2019-08-21 14:59:36 +0100

    adding the /id route to bottom of routes file so it all works

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,12 @@ Rails.application.routes.draw do
   # post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
 
-  # get '/:id', to: "users#show"
-
   root 'sessions#new'
 
   get '/update', to: "posts#update"
   post '/update', to: "posts#update"
   put '/update', to: "posts#update"
 
-  # get '/posts' => 'posts#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -32,5 +29,7 @@ Rails.application.routes.draw do
   end
 
   # resources :id, path: '/users/id'
+
+    get '/:id', to: "users#show"
 
 end
