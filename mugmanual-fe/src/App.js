7708b8bf303c08d4568ae508deb226bb251abae4
TBrockster <tobybrock93@gmail.com>
2019-08-21 14:39:16 +0100

    front end framework set up

--- mugmanual-fe/src/App.js
+++ mugmanual-fe/src/App.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import Posts from './components/Posts'
+import React, { Component } from 'react';
+import Posts from './components/Posts';
+import AddPost from './components/AddPost';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div>
-      hello world
-      <Posts />
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    axios.get('https://mugmanual.herokuapp.com/post_api')
+    .then(res => this.setState({ posts: res.data}))
+    console.log(this.state.posts)
+  }
+
+  // Delete Post
+  delTodo = (id) => {
+    this.setState({ posts: [...this.state.posts.filter(post => post.id !== id)]});
+  }
+
+  // Add Post
+  addPost = (message) => {
+    const newTodo ={
+      id: 4,
+      message,
+    }
+    this.setState({ posts: [...this.state.posts, newTodo] });
+  }
+
+  render() {
+    // console.log(this.state.posts) 
+    return (
+      <div>
+        <AddPost addPost={this.addPost} />
+        <Posts 
+        posts={this.state.posts}
+        delPost={this.delTodo}/>
+      </div>
+    );
+  };
 }
 
 export default App;
--- mugmanual-fe/src/components/Posts.js
+++ mugmanual-fe/src/components/Posts.js
@@ -1,66 +1,21 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import PostItem from './PostItem';
+import PropTypes from 'prop-types';
 
-export class Posts extends Component {
-  state = {
-    posts: []
+class Posts extends Component {
+  render() {
+    return this.props.posts.map((post) => (
+      <PostItem 
+      key={post.id} 
+      post={post} 
+      delPost={this.props.delPost}/>
+    ));
   }
-
-  componentDidMount() {
-    axios.get('https://mugmanual.herokuapp.com/post_api')
-    .then(res => this.setState({ posts: res.data }))
- }
-
- render() {
-  return (
-    <div>
-      return this.state.posts.map((post) => {
-        <PostItem key={post.id} post={post} />
-      })
-    </div>
-  )
 }
-  }
 
-// componentDidMount() {
-//   fetch("https://mugmanual.herokuapp.com/post_api")
-//     .then(res => res.json())
-//     .then(
-//       (result) => {
-//         this.setState({
-//           isLoaded: true,
-//           items: result.items
-//         });
-//       },
-//       // Note: it's important to handle errors here
-//       // instead of a catch() block so that we don't swallow
-//       // exceptions from actual bugs in components.
-//       (error) => {
-//         this.setState({
-//           isLoaded: true,
-//           error
-//         });
-//       }
-//     )
-//   }
+// PropTypes
+Posts.propTypes = {
+  posts: PropTypes.array.isRequired
+}
 
-// render() {
-//   const { error, isLoaded, items } = this.state;
-//   if (error) {
-//     return <div>Error: {error.message}</div>;
-//   } else if (!isLoaded) {
-//     return <div>Loading...</div>;
-//   } else {
-//     return (
-//       <ul>
-//         {items.map(item => (
-//           <li key={item.message}>
-//           </li>
-//         ))}
-//       </ul>
-//     );
-//   }
-// }
-// }
-
-export default Posts
+export default Posts;
