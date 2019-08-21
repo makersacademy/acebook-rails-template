35e3586d470cd111a361b1c4339c584b192b902a
Kareem <kareem335@yahoo.co.uk>
2019-08-21 14:53:13 +0100

    changed config in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "acebook_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.exceptions_app = self.routes 
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
