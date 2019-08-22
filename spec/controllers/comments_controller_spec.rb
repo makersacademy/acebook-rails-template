0077d76e83cef348affd2a1129c4684cf32a7f7f
gennyallcroft <gennyallcroft@hotmail.co.uk>
2019-08-22 15:41:14 +0100

    test for comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  describe "GET /new " do
+    it "responds with 200" do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
 
+  describe "POST /" do
+    let(:user) {User.create!(username: "yo man", password: "1234567")}
+
+    it "responds with 200" do
+      session[:user_id] = user.id
+      post :create, params: { post: { message: "Hello, world!" } }
+      expect(response).to redirect_to(posts_url)
+    end
+
+    it "creates a post" do
+      session[:user_id] = user.id
+      post :create, params: { post: { message: "Hello, world!" } }
+      expect(Post.find_by(message: "Hello, world!")).to be
+    end
+  end
 end
