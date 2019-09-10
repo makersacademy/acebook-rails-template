54c9e1aca2a530552bb6f8a33cb938b8c0eaf74d
kierepkae <kierepkaedyta@gmail.com>
2019-09-10 10:51:03 +0100

    user can sign up, log in and log out > > Co-authored-by: jaitone <jaitoone@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20190910093748) do
   create_table "users", force: :cascade do |t|
 
     t.datetime "created_at", null: false
+
     t.datetime "updated_at", null: false
     t.string "email", null: false
     t.string "encrypted_password", limit: 128, null: false
