ce6e8e681d1b5d5cf4491976eef6464f79423033
Laurenlau <harris_lau@hotmail.co.uk>
2019-09-10 15:34:46 +0100

    Timestamp for new posts with posts in chronological order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,13 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+
   end
 
   private
 
   def post_params
+
     params.require(:post).permit(:message)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # get 'welcome/index'
+  get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   get 'posts/index'
 
-  # root 'welcome#index'
+  root 'welcome#index'
 end
