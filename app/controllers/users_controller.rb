89f58939288aa381cddc5ef02803990533c0ff65
dtrts <djt210@gmail.com>
2019-09-10 16:30:46 +0100

    Finishing touches to users can sign up. Helpful message when account created.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < Clearance::UsersController
+  def create
+    # completely overwriting it
+    @user = user_from_params
+
+    if @user.save
+      sign_in @user
+      flash[:message] = 'A helpful message to say you\'ve been signed in!'
+      redirect_back_or url_after_create
+    else
+      render template: 'users/new'
+    end
+  end
+
   # protected
   def url_after_create
     '/posts'
--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -1,41 +1,47 @@
 require 'rails_helper'
 
-RSpec.feature "Log in/out", type: :feature do
-  scenario "Can Sign Up" do
-    visit "/sign_up"
-    fill_in "user_email", with: "test@test.com"
-    fill_in "user_password", with: "password"
-    click_button "Sign up"
-    expect(page).to have_content("Hello")
+RSpec.feature 'Log in/out', type: :feature do
+  scenario 'Can Sign Up' do
+    visit '/sign_up'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: 'password'
+    click_button 'Sign up'
+    expect(page).to have_content('A helpful message to say you\'ve been signed in!')
   end
 
-  scenario "Can Log In When Already Signed Up" do 
-    visit "/sign_up"
-    fill_in "user_email", with: "test@test.com"
-    fill_in "user_password", with: "password"
-    click_button "Sign up"
-    click_button "Sign out"
-    visit "/sign_in"
-    fill_in "session_email", with: "test@test.com"
-    fill_in "session_password", with: "password"
-    click_button "Sign in"
-    expect(page).to have_content("Hello")
+  scenario 'can not sign up with invalid email', type: :feature do
+    visit '/sign_up'
+    fill_in 'user_email', with: 'test.com'
+    click_button 'Sign up'
+    expect(page).not_to have_content('A helpful message to say you\'ve been signed in! Sign up Email Password Sign in')
   end
 
-  scenario "Can request password reset email" do
-    visit "/passwords/new" 
-    fill_in "password_email", with: "test@test.com"
-    click_button "Reset password"
-    expect(page).to have_content("changing your password.")
+  scenario 'Can Log In When Already Signed Up' do
+    visit '/sign_up'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: 'password'
+    click_button 'Sign up'
+    click_button 'Sign out'
+    visit '/sign_in'
+    fill_in 'session_email', with: 'test@test.com'
+    fill_in 'session_password', with: 'password'
+    click_button 'Sign in'
+    expect(page).to have_content('Hello')
   end
 
-  scenario "Can Log Out when Logged In" do 
-    visit "/sign_up"
-    fill_in "user_email", with: "test@test.com"
-    fill_in "user_password", with: "password"
-    click_button "Sign up"
-    click_button "Sign out"
-    expect(page).to have_content("Sign in")
+  scenario 'Can request password reset email' do
+    visit '/passwords/new'
+    fill_in 'password_email', with: 'test@test.com'
+    click_button 'Reset password'
+    expect(page).to have_content('changing your password.')
+  end
+
+  scenario 'Can Log Out when Logged In' do
+    visit '/sign_up'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: 'password'
+    click_button 'Sign up'
+    click_button 'Sign out'
+    expect(page).to have_content('Sign in')
   end
 end
-
