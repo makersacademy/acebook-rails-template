6202bfaf6de6fbb3ad6024fee63940aabee8dd33
Jamal Springer <jamal.springer@outlook.comm>
2019-09-10 12:49:24 +0100

    Created initial post route and added header to page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
 
-  root 'welcome#index'
+  get 'posts/index'
+
+  # root 'welcome#index'
 end
