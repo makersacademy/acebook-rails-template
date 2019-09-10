49bf60528411b48f6c123f651940938a97f8b019
Student <farnazardenti@gmail.com>
2019-09-10 16:37:23 +0100

    testing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 
 class WelcomeController < ApplicationController
   def index
-    # this will pass the current email address in Views 
+    # this will pass the current email address in Views
     if current_user
       @user = current_user.email
     end
