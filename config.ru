5e6dce85b9913da845551571eac34c24417a15d3
EvelinaClayton <clayton.evelina@gmail.com>
2019-09-10 12:15:36 +0100

    add welcome route

--- config.ru
+++ config.ru
@@ -1,6 +1,6 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative 'config/environment'
-require './app'
+# require './app'
 
 run Rails.application
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
+
+  root 'welcome#index'
 end
