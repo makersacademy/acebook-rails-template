4e6d1a50e32245a8cef2210587154850be801d9d
Student <student@Makerss-MacBook-Pro-17.local>
2019-09-10 16:29:05 +0100

    Implemnted user login, logout, signup via devise
    Co-authored-by: Stegosauruss <33098467+Stegosauruss@users.noreply.github.com>

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe WelcomeController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
+      User.create(email: "new@gmail.com", password: "password", password_confirmation: "password")
       get :index
       expect(response).to have_http_status(:success)
     end
