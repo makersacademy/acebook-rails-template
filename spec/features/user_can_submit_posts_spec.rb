44936bf27d09094e6868b85dd41d464889b2e366
Guy Calloway <[guyfcalloway@gmail.com]>
2019-09-10 14:47:09 +0100

    Feature: Sorting by new, and passing feature test

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -8,4 +8,17 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+
+
+  scenario "Can submit posts and view them in correct order" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world2!"
+    click_button "Submit"
+    p "inthe test"
+    expect(page.find('p:nth-child(1)')).to have_content 'world2'
+  end
 end
