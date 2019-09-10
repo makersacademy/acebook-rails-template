099166e227a9fe35b9f647d0da75136a09dfc0e9
Kat <vampirebatkat@gmail.com>
2019-09-10 10:22:38 +0100

    index erb amended

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
+# This is your application's routing file which holds entries in a special DSL 
+# (domain-specific language) that tells Rails how to connect incoming requests to controllers and actions.
+
+
+
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
