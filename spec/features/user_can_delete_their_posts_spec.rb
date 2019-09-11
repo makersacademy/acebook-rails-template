e608f6a6fc7843d095d0ac0533de6dff3557eb00
Jodi <jodiallysonfoster@gmail.com>
2019-09-11 16:14:26 +0100

    Wrote feature test user can delete a post

---
+++ spec/features/user_can_delete_their_posts_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature 'Delete posts', type: :feature do
+  scenario 'A user can delete their posts' do
+    visit '/sign_up'
+    fill_in 'user_email', with: 'test@test.com'
+    fill_in 'user_password', with: 'password'
+    click_button 'Sign up'
+    click_on 'demo'
+    fill_in 'post_message', with: 'My new post'
+    click_on 'Submit'
+    click_button 'delete'
+
+     expect(page).not_to have_content('My new post')
+  end
+end
