3b3ae0b872e8e1ddf7d5f2865703ca505e004ed4
Guillaume <liard.guillaume@gmail.com>
2019-09-11 15:41:17 +0100

    Linked posts to a user via foreign key
    Co-authored-by: EvelinaClayton <clayton.evelina@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190910095155) do
+ActiveRecord::Schema.define(version: 20190911143431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20190910095155) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +33,5 @@ ActiveRecord::Schema.define(version: 20190910095155) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "posts", "users"
 end
