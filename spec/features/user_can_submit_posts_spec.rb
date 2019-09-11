32b36531a754725c24b64829bf5cd825117d15ee
fentonhaslam <fentonhaslam@gmail.com>
2019-09-11 18:29:44 +0100

    fixed rspec

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,13 +1,13 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.feature 'Timeline', type: :feature do
-  scenario 'Can submit posts and view them' do
+RSpec.feature "Timeline", type: :feature do
+  scenario "Can submit posts and view them" do
     # TODO: add login helper method
     sign_in
-    visit '/posts'
-    click_link 'New post'
-    fill_in 'Message', with: 'Hello, world!'
-    click_button 'Submit'
-    expect(page).to have_content('Hello, world!')
+    visit "/posts"
+    click_button "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world!")
   end
 end
