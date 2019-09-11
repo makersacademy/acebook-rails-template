e838d4bae0c9637a3c33d4574a058d168cf7e59d
RMaughan93 <rhys.maughan93@gmail.com>
2019-09-11 15:27:24 +0100

    afternoon recap push to master

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,18 +4,23 @@ class PostsController < ApplicationController
   end
 
   def create
-    p post_params
-    @post = Post.create(post_params)
+    # p post_params
+    # p current_user
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
     redirect_to posts_url
   end
 
   def index
-    @posts = Post.all
+    ordered_posts = Post.order('created_at DESC')
+    @posts = ordered_posts
   end
 
   private
 
   def post_params
+    # p params
     params.require(:post).permit(:message)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    def get_time
+        self.created_at.strftime("%I:%M %p, %d of %B")
+    end
 end
