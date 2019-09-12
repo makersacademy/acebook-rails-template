5fa785c08a545087f7a114fc9c20285b902ee6ae
fentonhaslam <fentonhaslam@gmail.com>
2019-09-12 11:15:19 +0100

    added feature test for update

---
+++ spec/features/user_can_update_post_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.feature "Timeline", type: :feature do
+  scenario "Can edit posts and view them" do
+    # TODO: add login helper method
+    sign_in
+    visit "/posts"
+    click_button "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_button "Edit Post"
+    fill_in "Message", with: "Hello, Dream world!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, Dream world!")
+  end
+end
