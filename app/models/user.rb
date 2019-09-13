adf9d77f6a7f50a1588306989421d02f6521e9f7
Kat <vampirebatkat@gmail.com>
2019-09-13 14:42:00 +0100

    added min 6 characters for password to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   include Clearance::User
   has_many :posts
   validates :username, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }
 end
