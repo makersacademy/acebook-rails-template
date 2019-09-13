717ec5067fa1334318231a11acea5ebcfb38f459
monmon8 <student@Makerss-MacBook-Pro.local>
2019-09-13 15:15:23 +0100

    edit and delete button

--- spec/features/user_can_update_post_spec.rb
+++ spec/features/user_can_update_post_spec.rb
@@ -13,4 +13,19 @@ RSpec.feature 'Timeline', type: :feature do
     click_button 'Submit'
     expect(page).to have_content('Hello, Dream world!')
   end
+
+  scenario "A user cannot edit other peoples posts" do
+    sign_in
+    visit("/posts")
+    click_on "New Post"
+    fill_in "post_message", with: "My new post"
+    click_on "Submit"
+    sign_out
+    visit "/sign_up"
+    fill_in "user_email", with: "test@test.com"
+    fill_in "user_password", with: "password"
+    click_button "Sign up"
+    visit "/posts"
+    expect(page).to have_no_button('Edit Post')
+  end 
 end
