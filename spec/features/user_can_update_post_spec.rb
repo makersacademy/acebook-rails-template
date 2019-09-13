619e29e469abfe07f6b5fd1ada512956711aadeb
fentonhaslam <fentonhaslam@gmail.com>
2019-09-13 15:08:25 +0100

    added new test to test if edit post disappears

--- spec/features/user_can_update_post_spec.rb
+++ spec/features/user_can_update_post_spec.rb
@@ -26,4 +26,14 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("You can't edit your post after 10 mins foooool, gotta delete it mate")
   end
+  scenario "User can't see edit post button after 10 mins" do
+    travel_to Time.local(1994)
+    sign_in
+    visit "/posts"
+    click_button "New Post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    travel_to Time.local(1994) + 601
+    expect(page).not_to have_selector("Edit Post")
+  end
 end
