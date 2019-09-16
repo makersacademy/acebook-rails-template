dd910fbb816c19dd7a3aa2afb393b0c6d340aafc
fentonhaslam <fentonhaslam@gmail.com>
2019-09-16 16:06:21 +0100

    added create comment with user id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,19 @@
 class CommentsController < ApplicationController
-
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    @user_id = current_user.id
+    @comment = @post.comments.new(comment_params)
+    #@comment = @post.comments.create(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    @user = User.find(@comment.user_id)
+    @comment_owner = User.find(@comment.user_id)
+    p @comment_owner
+    @email = @comment_owner.email
     redirect_to posts_url
   end
 
   private
+
   def comment_params
     params.require(:comment).permit(:user_id, :body)
   end
--- spec/features/user_can_add_comment_spec.rb
+++ spec/features/user_can_add_comment_spec.rb
@@ -12,4 +12,18 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Create Comment"
     expect(page).to have_content("This is a comment")
   end
+
+  scenario "A user can see a comments owner" do
+    visit "/sign_up"
+    fill_in "user_email", with: "test@test.com"
+    fill_in "user_password", with: "password"
+    click_button "Sign up"
+    visit "/posts"
+    click_button "New Post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    fill_in "comment[body]", with: "This is a comment"
+    click_button "Create Comment"
+    expect(page).to have_content("Commenter: test@test.com")
+  end
 end
