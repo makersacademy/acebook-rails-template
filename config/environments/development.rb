96917a3ef427c780d57f67f9d637b0cb5c7e7e28
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-09-16 16:40:19 +0100

    Fixed time zone issue + Formatted time on posts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.time_zone = "London"
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
