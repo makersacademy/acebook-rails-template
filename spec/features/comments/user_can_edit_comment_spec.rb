537a3dfec565fe50dce2361ee1ca66a600d8614d
monmon8 <student@Makerss-MacBook-Pro.local>
2019-09-17 15:10:35 +0100

    can't edit comments after 10 min(test)

--- spec/features/comments/user_can_edit_comment_spec.rb
+++ spec/features/comments/user_can_edit_comment_spec.rb
@@ -1,7 +1,9 @@
 require "rails_helper"
 
+
+
 RSpec.feature "Timeline", type: :feature do
-  scenario "Can edit posts and view them" do
+  scenario "Can edit comments and view them" do
     sign_in
     visit "/posts"
     click_button "New Post"
@@ -13,5 +15,22 @@ RSpec.feature "Timeline", type: :feature do
     fill_in "comment[body]", with: "This is the second comment"
     click_button "Update Comment"
     expect(page).to have_content("This is the second comment")
+  end
+
+    scenario "Can't edit comments after 10 min" do
+    travel_to Time.local(1994)
+    sign_in
+    visit "/posts"
+    click_button "New Post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    fill_in "comment[body]", with: "This is a comment"
+    click_button "Create Comment"
+    
+    click_button "Edit Comment"
+    travel_to Time.local(1994) + 610
+    click_button "Update Comment"
+
+    expect(page).to have_content("You can't edit your comment after 10 mins foooool, gotta delete it mate")
   end
 end
