04972aab4f22a9152d0dae546a90728ce2bdd1be
Jamal Springer <jamal.springer@outlook.comm>
2019-09-17 12:44:36 +0100

    created feature test for the feature which allows users to only edit posts up until 10mins after creation

--- spec/features/posts_view_spec.rb
+++ spec/features/posts_view_spec.rb
@@ -27,17 +27,15 @@ RSpec.feature "Post", type: :feature do
     expect(page).not_to have_selector("p", text: "Hello, world!\nThis is a new line\nAnd another line.")
   end
 
-  scenario ""
 
   scenario "Post can only be edited up to 10min after they have been created" do
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!\nThis is a new line\nAnd another line."
     click_button "Submit"
+    current_time_plus_11 = Time.now + 660
+    allow(Time).to receive(:now).and_return(current_time_plus_11)
     first('.post').click_link('edit')
     fill_in "Message", with: "Edited, This has been edited"
-
-
-
   end
 end
