fa465f5dcfc0e48ecc552c6478b371151b68a812
Jodi <jodiallysonfoster@gmail.com>
2019-09-17 11:17:59 +0100

    Changed post.comments.build to Comment.new to fix bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,10 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
-    #@comment = @post.comments.create(comment_params)
+  
     @comment.user_id = current_user.id
     @comment.save
-    p @comment.user.email
+
     redirect_to posts_url
   end
 
