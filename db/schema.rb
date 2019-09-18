3b7416f6a7335fa47ee126103242d848678ae7b7
Student <farnazardenti@gmail.com>
2019-09-18 17:21:12 +0100

    Added wall_id to posts table, changed the new post route from post wall
    Co-authored-by: Stegosauruss <33098467+Stegosauruss@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190912113117) do
+ActiveRecord::Schema.define(version: 20190918161219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190912113117) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "wall_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
