8bbe8528d579fedd63defd5c0492f55fedadd432
harris_lau@hotmail.co.uk <harris_lau@hotmail.co.uk>
2019-09-18 11:29:55 +0100

    New view for username with corresponding unit tests

--- spec/features/clearance/visitor_signs_up_spec.rb
+++ spec/features/clearance/visitor_signs_up_spec.rb
@@ -11,37 +11,37 @@ RSpec.feature 'Visitor signs up' do
   end
 
   scenario 'with valid email and password' do
-    sign_up_with 'valid@example.com', 'password'
+    sign_up_with 'example_username', 'valid@example.com', 'password'
 
     expect_user_to_be_signed_in
   end
 
   scenario 'tries with invalid email' do
-    sign_up_with 'invalid_email', 'password'
+    sign_up_with 'invalid_username', 'invalid_email', 'password'
 
     expect_user_to_be_signed_out
   end
 
   scenario 'tries with blank password' do
-    sign_up_with 'valid@example.com', ''
+    sign_up_with  'example_username', 'valid@example.com', ''
 
     expect_user_to_be_signed_out
   end
 
   scenario 'tries with invalid password 1' do
-    sign_up_with 'valid@example.com', '12'
+    sign_up_with 'example_username', 'valid@example.com', '12'
     expect(current_path).to eq(sign_up_path)
     expect(page).to have_content('Password must be between 6 - 10 characters')
   end
 
   scenario 'tries with invalid password 2' do
-    sign_up_with 'valid@example.com', '12345678910'
+    sign_up_with 'example_username', 'valid@example.com', '12345678910'
     expect(current_path).to eq(sign_up_path)
     expect(page).to have_content('Password must be between 6 - 10 characters')
   end
 
   scenario 'sees a confirmation flash message that he has signed up' do
-    sign_up_with 'valid@example.com', 'password'
+    sign_up_with 'example_username', 'valid@example.com', 'password'
     expect(page).to have_content 'You are now registered to Acebook! Welcome!'
 
     click_link 'New post'
