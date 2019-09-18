0f94d0ea32e2a3f45e0888284890efe20b1661ca
dtrts <djt210@gmail.com>
2019-09-18 11:35:55 +0100

    Created helper methods

--- spec/features/user_wall_posts_spec.rb
+++ spec/features/user_wall_posts_spec.rb
@@ -16,11 +16,7 @@ RSpec.feature 'UserWallPosts', type: :feature do
 
   scenario 'user makes public post, it appears on their wall' do
     user = sign_in
-    visit '/posts'
-    click_button :"new-post-button" # "New Post"
-    fill_in :post_message, with: 'public post for public and wall viewing'
-    click_button 'Submit'
-    # Post.create(user_id: user.id, message: 'public post for public and wall viewing')
+    create_post('public post for public and wall viewing')
     visit('/posts')
     expect(page).to have_content('public post for public and wall viewing')
     visit("/users/#{user.id}")
@@ -30,12 +26,9 @@ RSpec.feature 'UserWallPosts', type: :feature do
   scenario 'user makes public and wall post' do
     user = sign_in
     visit '/posts'
-    click_button :"new-post-button" # "New Post"
-    fill_in :post_message, with: 'public post for public and wall viewing'
-    click_button 'Submit'
-    visit("/users/#{user.id}")
-    fill_in(:message, with: 'This is a wall post')
-    click_button 'post-submit'
+    create_post('public post for public and wall viewing')
+    create_wall_post('This is a wall post', user.id)
+
     visit("/users/#{user.id}")
     expect(page).to have_content('This is a wall post')
     expect(page).to have_content('public post for public and wall viewing')
--- spec/interaction_helper.rb
+++ spec/interaction_helper.rb
@@ -1,1 +1,14 @@
 require 'support/features/clearance_helpers'
+
+def create_post(message)
+  visit '/posts'
+  click_button 'new-post-button' # using the symbol to reference the button by id
+  fill_in 'post[message]', with: message
+  click_button 'Submit'
+end
+
+def create_wall_post(message, user_id)
+  visit("/users/#{user_id}")
+  fill_in(:message, with: message)
+  click_button 'post-submit'
+end
