74d8c24dadc2844fac59ad5275001441eccd558a
Michael Redgrave <michael@breathehr.com>
2019-09-18 12:05:18 +0100

    moved new username params to users controller to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,18 @@ class UsersController < Clearance::UsersController
 
   private
 
+  def user_from_params
+  email = user_params.delete(:email)
+  password = user_params.delete(:password)
+  username = user_params.delete(:username)
+
+  Clearance.configuration.user_model.new(user_params).tap do |user|
+    user.email = email
+    user.password = password
+    user.username = username
+  end
+end
+
   def password_is_invalid?
     @user.password.length < 6 || @user.password.length > 10
   end
