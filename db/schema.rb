d796492f8c7eb04cb707f2c8e28e17177f51f306
Guy Calloway <[guyfcalloway@gmail.com]>
2019-09-19 16:44:41 +0100

    comments initialized

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_130453) do
+ActiveRecord::Schema.define(version: 2019_09_19_135443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 2019_09_18_130453) do
     t.index ["user_id"], name: "index_albums_on_user_id"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "images", force: :cascade do |t|
     t.bigint "album_id"
     t.datetime "created_at", null: false
@@ -74,6 +83,7 @@ ActiveRecord::Schema.define(version: 2019_09_18_130453) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "albums", "users"
+  add_foreign_key "comments", "posts"
   add_foreign_key "images", "albums"
   add_foreign_key "posts", "users"
 end
