9e5acf4e060d51a1318f0d33df3cafe9f8f60411
Volker Bergner <volker@bergner.one>
2019-10-07 15:41:07 +0100

    initial setup

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :rememberable, :recoverable,
+  devise :database_authenticatable, :registerable,
+        :validatable, :trackable, :timeoutable
+end
