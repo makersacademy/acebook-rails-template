9dd6db305f31c0281ec020c388401ab31816e8cf
Mike <mjwalshe@gmail.com>
2019-10-08 12:29:58 +0100

    spike to add update function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update 
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_url
+  end
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
