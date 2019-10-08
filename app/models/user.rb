b19af39d280aec6a7faacf695778b74ce2a80e45
Oliver Antcliff <antcliffoc@gmail.com>
2019-10-08 16:35:41 +0100

    homepage redirects to login, use devises for authentication and sign_in/up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts
-  validates :username, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { in: 6..10,
-   wrong_length: "Your password is not asian enough, 6 to 10 characters.."}
-  validates :ethnicity, presence: true
+  # validates :username, presence: true, uniqueness: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :password, presence: true, length: { in: 6..10,
+  #  wrong_length: "Your password is not asian enough, 6 to 10 characters.."}
+  # validates :ethnicity, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  # match 'welcome/index' => "users#show", :as => :user_root, via: [:get, :post]
 
-  resources :users do
+  devise_for :users
+
+  authenticated :user do
+    root :to => 'users#show'
     resources :posts
+    resources :users
   end
+  unauthenticated :user do
+    root 'welcome#index'
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
