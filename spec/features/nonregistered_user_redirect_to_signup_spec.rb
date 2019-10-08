3a86c887c8e8732f7fad010208ffb534174be372
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-10-08 16:09:13 +0100

    test to check if unauthorised users are redirected to the sign up page is added

---
+++ spec/features/nonregistered_user_redirect_to_signup_spec.rb
@@ -0,0 +1,6 @@
+feature 'non regisered user goes to sig up' do
+  scenario 'if a user goes to any other path he is redirected to sign up' do
+    visit '/posts'
+    expect(page).to have_current_path('/users/sign_in')
+  end
+end
