2fb7d3b2754bd5713e71326bbe2cd1716e405886
Rafaela <rafalucy3@gmail.com>
2019-10-08 14:54:27 +0100

    Add test to see date on posts

--- spec/features/user_can_view_posts_spec.rb
+++ spec/features/user_can_view_posts_spec.rb
@@ -13,4 +13,13 @@ RSpec.feature "Timeline", type: :feature do
     index_of_second_post = page.body.index('Second Post')
     expect(index_of_second_post).to be < index_of_first_post
   end
+
+  scenario "Can see date of posts " do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Blah Blah"
+    click_button "Submit"
+    current_date = Time.now.strftime("%d/%m/%Y")
+    expect(page).to have_content "#{current_date}"
+  end
 end
