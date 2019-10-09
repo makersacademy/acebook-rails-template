c086a0dbfa51a667f0c456b270ca26b421d02e14
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-10-09 14:39:31 +0100

    all tests refractored and passing again Co-Authored-by: V-C-B Volker@bergner.one

--- spec/features/nonregistered_user_redirect_to_signup_spec.rb
+++ spec/features/nonregistered_user_redirect_to_signup_spec.rb
@@ -3,6 +3,9 @@ require 'test_database_helper'
 
 feature 'non regisered user goes to sig up' do
   scenario 'if a user goes to any other path he is redirected to sign up' do
+    user = FactoryBot.create(:user)
+    login_as(user, :scope => :user)
+    logout(:user)
     visit '/posts'
     expect(page).to have_current_path('/users/sign_in')
   end
