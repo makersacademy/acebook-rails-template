cb97ea80c7f421e86e4cc241b5cf7613139bc355
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-10-09 12:18:47 +0100

    all tests are passing Co-Authored-by: V-C-B Volker@bergner.one

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,19 +9,17 @@ describe PostsController do
   end
 end
 
-RSpec.describe PostsController, type: :controller do
+  describe PostsController, type: :controller do
   describe "GET /new " do
+    login_user
     it "responds with 200" do
-      @request.env['devise.mapping'] = Devise.mappings[:user]
-      sign_in @user
-      sign_in @user, scope: :user
       get :new
       expect(response).to have_http_status(200)
     end
   end
 
-
   describe "POST /" do
+    login_user
     it "responds with 200" do
       post :create, params: { post: { message: "Hello, world!" } }
       expect(response).to redirect_to(posts_url)
@@ -34,6 +32,7 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe "GET /" do
+    login_user
     it "responds with 200" do
       get :index
       expect(response).to have_http_status(200)
