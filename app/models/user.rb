0e425360efc31b5b6386be760ea5922ed6b4f703
Volker Bergner <volker@bergner.one>
2019-10-09 17:03:52 +0100

    deactivated email confirm requirement for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :rememberable, :recoverable,
   devise :database_authenticatable, :registerable,
-  :validatable, :timeoutable, :confirmable
+  :validatable, :timeoutable
 end
+# :confirmable
