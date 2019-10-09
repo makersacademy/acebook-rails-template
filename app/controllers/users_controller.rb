569f2ce736128873901e4ab367d03c7ebcc75cab
Julian George <jules104@gmail.com>
2019-10-09 15:14:48 +0100

    Update user controller with user params private method"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    render plain: params[:user].inspect
+    @user = User.new(user_params)
+    @user.save
+    redirect_to  '/'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :email)
+
   end
 
 end
