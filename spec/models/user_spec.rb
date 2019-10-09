3a92d772cede13e6beee4a57c1954dfc0c3bd342
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-10-10 00:52:36 +0100

    Refactor user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {is_expected.to be}
+
+  it 'is valid with all atributes' do
+    user = User.new(first_name: "Harry", last_name: "Potter", email: "harrypotter@fake.com", password: "111111", password_confirmation: "111111")
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a first name' do
+    user = User.new(first_name: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'is not valid without a valid email' do
+    user = User.new(first_name: "Harry", last_name: "Potter", email: "harrypotter", password: "111111", password_confirmation: "111111")
+    expect(user).to_not be_valid
+  end
 end
