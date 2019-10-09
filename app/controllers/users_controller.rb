fe7e95d42f364d810b6f636983d1ddcac50a0b68
James Holton <james_holton@yahoo.co.uk>
2019-10-09 10:36:57 +0100

    Remove p

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,14 @@ class UsersController < ApplicationController
       flash[:returnMessage] = "User successfully registered"
     rescue ActiveRecord::RecordNotUnique
       flash[:returnMessage] = "User already exists"
-      p "user already registered"
     rescue Exception
-      flash[:returnMessage] = "Something horrible happened"
-      p "Caught unexpected error" 
+      flash[:returnMessage] = "Something horrible happened" 
     end
 
     redirect_to root_url
   end
   def index
-    p "index"
-    p @user = User.new
+    @user = User.new
   end
   def post_params
     params.require(:user).permit(:email, :password)
