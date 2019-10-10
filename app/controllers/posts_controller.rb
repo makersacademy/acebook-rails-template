4da7a7dcc4e853ca8987732abbb9a3c6a18fad46
Sayem995 <sayem.bokhari.1@gmail.com>
2019-10-10 17:01:51 +0100

    edit method to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,28 @@
 class PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+  end 
+
   def new
     @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
   end
 
   def create
     @post = Post.create(post_params)
     redirect_to posts_url
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_url
+    else
+      render 'edit'
+    end
   end
 
   def index
