4cd575a67e8c334e2b023c46439fb309a11aea49
Thomas Ross <thomasross3610@gmail.com>
2019-10-10 10:16:22 +0100

    Time functionality on posts added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
 
+private
+
   def time_stamp
     if self.created_at.strftime("%x") == Time.now.strftime("%x")
       return "Posted today at #{self.created_at.strftime("%l:%M")}"
