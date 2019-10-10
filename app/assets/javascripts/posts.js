bd5b104d4cb205050a9fff329de6c31fd74eddee
James Holton <james_holton@yahoo.co.uk>
2019-10-10 09:57:51 +0100

    Quick cleanup of css and js

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,8 +1,3 @@
 // Place all the behaviors and hooks related to postcontroller here.
 
-function editPost(postID, postMessage){
-  var postElement = document.getElementById(postID);
-  returnHTML = "<textarea>" + postMessage + "</textarea>"
-             + "<button>Save</button>"
-  postElement.innerHTML = returnHTML;
-}
+
