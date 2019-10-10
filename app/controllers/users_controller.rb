e4db1dfc7ba51ee2d5b86a4164ff06ef3447c8b2
James Holton <james_holton@yahoo.co.uk>
2019-10-10 14:57:15 +0100

    sorted auth and refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,11 @@
 class UsersController < ApplicationController
 
   def new
-    p @user = User.new
+    @user = User.new
   end
 
   def create
-    if params[:login_button]
-      p "here"
-      p params[:user][:email]
-      p @user = User.find_by(email: params[:user][:email])
-      p session[:user_id] = @user.id
-      redirect_to posts_url
-      # if BCrypt::Password.new(@user.password) == params['password']
-      #   redirect '/spaces'
-      # else flash[:notice] = "Credentials Incorrect"
-      #   redirect '/login'
-      # end
-    else
-
-      begin
-        p @user = User.create(post_params)
-        if @user.errors.messages[:email][0]
-          flash[:returnMessage] = @user.errors.messages[:email][0]
-        end
-
-        if @user.errors.messages[:password][0]
-          flash[:returnMessage] = @user.errors.messages[:password][0]
-        end
-
-        if @user.valid?
-          flash[:returnMessage] = "User successfully registered"
-          redirect_to posts_url
-        else
-          redirect_to new_user_path
-        end
-
-      rescue ActiveRecord::RecordNotUnique
-        flash[:returnMessage] = "User already exists"
-        redirect_to new_user_path
-      rescue Exception
-        flash[:returnMessage] = "Something horrible happened"
-        redirect_to new_user_path
-      end
-    end
+    params[:login_button] ? login : register
   end
 
   def index
@@ -51,5 +14,47 @@ class UsersController < ApplicationController
 
   def post_params
     params.require(:user).permit(:email, :password)
+  end
+
+  private
+  def login
+    begin
+      @user = User.find_by(email: params[:user][:email])
+
+      if @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to posts_url
+      else
+        flash[:returnMessage] = "Log in details not valid"
+        redirect_to index
+      end
+    rescue Exception
+      flash[:returnMessage] = "Serious - Logon database error"
+      redirect_to index
+    end
+  end
+
+  def register
+    begin
+      @user = User.create(post_params)
+      flash[:returnMessage] = @user.errors.messages[:email][0] if @user.errors.messages[:email][0]
+      flash[:returnMessage] = @user.errors.messages[:password][0] if @user.errors.messages[:password][0] 
+      user_validation
+    rescue ActiveRecord::RecordNotUnique
+      flash[:returnMessage] = "User already exists"
+      redirect_to new_user_path
+    rescue Exception
+      flash[:returnMessage] = "Something horrible happened"
+      redirect_to new_user_path
+    end
+  end
+
+  def user_validation
+    if @user.valid?
+      flash[:returnMessage] = "User successfully registered"
+      redirect_to posts_url
+    else
+      redirect_to new_user_path
+    end
   end
 end
--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -8,4 +8,15 @@ RSpec.feature "Login", type: :feature do
     click_button('Submit')
     expect(current_path).to eq('/posts')
   end
+
+  scenario "User fails to login with incorrect password" do
+    user = User.create(email: 'test@example.com', password: 'abc123')
+    visit('/')
+    click_link('Log In')
+    fill_in "user[email]", with: "test@example.com"
+    fill_in "user[password]", with: "different"
+    click_button('Submit')
+    expect(page).to have_content("Log in details not valid")
+    expect(current_path).to eq('/users')
+  end
 end
