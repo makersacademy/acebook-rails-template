fc0ad10e0be7ccdcc206d99b0a3a777465aa7ce6
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-10-11 01:30:10 +0100

    Add log out (session destroy) method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,4 +8,13 @@ module SessionsHelper
     # returns the current logged-in user(if any)
     @current_user ||= User.find_by(id: session[:user_id])
   end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(user:id)
+    @current_user = nil
+  end
 end
