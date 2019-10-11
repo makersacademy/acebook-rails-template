6c838f1ebeb2e940c2eb8a0792c4bc5e03772fad
Thomas Ross <thomasross3610@gmail.com>
2019-10-11 11:34:39 +0100

    failing time stamp method feature test fixed, and time stamp unit tests added

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
 
   def time_stamp
     if self.created_at.strftime("%x") == Time.now.strftime("%x")
-      return "Posted today at #{self.created_at.strftime("%l:%M")}"
+      return "Posted today at #{self.created_at.strftime("%H:%M")}"
     else "Posted on #{self.created_at.strftime("%A %d %b")} at #{self.created_at.strftime("%H:%M")}"
     end
   end
