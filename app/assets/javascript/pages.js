7f285c818acc5f9c0fdff19ea35820d5bedc1f1f
Oliver Antcliff <antcliffoc@gmail.com>
2019-10-11 15:05:23 +0100

    messages now display with latest at top, and prepend with jquery

---
+++ app/assets/javascript/pages.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//   var messages = $('chatbox');
+//
+//   function init() {
+//     messages.scrollTop(messages[0].scrollHeight);
+//   }
+//
+//   window.setTimeout(init, 50);
+// });
