a4c888b16e0b343107c4a9772ef065cbf136cea8
Oliver Antcliff <antcliffoc@gmail.com>
2019-10-11 16:43:50 +0100

    cannot get message to autoload when sent from a different window

--- app/assets/javascript/pages.js
+++
@@ -1,9 +0,0 @@
-// $(document).ready(function() {
-//   var messages = $('chatbox');
-//
-//   function init() {
-//     messages.scrollTop(messages[0].scrollHeight);
-//   }
-//
-//   window.setTimeout(init, 50);
-// });
---
+++ app/javascript/channels/chat_channel.js
@@ -0,0 +1,19 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("ChatChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+    console.log('connected to chat channel');
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+    console.log('disconnected from chat channel');
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+    var messages = $('chatbox');
+    messages.prepend(data['message']);
+  }
+});
