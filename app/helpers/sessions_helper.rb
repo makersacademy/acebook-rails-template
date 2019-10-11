48a3dc26af1e9059ed8f067ce130a7bdc61b1d41
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-10-11 01:21:18 +0100

    Add log_in and current_user to session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,11 @@
 module SessionsHelper
   def log_in(user)
+    # logs in the given user
     session[:user_id] = user.id
+  end
+
+  def current_user
+    # returns the current logged-in user(if any)
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
