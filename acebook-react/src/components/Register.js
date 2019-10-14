af8976898ca6de405bbb1625f2570abc166118b1
mackacavs <mackenziegdixon@yahoo.com>
2019-10-14 16:29:47 +0100

    have added redirect after sign-up

--- acebook-react/src/components/Register.js
+++ acebook-react/src/components/Register.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom';
 
 class Register extends React.Component {
   constructor() {
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  state = {
+    redirect: false
+  }
+
+  setRedirect = () => {
+    this.setState({
+      redirect: true
+    })
+  }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to='/home' />
+    }
+  }
+
   handleSubmit(event) {
     event.preventDefault();
     const data = new FormData(event.target);
@@ -12,19 +30,26 @@ class Register extends React.Component {
       method: 'POST',
       body: data,
     }
-    ).then(response => response.json()).then(data => console.log(data));
+    ).then(response => response.json()).then(data => {
+      this.setRedirect()
+      this.renderRedirect()
+    });
   }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label htmlFor="email">Enter your email</label>
         <input id="email" name="email" type="email" />
-          <label htmlFor="email">Enter your password</label>
-          <input id="password" name="password" type="password" />
+        <label htmlFor="email">Enter your password</label>
+        <input id="password" name="password" type="password" />
+        {this.renderRedirect()}
         <button>Login</button>
-         
+
       </form>
     );
   }
- }
- export default Register;
+}
+
+export default Register;
+
