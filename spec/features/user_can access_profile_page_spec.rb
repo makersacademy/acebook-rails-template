63fa62626015f9cd1b58880379762c8f11190e55
Julian George <jules104@gmail.com>
2019-10-14 16:20:14 +0100

    Reduced numer of tests to tet Travis CI

--- spec/features/user_can access_profile_page_spec.rb
+++ spec/features/user_can access_profile_page_spec.rb
@@ -1,21 +1,21 @@
-require 'rails_helper'
-require 'spec_helper'
-
-RSpec.feature "Login", type: :feature do
-  scenario "User can view profile page" do
-    visit signup_path
-    fill_in "First Name", with: "Superman"
-    fill_in "Last Name", with:" Kent"
-    fill_in "Email", with: "superman@gmail.com"
-    fill_in "Password", with: "password"
-    fill_in "Confirm Password", with: "password"
-    click_button "Sign Up"
-    visit login_path
-    fill_in "Email", with: "superman@gmail.com"
-    fill_in "Password", with: "password"
-    click_button "Log in"
-    expect(page).to have_content("Your posts")
-    click_button "Profile Page"
-    expect(page).to have_content('Profile Page')
-  end
-end
+# require 'rails_helper'
+# require 'spec_helper'
+#
+# RSpec.feature "Login", type: :feature do
+#   scenario "User can view profile page" do
+#     visit signup_path
+#     fill_in "First Name", with: "Superman"
+#     fill_in "Last Name", with:" Kent"
+#     fill_in "Email", with: "superman@gmail.com"
+#     fill_in "Password", with: "password"
+#     fill_in "Confirm Password", with: "password"
+#     click_button "Sign Up"
+#     visit login_path
+#     fill_in "Email", with: "superman@gmail.com"
+#     fill_in "Password", with: "password"
+#     click_button "Log in"
+#     expect(page).to have_content("Your posts")
+#     click_button "Profile Page"
+#     expect(page).to have_content('Profile Page')
+#   end
+# end
--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,14 +1,14 @@
-require 'rails_helper'
-
-RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and view them with date" do
-    current_date = Time.now.strftime("%A %d %b")
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
-    expect(page).to have_content("Posted today at")
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.feature "Timeline", type: :feature do
+#   scenario "Can submit posts and view them with date" do
+#     current_date = Time.now.strftime("%A %d %b")
+#     visit "/posts"
+#     click_link "New post"
+#     fill_in "Message", with: "Hello, world!"
+#
+#     click_button "Submit"
+#     expect(page).to have_content("Hello, world!")
+#     expect(page).to have_content("Posted today at")
+#   end
+# end
