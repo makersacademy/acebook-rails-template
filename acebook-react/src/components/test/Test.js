985cc9077f02b1f8a862787d38e047c3484fdf14
James Holton <james_holton@yahoo.co.uk>
2019-10-14 14:38:31 +0100

    thin slice of login using react and api

--- acebook-react/src/components/test/Test.js
+++ acebook-react/src/components/test/Test.js
@@ -6,7 +6,8 @@ class Test extends Component {
  componentDidMount() {
    fetch(`http://localhost:3000/api/v1/users/`)
      .then(response => response.json())
-     .then(data => this.setState({
+     .then(data => 
+      this.setState({
        email: data[0].email
      }))
  }
