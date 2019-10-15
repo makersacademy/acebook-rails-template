d808ace10568e912957204ed48c75114cfa79568
Neil Campbell <neildcampbell@hotmail.com>
2019-10-15 12:21:32 +0100

    tried tests solving

--- spec/features/user_can_post_on_any_wall_spec.rb
+++ spec/features/user_can_post_on_any_wall_spec.rb
@@ -1,21 +1,19 @@
 require 'rails_helper'
 require 'spec_helper'
 
-RSpec.feature "Login", type: :feature do
-  scenario "Can Log In" do
+RSpec.feature "Post on Wall", type: :feature do
+  scenario "post a message" do
     visit signup_path
-    # p ("signup path = #{signup_path}")
     fill_in "First Name", with: "Superman"
     fill_in "Last Name", with:" Kent"
-    fill_in "Email", with: "1@gmail.com"
+    fill_in "Email", with: "superman@gmail.com"
     fill_in "Password", with: "password"
     fill_in "Confirm Password", with: "password"
     click_button "Sign Up"
-    visit '/login'
-    fill_in "Email", with: "1@gmail.com"
+    visit login_path
+    fill_in "Email", with: "superman@gmail.com"
     fill_in "Password", with: "password"
     click_button "Log in"
-    visit()
     expect(page).to have_content("Profile Page")
   end
 end
