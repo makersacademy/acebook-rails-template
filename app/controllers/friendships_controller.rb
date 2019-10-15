0778a2b111fc781b5704e839dec8ab26f217b822
Dan Gyi <daniel.gyi@gmail.com>
2019-10-15 10:48:41 +0100

    friend requests confirm/reject

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,6 +2,16 @@
 
 class FriendshipsController < ApplicationController
   protect_from_forgery with: :exception
+
+  def index
+    @friend_requests = Friendship.where(user_id: current_user.id, confirmed: false)
+    @current_friends = Friendship.where(user_id: current_user.id, confirmed: true)
+    @friend_ids_requests = @friend_requests.map { |friendship| 
+    friendship.friend_id
+    }
+    @request_user_models = User.find(@friend_ids_requests)
+  end
+
   def create
     if params.include?(:friend_id)
       @new_friendships = Friendship.create_reciprocal_for_ids(current_user.id, params[:friend_id])
@@ -14,7 +24,17 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    Friendship.destroy_reciprocal_for_ids(current_user_id, params[:friend_id])
+    @friendship_obj = Friendship.find(params[:id])
+    Friendship.destroy_reciprocal_for_ids(current_user.id, @friendship_obj.friend_id)
     redirect_to(request.referer)
-    end
+    # flash[:notice] = "Friend Confirmed"
+    # redirect_to friendships
+  end
+
+  def update
+    @friendship_obj = Friendship.find(params[:id])
+    Friendship.confirm(current_user.id, @friendship_obj.friend_id)
+    flash[:notice] = "Friend Confirmed"
+    redirect_to friendships
+  end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -8,12 +8,19 @@ class Friendship < ApplicationRecord
     user_friendship = Friendship.create(user_id: user_id, friend_id: friend_id)
     friend_friendship = Friendship.create(user_id: friend_id, friend_id: user_id)
     [user_friendship, friend_friendship]
-      end
+  end
 
   def self.destroy_reciprocal_for_ids(user_id, friend_id)
     friendship1 = Friendship.find_by(user_id: user_id, friend_id: friend_id)
     friendship2 = Friendship.find_by(user_id: friend_id, friend_id: user_id)
     friendship1.destroy
     friendship2.destroy
-      end
   end
+
+  def self.confirm(user_id, friend_id)
+    friendship1 = Friendship.find_by(user_id: user_id, friend_id: friend_id)
+    friendship2 = Friendship.find_by(user_id: friend_id, friend_id: user_id)
+    friendship1.update(confirmed: true)
+    friendship2.update(confirmed: true)
+  end
+end
