7f466f99423dd96fe2439a488f68b9038bf9026e
Dan Gyi <daniel.gyi@gmail.com>
2019-10-15 09:23:37 +0100

    update navigation with friend requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,6 @@ class UsersController < ApplicationController
   def nonfriends
     @friends = Friendship.where(user_id: current_user).map { |x| x.friend_id }
     @friends.push(current_user.id)
-    p @friends
     @non_friends = User.where.not(id: @friends)
-    p @non_friends
   end
 end
