1522e8e867f377e7357e84d304e87e9f6433b23c
Mark Dobbins <mjdobbins12@gmail.com>
2019-10-16 16:24:32 +0100

    test refactoring

--- spec/views/profile_pictures/show.html.erb_spec.rb
+++ spec/views/profile_pictures/show.html.erb_spec.rb
@@ -1,11 +1,11 @@
-require 'rails_helper'
-
-RSpec.describe "profile_pictures/show", type: :view do
-  before(:each) do
-    @profile_picture = assign(:profile_picture, ProfilePicture.create!())
-  end
-
-  it "renders attributes in <p>" do
-    render
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe "profile_pictures/show", type: :view do
+#   before(:each) do
+#     @profile_picture = assign(:profile_picture, ProfilePicture.create!())
+#   end
+#
+#   it "renders attributes in <p>" do
+#     render
+#   end
+# end
