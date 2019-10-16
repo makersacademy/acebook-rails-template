4b88aa90d8397a2364c069f856ad35cb5be1e4b7
mackacavs <mackenziegdixon@yahoo.com>
2019-10-16 15:47:09 +0100

    Have fixed re-loading error

--- acebook-react/src/components/Posts.js
+++ acebook-react/src/components/Posts.js
@@ -20,72 +20,72 @@ class Posts extends React.Component {
       method: 'DELETE',
       body: data,
     }
-  ).then(response => response.json()
-).then(data => this.fetchData())
-}
-
-fetchData() {
-  fetch('http://localhost:3000/api/v1/posts/', {
-    method: 'GET',
+    ).then(response => response.json()
+    ).then(data => this.fetchData())
   }
-).then(response => response.json())
-.then(data => this.setState({
-  isLoaded: true,
-  posts: data
-}))
-}
 
-handleSubmit(event) {
-  event.preventDefault();
-  const data = new FormData(event.target);
-  console.log(event);
-  fetch('http://localhost:3000/api/v1/posts/', {
-    method: 'POST',
-    body: data,
+  fetchData() {
+    fetch('http://localhost:3000/api/v1/posts/', {
+      method: 'GET',
+    }
+    ).then(response => response.json())
+      .then(data => this.setState({
+        isLoaded: true,
+        posts: data
+      }))
   }
-).then(response => response.json()
-).then(data => this.fetchData())
-}
 
-handleUpdate(event) {
-  event.preventDefault();
-  const data = new FormData(event.target);
-  fetch('http://localhost:3000/api/v1/posts/', {
-    method: 'PUT',
-    body: data,
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    console.log(event);
+    fetch('http://localhost:3000/api/v1/posts/', {
+      method: 'POST',
+      body: data,
+    }
+    ).then(response => response.json()
+    ).then(data => this.fetchData())
   }
-).then(response => response.json()
-).then(data => this.fetchData())
-document.getElementById(`${event.target[0].value}`).text="";
-console.log(document.getElementById(`${event.target[0].value}`))
-}
 
-render() {
-  const { posts } = this.state;
-  console.log(posts)
-  return (
-    <div>
-    <h1>Posts</h1>
-    {posts.map(post => (
-      <div key={post.id}>
-      <form onSubmit={this.handleDelete.bind(this)}>
-        {post.message}
-        <input id="id" name="id" value={post.id} type="text" readOnly hidden />
-        <button>delete</button>
-      </form>
-      <form onSubmit={this.handleUpdate.bind(this)}>
-        <input id="id" name="id" value={post.id} type="text" readOnly hidden />
-        <input id={post.id} name="message" type="text"/>
-        <button>update</button>
-      </form>
-    </div>
-    ))}
-    <form onSubmit={this.handleSubmit.bind(this)}>
-      <input id="newMessage" name="message" type="text"/><br/>
-    <button type="submit">Add Post</button>
-    </form>
-    </div>
-  );
-}
+  handleUpdate(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    fetch('http://localhost:3000/api/v1/posts/', {
+      method: 'PUT',
+      body: data,
+    }
+    ).then(response => response.json()
+    ).then(data => this.fetchData())
+    document.getElementById(`${event.target[0].value}`).value = "";
+    console.log(document.getElementById(`${event.target[0].value}`))
+  }
+
+  render() {
+    const { posts } = this.state;
+    console.log(posts)
+    return (
+      <div>
+        <h1>Posts</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <form onSubmit={this.handleDelete.bind(this)}>
+              {post.message}
+              <input id="id" name="id" value={post.id} type="text" readOnly hidden />
+              <button>delete</button>
+            </form>
+            <form onSubmit={this.handleUpdate.bind(this)}>
+              <input id="id" name="id" value={post.id} type="text" readOnly hidden />
+              <input id={post.id} name="message" type="text" />
+              <button>update</button>
+            </form>
+          </div>
+        ))}
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input id="newMessage" name="message" type="text" /><br />
+          <button type="submit">Add Post</button>
+        </form>
+      </div>
+    );
+  }
 }
 export default Posts;
