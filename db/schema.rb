10aa5f288500b9f63ce9348ada8ca41a040f9898
Neil Campbell <neildcampbell@hotmail.com>
2019-10-16 11:14:45 +0100

    dbmigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191010105028) do
+ActiveRecord::Schema.define(version: 20191015170407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "body", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.integer "recipient_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -33,5 +44,7 @@ ActiveRecord::Schema.define(version: 20191010105028) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
