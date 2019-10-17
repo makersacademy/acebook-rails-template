c5396b782ff3ff68fa1e730a9934dfd7c85ebce0
Mike <mjwalshe@gmail.com>
2019-10-17 21:29:01 +0100

    Fix test: create and view album

--- spec/features/user_cannot_create_empty_album_spec.rb
+++ spec/features/user_cannot_create_empty_album_spec.rb
@@ -5,9 +5,6 @@ RSpec.feature "photo_album", type: :feature do
     register
     visit "/albums"
     click_link "New Album"
-    fill_in "album[name]", with: "Toast"
-    fill_in "album[title]", with: "cool"
-    fill_in "album[body]", with: "nice pic"
     click_button "Create Album"
     expect(page).to have_content("Images are missing!")
   end
