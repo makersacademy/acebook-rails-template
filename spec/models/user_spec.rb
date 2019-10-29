539318dd4c73cb031739b40445353f5cbbaf840d
jordanadedeji <jdon4692@gmail.com>
2019-10-29 15:19:06 +0000

    email identifier added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
-
-RSpec.describe User, type: :model do
-    validates_email_realness_of :email
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# require 'rails_helper'
+#
+# RSpec.describe User, type: :model do
+#     validates_email_realness_of :email
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
