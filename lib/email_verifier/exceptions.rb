55ac2014001da4f91bab204383a0265695e6cdf0
Marion <marion.vagnerdamicourt@kpmg.co.uk>
2019-10-29 15:30:25 +0000

    email validate

--- lib/email_verifier/exceptions.rb
+++
@@ -1,6 +0,0 @@
-module EmailVerifier
-  class NoMailServerException < StandardError; end
-  class OutOfMailServersException < StandardError; end
-  class NotConnectedException < StandardError; end
-  class FailureException < StandardError; end
-end
