94fd85d3ecaddc8dd61a779ecd366844ce8d7b04
AdamHeffer <adamheffer@gmail.com>
2019-10-29 10:44:17 +0000

    updating for codeship rspevc tests pt 2

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PostsController, type: :controller do
   describe "GET /new " do
     it "responds with 200" do
       get :new
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(404)
     end
   end
 
