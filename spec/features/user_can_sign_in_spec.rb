f1eb99168797b79a20acfd6416ebf9f5d08ccbc7
sarahhhayle <sarah.vandervliet@bglgroup.co.uk>
2019-10-30 16:35:28 +0000

    Refactored tests by adding signing-up method in rails helper

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -2,14 +2,9 @@
 
 require 'rails_helper'
 
-RSpec.feature 'Signing up', type: :feature do
+RSpec.feature 'Signing in', type: :feature do
   scenario '- can sign in' do
-    visit '/'
-    click_link 'Sign up'
-    fill_in 'Email', with: 'sarah@test.com'
-    fill_in 'Password', with: 'qwerty'
-    fill_in 'Password confirmation', with: 'qwerty'
-    click_button 'Sign up'
+    signing_up
     click_link 'Log Out'
 
     visit '/'
--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -4,12 +4,7 @@ require 'rails_helper'
 
 RSpec.feature 'Timeline', type: :feature do
   scenario 'Can submit posts and view them' do
-    visit '/'
-    click_link 'Sign up'
-    fill_in 'Email', with: 'eoin@test.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-    click_button 'Sign up'
+    signing_up
 
     visit '/posts'
     click_link 'New post'
