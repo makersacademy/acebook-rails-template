b32439941010a7df9577b60d5e75a2571341b5c0
sarahhhayle <sarah.vandervliet@bglgroup.co.uk>
2019-10-30 15:41:20 +0000

    Corrected rubocop offenses

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature "Signing up", type: :feature do
-  scenario "- can sign in" do
-    visit "/"
-    click_link "Sign up"
-    fill_in "Email", with: "sarah@test.com"
-    fill_in "Password", with: "qwerty"
-    fill_in "Password confirmation", with: "qwerty"
-    click_button "Sign up"
-    click_link "Log Out"
+RSpec.feature 'Signing up', type: :feature do
+  scenario '- can sign in' do
+    visit '/'
+    click_link 'Sign up'
+    fill_in 'Email', with: 'sarah@test.com'
+    fill_in 'Password', with: 'qwerty'
+    fill_in 'Password confirmation', with: 'qwerty'
+    click_button 'Sign up'
+    click_link 'Log Out'
 
-    visit "/"
-    fill_in "Email", with: "sarah@test.com"
-    fill_in "Password", with: "qwerty"
-    click_button "Log in"
+    visit '/'
+    fill_in 'Email', with: 'sarah@test.com'
+    fill_in 'Password', with: 'qwerty'
+    click_button 'Log in'
 
-    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content('Signed in successfully.')
   end
 end
--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and view them" do
-    visit "/"
-    click_link "Sign up"
-    fill_in "Email", with: "eoin@test.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_button "Sign up"
+RSpec.feature 'Timeline', type: :feature do
+  scenario 'Can submit posts and view them' do
+    visit '/'
+    click_link 'Sign up'
+    fill_in 'Email', with: 'eoin@test.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
 
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+    expect(page).to have_content('Hello, world!')
   end
 end
