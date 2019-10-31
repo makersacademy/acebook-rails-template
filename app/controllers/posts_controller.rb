2dea616b19d52e9e0cead271f615e72e01f35234
sarahhhayle <sarah.vandervliet@bglgroup.co.uk>
2019-10-31 17:13:27 +0000

    Corrected rubocop offenses

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,11 +15,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
-   @post = Post.find(params[:id])
-   @post.destroy
+    @post = Post.find(params[:id])
+    @post.destroy
 
-   redirect_to posts_path
- end
+    redirect_to posts_path
+  end
 
   private
 
