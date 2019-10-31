a1b3463e3b6a3e7c932bddd82edd61220002944e
sarahhhayle <sarah.vandervliet@bglgroup.co.uk>
2019-10-31 17:06:47 +0000

    Fix to errors on codeship after deleting posts functionality commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:message, :users_id)
+    params.require(:post).permit(:message)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  # belongs_to :user, class_name: 'User'
 end
