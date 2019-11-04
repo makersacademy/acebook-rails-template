202cffef5f42c02949348095c8eea53b1c5678e4
Mezela <p_mezue@hotmail.com>
2019-11-04 16:54:19 +0000

    Add timestamp and refactor to add a feature helper method

--- spec/features/user_can_update_post_spec.rb
+++ spec/features/user_can_update_post_spec.rb
@@ -2,10 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Content", with: "Hello, world!"
-    click_button "Submit"
+    visit_page_and_post
     expect(page).to have_content("Hello, world!")
     click_link "Edit"
     fill_in "Content", with: "Goodbye, world!"
