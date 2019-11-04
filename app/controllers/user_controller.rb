26fc80ac9530778922ff55d60c047f8c9a08b88a
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-04 14:53:29 +0000

    creating user routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,2 @@
 class UserController < ApplicationController
-  def index
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
+
   get 'user/index'
 
-  root 'user#index'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
