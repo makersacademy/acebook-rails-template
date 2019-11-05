3bf1c314dd8bffb8703a0a414647cefe40c644b3
Nabille Helal <nabilleh@outlook.com>
2019-11-05 15:30:53 +0000

    Add password reset email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password:ENV["GMAIL_PASSWORD"]
+  }
+  # doesn't have to be Heroku, but you get the idea.
+  config.action_mailer.default_url_options = { :host => 'acebook-winkleface.herokuapp.com' }
 end
