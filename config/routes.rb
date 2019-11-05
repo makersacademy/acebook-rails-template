16cef0c27bac8e24be20dca93b89c0a4ad948fc7
Student <john.dennehy@gmail.com>
2019-11-05 11:42:06 +0000

    Initial setup of devise user object and login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
+
+  root to: "posts#index"
+
 end
