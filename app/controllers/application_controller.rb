19550ab2804c1c84fa7aaa58fecfba5a53dff6b7
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-11-05 12:50:03 +0000

    feat: redirect routes amended after sign in/ log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  protected
+  def after_sign_in_path_for(resource)
+    "/posts"
+  end
 
+  def after_sign_out_path_for(resource)
+    "/users/sign_in"
+  end
 
 end
