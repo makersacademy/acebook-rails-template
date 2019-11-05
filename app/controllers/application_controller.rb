bfff45ec070a698892a30a4c5233ce26191bdb4c
Student <john.dennehy@gmail.com>
2019-11-05 15:58:54 +0000

    Adds log out feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,9 @@ class ApplicationController < ActionController::Base
 
   protected
 
-       def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
 
-            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :password, :current_password)}
-       end
-
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :password, :current_password) }
+  end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -10,5 +9,4 @@ RSpec.describe WelcomeController, type: :controller do
   end
 
   describe ''
-
 end
