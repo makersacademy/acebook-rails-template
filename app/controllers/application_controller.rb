11c0dc5ae211846e9d3ee5029a5a729bbfa1bb0e
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-11-06 09:29:05 +0000

    added rubocop and yml file. Fixed offenses.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:user_name, :email, :password) }
-    # devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :password, :current_password) }
+    # devise_parameter_sanitizer.permit(:account_update) {
+  #   |u| u.permit(:first_name, :last_name, :password, :current_password) 
+  # }
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -31,7 +33,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Defines default URL options in dev environment 
+  # Defines default URL options in dev environment
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
