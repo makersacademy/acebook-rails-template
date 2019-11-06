ad471ac63be33ba5248ff04dd1e1920cced1de53
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-11-06 16:35:40 +0000

    Uses sessions to record current page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,23 +27,24 @@ end
 
   def update
     @comment = Comment.find(params[:id])
-
-    if @comment.update(comment_params)
-      redirect_to @comment
-    else
-      render 'edit'
-    end
+    @comment.update(comment_params)
+    redirect_to_previous_page
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-
-    redirect_to comments_path
+    redirect_to_previous_page
   end
 
 private
   def comment_params
     params.require(:comment).permit(:text,:post_id, :user_id, :id)
+  end
+
+  def redirect_to_previous_page
+    redirect = session[:path]
+    session[:path] = nil
+    redirect_to redirect
   end
 end
