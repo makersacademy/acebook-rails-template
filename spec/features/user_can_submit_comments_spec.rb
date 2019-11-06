ab3a10b603994025de2bac55376590281669ded9
Heli Sivunen <heli@mooncat.tv>
2019-11-06 15:01:08 +0000

    Add comment box to Newsfeed.

---
+++ spec/features/user_can_submit_comments_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.feature "Timeline", type: :feature do
+  scenario "User can submit comments" do
+    signup_as_new_user("Pam")
+    login_as_user("Pam")
+    create_new_post("Hello, world!")
+    fill_in "Comment[text]", with: "Great post!"
+    click_button "Save Comment"
+    expect(page).to have_content("Great post!")
+  end
+end
