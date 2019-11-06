7a349c8150a0d0ef65cd695d4c3e024303f4991f
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-11-06 11:31:12 +0000

    Fixes test except post controller

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit "/posts"
+    signup_as_new_user("Pam")
+    login_as_user("Pam")
     create_new_post("Hello, world!")
     expect(page).to have_content("Hello, world!")
   end
 
   scenario "Post displays the time and date created" do
-    visit "/posts"
+    signup_as_new_user("Pam")
+    login_as_user("Pam")
     create_new_post("Hello, world!")
     expect(page).to have_content(DateTime.now.strftime("%H:%M %a %e %b %Y"))
   end
