23c9e6eca38f58270fbb8ea699b9fb9c9dd268f6
Dom Tunstill <datunstill@gmail.com>
2019-11-06 20:06:36 +0000

    add user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'welcome/index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'}
+
+  devise_scope :user do
+    get 'users/profile', to: 'users/sessions#profile'
+  end
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
 
-  root to: "welcome#index"
+  # root to: "welcome#index"
 
 end
