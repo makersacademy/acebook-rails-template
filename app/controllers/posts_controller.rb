50671cb43d0423a69444ce609a5aa51b727f0207
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2019-11-06 12:02:37 +0000

    Added delete functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,12 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def destroy
+    @post = Post.find params[:id]
+    @post.destroy
+    redirect_to posts_url
+  end
+
   private
 
   def post_params
