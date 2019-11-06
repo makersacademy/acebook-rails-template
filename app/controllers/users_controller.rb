6e796ccbc35ad4d7805c2fb9fc9691735fbda6b5
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-06 11:50:45 +0000

    incorrect signin shows error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,10 @@ class UsersController < ApplicationController
     if @user.valid?
       redirect_to '/'
     else @user.invalid?
-      p @user.errors.messages.values.join(" ")
-      # flash[:notice] = @user.errors.messages.first
-      redirect_to '/signup', alert: @user.errors.messages.first.join(" ")
+      signin_error = @user.errors.messages.first.flatten.join(" ")
+      redirect_to '/signup', alert: signin_error
     end
+
   end
 
   def show
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :email, :full_name, :desired_attribute, :birthday, :profile_picture)
+    params.require(:user).permit(:username, :password, :email, :full_name, :lizard_species, :birthday, :profile_picture)
   end
 end
--- spec/features/user_can_sign_up.rb
+++ spec/features/user_can_sign_up.rb
@@ -10,6 +10,6 @@ RSpec.feature "sign up", type: :feature do
     signup_as_new_user("Duncan")
     signup_as_new_user("Duncan")
     expect(current_path).to eq("/signup")
-    expect(page).to have_content("Username already taken")
+    expect(page).to have_content("username has already been taken")
   end
 end
