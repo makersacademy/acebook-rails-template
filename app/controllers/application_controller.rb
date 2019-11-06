008647fa35674694e15d137fc156aae72f52f743
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-11-06 14:54:16 +0000

    Added helper method for redirection.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,20 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(
+      :username,
+      :email,
+      :password,
+      :password_confirmation
+      )
+    end
     # devise_parameter_sanitizer.permit(:account_update) {
   #   |u| u.permit(:first_name, :last_name, :password, :current_password)
   # }
   end
+
+  # def after_sign_in_path_for(resource)
+  #
+  # end
 end
