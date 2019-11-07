9a1eb4c85a21a8ad12f5d93e6f0f39d93394fbea
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-11-07 15:20:00 +0000

    Users shown unlike button if they already liked a post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -12,10 +12,21 @@ class LikesController < ApplicationController
     redirect_to redirect_path
   end
 
+  def destroy
+    @like = Like.find(params[:id])
+    @like.destroy
+    redirect_to_previous_page
+  end
+
   private
 
   def post_params
     params.require(:like).permit(:post_id)
   end
 
+  def redirect_to_previous_page
+    redirect = session[:path] || '/posts'
+    session[:path] = nil
+    redirect_to redirect
+  end
 end
