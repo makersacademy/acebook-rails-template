4f9f710c4b722ab819b2c24a4efa5a5aa7606052
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-11-07 14:27:07 +0000

    Users can only like a post once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,11 +5,9 @@ class LikesController < ApplicationController
   end
 
   def create
-    p "-----------------"
-    p params
-    p "-----------------"
-
-    @like = Like.create(post_id: post_params["post_id"], user_id: session[:user_id])
+    unless Like.find_by(post_id: post_params["post_id"], user_id: session[:user_id])
+      @like = Like.create(post_id: post_params["post_id"], user_id: session[:user_id])
+    end
     redirect_path = params.require(:like).permit(:redirect_path)[:redirect_path] || '/posts'
     redirect_to redirect_path
   end
