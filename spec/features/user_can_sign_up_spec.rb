f6045888eab8419e5d34fc7763c70b2c2e22e99c
Dom Tunstill <datunstill@gmail.com>
2019-11-07 12:38:42 +0000

    update password validation when creating to include maximum length of 10 chars and test for minimum length and email validation

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -12,10 +12,10 @@ RSpec.feature "Sign_up", type: :feature do
     click_link "Sign Up"
     expect(page).to have_content("Sign up")
     expect(page).to have_content("Email")
-    expect(page).to have_content("Password (6 characters minimum)")
+    expect(page).to have_content("Password (between 6 and 10 characters)")
   end
 
-  scenario "can't sign up within invalid email address 'testtest.com'" do
+  scenario "can't sign up with invalid email address 'testtest.com'" do
     visit "/"
     click_link "Sign Up"
     fill_in 'user[first_name]', with: 'John'
@@ -25,8 +25,44 @@ RSpec.feature "Sign_up", type: :feature do
     fill_in 'user[password_confirmation]', with: 'password'
     click_button 'Sign up'
     expect(page).to have_content("Email")
-    expect(page).to have_content("Password (6 characters minimum)")
-    expect(page).to have_content ("error prohibited this user from being saved: Email is invalid")
+    expect(page).to have_content("Password (between 6 and 10 characters)")
+    expect(page).to have_content (
+      "error prohibited this user from being saved: Email is invalid"
+      )
+  end
+
+  scenario "can't sign up with a password shorter than 6 characters" do
+    visit "/"
+    click_link "Sign Up"
+    fill_in 'user[first_name]', with: 'John'
+    fill_in 'user[last_name]', with: 'Doe'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: 'pass'
+    fill_in 'user[password_confirmation]', with: 'pass'
+    click_button 'Sign up'
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password (between 6 and 10 characters)")
+    expect(page).to have_content (
+      "error prohibited this user from being saved: 
+      Password is too short (minimum is 6 characters)"
+      )
+  end
+
+  scenario "can't sign up with a password longer than 10 characters" do
+    visit "/"
+    click_link "Sign Up"
+    fill_in 'user[first_name]', with: 'John'
+    fill_in 'user[last_name]', with: 'Doe'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: 'passwordpass'
+    fill_in 'user[password_confirmation]', with: 'passwordpass'
+    click_button 'Sign up'
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password (between 6 and 10 characters)")
+    expect(page).to have_content (
+      "error prohibited this user from being saved: 
+      Password is too long (maximum is 10 characters)"
+      )
   end
 
   scenario 'Can create a new user account' do
