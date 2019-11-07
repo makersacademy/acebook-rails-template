5ea95de2173db724f52c1e84a22b34b86d11ea14
Dom Tunstill <datunstill@gmail.com>
2019-11-07 10:34:15 +0000

    posts can have line breaks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_153737) do
+ActiveRecord::Schema.define(version: 2019_11_07_103129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_153737) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "message"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
