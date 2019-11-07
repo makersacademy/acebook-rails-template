6ca2371eaa0cea6d4debe70ba1583315017234f7
Olly Holovchenko <oleh.holo@gmail.com>
2019-11-07 17:28:34 +0000

    Fix feature tests for likes

--- spec/features/user_can_like_posts_spec.rb
+++ spec/features/user_can_like_posts_spec.rb
@@ -6,24 +6,24 @@ RSpec.feature "Like post", type: :feature do
     login_user
     visit '/posts'
     create_post
-    click_link "Buzz"
-    expect(page).to have_content("Buzzes: 1")
-    expect(page).to have_content('⚡️ Buzz')
+    click_link "Like"
+    # expect(page).to have_content("Buzzes: 1")
+    expect(page).to have_content('⚡️ Like 1')
   end
-
+  
   scenario "User can like other user's post and number of likes will change" do
     create_user
     login_user
     visit '/posts'
     create_post
-    click_link "Buzz"
+    click_link "Like"
     logout_user
-
+    
     create_user_two
     login_user_two
     visit '/posts'
-    click_link "Buzz"
-    expect(page).to have_content("Buzzes: 2")
+    click_link "Like"
+    expect(page).to have_content("2")
   end
   
   scenario "User can unlike the post" do
@@ -31,9 +31,10 @@ RSpec.feature "Like post", type: :feature do
     login_user
     visit '/posts'
     create_post
-    click_link "Buzz"
-    click_link "Buzz"
-    expect(page).to have_content("Buzzes: 0")
+    click_link "Like"
+    click_link "Like"
+    expect(page).not_to have_content('⚡️ Like 1')
+    expect(page).not_to have_content('⚡️ Like 2')
   end
-
+  
 end
