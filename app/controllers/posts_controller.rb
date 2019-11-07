23bc2af9023fc9a72096b78f446fd7f14e44c091
DanielleInkster <danielle.inkster@gmail.com>
2019-11-07 20:21:25 +0000

    Fix edit bugs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,10 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.where(id: params[:id]).first
+    if current_user != @post.user
+      flash[:alert] = "Sorry! You can't edit someone else's post."
+      redirect_to posts_url
+    end
     return if @post
 
     redirect_to root_path
@@ -26,6 +30,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.where(id: params[:id]).first
+    
     if @post.update(message: params[:post][:message])
       flash[:notice] = 'Successfully updated the post!'
       redirect_to posts_url
@@ -37,7 +42,9 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.where(id: params[:id]).first
-    if @post.destroy
+    if current_user != @post.user
+      flash[:alert] = "Sorry! You can't delete someone else's post." 
+    elsif @post.destroy
       flash[:notice] = 'Successfully deleted the post!'
     else
       flash[:alert] = 'Couldn’t delete the post...'
