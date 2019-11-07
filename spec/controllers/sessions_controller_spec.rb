94b7d5b5932f719121638977e1475f19f4fb68a7
Jessica Marais <jessica_marais@hotmail.com>
2019-11-07 14:01:53 +0000

    Line breaks added

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
-# TEST FAILING - NEEDS TO BE COMPLETED 
+# TEST FAILING - NEEDS TO BE COMPLETED
 
 RSpec.describe Users::SessionsController, type: :controller do
   describe 'POST #create' do
     pending
     it 'welcomes user back with username' do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      post :create, params: { username: "Tan", email: "tan@test.com" }
+      post :create, params: {username: "Tan", email: "tan@test.com"}
       # p user.username
       # expect(subject.welcome_message).to eq "Welcome back Tan"
       # p response.welcome_message
       p @params
+
 
     end
   end
