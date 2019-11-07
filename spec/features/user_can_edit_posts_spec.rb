a575b4744dacb520813b6943b27e2823140aedae
Olly Holovchenko <oleh.holo@gmail.com>
2019-11-07 12:08:40 +0000

    Add 10 min time restriction for editing the posts

--- spec/features/user_can_edit_posts_spec.rb
+++ spec/features/user_can_edit_posts_spec.rb
@@ -12,4 +12,18 @@ RSpec.feature "Edit post", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello again, world!")
   end
+
+  scenario "User can not edit post created more than 10 min ago" do
+    create_user
+    login_user
+    visit '/posts'
+    time = Time.utc(2019, 11, 05, 14, 58, 10)
+    Timecop.travel(time)
+    create_post
+    new_time = Time.utc(2019, 11, 05, 15, 9, 10)
+    Timecop.travel(new_time)
+    visit '/posts'
+    expect(page).to have_no_button "Edit"
+    Timecop.return
+  end
 end
