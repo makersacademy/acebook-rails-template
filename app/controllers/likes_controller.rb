1b78be9afe09f96c15427e2a89335e63e6347314
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-11-07 15:52:52 +0000

    feat: user can like and unlike any post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,19 +1,29 @@
 class LikesController < ApplicationController
-  before_action :find_likable
+  before_action :find_likeable
 
-  def create
-    @likable.liked_by current_user
+  # def create
+  #   @likeable.liked_by current_user
+  # end
+  #
+  # def destroy
+  #   @likeable.disliked_by current_user
+  #   # current_user.dislikes @likeable
+  # end
+
+  def upvote
+    @likeable.liked_by current_user
   end
 
-  def destroy
-    @likable.disliked_by current_user
+  def downvote
+    @likeable.disliked_by current_user
   end
 
   private
 
-  def find_likable
+  def find_likeable
+    p params[:likeable_type]
     @likeable_type = params[:likeable_type].camelize
-    @likeable = @likeable_type.constantize.find(params[:likable_id])
+    @likeable = @likeable_type.constantize.find(params[:likeable_id])
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
-    resources :likes
+    # resources :likes
     resources :comments do
     end
   end
+  post '/posts/:post_id/like', to: 'likes#upvote', as: 'like_post'
+  post '/posts/:post_id/unlike', to: 'likes#downvote', as: 'unlike_post'
 end
