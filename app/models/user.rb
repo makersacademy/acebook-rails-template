5c7f06eee70f5fb76d87df800e9a5c1ec65d2f1b
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-07 14:55:04 +0000

    validates emails are emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6, maximum: 10 }
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
   validates :full_name, presence: true
   validates :birthday, presence: true
 end
