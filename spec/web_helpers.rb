b6b23b688caf39f1f77f0426f3aa4c387386ea02
Brad Phillips <bjrp@protonmail.com>
2019-11-08 12:05:04 +0000

    Adds new web helper methods

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -12,6 +12,22 @@ def login_user
   click_button 'Log in'
 end
 
+def logout_user
+  click_link 'Logout'
+end
+
+def create_user_two
+  User.create(username: 'BartSimpson', email: 'test2@test.com', password: 'password')
+end
+
+def login_user_two
+  visit '/'
+  click_link 'Login'
+  fill_in 'user[email]', with: 'test2@test.com'
+  fill_in 'user[password]', with: 'password'
+  click_button 'Log in'
+end
+
 def create_post
   click_link "New post"
   fill_in "Message", with: "Hello, world!"
