edbe9b2f8aee3b7a23f903aa8584de13819fda22
Mezela <p_mezue@hotmail.com>
2019-11-08 17:09:49 +0000

    Fixes merge bug: add end statement

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20191108141107) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "comment_likes", force: :cascade do |t|
     t.bigint "user_id"
