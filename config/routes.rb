acf0201b0d5143e4a274c23ad535e8de7b87e4f8
AlecDWalker <walkeralec@hotmail.com>
2019-11-08 12:01:32 +0000

    Corrected typos

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
 
   root to: "welcome#index"
 
-  devise_scope :user do
-    selete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+  # devise_scope :user do
+  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
