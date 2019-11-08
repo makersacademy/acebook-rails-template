fe7b003b60ab2443df7afa34c864c7700e17f45f
AlecDWalker <walkeralec@hotmail.com>
2019-11-08 11:55:35 +0000

    Omniauth framework added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'posts' => 'posts#index', as: :user_root
 
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'usrs/omniauth_callbacks' }
 
   resources :posts do
     member do
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   end
 
   root to: "welcome#index"
+
+  devise_scope :user do
+    selete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_11_07_103129) do
-
+ActiveRecord::Schema.define(version: 2019_11_08_105151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +55,8 @@ ActiveRecord::Schema.define(version: 2019_11_07_103129) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "avatar"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
