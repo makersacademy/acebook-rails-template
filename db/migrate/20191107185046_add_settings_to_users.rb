b165fc7e98c1f33e6164cbf881e4bbcb60a11e34
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-08 12:51:33 +0000

    fixing migrations

--- db/migrate/20191107185046_add_settings_to_users.rb
+++ db/migrate/20191107185046_add_settings_to_users.rb
@@ -1,6 +1,6 @@
 class AddSettingsToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :font_family, :string
-    add_column :users, :background_colour, :string
+    add_column :users, :font_family, :string, :default => 'LightIkaros'
+    add_column :users, :background_colour, :string, :default => '#DEB887'
   end
 end
--- db/migrate/20191108094506_add_defaults_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultsToUsers < ActiveRecord::Migration[5.1]
-  def change
-    change_column :users, :font_family, :string, :default => 'LightIkaros'
-    change_column :users, :background_colour, :string, :default => '#DEB887'
-  end
-end
