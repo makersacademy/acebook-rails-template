bd45afe515b1c79872d7b6eb677706fac52043ac
Duncan Skinner <duncanskinner94@gmail.com>
2019-11-08 15:27:37 +0000

    Add tests and code for changing wall colour and font.

---
+++ spec/features/user_can_change_wall_colour_spec.rb
@@ -0,0 +1,31 @@
+
+require 'rails_helper'
+
+
+RSpec.feature "User setting", type: :feature do
+  scenario "User can change wall colour" do
+    signup_as_new_user("Pam")
+    login_as_user("Pam")
+    save_and_open_page
+    select 'green', from: "Background colour"
+    click_button "Lizardify me!"
+        save_and_open_page
+    within('body') do
+    page.body.should have_content ('background-color: green')
+
+    end
+  end
+
+  scenario "User can change font" do
+    signup_as_new_user("Pam")
+    login_as_user("Pam")
+    save_and_open_page
+    select 'Calibri', from: "Font family"
+    click_button "Lizardify me!"
+        save_and_open_page
+    within('body') do
+    page.body.should have_content ('font-family: Calibri')
+
+    end
+  end
+end
