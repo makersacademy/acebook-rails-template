0e4eee5c9efa8f5dd56090f23ccbc4af7fa25326
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-11-11 14:44:47 +0000

    comments table has been set up

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-  acts_as_commontable
+  has_many :comments
 
   def not_editable?
     (Time.now - created_at) > 600
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191108141945) do
+ActiveRecord::Schema.define(version: 20191111144156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.string "body"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "commontator_comments", force: :cascade do |t|
     t.bigint "thread_id", null: false
