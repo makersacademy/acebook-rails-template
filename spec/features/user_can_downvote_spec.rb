afb1a27e43f10ee56a6bb278f7291d33185c8431
Brad Phillips <bjrp@protonmail.com>
2019-11-11 17:13:55 +0000

    Adds feature tests for multiple users voting on posts

--- spec/features/user_can_downvote_spec.rb
+++ spec/features/user_can_downvote_spec.rb
@@ -22,4 +22,18 @@ RSpec.feature "Votes", type: :feature do
     expect(page).to have_content('Hedgehog 0')
     expect(page).to have_content('Badger 1')
   end
+
+  scenario "Additional users can add a downvote to a post" do
+    visit('/')
+    create_user
+    login_user
+    create_post
+    click_link 'Badger'
+    logout_user
+    create_user_two
+    login_user_two
+    click_link 'Badger'
+    expect(page).to have_content('Hedgehog 0')
+    expect(page).to have_content('Badger 2')
+  end
 end
--- spec/features/user_can_upvote_spec.rb
+++ spec/features/user_can_upvote_spec.rb
@@ -22,4 +22,18 @@ RSpec.feature "Votes", type: :feature do
     expect(page).to have_content('Hedgehog 1')
     expect(page).to have_content('Badger 0')
   end
+
+  scenario "Additional users can add an upvote to a post" do
+    visit('/')
+    create_user
+    login_user
+    create_post
+    click_link 'Hedgehog'
+    logout_user
+    create_user_two
+    login_user_two
+    click_link 'Hedgehog'
+    expect(page).to have_content('Hedgehog 2')
+    expect(page).to have_content('Badger 0')
+  end
 end
