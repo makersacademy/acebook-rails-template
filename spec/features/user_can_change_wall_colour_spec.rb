672a9b03114c88846ce23fa802b1b6712c31c493
Mezela <p_mezue@hotmail.com>
2019-11-11 16:58:46 +0000

    Fix merge test failure

--- spec/features/user_can_change_wall_colour_spec.rb
+++ spec/features/user_can_change_wall_colour_spec.rb
@@ -6,12 +6,7 @@ RSpec.feature "User setting", type: :feature do
   scenario "User can change wall colour" do
     signup_as_new_user("Pam")
     login_as_user("Pam")
-<<<<<<< HEAD
-    select '#9E764D', from: "Background colour"
-    # save_and_open_page
-=======
     select 'LawnGreen', from: "Background colour"
->>>>>>> master
     click_button "Lizardify me!"
     within('body') do
     page.body.should have_content ('background-color: LawnGreen')
