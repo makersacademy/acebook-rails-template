595e4d909919a26677ea350a057faf730e809363
zraso <zaira.rasool@hotmail.co.uk>
2019-11-11 10:43:17 +0000

    Factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,14 +6,15 @@ FactoryBot.define do
     id { 1 }
   end
 
-  factory :post do
-    id { 1 }
-    user_id { 1 }
-  end
+  # factory :post do
+  #   message { "Hello, world!" }
+  #   id { 1 }
+  #   user_id { 1 }
+  # end
 
-  factory :comment do
-    id { 1 }
-    post_id { 1 }
-  end
+  # factory :comment do
+  #   id { 1 }
+  #   post_id { 1 }
+  # end
 
 end
