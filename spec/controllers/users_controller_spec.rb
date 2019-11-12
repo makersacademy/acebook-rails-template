5e8b40def36162d31098ce670f8302194bb99c95
Dom Tunstill <datunstill@gmail.com>
2019-11-12 09:45:18 +0000

    fix failing profile test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe "GET #profile" do
-    xit "expect 302 code as user is not logged in" do
-      get :profile
+    it "expect 302 code as user is not logged in" do
+      get(:profile, params: { id: 1 })
       expect(response).to have_http_status(302)
     end
     
