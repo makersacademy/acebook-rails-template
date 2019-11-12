af975c1c6357798a12c32593bbb96d853d9e61d6
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2019-11-12 15:41:46 +0000

    rubocopped

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.where(wall_id:params[:id])
-
+    @posts = Post.where(wall_id: params[:id])
   end
 
 end
