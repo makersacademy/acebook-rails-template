37e700119f4c115e5ccd74706717b6c1646a75d2
Jessica Marais <jessica_marais@hotmail.com>
2019-11-13 14:42:24 +0000

    Fix issue with removing commontator gem

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   controller :posts do
-    
+
     get '/new_post' => :new, as: 'new_post'
     get 'users/:id' => 'users#show', as: 'user_post'
     get '/post/:id/edit' => :edit, as: 'edit_post'
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     delete '/post/:id/delete' => :destroy, as: 'destroy_post'
   end
 
-  mount Commontator::Engine => '/commontator'
+  # mount Commontator::Engine => '/commontator'
 
   resources :posts do
     # resources :comments
@@ -26,14 +26,14 @@ Rails.application.routes.draw do
       put "Dislike", to: "posts#downvote"
     end
   end
-  
+
   devise_scope :user do
     get 'users/:id' => 'posts#index', as: 'user'
   end
 
   resources :posts do
     resources :comments
-  end 
+  end
 
   resources :users, :only => [:show]
 end
