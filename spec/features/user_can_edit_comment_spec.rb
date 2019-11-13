b8d8f1b22d84ce4ade0bdcc6c36a10d24dedd682
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2019-11-13 11:25:20 +0000

    fixed pending tests

--- spec/features/user_can_edit_comment_spec.rb
+++ spec/features/user_can_edit_comment_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "Comment", type: :feature do
   scenario "Users can edit their own comments" do
-    pending
     visit('/')
     create_user
     login_user
     create_post
+    visit('/posts')
     create_comment
     edit_comment
     expect(page).to have_content('An edited comment')
--- spec/features/user_can_upvote_spec.rb
+++ spec/features/user_can_upvote_spec.rb
@@ -1,25 +1,25 @@
 # # frozen_string_literal: true
 
-# require 'rails_helper'
+ require 'rails_helper'
 
 RSpec.feature "Votes", type: :feature do
   scenario "Can upvote posts" do
-    pending
     visit('/')
     create_user
     login_user
     create_post
+    visit('/posts')
     click_link 'Hedgehog'
     expect(page).to have_content('Hedgehog 1')
     expect(page).to have_content('Badger 0')
   end
 
   scenario "A user cannot add more than one upvote to a post" do
-    pending
     visit('/')
     create_user
     login_user
     create_post
+    visit('/posts')
     click_link 'Hedgehog'
     click_link 'Hedgehog'
     expect(page).to have_content('Hedgehog 1')
@@ -27,15 +27,16 @@ RSpec.feature "Votes", type: :feature do
   end
 
   scenario "Additional users can add an upvote to a post" do
-    pending
     visit('/')
     create_user
     login_user
     create_post
+    visit('/posts')
     click_link 'Hedgehog'
     logout_user
     create_user_two
     login_user_two
+    visit('/posts')
     click_link 'Hedgehog'
     expect(page).to have_content('Hedgehog 2')
     expect(page).to have_content('Badger 0')
