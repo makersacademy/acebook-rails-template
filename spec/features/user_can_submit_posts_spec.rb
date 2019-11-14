c905673da4dcb8c62952bdccd4255c976208fe92
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-11-14 10:07:42 +0000

    Tests passing

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -5,7 +5,6 @@ RSpec.feature "Timeline", type: :feature do
     visit('/')
     create_user
     login_user
-    click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
