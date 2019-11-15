af8def26ba39afda34d412b94ba524a6280788da
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-11-15 10:57:10 +0000

    Fixed double quotes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,19 +19,19 @@ $(document).ready(function () {
     $("#myAlert").alert("close");
   });
 
-  var btn = $('#button');
+  var btn = $("#button");
 
   $(window).scroll(function() {
     if ($(window).scrollTop() > 300) {
-      btn.addClass('show');
+      btn.addClass("show");
     } else {
-      btn.removeClass('show');
+      btn.removeClass("show");
     }
   });
 
-  btn.on('click', function(e) {
+  btn.on("click", function(e) {
     e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '300');
+    $("html, body").animate({scrollTop:0}, "300");
   });
 
 });
