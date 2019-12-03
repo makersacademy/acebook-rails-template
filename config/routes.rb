bdcce677e83bb51514e96adfef7b02a6bc8b8a2b
Natalie <49326857+natyeo@users.noreply.github.com>
2019-12-03 16:04:10 +0000

    user class setup with devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get 'welcome/index'
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  
   resources :posts
+
+  root 'welcome#index'
 end
