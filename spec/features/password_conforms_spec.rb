245854ba79837337ea35d49f5c5eadee4f0fff39
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-12-03 15:55:52 +0000

    added password length test

---
+++ spec/features/password_conforms_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "Passwords", type: :feature do
+  scenario "must be at least 6 characters" do
+    visit "/users/sign_up"
+    fill_in "user_email", with: "bob@bob.com"
+    fill_in "user_password", with: "123"
+    fill_in "user_password_confirmation", with: "123"
+    click_button "Sign up"
+    expect(page).to have_content("1 error prohibited this user from being saved")
+    expect(page).to have_content("6 characters minimum")
+  end
+
+  scenario "must be no longer than 10 characters" do
+    visit "/users/sign_up"
+    fill_in "user_email", with: "bob@bob.com"
+    fill_in "user_password", with: "123123123123"
+    fill_in "user_password_confirmation", with: "123123123123"
+    click_button "Sign up"
+    expect(page).to have_content("1 error prohibited this user from being saved")
+    expect(page).to have_content("maximum is 10 characters")
+  end
+end
