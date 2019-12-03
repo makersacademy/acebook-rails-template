27ed3981c8917b4e460a8b967e51b7777b203f63
elfi yang <elfiyang16@gmail.com>
2019-12-03 11:23:26 +0000

    user can create new post 80% done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:message)
+    params.require(:post).permit(:message, :username)
   end
 end
