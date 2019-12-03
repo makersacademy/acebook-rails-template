1eda4889c38510bd99e574bc214963ab0d86a598
Emma Van Oss <e.vanoss@gmail.com>
2019-12-03 17:30:18 +0000

    unit tests for posts_controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  describe "GET /new " do
-    it "responds with 200" do
-      get :new
-      expect(response).to have_http_status(200)
-    end
-  end
+  # test not required as we are not using /new route
+  # describe "GET /new " do
+    # it "responds with 200" do
+      # get :new
+      # expect(response).to have_http_status(200)
+    # end
+  # end
 
   describe "POST /" do
-    it "responds with 200" do
-      post :create, params: { post: { message: "Hello, world!" } }
+    it "redirects to index" do
+      post :create, params: { post: { message: "Hello, world!", username: "Jo33" } }
       expect(response).to redirect_to(posts_url)
     end
 
-    it "creates a post" do
-      post :create, params: { post: { message: "Hello, world!" } }
+    it "creates an entry in the database" do
+      post :create, params: { post: { message: "Hello, world!", username: "Jo33" } }
       expect(Post.find_by(message: "Hello, world!")).to be
+      expect(Post.find_by(username: "Jo33")).to be
     end
   end
 
   describe "GET /" do
-    it "responds with 200" do
+    it "index responds with 200" do
       get :index
       expect(response).to have_http_status(200)
     end
