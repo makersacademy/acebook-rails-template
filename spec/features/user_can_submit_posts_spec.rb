c40ea7b0d637ca09708ae07273b5bc741195c954
Robert <bibbycodes@gmail.com>
2019-12-03 18:31:22 +0000

    testing again

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -13,7 +13,7 @@ end
 RSpec.feature "Failing", type: :feature do
   scenario "Can not go to this page " do
     visit "/index"
-    click_link "New most"
+    click_link "New post"
     expect(page).to have_content("Hello, bla!")
   end
 end
