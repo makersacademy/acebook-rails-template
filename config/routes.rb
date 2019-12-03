9344366c976807dff67aa41a52a712d4577b718c
liatmoss <liatmoss@hotmail.com>
2019-12-03 17:08:07 +0000

    fixed spelling error

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'post/index'
+  get 'posts/index'
 
 #   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
