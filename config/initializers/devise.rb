0e3ce770869b1344543bb81990d75fc59b2b6437
Ben Stuart <benarthurstuart@hotmail.com>
2019-12-04 15:51:06 +0000

    adds password length requirements

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,6 +3,9 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  #change password character length to be between 6 and 10
+  config.password_length = 6..10
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
