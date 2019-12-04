6d683cea7a5c0ffbd70ff2906eebc7403aff75ef
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-12-04 14:44:33 +0000

    tested redirect again via feature test

---
+++ spec/features/redirect_to_home_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+feature 'redirect to home page' do
+  scenario 'when user is not logged in' do
+    visit '/posts'
+    expect(page).to have_text 'Welcome to DashBook'
+  end
+end
