57e0fae3d723f5f5715533f399a7dad488fc6b12
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-12-04 14:40:09 +0000

    tested redirection back to home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to welcome_index_url
+    end
+  end
 end
