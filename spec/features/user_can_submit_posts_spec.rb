bd74a2e4988dbd3eb68505b3fbc59110d90d276a
Robert <bibbycodes@gmail.com>
2019-12-04 15:13:58 +0000

    adds error

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -14,6 +14,6 @@ RSpec.feature "Failing", type: :feature do
   scenario "Can not go to this page " do
     visit "/index"
     click_link "New post"
-    expect(page).to have_content("Hello, blca!")
+    expect(page).to have_content("Hello, bitches!")
   end
 end
