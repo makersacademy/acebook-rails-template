1c6d3eb46f95d2c4331c9a91f0df1064807b4748
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-12-04 15:55:24 +0000

    added additional test to cover list ordering on submit post

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'launchy'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.1'
 # Use postgresql as the database for Active Record
@@ -39,6 +39,8 @@ group :development, :test do
   gem 'rubocop'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'orderly'
+  gem 'launchy'
 end
 
 group :development do
--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,12 +1,27 @@
 require 'rails_helper'
 
-RSpec.feature "Timeline", type: :feature do
+
+RSpec.feature "Post", type: :feature do
   scenario "Can submit posts and view them" do
-    visit "/posts"
-    fill_in "Username", with: "Robot"
-    fill_in "Message", with: "Hello, world!"
+    visit "/"
+    fill_in "post_username", with: "Alistair Phipps"
+    fill_in "post_message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_css("ul", text: "Hello, world!")
-    expect(page).to have_css("ul", text: "Robot")
+    expect(page).to have_css("ul", text: "Alistair Phipps")
+
+  end
+
+  scenario "Posts are displayed in the correct order" do
+    visit "/"
+    fill_in "post_username", with: "Alistair Phipps"
+    fill_in "post_message", with: "Hello, world!"
+    click_button "Submit"
+    fill_in "post_username", with: "Alistair Phipps"
+    fill_in "post_message", with: "A later Hello World"
+    click_button "Submit"
+    expect("A later Hello World").to appear_before("Hello, world!")
+
+
   end
 end
