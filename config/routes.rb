8ac425d5ba527966925b5c8c0efa888bbc077480
Student <student@Makerss-MacBook-Pro.local>
2019-12-04 15:11:49 +0000

    redirects correctly and has sign in button on the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { registrations: 'users/registrations', confirmations: 'users/confirmations', sessions: 'users/sessions', mailer: 'users/mailer', passwords: 'users/passwords', shared: 'users/shared', unlocks: 'users/unlocks' }
 
-  
+
   resources :posts
   resources :users
 
