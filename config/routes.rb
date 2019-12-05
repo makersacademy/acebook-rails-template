6c708cf8d69aa3d68b148b0a1e2b54b0b082e811
Robert <bibbycodes@gmail.com>
2019-12-05 14:09:10 +0000

    sharing with everyon

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'users/sessions', registrations: 'users/registrations'}
   get 'welcome/index'
 
-  resources :posts
 
-  delete 'users/sign_out'
+  # Resource routing allows you to quickly declare all of the common routes for a given resourceful controller. 
+  # Instead of declaring separate routes for your index, show, new, edit, create, update and destroy actions, 
+  # a resourceful route declares them in a single line of code.
+  resources :posts, :users
 
+  # delete 'users/sign_out'
   root 'welcome#index'
 
 end
