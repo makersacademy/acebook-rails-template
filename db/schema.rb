970b8b9d5b025ed019297cd3e3d54d7dfd78b77c
Topher <topherb99@gmail.com>
2019-12-05 15:22:14 +0000

    posts linked to users, and edit functionality in progress

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20191203160047) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
