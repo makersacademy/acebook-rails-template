a5ad80fe5b135618486b1bc6f4ac28acdc5fc90b
Student <student@Makerss-MacBook-Pro.local>
2019-12-05 13:52:04 +0000

    enabled rubocop

--- spec/linters/rubocop_spec.rb
+++ spec/linters/rubocop_spec.rb
@@ -1,8 +1,8 @@
 # require 'spec_helper'
-
+#
 # RSpec.describe 'rubocop analysis' do
 #   subject(:report) { `rubocop` }
-
+#
 #   it 'has no offences' do
 #     expect(report).to match(/no\ offences\ detected/)
 #   end
