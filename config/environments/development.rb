093a81e2d0e8464ca2b37292a9bf743ac5c6e93b
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-12-06 15:11:40 +0000

    add suggested rubocop changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -36,7 +38,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
-  
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
