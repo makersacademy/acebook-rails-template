eb0c8a5814cab5bc778c874672dbca0db4d405d5
Ben230 <53790237+Ben230@users.noreply.github.com>
2019-12-06 13:48:23 +0000

    removed comments on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,10 @@ Post.destroy_all
 
 user1 = User.create({email: 'elfi@test.com', password: '1234567', username: "elfi"})
 user2 = User.create({email: 'angela@test.com', password: 'password', username: "angela"})
-# user3 = User.create({username: 'sam', email: 'sam@test.com', password: 'password'})
 
 posts = [
   {message: 'What do sprinters eat before a race? Nothing, they fast!', username: "elfi", user_id: user1.id},
   {message: 'Why did the scarecrow win an award? Because he was outstanding in his field!',username: "angela", user_id: user2.id}
-#   {message: 'What do you call a macncheese that gets all up in your face? Too close for comfort food!', user_id: user3.id },
-#   {message: 'Why couldnt the bicycle stand up by itself? It was two tired!', user_id: user1.id},
-#   {message: 'Did you hear about the restaurant on the moon? Great food, no atmosphere!', user_id: user1.id},
-#   {message: 'What do you call a fish with two knees? A two-knee fish!', user_id: user3.id},
-#   {message: 'Why do melons have weddings? Because they cantaloupe!', user_id: user2.id}
  ]
 
 posts.each do |post|
