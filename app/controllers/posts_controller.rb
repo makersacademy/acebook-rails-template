e04de849a4d2b3963e415d512e6b683390165980
Natalie <49326857+natyeo@users.noreply.github.com>
2019-12-06 10:34:43 +0000

    can now show individual posts and go back to all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+before_action :set_post, only: [:show, :edit, :destroy]
+
   def new
     @post = current_user.posts.build
   end
@@ -12,7 +14,17 @@ class PostsController < ApplicationController
     @posts = current_user.posts.order(created_at: :desc)
   end
 
+  def show
+  end
+
+  def edit
+  end
+
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:id, :message, :user_id)
