7d2f63f2104560a0c5cda711d31690117ae6acf4
Robert <bibbycodes@gmail.com>
2019-12-06 12:15:10 +0000

    redirects to posts on login

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,9 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
   # def create
@@ -19,6 +19,10 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # protected
+
+  def after_sign_in_path_for(resource)
+    :posts
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
