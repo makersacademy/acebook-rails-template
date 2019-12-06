917fe7d4c69f08484b4e53d4e3ee1657ae5e0dc7
Student <student@Makerss-MacBook-Pro.local>
2019-12-06 10:54:45 +0000

    capybara commands changed to make tests pass

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     sign_up
     visit "/posts"
-    click_link "New post"
+    click_link "New Post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
