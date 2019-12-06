8ba6b40b66adc9101091a906f4673196b2b4b76e
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-12-06 15:17:39 +0000

    add more rubocop suggested changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
@@ -14,5 +16,5 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     :posts
   end
-  
+
 end
