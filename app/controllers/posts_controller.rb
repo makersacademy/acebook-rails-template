fe8b3da5649c2610996c4af6c3905c3c5aebc79d
Andy Jones <andytjones@icloud.com>
2019-12-06 11:58:29 +0000

    Can see individual posts. Users can only see update for posts they created.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
 
 
@@ -7,9 +8,25 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.create(post_params.merge(user_id: current_user.id))
     redirect_to posts_url
+  end
+
+  def update
+    @post =  Post.find(params[:id])
+
+    @post.update(post_params)
+    redirect_to posts_url
+
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def index
