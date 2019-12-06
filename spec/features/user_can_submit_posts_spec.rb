faf9289feb9d4b53fd7acf6f612c96f7a38e7637
Ben Stuart <benarthurstuart@hotmail.com>
2019-12-06 12:16:44 +0000

    fixed merged test

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -20,13 +20,21 @@ RSpec.feature "Post", type: :feature do
   end
 
   scenario "Posts are displayed in the correct order" do
+    visit "/users/sign_up"
+    fill_in "Email", with: "beep@robot.com"
+    fill_in "Password", with: "cyberdog"
+    fill_in "Password confirmation", with: "cyberdog"
+    click_button "Sign up"
+
     visit "/"
     fill_in "post_username", with: "Alistair Phipps"
     fill_in "post_message", with: "Hello, world!"
     click_button "Submit"
+
     fill_in "post_username", with: "Alistair Phipps"
     fill_in "post_message", with: "A later Hello World"
     click_button "Submit"
+    
     expect("A later Hello World").to appear_before("Hello, world!")
   end
 end
