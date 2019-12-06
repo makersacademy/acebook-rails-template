7db6d31bc2eb2a5756f3168b0e93b632d923e19c
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-12-06 16:04:22 +0000

    added jquery, listener for edit button to dyanmically update save path in modal

--- Gemfile
+++ Gemfile
@@ -4,7 +4,10 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
+
 gem 'devise'
+gem 'bootstrap'
+gem 'jquery-rails'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,10 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.addEventListener('keydown', function(e) {
-    if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
-      if (e.target.nodeName == 'INPUT' && e.target.type == 'text') {
-        e.preventDefault();
-        return false;
-      }
-    }
-  }, true);
-})
+$(document).ready(function() {
+  $( "[id^=post_edit]" ).click(function() {
+
+    // get id from div above
+    let post_id = this.id.split("_");
+    let post_edit_url = "/posts/" + post_id[2]
+    //update modal form to new path
+  $("#editForm").attr("action",post_edit_url)
+
+   });
+
+     document.addEventListener('keydown', function(e) {
+       if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
+         if (e.target.nodeName == 'INPUT' && e.target.type == 'text') {
+           e.preventDefault();
+           return false;
+         }
+       }
+     }, true);
+});
