e57e27393ca583d4f9b94e343c40d78d2375cace
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-12-09 15:02:34 +0000

    tidy up formatting

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -5,17 +5,17 @@ $(document).ready(function() {
     let post_div_id = "#post_text_" + post_id[2];
     let post_edit_url = "/posts/" + post_id[2] + "/edit"
     //update modal form to new path and prepopulate wih exisiting value
-  $("#editForm").attr("action",post_edit_url)
-  $("#editTextField").attr("value",$(post_div_id).text())
+    $("#editForm").attr("action",post_edit_url)
+    $("#editTextField").attr("value",$(post_div_id).text())
 
-   });
+  });
 
-     document.addEventListener('keydown', function(e) {
-       if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
-         if (e.target.nodeName == 'INPUT' && e.target.type == 'text') {
-           e.preventDefault();
-           return false;
-         }
-       }
-     }, true);
+  document.addEventListener('keydown', function(e) {
+    if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
+      if (e.target.nodeName == 'INPUT' && e.target.type == 'text') {
+        e.preventDefault();
+        return false;
+      }
+    }
+  }, true);
 });
