dd1064bab33248840243e874f0bf02b8a57720ac
ellieSMASH <eliseaston@gmail.com>
2019-12-09 12:37:44 +0000

    remove jquery. user story complete (with redirect)

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,11 +1,10 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
-
-$(document).ready(function() {
-
-  $('.like-button').on('click', function() {
-    // console.log(this.parent)
-    $(document).find('.like-count').text('<%= post.likes.count %>');
-  })
-})
+// // Place all the behaviors and hooks related to the matching controller here.
+// // All this logic will automatically be available in application.js.
+//
+//
+// $(document).ready(function() {
+//
+//   $('.like-button').on('click', function() {
+//     $(document).find('.like-count').html('<%= post.likes.count %>');
+//   })
+// })
