953a0d1bece6ea536a9e1c825f0e58bc3f1580e2
Ed Courage <edcourage@live.com>
2019-12-09 13:27:35 +0000

    10 min update functionality complete

--- spec/features/users_can_edit_posts_spec.rb
+++ spec/features/users_can_edit_posts_spec.rb
@@ -12,4 +12,20 @@ RSpec.feature "Edit/update posts", type: :feature do
     page.refresh
     expect(page).to_not have_link("Update")
   end
+
+  scenario "redirected to posts page if you try to access edit url after 10 mins" do
+    sign_up
+    visit "/posts"
+    click_link "New Post"
+    post_creation_time = Time.now - 650
+    Timecop.freeze(post_creation_time)
+      fill_in "Message", with: "Hello, world!"
+      click_button "Submit"
+    Timecop.return # "turn off" Timecop
+    url_id = Post.find_by(message: "Hello, world!").id
+    visit "/posts/#{url_id}/edit"
+    expect(current_url).to eq("http://www.example.com/posts")
+  end
+
+
 end
