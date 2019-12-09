f42a87b5a19e7e03df1a4ce9d2777630f0d32f4d
ellieSMASH <eliseaston@gmail.com>
2019-12-09 10:53:19 +0000

    redirect to refresh page on like and unlike

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,14 @@
 class LikesController < ApplicationController
    before_action :find_post
    before_action :find_like, only: [:destroy]
+
    def create
      if already_liked?
        flash[:notice] = "You can't like more than once"
      else
        @post.likes.create(user_id: current_user.id)
      end
+     redirect_to post_path(@post)
    end
 
    def destroy
@@ -15,8 +17,11 @@ class LikesController < ApplicationController
     else
       @like.destroy
     end
+    redirect_to post_path(@post)
   end
+
   private
+
   def find_post
     @post = Post.find(params[:post_id])
   end
@@ -29,4 +34,5 @@ class LikesController < ApplicationController
     Like.where(user_id: current_user.id, post_id:
     params[:post_id]).exists?
   end
+
 end
