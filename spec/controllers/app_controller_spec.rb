7ea704286096b39b9a41f503b172c9bb694f0477
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-12-09 12:03:53 +0000

    fixed deliberate code error

--- spec/controllers/app_controller_spec.rb
+++ spec/controllers/app_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ApplicationController, type: :controller do
   describe 'user session' do
     it 'redirects to home page' do
       :authenticate_user!
-      expect(response).to have_http_status(500)
+      expect(response).to have_http_status(200)
     end
   end
 end
