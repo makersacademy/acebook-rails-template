468578851d77a6c78e39eedb22e116f2e8408742
Robert Woolley <rob@robertwoolley.co.uk>
2019-12-09 15:17:24 +0000

    Changes to rails_helper.rb in spec - trying to get chrome to pass in Travis CI

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,8 +41,11 @@ RSpec.configure do |config|
   # It makes it use the chrome browser, but can also be
   #  configured to user Firefox, etc.
   Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, browser: :chrome)
+    options = Selenium::WebDriver::Chrome::Options.new(args: %w[no-sandbox headless disable-gpu])
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
   end
+  
+  Capybara.javascript_driver = :selenium
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
