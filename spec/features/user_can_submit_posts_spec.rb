2cab32d6ea82ac25fff99db9d5bf7355a8da98db
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-12-09 15:39:19 +0000

    1 more user test passing

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,18 +2,9 @@ require 'rails_helper'
 
 RSpec.feature "Post", type: :feature do
   scenario "Can submit posts and view them" do
-<<<<<<< HEAD
-    visit "/"
-    fill_in "post_username", with: "Alistair Phipps"
-    fill_in "post_message", with: "Hello, world!"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
-    expect(page).to have_content("Alistair Phipps")
-=======
+
     sign_up_user
     make_post
->>>>>>> 8250293df34bd888b32b8a587f89f0415bc08534
-
     expect(page).to have_css("ul", text: "Beep beep")
     expect(page).to have_css("ul", text: "Robot")
   end
