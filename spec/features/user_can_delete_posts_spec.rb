b6b67b01487dc0a7a66801b603688738e6712223
Sara <srancati0@gmail.com>
2019-12-10 12:49:27 +0000

    refactored posts controller tests

--- spec/features/user_can_delete_posts_spec.rb
+++ spec/features/user_can_delete_posts_spec.rb
@@ -7,21 +7,24 @@ RSpec.feature "Timeline", type: :feature do
 
   scenario "Can submit posts and view them" do
     sign_up_and_go_to_new_post
-    fill_in "Message", with: "Hello, world!"
+    fill_in "Message", with: "Hello, world!!"
     click_button "Submit"
-    click_link "Delete"
+    post = Post.find_by(message: "Hello, world!!" )
+    find("#post_#{post.id}").click_link "Delete"
     expect(page).to have_css('.header', text: "Your post has been deleted")
-    expect(page).not_to have_content("Hello, world!")
+    expect(page).not_to have_content("Hello, world!!")
   end
 
   scenario "cannot delete other people's posts" do
     sign_up_and_go_to_new_post
-    fill_in "Message", with: "Hello, world!"
+    fill_in "Message", with: "Hello, world!!!"
     click_button "Submit"
     click_on "navdropdown"
     click_on "Sign out"
     sign_up_second_user
-    click_link "Delete"
+    post = Post.find_by(message: "Hello, world!!!" )
+    find("#post_#{post.id}").click_link "Delete"
     expect(page).to have_css('.header', text: "You cannot delete other people's posts")
+    expect(page).to have_css('.post', text: "Hello, world!!!")
   end
 end
--- spec/features/users_can_edit_posts_spec.rb
+++ spec/features/users_can_edit_posts_spec.rb
@@ -30,7 +30,8 @@ RSpec.feature "Edit/update posts", type: :feature do
     sign_up_and_go_to_new_post
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
-    click_link "Update"
+    post = Post.find_by(message: "Hello, world!" )
+    find("#post_#{post.id}").click_link "Update"
     fill_in "Message", with: "Hi, there!"
     click_button "Submit"
     expect(page).not_to have_content("Hello, world!")
@@ -40,14 +41,15 @@ RSpec.feature "Edit/update posts", type: :feature do
 
   scenario "cannot edit other people's posts" do
     sign_up_and_go_to_new_post
-    fill_in "Message", with: "Hello, world!"
+    fill_in "Message", with: "Hello, this is my first post"
     click_button "Submit"
     click_on "navdropdown"
     click_on "Sign out"
     sign_up_second_user
-    click_link "Update"
+    post = Post.find_by(message: "Hello, this is my first post" )
+    find("#post_#{post.id}").click_link "Update"
     expect(page).to have_css('.header', text: "You cannot update other people's posts")
-    expect(page).to have_css('.post', text: "Hello, world!")
+    expect(page).to have_css('.post', text: "Hello, this is my first post")
   end
 
 end
