58fcdd567ec68a866b682d944164f9008bc9a3c3
Amy Balmforth <amy.balmforth@gmail.com>
2019-12-10 16:43:33 +0000

    signing in takes you to users wall

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'welcome/index'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
