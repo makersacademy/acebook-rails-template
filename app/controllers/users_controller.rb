2d2fd628c3f950f19fbecf2e3d491ccee9644738
Amy Balmforth <amy.balmforth@gmail.com>
2019-12-10 16:26:31 +0000

    progress on user walls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,16 @@ class UsersController < ApplicationController
     @user = User.find_by(id: current_user.id)
   end
 
+  def users
+    redirect_to user_path(current_user)
+  end
+
   def show
-    @user = User.find(params[:id])
+    if params[:id] == nil
+      @user = current_user
+    else
+      @user = User.find(params[:id])
+    end
     @userposts = Post.where(user_id: @user.id)
   end
 
