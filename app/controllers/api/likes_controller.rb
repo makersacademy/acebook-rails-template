b01992c7a95cdbae292874a1e211c0928bb1b403
Topher <topherb99@gmail.com>
2019-12-10 17:00:31 +0000

    aligned to left

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -24,4 +24,10 @@ class Api::LikesController < ApplicationController
     end
   end
 
+  private
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id:
+    params[:post_id]).exists?
+  end
 end
