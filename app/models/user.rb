e389f96387018d563ba804c30b6be580b6854a24
Ed Courage <edcourage@live.com>
2019-12-11 14:22:36 +0000

    controller has now been refactored.. happy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
-  has_many :comments, through: :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
 end
