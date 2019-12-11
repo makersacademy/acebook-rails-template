70a52ad59d698ea0d31ff9edef995da093adb037
elfi yang <elfiyang16@gmail.com>
2019-12-11 10:41:31 +0000

    nest user and post

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts do
-    # get '/:user_id', on: :collection, action: :create_wall
-    # , defaults: { user_id: true}
-    member do
-      get 'wall_create'
+
+  resources :users do
+    resources :posts, shallow:true
   end
- end
+ #  resources :posts do
+ #    # get '/:user_id', on: :collection, action: :create_wall
+ #    # , defaults: { user_id: true}
+ #    member do
+ #      get 'wall_create'
+ #  end
+ # end
 # resources :posts, only: :create_wall, param: :user_id
 
-
-controller 'posts' do
-  get '/:user_id'=> "posts#create_wall"
-end
+#
+# controller 'posts' do
+#   get '/:user_id'=> "posts#create_wall"
+# end
 
 
 
