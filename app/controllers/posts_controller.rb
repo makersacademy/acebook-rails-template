2a3aba9af5ae8098846b0d6d5032e75545e7fb31
Natalie <49326857+natyeo@users.noreply.github.com>
2019-12-12 15:08:57 +0000

    created helper for post older than 10 mins

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
   def update
   @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to @post
+      redirect_to posts_newsfeed_path
     else
       render 'edit'
     end
--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -2,4 +2,16 @@ module PostHelper
   def keep_line_break(post)
     post.message.gsub("\r\n", '<br/>').html_safe
   end
-end 
+
+  def check_if_liked(post, user)
+    if user.liked? post
+      'unlike'
+    else
+      'like'
+    end
+  end
+
+  def older_than_10_mins?(post)
+    Time.now - post.created_at < 600
+  end
+end
