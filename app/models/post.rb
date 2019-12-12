60029551b5860ededed3e8d96e14ffc9c2bf8a18
Natalie <49326857+natyeo@users.noreply.github.com>
2019-12-12 12:33:06 +0000

    refactored date parsing logic to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,15 @@
 class Post < ApplicationRecord
   # before_action :authenticate_user!
   acts_as_votable
-  belongs_to :user 
+  belongs_to :user
   has_many :comments
 
   def parse_date()
-    day = Date.new(self.created_at.wday).strftime("%a")
-    time_array = (self.created_at.to_s.split(" ")[1]).split(":")
+    date_array = self.created_at.to_s.split(' ')
+    date = Date.parse(date_array[0]).strftime('%A')
+    time_array = date_array[1].split(":")
     time_string = "#{time_array[0]}:#{time_array[1]}"
-    date_string = "#{day} at #{time_string}"
+    date_string = "#{date} at #{time_string}"
   end
-  
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it { is_expected.to be }
+  let(:post) { Post.new(message: 'Hello, world', created_at: '2019-12-12 11:53:11.7426', updated_at: '2019-12-12 11:53:11.7426') }
+
+  describe '#parse_date' do
+    it 'returns Thursday at 11:53 when called on a post that was created on Thursday at 11:53' do
+      expect(post.parse_date).to eq 'Thursday at 11:53'
+    end
+  end
 end
