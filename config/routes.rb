000798a97e8978e3f2abb713c038e8a4cf36b311
Rahul Shah <rahulshah7@outlook.com>
2019-12-16 14:06:14 +0000

    Update logo name and use rails link_to helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'posts#index'
 
   resources :posts
 end
