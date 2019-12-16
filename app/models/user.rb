1d8b3385d6c486014800e4aea8d3c618711c2235
Sam King <samuelking94@gmail.com>
2019-12-16 15:46:42 +0000

    Add empty line

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 # Manages users
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
