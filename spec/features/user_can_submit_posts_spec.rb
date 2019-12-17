ae6b7bab604d302e509305dd6e5c2066b8caadaa
Rahul Shah <rahulshah7@outlook.com>
2019-12-17 12:00:38 +0000

    Replace delete all users test helper sql with activerecord methodCo-authored-by: Sam King <samuelking94@gmail.com>

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,13 +1,17 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require_relative './login_helper'
 
-RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and view them" do
+RSpec.feature 'Timeline', type: :feature do
+  scenario ''
+
+  scenario 'Can submit posts and view them' do
     sign_up_user
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+    expect(page).to have_content('Hello, world!')
   end
 end
