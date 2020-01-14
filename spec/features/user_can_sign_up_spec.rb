b0cae801515bbd39fd44c880727351f085234b30
dbacall <dbacall@hotmail.co.uk>
2020-01-14 14:43:58 +0000

    use clearance gem for user sign up, sign in and authentication

---
+++ spec/features/user_can_sign_up_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "Sign up", type: :feature do
+  scenario "Can create an account" do
+    visit "/"
+    click_on "Sign in"
+    click_on "Sign up"
+    fill_in "First name", with: "David"
+    fill_in "Surname", with: "Bacall"
+    fill_in "Email", with: "dbacall@hotmail.co.uk"
+    fill_in "Password", with: "password"
+    within ".submit-field" do
+      click_on "Sign up"
+    end
+    expect(page).to have_content("Hello David!")
+  end
+end
