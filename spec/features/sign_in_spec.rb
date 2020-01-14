287333b7837dcb068ca8ae60dc2cd0b9fc453e75
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-01-14 12:03:29 +0000

    sign in, sign out, authentication and feature test added

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,11 @@
+feature 'sign in' do 
+  scenario ' user enters wrong username' do
+    visit '/sign_in'
+    fill_in 'session_email', with: 'daniel@test.com'
+    fill_in 'session_password', with: 'p1'
+    click_button 'Sign in'
+
+    expect(page).to have_content 'Bad email or password'
+    expect(page).to have_current_path '/session'
+  end 
+end 
