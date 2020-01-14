21969269c7fc655b73b1737f08fd2bc9b42f9592
Julie <j49411@gmail.com>
2020-01-14 18:09:25 +0000

    comment out welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :user
   # resources :sessions
 
-  # root 'welcome#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
