b4aefc7ce6b70b5ccbf10b898736bb1cacd97129
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-14 16:01:59 +0000

    chore: setting up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: 'posts#index'
+  root to: 'users#sign_up'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
+
 end
