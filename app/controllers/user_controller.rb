3ec76793d656d48d5943c227fb2c1f3cb4119e20
Julie <j49411@gmail.com>
2020-01-14 17:50:00 +0000

    add signup and login

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/posts', notice => "Signed up!"
+    else
+      redirect_to '/posts'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  get 'signup' => 'user#new'  
+  post 'signup' => 'user#create'  
+  resources :users
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'posts' => 'posts#index'
+  # root :to => 'user#new'
+  resources :user
+  # resources :sessions
+
+  # root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
