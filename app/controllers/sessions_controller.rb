af7c4287d3be255b5cc0eeaa4df4935ffe46771c
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-15 16:35:32 +0000

    Sign up and sign in features working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,21 +2,26 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def show
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+
   def create
     @user = User.find_by_email(params[:session][:email])
     
-    if @user&.authenticate(params[:session][:password])
+    if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to '/posts', notice => "Logged In!"
     else
-    # flash.now.alert = "Invalid email or password"
+      flash.now.alert = "Invalid email or password"
       redirect_to '/login'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to '/login'
   end
   
 end
