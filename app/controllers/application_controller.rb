b5396f2f4cb4354b59c6f40cac093c8b0832a2ed
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-15 11:59:41 +0000

    Rubocop and all tests passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
-  
+
   def current_user
-	  @current_user ||= User.find(session[:user_id]) if session[:user_id]
-	end
-  
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
   def require_user
-	  redirect_to '/login' unless current_user
-	end
-
+    redirect_to '/login' unless current_user
+  end
 end
-
