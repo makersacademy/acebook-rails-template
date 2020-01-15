8eefbbe3c14e549f20e5cdb9d3ddd12c565856ff
Jamie95187 <jamiewong63@googlemail.com>
2020-01-15 11:31:15 +0000

    exempt files from test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,10 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console
 ])
 SimpleCov.start 'rails' do
+  add_filter "app/mailers/application_mailer.rb"
+  add_filter "app/channels/application_cable/connection.rb"
+  add_filter "app/jobs/application_job.rb"
+  add_filter "app/channels/application_cable/channel.rb"
 end
 
 RSpec.configure do |config|
