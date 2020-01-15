7981532191fa74b205ca5938df0c27884e38b779
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-01-15 12:27:17 +0000

    chore: configuring tests, incomplete

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,28 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  login_user
   describe "GET /new " do
-    xit "responds with 200" do
-      post :new, params: { user: { email: "test_user1@test.com", password: "pass1234", password_confirmation: "pass1234" } }
+    it "responds with 200" do
+      
       get :new
       expect(response).to have_http_status(200)
     end
   end
 
   describe "POST /" do
-    xit "responds with 200" do
+    it "responds with 200" do
       post :create, params: { post: { message: "Hello, world!" } }
       expect(response).to redirect_to(posts_url)
     end
 
-    xit "creates a post" do
+    it "creates a post" do
       post :create, params: { post: { message: "Hello, world!" } }
       expect(Post.find_by(message: "Hello, world!")).to be
     end
   end
 
   describe "GET /" do
-    xit "responds with 200" do
+    it "responds with 200" do
       get :index
       expect(response).to have_http_status(200)
     end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'devise'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -6,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require_relative 'helpers/sign_up_sign_in_helper'
+require_relative 'support/controller_macros'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -30,6 +32,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.extend ControllerMacros, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
