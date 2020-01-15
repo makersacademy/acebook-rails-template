f932dc7b8f55550ca3d96e8e3620bd5574e47a3c
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-01-15 17:38:14 +0000

    feat: posts show their author

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    sign_up("email@test.com", "pass12", "pass12")
+    sign_up("email@example.com", "pass12", "pass12")
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     post_time = Time.now
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
     expect(page).to have_content("Date posted: #{post_time.strftime('%d %B %Y at %I:%M %p')}")
+    expect(page).to have_content("Posted by email@example.com")
   end
 
   scenario "posts are ordered reverse chronologically" do
-    sign_up("email@test.com", "pass12", "pass12")
+    sign_up("email@example.com", "pass12", "pass12")
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
