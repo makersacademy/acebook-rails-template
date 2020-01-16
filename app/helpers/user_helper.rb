f9ed22a9b40be7ffe65a9d17a89aa2b39776c26b
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-16 12:04:13 +0000

    Remove user helpers

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,18 +1,3 @@
  module UserHelper
-#   def signup
-#     visit "/signup"
-#     fill_in "Name", with: "Ben"
-#     fill_in "Email", with: "ben@gmail.com"
-#     fill_in "Password", with: "1234"
-#     fill_in "Confirm Password", with: "1234"
-#     click_button "Sign up"
-#   end
-
-#   def login
-#     visit "/login"
-#     fill_in "Email", with: "ben@gmail.com"
-#     fill_in "Password", with: "1234"
-#     click_button "Log in"
-#   end
  end
  
