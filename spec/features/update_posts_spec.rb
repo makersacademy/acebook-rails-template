ed3b01996d4c5539bc41030b6c3e176591d61a25
Student <student@Makerss-MacBook-Air.local>
2020-01-16 12:55:21 +0000

    fixed uupdate text, 92% coverage

--- spec/features/update_posts_spec.rb
+++ spec/features/update_posts_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.feature "Update", type: :feature do
   scenario "Can update posts they have created" do
+    visit "/signup"
+    fill_in "Name", with: "Ben"
+    fill_in "Email", with: "ben@gmail.com"
+    fill_in "Password", with: "1234"
+    fill_in "Confirm Password", with: "1234"
+    click_button "Sign up"
+    visit "/login"
+    fill_in "Email", with: "ben@gmail.com"
+    fill_in "Password", with: "1234"
+    click_button "Log in"
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
