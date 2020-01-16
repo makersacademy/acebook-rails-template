3d0921723e5f3767b2441ca251113bdb0c0f5c0d
Student <student@Makerss-MacBook-Air.local>
2020-01-16 13:52:13 +0000

    homepage is now signup

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :user
   resources :sessions
 
-  root 'welcome#index'
+  root 'user#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
