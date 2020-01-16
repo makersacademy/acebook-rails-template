9e5400fa05d6c7939e36f675a9fe6b33ccdc41cb
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-01-16 13:48:19 +0000

    test: add feature tests to sign in

---
+++ spec/features/user_singin_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "User Sign In", type: :feature do
+
+  before do
+    sign_up("email@example.com", "pass12", "pass12")
+    click_link("Sign out")
+    click_link("Log in")
+  end
+
+  scenario "displays a confirmation message when a user signs in" do
+    fill_in "user_email", with: "email@example.com"
+    fill_in "user_password", with: "pass12"
+    click_button("Log in")
+    expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_current_path('/')
+  end
+
+  scenario "displays an error message when the email address is incorrect" do
+    fill_in "user_email", with: "email@"
+    fill_in "user_password", with: "pass12"
+    click_button("Log in")
+    expect(page).to have_content("Invalid Email or password.")
+    expect(page).to have_current_path('/users/sign_in')
+  end
+
+  scenario "displays an error message when the password is incorrect" do
+    fill_in "user_email", with: "email@example.com"
+    fill_in "user_password", with: "pass1245"
+    click_button("Log in")
+    expect(page).to have_content("Invalid Email or password.")
+    expect(page).to have_current_path('/users/sign_in')
+  end
+
+end
