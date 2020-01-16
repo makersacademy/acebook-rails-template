a2458293655af380c007a03cbb0e98285b1651e0
Julie <j49411@gmail.com>
2020-01-16 15:24:44 +0000

    Add feature tests for all the flashes

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -6,4 +6,22 @@ RSpec.feature "Login", type: :feature do
     login
     expect(page).to have_content("New post")
   end
+
+  scenario "When a user enters invalid email" do
+    signup
+    visit "/login"
+    fill_in "Email", with: "ken@gmail.com"
+    fill_in "Password", with: "123456"
+    click_button "Log in"
+    expect(page).to have_content("Invalid email/password combination")
+  end
+
+  scenario "When a user enters invalid password" do
+    signup
+    visit "/login"
+    fill_in "Email", with: "ben@gmail.com"
+    fill_in "Password", with: "111111"
+    click_button "Log in"
+    expect(page).to have_content("Invalid email/password combination")
+  end
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -10,4 +10,26 @@ RSpec.feature "Sign up", type: :feature do
     click_button "Sign up"
     expect(page).to have_content("New post")
   end
+
+  scenario "When a registered user tries to sign up" do
+    signup
+    click_on "Logout"
+    visit "/signup"
+    fill_in "Name", with: "Ben"
+    fill_in "Email", with: "ben@gmail.com"
+    fill_in "Password", with: "123456"
+    fill_in "Confirm Password", with: "123456"
+    click_button "Sign up"
+    expect(page).to have_content("Already a user, please log in")
+  end
+  
+  scenario "When a user enters invalid password" do
+    visit "/signup"
+    fill_in "Name", with: "Ben"
+    fill_in "Email", with: "ben@gmail.com"
+    fill_in "Password", with: "1222"
+    fill_in "Confirm Password", with: "1222"
+    click_button "Sign up"
+    expect(page).to have_content("Password must be at least 6 characters and no more than 10")
+  end
 end
