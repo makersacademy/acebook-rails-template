650302af5fb6ae0970f697470dc95ce99c50793c
Hyan18 <Hyan18@github.com>
2020-01-16 14:57:42 +0000

    add test for updating post which you did not create

--- spec/features/update_posts_spec.rb
+++ spec/features/update_posts_spec.rb
@@ -14,3 +14,24 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to have_content("Updated message")
   end
 end
+
+RSpec.feature "Timeline", type: :feature do
+  scenario "Cannot update posts they did not create" do
+    signup
+    expect(current_path).to eq("/posts")
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(current_path).to eq("/posts")
+
+    visit "/signup"
+    fill_in "Name", with: "Dawid"
+    fill_in "Email", with: "dawid@gmail.com"
+    fill_in "Password", with: "password123"
+    fill_in "Confirm Password", with: "password123"
+    click_button "Sign up"
+
+    expect(current_path).to eq("/posts")
+    expect(page).to_not have_content("Edit")
+  end
+end
