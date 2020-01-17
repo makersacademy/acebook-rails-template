7f3ba8066bcde15bb324418118b8478f2878296e
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-01-17 16:03:50 +0000

    chore: Established recipient ID in posts db, ready for walls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200117142131) do
+ActiveRecord::Schema.define(version: 20200117154912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20200117142131) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "recipient_id"
+    t.index ["recipient_id"], name: "index_posts_on_recipient_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -43,6 +45,7 @@ ActiveRecord::Schema.define(version: 20200117142131) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "posts", "users", column: "recipient_id"
   add_foreign_key "walls", "posts"
   add_foreign_key "walls", "users"
 end
