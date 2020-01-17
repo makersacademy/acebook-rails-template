7de09c7b85e37d674f2e1c8470949c0f3bef6730
Student <student@Makerss-MacBook-Air.local>
2020-01-17 11:15:03 +0000

    posts display owners name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :log_in, :logged_in
+  helper_method :current_user, :log_in, :logged_in, :find_user
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
