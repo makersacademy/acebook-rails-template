d0e31d1fe07e75d485df2053f6f9678a5a29eadc
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-17 16:19:31 +0000

    Delete session store

--- config/initializers/session_store.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_acebook_november2019", domain: "acebook-november2019.herokuapp.com/"
-else
-  Rails.application.config.session_store :cookie_store, key: "_acebook_november2019"
-end
