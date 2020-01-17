3663a435cf22c4c90530d449a106d54c02d725c5
Hyan18 <Hyan18@github.com>
2020-01-17 10:33:46 +0000

    fix heroku session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  Rails.application.config.session_store :cookie_store, key: "_acebook_app", domain: "acebook.herouapp.com"
+  Rails.application.config.session_store :cookie_store, key: ENV["SECRET_KEY_BASE"], domain: "acebook.herouapp.com"
 else
   Rails.application.config.session_store :cookie_store, key: "_acebook.app"
 end
