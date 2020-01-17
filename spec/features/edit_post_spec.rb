da8e1ca4f913ba18cbcaa1aa6467d0a5c6f8ba3d
Daniel <demartinezgatell@gmail.com>
2020-01-17 12:21:16 +0000

    passing test for editing post with time limit

--- spec/features/edit_post_spec.rb
+++ spec/features/edit_post_spec.rb
@@ -11,10 +11,13 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to have_content('Updated message')
   end
 
-  # scenario 'unable to edit post more than 10 minutes after creation' do
-  #   sign_up
-  #   new_post
-  #   time_of_creation = Time.now
-  #   time_of_editing = Time.now + 11
-  #   expect(page).not_to have_content()
+  scenario 'unable to edit post more than 10 minutes after creation' do
+    sign_up
+    new_post
+    Timecop.freeze(Time.now + 660)
+    click_on 'Edit'
+    fill_in "post[message]", with: 'Updated message'
+    click_on 'Update Post'
+    expect(page).to have_content("Unable to edit post over 10 mins after creation")
+  end
 end
