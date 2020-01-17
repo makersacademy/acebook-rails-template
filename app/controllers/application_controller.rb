c7145b5310c62201cfeaf754d1b23cdf2642dd62
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-17 10:22:32 +0000

    Fix session store

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  skip_before_action :verify_authenticity_token
   helper_method :current_user, :log_in, :logged_in
 
   def current_user
