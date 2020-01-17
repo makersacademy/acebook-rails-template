7eaa096b170594595f8126ad23c67af29685544d
Daniel <demartinezgatell@gmail.com>
2020-01-17 12:03:17 +0000

    spike for time limit complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,10 +18,15 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
 
-    if @post.update(post_params)
-      redirect_to posts_url
+    if Time.now.utc < (@post.created_at.utc + 10.minutes).utc
+      if @post.update(post_params)
+        redirect_to posts_url
+      else
+        render 'edit'
+      end
+
     else
-      render 'edit'
+      redirect_to posts_path, flash: { error: "Unable to edit post over 10 mins after creation" }
     end
 
   end
@@ -35,4 +40,5 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:message)
   end
+
 end
