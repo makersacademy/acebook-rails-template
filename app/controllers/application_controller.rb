eea11bc6c613199e7ca461cf5cc0860b801dfc7c
Hyan18 <Hyan18@github.com>
2020-01-18 14:06:27 +0000

    change date format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
   private
 
   def require_login
