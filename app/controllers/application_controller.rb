d30e1c001c296f4c65e60c84fa4f871389ab4262
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-20 17:17:01 +0000

    Add redirect routes to user wall

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   skip_before_action :verify_authenticity_token
-  helper_method :current_user, :find_user
+
+  helper_method :current_user, :find_user, :require_login
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
   private
 
   def require_login
     redirect_to root_path unless current_user
   end
-
 end
