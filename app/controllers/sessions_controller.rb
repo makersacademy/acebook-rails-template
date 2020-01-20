76a0a37137a3c3c62258dd6a7faa02e06a2870c1
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-20 11:41:14 +0000

    Cleaning up sessions controller and view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,23 +2,14 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def index
-    if session[:user_id]
-      redirect_to '/posts'
-    else
-      session.clear
-      redirect_to '/login'
-    end
-  end
-
   def show
     session[:user_id] = nil
     redirect_to '/login'
   end
 
   def create
-    @user = User.find_by_email(params[:session][:email])
-    if @user&.authenticate(params[:session][:password])
+    @user = User.find_by_email(params[:user][:email])
+    if @user&.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to '/posts' 
     else
@@ -30,10 +21,4 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to '/login', notice: 'Logged out!' if session[:user_id].nil?
   end
-
-  # def notice
-  #   redirect_to '/login', notice: 'Logged out!'
-  # end
-  
-   
 end
