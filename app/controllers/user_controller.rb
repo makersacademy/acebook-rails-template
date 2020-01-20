8be2c2305254692c43047b1f0bf99c7da8fd15b5
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-20 17:28:33 +0000

    Comment out extra conditional statement

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,8 +11,8 @@ class UserController < ApplicationController
       redirect_to '/signup', notice: "Password must be at least 6 characters and no more than 10"
     elsif params[:user][:password] != params[:user][:password_confirmation]
       redirect_to '/signup', notice: "Please confirm your password again"
-    elsif params[:user][:name].empty?
-      redirect_to '/signup', notice: "Name field cannot be blank"
+    # elsif params[:user][:name].empty?
+    #   redirect_to '/signup', notice: "Name field cannot be blank"
     elsif @user.save
       @user = User.find_by_email(params[:user][:email])
       session[:user_id] = @user.id
