58fc5c4441f08764b98ba04126de43db7726347d
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-20 17:17:26 +0000

    Rewriting tests

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,45 +2,18 @@ require 'rails_helper'
 
 RSpec.describe UserController, type: :controller do
 
-  describe 'Posts' do
-    describe "GET posts#index" do
-      context "when the user is logged in" do
-        it "should render posts#index" do
-        end
-      end
+  describe 'POST #create' do
+    it 'creates a user and redirects them to the posts/wall page' do
+      post :create, params: { user: { name: 'Ben', email: 'ben@example.com', password: "123456", password_confirmation: "123456" } }
+      expect(User.find_by({ email: 'ben@example.com' })).to be
+      expect(response).to redirect_to('/posts')
+    # expect(response).to redirect_to('/wall/Ben')
+    end
 
-      context "when the user is logged out" do
-        it "should redirect to the login page" do
-        end
-      end
+    it 'redirects back to users/new if no name is provided' do
+      post :create, params: { user: { name: '', email: 'ben@example.com', password: "123456", password_confirmation: "123456" } }
+      expect(User.find_by({ email: 'ben@example.com' })).not_to be
+      expect(response).to redirect_to('/signup')
     end
   end
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  # describe "create" do
-  #   it "creates a user" do
-  #     get :new
-  #     post :create, params: { name: "Ben", email: "ben@gmail.com", password: "1234", password_confirmation: "1234"}
-  #     expect(User.find_by(email: "ben@gmail.com")).to be
-  #   end
-  # end
-
-  # describe "POST /signup" do
-  #   it "creates a user and redirects to home page" do
-  #     post :create, params: { name: "Ben", email: "ben@gmail.com", password: "1234"}
-  #     expect(response).to redirect_to('/posts')
-  #   end
-
-  #   it "creates a user and stores in database" do
-  #     post :create, params: { name: "Ben", email: "ben@gmail.com", password: "1234", password_confirmation: "1234"}
-  #     expect(User.find_by(email: "ben@gmail.com")).to be
-  #   end
-  # end
-
 end
