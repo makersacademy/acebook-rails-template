834d46462959676755bc322c7149f1c0e8d0a2e4
dbacall <dbacall@hotmail.co.uk>
2020-01-21 10:30:21 +0000

    test: create tests on sign in and sign up error messages

--- spec/features/authentification/sign_in_spec.rb
+++ spec/features/authentification/sign_in_spec.rb
@@ -7,4 +7,11 @@ RSpec.feature "sign in" do
     sign_in("harry_mumford@hotmail.co.uk", "password")
     expect(page).to have_content("Feed")
   end
+
+  scenario "it gives an error if password wrong" do
+    sign_up('Harry', 'Mumford', 'harry_mumford@hotmail.co.uk', 'password')
+    click_on 'Sign out'
+    sign_in("harry_mumford@hotmail.co.uk", "passwor")
+    expect(page).to have_content("Bad email or password")
+  end
 end
--- spec/features/authentification/user_sign_up_spec.rb
+++ spec/features/authentification/user_sign_up_spec.rb
@@ -12,4 +12,8 @@ RSpec.feature "Sign up", type: :feature do
     expect(page).to have_content "Password must be 6-10 characters!"
   end
 
+  scenario "No name sends you back to sign up page" do
+    sign_up("", "Bacall", "dbacallhotmail.co.uk", "password")
+    expect(page.current_path).to eq '/users'
+  end
 end
