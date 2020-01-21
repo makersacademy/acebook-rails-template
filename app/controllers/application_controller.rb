b06dc0a130349b272c583104e306eea96fb7f3b3
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-01-21 13:04:35 +0000

    chore: change user wall url from /users/:id to /:id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(user)
-    user_path(user)
+    user_wall_path(user)
   end
 
 end
