4be0cbd9946fb91f11cfb7be8ba6dfcbcad13471
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-21 17:48:22 +0000

    chore: list only available users to friend request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,17 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    # this could be better!!!
+
+    # get the list of users
+    @users = User.all 
+    # get this list of friend relations
+    @friendships = Friendship.all
+    # find friendship objects where user_id == current_user and convert objects array to array of friend id's
+    user_friends = @friendships.select{|f| current_user.id == f.user_id }.map{|e| e.friend_id}
+    # find users where they are not the current user and not already friends of the current user
+    @users = @users.select{|u| user_friends.exclude?(u.id) && current_user.id != u.id }
+
   end  
 
 end
