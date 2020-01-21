a014862ca9b087942accf627401adf4a79df4101
Raluca Ciucu <raluca.ciucu@gmail.com>
2020-01-21 14:39:47 +0000

    feat: create friendship function successful plus redirect and popup

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,12 +1,13 @@
 class FriendshipsController < ApplicationController
 
   def create
-    @friendship = current_user.friendships.create(post_params)
+    @friendship = current_user.friendships.create(:friend_id => params[:friend_id])
     if @friendship
       flash[:notice] = "Added friend."
     else
       flash[:notice] = "Unable to add friend."
     end
+    redirect_to users_path
   end
 
 
