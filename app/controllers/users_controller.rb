258f790c0cd674fc4e72e8d03b1ed5a8ce72d482
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-22 11:29:06 +0000

    test: on clicking add friend, user moves to friend list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,25 +10,8 @@ class UsersController < ApplicationController
   end
 
   def index
-    p session[:recipient_id]
     user = User.find(session[:recipient_id])
     @friends = user.friends + user.inverse_friends
     @non_friends = User.all - @friends - [user]
-
-    # p "user ctrllr index"
-    # print 'friends: '
-    # p current_user.friends
-    # print 'inverse friends: '
-    # p current_user.inverse_friends
-    # get the list of users
-    # @users = User.all 
-    # # get this list of friend relations
-    # @friendships = Friendship.all
-    # # find friendship objects where user_id == current_user and convert objects array to array of friend id's
-    # user_friends = @friendships.select{|f| current_user.id == f.user_id }.map{|e| e.friend_id}
-    # # find users where they are not the current user and not already friends of the current user
-    # @users = @users.select{|u| user_friends.exclude?(u.id) && current_user.id != u.id }
-
-    
   end  
 end
--- spec/features/user_can_add_friend_via_own_wall_spec.rb
+++ spec/features/user_can_add_friend_via_own_wall_spec.rb
@@ -13,11 +13,15 @@ RSpec.feature "Timeline", type: :feature do
         click_link "Add friends"
 
         expect(page).to have_current_path "/users"
+        expect(page).to have_css('div.non-friend')
+        expect(page).to have_no_css("div.friend")
         expect(page).to have_content("test@example.com")
         expect(page).to have_link("Add friend")
-        expect(page).not_to have_content("test@example.com")
-        expect(page).not_to have_link("Add friend")
 
+        click_link "Add friend"
+
+        expect(page).to have_no_css("div.non-friend")
+        expect(page).to have_css("div.friend")
     end
 
 
