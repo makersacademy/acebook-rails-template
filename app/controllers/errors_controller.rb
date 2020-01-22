6d6ec458798d638cf7bae3630850f33889464349
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-22 19:01:30 +0000

    Cut down to only the index route

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,7 @@
 class ErrorsController < ApplicationController
-
-  def show
-    status_code = 500
+  def index
+    status_code = 404
     flash.alert = "Status #{status_code}"
     render status_code.to_s, status: status_code
   end
-
 end
