ce4e245c3018cbd72ae82306f675794cb3aa7954
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-01-22 15:41:34 +0000

    Test: Fix comments controller tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe CommentsController, type: :controller do
 
   let(:user) { create(:user) }
+  let(:postA) { create(:post, :user_id => user.id, :recipient_id => user.id, :message => "Beep boop") }
 
   before(:each) do
     sign_in user
@@ -18,13 +19,13 @@ RSpec.describe CommentsController, type: :controller do
   describe "POST /comments" do
     it "responds with 200" do
 
-      post :create, params: { post: { message: "Excellent post!", post_id: 2 } }
+      post :create, params: { comment: { message: "Excellent post!", post_id: postA.id } }
       expect(response).to redirect_to(root_path)
     end
 
-    it "creates a post" do
+    it "creates a comment" do
 
-      post :create, params: { post: { message: "Excellent post!", post_id: 3 } }
+      post :create, params: { comment: { message: "Excellent post!", post_id: postA.id } }
       expect(Comment.find_by(message: "Excellent post!")).to be
     end
   end
