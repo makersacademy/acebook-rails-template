b12af0e0231ed8f2cf0bbb138f744c2e53e25403
Charlie Paterson <Charlieed.paterson@googlemail.com>
2020-01-22 17:05:19 +0000

    Tweak: give CSS styling to friends page

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200121120316) do
+ActiveRecord::Schema.define(version: 20200122103436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "comments", force: :cascade do |t|
-    t.string "message", null: false
+    t.string "message"
     t.bigint "post_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
   end
 
   create_table "posts", force: :cascade do |t|
@@ -57,6 +56,15 @@ ActiveRecord::Schema.define(version: 20200121120316) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "walls", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.index ["post_id"], name: "index_walls_on_post_id"
+    t.index ["user_id"], name: "index_walls_on_user_id"
+  end
+
   add_foreign_key "posts", "users"
   add_foreign_key "posts", "users", column: "recipient_id"
+  add_foreign_key "walls", "posts"
+  add_foreign_key "walls", "users"
 end
