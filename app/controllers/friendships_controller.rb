e1af26365cb867866f51e31277dce728b146e8fa
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-22 10:59:01 +0000

    feat: split users into lists of friends and non-friends. add remove link to friends of user. Hide own account in user index

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,10 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.create(:friend_id => params[:friend_id])
     if @friendship
+      p "here"
+      # User.find(params[:friend_id]).friendships.create(:friend_id => current_user.id)
       flash[:notice] = "Added friend."
+
     else
       flash[:notice] = "Unable to add friend."
     end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,20 @@
 class Friendship < ApplicationRecord
-belongs_to :user
-belongs_to :friend, :class_name => 'User'
+  # after_create :create_inverse_relationship
+  # after_destroy :destroy_inverse_relationship
+
+  belongs_to :user
+  belongs_to :friend, :class_name => 'User'
+
+
+  private
+
+  def create_inverse_relationship
+    friend.friendships.create(friend_id: user.id)
+  end
+
+  # def destroy_inverse_relationship
+  #   friendship = friend.friendships.find_by(friend: user)
+  #   friendship.destroy if friendship
+  # end
 
 end
