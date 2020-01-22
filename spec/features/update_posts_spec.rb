0e51b5bbcd61caff25b6a0f3bccf4cd5e41ca634
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-22 18:59:38 +0000

    Modify tests to expect user wall routes instead of post route

--- spec/features/update_posts_spec.rb
+++ spec/features/update_posts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can update posts they have created" do
     signup
-    expect(current_path).to eq("/posts")
+    expect(current_path).to eq("/#{my_user_id('ben@example.com')}")
     click_on "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
@@ -16,16 +16,15 @@ RSpec.feature "Timeline", type: :feature do
 
   scenario "Cannot update posts they did not create" do
     signup
-    expect(current_path).to eq("/posts")
+    expect(current_path).to eq("/#{my_user_id('ben@example.com')}")
     click_on "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(current_path).to eq("/posts")
     expect(page).to have_button("Edit")
     click_on "Logout"
-
     signup_user("Dawid", "Dawid@example.com", "password1")
-    expect(current_path).to eq("/posts")
+    expect(current_path).to eq("/#{my_user_id('Dawid@example.com')}")
     expect(page).to_not have_content("Edit")
   end
 end
@@ -40,12 +39,11 @@ RSpec.feature "Timeline", type: :feature do
   end
   scenario "Cannot update posts after 10 minutes" do
     signup
-    expect(current_path).to eq("/posts")
+    expect(current_path).to eq("/" + my_user_id('ben@example.com'))
     click_on "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(current_path).to eq("/posts")
-
     Timecop.travel(601)
     click_on "Edit"
     expect(current_path).to eq("/posts")
