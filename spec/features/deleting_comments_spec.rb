952784ee818e0b68a04ca941a41d72b8a2f3f621
Student <student@Makerss-MacBook-Air.local>
2020-01-23 15:27:36 +0000

    finished styling buttons, cards, comments

--- spec/features/deleting_comments_spec.rb
+++ spec/features/deleting_comments_spec.rb
@@ -10,9 +10,8 @@ RSpec.feature "Timeline", js: true, type: :feature do
 
     click_button "Comments"
     fill_in "comment_text", with: "Some comment"
-    click_button "Submit"
+    click_button "Add"
     expect(page).to have_content("Some comment")
-    
     
     click_on "delete_comment"
     expect(page.html).to_not have_content("Some comment")
