51ab6bb6bf9848ad65641e8d946f814600b7c310
Eve Noirault <student@Makerss-MacBook-Air.local>
2020-01-23 16:54:52 +0000

    feature test for wall post

--- spec/features/wall_posts_spec.rb
+++ spec/features/wall_posts_spec.rb
@@ -9,4 +9,24 @@ RSpec.feature "Wall Posts", type: :feature do
     expect(current_path).to eq "/#{my_user_id("ben@example.com")}"
     expect(page).to have_content "This posts on Ben's wall!"
   end
+
+  scenario "Submit post to another user's wall" do
+    signup_user("Eve", "eve@example.com", "password")
+    click_on "Logout"
+    signup
+    expect(current_path).to eq "/#{my_user_id("ben@example.com")}"
+
+    visit "/#{my_user_id("eve@example.com")}"
+    expect(current_path).to eq "/#{my_user_id("eve@example.com")}"
+    click_on "New Post"
+    fill_in "Message", with: "This posts on Eve's wall!"
+    click_button "Submit"
+    expect(current_path).to eq "/#{my_user_id("eve@example.com")}"
+    expect(page).to have_content "This posts on Eve's wall!"
+
+    visit "/#{my_user_id("ben@example.com")}"
+    expect(page).not_to have_content "This posts on Eve's wall!"
+  end
+
+
 end
