e169154734c783a1d01ee35ebb53fa7dfbf2d768
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-23 10:15:23 +0000

    Test for routes to custom 404

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe ErrorsController, type: :controller do
 
+  before(:each) do
+    get :index
+  end
+
+  it "displays the 404 page" do
+    expect(response.status).to be(404)
+  end
+
+  it 'displays a flash notice, telling the user the status code' do
+    expect(flash[:alert]).to eq "Status 404"
+  end
 end
