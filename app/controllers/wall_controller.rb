c6d2c40d4b58216829e3f6df2f844a2669401cdf
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-24 11:54:22 +0000

    Test to display all posts on /posts

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -1,12 +1,12 @@
 class WallController < ApplicationController
-  before_action :find_user
+  before_action :find_wall_owner
 
   def show
     @wall_id = params[:user_id]
     @posts = Post.where(wall_id: @wall_id).order("created_at DESC")
   end
 
-  def find_user
+  def find_wall_owner
     @wall_owner = User.find_by(id: params[:user_id])
     redirect_to '/404' if @wall_owner.blank?
     return @wall_owner
