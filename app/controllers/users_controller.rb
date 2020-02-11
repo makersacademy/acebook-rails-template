34907e91d38c4e14f145b5ab60016a2ee072301f
Umberto Valente <uvalente89@gmail.com>
2020-02-11 13:35:41 +0000

    feat: validates user email and password at creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
+require 'bcrypt'
+
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(article_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def article_params
+    params.require(:user).permit(:email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { in: 6..10,
+                                                 wrong_length: 'Please insert a password between 6 to 10 characters long' }
+
 end
