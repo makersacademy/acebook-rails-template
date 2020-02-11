9d6e2fd95984b30951340d2f19199b9241dbf618
cpcwood <cpcwood@outlook.com>
2020-02-11 14:12:05 +0000

    travis file updated

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 20200211125503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "pictures", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id"
     t.text "post_content"
