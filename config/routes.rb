e49701401f674c4057297e008762fe2a07be3802
Jakephillips55 <Jakep629@gmail.com>
2020-02-11 16:51:20 +0000

    controller?

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'posts/index'
+
+  root 'posts#index'
+  
   resources :posts
 end
