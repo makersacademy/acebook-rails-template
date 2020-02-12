6925c5d50a6547802b50f6cf6cfd5cf3a3c7d8c9
cpcwood <cpcwood@outlook.com>
2020-02-12 16:20:21 +0000

    user sign up tested

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe '#create' do
+    it 'creates new user and saves to database' do
+      allow(controller).to receive(:params).and_return(ActionController::Parameters.new(users: { email: 'test@email.com', password: 'password' }))
+      expect_any_instance_of(User).to receive(:save)
+      subject.create
+    end
+    it 'creates new user in the database' do
+      allow(controller).to receive(:params).and_return(ActionController::Parameters.new(users: { email: 'test@email.com', password: 'password' }))
+      subject.create
+      expect(session[:user].id).not_to eq(nil)
+      expect(session[:user].email).to eq('test@email.com')
+      expect(session[:user].password).not_to eq(nil)
+    end
+  end
 end
