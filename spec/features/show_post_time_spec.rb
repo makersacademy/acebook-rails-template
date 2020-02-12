b8b7d65d21e75b0c9c731f08849fa1a142ca6af6
Jakephillips55 <Jakep629@gmail.com>
2020-02-12 14:30:51 +0000

    no rubocop errors now

--- spec/features/show_post_time_spec.rb
+++ spec/features/show_post_time_spec.rb
@@ -8,6 +8,6 @@ RSpec.feature 'Timeline', type: :feature do
     click_link 'New post'
     fill_in 'Message', with: 'Hello, world!'
     click_button 'Submit'
-    expect(page).to have_content(Post.all[0].created_at.strftime(' Posted at %a %I:%M%p').to_s)
+    expect(page).to have_content(Post.all[0].created_at.strftime('Posted at %a %I:%M%p').to_s)
   end
 end
