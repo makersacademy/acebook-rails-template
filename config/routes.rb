83f24df778e6e2be664b26adbc3ce9f88deb8f4a
nicolasraffray <nraffray@gmail.com>
2020-02-12 21:46:45 +0000

    forgot to add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :user, only: [:new,:create]
-  get '/login', to: 'sessions#login'
-  post '/login', to: 'sessions#login'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   get '/', to: 'sessions#welcome'
+  
   get '/user/create', to: 'user#new'
   post '/user/create', to: 'user#create'
 
