9bf62823bba35f6e4bda51286378f390c5f7f2d7
Meghan <meghan.e.mccue@gmail.com>
2020-02-12 10:11:47 +0000

    Add User Sign Up Files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(post_params)
+    redirect_to users_url
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+
+  def post_params
+    params.require(:user).permit(:username)
+  end
+
+end
