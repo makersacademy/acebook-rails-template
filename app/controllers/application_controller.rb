c2de755636cce4565cfb89d3b92ffcdd6b57c880
cpcwood <cpcwood@outlook.com>
2020-02-12 22:07:37 +0000

    signup validates whether email is already taken

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  rescue_from ActiveRecord::RecordInvalid, with :email_invalid
 
-
-  private
-
-  def email_invalid(error)
-    redirect_to(root_path, :notice => 'Congratulations You Have Signed Up to AceBook')
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,16 @@ class UsersController < ApplicationController
   def create
     user_params = params.require(:users).permit(:email, :password)
     @user = User.new(user_params)
-    # begin
+    begin
       @user.save!
-      session[:user] = @user    
-    #   redirect_to(posts_path, :notice => 'Congratulations You Have Signed Up to AceBook') 
-    # rescue ActiveRecord::RecordInvalid => e
-      
-    #   redirect_to(new_users_path, :notice => 'NAH') 
-    # end
-      
-    
-
-
+      session[:user] = @user
+      redirect_to(posts_path, :notice => 'Congratulations You Have Signed Up to AceBook') 
+    rescue => e
+      if e.class == ActiveRecord::RecordInvalid
+        redirect_to('/signup', :notice => 'Email already taken, please chose another')
+      else
+        redirect_to('/signup', :notice => "#{e}")
+      end
+    end
   end
-
-
 end
