e7282e5abd4becb40d7c539b988d3b138fa2c98a
Meghan <meghan.e.mccue@gmail.com>
2020-02-12 13:38:00 +0000

    Remove existing User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
 
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
-  def create
-    @user = User.create(post_params)
-    redirect_to users_url
-  end
+  # def create
+  #   @user = User.create(post_params)
+  #   redirect_to users_url
+  # end
 
-  def index
-    @users = User.all
-    @current_user = @users.last
-  end
+  # def index
+  #   @users = User.all
+  #   @current_user = @users.last
+  # end
 
-  private
+  # private
 
-  def post_params
-    params.require(:user).permit(:username)
-  end
+  # def post_params
+  #   params.require(:user).permit(:username)
+  # end
 
 end
