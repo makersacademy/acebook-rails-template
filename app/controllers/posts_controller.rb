7f0f11b2f8f6462236da15b0340f5121b8a3159d
Ilias Grigoropoulos <ilias_grigorop2@hotmail.com>
2020-02-12 18:39:48 +0000

    Not working params into New Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
   end
 
   def create
+    p "user"
+    p @current_user = session[:user_id]
     @post = Post.create(post_params)
     redirect_to posts_url
   end
@@ -16,6 +18,8 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:message)
+    p 'params'
+    p params[:id] = @current_user
+    p params.require(:post).permit(:message, :id)
   end
 end
