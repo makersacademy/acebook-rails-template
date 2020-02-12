e18a26a3fe506980e17805e66fd5f25020ae7f2c
Jakephillips55 <Jakep629@gmail.com>
2020-02-12 21:08:21 +0000

    implemented the deleted find show function works as a link users can click to remove post

--- spec/features/delete_post_spec.rb
+++ spec/features/delete_post_spec.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.feature 'Timeline', type: :feature do
-  scenario 'Can delete posts from page by clicking delete' do
+  scenario 'Can delete an existing post' do
     visit '/posts'
     click_link 'New post'
-    fill_in 'Message', with: 'Hello, test delete!'
+    fill_in 'Message', with: 'Hello, test!'
     click_button 'Submit'
-    click_button 'Delete'
-    expect(page).to have_no_content('Hello, test delete!')
+    
+    click_link 'Destroy'
+    expect(page).to have_no_content('Hello, test!')
   end
 end
