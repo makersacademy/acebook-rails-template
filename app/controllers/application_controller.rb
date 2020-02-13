b886450673108942a2de41b143a1d3883491bb71
cpcwood <cpcwood@outlook.com>
2020-02-13 16:48:01 +0000

    If user not logged in page redirects to homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :require_login
+
+  protected
+
+  def require_login
+    unless session[:user]
+      redirect_to(root_path)
+    end
+  end
 end
