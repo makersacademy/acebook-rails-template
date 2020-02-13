9eaa6772a07e749dabe61e5b45dff1881ae0cfb9
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-02-13 12:25:17 +0000

    user can delete post

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get '/user/create', to: 'user#new'
   post '/user/create', to: 'user#create'
 
+  delete '/posts/:id(.:format)', to: 'posts#destroy'
+
   # get 'welcome/index'
   # root "session#welcome"
   resources :posts
---
+++ spec/features/user_can_delete_post_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Timeline", type: :feature do
+  scenario "Can view delete button" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+
+    expect(page).to have_button("Delete")
+  end
+
+  scenario "Can delete post" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_button "Delete"
+    accept_alert {"Are you sure?"}
+    # page.driver.browser.switch_to.alert.accept
+
+    expect(page).not_to have_content("Hello, world!")
+    expect(page).not_to have_content(Time.now.strftime("%Y-%m-%d %H:%M:%S").to_s)
+  end
+
+
+end
