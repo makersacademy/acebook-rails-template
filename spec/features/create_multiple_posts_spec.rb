0a24c40e85db9ba0b1688c2aea3d846dee13a1e2
Imraan Ali <imraan91@hotmail.co.uk>
2020-02-13 15:12:37 +0000

    Added additional fields to post view and implemented reverse chronological order in post view

--- spec/features/create_multiple_posts_spec.rb
+++ spec/features/create_multiple_posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'Users can create a post', type: :feature do
-  scenario 'User creates a post' do
+  scenario 'User creates a second post' do
     visit('/')
     click_on('Signup')
     fill_in('users[email]', with: 'test@test.com')
@@ -10,9 +10,14 @@ RSpec.feature 'Users can create a post', type: :feature do
     click_on 'New Post'
     fill_in 'post[post_content]', with: 'Hey there'
     click_on 'Create Post'
-    post_time = Post.all[0].created_at
+    click_on 'New Post'
+    fill_in 'post[post_content]', with: 'Hey again'
+    click_on 'Create Post'
+    post_time = Post.all[1].created_at
     expect(page).to have_content('Hey there')
+    expect(page).to have_content('Hey again')
     expect(page).to have_content(post_time)
-    expect(page).to have_content('test@test.com')
+    expect(find('table#posts td', match: :first)).to have_content('Hey again')
+
   end
 end
