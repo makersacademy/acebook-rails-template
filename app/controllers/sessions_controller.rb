c72f195d4aeab56934ab0ff7d29ae180ed1316e8
AliciaMerlino <acmerlino1@gmail.com>
2020-02-13 21:54:14 +0000

    update to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,9 +10,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to posts_url
-    # else 
-    #   flash[:alert] = "Email or Password not valid"
-    #   render :welcome
+    else 
+      flash[:alert] = "Email or Password not valid"
+      render :welcome
     end 
   end
 
