853fbd31c0d6a059d7f2fc391a647d786e0927cf
basselalsayed <alsayed.bassel@gmail.com>
2020-02-13 18:53:14 +0000

    login signup working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,12 @@
 class UsersController < ApplicationController
-  
-  before_action :save_login_state, :only => [:new, :create]
-  
+
   def new
     @user = User.new
   end
   
   def create
-    # if params[:commit] == 'create'
-      @user = User.create(params.require(:user).permit(:username, :password))
-      # session[:user_id] = @user.id
-      redirect_to '/posts/index'
-    # elsif params[:commit] == 'login'
-    #   @user = User.find_by(username: params[:username])
-    #   if @user && @user.authenticate(params[:password])
-    #   else
-    #     redirect_to '/'
-    #   end
-    # end
+    @user = User.create(params.require(:user).permit(:username, :password))
+    redirect_to '/posts/index'
   end
 
   def show
