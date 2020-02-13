8c5be02d3f76119db3d3148425384e5e6177b490
Jakephillips55 <Jakep629@gmail.com>
2020-02-13 17:48:59 +0000

    reducing from 81 chars to 80 chars

--- spec/features/show_post_time_spec.rb
+++ spec/features/show_post_time_spec.rb
@@ -8,7 +8,7 @@ RSpec.feature 'Timeline', type: :feature do
     click_link 'New post'
     fill_in 'Message', with: 'Hello, world!'
     click_button 'Submit'
-    actual_content = Post.all[0].created_at.strftime('Posted at %a%I:%M%p').to_s
+    actual_time = Post.all[0].created_at.strftime('Posted at %a %I:%M%p').to_s
     expect(page).to have_content(actual_content)
   end
 end
