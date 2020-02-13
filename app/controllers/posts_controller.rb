76df8c18740f74ec3926240a7a5aaa0c7e2dd4cf
niki-01 <niki.manoledaki@gmail.com>
2020-02-13 14:25:01 +0000

    Ready to merge

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,6 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
     @current_user = current_user
     @post = @current_user.posts.create(post_params)
     redirect_to posts_url
