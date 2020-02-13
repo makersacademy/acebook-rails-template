d8e718656577cc30a5b8cdc3664b5bbf596f7dd6
cpcwood <cpcwood@outlook.com>
2020-02-13 16:23:41 +0000

    update test password to match validation

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'posts/index', type: :view do
   before(:each) do
-    user = User.create!(email: 'test@abc.com', password: 'password123')
+    user = User.create!(email: 'test@abc.com', password: 'password')
     assign(:posts, [
              Post.create!(user_id: user.id, post_content: 'test message'),
              Post.create!(user_id: user.id, post_content: 'test message 2')
