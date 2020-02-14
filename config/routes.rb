fe72717968b4d86ab86a1d2db7791ac4f5918c7a
basselalsayed <alsayed.bassel@gmail.com>
2020-02-14 02:03:10 +0000

    added links to user's profiles in each post

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # resources :posts
+  resources :posts
   
   resources :users do
     resources :posts
