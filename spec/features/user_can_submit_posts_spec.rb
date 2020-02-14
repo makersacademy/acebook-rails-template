4febc55f8114fe008ded1e9d879125bfe8487aae
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-02-14 17:52:47 +0000

    solve rspec tests

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -3,13 +3,13 @@ require 'web_helpers'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them with created time" do
-    # Timecop.freeze # freezes the time
-    # sign_up
-    # click_link "New post"
-    # fill_in "Message", with: "Hello, world!"
-    # click_button "Submit"
-    # expect(page).to have_content("Hello, world!")
-    # expect(page).to have_content(Time.now.strftime("%Y/%m/%d %H:%M:%S").to_s)
+    Timecop.freeze # freezes the time
+    sign_up
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world!")
+    expect(page).to have_content(Time.now.strftime("%Y/%m/%d %H:%M:%S").to_s)
   end
 
 
--- spec/features/user_can_update_post_spec.rb
+++ spec/features/user_can_update_post_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 require 'web_helpers'
 
 RSpec.feature "Timeline", type: :feature do
-  # scenario "Can view update button" do
-  #   sign_up
-  #   click_link "New post"
-  #   fill_in "Message", with: "Hello, world!"
-  #   click_button "Submit"
+  scenario "Can view update button" do
+    sign_up
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
 
-  #   expect(page).to have_button("Update")
-  # end
+    expect(page).to have_button("Update")
+  end
   
   # travis doesn't like the "fill_in "Message", with: "Hello, Acebook Insane!"
   # scenario "Can update post" do
