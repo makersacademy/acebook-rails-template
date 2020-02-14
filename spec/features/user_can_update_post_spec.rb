0d1b7911de53ad72c58409cf1be996c1f74c8790
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-02-14 12:53:37 +0000

    submit post rspec done

--- spec/features/user_can_update_post_spec.rb
+++ spec/features/user_can_update_post_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
+require 'web_helpers'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can view update button" do
-    visit "/posts"
+    sign_up
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
@@ -11,18 +12,18 @@ RSpec.feature "Timeline", type: :feature do
   end
   
   # travis doesn't like the "fill_in "Message", with: "Hello, Acebook Insane!"
-  # scenario "Can update post" do
-  #   visit "/posts"
-  #   click_link "New post"
-  #   fill_in "Message", with: "Hello, world!"
-  #   click_button "Submit"
-  #   sleep(5) 
-  #   first('.post').click_button 'Update'
-  #   # find('#updatePost').set("Hello, Acebook Insane!")
-  #   fill_in "Message", with: "Hello, Acebook Insane!"
-  #   click_button 'Save Post'
+  scenario "Can update post" do
+    sign_up
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    sleep(5) 
+    first('.post').click_button 'Update'
+    # find('#updatePost').set("Hello, Acebook Insane!")
+    fill_in "Message", with: "Hello, Acebook Insane!"
+    click_button 'Save Post'
 
-  #   expect(page).to have_content("Hello, Acebook Insane!")
-  #   expect(page).to have_content(Time.now.strftime("%Y-%m-%d %H:%M:%S").to_s)
-  # end
+    expect(page).to have_content("Hello, Acebook Insane!")
+    expect(page).to have_content(Time.now.strftime("%Y/%m/%d %H:%M:%S").to_s)
+  end
 end
