00d44f8b55e02f6db505501184c424ff6ea505b6
cpcwood <cpcwood@outlook.com>
2020-02-14 16:31:25 +0000

    refactor

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,14 +9,6 @@ RSpec.describe SessionsController, type: :controller do
   end
 
   describe '#create' do
-    it 'redirects if entered email not found' do
-      get(:create, params: { login: { email: 'test', password: 'password' }})
-      expect(response).to redirect_to(login_path)
-    end
-    it 'redirects if entered password is incorrect' do
-      User.create!(email: 'test@test.com', password: 'password')
-      get(:create, params: { login: { email: 'test@test.com', password: 'passrd' }})
-      expect(response).to redirect_to(login_path)
-    end
+    
   end
 end
