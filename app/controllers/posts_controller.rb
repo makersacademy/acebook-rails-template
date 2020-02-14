9ce308f33e7552277e71757d870aca7222322d04
David Papamichael <davidpapamichael@gmail.com>
2020-02-14 09:22:18 +0000

    feat: update function added to posts on the wall

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,23 @@ class PostsController < ApplicationController
   skip_before_action  :authenticated_user
   def new; end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @user = User.find(params[:user_id])
     @post = @user.posts.create(post_params)
     redirect_back(fallback_location: home_path)
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render 'edit'
+    end
   end
 
   def show; end
@@ -16,6 +29,7 @@ class PostsController < ApplicationController
     @posts = Post.order(created_at: :desc)
   end
 
+  
   def destroy
     @post = Post.find(params[:id])
     if @post.user_id == session[:user_id]
