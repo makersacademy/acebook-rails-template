ed300f6ae741c3569b5a825742c57b89b249f9b4
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-14 16:41:30 +0000

    Different users can sign up and post. Messages show that they have been posted by different users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,47 +1,49 @@
 class PostsController < ApplicationController
   
-  def new
+  def new # action in the browser localhost:3000/posts/new
     @post = Post.new
   end
 
-  def create
-    @post = Post.create(post_params)
-    redirect_to posts_url
+  def create # method to create a post
+    @post = Post.create(post_params) # crud method
+    @post.user = User.first
+    @post.save
+    redirect_to posts_url # redirect to the posts page
   end
 
-  def index
+  def index # action - in the browser  localhost:30000/posts/index
     @posts = Post.all
   end
 
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
+  def destroy # CRUD method to delete post
+    @post = Post.find(params[:id]) # find the post by its id
+    @post.destroy  # delete the id
 
-    redirect_to posts_path
+    redirect_to posts_path # redirect to the posts page - localhost:3000/posts
   end
 
-  def edit
-    @post = Post.find(params[:id])
+  def edit #action - localhost:3000/action
+    @post = Post.find(params[:id]) # find/grab the post by its id
   end
 
-  def update
-    @post = Post.find(params[:id])
+  def update # CRUD method to update/edit a post
+    @post = Post.find(params[:id])  # find /grab the method by its id
 
     if @post.update(params[:post].permit(:message))
-      redirect_to posts_url
+      redirect_to posts_url # redirect to localhost:3000/posts
     else
-      render 'edit'
+      render 'edit' # render the edit.html.erb view
     end
   end
 
-  def ten_minutes_difference(updated_at)
+  def ten_minutes_difference(updated_at) # method to allow
     @post = Post.find(params[:id])
-    return Time.now - updated_at
+    return Time.now - updated_at # says what time the post was posts
   end
 
   private
 
-  def post_params
+  def post_params # helper linked to the create method - means that the post requires the text
     params.require(:post).permit(:message)
   end
 end
