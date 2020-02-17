5d71e733262f0f3205951640ac717602174913b0
benauld <ben@portobello-consultancy.co.uk>
2020-02-17 10:41:33 +0000

    edit screen working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index'
-  get '/posts/:id/edit', to: 'posts#edit', as: 'post'
+  get '/posts/:id/edit', to: 'posts#edit', as: 'patch'
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   
-
- 
   root 'users#new'
+
 end
