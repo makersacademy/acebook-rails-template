40863274c03ec09fc00595a0997848bd7a2ce815
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-17 12:39:01 +0000

    Adds homepage feature test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "HomePage", type: :feature do 
-  # scenario "Can See a sign in and fields for it and sign up link" do 
-  #   visit "/"
-  #   expect(page).to have_button("Sign Up")
-  #   expect(page).to have_button("Login")
+   scenario "Can See a sign in and fields for it and sign up link" do 
+    visit "/"
+     expect(page).to have_button("Sign Up")
+     expect(page).to have_button("Login")
     # expect(page).to have_field("username")
     # expect(page).to have_field("password")
     # expect(page).to have_selector(:link_or_button, "Sign In")
-  # end
+   end
 end 
