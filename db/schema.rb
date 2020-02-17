02e0bc226d7cf1237669f836f9bee023aa3b84da
Umberto Valente <uvalente89@gmail.com>
2020-02-17 18:39:37 +0000

    feat: add user ability to post on someone else wall

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200217142657) do
+ActiveRecord::Schema.define(version: 20200217155847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 20200217142657) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.index ["user_id"], name: "index_posts_on_users_id"
+    t.bigint "location_id"
+    t.index ["location_id"], name: "index_posts_on_location_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 20200217142657) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "posts", "users", column: "location_id"
 end
---
+++ spec/features/post_on_other_user_wall_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+feature 'post on other user wall' do
+  scenario 'a user can post on another user wall' do
+    sign_up
+    create_post
+    click_link 'Sign Out'
+    sign_up_two
+    click_link 'Home'
+    click_link 'uvalente'
+    fill_in 'post[message]', with: 'Hello Umberto'
+    click_button 'Add post'
+    expect(page).to have_content 'Hello Umberto'
+    click_link 'Home'
+    expect(page).not_to have_content 'Hello Umberto'
+  end
+end
