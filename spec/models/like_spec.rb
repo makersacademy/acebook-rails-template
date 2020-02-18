68c02dbfc39638339cabd4c66730823f23fd313a
Ben Gittins <57527172+squareben1@users.noreply.github.com>
2020-02-18 11:24:42 +0000

    Unit test to check that Post class contains a Like instance
    Signed-off-by: Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  "add some examples to (or delete) #{__FILE__}"
+  context "with 2 or more likes" do
+    it "tallies them" do
+      user = User.create({:email => "example@example.com", :password => "example"})
+      post = user.posts.create({:message => "Meow"})
+      like = post.likes.create(user_id: user.id)
+      expect(post.likes[0]).to eq(like)
+    end
+  end
 end
