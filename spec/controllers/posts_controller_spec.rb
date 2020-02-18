78188951e1df497f3c113f6a3b6edf0aac4a2a5e
Walker-TW <thomas.w.walker@live.co.uk>
2020-02-18 12:28:58 +0000

    Added testing for 10 min check

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+
 end
---
+++ spec/features/cannot_delete_post_after_10mins_spec.rb
@@ -0,0 +1,17 @@
+RSpec.feature 'Users can delete a post once created', type: :feature do
+  scenario 'User creates a second post' do
+    visit('/')
+    click_on('Signup')
+    fill_in('user[email]', with: 'test@example.com')
+    fill_in('user[password]', with: 'password')
+    click_on('Signup')
+    click_on 'New Post'
+    fill_in 'post[post_content]', with: 'Hey there'
+    click_on 'Create Post'
+    expect(page).to have_content('Hey there')
+    x = Time.now + 660
+    travel_to(x)
+    click_on 'Delete'
+    expect(page).to have_content('Hey there')
+  end
+end
