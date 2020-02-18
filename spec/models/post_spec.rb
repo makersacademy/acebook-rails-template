7d86dbe86f5fca0b547e848b7b18edb2118bccd9
Ben Gittins <57527172+squareben1@users.noreply.github.com>
2020-02-18 16:00:40 +0000

    Refactored passing unit tests
    Signed-off-by: Marcello Fabbri <56549133+Marcellofabbri@users.noreply.github.com>

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,17 +4,16 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe '#under_ten_mins' do
-    it 'should return false if post created more than 10 mins ago' do 
-      post = Post.new({:message => 'Hello world', :created_at => Time.now})#, created_at: nil, updated_at: nil, user_id: nil, wall_id: nil})
-      p post
+  post = Post.new({:message => 'Hello world', :created_at => Time.now})
+    
+    it 'should return false if post created > 10 mins ago' do
       Timecop.travel(Time.now + 11.minutes) do
         expect(subject.under_ten_mins(post)).to eq false
       end
     end
 
-    it 'should return true if post created less than 10 mins ago' do 
-      post = Post.new({:message => 'Hello world', :created_at => Time.now})
+    it 'should return true if post created < 10 mins ago' do
       expect(subject.under_ten_mins(post)).to eq true
-    end 
+    end
   end
 end
