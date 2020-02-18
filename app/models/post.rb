0684ecd2f636f2495caf8b8629f6b977302db39c
Jakephillips55 <Jakep629@gmail.com>
2020-02-18 16:38:27 +0000

    almost working logic for photo uploads

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,6 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+  mount_uploader :image, ImageUploader
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200213110250) do
+ActiveRecord::Schema.define(version: 20200218153725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200213110250) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
