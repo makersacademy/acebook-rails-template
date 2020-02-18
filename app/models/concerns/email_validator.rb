a02c847e6916a370b5addc7a17c5601b8eab81b8
cpcwood <cpcwood@outlook.com>
2020-02-18 11:25:46 +0000

    debugging production

--- app/models/concerns/email_validator.rb
+++ app/models/concerns/email_validator.rb
@@ -1,3 +1,5 @@
+require 'resolv'
+
 class EmailValidator < ActiveModel::Validator
   EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
 
@@ -23,7 +25,7 @@ class EmailValidator < ActiveModel::Validator
     puts 'before regex'
     domain = user.email.match(/(?<=@)(.+)/)[0]
     puts 'after regex'
-    mx = Resolv::DNS.open { |dns| dns.getresources(domain, Resolv::DNS::Resource::IN::MX) }
+    p mx = Resolv::DNS.open { |dns| dns.getresources(domain, Resolv::DNS::Resource::IN::MX) }
     puts 'resolv'
     ok = mx.size > 0
     user.errors.add(:email, 'Email format invaild, please enter valid email') unless ok
