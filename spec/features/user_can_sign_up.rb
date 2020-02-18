8ae7549d172f4c35d593a086fcab35461cc32f73
Vish Mayer <vish.mayer@gmail.com>
2020-02-18 09:08:06 +0000

    Refactored feature tests
    - added a web helper file to extract sign_up and log_in methods
    - refactored sign_in, sign_out and create posts feature tests.

--- spec/features/user_can_sign_up.rb
+++ spec/features/user_can_sign_up.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 
 feature "test sign up" do
  scenario "Can Sign up and see posts page with Hello on it" do
-    visit '/'
-    click_button "Sign Up"
-    fill_in "Email", with: "ben@gmail.com"
-    fill_in "Password", with: "Password"
-    click_button "create"
+    sign_up
     expect(page).to have_content("Hello")
   end
 end
