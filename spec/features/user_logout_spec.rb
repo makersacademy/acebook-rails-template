0a68fb1e0a698d9b884a7c44dae13544ef065814
AliciaMerlino <acmerlino1@gmail.com>
2020-02-18 15:22:36 +0000

    fix: spec for logout button

--- spec/features/user_logout_spec.rb
+++ spec/features/user_logout_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 require 'web_helpers'
 
-# RSpec.feature "Logout", type: :feature do
-#   scenario "User can logout after signing in" do
-#     sign_up
-#     expect(page).to have_link("Log Out")
-#   end
-# end
+RSpec.feature "Logout", type: :feature do
+  scenario "User can logout after signing in" do
+    sign_up
+    expect(page).to have_link("Log Out")
+  end
+end
