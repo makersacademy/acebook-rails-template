ef4f2cb314525d6b758e4a9fabacc954ad219a2b
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-02-18 11:26:23 +0000

    Unit test, refactored

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  context "with 2 or more likes" do
-    it "tallies them" do
+  context "Posts can have likes" do
+    it "creates instance of Like in User class" do
       user = User.create({:email => "example@example.com", :password => "example"})
       post = user.posts.create({:message => "Meow"})
       like = post.likes.create(user_id: user.id)
