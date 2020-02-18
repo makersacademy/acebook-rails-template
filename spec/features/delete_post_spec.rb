30e82d88f56fbc1ba0bc62fd9ce9184c457e1508
ratcliffetj <ratcliffetj89@gmail.com>
2020-02-18 11:33:58 +0000

    Added feature tests for validation on edit/delete posts'

--- spec/features/delete_post_spec.rb
+++ spec/features/delete_post_spec.rb
@@ -1,15 +1,33 @@
 RSpec.feature 'Users can delete a post once created', type: :feature do
-  scenario 'User creates a second post' do
-    visit('/')
-    click_on('Signup')
-    fill_in('user[email]', with: 'test@test.com')
-    fill_in('user[password]', with: 'password')
-    click_on('Signup')
+  scenario 'User deletes their own post' do
+    visit '/'
+    click_on 'Signup'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: 'password'
+    click_on 'Signup'
     click_on 'New Post'
     fill_in 'post[post_content]', with: 'Hey there'
     click_on 'Create Post'
     expect(page).to have_content('Hey there')
     click_on 'Delete'
     expect(page).not_to have_content('Hey there')
+  end
+
+  scenario 'User is not allowed to delete someone elses post' do
+    visit '/'
+    click_on 'Signup'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: 'password'
+    click_on 'Signup'
+    click_on 'New Post'
+    fill_in 'post[post_content]', with: 'Hey there'
+    click_on 'Create Post'
+    click_on 'Logout'
+    click_on 'Signup'
+    fill_in 'user[email]', with: 'test2@test.com'
+    fill_in 'user[password]', with: 'password'
+    click_on 'Signup'
+    click_on 'Delete'
+    expect(page).to have_content('Not authorized to delete this post')
   end
 end
--- spec/features/edit_post_spec.rb
+++ spec/features/edit_post_spec.rb
@@ -18,4 +18,22 @@ RSpec.feature 'Users can edit a post that has been created', type: :feature do
     click_on 'Back'
     expect(page).to have_content('Hey there')
   end
+
+  scenario 'User is not allowed to edit someone elses post' do
+    visit '/'
+    click_on 'Signup'
+    fill_in 'user[email]', with: 'test@test.com'
+    fill_in 'user[password]', with: 'password'
+    click_on 'Signup'
+    click_on 'New Post'
+    fill_in 'post[post_content]', with: 'Hey there'
+    click_on 'Create Post'
+    click_on 'Logout'
+    click_on 'Signup'
+    fill_in 'user[email]', with: 'test2@test.com'
+    fill_in 'user[password]', with: 'password'
+    click_on 'Signup'
+    click_on 'Edit'
+    expect(page).to have_content('Not authorized to update this post')
+  end
 end
