5a41df28dbb58b4d627bf99ebf5b4891eb314aac
shadz22 <shadz22@yahoo.co.uk>
2020-02-18 15:17:51 +0000

    push new branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # protected
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation)}
+  #   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password,)}
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # devise_scope :user do
+  #   get "/sign_in" => "devise/sessions#new"
+  #   get "/sign_up" => "devise/registrations#new", as: "new_user" # custom path to sign_up/registration
+  # end
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'posts/index'
+  get 'users/index'
 
   root 'posts#index'
 
-  resources :posts do
-    resources :users
-  end
+  resources :posts
+  resources :users
+  
 end
