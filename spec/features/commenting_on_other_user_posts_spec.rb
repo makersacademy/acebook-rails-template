5ad74d2670c80e5c2ea22bc1481786fc0b1a1f16
Asia <asiaellis5@gmail.com>
2020-02-18 12:41:23 +0000

    test: correct user link displayed and tested

--- spec/features/commenting_on_other_user_posts_spec.rb
+++ spec/features/commenting_on_other_user_posts_spec.rb
@@ -1,26 +1,32 @@
 # frozen_string_literal: true
 
-feature 'user can comment on post' do
+feature 'user can comment on other peoples posts' do
   scenario 'from the timeline' do
     sign_up
     create_post
+    click_link 'Sign Out'
+    sign_up_two
     click_link 'Home'
-    fill_in 'comment[body]', with: 'Hello'
+    fill_in 'comment[body]', with: 'Jack the Mac'
     click_button 'Add Comment'
     within('div.comment-container') do
-      expect(page).to have_content('Hello')
-      expect(page).to have_content('uvalente')
+      expect(page).to have_content('Jack the Mac')
+      expect(page).to have_content('asia')
     end
   end
 
   scenario 'from a user homepage' do
     sign_up
     create_post
+    click_link 'Sign Out'
+    sign_up_two
+    click_link 'Home'
+    click_link 'uvalente'
     fill_in 'comment[body]', with: 'Jack the Mac'
     click_button 'Add Comment'
     within('div.comment-container') do
       expect(page).to have_content('Jack the Mac')
-      expect(page).to have_content('uvalente')
+      expect(page).to have_content('asia')
     end
   end
 end
