0f9aed0ea620920511a989d84c5ece372c7f6314
Lucian Boatright <lucian.boatright@gmail.com>
2020-02-18 12:43:30 +0000

    css added - cards with shadow holding each message

--- spec/helpers/sign_up_helper.rb
+++ spec/helpers/sign_up_helper.rb
@@ -8,5 +8,5 @@ def sign_up
   fill_in 'user[email]', with: 'Ria@testing.com'
   fill_in 'user[password]', with: 'password'
   fill_in 'user[password_confirmation]', with: 'password'
-  click_on 'Sign up'
+  click_on 'Sign Up'
 end
