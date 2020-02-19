83fc3bbfe2cba27dd4c57ef35caf3990aecffddc
shadz22 <shadz22@yahoo.co.uk>
2020-02-19 09:55:57 +0000

    amending rubocop fails

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
 class ProfileController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
   def show
     @user = User.find(params[:id])
     @user_posts = @user.posts
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Profile < ApplicationRecord
   has_one :user
   has_many :posts
