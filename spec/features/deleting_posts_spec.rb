daa9143deabf48c18369a04d4ede1b744262b5b5
David Papamichael <davidpapamichael@gmail.com>
2020-02-19 12:24:27 +0000

    feature: when posts with comments are deleted, the comments are removed from the database

--- spec/features/deleting_posts_spec.rb
+++ spec/features/deleting_posts_spec.rb
@@ -8,4 +8,13 @@ feature 'user can delete posts' do
     click_link 'Delete'
     expect(page).not_to have_content('test message')
   end
+
+  scenario 'from the timeline, comments will also be destroyed' do
+    sign_up
+    create_post
+    create_comment
+    click_link 'Delete'
+    expect(page).not_to have_content('test message') 
+    expect(page).not_to have_content('test comment')
+  end
 end
