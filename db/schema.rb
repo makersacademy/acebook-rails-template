8a0944b39150f17cdd428d5549927b651519b364
nicolasraffray <nraffray@gmail.com>
2020-02-19 17:36:52 +0000

    username fix implemented now in database

--- db/schema.rb
+++ db/schema.rb
@@ -17,10 +17,11 @@ ActiveRecord::Schema.define(version: 20200219113656) do
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.integer "wall_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
