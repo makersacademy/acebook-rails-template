f7ed8151c44a3441a4f2654313626bf42a5364b6
Niki Manoledaki <18622989+nikimanoledaki@users.noreply.github.com>
2020-02-19 16:37:57 +0000

    User wall shows posts where post was written on index by wall owner
    Signed-off-by: Meghan Iankov <56200818+meghaniankov@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,11 +9,16 @@ RSpec.describe User, type: :model do
       @user = User.create({:email => "example@example.com", :password => "example"})
     end
 
-    it 'where user.id and wall_id' do
+    it 'where user.id equals wall_id' do
       post_1 = @user.posts.create({:message => "Meow", :wall_id => @user.id})
       post_2 = @user.posts.create({:message => "CatsRule", :wall_id => @user.id})
       post_3 = @user.posts.create({:message => "DogsDrool", :wall_id => 0})
       expect(User.filter(User.all, @user)).to eq [post_1, post_2]
+    end
+
+    it 'where post was written on index by wall owner' do
+      post = @user.posts.create({:message => "Meow", :wall_id => nil})
+      expect(User.filter(User.all, @user)).to eq [post]
     end
 
     it "by reverse chronological order" do
