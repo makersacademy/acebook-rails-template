baa7374364196c7759b8d00229a87b4ac4348e31
Jack Willee <jackwillee1995@gmail.com>
2020-02-19 15:59:19 +0000

    added like and unlike functionality

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,26 @@
 class LikesController < ApplicationController
   skip_before_action :authenticated_user
   before_action :find_post
+  before_action :find_like, only: [:destroy]
 
   def create
-    if already_liked?
-      flash.now.alert = "Apologies, you have already liked this"
-      @posts = Post.order(created_at: :desc)
-      render 'posts/index'
-    else
+    # if already_liked?
+    #   # destroy
+    #   # render 'posts/index'
+    # else
     @post.likes.create(user_id: current_user.id)
     redirect_to session[:url]
     end
-  end
+  # end
+
+  def destroy
+    if !(already_liked?)
+      flash.now.alert = "Apologies, you cannot unlike this"
+    else 
+      @like.destroy
+    end 
+    redirect_to session[:url]
+  end 
 
   private
 
@@ -22,5 +31,9 @@ class LikesController < ApplicationController
   def already_liked?
     Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
   end
+
+  def find_like
+    @like = @post.likes.find(params[:id])
+  end 
 
 end
