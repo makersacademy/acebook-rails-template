29259bdbc57be6208078ebed2dc6eed27516e710
cpcwood <cpcwood@outlook.com>
2020-02-19 17:56:18 +0000

    remove old routes

--- spec/views/users/show.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe 'users/show', type: :view do
-  before(:each) do
-    @user = assign(:user, User.create!(email: 'a@gmail.com', password: 'password'))
-  end
-
-  it 'renders attributes in <p>' do
-    render
-  end
-end
