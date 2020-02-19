88b49bc96766d457319f6375b19d82b58ec20a44
shadz22 <shadz22@yahoo.co.uk>
2020-02-19 16:07:20 +0000

    refactor rubocop

--- db/migrate/20200219151829_remove_filedname_from_profile.rb
+++ db/migrate/20200219151829_remove_filedname_from_profile.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveFilednameFromProfile < ActiveRecord::Migration[5.1]
   def change
     remove_column :profiles, :filedname, :string
--- spec/helpers/fill_form_helper.rb
+++ spec/helpers/fill_form_helper.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-def fill_in
+def fill_in_form
   click_on 'New post'
   fill_in 'Message', with: 'Hello, world!'
   click_button 'Submit'
