575fba72a7999b9ad6fd76f3823ee95866a3d51b
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-19 12:50:46 +0000

    Adds wall_id column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200211124321) do
+ActiveRecord::Schema.define(version: 20200219113656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.integer "user_id"
+    t.integer "wall_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
