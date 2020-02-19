77403af11e73cc6d851e3044404fd5d505d58b77
Asia <asiaellis5@gmail.com>
2020-02-19 14:45:38 +0000

    feature: stopped it being possible to like a post twice

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,24 @@ class LikesController < ApplicationController
   before_action :find_post
 
   def create
+    if already_liked?
+      flash.now.alert = "Apologies, you have already liked this"
+      @posts = Post.order(created_at: :desc)
+      render 'posts/index'
+    else
     @post.likes.create(user_id: current_user.id)
     redirect_to session[:url]
+    end
   end
 
   private
 
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
   end
 
 end
