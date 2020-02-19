4bc3ff28cc9ef610edbccfc22d483d9a185c2b9b
nicolasraffray <nraffray@gmail.com>
2020-02-19 16:36:58 +0000

    usernames are shown instead of emails

--- spec/features/login_success_spec.rb
+++ spec/features/login_success_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature "HomePage", type: :feature do 
   scenario "Login success" do 
     login
-    expect(page).to have_content("Welcome to Acebook you are logged in as: test@email.com")
+    expect(page).to have_content("Welcome to Acebook you are logged in as: BigJD")
   end
 end
