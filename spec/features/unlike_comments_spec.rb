56f851b87d909d1bf249a71122319e3b1f154635
David Papamichael <davidpapamichael@gmail.com>
2020-02-19 17:21:35 +0000

    test: likes for posts working and tested edge cases

---
+++ spec/features/unlike_comments_spec.rb
@@ -0,0 +1,22 @@
+feature 'user can like a post (and unlike a post once liked)' do
+  scenario 'from the timeline' do
+    sign_up
+    create_post
+    click_link 'Home'
+    click_button 'Like'
+    click_button 'Unlike'
+    expect(page).to have_content('0 Likes')
+    expect(page).to have_selector(:link_or_button, "Like")
+  end
+
+
+  scenario 'from the user homepage' do
+    sign_up
+    create_post
+    click_button 'Like'
+    click_button 'Unlike'
+    expect(page).to have_content('0 Likes')
+    expect(page).to have_selector(:link_or_button, "Like")
+  end
+  
+end
