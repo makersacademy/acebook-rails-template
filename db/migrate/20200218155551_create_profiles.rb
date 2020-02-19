6436e2d706dd9f4c6a694ea39d28299e4df78a1d
Lucian Boatright <lucian.boatright@gmail.com>
2020-02-19 15:00:39 +0000

    added direction to new_posts for profile

--- db/migrate/20200218155551_create_profiles.rb
+++ db/migrate/20200218155551_create_profiles.rb
@@ -3,6 +3,7 @@
 class CreateProfiles < ActiveRecord::Migration[5.1]
   def change
     create_table :profiles do |t|
+      t.string "message"
       t.references :posts, foreign_key: true
       t.references :users, foreign_key: true
       t.timestamps
