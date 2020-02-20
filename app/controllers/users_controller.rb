33fbca6341d3ad4b339dbc3f78b21c2d6f40c699
Asia <asiaellis5@gmail.com>
2020-02-20 10:13:52 +0000

    feat: added form for updating username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_details)
 
