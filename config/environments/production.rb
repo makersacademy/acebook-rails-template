0c542c47db809be706cbbbca01e3c0dbdfdcbc61
Umberto Valente <uvalente89@gmail.com>
2020-02-20 12:57:38 +0000

    chore: set production environment for chat

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_cable.mount_path = '/cable'
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -43,6 +42,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Mount Action Cable outside main process or domain
+  config.action_cable.mount_path = '/cable'
+  config.web_socket_server_url = "wss://acebook-u-jakd.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://acebook-u-jakd.herokuapp.com', 'http://acebook-u-jakd.herokuapp.com']
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
