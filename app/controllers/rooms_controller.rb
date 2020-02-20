667d608500b54c9e1d29cdbf073a318c436452fc
Umberto Valente <uvalente89@gmail.com>
2020-02-20 11:52:17 +0000

    chore: add chat link to navbar, change room chat path to chat

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,7 @@ class RoomsController < ApplicationController
   skip_before_action :authenticated_user
 
   def show
-    @messages = Message.all
+    @messages = Message.last(15)
     @message = Message.new
   end
 end
