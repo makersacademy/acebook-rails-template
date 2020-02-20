efa94c4945176755e520356f7d4a38e6ec232a5e
ratcliffetj <ratcliffetj89@gmail.com>
2020-02-20 10:56:57 +0000

    Added username to registration form and user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:email, :password, :profile_picture, :planet)
+    user_params = params.require(:user).permit(:email, :password, :profile_picture, :planet, :username)
     @user = User.new(user_params)
     begin
       @user.save!
