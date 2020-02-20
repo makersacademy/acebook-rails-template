5cd15c535fb661da27fadc63da059180e487e364
Ben Gittins <57527172+squareben1@users.noreply.github.com>
2020-02-20 12:04:31 +0000

    User can add comment
    Signed-off-by: Meghan Iankov <56200818+meghaniankov@users.noreply.github.com>

--- spec/features/posts_appear_from_newest_to_oldest_spec.rb
+++ spec/features/posts_appear_from_newest_to_oldest_spec.rb
@@ -15,13 +15,13 @@ RSpec.feature 'Posts', type: :feature do
     #{Time.now.strftime('%m/%d/%Y, %H:%M')}
     Edit Delete 
     Meow two 
-    ♡ 0 Likes
+    ♡ 0 Likes Add Comment
      
     cats@cats.com 
     #{Time.now.strftime('%m/%d/%Y, %H:%M')} 
     Edit Delete 
     Meow one 
-    ♡ 0 Likes")
+    ♡ 0 Likes Add Comment")
   end
 
   scenario 'On User Walls, posts are shown from newest to oldest' do
--- spec/features/user_can_comment_on_posts_spec.rb
+++ spec/features/user_can_comment_on_posts_spec.rb
@@ -4,8 +4,9 @@ RSpec.feature 'Comment', type: :feature do
   scenario 'user can submit comment on a post' do
     sign_up
     new_post
-    fill_in 'comment[body]', with: 'First Comment'
     click_link 'Add Comment'
+    fill_in 'comment[body]', with: 'First Comment'
+    click_on 'Save'
     expect(page).not_to have_content('First Comment')
   end
 end 
