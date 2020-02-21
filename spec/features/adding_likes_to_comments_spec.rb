c4a943ea78c6022236dec7e619678a954cb3b784
Asia <asiaellis5@gmail.com>
2020-02-21 11:34:22 +0000

    test: feature tests liking comments

--- spec/features/adding_likes_to_comments_spec.rb
+++ spec/features/adding_likes_to_comments_spec.rb
@@ -1,20 +1,35 @@
 # frozen_string_literal: true
-
-feature 'user can like a post' do
-  scenario 'from the timeline' do
+feature 'user can like a comment' do
+  scenario 'from another users homepage' do
     sign_up
-    create_post
     click_link 'Home'
-    click_button 'Like'
-    expect(page).to have_content('1 Like')
-    expect(page).to have_selector(:link_or_button, 'Unlike')
-  end
-
-  scenario 'from the user homepage' do
-    sign_up
     create_post
-    click_button 'Like'
+    click_link 'Sign Out'
+    sign_up_two
+    click_link 'Home'
+    click_link 'uvalente'
+    create_comment
+    click_button 'Like Comment'
     expect(page).to have_content('1 Like')
-    expect(page).to have_selector(:link_or_button, 'Unlike')
+    expect(page).to have_selector(:link_or_button, 'Unlike Comment')
+  end
+  scenario 'from the homepage' do
+    sign_up
+    click_link 'Home'
+    create_post
+    create_comment
+    click_button 'Like Comment'
+    expect(page).to have_content('1 Like')
+    expect(page).to have_selector(:link_or_button, 'Unlike Comment')
   end
 end
+
+
+
+
+
+
+
+
+
+
