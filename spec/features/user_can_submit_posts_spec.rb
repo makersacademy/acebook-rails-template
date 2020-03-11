ee444c407ab3f0abbdc4f92b24bebe6c2536c3f5
Ovy95 <jackoverton21@hotmail.com>
2020-03-11 10:39:55 +0000

    Add test for posts to appear in reverse chronological order
    Added gem file 'Orderly' to test for order of the content.

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -17,4 +17,21 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to have_content(Time.now.utc)
   end
 
+  scenario "Posts appear in reverse chronological order" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Last"
+    click_button "Submit"
+
+    click_link "New post"
+    fill_in "Message", with: "Middle"
+    click_button "Submit"
+
+    click_link "New post"
+    fill_in "Message", with: "First"
+    click_button "Submit"
+
+    expect("First").to appear_before("Middle")
+    expect("Middle").to appear_before("Last")
+  end
 end
