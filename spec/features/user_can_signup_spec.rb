94f82e69fb8fbd2cad42905423403fe4de2b99b5
Student <emilywright212@gmail.com>
2020-03-11 16:09:58 +0000

    Sign up forms only take email addresses

--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.feature "Sign Up", type: :feature do
   scenario "User can sign up" do
     visit "/users/new"
-    fill_in "Email", with: "testemail"
+    fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password"
     click_button "Submit"
-    expect(page).to have_content("New post")
+    expect(page).to have_link("New post")
   end
 end
