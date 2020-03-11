18b46881fbc3a7d301da095caf8bd88c7d370ab6
Student <student@Makerss-MacBook-Air-6.local>
2020-03-11 12:29:47 +0000

    Fix all test

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit("/posts") # should redirect to login
-    click_button("Sign up")
+    visit("/posts") # should redirect to sign_in
+    click_link("Sign up")
     fill_in "user_email", with: "test@example.com"
     fill_in "user_password", with: "example"
     fill_in "user_password_confirmation", with: "example"
