92ba77c69a414e6c02dfcb0aee4eb6a5eadf3c49
Student <emilywright212@gmail.com>
2020-03-11 11:41:56 +0000

    Signup can add params to database

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,9 +1,17 @@
 class SignupsController < ApplicationController
   def new
+    # @user = User.new
   end
   def create
-    # @user = Signup.new(params[:signup])
-    render plain: params[:signup].inspect
+    @user = User.create(user_params)
+    # @signup.save
+    render plain: user_params.inspect
     # render plain: @user.inspect
+  end
+
+  private
+
+  def user_params
+     params.require(:signup).permit(:email, :password)
   end
 end
