7f2bfa3e6cdef227e42494ba6c7a612b7a4d5258
Josh Blumberg <jlblumberg@gmail.com>
2020-03-11 09:30:55 +0000

    Add test for validate email

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Sign up", type: :feature do
+
   scenario 'User can sign up' do
     visit('/users/sign_up')
     fill_in "user_email", with: "test@example.com"
@@ -9,4 +10,14 @@ RSpec.feature "Sign up", type: :feature do
     click_button("Sign up")
     expect(current_path).to eq('/')
   end
+
+  scenario "User can't sign up with something other than an email address" do
+    visit('/users/sign_up')
+    fill_in "user_email", with: "testexample.com" # invalid, not am email
+    fill_in "user_password", with: "example"
+    fill_in "user_password_confirmation", with: "example"
+    click_button("Sign up")
+    expect(page).to have_content("Email is invalid")
+  end
+
 end
