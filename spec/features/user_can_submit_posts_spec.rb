dd6c50f444313308f22ef2f274aa733699d1e918
Josh Blumberg <jlblumberg@gmail.com>
2020-03-11 15:47:21 +0000

    Update posts tests to log in before creating post

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -16,12 +16,24 @@ RSpec.feature "Timeline", type: :feature do
   end
 
   scenario "Can submit posts and view them with the time they were posted at." do
-    visit "/posts"
+    visit("/posts") # all pages redirect to sign_in b/c user isn't signed up
+    click_link("Sign up")
+    fill_in "user_email", with: "test@example.com"
+    fill_in "user_password", with: "example"
+    click_button("Sign up")
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world!")
     expect(page).to have_content(DateTime.now.strftime('%e %B at %H:%M'))
   end
 
   scenario "Posts appear in reverse chronological order" do
-    visit "/posts"
+    visit("/posts") # all pages redirect to sign_in b/c user isn't signed up
+    click_link("Sign up")
+    fill_in "user_email", with: "test@example.com"
+    fill_in "user_password", with: "example"
+    click_button("Sign up")
     click_link "New post"
     fill_in "Message", with: "Last"
     click_button "Submit"
