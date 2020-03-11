25dd17386b263e5939f6dd3480d78e81cb84ed21
Josh Blumberg <jlblumberg@gmail.com>
2020-03-11 10:31:57 +0000

    Add passing test for password too long

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -13,7 +13,7 @@ RSpec.feature "Sign up", type: :feature do
 
   scenario "User can't sign up with something other than an email address" do
     visit('/users/sign_up')
-    fill_in "user_email", with: "testexample.com" # invalid, not am email
+    fill_in "user_email", with: "testexample.com" # invalid, not an email
     fill_in "user_password", with: "example"
     fill_in "user_password_confirmation", with: "example"
     click_button("Sign up")
@@ -32,7 +32,7 @@ RSpec.feature "Sign up", type: :feature do
   scenario "User should be able to signup with a valid password (6 characters)" do
     visit('/users/sign_up')
     fill_in "user_email", with: "test@example.com" 
-    fill_in "user_password", with: "123456" #password is valid
+    fill_in "user_password", with: "123456" # password is valid
     fill_in "user_password_confirmation", with: "123456"
     click_button("Sign up")
     expect(current_path).to eq('/')
@@ -41,10 +41,10 @@ RSpec.feature "Sign up", type: :feature do
   scenario "User can't sign up with something with password more than 10 (inclusive) characters" do
     visit('/users/sign_up')
     fill_in "user_email", with: "test@example.com" 
-    fill_in "user_password", with: "12345678910" #not a valid password
-    fill_in "user_password_confirmation", with: "12345"
+    fill_in "user_password", with: "12345678910" # not a valid password
+    fill_in "user_password_confirmation", with: "12345678910"
     click_button("Sign up")
-    expect(page).to have_content("Password is too short")
+    expect(page).to have_content("Password is too long")
   end
 
 end
