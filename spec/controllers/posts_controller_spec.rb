d34d0fec697a16c285a7dc4876f6c64c189b4e32
Student <student@Makerss-MacBook-Air-6.local>
2020-03-11 11:40:26 +0000

    Fix the GET/ test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe PostsController, type: :controller do
   describe "GET /" do
     it "responds with 200" do
       get :index
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
