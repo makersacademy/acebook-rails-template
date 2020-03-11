2b07afdc1925ec43daf7aab9eebf3da9fe5a552b
Ovy95 <jackoverton21@hotmail.com>
2020-03-11 10:46:28 +0000

    Refactored tests

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,19 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and view them" do
+  scenario "Can submit posts and view them with the time they were posted at." do
     visit "/posts"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
-  end
-
-  scenario "Posts have time" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
     expect(page).to have_content(Time.now.utc)
   end
 
