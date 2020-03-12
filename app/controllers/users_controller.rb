0b1e0b5a75a1513c147dd428ecd5621f3963d92d
Jason <jasylwong@gmail.com>
2020-03-12 14:59:39 +0000

    tdd restricting sign up password length

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    redirect_to posts_url
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to posts_url
+    else
+      render 'new'
+    end
   end
 
   private
--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -8,4 +8,21 @@ RSpec.feature "Sign Up", type: :feature do
     click_button "Submit"
     expect(page).to have_link("New post")
   end
+
+  scenario 'Returns error message if password too long' do
+    visit '/users/new'
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "passwordpassword"
+    click_button "Submit"
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Password is too long')
+  end
+  scenario 'Returns error message if password too short' do
+    visit '/users/new'
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "pass"
+    click_button "Submit"
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Password is too short')
+  end
 end
