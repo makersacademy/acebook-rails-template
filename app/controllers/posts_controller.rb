0dc3e34b5e12888ce8d29c47d85a85c6c09940a2
Joe Ellingworth <joeellingworth@gmail.com>
2020-03-12 15:04:20 +0000

    added user_id foreign key to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:message)
+    params.require(:post).permit(:message).merge(user_id: current_user.id)
   end
 end
