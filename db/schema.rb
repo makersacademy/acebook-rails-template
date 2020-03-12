39c57d85fa08ed3054e152e21a71639d5b460e73
Toby Dawson <tobydawson1@gmail.com>
2020-03-12 16:09:09 +0000

    Posts only shown if posted by user, partially tested

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20200311145316) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
-
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,5 +34,4 @@ ActiveRecord::Schema.define(version: 20200311145316) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "posts", "users"
 end
