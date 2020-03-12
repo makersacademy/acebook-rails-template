c9295cd92b3c14f33dbc1430f02c22e067dedccb
Richard Pattinson <richie.pattinson@googlemail.com>
2020-03-12 17:09:17 +0000

    passing test for updating post

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -7,9 +7,16 @@ def sign_up
     click_button "Sign up"
 end
 
-    def sign_in
-        visit('/users/sign_in')
-        fill_in "Email", with: "test@example.com"
-        fill_in "Password", with: "double"
-        click_button "Log in"
-    end
+def sign_in
+    visit('/users/sign_in')
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "double"
+    click_button "Log in"
+end
+
+def make_test_post
+    visit ('/')
+    click_link "New post"
+    fill_in 'post_message', with: "this is a unique test post"
+    click_button "Submit"
+end
