91cfdd60c752946c41e1cfce16aa9bacdf972f32
hansa-lallu <hansalallu@gmail.com>
2020-03-12 18:51:16 +0000

    Removal of commented out code

--- spec/web_helper/user_sign_up_helper.rb
+++ spec/web_helper/user_sign_up_helper.rb
@@ -4,8 +4,3 @@ def user_sign_up(email = "testing@test.com", password = "password")
   fill_in "Password", with: password
   click_button "Sign up"
 end
-
-# visit "/users/sign_up"
-# fill_in 'Email', with: email
-# fill_in 'Password', with: password
-# click_button 'Sign up'
