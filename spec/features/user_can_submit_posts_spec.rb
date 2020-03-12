785eab136dbdc8237698cee1bb904111d83164f5
hansa-lallu <hansalallu@gmail.com>
2020-03-12 19:46:34 +0000

    fixed test with time and date and added test for two posts

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -10,4 +10,20 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+
+  scenario "Can submit two posts and view them both on the same page" do
+    user_sign_up
+    
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    
+    click_link "New post"
+    fill_in "Message", with: "Hello, world again!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world!")
+    expect(page).to have_content("Hello, world again!")
+
+  end
 end
