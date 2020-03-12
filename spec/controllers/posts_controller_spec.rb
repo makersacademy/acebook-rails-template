112b7e3b209a95f930d029024a65abb721d644ef
hansa-lallu <hansalallu@gmail.com>
2020-03-12 20:06:46 +0000

    Added feature test for desc order posts

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -28,11 +28,4 @@ RSpec.describe PostsController, type: :controller do
       expect(Post.find_by(message: "Hello, world!")).to be
     end
   end
-
-  describe "GET /" do
-    it "responds with 200" do
-      get :index
-      expect(response).to have_http_status(200)
-    end
-  end
 end
