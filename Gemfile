6807e1fa7af69077d55f8539d55fb77a560f0543
Paulo Azoia <pazoia.gomes@gmail.com>
2020-03-12 15:42:22 +0000

    Added devise

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+# Installing devise for user
+gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.1'
 # Use postgresql as the database for Active Record
--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -1,28 +1,50 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# feature 'User signs up' do
-#   scenario 'with valid email and password' do
-#     user_sign_up 'valid@example.com', 'password'
+RSpec.feature "Sign up", type: :feature do
 
-#     expect(page).to have_content('Sign out')
-#   end
+  scenario "Non-signed up user can't visit any page other than sign_in" do
+    visit('/posts')
+    expect(current_path).to eq('/users/sign_in')
+  end
+  
+  scenario 'User can sign up' do
+    visit('/users/sign_up')
+    fill_in "user_email", with: "testing@test.com"
+    fill_in "user_password", with: "example"
+    click_button("Sign up")
+    expect(current_path).to eq('/')
+  end
 
-#   scenario 'with invalid email' do
-#     user_sign_up 'invalid_email', 'password'
+  scenario "User can only sign up with a email address" do
+    visit('/users/sign_up')
+    fill_in "user_email", with: "test.com" 
+    fill_in "user_password", with: "example"
+    click_button("Sign up")
+    expect(page).to have_content("Email is invalid")
+  end
 
-#     expect(page).to have_content('Sign in')
-#   end
+  scenario "User can't sign up with a password less than 6 (inclusive) characters" do
+    visit('/users/sign_up')
+    fill_in "user_email", with: "testing@test.com" 
+    fill_in "user_password", with: "12345" 
+    click_button("Sign up")
+    expect(page).to have_content("Password is too short")
+  end
 
-#   scenario 'with blank password' do
-#     user_sign_up 'valid@example.com', ''
+  scenario "User can signup with a valid password (6 characters)" do
+    visit('/users/sign_up')
+    fill_in "user_email", with: "testing@test.com" 
+    fill_in "user_password", with: "123456" 
+    click_button("Sign up")
+    expect(current_path).to eq('/')
+  end
 
-#     expect(page).to have_content('Sign in')
-#   end
+  scenario "User can't sign up with something with password more than 10 (inclusive) characters" do
+    visit('/users/sign_up')
+    fill_in "user_email", with: "testing@test.com" 
+    fill_in "user_password", with: "12345678910" 
+    click_button("Sign up")
+    expect(page).to have_content("Password is too long")
+  end
 
-#   # def sign_up_with(email, password)
-#   #   visit "/users/sign_up"
-#   #   fill_in 'Email', with: email
-#   #   fill_in 'Password', with: password
-#   #   click_button 'Sign up'
-#   # end
-# end
+end
