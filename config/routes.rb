a576e9c203830cf69d87f746a2cc26d3f8d5c8a0
Student <emilywright212@gmail.com>
2020-03-13 11:07:53 +0000

    Home page is sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
   resources :users
 
-  root 'welcome#index'
+  root 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
+
+  get '*path' => redirect('/users/new')
 end
--- spec/features/user_can_signup_spec.rb
+++ spec/features/user_can_signup_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "Sign Up", type: :feature do
+  scenario "Sign up page is seen as home page" do
+    visit "/"
+    expect(page).to have_content("Please Sign Up!")
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "password"
+    click_button "Submit"
+  end
   scenario "User can sign up" do
     visit "/users/new"
     fill_in "Email", with: "test@example.com"
