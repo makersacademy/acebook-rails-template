3504ef3562977fb720776c0831261e4764daf14d
DavidStewartLDN <davidstewart.work@gmail.com>
2020-03-16 10:21:02 +0000

    Add wall_id column to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200311145316) do
+ActiveRecord::Schema.define(version: 20200316100746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200311145316) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "wall_id"
   end
 
   create_table "users", force: :cascade do |t|
