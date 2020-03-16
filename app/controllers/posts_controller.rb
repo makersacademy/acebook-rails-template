04c6684417d40acfbd6162b0c761e4f91a6385dc
Ovy95 <jackoverton21@hotmail.com>
2020-03-16 14:19:11 +0000

    add Delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
   end
@@ -8,8 +13,10 @@ class PostsController < ApplicationController
     redirect_to posts_url
   end
 
-  def index
-    @posts = Post.all
+  def destroy 
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
