257051100e1f245a10f17c02e0cfdabb781ff15f
Mrtly <mrtly.ca@gmail.com>
2020-03-16 10:59:19 +0000

    changed test.com to example.com

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -13,25 +13,25 @@ RSpec.feature "Sign up", type: :feature do
   end
 
   scenario "User can only sign up with a email address" do
-    user_sign_up('test.com')
+    user_sign_up('example.com')
 
     expect(page).to have_content("Email is invalid")
   end
 
   scenario "User can't sign up with a password less than 6 (inclusive) characters" do
-    user_sign_up("testing@test.com", "12345")
+    user_sign_up("testin@example.com", "12345")
 
     expect(page).to have_content("Password is too short")
   end
 
   scenario "User can signup with a valid password (6 characters)" do
-    user_sign_up("testing@test.com", "123456")
+    user_sign_up("testin@example.com", "123456")
 
     expect(current_path).to eq('/')
   end
 
   scenario "User can't sign up with something with password more than 10 (inclusive) characters" do
-    user_sign_up("testing@test.com", "12345678910")
+    user_sign_up("testin@example.com", "12345678910")
 
     expect(page).to have_content("Password is too long")
   end
