3b8f9baee441b2ca02755a8224aa5ffb5958d552
Student <student@makerss-air.home>
2020-03-16 14:16:36 +0000

    update read me

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,8 @@ Rails.application.routes.draw do
   root  'posts#index'
   resources :posts
 end
+
+devise_for :users, skip: [:sessions]
+as :user do
+  get 'site-admin/login', to: 'devise/sessions#new', as: :new_user_session
+end
