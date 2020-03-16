e1e12c158ad0ce5cae8d8c726924e31315a05079
Dafna Libman <dafnalibman@gmail.com>
2020-03-16 11:08:29 +0000

    Update feature test

--- spec/webhelpers.rb
+++ spec/webhelpers.rb
@@ -5,3 +5,10 @@ def sign_up(email, password)
   click_button("Sign up")
 end
 
+def login(email, password)
+  User.create(name: 'Test Name', email: email, password: password)
+    visit('/posts')
+    fill_in "user_email", with: email
+    fill_in "user_password", with: password
+    click_button("Log in")
+end 
