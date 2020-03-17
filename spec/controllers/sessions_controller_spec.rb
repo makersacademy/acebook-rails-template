47639a060df18c99e5f206fcffa64adb1df9dc5d
Jason <jasylwong@gmail.com>
2020-03-17 11:47:44 +0000

    correct sessions redirect test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe SessionsController, type: :controller do
 
   describe "GET #new" do
-    it "returns http success" do
+    it "returns http redirect" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 end
