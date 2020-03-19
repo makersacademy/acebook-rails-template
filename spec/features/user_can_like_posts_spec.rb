f18d268c14cd57de2185a8e2d017ed696bd5f91f
Jason <jasylwong@gmail.com>
2020-03-19 14:20:08 +0000

    Added test for unliking a post

--- spec/features/user_can_like_posts_spec.rb
+++ spec/features/user_can_like_posts_spec.rb
@@ -9,9 +9,25 @@ RSpec.feature "Liking posts", type: :feature do
     fill_in "Message", with: "On my momma I be breathing"
     click_button "Submit"
     expect(current_path).to eq('/posts')
-    # expect(page).to have_link('&#128568')
     this = page.find_by_id('like')
     this.click
     expect(page).to have_content("\n1\n😸")
+  end
+  scenario "User can unlike posts" do
+    sign_up
+    expect(page).to have_content("You have no posts... :(")
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "On my momma I be breathing"
+    click_button "Submit"
+    expect(current_path).to eq('/posts')
+    this = page.find_by_id('like')
+    this.click
+    expect(page).to have_content("\n1\n😸")
+    this = page.find_by_id('liked')
+    this.click
+    expect(page).to have_content("😸")
+    page.refresh
+    expect(page).to have_content("\n0\n😸")
   end
 end
