70df88a2526113230805ca86dcb2032ac71fffad
Ellie Richardson-Jones <elliebethjones@gmail.com>
2020-03-20 10:19:20 +0000

    Add top navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
   
   def require_login
     redirect_to '/users/new' unless session[:current_user_id]
-
   end  
 
+  def user_signed_in
+    return true unless session[:current_user_id] == nil
+  end
 end
