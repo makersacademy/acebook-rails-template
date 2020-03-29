8921601d3212051b6fefd38f3f250ac5d95bd018
GuyCalloway <guyfcalloway@gmail.com>
2020-03-29 12:57:29 +0100

    Added unlike feature, removed cant like more than once test as  no longer needed

--- spec/features/user_can_like_post_spec.rb
+++ spec/features/user_can_like_post_spec.rb
@@ -14,23 +14,14 @@ RSpec.feature 'Liking Posts', type: :feature do
       expect(page).to have_content('1 Like')
     end
 
-      scenario 'Cant like post more than once' do
+      scenario 'can unlike a previously liked post' do
         create_post
         click_link 'See Post'
         click_button 'Like'
         click_link "Back"
         click_link 'See Post'
-        click_button 'Like'
-        expect(page).to have_content('You have already liked this post')
-      end
-
-      scenario 'User can unlike a previously liked post' do
-        create_post
-        click_link 'See Post'
-        click_button 'Like'
+        click_button 'Unlike'
         click_link "Back"
-        click_link 'See Post'
-        click_button 'unLike'
         expect(page).to have_content('0 Likes')
       end
     end
