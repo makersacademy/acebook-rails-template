c9519e297348dbb286cab613d2dfc74e33255f78
GuyCalloway <guyfcalloway@gmail.com>
2020-03-29 10:27:35 +0100

    Added Like button to user's wall, displays number of likes on posts index(aka homepage)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,19 @@
 class LikesController < ApplicationController
+  before_action :find_post
+
+  def create
+    @post.likes.create(user_id: current_user.id)
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id:
+    params[:post_id]).exists?
+  end
 end
