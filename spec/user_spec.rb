a686eb74317bb91ef59ae2efcfe29dafb704161d
Gareth Harris <gdharris1000@gmail.com>
2020-04-07 15:22:26 +0100

    login method

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,11 +1,10 @@
 require 'user.rb'
 describe User do
+  before(:each) do
+    @user = User.create(name: 'testing', dob: '04/04/20', cob: 'england', mob: 999, email: 'test@test.com', password: 'password')
+  end
 
   describe "#create" do
-
-    before(:each) do
-      @user = User.create(name: 'testing', dob: '04/04/20', cob: 'england', mob: 999, email: 'test@test.com', password: 'password')
-    end
 
     it 'Can create a new user' do
       expect(@user.email).to eq 'test@test.com'
@@ -18,4 +17,11 @@ describe User do
       expect{User.create(name: 'testing', dob: '04/04/20', cob: 'england', mob: 999, email: 'test@test.com', password: 'password')}.to raise_error "E-mail address already exists"  
     end
   end
+
+  describe "#login" do
+    it 'returns true if email and password are a match in the database' do
+      expect(User.login(email: 'test@test.com', password: 'password')).to be_truthy
+    end
+  end
+
 end
