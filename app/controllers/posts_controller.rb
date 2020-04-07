4067ffa0e73f23c1a588c68962ff7efd2ae07228
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-04-07 16:29:03 +0100

    Managed to save and post messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,18 +4,14 @@ class PostsController < ApplicationController
 #     @post = Post.new
 #   end
 
-#   def create
-#     @post = Post.create(post_params)
-#     redirect_to posts_url
-#   end
+  def create
+    #Please look at sessions
+    @user = User.create(name: 'testing', dob: '04/04/20', cob: 'england', mob: 999, email: 'test@test.com', password: 'password')
+    @post = Post.create(poster_id: @user.id, content: params[:message], time: Time.now)
+    redirect_to posts_url
+  end
 
-#   def index
-#     @posts = Post.all
-#   end
-
-#   private
-
-#   def post_params
-#     params.require(:post).permit(:message)
-#   end
- end
+  def index
+    @posts = Post.all
+  end
+end
