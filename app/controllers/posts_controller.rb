738eb159ccc1fbdc711210ff4aa39826d283672b
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-04-08 11:58:53 +0100

    "Added create posts"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,9 @@
 class PostsController < ApplicationController
   def new
-    @post = Post.new
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_url
+    render plain: params[:post].inspect
   end
 
-  def index
-    @posts = Post.all
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:message)
-  end
 end
