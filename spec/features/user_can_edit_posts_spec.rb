d5229c0c509990eae07b796415772546d67c611e
liam roberts <liamroberts77@googlemail.comgit config --global user.email liamroberts77@googlemail.com>
2020-04-08 09:56:08 +0100

    edit feature test passing

---
+++ spec/features/user_can_edit_posts_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature 'Timeline', type: :feature do
+  scenario 'Can edit posts and view them' do
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+    click_link 'Edit Post'
+    fill_in 'Message', with: 'Hi'
+    click_button 'Submit'
+    expect(page).to have_content('Hi')
+  end
+end
