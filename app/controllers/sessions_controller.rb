14e491b5dd0c4bf54c11e38750a2042b03b87cff
Patrick Oliver <poliver24@hotmail.com>
2020-04-08 10:11:11 +0100

    spelling correction on login error alert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now[:alert] = "Email or password if invalid"
+      flash.now[:alert] = "Email or password is invalid"
       render "new"
     end
   end
