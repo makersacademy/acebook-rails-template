da240daf692fefb48bd24b156b0b21fdefa7679c
James Oddy <jamesoddy8@gmail.com>
2020-04-08 16:09:56 +0100

    can delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,17 @@
 class PostsController < ApplicationController
+  
   def index
     @posts = Post.all
   end
+
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def new
     @post=Post.new
   end
+
   def edit
     @post=Post.find(params[:id])
   end
@@ -22,15 +25,22 @@ class PostsController < ApplicationController
     render 'new'
    end
   end
+
   def update
     @post = Post.find(params[:id])
- 
-  if @post.update(post_params)
-    redirect_to @post
-  else
-    render 'edit'
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private
