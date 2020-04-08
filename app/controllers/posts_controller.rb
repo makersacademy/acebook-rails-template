0da9f109c17489b30f0b499316355cfbb4d546a5
sgneha <sgneha05@gmail.com>
2020-04-08 15:04:27 +0100

    gives error when either title or text is empty

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,13 +7,17 @@ class PostsController < ApplicationController
   end
   
   def new
+    @post=Post.new
   end
 
   def create
     @post = Post.new(post_params)
 
-    @post.save
+   if @post.save
     redirect_to @post
+   else
+    render 'new'
+   end
   end
 
   private
