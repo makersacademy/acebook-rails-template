386e4d018840d9575797d56343443bb5eb97cf38
sgneha <sgneha05@gmail.com>
2020-04-08 15:28:06 +0100

    created edit page which is autofilled

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,9 @@ class PostsController < ApplicationController
   def new
     @post=Post.new
   end
+  def edit
+    @post=Post.find(params[:id])
+  end
 
   def create
     @post = Post.new(post_params)
@@ -18,6 +21,16 @@ class PostsController < ApplicationController
    else
     render 'new'
    end
+  end
+  def update
+    @post = Post.find(params[:id])
+ 
+  if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit'
+  end
+
   end
 
   private
