c2b3a60c0ce844414999b8ca71b66838a5e35f2a
James Oddy <jamesoddy8@gmail.com>
2020-04-08 17:39:30 +0100

    added posts table

--- db/schema.rb
+++ db/schema.rb
@@ -15,4 +15,11 @@ ActiveRecord::Schema.define(version: 20200408110213) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
