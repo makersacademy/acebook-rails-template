3bb960ecff2ae357db2fe09ecafcee218cee0e89
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-04-09 16:11:56 +0100

    added tests for Like class

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,18 +1,31 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe PostsController, type: :controller do
-#   describe "GET /new " do
-#     it "responds with 200" do
-#       get :new
-#       expect(response).to have_http_status(200)
-#     end
+RSpec.describe PostsController, type: :controller do
+  describe "GET /new " do
+    it "responds with 200 if not logged in" do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+end
+end
+
+#     it "sets up the homepage" do
+#         user = create(:user)
+#         sign_in user
+#         get :users
+#         expect(subject).to render_template('users')
 #   end
+# end
+
 
 #   describe "POST /" do
-#     it "responds with 200" do
-#       post :create, params: { post: { message: "Hello, world!" } }
+#     it "redirects to posts url when post is created" do
+#       user = create_a_user(:user)
+#       sign_in user
+#       post :create_a_user, params: { post: { message: "What up"} }
 #       expect(response).to redirect_to(posts_url)
 #     end
+# end
 
 #     it "creates a post" do
 #       post :create, params: { post: { message: "Hello, world!" } }
