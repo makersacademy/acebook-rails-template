b07bf0b64f6cdde57f762c24db789e53bbdf4d3d
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-04-09 15:39:21 +0100

    fixed tests

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,5 +2,5 @@ require 'pg'
 def setup_test_database 
   p 'Setting up test database'
   connection = PG.connect(dbname: 'pgapp_test')
-  connection.exec('TRUNCATE users;')
+  connection.exec('TRUNCATE users, likes;')
 end
