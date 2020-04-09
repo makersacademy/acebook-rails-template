004a3756b57f621c2b28dc3816c26d6f4598a99f
Orion Reed <orionreedt@gmail.com>
2020-04-09 12:01:16 +0100

    fixed(?) users view tests

--- spec/views/users/edit.html.erb_spec.rb
+++ spec/views/users/edit.html.erb_spec.rb
@@ -2,13 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "users/edit", type: :view do
   before(:each) do
-    @user = assign(:user, User.create!())
+    assign(:user, User.new())
   end
 
   it "renders the edit user form" do
     render
-
-    assert_select "form[action=?][method=?]", user_path(@user), "post" do
-    end
+    assert_select "form[action=?][method=?]", users_path, "post"
   end
 end
