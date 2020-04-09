becf2c74a78cebaf96f580aa6685a1b6497725df
WilliamJGrace <williamjgrace@outlook.com>
2020-04-09 16:21:53 +0100

    User get redirect to landing page if not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
+
+
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -10,6 +13,11 @@ class ApplicationController < ActionController::Base
   def create_session(id)
     session[:user_id] = id
   end
-  
+
+  def require_login
+     redirect_to ('/') unless current_user
+
+  end
+
   helper_method :current_user, :create_session
 end
