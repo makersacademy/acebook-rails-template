72aed1aa1d1eefb049c5b0b45f24686c635d1ac7
WilliamJGrace <williamjgrace@outlook.com>
2020-04-09 12:23:30 +0100

    Updated unit tests for editing and updating a post

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,6 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  let(:valid_attributes) {
+    skip(message: "test")
+  }
+
+  let(:valid_session) { {} }
+
+
   describe 'GET /new ' do
     it 'responds with 200' do
       get :new
@@ -10,13 +17,29 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-  describe 'GET /edit ' do
-    it 'responds with 200' do
-      post :create, params: { post: { message: 'Hello, world!', id: 1 } }
-      get :posts/1/edit
-      expect(response).to have_http_status(200)
+  # describe 'GET /edit ' do
+  #   it 'responds with 200' do
+  #     post :create, params: { post: { message: 'Hello, world!', id: 1 } }
+  #     get :posts/1/edit
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
+  describe "GET #edit" do
+    it "returns a success response" do
+      post = Post.create(message: "hi")
+      get :edit, params: {id: post.to_param}, session: valid_session
+      expect(response).to be_success
     end
   end
+  describe "POST #edit" do
+    it "updates the requested user" do
+       post = Post.create(message: "hi")
+       put :update, params: {id: post.to_param, post: {message: "hello"}}, session: valid_session
+       post.reload
+       expect(post.message).to_not eq "hi"
+     end
+   end
+
 
   describe 'POST /' do
     it 'responds with 200' do
