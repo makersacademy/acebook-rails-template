a77794235309d04ad0a762fa4a4a2d38d44ed304
Orion Reed <orionreedt@gmail.com>
2020-04-09 17:51:21 +0100

    added root rails route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
-  get 'sessions/new'
+  root 'posts#index'
+  #get 'sessions/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 # get 'site/home'
