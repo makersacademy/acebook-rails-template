1da39070ade5cfe77f7912661a983d2f28182b71
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-04-09 16:18:55 +0100

    Refector Like Tests

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,4 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
-  belongs_to :likeable
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -5,12 +5,7 @@ RSpec.describe Like, type: :model do
     association = described_class.reflect_on_association(:user).macro
     expect(association).to eq :belongs_to
   end
-
-  it 'belongs to likeable' do
-    association = described_class.reflect_on_association(:likeable).macro
-    expect(association).to eq :belongs_to
-  end
-
+  
   it 'belongs to posts' do
     association = described_class.reflect_on_association(:post).macro
     expect(association).to eq :belongs_to
