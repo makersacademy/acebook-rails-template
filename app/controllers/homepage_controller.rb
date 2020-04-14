5d7d42e7749431de82b79611faa7818e7e2cef56
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-04-14 11:10:25 +0100

    Added functionality so that logged in users only see posts by users that they follow, in order that they were created

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -3,7 +3,12 @@ class HomepageController < ApplicationController
     def index
         @user = User.find(session[:current_user])
         @users = User.all
-        @posts = Post.all
+        @posts = []
+        @user.following.each do | follower |
+            @posts << Post.where(:poster_id => follower.id) 
+            @posts = @posts.flatten.sort_by{ | post | post.time }
+        end
+        # @posts = Post.all
         @comments = Comment.all
     end
 
