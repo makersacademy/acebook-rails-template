eb614cf64117d13ecd79270c443fead41f3cf417
Gareth Harris <gdharris1000@gmail.com>
2020-04-15 14:51:07 +0100

    tests and like+dislike icons

--- spec/features/comment_feature_spec.rb
+++ spec/features/comment_feature_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "comments", type: :feature do
+RSpec.feature "Comments", type: :feature do
     scenario 'can comment on other peoples posts' do
         User.create(name: "stephan", dob: '2020-04-04', cob: 'south africa', mob: 112, email: 'stephan@test.com', password: "password")
         visit '/'
@@ -8,9 +8,9 @@ RSpec.feature "comments", type: :feature do
         fill_in 'password', with: "password"
         click_button 'Sign In'
         visit '/home'
-        click_link 'Write a new post'
+        click_link 'Make a new post'
         fill_in 'message', with: 'This is a new post'
-        click_button 'Post'
+        click_button 'Submit Post'
         expect(page).to have_content("This is a new post")
     end
 end
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,5 +2,5 @@ require 'pg'
 def setup_test_database 
   p 'Setting up test database'
   connection = PG.connect(dbname: 'pgapp_test')
-  connection.exec('TRUNCATE users, likes, dislikes, albums;')
+  connection.exec('TRUNCATE users, likes, dislikes, albums, images;')
 end
