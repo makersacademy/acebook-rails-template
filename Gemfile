aff848c03120c12ef38e45f68590a3e6ec2cae08
plkujaw <jawor.jakub@gmail.com>
2020-04-15 10:44:56 +0100

    added paperclip gem, updated post model to inlcude image attachement

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'rails', '~> 5.1.1'
 gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
+gem 'paperclip'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,7 @@ class Post < ApplicationRecord
     belongs_to :user
 
     validates :message, presence: true
-    # validates 
+    # validates
+    has_attached_file :photo, styles { medium: '300x300>', thumb: '100x100>'}
+    validates_attachement_content_type :photo, content_type: /\Aimage\/.*\z/
 end
