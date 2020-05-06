e425018b6c46994cac5e6834ab2873c2b9a1dcf9
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-06 15:53:39 +0100

    fixed errors according to rubocop

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.1'
 # Use postgresql as the database for Active Record
@@ -49,9 +48,8 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :development, :test do
   gem "rubocop", "0.79.0"
-  gem 'simplecov', :require => false
+  gem 'simplecov',  require: false
   gem "rubycritic", require: false
-
 end
 
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 
-#  require 'simplecov'
-#  SimpleCov.start
-
- require 'simplecov'
- SimpleCov.start 'rails'
-
+require 'simplecov'
+SimpleCov.start 'rails'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -37,7 +33,6 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
