cd7079e29bb99b43f5ccf952bff65bb735cc35f7
Alexa <alexakearns@outlook.com>
2020-05-06 11:59:43 +0100

    create feature test for editing post - red

---
+++ spec/features/user_can_edit_post_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "Edit Post", type: :feature do
+  scenario "user can edit existing post" do
+    post = Post.create!(message: "Hello World!")
+    visit "/posts"
+    click_button "Edit post"
+    fill_in "Message", with: "I'm a new message"
+    click_button "Submit"
+    expect(page).to have_content("I'm a new message")
+    expect(page).not_to have_content("Hello World!")
+  end
+end
