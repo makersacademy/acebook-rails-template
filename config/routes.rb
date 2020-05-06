3646fedeaa1d44ce6bf2e3321acaf8cb96a17203
Steven <Steven@Stevens-iMac.local>
2020-05-06 14:27:41 +0100

    added login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create]
 
-  resources :posts
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
+  get 'welcome', to: 'sessions#welcome'
+  
+  get 'authorized', to: 'sessions#page_requires_login'
 end
