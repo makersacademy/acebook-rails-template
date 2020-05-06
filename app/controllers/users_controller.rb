9257c7b35cc74856ae13b64a85c7074adab756e0
Steven <Steven@Stevens-iMac.local>
2020-05-06 15:39:01 +0100

    login now has email, name, password, needs a log out button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   end
   def create
      @user = User.create(params.require(:user).permit(:username,        
-     :password))
+     :password, :email, :firstname, :lastname))
      session[:user_id] = @user.id
      redirect_to '/welcome'
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200506094328) do
+ActiveRecord::Schema.define(version: 20200506143407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,12 @@ ActiveRecord::Schema.define(version: 20200506094328) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
   end
 
 end
