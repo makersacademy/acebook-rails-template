9dd2a377aa989c4276ab4b113f0f8084eb4b7fb8
Angelica <Angelica137@users.noreply.github.com>
2020-05-07 16:50:24 +0100

    add user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    p params
+    @user = User.new(user_params)
+    
+    p @user.save
+    p @user
+    @this_user = User.find(@user.id)
+    p @this_user
+    render plain: @user.inspect
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+      p 'Hello!'
+    end
 end
--- spec/features/user_can_singup_spec.rb
+++ spec/features/user_can_singup_spec.rb
@@ -3,13 +3,12 @@ require 'rails_helper'
 
 RSpec.feature "Signup", type: :feature do
   scenario "Can signup with their name, last name, email and password" do
-    visit "/"
-    click_button "Sign up"
-    fill_in "First_name", with: "Kevin"
-    fill_in "Last_name", with: "Bacon"
-    fill_in "Email", with: "email@bacon.com"
-    fill_in "Password", with: "password"
-    click_button "submit"
+    visit "/users/new"
+    fill_in "user[first_name]", with: "Kevin"
+    fill_in "user[last_name]", with: "Bacon"
+    fill_in "user[email]", with: "email@bacon.com"
+    fill_in "user[password]", with: "password"
+    click_button "Submit"
     expect(page).to have_content("Welcome")
   end
 end
