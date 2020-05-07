d2535cce8607dea1dce034e48d7d6cca4b1f068b
Angelica <Angelica137@users.noreply.github.com>
2020-05-07 12:10:28 +0100

    create user controller

--- spec/features/user_can_singup_spec.rb
+++ spec/features/user_can_singup_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 
 RSpec.feature "Signup", type: :feature do
   scenario "Can signup with their name, last name, email and password" do
-    visit "/"
-    click_button "Sign up"
+    visit "/users/new"
     fill_in "First_name", with: "Kevin"
     fill_in "Last_name", with: "Bacon"
     fill_in "Email", with: "email@bacon.com"
