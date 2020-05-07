e18427af9ed4c7623b6252cabb57ae7baeb9c03b
Angelica <Angelica137@users.noreply.github.com>
2020-05-07 12:10:28 +0100

    create user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
+  get 'user/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
