7dde20a106ad12bfa3178c66a2b85df47c1b9539
Alexa <alexakearns@outlook.com>
2020-05-11 11:27:10 +0100

    sign up spec

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -6,9 +6,11 @@ RSpec.feature "Sign-up", type: :feature do
     expect(page).to have_content("Sign-up")
   end
 
-  scenario "User can sign up with their name" do
+  scenario "User can sign up with their name, email and password" do
     visit "/"
     fill_in "user[name]", with: "Gina"
+    fill_in "user[email]", with: "gina@example.com"
+    fill_in "user[password]", with: "12345"
     click_on "Sign up"
     expect(page).to have_content("Welcome Gina!")
   end
