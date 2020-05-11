02a34f280e1882defcfa3583bcf857668c93bfbe
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-11 14:26:46 +0100

    email and password need to be present to validate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,28 +2,37 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe 'valid user' do
+  describe 'valid user:' do
     it "standard user should be valid" do
       user = User.new(name: "Gina", email: "gina@example.com", password_digest: "12345")
-
       expect(user).to be_valid
     end
   end
 
   describe 'valid user name' do
     it "standard user name should be valid" do
-      user = User.new(name: "Gina")
+      user = User.new(name: "Gina", email: "gina@example.com", password_digest: "12345")
 
       expect(user).to be_valid
     end
 
     it "no user name should be invalid" do
-      user = User.new(name: nil)
-
+      user = User.new(name: nil, email: "gina@example.com", password_digest: "12345")
       expect(user).to_not be_valid
     end
   end
 
-  
-  
+  describe 'valid user email:' do
+    it  "if no email is entered user is invalid" do
+      user = User.new(name: 'Gina', email: nil, password_digest: "12345")
+      expect(user).to_not be_valid
+    end 
+  end 
+
+  describe 'valid user password:' do
+    it "if no password is entered user is invalid" do
+      user = User.new(name: "Gina", email: "gina@example.com", password_digest: nil)
+      expect(user).to_not be_valid
+    end 
+  end
 end
