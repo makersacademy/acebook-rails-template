6c50c40d767c3495a325d474df215f2d8a15ce67
Edward Phillips <edd0ddi0@gmail.com>
2020-05-11 12:13:32 +0100

    G: can't sign up with email address already in use

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    session[:user_id] = @user.id
-    redirect_to posts_path
+    @user = User.new(user_params)
+    if @user.save 
+      session[:user_id] = @user.id
+      redirect_to posts_path
+    else
+      flash[:warning] = "That email is already in use"
+      render "new"
+    end
   end
 
   private
