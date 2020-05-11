7ae5319ceef1f204483c3469f5226813d8a2be27
Gina Frankel <ginafrankel@Ginas-MacBook-Pro.local>
2020-05-11 16:34:40 +0100

    passing - password need to be   between 6 to 10 characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\w\S*@[a-z]*\.[a-z]{2,30}([a-z]|\.[a-z]{2,30})/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
   has_secure_password
-  validates :password, presence: true
+  validates :password, presence: true,  length: { in: 6..10}
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,26 +4,26 @@ RSpec.describe User, type: :model do
 
   describe 'valid user:' do
     it "standard user should be valid" do
-      user = User.new(name: "Gina", email: "gina@example.com", password: "12345")
+      user = User.new(name: "Gina", email: "gina@example.com", password: "12345678")
       expect(user).to be_valid
     end
   end
 
   describe 'valid user name' do
     it "standard user name should be valid" do
-      user = User.new(name: "Gina", email: "gina@example.com", password: "12345")
+      user = User.new(name: "Gina", email: "gina@example.com", password: "12345678")
 
       expect(user).to be_valid
     end
 
     it "no user name should be invalid" do
-      user = User.new(name: nil, email: "gina@example.com", password: "12345")
+      user = User.new(name: nil, email: "gina@example.com", password: "12345678")
       expect(user).to_not be_valid
     end
 
     it "user name invalid if already exists" do
-      user = User.create(name: "Gina", email:"gina@example.com", password_digest: "12345")
-      user2 = User.new(name: "Gina", email:"smiley@example.com", password: "12345")
+      user = User.create(name: "Gina", email:"gina@example.com", password: "12345678")
+      user2 = User.new(name: "Gina", email:"smiley@example.com", password: "12345678")
 
       expect { user2.save validate: false}.to raise_error(ActiveRecord::RecordNotUnique)
     end
@@ -31,20 +31,20 @@ RSpec.describe User, type: :model do
 
   describe 'valid user email:' do
     it  "if no email is entered user is invalid" do
-      user = User.new(name: 'Gina', email: nil, password: "12345")
+      user = User.new(name: 'Gina', email: nil, password: "12345678")
       expect(user).to_not be_valid
     end 
 
     it "user email invalid if already exists" do
-      user = User.create(name: "Gina", email:"gina@example.com", password: "12345")
-      user2 = User.new(name: "Alexa", email:"gina@example.com", password: "12345")
+      user = User.create(name: "Gina", email:"gina@example.com", password: "123456")
+      user2 = User.new(name: "Alexa", email:"gina@example.com", password: "123456")
       expect { user2.save validate: false}.to raise_error(ActiveRecord::RecordNotUnique)
     end
 
     it "user email needs to  have a valid format" do
       invalid_emails = ["gina.example", '23567b', '@google.com']
       invalid_emails.each do |invalid_email| 
-        user = User.new(name: "Gina", email: invalid_email, password: "12345")
+        user = User.new(name: "Gina", email: invalid_email, password: "123456")
         expect(user).to_not be_valid 
       end 
     end 
@@ -57,25 +57,15 @@ RSpec.describe User, type: :model do
     end 
 
     it 'password is encrypted ' do
-      password = '12345'
+      password = '123456'
       user = User.create(name: "Gina", email: "gina@example.com", password: password)
       user.reload
       expect(user.password_digest).to_not eq password
     end 
-    it 'password under 6 characters is not valid' do 
+    it 'password is between 6 to 10 characters' do 
       short_password = 'a' * 5
       user = User.create(name: "Gina", email: "gina@example.com", password: short_password)
       expect(user).to_not be_valid
-    end 
-    it 'password is above 10 characters is not valid' do 
-      long_password = 'a' * 11
-      user = User.create(name: "Gina", email: "gina@example.com", password: long_password)
-      expect(user).to_not be_valid
-    end 
-    it 'password between 6 and 10 characters is valid' do
-      valid_password = 'a' * 9
-      user = User.create(name: "Gina", email: "gina@example.com", password: valid_password)
-      expect(user).to be_valid
     end
   end
 end
