978f14c21f77a049e4b0f0b8a1b35c887eb2dc9b
Edward Phillips <edd0ddi0@gmail.com>
2020-05-11 11:55:07 +0100

    R: user cannot login with incorrect credentials

---
+++ spec/features/user_cannot_sign_in_with_invalid_credentials_spec.rb
@@ -0,0 +1,15 @@
+
+require 'rails_helper'
+
+RSpec.feature "Login", type: :feature do
+  scenario "Cannot login with incorrect email" do
+    User.create(first_name: "Sam", last_name: "Gangi", email: "samgangi@lotr.com", password: "theshire")
+    visit "/sessions/new"
+
+    fill_in "user[email]", with: "samgangsi@lotr.com"
+    fill_in "user[password]", with: "theshire"
+    click_button "Submit"
+
+    expect(page).to have_content("Incorrect email/password")
+  end
+end
