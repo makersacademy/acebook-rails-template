f418d736320f21758dceca2e68fbeef2045faf93
Harry Turnbull <hturnbull@live.co.uk>
2020-05-11 10:57:32 +0100

    pended autogen tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe UsersController, type: :controller do
+RSpec.xdescribe UsersController, type: :controller do
 
   describe "GET #new" do
     it "returns http success" do
