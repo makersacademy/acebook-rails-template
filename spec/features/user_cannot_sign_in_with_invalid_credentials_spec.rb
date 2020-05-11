0316c3747dcef7b2cda8cdec672f2f96946dee32
Edward Phillips <edd0ddi0@gmail.com>
2020-05-11 11:56:53 +0100

    added test for incorrect password on sign in

--- spec/features/user_cannot_sign_in_with_invalid_credentials_spec.rb
+++ spec/features/user_cannot_sign_in_with_invalid_credentials_spec.rb
@@ -12,4 +12,15 @@ RSpec.feature "Login", type: :feature do
 
     expect(page).to have_content("Incorrect email/password")
   end
+
+  scenario "Cannot login with incorrect email" do
+    User.create(first_name: "Sam", last_name: "Gangi", email: "samgangi@lotr.com", password: "theshire")
+    visit "/sessions/new"
+
+    fill_in "user[email]", with: "samgangi@lotr.com"
+    fill_in "user[password]", with: "sauron"
+    click_button "Submit"
+
+    expect(page).to have_content("Incorrect email/password")
+  end
 end
