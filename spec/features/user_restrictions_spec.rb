7b2db498bc16ab3ba5ef20dc6495513ea36ea9e9
Graham <grahamalconer@icloud.com>
2020-05-12 15:58:25 +0100

    Updated user restrictions, changed tests to fit restrictions

---
+++ spec/features/user_restrictions_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.feature "Restricted site access if logged out", type: :feature do
+  scenario "users who aren't signed in will visit page sites besides signup and login, and get redirected to login" do
+    visit "/posts"
+#    expect(page).to have_current_path sessions_new_path
+  end
+end
