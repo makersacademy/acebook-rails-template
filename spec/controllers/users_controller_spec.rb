721d57ba217368aaf563d1bb4ae5c0eac7d52051
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-12 14:19:41 +0100

    Add missing tests to users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe 'GET /new' do
+    it 'responds with 200' do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
 
+  describe 'POST /' do
+    it 'redirects to the posts page' do
+      post :create, params: { user: { name: "Gina", password: "123456", email: "gina@example.com" } }
+      expect(response).to redirect_to posts_path
+    end
+  end
 end
