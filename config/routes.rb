b3d5597c6ad55857546c86e36523f4399c51e101
Angelica <Angelica137@users.noreply.github.com>
2020-05-12 14:19:50 +0100

    add changed files for api conversion

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts
-      resources :sessions, only[:new, :create, :destroy]
+      resources :sessions, only:[:new, :create, :destroy]
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200507101056) do
+ActiveRecord::Schema.define(version: 20200512104515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20200507101056) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
   create_table "users", force: :cascade do |t|
