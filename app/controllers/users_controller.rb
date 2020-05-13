5c3f5310afbe9e5912e6cb16e180cf43038d10c5
Alexa <alexakearns@outlook.com>
2020-05-13 14:53:28 +0100

    error messages at sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    User.create(user_params)
-    redirect_to posts_path
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def show
--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -15,4 +15,16 @@ RSpec.feature "Sign-up", type: :feature do
     click_on "Sign up"
     expect(page).to have_content("Welcome Gina!")
   end
+
+  scenario "error message for invalid credentials" do
+    User.create(name: "Gina", email: "gina@example.com", password: "123456")
+    visit "/"
+    fill_in "user[name]", with: "Gina"
+    fill_in "user[email]", with: "gina.example"
+    fill_in "user[password]", with: "1234"
+    click_on "Sign up"
+    expect(page).to have_content("Name has already been taken")
+    expect(page).to have_content("Not a valid email address")
+    expect(page).to have_content("Password too short")
+  end
 end
