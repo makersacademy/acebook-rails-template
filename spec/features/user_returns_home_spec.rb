546a54f77b7b5cdf8e103834f43631482a2171e3
Alex Lewis <alexlewis374@gmail.com>
2020-05-13 11:40:39 +0100

    add button to home page from user wall

---
+++ spec/features/user_returns_home_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature 'user can return home', type: :feature do
+    scenario "user can return to home page from wall" do
+        User.create(name: "Gina", password: "123456", email: "gina@example.com")
+        visit "/sessions/new"
+        fill_in "session[name]", with: "Gina"
+        fill_in "session[password]", with: "123456"
+        click_on "Log in"
+        click_on "Home"
+        expect(page).to have_content('GAPAGbook')
+    end
+end
