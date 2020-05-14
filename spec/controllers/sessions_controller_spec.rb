73c9a409a698c746dfe5b47087cdfb948fe14e3e
Harry Turnbull <hturnbull@live.co.uk>
2020-05-14 12:40:01 +0100

    actual way of doing controller tests for session

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,48 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe SessionsController, type: :controller do
+RSpec.describe Api::V1::SessionsController, type: :controller do
 
-# end
+  before(:each) do
+    @user = User.create(
+    first_name: "Sam",
+    last_name: "Gangi",
+    email: "samgangi@lotr.com",
+    password: "theshire"
+    )
+  end
+
+  it "POST /sessions correct credentials, responds with json status 200 and user details " do
+    post :create, params: { 
+      email: "samgangi@lotr.com",
+      password: "theshire"
+    }
+    json = JSON.parse(response.body)
+
+    expect(json['status']).to eq "created"
+    expect(json['user']['email']).to eq @user.email
+    expect(json['user']['first_name']).to eq @user.first_name
+    expect(json['user']['last_name']).to eq @user.last_name
+    expect(json['user']['id']).to eq @user.id
+    expect(json['logged_in']).to eq true
+  end     
+
+  it "POST /sessions incorrect email, responds with json status 401 " do
+    post :create, params: { 
+      email: "INCORRECT@lotr.com",
+      password: "theshire"
+    }
+    json = JSON.parse(response.body)
+
+    expect(json['status']).to eq 401
+  end
+
+  it "POST /sessions incorrect password, responds with json status 401 " do
+    post :create, params: { 
+      email: "samgangi@lotr.com",
+      password: "INCORRECT"
+    }
+    json = JSON.parse(response.body)
+
+    expect(json['status']).to eq 401
+  end     
+end
