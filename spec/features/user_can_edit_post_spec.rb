c8d0e725b2bdd096dd9d65b9f0b7697e3c2b2c1d
Alexa <alexakearns@outlook.com>
2020-05-14 11:26:51 +0100

    3 pending tests resolved

--- spec/features/user_can_edit_post_spec.rb
+++ spec/features/user_can_edit_post_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 RSpec.feature "Edit Post", type: :feature do
-  xscenario "user can edit existing post" do
+  scenario "user can edit existing post" do
     User.create(name: "Gina", password: "123456", email: "gina@example.com")
-    post = Post.create!(message: "Hello World!")
-    visit "/sessions/new"
-    fill_in "session[name]", with: "Gina"
-    fill_in "session[password]", with: "123456"
-    click_on "Log in"
-    visit "/posts"
-    click_button "Edit post"
+    log_in_gina
+    click_on "New post"
+    fill_in "Message", with: "Hello World!"
+    click_on "Submit"
+    click_on "Edit post"
     fill_in "post[message]", with: "I'm a new message"
     click_button "Update Post"
     expect(page).to have_content("I'm a new message")
