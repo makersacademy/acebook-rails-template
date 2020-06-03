a98816a55f998eaf6568bd3bda1e594e0791fb25
Ed <lookatemail@gmail.com>
2020-06-03 14:56:59 +0100

    create DB table for user

--- db/migrate/20200603134944_create_users.rb
+++ db/migrate/20200603134944_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string, :fname
-      t.string, :lname
-      t.string, :email
-      t.string, :password
+      t.string :fname
+      t.string :lname
+      t.string :email
+      t.string :password
       t.string :profile_picture
 
       t.timestamps
