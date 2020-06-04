72d4c2751f24d7d2d11938326fbb2cbd1aa85e36
pauladarias <pauladarias@gmail.com>
2020-06-04 10:36:58 +0100

    bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  # get 'sessions/new'
+  root 'users#new'
 
   get 'sessions/destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#new'
+  
   get '/sessions/new', to: 'sessions#new'
   post '/users', to: 'users#create'
+  #resources :sessions, only: [:new, :destroy]
   resources :posts
 end
