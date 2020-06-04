160658026401d9d0257a78c5571a7fa06de3ce60
Ed <lookatemail@gmail.com>
2020-06-04 11:55:04 +0100

    able to validate emty post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it { is_expected.to be }
+
+  it 'User post input not to be nil' do
+    post = Post.create(message: 'This is a new message!')
+    expect(post).to be_valid
+  end
+
+  it 'User post input to be nil' do
+    post = Post.create(message: nil)
+    expect(post).to_not be_valid
+  end
+
 end
