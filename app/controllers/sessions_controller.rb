8be3f3b53bbeab82c036532de84fd287fcbecdb3
mbrad26 <mbrad26@gmail.com>
2020-06-04 16:45:37 +0100

    implemented test for when the user enters the wrong email address

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,11 @@ class SessionsController < ApplicationController
   def create
     puts params
     @user = User.find_by(email: params[:user][:email].downcase)
-    # render "/users/#{@user.id}"
-    redirect_to "/users/#{@user.id}"
+    if @user && @user.password == params[:user][:password]
+      redirect_to "/users/#{@user.id}"
+    else
+      # flash[:alert] = 'Email and/or password is incorrect'
+      redirect_to '/sessions/new', notice: 'Email and/or password is incorrect'
+    end
   end
 end
--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -1,22 +1,5 @@
 require 'rails_helper'
 
-# feature 'User account registration', type: :feature do
-#   scenario 'a user can register with an account' do
-#     visit '/'
-
-#     expect(page).to have_content 'Create account'
-
-#     fill_in 'user[fname]', with: 'John'
-#     fill_in 'user[lname]', with: 'Doe'
-#     fill_in 'user[email]', with: 'john@example.com'
-#     fill_in 'user[password]', with: 'password'
-#     find('[name=commit]').click
-
-#     expect(current_path).to eq '/sessions/new'
-#     expect(page).to have_content 'Welcome John Doe'
-#   end
-# end
-
 feature 'User can login', type: :feature do
   scenario 'a user can login' do
     #signs the user in
@@ -28,8 +11,20 @@ feature 'User can login', type: :feature do
     fill_in 'user[email]', with: 'john.doe@example.com'
     fill_in 'user[password]', with: 'password'
     click_button 'Sign in'
-    
+
     expect(current_path).to eq "/users/#{user.id}"
     expect(page).to have_content "Welcome #{user.fname} #{user.lname}"
+  end
+
+  scenario 'user gets redirected when the log in is unsuccessful' do
+    User.create(fname: 'jon', lname: 'doe', email: 'john.doe@example.com', password: 'password')
+
+    visit '/sessions/new'
+    fill_in 'user[email]', with: 'wrong@example.com'
+    fill_in 'user[password]', with: 'password'
+    click_button 'Sign in'
+
+    expect(current_path).to eq '/sessions/new'
+    expect(page).to have_content 'Email and/or password is incorrect'
   end
 end
