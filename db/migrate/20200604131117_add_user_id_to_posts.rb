4c4815c771ed0a8b202449cfb02f96fa6273768b
Ed <lookatemail@gmail.com>
2020-06-04 16:48:25 +0100

    able to delete a post

--- db/migrate/20200604131117_add_user_id_to_posts.rb
+++ db/migrate/20200604131117_add_user_id_to_posts.rb
@@ -1,6 +1,6 @@
 class AddUserIdToPosts < ActiveRecord::Migration[5.1]
   def change
     add_column :posts, :user_id, :integer
-    add_foreign_key :posts, :user_id
+    # add_foreign_key :posts, :user_id
   end
 end
---
+++ spec/features/user_able_to_delete_post_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature 'Delete Post', type: :feature do
+  scenario 'User able to delete post' do
+    visit '/posts'
+
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+
+    click_button 'Delete Post'
+
+    expect(page).to_not have_content('Hello, world!')
+  end
+end
