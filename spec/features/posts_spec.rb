7638190faa2ea2d8ad572c97a532d54f8f2cf1cf
Joe <josephhull676@yahoo.co.uk>
2020-06-11 15:26:34 +0100

    Removed tests for post controller. Will be added again later.

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -4,15 +4,12 @@ require 'launchy'
 RSpec.feature "Posts", type: :feature do
   scenario "Can submit posts and view them" do
     sign_up
-    p ENV['RAILS_ENV']
-    save_and_open_page
     add_post
     expect(page).to have_content("Hello, world!")
   end
 
   scenario "Signed in user can edit posts" do
     sign_up
-    save_and_open_page
     add_post
     click_link "Edit"
     fill_in "Message", with: "Goodbye, world!"
