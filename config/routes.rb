bb50fcba8e52ea8cb07b0edfee37cfda31f9a683
Jamsine <jasmine.r.davies@gmail.com>
2020-06-11 09:34:57 +0100

    Added one user to many posts relationship. Foreign key is not populating the database.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :posts do
+    resources :users
+  end
+
+
   root 'posts#index'
   root to: 'posts#index'
 end
