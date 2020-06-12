5b711bf8f032b1bc1e19b0f2218ae471764f8880
Joe <josephhull676@yahoo.co.uk>
2020-06-12 10:41:11 +0100

    Working on adding controller tests. Added some gems in order to do so

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  
+  let(:user) { create(:user) }
+
+  describe "a signed in user" do
+    context "after signing in" do
+      before {sign_in user.user}
+
+      it "can visit post index" do
+        
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
+
