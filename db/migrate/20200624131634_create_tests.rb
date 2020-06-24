156c9b8b2fc491660bc9911e65ac32da982aff60
Jonathan Filar <jonathanfilar@hotmail.com>
2020-06-24 14:34:44 +0100

    Fixed error in db migration file

--- db/migrate/20200624131634_create_tests.rb
+++ db/migrate/20200624131634_create_tests.rb
@@ -2,7 +2,7 @@ class CreateTests < ActiveRecord::Migration[5.1]
   def change
     create_table :tests do |t|
       t.string :name
-      t.text_ :description
+      t.text :description
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200623143638) do
+ActiveRecord::Schema.define(version: 20200624131634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
