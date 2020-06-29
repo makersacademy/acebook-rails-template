98ff605904d5c663880c25912b51cf664198c46c
Jamsine <jasmine.r.davies@gmail.com>
2020-06-29 17:52:50 +0100

    got tests to pass

--- spec/features/forum_spec.rb
+++ spec/features/forum_spec.rb
@@ -2,18 +2,14 @@ require 'rails_helper'
 require 'web_helpers'
 
 RSpec.feature "Forum", type: :feature do
-  scenario "users can submit a post" do
-    sign_up_mentor
-    click_link "Forum"
-    save_and_open_page
-    # within ".card card-body" do
+  scenario "users can create a category and submit a post" do
+      sign_up_mentor
+      create_category
       click_link "Ask A Question"
-    # end
-    select "General", :from => "Choose a Category"
-    fill_in 'Title', with: 'Testing'
-    fill_in "what do you need help with?", with: "How do i test?"
-    click_button "Ask your Question"
-    expect(page).to include?("How do i test?")
+      select "General", from: "Choose a Category"
+      fill_in 'Title', with: 'Testing'
+      fill_in "What do you need help with?", with: "How do i test?"
+      click_button "Ask Your Question"
+      expect(page).to have_content("How do i test?")
   end
-
 end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -37,3 +37,13 @@ def sign_in_mentor
   fill_in "Password", with: "12345q"
   click_button "Log in"
 end
+
+def create_category
+  click_link "Forum"
+  # within (all(".btn.btn-outline-primary.btn-block")[1]) do
+    click_link "Create A New Category"
+  # end
+
+  fill_in "Name", with: "General"
+  click_button "Create"
+end
