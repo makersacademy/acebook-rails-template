22c51e81091058045a59810aae73eaa0331280a2
Nick Butler <nwmbutler@gmail.com>
2020-06-30 15:42:01 +0100

    simplecov runnning and visualising

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,12 @@
 require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+
 SimpleCov.start 'rails' do
   add_filter '/db/'
   add_filter '/bin/'
