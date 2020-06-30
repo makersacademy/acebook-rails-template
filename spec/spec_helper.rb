dcbefd692733e71da49d43a8fce047921d3e89a5
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-30 11:59:36 +0100

    added motivation to test results

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,6 +26,15 @@ SimpleCov.start
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+    config.after(:suite) do
+      motivation = [
+        'You go Team!',
+        'Drink water!',
+        'aaaand breathe',
+      ].sample
+      puts
+      puts "\e[33m#{motivation}\e[0m"
+    end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
