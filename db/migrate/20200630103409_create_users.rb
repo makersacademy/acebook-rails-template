72aedb8813e6956392fcf5cb8eae3ccacea711fe
Tristan Langford <tristanlangford@hotmail.com>
2020-06-30 16:24:16 +0100

    amended db migrate

--- db/migrate/20200630103409_create_users.rb
+++ db/migrate/20200630103409_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.timestamps null: false
-      t.string :email, null: false
+      t.string :email, null: false, unique: true
       t.string :encrypted_password, limit: 128, null: false
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128, null: false
