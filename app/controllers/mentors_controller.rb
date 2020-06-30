b2da57577d5b590d75b220ddc9f1c1bb0bdc04d5
Joe <josephhull676@yahoo.co.uk>
2020-06-30 15:56:58 +0100

    Removed changes from another branch

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -8,11 +8,6 @@ class MentorsController < ApplicationController
     @mentor = Mentor.find(params[:id])
     @conversations = Conversation.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
     @students = Student.all
-    @mentor.students.reload
-    p "++++++++++++++++++++"
-    students_connected = []
-    @mentor.students.each {|student| students_connected << student[:email]}
-    p students_connected
   end
 
   def update
