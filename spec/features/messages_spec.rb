21f1a72940d06e90aa3560ab3d25e4225193aebc
Joe <josephhull676@yahoo.co.uk>
2020-06-30 15:55:54 +0100

    Modified link on messages page. Wrote feature test to test is active conversations are displayed on the user's dashboard

--- spec/features/messages_spec.rb
+++ spec/features/messages_spec.rb
@@ -11,8 +11,18 @@ RSpec.feature "Messages", type: :feature do
     sign_in_mentor
     add_student
     send_message
-    save_and_open_page
     expect(page).to have_content("test message")
   end
 
+  scenario "can see current conversations on the dashboard" do
+    sign_up_mentor
+    sign_out
+    sign_up_student
+    sign_out
+    sign_in_mentor
+    add_student
+    send_message
+    click_link "Back to Your Dashboard"
+    expect(page).to have_content("test123@student.com")
+  end
 end
