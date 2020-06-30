6f09437542dc38494ec63c8144b52c1412fe9e4e
Joe <josephhull676@yahoo.co.uk>
2020-06-30 10:31:08 +0100

    Debugging the test failure

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -12,6 +12,12 @@ class MentorsController < ApplicationController
 
   def update
     student = Student.find_by(email: params[:mentor][:student_email])
+    p "-----------------------"
+    p "params are #{params}"
+    p "+++++++++++++++++++++++"
+    p "params for student_email are #{params[:mentor][:student_email]}"
+    p "-----------------------"
+    p "student found by email is - #{Student.find_by(email: "Test123@student.com")}"
     current_user.students << student
     current_user.save
   end
