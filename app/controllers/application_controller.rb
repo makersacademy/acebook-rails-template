af7d273109d72844dee58f04088a9b03ad75c767
George Holborn <62567622+george-bit@users.noreply.github.com>
2020-06-30 11:45:14 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    added_attrs = [:first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :moderator]
+    added_attrs = [:first_name, :last_name, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
