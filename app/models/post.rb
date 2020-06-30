ef8cabd78615341506a60f31a0e807b2b321d100
Rachael <rachaelewins@hotmail.com>
2020-06-30 10:56:58 +0100

    Refactor out create a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,2 @@
 class Post < ApplicationRecord
-  def date
-    time = self.created_at
-    time(0..10)
-  end
 end
--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
+def creates_a_post
+  visit "/posts"
+  click_link "New post"
+  fill_in "Message", with: "Hello, world!"
+  click_button "Submit"
+end
+
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
+    creates_a_post
     expect(page).to have_content("Hello, world!")
   end
 end
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
+    creates_a_post
     expect(page).to have_content("#{Time.now.strftime("%Y-%m-%d")}")
   end
 end
