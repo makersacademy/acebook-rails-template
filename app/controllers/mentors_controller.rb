ea343e47c8dde1c02d7f45564a3faaca1092e14c
Joe <josephhull676@yahoo.co.uk>
2020-06-30 15:17:42 +0100

    Debugging to find why students list on mentor dashboard doesn't update after refresh

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -8,12 +8,17 @@ class MentorsController < ApplicationController
     @mentor = Mentor.find(params[:id])
     @conversations = Conversation.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
     @students = Student.all
+    @mentor.students.reload
+    p "++++++++++++++++++++"
+    students_connected = []
+    @mentor.students.each {|student| students_connected << student[:email]}
+    p students_connected
   end
 
   def update
     student = Student.find_by(email: params[:mentor][:student_email])
     current_user.students << student
     current_user.save
-    redirect_to mentor_path(current_user.id)
+    redirect_to "/mentors/#{current_user.id}/dashboard"
   end
 end
