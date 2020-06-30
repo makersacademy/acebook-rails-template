01ea80f98d0a8788ac7a5036b29239a95c3a84f8
J Perrin <perrinjack96@gmail.com>
2020-06-30 10:52:16 +0100

    belongs to dependency removed in models

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,17 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
   def new
     @post = Post.new
+
   end
 
   def create
     @post = Post.create(post_params)
+    puts post_params
+    puts @post.save
+    puts @post.errors.full_messages
+    puts 'CREATE'
     redirect_to posts_url
   end
 
