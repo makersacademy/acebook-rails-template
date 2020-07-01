1324449f7a9e8abd457a42b77dcbddba4220585f
Emanuele Pace <emanuele.10@outlook.it>
2020-07-01 16:07:44 +0100

    created users and sessions and users db

--- db/schema.rb
+++ db/schema.rb
@@ -21,4 +21,22 @@ ActiveRecord::Schema.define(version: 20170526114520) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index [“email”], name: “index_users_on_email”, unique: true
+    t.index [“name”], name: “index_users_on_name”, unique: true
+  end
+
+
+  add_foreign_key “posts”, “users”
+
+
+
+
+
+
 end
