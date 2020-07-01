0ac7612057e1193629c1c19971ae64ab78350d18
Joe <josephhull676@yahoo.co.uk>
2020-07-01 09:51:01 +0100

    Refresh fixed

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -8,17 +8,12 @@ class MentorsController < ApplicationController
     @mentor = Mentor.find(params[:id])
     @conversations = Conversation.where("sender_id = ? OR receiver_id = ?", current_user.id, current_user.id)
     @students = Student.all
-    @mentor.students.reload
-    p "++++++++++++++++++++"
-    students_connected = []
-    @mentor.students.each {|student| students_connected << student[:email]}
-    p students_connected
   end
 
   def update
     student = Student.find_by(email: params[:mentor][:student_email])
     current_user.students << student
     current_user.save
-    redirect_to "/mentors/#{current_user.id}/dashboard"
+    redirect_back(fallback_location: root_path)
   end
 end
