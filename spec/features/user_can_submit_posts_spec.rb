9f922f3cfd6cbe28502fd8bef3a97951a6742ebc
nadinedelia <nadine.delia@yahoo.com>
2020-07-01 11:49:16 +0200

    change time position

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Timeline", type: :feature do
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
-    expect(page).to have_content("Hello, world!")
     expect(page).to have_content(Time.now.strftime("%d %b %y"))
+    expect(page).to have_content("Hello, world!")
   end
 end
