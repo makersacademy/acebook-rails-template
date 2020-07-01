309939d1354fbcb89dfc4ab4cfe253cf19fac195
Sophie <sophiebrown438@gmail.com>
2020-07-01 12:31:12 +0100

    Refactor pass tests for sign-in gatekeeping

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
   protect_from_forgery with: :exception
+
+  before_action :require_login
+
+  private
+
+  def require_login
+    unless current_user
+      redirect_to sign_in_path
+    end
+  end
 end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe WelcomeController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
+      sign_in
       get :index
       expect(response).to have_http_status(:success)
     end
