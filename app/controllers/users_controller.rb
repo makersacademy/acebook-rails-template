cad093a61f7d3218a4038865be6c0cccbe7c6b18
Rich Ewin <richardewinlambert@gmail.com>
2020-07-01 18:20:34 +0100

    user can sign up with name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
   def new
+  end
+
+  def create
+    session[:user_name] = params[:user][:name]
+    redirect_to posts_url
   end
 end
--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "Sign Up", type: :feature do
-  scenario "Can sign up in acebook" do
+  scenario "User can sign up to acebook" do
     visit "/"
     expect(page).to have_content("Sign Up")
   end
-end 
+
+  scenario "User can sign up with a name" do
+      visit "/"
+      fill_in "user[name]", with: "Emanuele"
+      click_on "Sign up"
+      expect(page).to have_content("Emanuele")
+    end
+  end
