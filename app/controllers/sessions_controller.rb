00f713ebc994666b53e54a50a4003cc7cd99ff08
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-01 12:09:14 +0100

    As far as we got creating sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by (:email params["user"]["email"])
-    .try(:authenticate, params["user"]["password"])
-    if user 
+    user = User.find_by(email: params[:email])
+    
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id 
       redirect_to :action => post_path, :id => user.id, notice: "You successfully logged in!"
     else
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
 
   get 'sessions/destroy'
 
+  # resources :sessions, only: [:new, :create, :destroy]
+  # get ‘signup’, to: "users#new", as: ‘signup’
+  # get ‘login’, to: "sessions#new", as: ‘login’
+  # get ‘logout’, to: "sessions#destroy", as: ‘logout’
+
   resources :users
   # get 'welcome/index'
   root "welcome#index"
