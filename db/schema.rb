493647e080a50a92f59b9cfb2fcf57a3ea484083
Rich Ewin <richardewinlambert@gmail.com>
2020-07-01 17:24:57 +0100

    added users db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526114520) do
+ActiveRecord::Schema.define(version: 20200701162126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,16 +27,6 @@ ActiveRecord::Schema.define(version: 20170526114520) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index [“email”], name: “index_users_on_email”, unique: true
-    t.index [“name”], name: “index_users_on_name”, unique: true
   end
-
-
-  add_foreign_key “posts”, “users”
-
-
-
-
-
 
 end
