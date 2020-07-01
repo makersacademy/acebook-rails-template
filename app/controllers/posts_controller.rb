1d58071c44e166828d71c555d9b23018de58c23b
nadinedelia <nadine.delia@yahoo.com>
2020-07-01 12:52:34 +0200

    posts can be edited, tests passing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,18 @@ class PostsController < ApplicationController
 
     redirect_to posts_path
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    
+    redirect_to posts_path
+
+  end
   
   private
 
--- spec/features/update_post_spec.rb
+++ spec/features/update_post_spec.rb
@@ -6,7 +6,7 @@ feature 'Delete post' do
     click_link "New post"
     fill_in "Message", with: "Delete me!"
     click_button "Submit"
-    click_link 'Delete post'
+    click_link 'Delete'
     expect(page).to_not have_content("Delete me!")
   end
 end
@@ -19,6 +19,7 @@ feature 'Update post' do
     click_button "Submit"
     click_link 'Edit'
     fill_in "Message", with: "I'm changed I promise!!!"
+    click_button "Submit"
     expect(page).to have_content("I'm changed I promise!!!")
     expect(page).to_not have_content('Update me!')
   end
