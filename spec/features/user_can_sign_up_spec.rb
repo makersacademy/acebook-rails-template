ce4b37a297c96bbbaacb336893715b13e87c626d
Rich Ewin <richardewinlambert@gmail.com>
2020-07-01 18:24:24 +0100

    user can sign up with email and password

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -7,9 +7,11 @@ RSpec.feature "Sign Up", type: :feature do
   end
 
   scenario "User can sign up with a name" do
-      visit "/"
-      fill_in "user[name]", with: "Emanuele"
-      click_on "Sign up"
-      expect(page).to have_content("Emanuele")
+    visit "/"
+    fill_in "user[name]", with: "Emanuele"
+    fill_in "user[email]", with: "ema@test.com"
+    fill_in "user[password]", with: "12345"
+    click_on "Sign up"
+    expect(page).to have_content("Emanuele")
     end
   end
