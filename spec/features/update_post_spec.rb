a18331a2a14e6de0e07cc6282270ab416b590d4b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-01 12:06:58 +0100

    added test to check edit link is removed after post is 10 min old

--- spec/features/update_post_spec.rb
+++ spec/features/update_post_spec.rb
@@ -24,3 +24,22 @@ feature 'Update post' do
     expect(page).to_not have_content('Update me!')
   end
 end
+
+feature 'Update post after 10 mins' do
+  
+  before do
+    travel_to Time.zone.local(2004)
+  end
+
+  after do
+    travel_back
+  end
+
+  scenario 'User cant update a post after 10 mins' do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Update me!"
+    click_button "Submit"
+    expect(page).to_not have_link("Edit")
+  end
+end
