2ec323ffc4c2360017cc29d666e4e296becf943a
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-01 13:00:13 +0100

    Update welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def sign_up
+    @user = new.User
+  end
+
+  def login
+  end
+
+  def logout
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
 
   get 'sessions/destroy'
 
-  # resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   # get ‘signup’, to: "users#new", as: ‘signup’
-  # get ‘login’, to: "sessions#new", as: ‘login’
-  # get ‘logout’, to: "sessions#destroy", as: ‘logout’
+  get ‘login’, to: "sessions#new", as: ‘login’
+  get ‘logout’, to: "sessions#destroy", as: ‘logout’
 
   resources :users
   # get 'welcome/index'
-  root "welcome#index"
+  root "welcome/index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
