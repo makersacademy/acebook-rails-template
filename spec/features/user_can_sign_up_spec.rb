0312212e9125999515b894191751e9b006c14057
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-01 15:48:25 +0100

    reviewing tests

---
+++ spec/features/user_can_sign_up_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.feature "Signing up", type: :feature do
+  # scenario "Can sign up" do
+  #   visit "/"
+  #   click_link "Sign Up"
+  #   fill_in "First Name", with: "Al"
+  #   fill_in "Last Name", with: "Sumner"
+  #   fill_in "Email", with: "al@gmail.com"
+  #   fill_in "Password", with: "123456"
+  #   click_button "Create User"
+  #   expect(page).to have_content("User was successfully created")
+  # end
+end
