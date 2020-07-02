c4a100838afa579d23317e16a0e06b24732f7c3d
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-02 10:53:34 +0100

    after sign up the user is sent to welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to root_url, notice: 'User was successfully created.' }
+        #format.json { render :show, status: :created, location: root_url }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
