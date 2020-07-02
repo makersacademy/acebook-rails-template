1de278e684755a1268225ac634eef45b78149ed0
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-02 15:24:47 +0100

    user can sign up + possible errors tested

--- db/schema.rb
+++ db/schema.rb
@@ -21,14 +21,4 @@ ActiveRecord::Schema.define(version: 20200630160216) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-  end
-
 end
