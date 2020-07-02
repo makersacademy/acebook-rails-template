ec40ab5f7db7d280866a492000f47c4fbcdb57b1
J Perrin <perrinjack96@gmail.com>
2020-07-02 13:23:32 +0100

    signup is now a route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts, :users
-  root 'users#new'
+  # root 'users#new'
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
 
 end
