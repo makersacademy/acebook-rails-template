4ad22f66082b616fd26b27c52f5562730cbf25d7
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-02 15:30:33 +0100

    Update edit method eith time limit on editing

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,10 @@ class Post < ApplicationRecord
     Time.now - self.created_at < 10.seconds
   end
 
+  # def validate_is_editable
+  #   if self.persisted? && !self.editable?
+  #     self.errors[:editable] << "can edit in just 10 minutes after creation"
+  #   end
+  # end
 
 end
