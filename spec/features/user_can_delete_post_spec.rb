8652fdffe9298ed4b91ee55c1f6c1bfd13393d5e
Nick Butler <nwmbutler@gmail.com>
2020-07-02 14:42:02 +0100

    test coverage update

--- spec/features/user_can_delete_post_spec.rb
+++ spec/features/user_can_delete_post_spec.rb
@@ -4,7 +4,7 @@ require_relative '../support/features/post_helpers.rb'
 RSpec.feature "Delete", type: :feature do
   scenario "user can delete their own posts" do
     creates_a_post
-    click_link "delete"
+    click_link 'delete'
     expect(page).to_not have_content(/bob1.*Hello, world!/)
   end
 end
