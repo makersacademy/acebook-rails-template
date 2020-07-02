aeb496f0c0279e3d2c40d461c16e4ba1f2f92aa6
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-02 18:12:19 +0100

    Remove old posts view code

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -2,13 +2,13 @@
 
 require 'rails_helper'
 
-RSpec.feature 'Timeline', type: :feature do
-  scenario 'Can submit posts and view them' do
-    visit '/posts'
-    fill_in 'Message', with: 'Hello, world!'
-    click_button 'Submit'
-    expect(page).to have_content('Hello, world!')
-  end
+# RSpec.feature 'Timeline', type: :feature do
+#   scenario 'Can submit posts and view them' do
+#     visit '/posts'
+#     fill_in 'Message', with: 'Hello, world!'
+#     click_button 'Submit'
+#     expect(page).to have_content('Hello, world!')
+#   end
 
   # scenario 'Can see the last updated time for a post' do
   #   visit '/posts'
@@ -18,12 +18,12 @@ RSpec.feature 'Timeline', type: :feature do
   # end
   # Travis using different time zone so wasn't passing checks
 
-  scenario 'the page displays the newest message first' do
-    visit '/posts'
-    fill_in 'Message', with: 'Hello, world!'
-    click_button 'Submit'
-    fill_in 'Message', with: 'I should be displayed first!'
-    click_button 'Submit'
-    expect(first('p')).to have_content('I should be displayed first!')
-  end
-end
+#   scenario 'the page displays the newest message first' do
+#     visit '/posts'
+#     fill_in 'Message', with: 'Hello, world!'
+#     click_button 'Submit'
+#     fill_in 'Message', with: 'I should be displayed first!'
+#     click_button 'Submit'
+#     expect(first('p')).to have_content('I should be displayed first!')
+#   end
+# end
