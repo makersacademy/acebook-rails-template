f603202b8ca31409c8783e5ab602d7a614fd7daa
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-02 16:00:11 +0100

    added method to redirect user to homepage on log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private 
+
+  def after_sign_out_path_for(resource_or_scope)
+    login_path
+  end
+
 end
