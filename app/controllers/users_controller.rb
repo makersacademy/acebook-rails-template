92733b614c42f47c6393e9c1db182579cad66bb8
Rich Ewin <richardewinlambert@gmail.com>
2020-07-02 12:07:26 +0100

    we can create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
-    p params
-    p params[:user]
-   
-  
     @user = User.create(user_params)
     redirect_to posts_url
-   
   end
-  
-  def index
-    @posts = User.all
-    @user_name = session[:user_name]
-  end
-  
+
   private
-  
+
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password_digest)
   end
- end
- 
+end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,19 +1,14 @@
 
 require 'rails_helper'
- 
+
 RSpec.describe UsersController, type: :controller do
- 
-xdescribe "POST /" do
-    it "creates a post" do
-       post :create, params: { user: {name: "John", email: 'test@hello.com', password_digest: 'pass' }}
-       expect(User.find_by(name: "John")).to be
-       expect(User.find_by(password_digest: 'pass')).to be
-       expect(User.find_by(email: "test@hello.com")).to be
 
-     end
+  describe 'POST /' do
+    it 'creates a new user' do
+      post :create, params: { user: {name: 'Emanuele', email: 'ema@test.com', password_digest: '12345' }}
+      expect(User.find_by(name: 'Emanuele')).to be
+      expect(User.find_by(password_digest: '12345')).to be
+      expect(User.find_by(email: 'ema@test.com')).to be
+    end
+  end
 end
- 
-end
-
-
-
