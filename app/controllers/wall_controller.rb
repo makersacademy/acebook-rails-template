20ad04dc1e4ea8e65cef6c175377a9d51470181f
Rachael <rachaelewins@hotmail.com>
2020-07-03 15:06:06 +0100

    users wall test, uses show action

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -2,6 +2,7 @@ class WallController < ApplicationController
 
   def show
     user_id = params[:user_id]
+    @posts = Post.all.sort_by{ |post| post[:created_at] }.reverse
   end
 
 end
--- spec/controllers/user_wall_controller_spec.rb
+++ spec/controllers/user_wall_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe WallController, type: :controller do
   describe "GET /:user_id " do
     it "responds with 200", :focus => true do
       sign_in
-      get :index, params: { use_route: 'wall/', id: 1 }
+      get :show, params: { use_route: 'wall/', id: 1 }
       expect(response).to have_http_status(200)
     end
   end
