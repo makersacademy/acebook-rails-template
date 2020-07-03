0ab0c1fddf94bb0d5126f0065e892a28b09f6846
Rachael <rachaelewins@hotmail.com>
2020-07-03 15:45:04 +0100

    focus tag removed

--- spec/controllers/user_wall_controller_spec.rb
+++ spec/controllers/user_wall_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe WallController, type: :controller do
 
   describe "GET /:user_id " do
-    it "responds with 200", :focus => true do
+    it "responds with 200" do
       sign_in
       get :show, params: { use_route: 'wall/', id: 1 }
       expect(response).to have_http_status(200)
