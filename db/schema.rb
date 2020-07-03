ff40860f1242d746a2a95f8961d1baa8e9f22334
Rachael <rachaelewins@hotmail.com>
2020-07-03 15:30:07 +0100

    wall_id in posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200701151945) do
+ActiveRecord::Schema.define(version: 20200703142823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 20200701151945) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "wall_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+    t.index ["wall_id"], name: "index_posts_on_wall_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,5 +39,5 @@ ActiveRecord::Schema.define(version: 20200701151945) do
   end
 
   add_foreign_key "posts", "users"
-  add_foreign_key "posts", "users", column: "creator_id", primary_key: "username", name: "posts_creator_id_fkey"
+  add_foreign_key "posts", "users", column: "wall_id"
 end
