0b7c48e992d27b36580c351eedd36249f3a206a1
Tristan Langford <tristanlangford@hotmail.com>
2020-07-03 12:25:48 +0100

    reverted to last commit. file path on hompage image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,19 +27,8 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-
-  # Disable Rails's static asset server (Apache or nginx will already do this)  
-config.serve_static_assets = false
-
-# Compress JavaScripts and CSS  
-config.assets.compress = true
-
-# Don't fallback to assets pipeline if a precompiled asset is missed  
-config.assets.compile = false
-
-# Generate digests for assets URLs  
-config.assets.digest = true
-
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
