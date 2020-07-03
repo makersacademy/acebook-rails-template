6343e7bf8600349281d4318015dab42e55b562cf
Tristan Langford <tristanlangford@hotmail.com>
2020-07-03 12:13:24 +0100

    heroku image issue take 4. changes to congif/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,19 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)  
+config.serve_static_assets = false
+
+# Compress JavaScripts and CSS  
+config.assets.compress = true
+
+# Don't fallback to assets pipeline if a precompiled asset is missed  
+config.assets.compile = false
+
+# Generate digests for assets URLs  
+config.assets.digest = true
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
