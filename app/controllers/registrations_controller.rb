510b76d038a16c1b442ac61e02e13de2c1a2560f
Olatunji Oduola <ola.oduola@gmail.com>
2020-07-03 11:31:05 +0100

    Added comments for Registration Controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
 
+# RegistrationsController is inherited / imported from Devise::RegistrationsController class (available to view on GitHub)
+
   private
+
+# The params is an instance of the ActionController::Parameters which gives us access to a number of methods. 
+# require method - ensures that a specific parameter is present, and if it's not provided, the require method throws an error.
+# permit method - method returns a copy of the parameters object, returning only the permitted keys and values.
+
+# only name, email, password, password_confirmation is going to be passed into our Model.  
 
   def sign_up_params
     params.require(:user).permit( :name, 
