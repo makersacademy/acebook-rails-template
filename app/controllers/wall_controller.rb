e2d709b86f668bdde7d62be052ba3499de2e251c
Nick Butler <nwmbutler@gmail.com>
2020-07-03 14:43:05 +0100

    user id walls inplemented

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -34,6 +34,10 @@ class WallController < ApplicationController
     # @posts = Post.all.sort_by{ |post| post[:created_at] }.reverse
   end
 
+  def show
+    user_id = params[:user_id]
+  end
+
   private
 
   def post_params
--- spec/controllers/user_wall_controller_spec.rb
+++ spec/controllers/user_wall_controller_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe WallController, type: :controller do
     end
   end
 
+  describe "GET /:user_id " do
+    it "responds with 200", :focus => true do
+      sign_in
+      get :index, params: { use_route: 'wall/', id: 1 }
+      expect(response).to have_http_status(200)
+    end
+  end
+
 end
