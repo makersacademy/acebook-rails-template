5e4026c21e17827746748ea9d1dabb823eadd58c
Anthony <a.a.donovan@outlook.com>
2020-07-03 14:37:03 +0100

    adds tests for name above post

---
+++ spec/features/users_name_appears_above_their_post_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.feature "Timeline", type: :feature do
+  before(:each) do
+    visit "/"
+    click_link "Sign Up"
+    fill_in "First Name", with: "testfirstname"
+    fill_in "Last Name", with: "testlastname"
+    fill_in "Email", with: "test@gmail.com"
+    fill_in "Password", with: "123456"
+    fill_in "Confirm Password", with: "123456"
+    click_button "Create User"
+    fill_in "Email", with: "test@gmail.com"
+    fill_in "Password", with: "123456"
+    click_button "Login"
+  end
+
+  scenario "Can see their name above their post" do
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content("testfirstname")
+  end
+
+  # Needs sign out button implementation
+  # scenario "Can see another users name above that users post" do
+  #   fill_in "Message", with: "Hello, world!"
+  #   click_button "Submit"
+  #   click_button "Sign Out"
+  #   click_link "Sign Up"
+  #   fill_in "First Name", with: "test2firstname"
+  #   fill_in "Last Name", with: "testlastname"
+  #   fill_in "Email", with: "test2@gmail.com"
+  #   fill_in "Password", with: "123456"
+  #   fill_in "Confirm Password", with: "123456"
+  #   click_button "Create User"
+  #   fill_in "Email", with: "test2@gmail.com"
+  #   fill_in "Password", with: "123456"
+  #   click_button "Login"
+  #   expect(page).to have_content("testfirstname")
+  # end
+end
