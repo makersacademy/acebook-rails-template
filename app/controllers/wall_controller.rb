0e35473fa0fce3d3983624761ce9505039dbc396
Rachael <rachaelewins@hotmail.com>
2020-07-03 15:00:43 +0100

    remove unused wall controller paths

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -1,6 +1,4 @@
 class WallController < ApplicationController
-  before_action :require_permission, only: [:edit, :destroy]
-  before_action :require_time_check, only: :edit
 
   def show
     user_id = params[:user_id]
