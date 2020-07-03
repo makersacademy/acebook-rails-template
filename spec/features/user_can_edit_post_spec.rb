bd3a4d5a5670c7add7e8439f74b166e0c39fa710
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-03 14:48:58 +0100

    Add test for user not being able to edit another users post and some additional that need sign out button which is pending

--- spec/features/user_can_edit_post_spec.rb
+++ spec/features/user_can_edit_post_spec.rb
@@ -23,4 +23,25 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world editted!")
   end
+
+  # Need sign out button
+  # scenario "Can't edit someone else's post" do
+  #   fill_in "Message", with: "Hello, world!"
+  #   click_button "Submit"
+  #   click_button "Sign Out"
+
+  #   click_link "Sign Up"
+  #   fill_in "First Name", with: "test2firstname"
+  #   fill_in "Last Name", with: "test2lastname"
+  #   fill_in "Email", with: "test2@gmail.com"
+  #   fill_in "Password", with: "123456"
+  #   fill_in "Confirm Password", with: "123456"
+  #   click_button "Create User"
+  #   fill_in "Email", with: "test2@gmail.com"
+  #   fill_in "Password", with: "123456"
+  #   click_button "Login"
+
+  #   expect(page).not_to have_link("Edit")
+  # end
+
 end
