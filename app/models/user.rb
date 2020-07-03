bcf4963258cbca102e75e37e88e2154b90fea5e1
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-03 12:22:56 +0100

    added a high level explanation of modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
+# everything after the : are Devise modules 
+# each is responsible for a different action from encrypting passwords
+# to offering recoverable actions
+#https://subscription.packtpub.com/book/application_development/9781782167044/1/ch01lvl1sec08/devise-modules
+#full list here
+#modules are similar to classes in that they hold methods but instead if being attached to one class
+#they can be shared across all classes.
+
+#http://ruby-for-beginners.rubymonstas.org/advanced/modules.html
