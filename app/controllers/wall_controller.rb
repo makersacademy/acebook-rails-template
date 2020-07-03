f00aedf7a99ece8f40758568434dca868a15acf2
Nick Butler <nwmbutler@gmail.com>
2020-07-03 14:47:02 +0100

    delete wall index page

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -29,11 +29,6 @@ class WallController < ApplicationController
     redirect_to posts_url
   end
 
-  def index
-    
-    # @posts = Post.all.sort_by{ |post| post[:created_at] }.reverse
-  end
-
   def show
     user_id = params[:user_id]
   end
--- spec/controllers/user_wall_controller_spec.rb
+++ spec/controllers/user_wall_controller_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 
 RSpec.describe WallController, type: :controller do
 
-  describe "GET / " do
-    it "responds with 200", :focus => true do
-      sign_in
-      get :index
-      expect(response).to have_http_status(200)
-    end
-  end
-
   describe "GET /:user_id " do
     it "responds with 200", :focus => true do
       sign_in
