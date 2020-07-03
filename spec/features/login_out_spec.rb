294d3d5d80f44156c4e7f4964ea5e4c7173b319b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-03 15:56:58 +0100

    added second test for logging out of site

--- spec/features/login_out_spec.rb
+++ spec/features/login_out_spec.rb
@@ -8,6 +8,18 @@ feature 'Log in/ log out' do
     fill_in 'password', with: 'TestPasW0rd'
     fill_in 'user_password_confirmation', with: 'TestPasW0rd'
     click_button 'Sign up'
-    expect(page).to have_content('New post')
+    expect(page).to have_current_path('/')
+  end
+  #may need to edit this test if we change root page for the site
+
+  scenario "user can log in and log out of an account" do
+    visit ('/signup')
+    fill_in 'user_name', with: 'Dave'
+    fill_in 'email', with: 'dave@test.co.uk'
+    fill_in 'password', with: 'TestPasW0rd'
+    fill_in 'user_password_confirmation', with: 'TestPasW0rd'
+    click_button 'Sign up'
+    click_link 'Log Out'
+    expect(page).to have_current_path('/login')
   end
 end
