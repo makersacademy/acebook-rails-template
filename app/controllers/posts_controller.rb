1eb2d94ed5c039ee9ef9790f4444e55754ce573b
Joshua Brook <joshuabrookuk@gmail.com>
2020-07-03 11:37:24 +0100

    Created new branch, accidently unstaged my code.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    puts params
+    params
     @post = Post.find_by(id: params[:id])
     @post.destroy
     redirect_to posts_url
