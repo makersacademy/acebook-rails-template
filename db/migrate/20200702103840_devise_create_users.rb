4e4428ece2946308dd256697890677b7bfb5339b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-03 12:32:23 +0100

    added further comments

--- db/migrate/20200702103840_devise_create_users.rb
+++ db/migrate/20200702103840_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string :name, null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      #data type :columname null:false not empty
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
