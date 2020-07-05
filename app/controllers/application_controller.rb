6964019def16ed6f1b9d75b9df3b80b33e946cc8
Tim <TimCPB@github.com>
2020-07-05 19:14:14 +0100

    modify 'app/controllers/application_controller.rb' to authenticate users, redirect non-signed-in users to index and signed-in users to the '/posts' page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    # return the path based on resource
+    '/posts'
+  end
 end
