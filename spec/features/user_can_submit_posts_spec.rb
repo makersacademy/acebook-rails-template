8b0acc2a7271a02944fe6b22c7d3b4d54c230f6a
Rich Ewin <richardewinlambert@gmail.com>
2020-07-06 14:00:28 +0100

    time and date ammended

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
-RSpec.feature "Timeline", type: :feature do
-  scenario "Can submit posts and view them" do
-    visit "/posts"
-    click_link "New post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "New Post"
-    expect(page).to have_content("Hello, world!")
+RSpec.feature 'Timeline', type: :feature do
+
+  scenario 'Can submit posts and view them' do
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'New Post'
+    expect(page).to have_content('Hello, world!')
+  end
+
+  scenario 'Can submit a post and present a date and time' do
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'Whats the time?'
+    click_button 'New Post'
+    expect(page).to have_content("#{Time.now.strftime('%B %d %Y')}")
   end
 end
