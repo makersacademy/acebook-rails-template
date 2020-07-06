fcd9d3ef40b7c3ae4d6e81354963f5ae54015452
Tristan Langford <tristanlangford@hotmail.com>
2020-07-06 15:37:39 +0100

    redirect to wall after sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,46 @@
+class SessionsController < Clearance::BaseController
+    before_action :redirect_signed_in_users, only: [:new]
+    skip_before_action :require_login, only: [:create, :new, :destroy], raise: false
+  
+    def create
+      @user = authenticate(params)
+  
+      sign_in(@user) do |status|
+        if status.success?
+          redirect_back_or wall_url(params[:id] = current_user.id)
+        else
+          flash.now.alert = status.failure_message
+          render template: "sessions/new", status: :unauthorized
+        end
+      end
+    end
+  
+    def destroy
+      sign_out
+      redirect_to url_after_destroy
+    end
+  
+    def new
+      render template: "sessions/new"
+    end
+  
+    private
+  
+    def redirect_signed_in_users
+      if signed_in?
+        redirect_to url_for_signed_in_users
+      end
+    end
+  
+    def url_after_create
+      Clearance.configuration.redirect_url
+    end
+  
+    def url_after_destroy
+      sign_in_url
+    end
+  
+    def url_for_signed_in_users
+      url_after_create
+    end
+  end
--- spec/features/wall_spec.rb
+++ spec/features/wall_spec.rb
@@ -6,4 +6,11 @@ RSpec.feature "wall", type: :feature do
     expect(page).to have_content('wall of, testingName')
   end
 
+  scenario "user can delete their own posts" do
+    sign_up_with('email@eamil', 'testingName', "password")
+    sign_out
+    sign_in_with('email@eamil', "password")
+    expect(page).to have_content('wall of, testingName')
+  end
+
 end
