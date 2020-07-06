477ce87c9916e97e993e2dd69ac5dfdd3dbf07c7
Sophie <sophiebrown438@gmail.com>
2020-07-06 13:53:35 +0100

    Generate room controller and message model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'rooms/show'
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
