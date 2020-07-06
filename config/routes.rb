ce8e2989d0441ed0c381eb12b20c281d1c37e401
Sophie <sophiebrown438@gmail.com>
2020-07-06 14:20:53 +0100

    Generate Room channel

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'rooms/show'
-
+  mount ActionCable.server => '/cable'
+  
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
