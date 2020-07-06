da5cec7cbc23ad3a96b662e13198a67e712093af
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-07-06 16:21:13 +0100

    ability to comment complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
+    @comment = @post.comments.create(body: params[:comment][:body], user_id: current_user.id)
     redirect_to posts_url
   end
-
-
-  private
-    def comment_params
-     params.require(:comment).permit(:body)
-    end
 end
-
