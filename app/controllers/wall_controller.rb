d1ab3655988068feefe47771e57523847c4d966b
Rachael <rachaelewins@hotmail.com>
2020-07-06 18:26:58 +0100

    idea for POST wall/:id to link to posts/new

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -1,5 +1,12 @@
 class WallController < ApplicationController
 
+  def new
+    p params
+    @wall = params[:id]
+    @post = Post.new
+    @user_id = current_user.id
+  end
+
   def show
     @username = User.find(params[:id]).username
     @posts = Post.all.sort_by{ |post| post[:created_at] }.reverse
