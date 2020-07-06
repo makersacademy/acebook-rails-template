0f92fe463db462ec4774a00c9f63c88aebfd25e7
Tristan Langford <tristanlangford@hotmail.com>
2020-07-06 15:40:56 +0100

    wall test names fixed

--- spec/features/wall_spec.rb
+++ spec/features/wall_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "wall", type: :feature do
-  scenario "user can delete their own posts" do
+  scenario "user goes to their wall after sign up" do
     sign_up_with('email@eamil', 'testingName', "password")
     expect(page).to have_content('wall of, testingName')
   end
 
-  scenario "user can delete their own posts" do
+  scenario "user goes to their wall after sign in" do
     sign_up_with('email@eamil', 'testingName', "password")
     sign_out
     sign_in_with('email@eamil', "password")
