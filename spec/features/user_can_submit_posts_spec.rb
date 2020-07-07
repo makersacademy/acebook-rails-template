5c660fdf4c0f5e4ed4c1fbf41217310cdc4d3bd5
Emanuele Pace <emanuele.10@outlook.it>
2020-07-07 10:04:34 +0100

    New database addfriend and change test

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -6,8 +6,11 @@ RSpec.feature 'Timeline', type: :feature do
 
 
   scenario 'Can submit posts and view them' do
-    visit '/posts'
-
+    visit '/signup'
+    fill_in 'user[name]', with: 'Emanuele'
+    fill_in 'user[email]', with: 'ema@test.com'
+    fill_in 'user[password]', with: '12345'
+    click_on 'Create Account'
     click_link 'New post'
     fill_in 'Message', with: 'Hello, world!'
     click_button 'New Post'
@@ -17,7 +20,11 @@ RSpec.feature 'Timeline', type: :feature do
   end
 
   scenario 'Can submit a post and present a date and time' do
-    visit '/posts'
+    visit '/signup'
+    fill_in 'user[name]', with: 'Emanuele'
+    fill_in 'user[email]', with: 'ema@test.com'
+    fill_in 'user[password]', with: '12345'
+    click_on 'Create Account'
     click_link 'New post'
     fill_in 'Message', with: 'Whats the time?'
     click_button 'New Post'
@@ -26,7 +33,11 @@ RSpec.feature 'Timeline', type: :feature do
   end
 
   scenario 'Post can be added with line breaks' do
-    visit '/posts'
+    visit '/signup'
+    fill_in 'user[name]', with: 'Emanuele'
+    fill_in 'user[email]', with: 'ema@test.com'
+    fill_in 'user[password]', with: '12345'
+    click_on 'Create Account'
     click_link 'New post'
     fill_in 'Message', with: "line" + "\r\n" + "break!"
     click_button 'New Post'
