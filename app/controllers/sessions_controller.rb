7d49365c1ab08957c8bcaaba6f8e93edc6c64a48
Rich Ewin <richardewinlambert@gmail.com>
2020-07-07 18:41:21 +0100

    login working again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
-# frozen_string_literal: true
+  # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+
+  skip_before_action :require_login
   def new; end
 
   def create
