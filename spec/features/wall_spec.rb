699ece847ad32d38601b036f0b1f7d382bcc9ea6
Tristan Langford <tristanlangford@hotmail.com>
2020-07-07 12:24:03 +0100

    css title bar wall view

--- spec/features/wall_spec.rb
+++ spec/features/wall_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.feature "wall", type: :feature do
   scenario "user goes to their wall after sign up" do
     sign_up_with('email@eamil', 'testingName', "password")
-    expect(page).to have_content('wall of, testingName')
+    expect(page).to have_content('wall of... testingName')
   end
 
   scenario "user goes to their wall after sign in" do
     sign_up_with('email@eamil', 'testingName', "password")
     sign_out
     sign_in_with('email@eamil', "password")
-    expect(page).to have_content('wall of, testingName')
+    expect(page).to have_content('wall of... testingName')
   end
 
   scenario "user redirected to sign in" do
