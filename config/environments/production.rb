f20ab657947e9e0dd7cccfa8dfa9acba733fd706
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-07 16:10:17 +0100

    Some secret stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.require_master_key = true
+  config.read_encrypted_secrets = true
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   # Full error reports are disabled and caching is turned on.
