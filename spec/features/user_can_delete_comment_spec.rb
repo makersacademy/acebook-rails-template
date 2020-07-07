6003d11552453ad43aaae1e5cc8241f50b290133
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-07 17:20:03 +0100

    tests for deleting comments

---
+++ spec/features/user_can_delete_comment_spec.rb
@@ -0,0 +1,51 @@
+RSpec.feature "Timeline", type: :feature do
+  before(:each) do
+    visit "/"
+    click_link "Sign Up"
+    fill_in "user_firstname", with: "Bene"
+    fill_in "user_lastname", with: "Arinci"
+    fill_in "user_email", with: "test@gmail.com"
+    fill_in "user_password", with: "123456"
+    fill_in "user_password_confirmation", with: "123456"
+    click_button "Create User"
+    fill_in "email", with: "test@gmail.com"
+    fill_in "password", with: "123456"
+    click_button "Login"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    fill_in "comment_body", with: "new comment"
+    click_button "Comment"
+  end
+
+  scenario "comments have a delete link" do
+    within("#singlecomment") { expect(page).to have_link("Delete") }
+    #expect("singlecomment").not_to have_content("Hello, world!")
+  end 
+
+  scenario "users can delete their own comments" do 
+    within("#singlecomment") {click_link("Delete")}
+    expect(page).not_to have_content("new comment")
+  end 
+
+  scenario "user receives a confirmation message after deleting a post" do
+    within("#singlecomment") {click_link("Delete")}
+    expect(page).to have_content("comment successfully deleted")
+  end
+
+  scenario "user cannot delete other user's posts" do
+    click_link("Log out")
+    click_link "Sign Up"
+    fill_in "user_firstname", with: "Anthony"
+    fill_in "user_lastname", with: "D"
+    fill_in "user_email", with: "ad@gmail.com"
+    fill_in "user_password", with: "123456"
+    fill_in "user_password_confirmation", with: "123456"
+    click_button "Create User"
+    fill_in "email", with: "ad@gmail.com"
+    fill_in "password", with: "123456"
+    click_button "Login"
+    within("#singlecomment") { expect(page).not_to have_link("Delete") }
+  end 
+
+
+end
