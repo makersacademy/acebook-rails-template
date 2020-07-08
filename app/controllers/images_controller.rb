980c086387071074d45410943f8d2bf218f7b5af
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-08 16:37:51 +0100

    Fix issue with deleting images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,7 @@
 class ImagesController < ApplicationController
   def destroy
     image_id = ActiveStorage::Attachment.find_by(:blob_id => params[:id]).id
-    ActiveRecord::Base.connection.execute("DELETE FROM active_storage_attachments WHERE blob_id = #{image_id};")
+    ActiveRecord::Base.connection.execute("DELETE FROM active_storage_attachments WHERE id = #{image_id};")
     redirect_to user_album_path(:user_id => params[:user_id], :id => params[:album_id])
   end
 end
