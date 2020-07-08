704d6a790f946fb57cb798ca1ffe428e1587333c
J Perrin <perrinjack96@gmail.com>
2020-07-08 10:33:38 +0100

    shows friends, pending and requests in list within profile

--- app/controllers/addfriends_controller.rb
+++ app/controllers/addfriends_controller.rb
@@ -2,6 +2,7 @@ class AddfriendsController < ApplicationController
   def index
   # pull data from database
   params[:id] = session[:user_id]
+  @current_user = User.find_by(id:  session[:user_id])  #current user set from session id. 
   end
 
 end
