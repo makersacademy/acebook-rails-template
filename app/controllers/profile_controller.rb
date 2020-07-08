b14e83f10f05aa3ee2161493cf0d1d5d7e11684b
Anthony <a.a.donovan@outlook.com>
2020-07-08 17:11:32 +0100

    a hardcoded image id appears on profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,13 +1,10 @@
 class ProfileController < ApplicationController
   
   def index
-    puts "hello"
-    p params.inspect
+    @photo = ActiveStorage::Blob.find_by(:id => 26)
     @user = User.find_by_id(params[:id])
-    id = @user.id
+    #id = @user.id
     @posts = Post.all
     @albums = Album.all
   end
-
-
 end
