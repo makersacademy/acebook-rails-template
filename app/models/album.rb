0bda14cfef3d90fa6bcb894dc08ebada030ff95f
Sumner1185 <alastair.sumner@yahoo.com>
2020-07-08 10:38:42 +0100

    Add validation to albums so title cannot be blank and image must be JPEG or PNG

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,24 @@
 class Album < ApplicationRecord
   has_many_attached :images
+  validates :title, presence: true
+  validate :image_type
+
   def thumbnail input
     return self.images[input].variant(resize: '300x300!').processed
   end
+
+  private
+
+  def image_type
+    if images.attached? == false
+      errors.add(:images, "are missing!")
+    end
+
+    images.each do |image|
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:images, 'needs to be a JPEG or PNG')
+      end
+    end
+  end
+
 end
