53e7efd58e02a547c37a4b8b5f6cf3904aeb465f
Tristan Langford <tristanlangford@hotmail.com>
2020-07-09 16:08:56 +0100

    heroku test 7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://damp-reaches-86620.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://damp-reaches-86620.herokuapp.com', /http:\/\/damp-reaches-86620.herokuapp.*/ ]
+  # config.action_cable.url = 'wss://damp-reaches-86620.herokuapp.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://damp-reaches-86620.herokuapp.com', /http:\/\/damp-reaches-86620.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
