783da3ba671d67927e6e1f2f23d4a8985985f5d4
Tristan Langford <tristanlangford@hotmail.com>
2020-07-09 16:02:12 +0100

    heroku test 6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.web_socket_server_url = "wss://damp-reaches-86620.herokuapp.com/cable" 
+  # config.web_socket_server_url = "wss://damp-reaches-86620.herokuapp.com/cable" 
 
-  config.action_cable.allowed_request_origins = ['https://damp-reaches-86620.herokuapp.com', 'http://damp-reaches-86620.herokuapp.com']
+  # config.action_cable.allowed_request_origins = ['https://damp-reaches-86620.herokuapp.com', 'http://damp-reaches-86620.herokuapp.com']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -45,8 +45,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://damp-reaches-86620.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://damp-reaches-86620.herokuapp.com', /http:\/\/damp-reaches-86620.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
