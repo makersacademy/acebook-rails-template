c59b841f94ba1bfaf6e5a0dbf6d47ae370127372
Tristan Langford <tristanlangford@hotmail.com>
2020-07-09 15:44:00 +0100

    heroku test 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.web_socket_server_url = "wss://damp-reaches-86620.herokuapp.com/cable" 
+
+  config.action_cable.allowed_request_origins = ['https://damp-reaches-86620.herokuapp.com', 'http://damp-reaches-86620.herokuapp.com']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
