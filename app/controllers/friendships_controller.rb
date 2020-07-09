55ad756c75ead32a12e542da665d4c4600607196
Rich Ewin <richardewinlambert@gmail.com>
2020-07-09 10:26:37 +0100

    user and friend move into DB

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,18 +2,16 @@ class FriendshipsController < ApplicationController
   def index
   # pull data from database
   params[:id] = session[:user_id]
-  @current_user = User.find_by(id:  session[:user_id])  #current user set from session id. 
-  
+  @current_user = User.find_by(id:  session[:user_id])  #current user set from session id.
+
   end
 
   def create
-    hash = { 'user_id' => 57 , 'friend_id' => 58 , 'confirmed' => TRUE   }
-    hash2 = { 'user_id' => 58 , 'friend_id' => 57 , 'confirmed' => TRUE   }
-     @friends = Friendship.create(hash)
-     @friends_inverse = Friendship.create(hash2)
-    p @friends.save
-    p @friends.errors.full_messages
+    @user_id = session[:user_id]
+    left = { 'user_id' => @user_id, 'friend_id' => params[:friend_id], 'confirmed' => TRUE   }
+    right = { 'user_id' => params[:friend_id], 'friend_id' => @user_id, 'confirmed' => TRUE   }
+    @friends = Friendship.create(left)
+    @friends_inverse = Friendship.create(right)
     redirect_to posts_path
   end
-
 end
