6e3ef4603bb7f580b2ac8534c3c86250179bbd90
Dhara <dhara.patel95@hotmail.co.uk>
2020-07-28 15:11:35 +0100

    Added an error message if article title isn't entered or less than 5 characters long

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,3 @@
 class Article < ApplicationRecord
-  validates :title, presence: true,
-                      length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 5 }
   end
