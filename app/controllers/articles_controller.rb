ae5dc5759d98d96164f04a7eea9e43ffe09ac5dd
Rabee93 <rabei_mghrabi@yahoo.com>
2020-07-28 14:41:12 +0100

    added validation functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,26 @@
 class ArticlesController < ApplicationController
     def index
       @articles = Article.all
-    end  
-    
+    end
+
     def show
       @article = Article.find(params[:id])
     end
 
     def new
+      @article = Article.new
     end
 
-    def create
-      
+
+   def create
+
       @article = Article.new(article_params)
- 
-      @article.save
-      redirect_to @article
+
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end
     end
 
     private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
-end
+  validates :title, presence: true,
+                      length: { minimum: 5 }
+  end
