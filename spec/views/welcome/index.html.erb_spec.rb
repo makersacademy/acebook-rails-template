5720966893d303aad8e4605fa02a2c65a0d1f889
Josh <joshua.ng2332@gmail.com>
2020-07-28 15:19:58 +0100

    Created first test. Can see the welcome message.

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "welcome/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe "welcome/index.html.erb", type: :view do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+# describe Signup do
+#   it 'has a welcome message' do
+#     expect(page).to 
+#   end
+# end
+
+RSpec.feature "Welcome", type: :feature do
+  scenario "Can see the welcome message" do
+    visit "/welcome/index"
+    expect(page).to have_content("Welcome to Acebook!")
+  end
 end
