9225587ec9488eda1605c8461db1830a6bd58fbe
sam landman <sam landman>
2020-07-28 12:44:40 +0100

    Created homepage that displays all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,25 @@
 class ArticlesController < ApplicationController
+    def index
+      @articles = Article.all
+    end  
+    
+    def show
+      @article = Article.find(params[:id])
+    end
+
     def new
     end
 
     def create
-      render plain: params[:article].inspect
+      
+      @article = Article.new(article_params)
+ 
+      @article.save
+      redirect_to @article
+    end
+
+    private
+    def article_params
+      params.require(:article).permit(:title, :text)
     end
 end
