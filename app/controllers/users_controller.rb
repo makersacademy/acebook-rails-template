019348bf6f920f53e6c9edb512375f4444ca517c
Dhara <dhara.patel95@hotmail.co.uk>
2020-07-29 14:23:32 +0100

    Added links between welcome and signup pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
+
+  def index
   end
 
-  def new
+  def show
+    @user = User.find(params[:id])
   end
 
   def create
