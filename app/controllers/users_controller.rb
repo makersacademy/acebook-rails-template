5f72ea63dab82295ac1880320f642e54f79b1eb9
Dhara <dhara.patel95@hotmail.co.uk>
2020-07-30 15:35:45 +0100

    user_id only stored in session once logged in and newest post listed first

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   def create
 
     @user = User.create(user_params)
-    session[:user_id] = @user.id
+    # session[:user_id] = @user.id
     redirect_to '/welcome'
 
     # @user = User.new(user_params)
