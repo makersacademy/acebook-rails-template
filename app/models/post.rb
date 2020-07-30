b2fccbbe8c4dd381b2ae2433c2834afe8e749559
Dhara <dhara.patel95@hotmail.co.uk>
2020-07-30 17:55:46 +0100

    started creating one to many relationships for posts/users tables

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  
   validates :message, presence: true,
             length: { minimum: 10 }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create] 
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
