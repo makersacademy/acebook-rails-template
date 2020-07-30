6fcaa2b945c6938611f0ac5f0cb1656bbed585bb
Joanne Chen <joanne_r_chen@yahoo.com>
2020-07-30 12:12:43 +0100

    linked to /posts after login sucessful

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
        session[:user_id] = @user.id
-       redirect_to '/welcome'
+       redirect_to '/posts'
     else
        redirect_to '/login'
     end
