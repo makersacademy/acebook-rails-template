1cb5e49a078dfe0e05da80fbbf2a638315ae61f6
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-07-30 17:27:06 +0100

    Continued work on posts update functionality. Feature test for update post passes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,17 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    # @post = Post.update(post_params)
+    @post = Post.find(params[:id])
+    @post.update_attributes(post_params)
+    redirect_to posts_url
+  end
 
   private
 
