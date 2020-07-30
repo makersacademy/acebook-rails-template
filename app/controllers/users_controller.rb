1a7bf6abc1dafe73369c7fb853a75863d676e236
Rabee93 <rabei_mghrabi@yahoo.com>
2020-07-30 16:30:07 +0100

    added password number of characters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,14 @@
 class UsersController < ApplicationController
-
-  skip_before_action :authorized, only: [:new, :create]
-
-
   def new
     @user = User.new
   end
 
-  def index
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
   def create
-
-    @user = User.create(user_params)
-    # session[:user_id] = @user.id
-    redirect_to '/welcome'
-
-    # @user = User.new(user_params)
- 
-    # @user.save
-    # redirect_to @user
-    
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:email, :password)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+    render plain: params[:user].inspect
   end
 end
