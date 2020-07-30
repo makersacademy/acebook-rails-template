f39cb47c8fb190026de27c331415e86dc8a32f37
Joanne Chen <joanne_r_chen@yahoo.com>
2020-07-30 12:47:14 +0100

    directed home page to link without having to type /welcome

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :posts
 
-  # root 'welcome#index'
+  root 'sessions#welcome'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
