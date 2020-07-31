557323df75d6afdc4a7ecdd099e22fb624777f3c
Katie Jones <katieljones29@gmail.com>
2020-07-31 12:46:41 +0100

    fixing sign up button

--- db/migrate/20200728111005_create_posts.rb
+++ db/migrate/20200728111005_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :message
-      t.integer :users_id
+      t.integer :user_id
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
