a2aa92185f1983800144de4ae3f0838c7ecaadaa
Makers Admin <makersadmin@Makerss-MacBook-Air.local>
2020-07-31 13:38:49 +0100

    Test for blamnk message edge case

--- spec/features/user_can_update_posts_spec.rb
+++ spec/features/user_can_update_posts_spec.rb
@@ -17,6 +17,35 @@ RSpec.feature 'Timeline', type: :feature do
     expect(page).to have_content('Goodbye, world!')
   end
 
+  scenario 'Cannot update posts with blank message' do
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello World!'
+    click_button 'Submit'
+    
+    within(first('div.posts')) do
+      click_link 'Update post'
+    end
+
+    fill_in 'Message', with: ''
+    click_button 'Submit'
+    expect(page).to have_content('Hello World!')
+  end
+
+  scenario 'Cannot update posts with same message' do
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello World!'
+    click_button 'Submit'
+    
+    within(first('div.posts')) do
+      click_link 'Update post'
+    end
+
+    fill_in 'Message', with: 'Hello World!'
+    expect {click_button 'Submit'}.to raise_error('Not possible. Message has not been changed')
+  end
+
   scenario 'cannot update a post if wait longer than wait time' do
     p Time.new
     visit '/posts'
@@ -24,17 +53,32 @@ RSpec.feature 'Timeline', type: :feature do
     fill_in 'Message', with: 'Hello, world!'
     click_button 'Submit'
 
-    find('div.posts', wait: 10)
+    # find('div.posts', wait: 10)
 
-    visit '/'
-    visit '/posts'
-    
-    within(first('div.posts')) do
-      click_link('Update post')
-      p 'do we get here?'
+    # visit '/'
+    # visit '/posts'
+  
+
+
+
+    Capybara.using_wait_time 5 do
       p Time.new
+      visit '/'
+      visit '/posts'
+      within(first('div.posts')) do
+        click_link('Update post')
+        p 'do we get here?'
+        p Time.new
+      end
+      expect(page).to have_content("This is the posts feed")
     end
+    
+    # within(first('div.posts')) do
+    #   click_link('Update post')
+    #   p 'do we get here?'
+    #   p Time.new
+    # end
 
-    expect(page).to have_content("This is the posts feed")
+    # expect(page).to have_content("This is the posts feed")
   end
 end
