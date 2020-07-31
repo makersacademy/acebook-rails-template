b4c9bfe41cad40515c075ff6c15e48ad7cde0c28
Katie Jones <katieljones29@gmail.com>
2020-07-31 15:41:34 +0100

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:new, :create] 
+  resources :users, only: [:new, :create]
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
