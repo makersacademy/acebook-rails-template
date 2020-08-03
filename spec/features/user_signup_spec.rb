4d027172ba732e3e04d27914d20660400ba54483
Dhara <dhara.patel95@hotmail.co.uk>
2020-08-03 12:24:38 +0100

    sign up feature test passes

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.feature "signup", type: :feature do
   scenario "Can signup" do
     visit "/"
-    click_button "Sign up"
-    fill_in "Email", with: "test@test.com"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
+    click_button "Sign Up"
+    fill_in "user[email]", with: "test@test.com"
+    fill_in "user[password]", with: "123456"
+    click_button "Save User"
+    expect(page).to have_content("Welcome")
   end
 end
