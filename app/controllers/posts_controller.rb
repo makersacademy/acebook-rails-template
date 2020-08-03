652b65eeec58a4f9cd9951187e8fe2cc1698c950
Arav <arav.patel@gmail.com>
2020-08-03 15:03:39 +0100

    Add user to the post table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:message)
+    params.require(:post).permit(:message).merge(user_id: current_user.id)
   end
 
 end
