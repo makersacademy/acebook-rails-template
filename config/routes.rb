94d74a8e5401d240b655bb8fe51c5297675dcc8e
James Forster <jamesalexforster@gmail.com>
2020-08-04 14:31:54 +0100

    removed tests that conflicted with deleted routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :people
   resources :people
-  # get 'login/index'
-  # get 'welcome/index'
-  # post 'welcome/index'
-  # get 'signup/index'
+  get 'login/index'
+  get 'welcome/index'
+  post 'welcome/index'
+  get 'signup/index'
   # I don't actually think that we need any of the stuff above - I think that the routes are themselves defined in resources
   resources :posts
 
