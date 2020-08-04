15342b5ee19ab2400955d3a7acfd18d36b0dcc2c
Katie Jones <katieljones29@gmail.com>
2020-08-04 15:32:10 +0100

    css and adding logo

---
+++ spec/test_helper.rb
@@ -0,0 +1,9 @@
+module SignInHelper
+  def sign_in_as(user)
+    post sign_in_url(email: user.email, password: user.password)
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include SignInHelper
+end
