4c7992f93ec9a61da90c2389d16c5c603e05fd64
Katie Jones <katieljones29@gmail.com>
2020-08-05 12:31:43 +0100

    moved spec folder

--- app/spec/test_helper.rb
+++
@@ -1,9 +0,0 @@
-module SignInHelper
-  def sign_in_as(user)
-    post sign_in_url(email: user.email, password: user.password)
-  end
-end
-
-class ActionDispatch::IntegrationTest
-  include SignInHelper
-end
