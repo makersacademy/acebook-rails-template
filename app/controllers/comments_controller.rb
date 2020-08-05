8d7f72df24d38cd2be30456995d347f50e8633c8
sam landman <sam landman>
2020-08-05 17:51:29 +0100

    Sorted CSS and the comment for Body

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,20 +2,10 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    p @post
     @comment = @post.comments.create(comment_params)
-    p 'We have created a comment'
-    p @comment
-    p comment_params
-    p current_user.id
-    p @comment.user_id
     @comment.user_id = current_user.id
-    p 'We have reset user_id'
-    p @comment.user_id
-    p 'We have saved user_id in the database'
     @comment.save
-    p @comment.user_id
-    redirect_to post_path(@post)
+    redirect_to '/posts'
   end
 
   def destroy
