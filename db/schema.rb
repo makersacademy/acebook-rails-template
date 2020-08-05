2d9ff4d725e57b245e8fe53e53a95f1fb9f85b78
James Forster <jamesalexforster@gmail.com>
2020-08-05 11:13:59 +0100

    before destruction of posts

--- db/schema.rb
+++ db/schema.rb
@@ -37,10 +37,10 @@ ActiveRecord::Schema.define(version: 20200804155135) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email_address"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password"
   end
 
   add_foreign_key "posts", "people", column: "people_id"
