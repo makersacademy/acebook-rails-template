124655603995462262dba945c79a9bafde46b4f5
Arav <arav.patel@gmail.com>
2020-08-06 17:50:15 +0100

    updated yarn to include style loader and css loader and sass loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,4 +9,22 @@ environment.plugins.append('Provide',
   })
 )
 
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+};
+
 export default environment
