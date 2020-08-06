927cb7851c290902358bdccd8edbe6a185866410
Katie Jones <katieljones29@gmail.com>
2020-08-06 17:02:24 +0100

    added username parameter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,6 @@ class SessionsController < ApplicationController
     @user = User.new
   end
 
-  # def login
-  # end
-
   def create
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20200806091425) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
+    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
