b8319c300c277fa25a608ab4e955c2044a46f157
Ryan Viegas <rjkviegas@gmail.com>
2020-08-26 15:16:48 +0100

    User model and config routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
+  resources :users do
+    resources :posts
+  end
 
   root 'welcome#index'
 end
