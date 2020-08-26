6ab3f59c30614797151fe418754ea552eda2d89c
Patricia <prmonday@gmail.com>
2020-08-26 17:24:15 +0100

    add test for posts in descending order

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -8,4 +8,14 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+  scenario "Can submit posts and view them" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_link "New post"
+    fill_in "Message", with: "Hello, acebook-team2!"
+    click_button "Submit"
+    expect(Post.order(created_at: :desc)[0].message).to eq("Hello, acebook-team2!")
+  end
 end
