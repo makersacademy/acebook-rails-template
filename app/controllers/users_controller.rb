3a0fbe13ad3a1b2ac756d07f754d92803fad1a2d
Paul Humphreys <paulhumphreys81@aol.co.uk>
2020-08-26 17:57:19 +0100

    added user model/controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+
   def new
-    "Welcome to Acebook"
+    @user
   end
+
+  def create
+    @user = User.new(get_params)
+    @user.save
+    redirect_to @user
+  end
+
+private
+
+  def get_params
+    params.require(:user_details).permit(:first_name, :last_name, :password, :email)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
 
   resources :posts
+
+  root 'users#index'
 end
