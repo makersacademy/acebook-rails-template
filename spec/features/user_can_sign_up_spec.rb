c192bc74d576f013c046569beb08830b53f7f447
Harry LB <harry.lingardbright@gmail.com>
2020-08-27 11:56:11 +0100

    refactored form partial

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -16,17 +16,14 @@ RSpec.feature "Sign Up", type: :feature do
     expect(page).to have_field("user[name]")
     expect(page).to have_field("user[email]")
     expect(page).to have_field("user[password]")
-    fill_in "user[name]", with: "Test Johnson"
-    fill_in "user[email]", with: "testjohnson@testmail.com"
-    fill_in "user[password]", with: "1234"
-    click_button 'Save User'
+    sign_up
     expect(page).to have_content("Hello Test Johnson")
   end
   scenario 'users cannot sign up with invalid credentials' do
     visit '/users/new'
     fill_in "user[name]", with: "Big Suze"
     fill_in "user[password]", with: "Mental Posho"
-    click_button 'Save User'
+    click_button 'Create User'
     expect(page).to have_content "Please fill all fields."
     expect(page).to_not have_content "Hello Big Suze"
   end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -3,5 +3,5 @@ def sign_up
   fill_in "user[name]", with: "Test Johnson"
   fill_in "user[email]", with: "testjohnson@testmail.com"
   fill_in "user[password]", with: "1234"
-  click_button 'Save User'
+  click_button 'Create User'
 end
