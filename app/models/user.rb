5bb33afed1868ec984866a6446e55ec8fbec34cc
Daniel M <danielim@hotmail.co.uk>
2020-09-02 13:54:10 +0100

    Comment count implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   has_secure_password
 
+  def comments
+    count = 0
+    Comment.all.map { |comment| count += 1 if comment.poster.to_i == id }
+    count
+  end
+
 end
