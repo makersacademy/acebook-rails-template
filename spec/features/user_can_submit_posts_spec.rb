73e633965243fab8c3b01abc0e01fdc2697c539f
Iliyan-Y <maximizebg@gmail.com>
2020-09-22 14:38:33 +0100

    Display posts in reverse order test added

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -8,4 +8,19 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+
+  scenario "Can submit posts and view them" do
+     visit "/posts"
+     click_link "New post"
+     fill_in "Message", with: "Hello, world 1 !"
+     click_button "Submit"
+
+      visit "/posts"
+      click_link "New post"
+      fill_in "Message", with: "Hello, world 2 !"
+      click_button "Submit"
+  
+    expect(page).to have_content("Hello, world 2 !\nHello, world 1 !")
+  end
+
 end
