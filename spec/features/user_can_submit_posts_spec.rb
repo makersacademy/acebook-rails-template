799637f9d575e3ea9aadc8b86044798380fe3a7b
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-09-22 15:11:46 +0100

    Added timestamp, passing tests and timestamp formatting

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to have_content("Hello, world!")
   end
 
-  scenario "Can submit posts and view them" do
+  scenario "Newest post will show first" do
      visit "/posts"
      click_link "New post"
      fill_in "Message", with: "Hello, world 1 !"
@@ -20,7 +20,17 @@ RSpec.feature "Timeline", type: :feature do
       fill_in "Message", with: "Hello, world 2 !"
       click_button "Submit"
   
-    expect(page).to have_content("Hello, world 2 !\nHello, world 1 !")
+    expect(page).to have_content("Hello, world 2 !\n#{Time.now.to_formatted_s(:long)}\nHello, world 1 !")
   end
+
+  scenario "User can see date and time post was created" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world 1 !"
+    click_button "Submit"
+    Time.zone = "London"
+    expect(page).to have_content(Time.now.to_formatted_s(:long))
+  end
+
 
 end
