a9d76d3397c01bf7ec6237215a394e496f67b98b
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-24 12:24:39 +0100

    comment functionality working, restricted access to make comments to logged in users, restricted access to edit or delete comments to user who made the comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,22 +24,20 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    p params
-    p comment_params
-    @comment = Comment.new(comment_params)
-    # respond_to do |format|
-    #   if @comment.save
-    #     format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-    #     format.json { render :show, status: :created, location: @comment }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @comment.errors, status: :unprocessable_entity }
-    #   end
-    # end
+    @comment = current_user.comments.create(comment_params.merge(post_id: params[:post_id]))
+     respond_to do |format|
+       if @comment.save
+         format.html { redirect_to root_url, notice: 'Comment was successfully created.' }
+         format.json { render :show, status: :created, location: @comment }
+       else
+         format.html { render :new }
+         format.json { render json: @comment.errors, status: :unprocessable_entity }
+       end
+     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
+   # PATCH/PUT /comments/1
+   # PATCH/PUT /comments/1.json
   def update
     respond_to do |format|
       if @comment.update(comment_params)
