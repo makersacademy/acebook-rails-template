6bafd4702c0c1bc0d97264436cc35140f1edb4ce
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-24 16:42:06 +0100

    fixed redirect

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -55,7 +55,7 @@ class CommentsController < ApplicationController
   def destroy
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to root_url, notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
