c52e39c7986ef206a17b371eed733e6ae4f37385
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-28 15:39:12 +0100

    added test for being unable to log out if already logged out

--- spec/features/log_out_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature "Login", type: :feature do
-  scenario "Can  create account and log in when logged out" do
-    signup_and_login
-    expect(page).to have_content("Logged in as testuser.")
-  end
-end
