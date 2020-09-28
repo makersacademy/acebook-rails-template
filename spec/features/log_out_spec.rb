aba822cdb0abd1c21d4e7dbe8476ffb49abc37b0
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-28 15:31:02 +0100

    added tests for being able to create account and log in when logged out

--- spec/features/log_out_spec.rb
+++ spec/features/log_out_spec.rb
@@ -1,10 +1,8 @@
 require 'rails_helper'
 
-RSpec.feature "Logout", type: :feature do
-  scenario "Can log out when logged in" do
+RSpec.feature "Login", type: :feature do
+  scenario "Can  create account and log in when logged out" do
     signup_and_login
     expect(page).to have_content("Logged in as testuser.")
-    click_link "Log Out"
-    expect(page).not_to have_content("Hello, world!")
   end
 end
