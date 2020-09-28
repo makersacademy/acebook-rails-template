279e14362a280b89fdb3e8fde7d428e9ba441eaf
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-28 15:35:28 +0100

    added test to not be able to view profile when logged out

--- spec/features/view_profile_spec.rb
+++ spec/features/view_profile_spec.rb
@@ -10,4 +10,13 @@ RSpec.feature "View Profile", type: :feature do
     expect(page).to have_content("Destroy")
     expect(page).to have_content("Back")
   end
+  
+  scenario "Cant view profile when logged out" do
+    signup_and_login
+    expect(page).to have_content("My Profile")
+    expect(page).to have_content("Logged in as testuser.")
+    click_link "Log Out"
+    expect(page).not_to have_content("My Profile")
+    expect(page).not_to have_content("Logged in as testuser.")
+  end
 end
