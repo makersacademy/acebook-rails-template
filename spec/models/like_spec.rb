297c3dddaba2afe8238058ab866c0e9f23feaa59
Iliyan-Y <maximizebg@gmail.com>
2020-09-29 15:06:42 +0100

    Feature tests added

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # it "is valid with valid attributes" do
+  
+  #   expect(subject).belong_to(:user, :post)
+  # end
 end
