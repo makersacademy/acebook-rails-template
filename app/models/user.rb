a6efd50adc676ed4b02e6ede6b8c93264d21a29c
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-30 15:36:48 +0100

    fixed issue with deleting user causing error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_one_attached :photo
-  has_many :posts
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
