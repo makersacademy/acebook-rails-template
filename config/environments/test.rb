a815f41bd47f9cf7e9ff1274c762a46303332672
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-10-01 11:25:13 +0100

    added settings to test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.assets.check_precompiled_asset = false
+  config.assets.debug = false
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
