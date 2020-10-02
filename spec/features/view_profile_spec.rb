9dd37c153678794d8c361f91a9818e952061fdbb
Charlotte Smallwood-Smith <c.smallwood.smith@gmail.com>
2020-10-02 11:48:23 +0100

    Fixed failing tests and div tags

--- spec/features/view_profile_spec.rb
+++ spec/features/view_profile_spec.rb
@@ -5,10 +5,9 @@ RSpec.feature "View Profile", type: :feature do
     signup_and_login
     expect(page).to have_content("testuser's profile")
     click_link "My Profile"
-    expect(page).to have_content("Email: test@test.com")
+    expect(page).to have_content("test@test.com")
     expect(page).to have_content("Edit")
-    expect(page).to have_content("Destroy")
-    expect(page).to have_content("Back")
+    expect(page).to have_content("Delete Profile")
   end
   
   scenario "Cant view profile when logged out" do
