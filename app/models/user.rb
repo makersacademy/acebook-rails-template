cb51056e8bd2e036c5c3e67f7f678e71e795291c
Xavier Defontaine <xdefontaine@gmail.com>
2020-10-19 16:48:58 +0100

    messing around with ActiveRecord migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+  def initialize(email: , password:, first_name:, last_name:)
+    @email = email
+    @password = password
+    @first_name = first_name
+    @last_name = last_name
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
-require 'rails_helper'
+require './app/models/user.rb'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  
+  let(:user) { User.new(email: "test@test.com", password: "batman98!", first_name: "Bruce", last_name: "Wayne") }
+
+ 
+  it 'instantiates with the above' do
+    expect(user.email).to eq("test@test.com")
+    expect(user.password).to eq("batman98!")
+    expect(user.first_name).to eq("Bruce")
+    expect(user.last_name).to eq("Wayne")
+  end
+
+
 end
