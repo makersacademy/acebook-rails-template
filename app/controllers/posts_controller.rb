020abda1a1f2473f283a3b963617254f26fe7367
Johnny Yip <jyippy2012@gmail.com>
2020-10-20 11:12:50 +0100

    added update and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,24 @@ class PostsController < ApplicationController
     redirect_to posts_url
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else 
+      render 'edit'
+    end
+  end
+
+
   def index
     @posts = Post.all
   end
