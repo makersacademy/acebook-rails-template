06897a0ec6339c09b208421a9761e754ffaf0317
Louis Kirkham <louiskirkham@gmail.com>
2020-10-20 17:22:47 +0100

    CRUD feature testing complete

--- spec/features/edit_spec.rb
+++ spec/features/edit_spec.rb
@@ -2,9 +2,9 @@ describe "#edit" do
 	it "let's a user edit a post" do 
 		newpost
 		click_link "Edit"
-		find("edit").set("Testing Article")
+		find_field('post[message]').set("goodbye, world!")
 		click_button "Submit"
 		expect(page).not_to have_content("Hello, world")
-		expect(page).to have_content("Testing Article")
+		expect(page).to have_content("goodbye, world")
 	end 
 	end 
