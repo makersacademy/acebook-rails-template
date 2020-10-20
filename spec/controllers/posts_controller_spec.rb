a313f31bb21f44dbbf3098998042bec98810a4d4
Johnny Yip <jyippy2012@gmail.com>
2020-10-20 15:10:49 +0100

    update test dont working

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -25,5 +25,32 @@ RSpec.describe PostsController, type: :controller do
       get :index
       expect(response).to have_http_status(200)
     end
+    it "show returns 200" do
+      post = Post.create message: "hello"
+      get :show, params: { id: post.id }
+      expect(response).to have_http_status(200)
+    end
+
+    it "edit responds with 200" do
+      post = Post.create message: "hello"
+      get :edit, params: {id: post.id}
+      expect(response).to have_http_status(200)
+    end
+
+    # it "update method changes post" do
+    #   post = Post.create message: "hello"
+    #   get :edit, params: {id: post.id, message: "bye"}
+    #   # patch :update, params: {id: post.id, message: "bye"}
+    #   expect(Post.find_by(message: "bye")).to be
+    # end
+  end
+
+  describe "PUT update/:id" do
+    it "allows post to be updated" do
+      item = Post.create message: "hello"
+      post :update, item: {message: "Bye"}
+      item.reload  
+      expect(item.message).to eq("Bye")
+    end
   end
 end
