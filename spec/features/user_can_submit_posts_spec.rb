23d30e014e890d4c797ca5a83f8d7ac32188a80e
ollie beney <ombeney@gmail.com>
2020-10-21 17:28:03 +0100

    refactor to use updated rspec syntax

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,16 +1,27 @@
 require 'rails_helper'
 
-RSpec.feature "Timeline", type: :feature do
+feature "Timeline" do
   scenario "Can submit posts and view them" do
-    visit "/welcome"
-    click_button "Sign Up"
-    fill_in "Name", with: "Bob"
-    fill_in "Email", with: "bob@test.com"
-    fill_in "Password", with: "Bobobob"
-    click_button "Create User"
+    sign_up
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
+  end
+
+  scenario "can submit post and see users name" do
+    sign_up
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content("Bob:")
+  end
+
+  scenario "can submit post and see date of submission" do
+    sign_up
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content(/\d{4}-\d{2}-\d{2}/)
   end
 end
