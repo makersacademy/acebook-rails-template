71a5c51fd64b78194f81f04c1737ca5f58c948ad
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-10-21 17:04:16 +0100

    Setup comments table database.
    Comments will be used for storing comments on posts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201020150818) do
+ActiveRecord::Schema.define(version: 20201021155652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
