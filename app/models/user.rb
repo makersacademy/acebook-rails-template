0023a39787f65b01b730e17538248299ebc85fe5
Veronica Lee <veronicavlee@icloud.com>
2020-10-21 16:51:15 +0100

    Add email uniqueness check to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6, maximum: 10 }
 end
