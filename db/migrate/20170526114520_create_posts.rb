2bb50bcb5d51b6ac42cb32f94acd710f8f06ec17
Xavier Defontaine <xdefontaine@gmail.com>
2020-10-21 17:14:20 +0100

    FIXED DEVISE

--- db/migrate/20170526114520_create_posts.rb
+++ db/migrate/20170526114520_create_posts.rb
@@ -3,10 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :message
-<<<<<<< HEAD
     
-=======
->>>>>>> e4445b122def18e30507ffa56faddf5f6f743349
       t.timestamps
     end
   end
