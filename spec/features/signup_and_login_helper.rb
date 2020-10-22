b6cb5132fc50824ab765a0424c5ffa042a657f2d
Oliver Beney <62512189+obean@users.noreply.github.com>
2020-10-22 10:39:49 +0100

    Refactorspecs (#9)
    * refactor to use updated rspec syntax
    
    * delete unused files

--- spec/features/signup_and_login_helper.rb
+++
@@ -1,8 +0,0 @@
-def signup
-  visit "/welcome"
-  click_button "Sign Up"
-  fill_in "Name", with: "Bob"
-  fill_in "Email", with: "bob@test.com"
-  fill_in "Password", with: "Bobobob"
-  click_button "Create User"
-end
