40f7a343c39901f4e9300891091cb07c736b794d
Oliver Beney <62512189+obean@users.noreply.github.com>
2020-10-22 11:28:20 +0100

    Unique email (#10)
    * Add email uniqueness check to User model
    
    * refactor to use updated rspec syntax
    
    * Add db migration for email index on Users
    
    * delete unused files
    
    * Add email uniqueness check to User model
    
    * Add db migration for email index on Users
    
    * add feature test for duplicate email sign up
    
    Co-authored-by: Veronica Lee <veronicavlee@icloud.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :posts
   has_secure_password
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6, maximum: 10 }
 end
