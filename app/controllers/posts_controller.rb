f94b97221ba8047c0f3e8eaff3c736b55cfa2d4d
Linus Mjornstedt <carl.mjornstedt@gmail.com>
2020-10-22 11:51:58 +0100

    Added user id to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
 
   private
 
-  def post_params
-    params.require(:post).permit(:message)
+  def post_params        
+  params.require(:post).permit(:message, :user_id).merge(user_id: current_user.id)      
   end
 end
