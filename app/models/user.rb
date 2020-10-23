1c8c687eaa691d71beaa7916ae1fba6f9b87f9b8
Victoria Jusko <vikjusko@gmail.com>
2020-10-23 11:38:54 +0100

    Added devise and users sign in and sign up

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+   validates_presence_of :password, :email
+   has_many :posts, dependent: :destroy      
+end
