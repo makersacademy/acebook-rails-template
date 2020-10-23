fb7c06be6d34161281acf7e960ea3ed8484e57f3
Louis Kirkham <louiskirkham@gmail.com>
2020-10-23 15:37:13 +0100

    fixed CI test

--- spec/controllers/registrations_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe RegistrationsController, type: :controller do
-  describe 'create' do
-    it 'does not create a user without a valid email' do
-    end
-  end
-end
