75b58016a3a110d2e05df7fc10d3ddae709874ad
Victoria Jusko <vikjusko@gmail.com>
2020-10-23 14:23:03 +0100

    fixing feature tests and changing content of website

--- spec/features/delete_post_spec.rb
+++ spec/features/delete_post_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.feature "Delete", type: :feature do 
   scenario 'user can delete a post' do 
-    newpost
-    click_link 'Delete'
-    expect(page).not_to have_content('Hello world')
+    visit "/"
+    click_on "Sign up"
+    fill_in "Email", with: "user@test.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Confirm"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world!")
+    click_link "Delete"
   end 
 end 
