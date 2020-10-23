2d62b97071b64d2b2d0117a64bd8d4a3e199e4f7
Victoria Jusko <vikjusko@gmail.com>
2020-10-23 14:27:10 +0100

    Feature tests fixed

--- spec/features/test_helpers.rb
+++ spec/features/test_helpers.rb
@@ -5,3 +5,18 @@ def newpost
 	fill_in "Message", with: "Hello, world!"
 	click_button "Submit"
 end 
+
+def sign_up
+	visit "/"
+	click_on "Sign up"
+	fill_in "Email", with: "user@test.com"
+	fill_in "Password", with: "password"
+	fill_in "Password confirmation", with: "password"
+	click_on "Confirm"
+end
+
+def write_post
+	click_link "New post"
+	fill_in "Message", with: "Hello, world!"
+	click_button "Submit"
+end 
