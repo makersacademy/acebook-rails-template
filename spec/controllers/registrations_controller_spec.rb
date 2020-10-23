08113eb2930d1fbbf8f0be7732632e441152d4ba
Louis Kirkham <louiskirkham@gmail.com>
2020-10-23 14:32:54 +0100

    added name to edit form

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
-
+  describe 'create' do
+    it 'does not create a user without a valid email' do
+    end
+  end
 end
