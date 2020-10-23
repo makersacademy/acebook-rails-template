7e88e86d2af796ad56dc4cea8cd628ea262e2f01
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-10-23 15:47:56 +0100

    Adding Routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users,  controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users, controllers: {
+            sessions: "users/sessions",
+          }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "posts#index"
 
   resources :posts
   resources :comments
