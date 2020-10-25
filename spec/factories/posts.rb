ccd552ee81a9a3579d0e4e6df6d1f415dfbe0a2f
Connor Worthington <worthington.connor@gmail.com>
2020-10-25 12:46:16 +0000

    adds name to user Fake

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :post do 
     message { 'Hello world' }
     id { Faker::Number.unique.number }
-    user_id { 210 }
+    user_id { 5 }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,8 @@ require 'faker'
 
 FactoryBot.define do
   factory :user do 
+    name { Faker::Name.name }
     email { Faker::Internet.unique.email }
-    password { Faker::Internet.password }
+    password { Faker::Internet.password(max_length: 10) }
   end
 end
