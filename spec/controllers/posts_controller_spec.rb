514c7aaf41782e2c7d6eddbf30bc88b5f1f8b188
Connor Worthington <worthington.connor@gmail.com>
2020-10-26 10:51:06 +0000

    refactored testing

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,15 +2,14 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
   
-  # before(:each) do 
-  #   sign_in(user)
-  # end 
+  before(:each) do 
+    user = FactoryBot.create(:user)
+    @new_post = FactoryBot.create(:post) 
+    sign_in(user)
+  end 
   
   describe "GET / " do
     it "responds with 200" do
-      user = FactoryBot.create(:user)
-      new_post = FactoryBot.create(:post) 
-      sign_in(user)
       get :new
       expect(response).to have_http_status(200)
     end
@@ -18,17 +17,11 @@ RSpec.describe PostsController, type: :controller do
 
   describe "POST /" do
     it "responds with 200" do
-      user = FactoryBot.create(:user)
-      new_post = FactoryBot.create(:post) 
-      sign_in(user)
       post :create, params: { post: { message: "Hello, world!" } }
       expect(response).to redirect_to(posts_url)
     end
 
     it "creates a post" do
-      user = FactoryBot.create(:user)
-      new_post = FactoryBot.create(:post) 
-      sign_in(user)
       post :create, params: { post: { message: "Hello, world!" } }
       expect(Post.find_by(message: "Hello, world!")).to be
     end
@@ -36,64 +29,38 @@ RSpec.describe PostsController, type: :controller do
 
   describe "GET /" do
     it "responds with 200" do
-      user = FactoryBot.create(:user)
-      new_post = FactoryBot.create(:post) 
-      sign_in(user)
       get :index
       expect(response).to have_http_status(200)
     end
     it "show returns 200" do
-      user = FactoryBot.create(:user)
-      new_post = FactoryBot.create(:post) 
-      sign_in(user)
-      #post :create, params: { post: { message: "Hello, world!", id: 2 } }
-      #post = Post.create message: "hello", id: 2
-      post = new_post
-      get :show, params: { id: post.id }
+      get :show, params: { id: @new_post.id }
       expect(response).to have_http_status(200)
     end
 
     it "edit responds with 200" do
-      user = FactoryBot.create(:user)
-      new_post = FactoryBot.create(:post) 
-      sign_in(user)
-      post = new_post
-      #post = Post.create message: "hello"
-      get :edit, params: {id: post.id}
+      get :edit, params: {id: @new_post.id}
       expect(response).to have_http_status(200)
     end
 
     it "update method changes post" do
-      user = FactoryBot.create(:user) 
-      new_post = FactoryBot.create(:post)
-      sign_in(user)
-      patch :update, params: {id: new_post.id, post: { message: "bye" } }
+      patch :update, params: {id: @new_post.id, post: { message: "bye" } }
       expect(Post.find_by(message: "bye")).to be
     end
   end
 
   describe "PUT update/:id" do
     it "allows post to be updated" do
-      user = FactoryBot.create(:user) 
-      new_post = FactoryBot.create(:post)
-      sign_in(user)
-      patch :update, params: { id: new_post.id, post: { message: "Bye" } }
+      patch :update, params: { id: @new_post.id, post: { message: "Bye" } }
       expect(response).to be_redirect
     end
   end
 
   describe "DELETE" do  
     it 'responds with 200' do
-      user = FactoryBot.create(:user) 
-      new_post = FactoryBot.create(:post) 
-      new_post.destroy
       expect(response).to have_http_status(200)
     end 
     it 'should delete a post' do
-      user = FactoryBot.create(:user)
-      new_post = FactoryBot.create(:post) 
-      sign_in(user)
-      expect { new_post.destroy }.to change(Post, :count).by(-1) 
+      expect { @new_post.destroy }.to change(Post, :count).by(-1) 
     end 
    end 
 end
