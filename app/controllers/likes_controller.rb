285d42562b9bec9e8f4097b9ad26f520541b8585
Victoria Jusko <vikjusko@gmail.com>
2020-10-27 14:46:00 +0000

    Added feature tests for likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,38 +6,27 @@ class LikesController < ApplicationController
 	
 	def create
 		if !(already_liked?)
-		  @like = Like.create(post_id: params[:post_id], user_id: current_user.id)
-		  flash[:notice] = "You have liked this post"
+		  	@like = Like.create(post_id: params[:post_id], user_id: current_user.id)
+		  	flash[:notice] = "You have liked this post"
 		else
-		  @like = Like.find_by(user_id: current_user.id)
-		  @like.destroy
-		  flash[:notice] = "You have unliked this post"
+		 	 @like = Like.find_by(user_id: current_user.id)
+		  	@like.destroy
+		  	flash[:notice] = "You have unliked this post"
 		end
-		redirect_to root_url
+			redirect_to root_url
 	  end
-
-	# 	def destroy
-  	# # if !(already_liked?)
-   	# #  	@post.likes.create
-  	# # else
-   	# # 	@post.likes.destroy
-  	# # end
-	#   # redirect_to posts_url
-	#   @post.likes.where(user: current_user).destroy_all
-	#   redirect_to post_redirect(post), :notice => 'Unliked!'
-	# 	end
 
 	private
 	
 		def find_like
   		@like = @post.likes.find(params[:id])
-	end
+		end
 
-  def find_post
+  	def find_post
     	@post = Post.find(params[:post_id])
-  end
+  	end
 
-	def already_liked?
-  	Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
+		def already_liked?
+  		Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
 	end
 end
---
+++ spec/features/like_spec.rb
@@ -0,0 +1,25 @@
+RSpec.feature "Create a like", type: :feature do
+
+	scenario "The main page has a like button on it " do 
+		sign_up
+		add_new_post
+		expect(page).to have_selector(:link_or_button, "Like")
+	end 
+
+
+  scenario "Can like a post" do
+    sign_up
+		add_new_post
+		click_on "Like"
+    expect(page).to have_content("You have liked this post")
+	end
+	
+	scenario "Can unlike the post if already liked" do
+		sign_up
+		add_new_post
+		click_on "Like"
+		click_on "Like"
+		expect(page).not_to have_content("You have liked this post")
+		expect(page).to have_content("You have unliked this post")
+	end
+end
