b7fa718bb86a1f3c9152f648520d5185a546d61f
Louis Kirkham <louiskirkham@gmail.com>
2020-10-27 08:18:40 +0000

    merge conflict on schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -45,9 +45,6 @@ ActiveRecord::Schema.define(version: 20201026142004) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-<<<<<<< HEAD
-=======
 
   add_foreign_key "posts", "users"
->>>>>>> master
 end
