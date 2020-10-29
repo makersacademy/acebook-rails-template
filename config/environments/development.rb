bee5b391cef805065ccf6b2f092cdb7342836901
Xavier Defontaine <xdefontaine@gmail.com>
2020-10-29 11:32:59 +0000

    fixed photos and emails! (#19)
    * fixed photos and emails
    
    * trying new google cred
    
    * Update _user_post.html.erb
    
    Co-authored-by: Charlotte Brandhorst-Satzkorn <46385858+Catzkorn@users.noreply.github.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,18 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: "robotlizard2020@gmail.com",
+    password: "makersss123!",
+    domain: "localhost:3000",
+    address: "smtp.gmail.com",
+    port: "587",
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: "robotlizard2020@gmail.com",
+    password: "makersss123!",
+    domain: "localhost:3000",
+    address: "smtp.gmail.com",
+    port: "587",
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
