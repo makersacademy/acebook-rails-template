8ba76cedabd80fcae70b92ba429ffe9e64736a88
Dan Holmes <dh@danielholmes.com>
2020-11-17 16:12:48 +0000

    Hide sign up and log in buttons if logged in.

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -5,6 +5,10 @@ RSpec.feature "Sign Up", type: :feature do
     user_signup
     expect(page).to have_content("Welcome Arabella Knowles")
   end
-  # scenario "Sign up button not visible once signed in" do
+  scenario "Sign up button not visible once signed up" do
+    user_signup
+    expect(page).not_to have_content("Login")
+    expect(page).not_to have_content("Sign Up")
+  end
     
 end
