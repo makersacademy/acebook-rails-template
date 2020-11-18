8e04b448a007d8eba918248141609092954fe010
Gonzalo Berhouet <gonzaloberhouet@Gonzalos-Air.fritz.box>
2020-11-18 10:16:38 +0000

    login form and sessioncontroller updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,25 @@
 class SessionsController < ApplicationController
-
-  def new 
+  before_action only: [:new, :create, :destroy]
+  
+  def new
   end
 
   def create
-    @user = User.authenticate(username: params[:username], password: params[:password])
+    @user = User.authenticate(email: params[:email], password: params[:password])
     if @user 
-      session[:user_id] = user.id
-      redirect_to route_url
+      session[:user_id] = @user.id
+      render json: {
+        status: :created,
+        logged_in: :true,
+        user: @user
+      }
     else
-      flash[:notice] = "Username or password incorrect, please try again."
-      redirect_to new
+      render json:{status: 401}
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Logged out!'
   end
 end
