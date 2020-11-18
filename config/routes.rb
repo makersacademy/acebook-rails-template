22e94fb8e084ea44b4816d41531aa3af9eada77c
Emily Sesto <emilyalicesesto@gmail.com>
2020-11-18 14:20:20 +0000

    User can log out, log out notice is displayed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
+  get '/log_out' => 'sessions#destroy'
   resources :sessions
+  
 end
---
+++ spec/features/user_can_sign_out_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "Sign Out", type: :feature do
+  scenario "Can sign out" do
+    add_new_user
+    visit '/sessions/new'
+    fill_in "email", with: "test@testing.com"
+    fill_in "password", with: "test1234"
+    click_button("Log In")
+    click_link("Log Out")
+    save_and_open_page
+
+    expect(page).to have_content("Logged out!")
+    expect(page).not_to have_content("Test Person")
+  end
+end
