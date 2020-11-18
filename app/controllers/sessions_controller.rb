64a7d567b86dfe8a6f0adc9ec1512aa6d2917a6a
KC Thomas <thisiskcthomas@gmail.com>
2020-11-18 16:34:21 +0000

    Added code to validate emails using URI. Also Users who do not exist cannot sign in. Users who input an incorrect password cannot sign in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
-    # authenticate(email: params[:email], password: params[:password])
+
     respond_to do |format|
-      if @user
-        format.html { redirect_to posts_path}
+      if !@user
+        format.html { redirect_to root_url, notice: 'Email or Password is incorrect. Try again, or sign up!' }
+      else
+        format.html { redirect_to posts_path }
         session[:user] = @user
         format.json {
           render :show,
@@ -17,8 +19,6 @@ class SessionsController < ApplicationController
           logged_in: :true,
           user: @user
         }
-      else
-        render json:{status: 401}
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, email: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 end
