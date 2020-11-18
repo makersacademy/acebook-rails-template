96d5ed46f248d3214b840bbcb5575967d3e11446
Dan Holmes <dh@danielholmes.com>
2020-11-18 14:24:57 +0000

    Add minimum coverage and comment out tests. Expected to fail.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ SimpleCov.start 'rails' do
   add_filter "/app/jobs/"
   add_filter "/app/mailers/"
 end
+SimpleCov.minimum_coverage 100
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
