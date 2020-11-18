fad9aede36139197310ca2c0a4fc14367b7360c1
charlie-galb <galbraith.charlie@gmail.com>
2020-11-18 17:04:37 +0000

    Finish unit tests for Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  include ActiveSupport::Testing::TimeHelpers
   it { is_expected.to be }
+  it "time and date objects" do
+    @post = Post.new(message: "test")
+    Time.current 
+    travel_to Time.zone.local(2004, 11, 24, 01, 04, 44)
+    expect(@post.simplifydate(Time.current)).to eq("24-11-2004")
+    expect(@post.simplifytime(Time.current)).to eq("01:04")
+  end
 end
 
-before :each do
-  @post = Post.new(message: "test")
-  # let
-)
-it "time and date objects" do
-  expect(@post.simplifydate).to eq("")
-  
-end
   # render
 
 
