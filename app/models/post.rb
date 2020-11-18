aff3440d1eae120b4ccbe5164863c984be20c773
Gonzalo Berhouet <gonzaloberhouet@Gonzalos-Air.fritz.box>
2020-11-18 16:12:05 +0000

    reverse crono order, time and date stamp added on the posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  def simplifydate
+    return self.created_at.strftime('%d-%m-%Y')
+  end
+
+  def simplifytime
+    return self.created_at.strftime('%H:%M')
+  end
 end
