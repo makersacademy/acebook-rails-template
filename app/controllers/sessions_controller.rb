639d67711e42c0207a844709c1b685e14f7e4853
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-19 10:44:27 +0000

    Changed redirect route to posts url in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,24 @@ class SessionsController < ApplicationController
   # encrypt passwords 
 
   def new
-    
+
   end
 
-  def create
+  def create 
     # they log with their username:
     # if find_by_username(params["username"]) returns not nil - they are a user - if not throw flash message
     # check that their password matches their username, if not throw flash message, but we also need to encrypt the password
     @current_user = User.find_by_username(user_params["username"])
     session[:current_user_id] = @current_user.id
-    
+
     if @current_user.nil?
       flash[:notice] = "The username #{user_params["username"]} does not exist"
       redirect_to('/sessions/new')
-    # password authentication
+      # link to sign up - incase they haven't already
+    
+      # password authentication
     else
-      redirect_to('/posts')
+      redirect_to posts_url
     end
   end
   
