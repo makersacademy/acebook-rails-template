f28bb8312c03ef23fcb2bece8a5a849417fa9eb3
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-19 16:23:25 +0000

    Added valid email checker to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   def self.unique_email_and_username?(username, email)
     unique_email?(email) && unique_username?(username)
@@ -11,6 +12,10 @@ class User < ApplicationRecord
 
   def self.unique_username?(username)
     User.find_by_username(username).nil?
+  end
+
+  def self.valid_email?(email)
+    ((email =~ VALID_EMAIL_REGEX) == 0) ? true : false
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { is_expected.to be }
+
   it "checks if username and email are unique" do
     expect(User.unique_email_and_username?("arakno", "arakno@makers.com")).to eq(true)
   end
@@ -10,5 +11,11 @@ RSpec.describe User, type: :model do
   end
   it "checks if email is unique" do
     expect(User.unique_email?("arakno@makers.com")).to eq(true)
+  end
+  it "checks if email is valid - passed valid email" do
+    expect(User.valid_email?("arakno@makers.com")).to eq(true)
+  end
+  it "checks if email is valid - passed invalid email" do
+    expect(User.valid_email?("araknomakers.com")).to eq(false)
   end
 end
