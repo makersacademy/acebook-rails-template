f018b9e5df1fdc39959c88592d2984745bc6e47c
Dan Holmes <dh@danielholmes.com>
2020-11-19 15:35:20 +0000

    Write test for not editing others' posts.

--- spec/features/editing_posts_spec.rb
+++ spec/features/editing_posts_spec.rb
@@ -24,4 +24,18 @@ RSpec.feature "Editing posts", type: :feature do
     user_signup('ds.danielh', 'dan@makers.com')
     expect(page).not_to have_link("edit")
   end
+
+  scenario "User receives an error message if they try to edit someone else's post" do
+    user_signup('arakno', 'arakno@makers.com')
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    click_link "Logout"
+    user_signup('ds.danielh', 'dan@makers.com')
+    post_id = Post.find_by(message: "Hello, world!").id
+    visit "/posts/#{post_id}/edit"
+    fill_in "Message", with: "New message"
+    click_button "Submit"
+    expect(page).to have_content("Error: You can only edit your own posts.")
+  end
 end
