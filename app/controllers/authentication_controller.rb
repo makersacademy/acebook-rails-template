e375bc207d7e1ce872bcfdccd91220f261197884
KC Thomas <thisiskcthomas@gmail.com>
2020-11-26 16:27:13 +0000

    Solve bug with user not being passed in json

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,12 +2,12 @@ class AuthenticationController < ApplicationController
   skip_before_action :authenticate_request
  
   def authenticate
-    command = AuthenticateUser.call(params[:email], params[:password])
+    command = AuthenticateUser.call(params[:user][:email], params[:user][:password])
  
     if command.success?
       render json: { 
         auth_token: command.result,
-        user: User.find_by(email: params[:email]),
+        user: User.find_by(email: params[:user][:email]),
         logged_in: true 
       }
     else
