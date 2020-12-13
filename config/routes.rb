fa33a7e26bb9da6e1bacc7404f59d4ee7e4aed8c
Sabiha Teladia <s.telads@gmail.com>
2020-12-13 14:03:22 +0000

    removed old user controller, replaced with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :posts
-  resources :users
-  # get "/sign-up", to: "devise/registrations#new", as: "sign_up"
   root to: "posts#index"
 end
