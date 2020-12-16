b2a2b9efa1439d6c30f14b3bb867f9d290da9520
stringiest <lucyjstringer@gmail.com>
2020-12-16 17:09:09 +0000

    added yarn and a test for creating posts

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara'
   gem 'rspec-rails'
+  gem 'selenium-webdriver'
 end
 
 group :development do
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'creates a new post' do
+    user = User.create(username: "test", email: "test@test.com", password_digest: "test123", bio: "testing since before I could test", profile_picture: "test")
+    post = Post.create(user_id: user.id, body: "I love testing posts")
+    expect(post.body).to eq("I love testing posts")
+  end
 end
