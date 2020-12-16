c5aa2ea3a4a7f801408b09b9ab3ed1035db6a2da
Alex Withington-Smith <alex@awithsmith.com>
2020-12-16 10:35:26 +0000

    added Post model with appropriate relationship to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_102139) do
+ActiveRecord::Schema.define(version: 2020_12_16_102955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_12_16_102139) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
