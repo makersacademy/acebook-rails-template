6a725051c894705b96ea8fe5b34a4ac8a69f6871
Alex Withington-Smith <alex@awithsmith.com>
2020-12-16 10:13:03 +0000

    reinit of project to use rails 6.1 and rspec/capybara

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :posts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
