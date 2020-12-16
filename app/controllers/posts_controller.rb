c535b02ef06b90b8ed6b07be87b1878f093d3ddd
stringiest <lucyjstringer@gmail.com>
2020-12-16 18:16:29 +0000

    added route & method to create post, needs sign-in to test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   get "/posts", to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "posts", to: "posts#create"
+
+  get "/posts/new", to: "posts#new"
 end
