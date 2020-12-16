0507defcf9d7aec95e1a05e5421e991c745ffb19
CodeZeus-dev <constantine.akritides@gmail.com>
2020-12-16 17:16:13 +0200

    updated bundler and generated migration called EnableUuid

--- db/migrate/20170526114520_create_posts.rb
+++ db/migrate/20170526114520_create_posts.rb
@@ -1,6 +1,6 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
-    create_table :posts do |t|
+    create_table :posts, id: :uuid do |t|
       t.string :message
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526114520) do
+ActiveRecord::Schema.define(version: 20201216151357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pgcrypto"
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
