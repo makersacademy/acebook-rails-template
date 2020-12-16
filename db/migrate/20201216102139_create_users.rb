0c91bfcfb4b57e985fb26fe6bc2720db662793f1
Alex Withington-Smith <alex@awithsmith.com>
2020-12-16 10:28:45 +0000

    added User model

---
+++ db/migrate/20201216102139_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :profile_picture
+      t.text :bio
+
+      t.timestamps
+    end
+  end
+end
