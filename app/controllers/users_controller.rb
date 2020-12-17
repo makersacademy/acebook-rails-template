b70964c068761469d5223c292410a736ded6b959
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-17 10:39:31 +0000

    User can sign up, username and email must be unique, passwords must match

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,36 @@
 class UsersController < ApplicationController
-  def index
-    render 'users/index'
+  def new
+    render 'new'
   end
 
   def create
+    if User.find_by(username: params[:username])
+      redirect_to '/signup', notice: 'Username taken, please choose a different username'
+      return
+    end
+
+    if User.find_by(email: params[:email])
+      redirect_to '/signup', notice: 'Email already registered'
+      return
+    end
+
+    if params[:password] != params[:password_confirmation]
+      redirect_to '/signup', notice: 'Password does not match confirmation'
+      return
+    end
+
     user = User.create(
       username: params['username'],
       email: params['email'],
       password: params['password'],
       password_confirmation: params['password_confirmation'],
-      profile_picture: params['profile_picture'],
-      bio: params['bio']
-    ) 
+      profile_picture: params['profile_picture']
+    )
+
+    if user
+      redirect_to '/posts'
+    else
+      redirect_to '/signup'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "posts#index"
   
-  get '/signup', controller: 'users', action: 'index'
+  get '/signup', controller: 'users', action: 'new'
+  post '/users', controller: 'users', action: 'create'
 
   get "/posts", to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
