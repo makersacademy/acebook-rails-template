a36304b842c32b25566726daf2207c4bd1062745
KAB <>
2020-12-17 15:51:22 +0000

    Update travis and run user authentication walkthrough

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
