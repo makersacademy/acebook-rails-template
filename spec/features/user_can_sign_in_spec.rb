b2002d5e646e5b4d8ddcb1dd1e66b1d2ed63a661
elsstrange <ellenabstrange@gmail.com>
2020-12-17 20:13:28 +0000

    Added route to log in button on index Updated feature test for log in to use log-in button on index, not going direct to /users/sign_in

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 feature 'signing in' do
   scenario 'user signs in with valid email and password' do
     User.create(:email => 'testenv@example.com', :password => 'testpass')
-    visit 'users/sign_in'
+    visit '/'
+    click_button 'Log in'
     fill_in 'Email', with: 'testenv@example.com'
     fill_in 'Password', with: 'testpass'
     click_button 'Log in'
