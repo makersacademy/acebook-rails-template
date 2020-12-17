93b5fcaf204f12db5e6a48eda32ab8a828b0c75b
CodeZeus-dev <constantine.akritides@gmail.com>
2020-12-17 15:16:23 +0200

    deleted posts implementation and generated posts scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
   # get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
 end
--- db/migrate/20201217123749_create_posts.rb
+++ db/migrate/20201217123749_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts, id: :uuid do |t|
-      t.string :message
-      # t.uuid :user_id
+      t.string :postBody
+      t.uuid :user_id
       t.timestamps
     end
   end
