5739621b65e061f9d27a8c0ec053353931d87e95
Margarida Martelo <margmartelo@gmail.com>
2020-12-17 16:43:19 +0000

    Added second feature test for registration

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,17 +1,29 @@
-
 feature 'registration' do
   scenario 'user signs up successfully' do
     visit('/signup')
-    p current_path
+
     fill_in('username', with: 'Margarida')
     fill_in('email', with: 'margarida@example.pt')
     fill_in('password', with: '2020')
     fill_in('password_confirmation', with: '2020')
     fill_in('profile_picture', with: '')
     click_button('Submit')
-    p current_path
 
     expect(current_path).to eq('/posts')
-    expect(page).to have_content('posts')
+    expect(page).to have_content('Posts!')
+  end
+
+  scenario "user's passwords do not match" do
+    visit('/signup')
+
+    fill_in('username', with: 'Margarida')
+    fill_in('email', with: 'margarida@example.pt')
+    fill_in('password', with: '2020')
+    fill_in('password_confirmation', with: '2021')
+    fill_in('profile_picture', with: '')
+    click_button('Submit')
+
+    expect(current_path).to eq('/signup')
+    expect(page).to have_content('Password does not match confirmation')
   end
 end
