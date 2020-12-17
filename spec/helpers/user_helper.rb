121550741035aaeb6e240d72963829ecf763b0ee
Symion Edwards <symion.edwards@gmail.com>
2020-12-17 13:18:48 +0000

    Written feature tests for user sign up. Created a helper method to sign up

---
+++ spec/helpers/user_helper.rb
@@ -0,0 +1,15 @@
+def sign_up
+  fill_in "user_first_name", with: "Wendy"
+  fill_in "user_last_name", with: "McWend"
+  fill_in "user_email", with: "wendy@wendy.com"
+  fill_in "user_password", with: "WendyWendy"
+  fill_in "user_password_confirmation", with: "WendyWendy"
+  click_button "Sign up"
+end
+    # def sign_in
+    #   visit '/'
+    #   click_link 'Sign In'
+    #   fill_in 'Email', with: 'test@test.com'
+    #   fill_in 'Password', with: 'Test123'
+    #   click_button 'Log in'
+    # end
