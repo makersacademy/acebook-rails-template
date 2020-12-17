e7ecbae16480e2527b01cf8fa74ba237db220b52
elsstrange <ellenabstrange@gmail.com>
2020-12-17 20:05:15 +0000

    User can sign in Added feature test for user sign in, accepting on current path and flash message visibility

---
+++ spec/features/user_can_sign_in_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature 'signing in' do
+  scenario 'user signs in with valid email and password' do
+    User.create(:email => 'testenv@example.com', :password => 'testpass')
+    visit 'users/sign_in'
+    fill_in 'Email', with: 'testenv@example.com'
+    fill_in 'Password', with: 'testpass'
+    click_button 'Log in'
+    expect(current_path).to eq '/'
+    expect(page).to have_content 'Signed in successfully.'
+  end
+end
