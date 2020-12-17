296dde663655e94d441624d7e485719ce0f6ff65
Margarida Martelo <margmartelo@gmail.com>
2020-12-17 16:56:32 +0000

    Added feature test for when email is already registered

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -32,7 +32,6 @@ feature 'registration' do
       password: '2020',
       password_confirmation: '2020'
     )
-    p user
 
     visit('/signup')
 
@@ -44,5 +43,25 @@ feature 'registration' do
 
     expect(current_path).to eq('/signup')
     expect(page).to have_content('Username taken, please choose a different username')
+  end
+
+  scenario 'email is taken' do
+    user = User.create(
+      username: 'Peter',
+      email: 'peter@example.pt',
+      password: '2020',
+      password_confirmation: '2020'
+    )
+
+    visit('/signup')
+
+    fill_in('username', with: 'Margarida')
+    fill_in('email', with: 'peter@example.pt')
+    fill_in('password', with: '2020')
+    fill_in('password_confirmation', with: '2020')
+    click_button('Submit')
+
+    expect(current_path).to eq('/signup')
+    expect(page).to have_content('Email already registered')
   end
 end
