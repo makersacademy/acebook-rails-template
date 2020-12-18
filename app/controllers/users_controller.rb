d68ae13b7de07eb00b6ccd6e69dfcc316591aae9
KAB <>
2020-12-18 12:39:06 +0000

    Conflicts resolved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,12 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-=======
 
   skip_before_action :authorized, only: [:new, :create]
 
->>>>>>> 7f51707f6b181b2f9ad4ceffc03750b95057a621
   def new
     @user = User.new
   end
 
   def create
-<<<<<<< HEAD
-    @user = User.create(user_params)
-    session[:user_id] = @user.id
-    redirect_to '/posts'
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password)
-  end
-=======
     @user = User.create(params.require(:user).permit(:username, :password))
     session[:user_id] = @user.id
     redirect_to '/posts' #sub this to /posts later
@@ -34,5 +19,4 @@ class UsersController < ApplicationController
     User.new(id: result[0]['id'], email: result[0]['email'])
   end
 
->>>>>>> 7f51707f6b181b2f9ad4ceffc03750b95057a621
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-  has_secure_password
-=======
 
     has_secure_password
-    
->>>>>>> 7f51707f6b181b2f9ad4ceffc03750b95057a621
+
 end
