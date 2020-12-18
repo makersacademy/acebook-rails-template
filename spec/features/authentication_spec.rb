250844564c6a7d74ef15ec0dda43c1ed0859dd3a
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-18 11:52:28 +0000

    Added authentication test for when user enters wrong password

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -33,5 +33,23 @@ feature 'authentication' do
 
     expect(current_path).to eq('/login')
     expect(page).to have_content('Invalid login details')
-  end 
+  end
+
+  scenario 'user enters incorrect password' do
+    User.create(
+      username: 'Malachi',
+      email: 'm.spencer@makers.com',
+      password: '2020',
+      password_confirmation: '2020'
+    )
+
+    visit('/login')
+
+    fill_in('email', with: 'm.spencer@makers.com')
+    fill_in('password', with: 'wrongPassword')
+    click_button('Submit')
+
+    expect(current_path).to eq('/login')
+    expect(page).to have_content('Invalid login details')
+  end
 end
