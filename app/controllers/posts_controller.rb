dafdddda430b466d5970414518cc3b231663dfea
Symion Edwards <symion.edwards@gmail.com>
2020-12-18 15:01:29 +0000

    Refactored old syntax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-
-  before_action :authenticate_user!, :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   # Might need to split it into multiple lines
 
   # GET /posts
