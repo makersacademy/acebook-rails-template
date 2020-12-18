4be85a112dbd1f9b55abd74c3a907c14ef5e0c47
Chris Walsh <highwalshy@gmail.com>
2020-12-18 12:32:30 +0000

    Fixed failing test

--- spec/features/user_can_delete_account_spec.rb
+++ spec/features/user_can_delete_account_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'User can delete account', type: :feature do
   scenario 'Can delete their account on SCZL' do
     register
     click_link 'User Settings'
-    click_button 'Delete my account'
+    click_button 'Delete account'
     expect(page).to have_content('Bye! Your account has been successfully cancelled. We hope to see you again soon.')
   end
 end
