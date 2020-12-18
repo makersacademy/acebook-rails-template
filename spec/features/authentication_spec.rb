a5a223bc77045fb4272acbf85fb782c9383aebff
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-12-18 11:45:14 +0000

    Added authentication test for incorrect email

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,6 +1,6 @@
 feature 'authentication' do
   scenario 'user logs in successfully' do
-    user = User.create(
+    User.create(
       username: 'Malachi',
       email: 'm.spencer@makers.com',
       password: '2020',
@@ -16,4 +16,22 @@ feature 'authentication' do
     expect(current_path).to eq('/posts')
     expect(page).to have_content('Successfully logged in')
   end
+
+  scenario 'user enters incorrect email' do
+    User.create(
+      username: 'Malachi',
+      email: 'm.spencer@makers.com',
+      password: '2020',
+      password_confirmation: '2020'
+    )
+
+    visit('/login')
+
+    fill_in('email', with: 'ai@makers.com')
+    fill_in('password', with: '2020')
+    click_button('Submit')
+
+    expect(current_path).to eq('/login')
+    expect(page).to have_content('Invalid login details')
+  end 
 end
