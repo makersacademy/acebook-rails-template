1c5ba7051b9a7870e29af0fd9c0f81a2add29616
CodeZeus-dev <constantine.akritides@gmail.com>
2020-12-27 13:36:14 +0200

    created association between users and posts and updated the schema with the foreign key

--- db/migrate/20201217123749_create_posts.rb
+++ db/migrate/20201217123749_create_posts.rb
@@ -2,8 +2,6 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts, id: :uuid do |t|
       t.string :postBody
-      # t.uuid :user_id
-      t.timestamps
     end
   end
 end
