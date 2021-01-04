c9b60b036bad6019fa9457e22e9be9e929771e15
CodeZeus-dev <constantine.akritides@gmail.com>
2021-01-04 16:38:40 +0200

    fixed an error for nil post.user and updated the posts migration so that it includes the timestamp columns

--- db/schema.rb
+++ db/schema.rb
@@ -29,8 +29,6 @@ ActiveRecord::Schema.define(version: 20210104105540) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
