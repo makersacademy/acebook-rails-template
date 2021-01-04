5328c0ac57fd346053012e6e84872c327bb3b907
elsstrange <ellenabstrange@gmail.com>
2021-01-04 20:11:01 +0000

    Sign up requires a unique email address User can easily move from sign up to log in if they realise they're already registered. Updated devise standard error message for duplicate email and standard log in hyperlink.

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 feature "Signing Up" do
 
-  scenario "New user signs up from index page  " do
+  scenario "New user signs up from index page" do
     visit "/"
     click_button "Sign up"
     expect(current_path).to eq('/users/sign_up')
   end
-
 
   scenario "Can sign up with valid details" do
     visit "/users/sign_up"
@@ -59,4 +58,14 @@ feature "Signing Up" do
     expect(current_path).to eq("/users")
   end
 
+  scenario "signing up with a duplicate email address can easily log in instead" do
+    User.create(:email => 'testenv@example.com', :password => 'testpass')
+    visit "/users/sign_up"
+    fill_in "Email", with: "testenv@example.com"
+    fill_in "Password", with: "123456"
+    fill_in "Password confirmation", with: "123456"
+    click_button "Sign up"
+    expect(page).to have_content("This email address has already signed up")
+    expect(page).to have_link("Already registered? Log in here", :href => "/users/sign_in")
+  end
 end
