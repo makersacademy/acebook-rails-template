7028700efed5cc49ce14fac524fc7fdb4a880373
Michael Linford <linford.mike@gmail.com>
2021-01-05 16:31:45 +0000

    Users can post to acebook-the-facebook-the-remix

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
+  attr_accessor :user_id
 end
---
+++ db/migrate/20210105143718_add_user_id_to_posts.rb
@@ -0,0 +1,5 @@
+class AddUserIdToPosts < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :posts, :user, foreign_key: { to_table: :users }
+  end
+end
