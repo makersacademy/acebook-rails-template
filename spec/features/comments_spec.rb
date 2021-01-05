4fa756a9ccd929e966a62b36c80110172fe224b7
stringiest <lucyjstringer@gmail.com>
2021-01-05 14:14:19 +0000

    created comments controller and added comments form to posts view, need to add create method for comments next

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -1,11 +1,11 @@
 feature 'comments' do
   scenario 'user can comment on post' do
     login_and_make_post
-    click_link 'Comments'
     fill_in('comment_body', with: 'It sure is!')
     click_button 'Comment'
 
-    expect(current_path).to eq('/posts/1')
+    expect(current_path).to eq('/posts')
+    click_link 'Comments'
     expect(page).to have_content('It sure is!')
   end
 end
