3dbbe4c1610bb78e94792fb5d44bcb9d87dd74b4
Malachi Spencer <malachispencer@MA309.fritz.box>
2021-01-06 00:41:54 +0000

    Removed unnecessary get_name method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :get_name
+  helper_method :current_user
   
   private
 
   def current_user
     User.find_by(id: session[:user_id])
-  end
-
-  def get_name(id:)
-    User.find_by(id: id).username
   end
 end
