66508daef8f1af8b6de4821c16c4f21e347af5f0
Amalia-Diana Palaghia <46627472+AmelieSK@users.noreply.github.com>
2021-01-11 19:06:11 +0000

    Update walls_controller.rb

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -1,18 +1,5 @@
 class WallsController < ApplicationController
   def show   
-    # p params[:id]
-    # session[:wall_id] = params[:id] || current_user.id
-    # # binding.pry
-    # p "before if"
-    # if User.find_by(id: session[:wall_id]).nil?
-    #   p "in if before redirect"
-    #   redirect_to "/#{current_user.id}"
-    #   p "in if after redirect"
-    # end
-    # p "after if"
-    # @user = User.find_by(id: session[:wall_id]) # || User.find_by(id: current_user.id)
-    # Wall.create(id: @user.id) if Wall.find_by(id: @user.id).nil?
-    # @wall = Wall.find_by(id: @user.id)
 
     session[:wall_id] = params[:id]
     @user = User.find_by(id: params[:id])
