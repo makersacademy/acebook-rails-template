7ae9e18d00b6404becad5ddf96fd221491c63072
elsstrange <ellenabstrange@gmail.com>
2021-01-12 21:12:30 +0000

    User updates a post All posts have update buttons, and all users can update all posts - future work to restrict this! Added #edit and #update actions to the posts controller Added view file for the update post form Linked the Update button in the wall view

--- spec/features/user_can_update_posts_spec.rb
+++ spec/features/user_can_update_posts_spec.rb
@@ -1,10 +1,12 @@
 describe 'updating posts' do
+  let(:original_post) { 'Updating this post' }
+  let(:updated_post) { 'This is an updated post message' }
   let!(:post_writer) { User.create(email: 'update@example.com', password: 'testtest') }
   let!(:wall_owner) { User.create(email: 'owner@example.com', password: 'testtest') }
   let!(:post) do
-    Post.create(message: 'Updating this post', user_id: post_writer.id, wall_id: wall_owner.id)
-  end 
-    
+    Post.create(message: original_post, user_id: post_writer.id, wall_id: wall_owner.id)
+  end
+
   before do
     visit '/users/sign_in'
     fill_in 'Email', with: post_writer.email
@@ -14,6 +16,17 @@ describe 'updating posts' do
 
   it 'displays an update button' do
     visit "/#{wall_owner.id}"
-    expect(page).to have_button "Update"
+    expect(page).to have_button 'Update'
+  end
+
+  it 'can update a post' do
+    visit "/#{wall_owner.id}"
+    click_button 'Update'
+    expect(page).to have_current_path "/posts/#{post.id}/edit", ignore_query: true
+    fill_in 'Message', with: updated_post
+    click_button 'Update'
+    expect(page).to have_current_path "/#{wall_owner.id}", ignore_query: true
+    expect(page).not_to have_content(original_post)
+    expect(page).to have_content(updated_post)
   end
 end
