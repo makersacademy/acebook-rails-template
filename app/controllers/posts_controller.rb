5e62f722d543fe5c7ddcb8817b0c774f524883be
elsstrange <ellenabstrange@gmail.com>
2021-01-12 21:12:30 +0000

    User updates a post All posts have update buttons, and all users can update all posts - future work to restrict this! Added #edit and #update actions to the posts controller Added view file for the update post form Linked the Update button in the wall view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,16 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to "/#{@post.wall_id}"
+  end
+
   private
 
   def post_params
