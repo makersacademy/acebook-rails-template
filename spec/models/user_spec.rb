1dd7b911d643bc4238c5ce4464e773edb12eaa5b
Sarah Power <sarahjonipower0@gmail.com>
2021-01-26 16:42:04 +0000

    feature test for sign up added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to be }
+
+  it 'creates a new instance of the User class' do
+    user = User.new 
+    expect(user).to be_instance_of(User)
+  end
 end
