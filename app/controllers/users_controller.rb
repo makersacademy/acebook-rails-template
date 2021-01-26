01ddc6bfebe7b7c1f81b5e4e512769415e570af6
Natasha McIntyre <natashamc.cs@gmail.com>
2021-01-26 17:00:25 +0000

    flash successful sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    redirect_to @user
+    redirect_to @user, notice: "You have successfully signed up to Acebook!"
   end
 
   def show
