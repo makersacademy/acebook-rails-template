f0725111e0007dafde39eaa658b86e9387f7c507
calavell <calavell@tcd.ie>
2021-01-26 15:01:37 +0000

    validates email and password on signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /\A(.+)@(.+)\z/,message: "invalid"  }
+  validates :password, :presence => true,
+                   :uniqueness => {:case_sensitive => true},
+                   :length => {:within => 6..10}
 end
