8a06cc4ad8ae379a03c6a4cc37b2b0513c90dc22
WillDixon93 <dixon.will1@gmail.com>
2021-01-26 11:19:55 +0000

    display user welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create 
+    @user = User.new(name: "...", email: "...", password: "...")
+
+    if @user.save 
+      redirect_to # users homepage 
+    else
+      render :new
+    end
   end
 end
+  
