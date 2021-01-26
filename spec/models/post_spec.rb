3cb26b1f55c86baabb2725343a89e015be80fea6
AJ <aj.monty@gmail.com>
2021-01-26 16:07:44 +0000

    added failing test and pink bg

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,3 +3,9 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   it { is_expected.to be }
 end
+
+describe "here is a failing test" do
+  it "should fail" do
+    expect("hello").to eq "goodbye"
+  end
+end
