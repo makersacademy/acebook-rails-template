f673e17f0dab97a85c1ea415dabdc7a24b14992f
calavell <calavell@tcd.ie>
2021-01-26 15:40:50 +0000

    redirects to new user page if not signed up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,25 @@
 class UsersController < ApplicationController
   def index
+    check_for_user
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    check_for_user
+    @user = User.find_by(id: session[:user_id])
   end
 
   def new
+    session[:user_id] = nil
     @user = User.new
   end
 
-  def create 
+  def create
     @user = User.new(user_params)
 
-    if @user.save 
-      redirect_to @user 
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
     else
       render :new
     end
@@ -25,5 +29,10 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password)
     end
+
+    def check_for_user
+      if session[:user_id] === nil
+        redirect_to new_user_url
+      end
+    end
 end
-  
