e94262b48ded3623d9e42d39123d9b0f10732b71
Natasha McIntyre <natashamc.cs@gmail.com>
2021-01-27 15:19:28 +0000

    password encryption

--- db/migrate/20210127151651_create_users.rb
+++ db/migrate/20210127151651_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :username
       t.string :email
-      t.string :password
+      t.string :password_digest
 
       t.timestamps
     end
