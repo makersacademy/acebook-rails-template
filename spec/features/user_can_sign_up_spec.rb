d0ed74b703aed7c872bfe59b10f84130c657b68b
Sarah Power <sarahjonipower0@gmail.com>
2021-01-27 12:33:20 +0000

    password hidden

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -4,10 +4,19 @@ RSpec.feature "Sign-Up", type: :feature do
   scenario "Can sign up to acebook" do
     visit "/"
     click_link "Sign up please!"
-    fill_in "Username", with: "Ian"
-    fill_in "Email", with: "ian@acebook.com"
-    fill_in "Password", with: "password"
+    fill_in "user_username", with: "Ian"
+    fill_in "user_email", with: "ian@acebook.com"
+    fill_in "user_password", with: "password"
     click_button "Sign Me Up!"
     expect(page).to have_content("Your homepage!")
+  end
+
+  scenario "Can sign up to acebook with hidden password" do
+    visit "/"
+    click_link "Sign up please!"
+    fill_in "Username", with: "Ian"
+    fill_in "Email", with: "ian@acebook.com"
+    fill_in "Password", with: "personal_password"
+    expect(page).to_not have_content("personal_password")
   end
 end
