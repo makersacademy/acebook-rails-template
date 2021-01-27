1f259d181bdc3604c9373d6bb4c4584d78c37dd1
Irina <igiosanu29@gmail.com>
2021-01-27 22:31:18 +0000

    Add User model with password and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,30 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
-    has_many :posts
+  include BCrypt
+
+  has_many :posts
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+
+  validates :user,
+    presence: true,
+    length: { minimum: 3, maximum: 25 }
+  validates :email,
+    presence: true,
+    length: { minimum: 10, maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX, message: "email address invalid, please try again" },
+    uniqueness: { case_sensitive: false } #to avoid email duplication
+  validates :password,
+    presence: true,
+    length: { minimum: 6, maximum: 255 }
 end
