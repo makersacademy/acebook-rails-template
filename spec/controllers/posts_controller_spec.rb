32a7ae26507355773714d6ce23e7871c1f759605
ollienorman <oliver.norman1991@gmail.com>
2021-01-27 12:38:49 +0000

    broken tests fixed

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+
   describe "GET /new " do
     it "responds with 200" do
+      expect(controller).to receive(:authorize).and_return("stubbed value")
       get :new
       expect(response).to have_http_status(200)
     end
@@ -10,11 +12,13 @@ RSpec.describe PostsController, type: :controller do
 
   describe "POST /" do
     it "responds with 200" do
+      expect(controller).to receive(:authorize).and_return("stubbed value")
       post :create, params: { post: { message: "Hello, world!" } }
       expect(response).to redirect_to(posts_url)
     end
 
     it "creates a post" do
+      expect(controller).to receive(:authorize).and_return("stubbed value")
       post :create, params: { post: { message: "Hello, world!" } }
       expect(Post.find_by(message: "Hello, world!")).to be
     end
--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     visit "/posts"
+    click_link "Sign Up"
+    fill_in "Email", with: "mrtester@test.com"
+    fill_in "Password", with: "test123"
+    fill_in "Password confirmation", with: "test123"
+    click_button "Sign Up"
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
