0543ff68253c947c9a056b37d6cdb9bb04ec1545
Sujeevan Rajagopalan <rsujee@MA307.local>
2021-01-27 15:58:11 +0000

    user can sign up spec working

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-#   describe "GET #index" do
-#     it "returns http success" do
-#       # get :new
-#       # fill_in(:name, with: "name")
-#       # fill_in(:email, with: "test@gmail.com")
-#       # fill_in(:password, with: "namepass")
-#       # click_button("Create User")
-#       get :index
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-#
+  describe "GET #index" do
+    it "returns http success" do
+      # get :new
+      # fill_in "Name", with: "name"
+      # fill_in "Email", with: "test@gmail.com"
+      # fill_in "Password", with: "namepass"
+      # fill_in "Password_confirmation", with: "namepass"
+      # click_button "Create User"
+      # get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
  end
