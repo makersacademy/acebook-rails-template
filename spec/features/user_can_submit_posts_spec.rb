6baf74901eee8563eb70fc20841b95717c47419e
Kiki Dawson <kikiadawson@gmail.com>
2021-01-27 12:30:28 +0000

    test added for dates on posts

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
+TIMESTAMP = Time.now
+FORMATTED_TIME = TIMESTAMP.strftime("%d %b %Y %k:%M")
+
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     visit "/posts"
@@ -7,6 +10,6 @@ RSpec.feature "Timeline", type: :feature do
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
-    expect(page).to have_content("Added at")
+    expect(page).to have_content("Added at #{FORMATTED_TIME}")
   end
 end
