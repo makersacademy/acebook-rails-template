c7bad4fe427484286b795a33792d4d346eb2d8f1
Ian Fleming <>
2021-01-28 11:38:47 +0000

    password error message

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,27 +2,53 @@ window.onload = function() {
   const userForm = document.getElementById('user-sign-up-form')
 
   const email = document.getElementById('user_email')
-  
+
   const emailError = document.querySelector('#user_email + span.error')
-  
-  email.addEventListener('input', function (event) {  
+
+  email.addEventListener('input', function (event) {
     if (email.validity.valid) {
-      console.log(email.validity)
-      emailError.textContent = ''; 
-      emailError.className = 'error'; 
+      emailError.textContent = '';
+      emailError.className = 'error';
     } else {
       showError();
     }
   });
-  
+
   function showError() {
     if (email.validity.valueMissing) {
       emailError.textContent = "You need to enter an email address."
     } else if (email.validity.typeMismatch) {
       emailError.textContent = "Entered value needs to be an email address."
-    } 
+    }
     emailError.className = 'error active';
   }
-  
-}
 
+
+
+  const password = document.getElementById('user_password')
+
+  const passwordError = document.querySelector('#user_password + span.error')
+
+  password.addEventListener('input', function (event) {
+    if (password.validity.valid) {
+      console.log('hello')
+      passwordError.textContent = '';
+      passwordError.className = 'error';
+    } else {
+      console.log('hi')
+      passwordShowError();
+    }
+  });
+
+  function passwordShowError() {
+    if (password.validity.valueMissing) {
+      passwordError.textContent = "You need to enter a password."
+      console.log(passwordError.textContent)
+    } else if (password.validity.tooShort) {
+      passwordError.textContent = "Password too short."
+      console.log(passwordError.textContent);
+    }
+    passwordError.className = 'error active';
+  }
+
+}
