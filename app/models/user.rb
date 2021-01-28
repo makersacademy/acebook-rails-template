59c5a135b0927e8a46846eee9a006c28dfc1c49d
Irina <igiosanu29@gmail.com>
2021-01-28 10:40:48 +0000

    added email and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,29 +2,25 @@ require 'bcrypt'
 
 class User < ApplicationRecord
   include BCrypt
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
 
   has_many :posts
 
-  def password
-    @password ||= Password.new(password_hash)
+  def password_string
+    @password_string ||= Password.new(self.password)
   end
 
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
+  def password_string=(new_password)
+    @password_string = Password.create(new_password)
+    self.password = @password_string
   end
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
-
-  validates :user,
-    presence: true,
-    length: { minimum: 3, maximum: 25 }
   validates :email,
     presence: true,
     length: { minimum: 10, maximum: 255 },
     format: { with: VALID_EMAIL_REGEX, message: "email address invalid, please try again" },
     uniqueness: { case_sensitive: false } #to avoid email duplication
-  validates :password,
+  validates :password_string,
     presence: true,
     length: { minimum: 6, maximum: 255 }
 end
