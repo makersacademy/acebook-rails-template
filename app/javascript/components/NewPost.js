aa8c93b9e9f96dc1df314ff658c5b0e961e3acd2
ollienorman <oliver.norman1991@gmail.com>
2021-01-29 15:35:07 +0000

    backend updated by react form

--- app/javascript/components/NewPost.js
+++ app/javascript/components/NewPost.js
@@ -3,11 +3,11 @@ import PropTypes from "prop-types"
 class NewPost extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { message: 'HARDCODE' };
+    this.state = { message: '' };
   }
 
   handleChange = (event) => {
-    this.setState({[event.target.message]: event.target.value});
+    this.setState({message: event.target.value});
   }
 
   handleSubmit = (event) => {
@@ -16,6 +16,9 @@ class NewPost extends React.Component {
     fetch('http://localhost:3000/posts', {
         method: 'POST',
         // We convert the React state to JSON and send it as the POST body
+        headers: {
+          'Content-Type': 'application/json',
+       },
         body: JSON.stringify(this.state)
       }).then(function(response) {
         console.log(response)
@@ -30,7 +33,7 @@ class NewPost extends React.Component {
       <form onSubmit={this.handleSubmit}>
         <label>
           message:
-          <input type="text" value={this.state.value} message="message" onChange={this.handleChange} />
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
