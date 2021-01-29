7995178828e0cfc9685bfde51e990c22ff84be92
Lisa <bardelli.lisa@gmail.com>
2021-01-29 08:45:41 +0000

    feat: add existing user check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+  # def current_user
+  #   if session[:user_id]
+  #     @current_user ||= User.find(session[:user_id])
+  #   else
+  #     @current_user = nil
+  #   end
+  # end
+
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    else
-      @current_user = nil
-    end
+    User.find_by(id: session[:user_id])
   end
 
   def authenticate_user!
