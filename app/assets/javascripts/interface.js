48b68a176d181f8b27dceb59a63c1c13ea5871ce
AJ <aj.monty@gmail.com>
2021-02-02 11:04:08 +0000

    started implementing like button

--- app/assets/javascripts/interface.js
+++ app/assets/javascripts/interface.js
@@ -7,40 +7,41 @@ fetch('/posts_api')
     let divPosts = document.getElementById("posts")
     divPosts.innerHTML = ''
     data.forEach(element => {
-
         let div = document.createElement('div')
         div.setAttribute('id', 'post')
         let user = `<p>${element.user_id}</p><br>`
         let message = `<p>${element.message}</p><br>`
         let time = `<p>${element.created_at}</p><br>`
         div.innerHTML = user + message + time
+        let button = React.createElement;
+        button.setAttribute('id', 'like-button');
+        const domContainer = document.getElementById('like-button');
+        ReactDOM.render(button(LikeButton), domContainer);
         divPosts.appendChild(div)
     });
   });
 
-// class LikeButton extends React.component {
-//     constructor(props) {
-//         super(props);
-//     }
-// }
 
-//   const e = React.createElement;
+  const e = React.createElement;
   
-//   class LikeButton extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = { liked: false };
-//     }
+  class LikeButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { liked: false };
+    }
   
-//     render() {
-//       if (this.state.liked) {
-//         return 'You liked this.';
-//       }
+    render() {
+      if (this.state.liked) {
+        return 'You liked this.';
+      }
   
-//       return e(
-//         'button',
-//         { onClick: () => this.setState({ liked: true }) },
-//         'Like'
-//       );
-//     }
-//   }
+      return e(
+        'button',
+        { onClick: () => this.setState({ liked: true }) },
+        'Like'
+      );
+    }
+  }
+
+const domContainer = document.getElementById('like-button');
+ReactDOM.render(e(LikeButton), domContainer);
