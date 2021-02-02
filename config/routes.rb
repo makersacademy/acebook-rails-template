0c4b5e4ea9247f464527d1a0ac31d492f1e5efb6
AJ <aj.monty@gmail.com>
2021-02-02 11:12:50 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "users#new"
 
+  get "/posts_api", to: "posts#posts_api"
   resources :sessions, only: [:create, :new]
 
   delete 'session' => 'sessions#destroy'
