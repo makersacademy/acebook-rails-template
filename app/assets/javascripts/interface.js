0fa3f9ce65dc79c49d2e79aaf8a05720ba0834a5
AJ <aj.monty@gmail.com>
2021-02-02 11:07:40 +0000

    starting on react

---
+++ app/assets/javascripts/interface.js
@@ -0,0 +1,43 @@
+console.log("I'm in the interface")
+
+fetch('/posts_api')
+  .then(response => response.json())
+  .then(data => {
+    
+    let divPosts = document.getElementById("posts")
+    divPosts.innerHTML = ''
+    data.forEach(element => {
+        let div = document.createElement('div')
+        div.setAttribute('id', 'post')
+        let user = `<p>${element.user_id}</p><br>`
+        let message = `<p>${element.message}</p><br>`
+        let time = `<p>${element.created_at}</p><br>`
+        div.innerHTML = user + message + time
+        divPosts.appendChild(div)
+    });
+  });
+
+
+  const e = React.createElement;
+  
+  class LikeButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { liked: false };
+    }
+  
+    render() {
+      if (this.state.liked) {
+        return 'You liked this.';
+      }
+  
+      return e(
+        'button',
+        { onClick: () => this.setState({ liked: true }) },
+        'Like'
+      );
+    }
+  }
+
+const domContainer = document.getElementById('like-button');
+ReactDOM.render(e(LikeButton), domContainer);
