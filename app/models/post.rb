9f1a39b8b704b516060dadf0152de7044055e9ae
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-02-02 15:00:59 +0000

    Add tests for post model -> Checks valid user and content cannot be nil

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates_presence_of :content
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it { is_expected.to be }
+  it "is not valid without valid user" do
+    expect(Post.new).not_to be_valid
+  end
+
+  it "is not valid without content" do
+    User.create(id: "1", username: "test_user", password: "very_secure_pw")
+    new_post = Post.new
+    new_post.user_id = "1"
+    expect(new_post).not_to be_valid
+  end
+
+  it "is valid with valid attributes" do
+    User.create(id: "1", username: "test_user", password: "very_secure_pw")
+    new_post = Post.new
+    new_post.user_id = "1"
+    new_post.content = "Hello World!"
+    expect(new_post).to be_valid
+  end
 end
