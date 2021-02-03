8e4a6e026b56798685f00207bcba2599e67e802e
LiviaBas <livia.basiu@gmail.com>
2021-02-03 18:58:33 +0000

    routes, boostrap, css, login, sign page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
+  devise_scope :user do
+    unauthenticated do
+     root :to => 'devise/sessions#new'
+    end
+
+    authenticated do
+      root :to => 'posts#index'
+    end
+  end
 end
