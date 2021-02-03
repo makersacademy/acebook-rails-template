ecc4a82fbb122771673a3f5899aa01b9029ab253
Mio Chung <miochung7@gmail.com>
2021-02-03 16:20:04 +0000

    added associations with post and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
+  has_many :posts, dependent: :destroy
 end
