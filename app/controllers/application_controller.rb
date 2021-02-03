3f9c1d16a27a3020c40f92f508921add6faa21ee
rayhan <rayhanm@hotmail.co.uk>
2021-02-03 10:30:48 +0000

    working sign-in and sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  helper_method :current_user
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    unless current_user
+      redirect_to login_path, notice: 'Please login'
+    end
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
