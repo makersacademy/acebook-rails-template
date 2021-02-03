886f8bf16978749e819121a1ba138f3a71c837c6
Mio Chung <miochung7@gmail.com>
2021-02-03 11:45:32 +0000

    amended database

--- db/migrate/20210202142059_create_posts.rb
+++ db/migrate/20210202142059_create_posts.rb
@@ -1,11 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      add_foreign_key :posts, :users
-      t.text :post
+      t.integer :user_id
+      t.text :post_content
 
       t.timestamps
       
     end
+    add_foreign_key :posts, :users
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20210202170234) do
-
-
+ActiveRecord::Schema.define(version: 20210202142059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.text "post"
+    t.integer "user_id"
+    t.text "post_content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
