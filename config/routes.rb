3cd5585dc51ed6a073948f42053b0a2cc49f2ab0
LiviaBas <livia.basiu@gmail.com>
2021-02-04 12:15:12 +0000

    css updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
+
   devise_scope :user do
     unauthenticated do
      root :to => 'devise/sessions#new'
     end
 
     authenticated do
-      root :to => 'posts#index'
-    end
+     root :to => 'posts#index'
   end
-end
+   end
+  end
