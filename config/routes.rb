2dda4a91c68f51b810e2dc178c38603cc6613d7b
AJ <aj.monty@gmail.com>
2021-02-04 09:36:16 +0000

    fixed log out

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:create, :new]
 
-  delete 'session' => 'sessions#destroy'
+  get 'session' => 'sessions#destroy'
 
   resources :posts
   resources :users
