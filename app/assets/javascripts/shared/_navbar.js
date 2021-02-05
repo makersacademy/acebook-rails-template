0b18a2d8790f98e364cbbd871aeaebef6e39e462
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-02-05 20:04:19 +0000

    Integrate profile page and make nav bar functional

---
+++ app/assets/javascripts/shared/_navbar.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    var thisUrl = window.location.pathname;
+    $("li").has(`a[href$="${thisUrl}"]`).attr("aria-current", "page");
+    $("li").has(`a[href$="${thisUrl}"]`).addClass("breadcrumb-item active");
+    $(`a[href$="${thisUrl}"]`).contents().unwrap();
+});
