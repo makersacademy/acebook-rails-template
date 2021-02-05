bfa58db0bca56fda15fbaba247b1e2163020d401
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-02-05 17:18:08 +0000

    Add basic like button / post layout

--- app/assets/javascripts/shared/_show_posts.js
+++ app/assets/javascripts/shared/_show_posts.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+  
   $(".post").hover(function(){
     $(this).css("border", "solid 3px black");
     }, function(){
     $(this).css("border", "solid 1px gainsboro");
   });
+  
 });
