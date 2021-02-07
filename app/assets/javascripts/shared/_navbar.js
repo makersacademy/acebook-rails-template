b0c252ec35a1e1ade27d689ece49237a93b178e4
atiyyah <atiyyah.rawat@gmail.com>
2021-02-07 18:10:13 +0000

    fix bug where user links on profile page posts were disabled

--- app/assets/javascripts/shared/_navbar.js
+++ app/assets/javascripts/shared/_navbar.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+    $(document).ready(function(){
     var thisUrl = window.location.pathname;
     $("li").has(`a[href$="${thisUrl}"]`).attr("aria-current", "page");
     $("li").has(`a[href$="${thisUrl}"]`).addClass("breadcrumb-item active");
-    $(`a[href$="${thisUrl}"]`).contents().unwrap();
+    $(`li > a[href$="${thisUrl}"]`).contents().unwrap();
 });
