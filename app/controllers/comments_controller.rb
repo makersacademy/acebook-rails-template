fc6892d4a389d07cc08465103d2a565e7de57589
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-02-08 13:12:41 +0000

    Write CommentsController & corresponding tests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.where(post_id: params[:post_id]).order(created_at: :desc)
+  end
+
+  def create
+    begin
+      @comment = Comment.create!(post_id: params[:post_id], user_id: session[:user]["id"], content: comment_params["content"])
+      flash[:primary] =  "Added Comment!"
+    rescue => exception
+      flash[:danger] = exception.message
+      # if invalid post, flashes error message & goes back to posts/new
+    end 
+    redirect_back fallback_location: "/"
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
