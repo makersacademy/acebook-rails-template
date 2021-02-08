17c214471e2da402742317a653840bf6af2ce09f
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-02-08 12:35:00 +0000

    Add Comment controller tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  describe "GET /posts/1/comments" do
+    it "responds with 200" do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
 
 end
