ee1b63b71eeb65a2859ff91fd4c6eabe2796811b
arbOP <newarbnor@hotmail.com>
2021-02-09 23:30:50 +0000

    fully completed homepage with some timeline addition

--- app/assets/javascripts/shared/_navbar.js
+++ app/assets/javascripts/shared/_navbar.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
     var thisUrl = window.location.pathname;
-    if( $(".form-group").length ){
-        thisUrl  = '/';
+    if( thisUrl == “/”){
+        $(“li”).has(“#profile”).attr(“aria-current”, “page”);
+        $(“li”).has(“#profile”).addClass(“breadcrumb-item active”);
+        $(“#profile”).contents().unwrap();
+    } else{
+        $(“li”).has(`a[href$=“${thisUrl}“]`).attr(“aria-current”, “page”);
+        $(“li”).has(`a[href$=“${thisUrl}“]`).addClass(“breadcrumb-item active”);
+        $(`li > a[href$=“${thisUrl}“]`).contents().unwrap();
     }
-    $("li").has(`a[href$="${thisUrl}"]`).attr("aria-current", "page");
-    $("li").has(`a[href$="${thisUrl}"]`).addClass("breadcrumb-item active");
-    $(`li > a[href$="${thisUrl}"]`).contents().unwrap();
 });
