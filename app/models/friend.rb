b5e8b5a6490ba188ffbbc9bcb2e99efac1b60d36
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-02-10 14:37:02 +0000

    Set up friend model

---
+++ app/models/friend.rb
@@ -0,0 +1,4 @@
+class Friend < ApplicationRecord
+  belongs_to :requester, class_name: "User", foreign_key: :requester_id
+  belongs_to :receiver, class_name: "User", foreign_key: :receiver_id
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :friends, dependent: :destroy
   validates_presence_of :username, :password
   has_secure_password
 end
