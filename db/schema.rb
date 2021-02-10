824967cfa12af80acd30804750c57288bcd5fea0
arbOP <newarbnor@hotmail.com>
2021-02-10 13:04:42 +0000

    removed little error on timeline post - it had the word git randomly showing

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20210209124730) do
-=======
 ActiveRecord::Schema.define(version: 20210209153305) do
->>>>>>> views
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
