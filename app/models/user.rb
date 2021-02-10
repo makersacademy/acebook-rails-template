daa7a88f7604cd77c8a1690489159b5d59066796
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-02-10 22:32:36 +0000

    Make user username have to be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :friends, dependent: :destroy
   validates_presence_of :username, :password
+  validates_uniqueness_of :username
   has_secure_password
 end
