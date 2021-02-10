953a92e31b7a75097dc6572d1f1b7b2a64f5b7eb
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-02-10 15:23:17 +0000

    Added accept friend_request method

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,12 +4,19 @@ class FriendsController < ApplicationController
     redirect_back fallback_location: "/"
   end
 
+  def update
+    friend_request = Friend.find_by(friend_params)
+    friend_request.status = "Accepted"
+    friend_request.save
+    redirect_back fallback_location: "/"
+  end
+
   private
   
   def friend_params
-    comment = params.require(:friend).permit
-    comment[:receiver_id] = params[:user_id]
-    comment[:requester_id] = session[:user]["id"]
-    return comment
+    friend_request = params.require(:friend).permit(:id)
+    friend_request[:receiver_id] = params[:user_id]
+    friend_request[:requester_id] = session[:user]["id"]
+    return friend_request
   end
 end
--- spec/controllers/friends_controller_spec.rb
+++ spec/controllers/friends_controller_spec.rb
@@ -9,7 +9,26 @@ RSpec.describe FriendsController, type: :controller do
 
     it "creates a friend request" do
       post :create, params: { user_id: 1, friend: { requester_id: 1 } }
-      expect(Friend.find_by(requester_id: 1)).to be
+      friend_request = Friend.find_by(requester_id: 1)
+      expect(friend_request.receiver_id).to eq 1
     end
-  end  
+  end
+
+  describe "PATCH /users/:user_id/comments/:id" do
+    before do
+      post :create, params: { user_id: 1, friend: { requester_id: 1 } }
+      # create friend request
+      patch :update, params: { user_id: 1, id: 1, friend: { requester_id: 1 } }
+      # accept friend request
+    end
+
+    it "responds with 200" do
+      expect(response).to redirect_to("/")
+    end
+
+    it "accepts a friend request" do  
+      friend_request = Friend.find_by(requester_id: 1, receiver_id: 1)
+      expect(friend_request.status).to eq "Accepted"
+    end
+  end
 end
