d8f6f1f386d0bee222d6a2c934a897adcdfb8e2a
LiviaBas <livia.basiu@gmail.com>
2021-02-10 18:37:10 +0000

    First commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,4 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user_posts = @user.posts
   end
-
-  def sign_up_params
-    params.require(:user).permit(:bio)
-  end
-
 end
- 
