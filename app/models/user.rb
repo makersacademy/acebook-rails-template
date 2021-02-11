c0f449a03ae9d362ecd65b2aa7c8ccf4c28bb31c
Bianca Iordache <biannca.iordache@gmail.com>
2021-02-11 10:15:04 +0000

    add relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,31 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  has_friendship
+
+  def friends?
+    self.friends
+  end
+
+  def friend_requests?
+    self.requested_friends.any?
+  end
+
+  def requested_friends?
+    self.pending_friends.any?
+  end
+
+  def invite_friend(user)
+    self.friend_request(user)
+  end
+
+  def not_friends
+    potential = []
+    User.all.each do |user|
+      if(self.friends_with?(user) != true && self != user && self.friends.include?(user) != true && self.pending_friends.include?(user) != true && self.requested_friends.include?(user) != true)
+          potential << user
+      end
+    end
+    potential
+  end
 end
