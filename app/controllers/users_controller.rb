131b2792426ca97b6c119849c3705a3b6662b071
Bianca Iordache <biannca.iordache@gmail.com>
2021-02-11 10:12:48 +0000

    modify users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
     @user_posts = @user.posts
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
 end
