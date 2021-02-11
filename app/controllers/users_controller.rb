37d9e7a13cf50fe6f8a94c1cc01ed8b8703ab164
Bianca Iordache <biannca.iordache@gmail.com>
2021-02-11 14:18:25 +0000

    add avatar table to db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
+  end
 end
