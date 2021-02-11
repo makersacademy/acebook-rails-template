8cb9831311229e8b193dda49e11196cb614042eb
Roury Hinds <roury_hinds@outlook.com>
2021-02-11 12:17:42 +0000

    refectored some code

--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_02_10_180050) do
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
+    t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
