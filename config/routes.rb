095476329128b880f1f8dbbc87bdf721764fcd12
Bianca Iordache <biannca.iordache@gmail.com>
2021-02-11 10:32:03 +0000

    add friends routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
   registrations: 'registrations'
 }
-  resources :posts
-  resources :users
 
   devise_scope :user do
     unauthenticated do
@@ -14,4 +12,15 @@ Rails.application.routes.draw do
       root :to => 'posts#index'
     end
   end
+
+  post "/friends/add" => "friends/add"
+  post "/friends/reject" => "friends/reject"
+  post "/friends/remove" => "friends/remove"
+  get "/friends/search" => "friends/search"
+  post "/friends/search" => "friends/search"
+
+  resources :posts
+  resources :users
+  resources :friends, only: [:index, :create]
+
 end
