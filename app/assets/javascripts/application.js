b4b052052584f9fd6a996b802b32f84482b89453
atiyyah <atiyyah.rawat@gmail.com>
2021-02-12 01:14:03 +0000

    Render friends on profile page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,10 +21,27 @@ function fadeAlert(){
 
 window.setTimeout(fadeAlert, 4000);
 
-$(document).ready(function(){
+function fixFooter(){
   var containerHeight = $('.container').outerHeight(true);
   var windowHeight = $('body').height();
   if(containerHeight < windowHeight ){
      $('.footer').css('position','fixed');
+  }else{
+    $('.footer').css('position','static');
   }
-});
+  console.log('AAAAA');
+}
+
+$(document).ready(function(){
+  fixFooter();
+  $('a').click(  function(){ 
+    var containerHeight = $('.container').outerHeight(true);
+    var windowHeight = $('body').height();
+    if(containerHeight < windowHeight ){
+      $('.footer').css('position','fixed');
+    }else{
+      $('.footer').css('position','static');
+    }
+    console.log('AAAAA');
+  });
+})
