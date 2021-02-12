d761468adcd99bd319d2478a8f3404d0f076fb6d
arbOP <newarbnor@hotmail.com>
2021-02-12 11:40:45 +0000

    changed timeline a bit, not much

--- app/assets/javascripts/shared/_navbar.js
+++ app/assets/javascripts/shared/_navbar.js
@@ -1,8 +1,7 @@
 // I DONT THINK WE NEED THIS ANYMORE, PREVIOUS JS FOR NAV BAR
 // TO HIGHLIGHT WHAT PAGE
 
-/*$(document).ready(function(){
+$(document).ready(function(){
     var thisUrl = window.location.pathname;
     $(`.sidenav > a[href$="${thisUrl}"]`).contents().unwrap().wrap("<p></p>");
 });
-*/
