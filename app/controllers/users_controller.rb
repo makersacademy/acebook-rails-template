55b1dd1d299193145d442f41704ccd1313b4cf58
Cintia <cintia_gloker@hotmail.com>
2021-02-16 18:19:44 +0000

    updated application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,22 +8,6 @@ class UsersController < ApplicationController
     redirect_to action: "login"
   end
 
-  def authenticate
-    user = User.find_by(username: params["username"])
-    if user && user.authenticate(params["password"])
-      session[:user_id] = user.id
-      flash[:primary] =  "You have logged in!"
-    else
-      flash[:danger] =  "Incorrect username or password"
-       # if invalid login, flashes error message & goes back to users/login
-    end
-    redirect_back fallback_location: "/"
-  end
-
-  def logout
-    session[:user_id] = nil
-    redirect_to "/"
-  end
 
   private
 
