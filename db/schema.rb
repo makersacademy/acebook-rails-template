06f4c4f79136cae18ef83d6533767cb85f33d4aa
K.T <64843479+Ghostglass@users.noreply.github.com>
2021-02-17 18:08:25 +0000

    "Made changes to the Schema for tests to pass"

--- db/schema.rb
+++ db/schema.rb
@@ -10,43 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20210211142201) do
+ActiveRecord::Schema.define(version: 20210215161615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "friendships", id: :serial, force: :cascade do |t|
-    t.string "friendable_type"
-    t.integer "friendable_id"
-    t.integer "friend_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "blocker_id"
-    t.integer "status"
-    t.index ["friendable_id", "friend_id"], name: "index_friendships_on_friendable_id_and_friend_id", unique: true
-  end
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
