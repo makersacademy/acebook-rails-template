a1ea4083353fccb6ee060f637198a6e0a95a52d8
LiviaBas <livia.basiu@gmail.com>
2021-02-17 17:51:20 +0000

    changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def current_url_contains segment
+    return request.env['PATH_INFO'].include? segment
+  end
+  def user_logged_in?
+    return session[:user_id] != nil
+  end
 end
