8f195de3462a567eaf65f3241c69a625ad91741e
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-02-19 10:21:47 +0000

    fix posts controller specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,19 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe PostsController, type: :controller do
+RSpec.describe "/courses/:course_id/posts", type: :request do
   describe "GET /" do
     it "responds with 200" do
-      get :index, params: { course_id: 1 }
+      get course_posts_url(1) , params: { course_id: 1 }
       expect(response).to have_http_status(200)
     end
   end
 
   describe "GET /new " do
     it "responds with 200" do
-      get :new, params: { course_id: 1 }
+      get new_course_post_url(1)
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET courses/:course_id/posts/:id" do
+    it "responds with 200" do
+      get course_posts_url(1,1)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET courses/:course_id/posts/:id/edit" do
+    it "responds with 200" do
+      get edit_course_post_url(1,1)
+      expect(response).to have_http_status(200)
+    end
+  end
+  
+end
+
+RSpec.describe PostsController, type: :controller do
 
   describe "POST /" do
     it "responds with 200" do
@@ -24,20 +42,6 @@ RSpec.describe PostsController, type: :controller do
     it "creates a post" do
       post :create, params: { course_id: 1, post: { content: "Hello, world!" } }
       expect(Post.find_by(content: "Hello, world!")).to be
-    end
-  end
-
-  describe "GET courses/:course_id/posts/:id" do
-    it "responds with 200" do
-      get :show, params: { course_id: 1, id: 1}
-      expect(response).to have_http_status(200)
-    end
-  end
-
-  describe "GET courses/:course_id/posts/:id/edit" do
-    it "responds with 200" do
-      get :edit, params: {course_id: 1, id: 1}
-      expect(response).to have_http_status(200)
     end
   end
 
