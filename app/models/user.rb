42101d48c6c67b11c2e6019e40b6a910b47c52dd
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-02-19 22:08:24 +0000

    show avatar on profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,14 @@ class User < ApplicationRecord
 
   has_many :courses, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
+  
   has_one_attached :avatar
+  after_commit :add_default_avatar, on: [:create, :update]
+
+  private def add_default_avatar
+    unless avatar.attached?
+      self.avatar.attach(io: File.open(Rails.root.join("app", "assets", "images", "default-profile.png")), filename: 'default-profile.png' , content_type: "image/png")
+    end
+  end
+
 end
