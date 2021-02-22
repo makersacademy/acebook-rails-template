904038e35fbe29a4564fbd41cf2657ab9845a9c5
Cintia <cintia_gloker@hotmail.com>
2021-02-22 20:40:40 +0000

    added checkbox to remember login details

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ApplicationRecord
 
   validates_presence_of :username, :password
   validates_uniqueness_of :username
+  validates :password, confirmation: true
   has_secure_password
 
   has_many :courses, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
-  
+
   has_one_attached :avatar
 
 end
