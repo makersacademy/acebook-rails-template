581ad198ececee8dd4946d811a60c2f69b9f7a92
Reeshul <reeshul@hotmail.com>
2021-02-24 14:53:33 +0000

    feature test for user registration

--- db/migrate/20210224105515_create_users.rb
+++ db/migrate/20210224105515_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :email, null: false
       t.string :password_digest
-
+      t.string :first_name, null: false
+      t.string :last_name
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2021_02_24_105515) do
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
+    t.string "first_name", null: false
+    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
