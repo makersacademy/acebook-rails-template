8c2c0cf8f9d28d05d949792372be30a5f694ff09
bateso88 <bateso@hotmail.co.uk>
2021-02-24 16:14:53 +0000

    added update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,16 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to posts_url
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   private
 
   def post_params
