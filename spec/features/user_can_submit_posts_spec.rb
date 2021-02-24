07c267a369597c608da6c217d9a33c0e138f8c71
Karsten Finlay <karstenfinlay@hotmail.com>
2021-02-24 12:41:35 +0000

    Tested that posts are shown in order from newest first

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -10,4 +10,15 @@ RSpec.feature 'Timeline', type: :feature do
     click_button 'Submit'
     expect(page).to have_content('Hello, world!')
   end
+
+  scenario 'Posts are shown in order of creation' do
+    visit '/posts'
+    click_link 'New post'
+    fill_in 'Message', with: 'I am second'
+    click_button 'Submit'
+    click_link 'New post'
+    fill_in 'Message', with: 'I am first'
+    click_button 'Submit'
+    expect(page.all('p')[0]).to have_content 'I am first'
+  end
 end
