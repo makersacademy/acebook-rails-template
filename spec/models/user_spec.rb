f7c2b5c7f519f5965819fd83a30cec6d9a5af08d
Reeshul <reeshul@hotmail.com>
2021-02-24 14:14:09 +0000

    changed email for test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe User, type: :model do
   describe '#create' do
     
     it 'creates a user with a password, and email' do
-      User.create({email: "test@email.com", password: "testPassword",  password_confirmation: "testPassword" })
-      expect(User.first.email).to eq("test@email.com")
+      User.create({email: "test@anotheremail.com", password: "testPassword",  password_confirmation: "testPassword" })
+      expect(User.first.email).to eq("test@anotheremail.com")
     end
     
   end
