8ede20f1ad042e589dfd29c15edeb9864207c07e
jshields123 <jackshields@live.co.uk>
2021-02-24 10:29:16 +0000

    added to the route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'post#index'
+  root to: 'post#index'
+  get 'new' => 'posts#new'
   resources :posts
 end
