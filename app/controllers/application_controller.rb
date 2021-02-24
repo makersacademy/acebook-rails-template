99671dfd675d322f694f68a0ffbe32a2dd36bed7
Hamish <hamisharro@icloud.com>
2021-02-24 12:38:39 +0000

    add redirects for when the user isnt signed in and controllers for the Users class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :require_login, except:[new_user_registration]
+
+  private
+
+  def require_login
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+  skip_before_action :require_login
   def index
+    if user_signed_in?
+      redirect_to posts_path
+    end
   end
 end
