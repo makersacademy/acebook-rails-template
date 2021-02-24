bbbe0502942f70e05eb4d3ed23153448f354b3dc
Salar Ghotaslo <Salar_@live.co.uk>
2021-02-24 11:48:10 +0000

    user can sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
 
-  root 'posts#index'
+  get 'posts/index'
+
+  root to: "welcome#index"
 end
