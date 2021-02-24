fd14b8770d7680cfb3b351bff03f1e87145af6cb
twigz826 <thomastwigden@gmail.com>
2021-02-24 13:13:43 -0300

    Working on the registration feature test

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,20 @@
+class RegistrationsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to home, notice: "Welcome #{@user.first_name}!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index', as: 'home'
+  root to: 'users#index'
+
+  get 'sign_up', to: 'registrations#new'
+
+  post 'sign_up', to: 'registrations#create'
 
   resources :posts
 end
