fd7f3afcd650ad0696cac802dd6a409f09df1e9a
bullhornfixie <dawson.jon@hotmail.co.com>
2021-02-24 15:47:12 +0000

    added new coulmns to devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_114628) do
+ActiveRecord::Schema.define(version: 2021_02_24_151549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,17 +21,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_114628) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "scratches", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
-    t.string "username", default: "", null: false
-    t.string "firstname", default: "", null: false
-    t.string "lastname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -39,16 +29,11 @@ ActiveRecord::Schema.define(version: 2021_02_24_114628) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
-  end
-
-  create_table "usertwos", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
