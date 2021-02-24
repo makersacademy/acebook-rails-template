8615d0a13db220b6dbb2cad2489fd62de30d5332
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-02-24 11:45:11 +0000

    devise installed views/models not user yet

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
+
+  resources :tests
+
+  root to: "posts#index"
 end
