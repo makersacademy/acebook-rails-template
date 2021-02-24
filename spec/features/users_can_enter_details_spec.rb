fa5a9d7a52ba5bde54d7f945ff7fdcfc5ca41537
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2021-02-24 15:20:33 +0000

    Half way through second user details feature test

--- spec/features/users_can_enter_details_spec.rb
+++ spec/features/users_can_enter_details_spec.rb
@@ -7,4 +7,17 @@ feature 'users can visit sign up page' do
     click_button 'Sign up'
     expect(page).to have_content('Welcome! You have signed up successfully.')
   end
-end 
+
+
+  scenario 'and enter emails incorrectly ' do
+    Capybara.current_driver = :selenium
+    visit '/users/sign_up'
+    fill_in 'Email', with: 'test'
+    fill_in 'Password', with: 'testpass'
+    fill_in 'Password confirmation', with: 'testpass'
+    click_button 'Sign up'
+    expect(alert).to have_content("Please include an '@' in the email address")
+  end
+
+  Capybara.use_default_driver
+end
