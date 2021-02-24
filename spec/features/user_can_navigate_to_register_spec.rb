1b06e11196ea5c88854f71863bbad44530cbc77a
Glykeria Stravodimou <strglykeria@gmail.com>
2021-02-24 14:50:02 +0000

    linked register and sign in buttons to routes

---
+++ spec/features/user_can_navigate_to_register_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "Registration", type: :feature do
+  scenario "You can press the button and go register" do
+    visit("/")
+    click_link("Register")
+    expect(page).to have_content("Sign up")
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password")
+  end
+end
