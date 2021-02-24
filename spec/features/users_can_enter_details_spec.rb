7333f4cfeb7e5f258d45265ca9db45625e5c2e79
Hugh Cavanagh <hacavanagh@gmail.com>
2021-02-24 15:51:52 +0000

    Testing for password too short

--- spec/features/users_can_enter_details_spec.rb
+++ spec/features/users_can_enter_details_spec.rb
@@ -1,5 +1,5 @@
-feature 'users can visit sign up page' do
-  scenario 'and enter emails and passwords' do
+feature 'sign up page' do
+  scenario 'user can enter email and password to sign up' do
     visit '/users/sign_up'
     fill_in 'Email', with: 'test@email.com'
     fill_in 'Password', with: 'testpass'
@@ -9,7 +9,7 @@ feature 'users can visit sign up page' do
   end
 
 
-  scenario 'and enter emails incorrectly ' do
+  scenario 'cannot sign up with invalid email' do
     Capybara.current_driver = :selenium
     visit '/users/sign_up'
     fill_in 'Email', with: 'test'
@@ -20,4 +20,15 @@ feature 'users can visit sign up page' do
   end
 
   Capybara.use_default_driver
+
+  context 'when password is too short' do
+    scenario 'user cannot sign up and is warned' do
+      visit '/users/sign_up'
+      fill_in 'Email', with: 'test@email.com'
+      fill_in 'Password', with: 'short'
+      fill_in 'Password confirmation', with: 'short'
+      click_button 'Sign up'
+      expect(page).to have_content('Password is too short')
+    end
+  end
 end
