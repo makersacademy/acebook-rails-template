d38d54a94d5c3368b3cb52c80a3806795363401b
Katy Day <day.katy@gmail.com>
2021-02-25 15:25:08 +0000

    adding bits that don't really make a difference

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,13 +18,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-def update
-        p params 
-      
-        post = Post.find(params[:id])
-        post.update(post_params)
-        redirect_to(post_path(post))
-    end
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to(post_path(post))
+  end
 
     def show
       redirect_to posts_url
