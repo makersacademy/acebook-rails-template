09097e75ad0f52f4b39d5d41090eade443b977e3
Katy Day <day.katy@gmail.com>
2021-02-25 15:18:36 +0000

    user can now edit post; added feature test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,22 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+def update
+        p params 
+      
+        post = Post.find(params[:id])
+        post.update(post_params)
+        redirect_to(post_path(post))
+    end
+
+    def show
+      redirect_to posts_url
+    end
+
   private
 
   def post_params
