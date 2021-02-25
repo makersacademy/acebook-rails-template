000de3710c96ccacca7d1c49e4474135767898b8
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-02-25 16:23:21 +0000

    new post hidden for non signed in people

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.feature 'Timeline', type: :feature do
   scenario 'Can submit posts and view them' do
-    visit '/posts'
+    register
     click_link 'New post'
     fill_in 'Message', with: 'Hello, world!'
     click_button 'Submit'
