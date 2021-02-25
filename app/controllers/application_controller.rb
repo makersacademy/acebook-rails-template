b1325246f2b5e6663ee216e63da8cba717de8997
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-02-25 12:02:20 +0000

    User sign in & sign up capabilities now functional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :require_login, except:[new_user_registration]
-
-  private
-
-  def require_login
-    unless user_signed_in?
-      redirect_to root_path
-    end
-  end
+  before_action :authenticate_user!, except: [:home]
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
-  skip_before_action :require_login
   def index
     if user_signed_in?
       redirect_to posts_path
