4dbccd14acb4e0e96b3285dc3c3cf03e0c689a00
Reeshul <reeshul@hotmail.com>
2021-02-26 16:40:18 +0000

    added some more feature tests

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -1,13 +1,37 @@
 RSpec.feature 'Registration', type: :feature do
   scenario 'Users can sign up on the sign up page' do
-    sign_in
+    sign_up
     expect(page).to have_content('Welcome, Joe!')
   end
 
   scenario "Users can't sign up if email is already in database" do 
-    sign_in
-    click_button 'Logout'
-    sign_in
+    sign_up
+    sign_out
+    sign_up
     expect(page).to have_content("Email joebloggs@test.com already exists. Please enter a new email address")
   end
+
+  scenario 'Users can log in and see confirmation' do
+    sign_up
+    sign_out
+    sign_in
+    expect(page).to have_content("Logged in")
+  end
+
+  scenario 'Users can log out and see confirmation' do
+    sign_up
+    sign_out
+    expect(page).to have_content("Logged out")
+  end
+
+  scenario "User can not sign in with invalid password or email" do
+    sign_up
+    sign_out
+    visit 'sign_in'
+    fill_in 'email', with: 'joebloggs@test.com'
+    fill_in 'password', with: 'passwordz'
+    click_button 'Sign In'
+    expect(page).to have_content("Invalid details")
+  end
+
 end
--- spec/helpers/feature_helpers_spec.rb
+++ spec/helpers/feature_helpers_spec.rb
@@ -1,4 +1,4 @@
-def sign_in
+def sign_up
   visit '/sign_up'
   fill_in 'user[first_name]', with: 'Joe'
   fill_in 'user[last_name]', with: 'Bloggs'
@@ -7,3 +7,14 @@ def sign_in
   fill_in 'user[password_confirmation]', with: 'password'
   click_button 'Sign up'
 end 
+
+def sign_out
+  click_button 'Logout'
+end
+
+def sign_in
+  visit 'sign_in'
+  fill_in 'email', with: 'joebloggs@test.com'
+  fill_in 'password', with: 'password'
+  click_button 'Sign In'
+end
