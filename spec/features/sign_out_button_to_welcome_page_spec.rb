b81ad49433941b05b79d5f538062d259f48c294a
Glykeria Stravodimou <strglykeria@gmail.com>
2021-02-26 15:14:37 +0000

    sign out feature test

--- spec/features/sign_out_button_to_welcome_page_spec.rb
+++ spec/features/sign_out_button_to_welcome_page_spec.rb
@@ -3,6 +3,9 @@ include WelcomeHelper
 
 RSpec.feature "Sign out", type: :feature do
   scenario 'Redirect to welcome page after signing out' do
-
+    sign_up
+    click_link('Sign out')
+    expect(current_path).to eq('/')
+    expect(page).to have_content('Signed out successfully.')
   end
 end
