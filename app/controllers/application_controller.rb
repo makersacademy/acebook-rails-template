14d7170a44d4c97ccad3560908488be1333c77a3
CodeRed30 <katrinaharradine@gmail.com>
2021-02-26 10:52:33 +0000

    Redirect home when not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!, except: [:home]
+
+  private
+
+    def authenticate_user!
+      if user_signed_in?
+        super
+      else
+        redirect_to root_path, notice: "Please Login to view that page!" if request.original_fullpath != root_path
+      end
+    end
 end
