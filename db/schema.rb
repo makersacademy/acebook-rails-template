bf5ec20ef50338ab119f8702f1a971103303a38d
PetarSimonovic <petar@simonovic.com>
2021-03-01 12:04:27 +0000

    posts restored

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20210223153630) do
+=======
+ActiveRecord::Schema.define(version: 20210225152333) do
+>>>>>>> parent of 42a52ea... Merge pull request #4 from PetarSimonovic/development2
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
