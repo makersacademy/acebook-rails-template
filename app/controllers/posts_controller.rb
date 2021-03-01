6b34f4fad8dd9adcd01ad903a3aadaca0cfbcffe
RTurney <r.turney93@gmail.com>
2021-03-01 15:55:23 +0000

    Add option for users to delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,6 +39,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+
+      redirect_to '/posts'
+  end
 
   private
 
