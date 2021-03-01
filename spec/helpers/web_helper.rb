ae6d27a68e171c06fc586ec2456873171f4223ed
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2021-03-01 17:16:51 +0000

    Added web helper methods and changed layout of alerts

--- spec/helpers/web_helper.rb
+++ spec/helpers/web_helper.rb
@@ -5,3 +5,18 @@ def create_a_new_post_and_see_it_on_the_feed(text)
   click_button 'Submit'
   expect(page).to have_content(text)
 end
+
+def sign_up(email:, password:)
+  visit '/users/sign_up'
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  fill_in 'Password confirmation', with: password
+  click_button 'Sign up'
+end
+
+def sign_in(email:, password:)
+  visit '/users/sign_in'
+  fill_in 'Email', with: email
+  fill_in 'Password', with: password
+  click_button 'Log in'
+end
