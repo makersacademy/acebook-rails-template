f4b247deb9d6dd33fdc33275833a4db163b50f5d
Karsten Finlay <karstenfinlay@hotmail.com>
2021-03-01 12:43:46 +0000

    User can't edit other people's posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
   def new
-    @post = Post.new
+    # @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
     @post = Post.create(post_params)
+    # @post = current_user.posts.build(post_params)
     redirect_to posts_url
   end
 
@@ -28,6 +37,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_url
+  end
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to posts_path, notice: "Not authorised!" if @post.nil? 
   end
 
   private
