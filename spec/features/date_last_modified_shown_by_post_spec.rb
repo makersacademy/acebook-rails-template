9a4e706cd0a8d3b7aafbaf57eb8c904169857324
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2021-03-01 16:58:17 +0000

    Altered time tests

--- spec/features/date_last_modified_shown_by_post_spec.rb
+++ spec/features/date_last_modified_shown_by_post_spec.rb
@@ -10,6 +10,6 @@ RSpec.feature 'Timeline', type: :feature do
     fill_in 'Password confirmation', with: 'testpass'
     click_button 'Sign up'
     create_a_new_post_and_see_it_on_the_feed("Hello, world!")
-    expect(Time.zone.now - first('.date').text.to_datetime).to be < 1
+    expect(Time.zone.now - first('.date').text.to_datetime).to be < 60
   end
 end
