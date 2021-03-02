0b4da7754f7cae95fb8ee76593deae2cfd9e7189
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-02 12:34:43 +0000

    trying to add postee id to database

--- app/controllers/profile_posts_controller.rb
+++ app/controllers/profile_posts_controller.rb
@@ -1,5 +1,27 @@
 class ProfilePostsController < ApplicationController
-  def new 
-    @profile_posts = current_user.posts.build
-  end 
+  def new
+    @user = User.find(params[:postee_id])
+    @profile_posts = current_user.profile_posts.build
+  end
+
+  def create
+    @profile_post = current_user.profile_posts.build(post_params)
+    p params
+    respond_to do |format|
+      if @profile_post.save
+        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @profile_post }
+      else
+        format.html { render :new }
+        format.json { render json: @profile_post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:profile_post).permit(:message, :user_id, :postee_id)
+  end
+
 end
--- app/models/profile_post.rb
+++ app/models/profile_post.rb
@@ -1,3 +1,3 @@
 class ProfilePost < ApplicationRecord
-  
+  belongs_to :user
 end
