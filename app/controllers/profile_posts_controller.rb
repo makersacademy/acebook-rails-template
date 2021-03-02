f83afd2e2a18bac436deea0c86e7c681f2c16413
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-02 14:36:53 +0000

    now adding postee_id to db needs rerouting

--- app/controllers/profile_posts_controller.rb
+++ app/controllers/profile_posts_controller.rb
@@ -1,11 +1,11 @@
 class ProfilePostsController < ApplicationController
   def new
     @user = User.find(params[:postee_id])
-    @profile_posts = current_user.profile_posts.build
+    @profile_posts = current_user.profile_posts.create
   end
 
   def create
-    @profile_post = current_user.profile_posts.build(post_params)
+    @profile_post = current_user.profile_posts.build(params[:message, :user_id, :postee_id])
     p params
     respond_to do |format|
       if @profile_post.save
@@ -16,6 +16,12 @@ class ProfilePostsController < ApplicationController
         format.json { render json: @profile_post.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def update
+    profile_post = ProfilePost.find(params[:id])
+    profile_post.update(post_params)
+    redirect_to(root_path)
   end
 
   private
