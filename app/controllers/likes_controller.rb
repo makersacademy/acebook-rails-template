414e33725c56b606cd2c44f0f5867c0fb52a7974
Salar Ghotaslo <Salar_@live.co.uk>
2021-03-02 12:46:58 +0000

    only allow one like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,23 @@ class LikesController < ApplicationController
 
 
     def create
-        @post.likes.create(user_id: current_user.id)
-        redirect_to posts_path
+        if already_liked?
+            redirect_to posts_path, notice: "Yo, stop liking this"
+        else
+            @post.likes.create(user_id: current_user.id) 
+            redirect_to posts_path
+        end
     end
 
     private
 
     def find_post
         @post = Post.find(params[:post_id])
+    end
+    
+    def already_liked?
+        Like.where(user_id: current_user.id, post_id:
+        params[:post_id]).exists?
     end
 
 end
