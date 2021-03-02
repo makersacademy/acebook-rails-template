da93d974f87158b62da5455893494d91ba6c7a1f
Sean Edwards <seantrevoredwards@gmail.com>
2021-03-02 16:22:41 +0000

    rspec tests written for edit friend posts on users wall ready for code

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -24,5 +24,29 @@ RSpec.feature 'User Wall', type: :feature do
     click_button 'Post'
     expect(page).to have_content('Hello Katy')
     expect(page).to have_link('John Smith')
-  end 
-end 
+  end
+
+  scenario 'Signed in user can not post on thier own friends section' do
+    register
+    click_link 'Profile'
+    expect(page).not_to have_link('Post on wall')
+    expect(page).to have_link('New post')
+  end
+
+  xscenario 'Signed in user can only edit friends post they have created' do
+    register
+    submit_post
+    click_link 'Sign out'
+    register_second_user
+    click_link 'Katy Day'
+    click_link 'Post on wall'
+    fill_in 'Message', with: 'Hello Katy'
+    click_button 'Post'
+    click_link("Edit this post")
+    fill_in 'Message', with: 'This is an updated post, but I am still awesome'
+    click_button("Submit")
+    expect(page).to have_content('This is an updated post, but I am still awesome')
+    expect(page).not_to have_content('Hello Katy')
+    expect(page).to have_link('John Smith')
+  end
+end
