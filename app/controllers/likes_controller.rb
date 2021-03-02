55656c953848729ef6438d49671c412be6628617
bullhornfixie <dawson.jon@hotmail.co.com>
2021-03-02 15:09:22 +0000

    like and unlike feature implemented

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,37 @@
 class LikesController < ApplicationController
   before_action :find_post
-
+  before_action :find_like, only: [:destroy]
 
   def create
-  p @post
-  @post.likes.create(user_id: current_user.id)
-  redirect_to post_path(@post)
+    if already_liked?
+      flash[:notice] = "You can't like more than once"
+    else
+      @post.likes.create(user_id: current_user.id)
+    end 
+    redirect_to post_path(@post)
   end
 
   def find_post
-     @post = Post.find(params[:post_id])
- end
+    @post = Post.find(params[:post_id])
+  end
+
+   def destroy 
+    if !(already_liked?)
+      flash[:notice] = "Cannot unlike"
+    else
+      @like.destroy
+    end
+    redirect_to post_path(@post)
+   end
+
+   def find_like 
+    @like = @post.likes.find(params[:id])
+   end
+
+  private 
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id: params[:post_id]).exists?
+  end
 
 end
