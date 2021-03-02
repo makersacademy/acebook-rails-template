f8003ddff74d9dfa338eb8cad0b4fafef51b934c
Dan T <dp.tyler@hotmail.co.uk>
2021-03-02 10:57:22 +0000

    Users can submit posts test working

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
 RSpec.describe 'Timeline', type: :feature do
   it 'Can submit posts and view them' do
+    sign_up
     visit '/posts'
     click_link 'New post'
     fill_in 'Message', with: 'Hello, world!'
