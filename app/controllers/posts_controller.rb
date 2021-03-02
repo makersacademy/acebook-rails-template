9c8afa91c5de7a806a7def75d7eb8599588cff0c
Joshua Sinyor <>
2021-03-02 16:02:16 +0000

    Updated files. Reflects updated CSS for registrations page and controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-
   # Before action calls the login_required method from application controller,
   # to create a post you must be signed in
-  before_action :login_required, :only => :new
+  before_action :login_required, only: :new
 
   def new
     @post = Post.new
