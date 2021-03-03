909f8b022886c614a37992d4d41dcd3b01760953
Glykeria Stravodimou <strglykeria@gmail.com>
2021-03-03 10:37:12 +0000

    added username area to tests

--- spec/features/post_and_user_spec.rb
+++ spec/features/post_and_user_spec.rb
@@ -6,6 +6,6 @@ RSpec.feature 'A post has a creator' do
     click_link "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
-    expect(page).to have_content("created by: example@example.com")
+    expect(page).to have_content("created by: username")
    end
 end
