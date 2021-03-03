c96f18d21e24000a9dcda834ec687f5b2e8e9e31
bullhornfixie <dawson.jon@hotmail.co.com>
2021-03-03 12:15:23 +0000

    FriendsController setup

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,9 +1,18 @@
 class FriendsController < ApplicationController
 
   def index
+    # requested_friends
+    # pending friends 
+    # friends
   end
 
   def create 
+    current_user.friend_request(User.find(params[:id]))
+    redirect_to user_path(params[:id])
+  end
+
+  def requests
+    @friend_requests = current_user.requested_friends
   end
 
   def destroy 
