2fc32578c69c6f7b9bf796d4d46977b1bdcbeda4
HolsDuckett <hf.duckett@gmail.com>
2021-03-03 14:43:01 +0000

    custom 404 user not found page added to views folder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,15 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
   def show
-    @user = User.find(params[:id])
-    @posts = @user.posts.order("created_at DESC")
-    # @profile_posts = ProfilePost.find.order("created_at DESC")
-    @profile_posts = ProfilePost.where(user_profile_id: params[:id]).order("created_at DESC")
-    p @profile_posts
+    begin User.find(params[:id]).present?
+      @user = User.find(params[:id])
+      @posts = @user.posts.order("created_at DESC")
+      # @profile_posts = ProfilePost.find.order("created_at DESC")
+      @profile_posts = ProfilePost.where(user_profile_id: params[:id]).order("created_at DESC")
+
+  #    p @profile_posts
+    rescue
+      render file: "#{Rails.root}/app/views/errors/404", status: :not_found
+    end
   end
-
-
-
 end
