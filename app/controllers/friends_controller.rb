c928dd41b3c66bf3636cdbdb2ced0d29b21ac7ba
Katy Day <day.katy@gmail.com>
2021-03-03 15:05:11 +0000

    added friend request paths and views

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,11 +1,5 @@
 class FriendsController < ApplicationController
 
-  def index
-    # requested_friends
-    # pending friends 
-    # friends
-  end
-
   def create 
     current_user.friend_request(User.find(params[:id]))
     redirect_to user_path(params[:id])
@@ -15,7 +9,18 @@ class FriendsController < ApplicationController
     @friend_requests = current_user.requested_friends
   end
 
-  def destroy 
+  def decline
+    current_user.decline_request(User.find(params[:id]))
+    redirect_to friend_requests_path   
+  end
+
+  def accept 
+    current_user.accept_request(User.find(params[:id]))
+    redirect_to friend_requests_path
+  end
+
+  def list
+    @friends_list = User.find(params[:id]).friends
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,12 @@ end
   resources :profile_posts
 
   root to: 'posts#index'
+
+  get 'friends/requests' => 'friends#requests', :as => :friend_requests
+
+  get 'friends/accept/:id' => 'friends#accept', :as => :accept_request 
+
+  get 'friends/decline/:id'=> 'friends#decline', :as => :decline_request
+
+
 end
