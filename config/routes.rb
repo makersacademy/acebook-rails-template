ab35306e91265af788dbd64df93d684d65d0267a
Glykeria Stravodimou <strglykeria@gmail.com>
2021-03-03 14:42:29 +0000

    new rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => {:registrations => "registrations"}
+  root 'welcome#index'
+  resources :posts
 
-  
-   root 'welcome#index'
-
-
-   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   # root to: 'post#index'
-   # get 'new' => 'posts#new'
-   resources :posts
-   
 end
