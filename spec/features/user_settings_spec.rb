981410c57a688a5c0b29549b66157ef864fb715d
Karsten Finlay <karstenfinlay@hotmail.com>
2021-03-03 12:18:05 +0000

    Changed profile to settings

--- spec/features/user_settings_spec.rb
+++ spec/features/user_settings_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.feature 'User Profle', type: :feature do
-  scenario 'User can visit their own Profile page' do
+RSpec.feature 'User settings', type: :feature do
+  scenario 'User can visit the settings page' do
     sign_up(email: 'test@email.com', username: 'Troy', password: 'testpass')
-    click_link "Profile"
+    click_link "Settings"
     expect(page).to have_current_path('/users/edit')
   end
 
   scenario 'User can delete their own Profile' do
     sign_up(email: 'test@email.com', username: 'Troy', password: 'testpass')
     create_a_new_post_and_see_it_on_the_feed("Hello world!")
-    click_link "Profile"
+    click_link "Settings"
     click_button "Cancel my account"
     sign_up(email: 'test2@email.com', username: 'Troy', password: 'testpass')
     expect(page).not_to have_content("Hello world!")
