e2debd0ed8c6eafef1cfd546ce626c4be1463e4e
PetarSimonovic <petar@simonovic.com>
2021-03-03 11:09:08 +0000

    sign up from homepage test fixed

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.feature 'Sign up', type: :feature do
   scenario 'Can sign up from homepage' do
     visit '/'
-    # click_button 'Sign up'
+    click_button 'Sign up'
     expect(current_path).to eq('/users/sign_up')
     fill_in 'user_email', with: 'anna.cavalla@gmail.com'
     fill_in 'user_password', with: 'password'
     fill_in 'user_password_confirmation', with: 'password'
     click_button 'Sign up'
     expect(page).to have_content('Welcome! You have signed up successfully')
-    expect(current_path).to eq('/')
+    expect(current_path).to eq('/posts')
   end
 
   xscenario 'Gets redirected to / when not signed in and viewing /posts' do
