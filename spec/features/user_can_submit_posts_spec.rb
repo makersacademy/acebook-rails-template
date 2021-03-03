0705c90291eeebac08e1e11ec6ea738bc6fe72bb
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-03-03 12:07:39 +0000

    testing for deleting posts

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -6,8 +6,26 @@ RSpec.feature 'Timeline', type: :feature do
   scenario 'Can submit posts and view them' do
     sign_up
     click_link 'New post'
-    fill_in 'Message', with: 'Hello, world!'
+    fill_in 'Message', with: DEFAULT_POST
     click_button 'Submit'
-    expect(page).to have_content('Hello, world!')
+    expect(page).to have_content(DEFAULT_POST)
   end
+
+  scenario 'Can delete own post' do
+    sign_up
+    create_post
+    click_on 'Move to recycle bin'
+    expect(page).not_to have_content(DEFAULT_POST)
+  end
+
+  scenario "Don't see delete button on others posts" do
+    sign_up
+    create_post
+    click_on 'Sign out'
+    sign_up(SECONDARY_EMAIL, SECONDARY_PASSWORD)
+    expect(page).not_to have_content('Move to recycle bin')
+  end
+
 end
+
+
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,17 +1,28 @@
 DEFAULT_EMAIL = "test@test.com"
 DEFAULT_PASSWORD = "password"
+DEFAULT_POST = "Hello, world!"
+SECONDARY_EMAIL = "test2@test.com"
+SECONDARY_PASSWORD = "password2"
+SECONDARY_POST = "Goodbye, cruel world!"
 
-def sign_up
+def sign_up(email = DEFAULT_EMAIL, password = DEFAULT_PASSWORD)
   visit '/'
   click_button 'Sign up'
-  fill_in 'user_email', with: DEFAULT_EMAIL
-  fill_in 'user_password', with: DEFAULT_PASSWORD
-  fill_in 'user_password_confirmation', with: DEFAULT_PASSWORD
+  fill_in 'user_email', with: email
+  fill_in 'user_password', with: password
+  fill_in 'user_password_confirmation', with: password
   click_button 'Sign up'
 end
 
-def sign_in
-  fill_in 'inputEmail', with: DEFAULT_EMAIL
-  fill_in 'inputPassword', with: DEFAULT_PASSWORD
+def sign_in(email = DEFAULT_EMAIL, password = DEFAULT_PASSWORD)
+  fill_in 'inputEmail', with: email
+  fill_in 'inputPassword', with: password
   click_button "Log in"
+end
+
+def create_post(post = DEFAULT_POST)
+  click_link 'New post'
+  fill_in 'Message', with: post
+  click_button 'Submit'
+  expect(page).to have_content(post)
 end
