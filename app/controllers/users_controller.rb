b62818e55748bb85c8ee304907dd643bd8c7b511
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-03-04 10:40:09 +0000

    fixed showing users wall posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,8 @@ class UsersController < ApplicationController
       @user = User.friendly.find(params[:id])
       @posts = @user.posts.order("created_at DESC")
       # @profile_posts = ProfilePost.find.order("created_at DESC")
-      @profile_posts = ProfilePost.where(user_profile_id: params[:id]).order("created_at DESC")
+      @profile_posts = ProfilePost.where(user_profile_id: @user.id).order("created_at DESC")
 
-  #    p @profile_posts
     rescue
       render file: "#{Rails.root}/app/views/errors/404", status: :not_found
     end
