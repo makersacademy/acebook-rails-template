20c81efff9aa8f9e3e0b701d85f55992b44b1fb2
Katy Day <day.katy@gmail.com>
2021-03-04 14:55:14 +0000

    css mucking about

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_03_03_153146) do
 
   # These are extensions that must be enabled in order to support this database
@@ -25,6 +24,17 @@ ActiveRecord::Schema.define(version: 2021_03_03_153146) do
     t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
     t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
     t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
+  create_table "friendships", id: :serial, force: :cascade do |t|
+    t.string "friendable_type"
+    t.integer "friendable_id"
+    t.integer "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer "blocker_id"
+    t.integer "status"
+    t.index ["friendable_id", "friend_id"], name: "index_friendships_on_friendable_id_and_friend_id", unique: true
   end
 
   create_table "likes", force: :cascade do |t|
--- spec/features/user_can_delete_post_spec.rb
+++ spec/features/user_can_delete_post_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'Delete posts', type: :feature do
   scenario 'A user can delete their own post' do
     register
     submit_post
-    click_link 'Delete post'
+    click_link 'Delete'
     expect(page).to_not have_content('Hello, world!')
   end
 end
