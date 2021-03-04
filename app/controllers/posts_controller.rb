3474b242272ac1713caeca3996d6ee099bf93671
Mornevanzyl <catbeatsadrum@gmail.com>
2021-03-04 11:51:02 +0000

    Udpated update and delete methods for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,9 @@
 class PostsController < ApplicationController
   # Before action calls the login_required method from application controller,
   # to create a post you must be signed in
-  before_action :login_required, only: :new
+  before_action :login_required, only: [:new, :destroy]
+  # before_action :require_user_logged_in!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def new
     @post = Post.new
@@ -14,9 +16,19 @@ class PostsController < ApplicationController
     @post.save
     if @post.save
       redirect_to root_path, notice: 'Post successfully created'
+      return
     else
       flash[:alert] = "Post message is blank, try again."
       render :new
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+        redirect_to root_path, notice: 'Post successfully updated'
+        return
+    else
+        render :edit
     end
   end
 
@@ -24,9 +36,22 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def destroy
+    @post.destroy
+    redirect_to root_path, notice: 'Post has been deleted'
+  end
+
+  def edit
+
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:message)
+  end
+
+  def set_post
+    @post = Current.user.posts.find(params[:id])
   end
 end
