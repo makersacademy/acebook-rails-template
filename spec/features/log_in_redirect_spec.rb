82f47377224d9153f8923efc9085bea5bf6268e0
Harry Thomson <haroldt95@hotmail.com>
2021-03-11 10:50:07 +0000

    Added tests, added default route after sign in

---
+++ spec/features/log_in_redirect_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.feature "Log in", type: :feature do
+  scenario 'redirects a user to /posts after logging in' do
+    sign_up
+    expect(page).to have_content("New post")
+  end
+end
