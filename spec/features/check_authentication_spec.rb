34c66db0ac77abdc53e8d104b2decc1344b2ff6b
mitchell9-1 <mitchell.perry001@gmail.com>
2021-03-16 09:57:31 +0000

    Updated styling

--- spec/features/check_authentication_spec.rb
+++ spec/features/check_authentication_spec.rb
@@ -5,6 +5,6 @@ require 'rails_helper'
 RSpec.feature 'Posts', type: :feature do
   scenario 'user needs to log in to view posts' do
     visit '/posts'
-    expect(page).to have_content('Log in')
+    expect(page).to have_content('Login')
   end
 end
