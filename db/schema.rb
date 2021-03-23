005fae2301cf8a832bd43c1ec4222c952bf0d1e5
Nicolas Bermejo <nicbermejo@gmail.com>
2021-03-23 17:15:16 +0100

    user class, authentication, edge cases

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_121418) do
+ActiveRecord::Schema.define(version: 2021_03_23_153454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2021_03_23_121418) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
