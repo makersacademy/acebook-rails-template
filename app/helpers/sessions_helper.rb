9f00e9ff45f1f6de42133f59a4ede7c55b539689
Joey <joey.s.hornsby@gmail.com>
2021-03-25 11:46:13 +0000

    added signout path

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,12 +5,21 @@ module SessionsHelper
         self.current_user = user
     end
 
+    def signed_in?
+        !!current_user
+    end
+
     def current_user=(user)
         @current_user = user
     end
 
     def current_user
         @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+    end
+
+    def sign_out
+        self.current_user = nil
+        cookies.delete(:remember_token)
     end
 
 end
