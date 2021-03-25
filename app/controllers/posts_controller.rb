1b4da7d01ff5402ce41870037e6cb275309b8414
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-03-25 15:07:15 +0000

    Add update method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to @post
+      redirect_to posts_url
     else 
       render :edit
     end 
