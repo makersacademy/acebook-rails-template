807c518b0a012794e53832da1bb087adb8be4dfd
Adam Jonas <adamjonas25@gmail.com>
2021-03-29 16:12:19 +0100

    Adds time and date to posts view

--- spec/features/post_shows_user_spec.rb
+++
@@ -1,9 +0,0 @@
-feature 'showing posts attributes' do
-  scenario 'post shows user it was created by' do
-   sign_up_as_testy
-   click_link 'New post'
-    fill_in 'Message', with: 'Hello, world!'
-    click_button 'Submit'
-    expect(page).to have_selector(".name", text: "Testy Testerson")
-  end
-end
