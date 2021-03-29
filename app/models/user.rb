6d2e4b37655125e42324433fcd9be1501e2ae21f
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-03-29 18:06:48 +0200

    test for user uploading profile photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  has_one_attached :avatar
 end
