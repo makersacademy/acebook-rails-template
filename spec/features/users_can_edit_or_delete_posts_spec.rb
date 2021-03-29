896f23e2b86f1ae04b6c139630ab74b646bd12a5
katiewhitlock <katiejwhitlock@gmail.com>
2021-03-29 15:04:43 +0100

    delete post functionality and test added

---
+++ spec/features/users_can_edit_or_delete_posts_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'delete_or_edit_posts' do
+  before(:each) do
+    @user = User.new(name: "Lizardo", email: "lizard@example.com", password: "123secure", password_confirmation: "123secure")
+    @user.save
+    sign_in_helper(@user)
+    click_link "New Post"
+    fill_in "Message", with: "Hello Lizard"
+    click_button "Submit"
+  end
+
+scenario 'delete posts' do
+  expect(page).to have_content("Hello Lizard")
+  click_link "Delete Post"
+  expect(page).not_to have_content("Hello Lizard")
+  expect(page).to have_link("New post")
+end
+end
