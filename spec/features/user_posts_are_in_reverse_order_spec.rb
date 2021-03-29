951f17dbc4fb0b4b0f35239242b09bf876615ea7
Christine Chee <christinechee88@hotmail.com>
2021-03-29 22:41:09 +0100

    Fix all feature tests that broke after styling added due to missing div tags in /signin

--- spec/features/user_posts_are_in_reverse_order_spec.rb
+++ spec/features/user_posts_are_in_reverse_order_spec.rb
@@ -10,10 +10,10 @@ RSpec.feature "Timeline", type: :feature do
   scenario 'create a post and it appears on the home page' do
     visit "posts#index"
     click_link "New post"
-    fill_in "Message", with: "Hello World"
+    fill_in "post[message]", with: "Hello World"
     click_button "Submit"
     click_link "New post"
-    fill_in "Message", with: "Wello Horld"
+    fill_in "post[message]", with: "Wello Horld"
     click_button "Submit"
     expect {"Wello Horld".appears_before("Hello World")}
   end
