b218505b974bd0f9c7ca87a7a5e3068f054a2c78
ianmcnicholas <ianjmcnicholas@.com>
2021-03-30 16:43:07 +0100

    Checking over end-of-day merge on Weds 30 Mar

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_131559) do
-
+ActiveRecord::Schema.define(version: 2021_03_30_132924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
