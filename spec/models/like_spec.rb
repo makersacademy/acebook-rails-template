d15452b5abe92d29333522cb23d18d85938a4c7e
katiewhitlock <katiejwhitlock@gmail.com>
2021-03-30 14:59:04 +0100

    Unit tests: two written and passing for likes

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = User.new(name: "Lizardo", email: "lizard@example.com", password: "123secure", password_confirmation: "123secure")
+    @user.save
+    @post = @user.posts.build(message: "Hello world")
+    @post.save
+    @like = @post.likes.build(user_id: @post.user_id)
+  end
+
+  subject { @like }
+
+it { is_expected.to be_valid }
+
+describe "when user_id is not present" do
+  before { @like.user_id = nil }
+  it { is_expected.not_to be_valid}
+end
 end
