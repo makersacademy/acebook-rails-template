2ba1f832b59f07606bced8b5368ff97d790327c5
Nicolas Bermejo <nicbermejo@gmail.com>
2021-03-31 11:27:42 +0200

    last minute changes css

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 2021_03_30_100806) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "likes", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.bigint "user_id", null: false
@@ -44,6 +54,8 @@ ActiveRecord::Schema.define(version: 2021_03_30_100806) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
