df3c4fbbabab7286b4223881702bf95c9bf4d149
Nicolas Bermejo <nicbermejo@gmail.com>
2021-03-31 12:45:09 +0200

    changed buttons

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
 
   def new
     not_signed_in_redirect
+    session[:return_to] ||= request.referer
     @post = Post.new
   end
 
@@ -11,7 +12,7 @@ class PostsController < ApplicationController
     not_signed_in_redirect
     @user = current_user
     @post = @user.posts.create(post_params)
-    redirect_to posts_url
+    redirect_to session.delete(:return_to)
   end
 
   def index
@@ -21,22 +22,25 @@ class PostsController < ApplicationController
   end
 
   def edit
+    not_signed_in_redirect
+    session[:return_to] ||= request.referer
     @post = Post.find(params[:id])
   end
 
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
-    redirect_to posts_url
+    redirect_to session.delete(:return_to)
   end
 
   def destroy
+    session[:return_to] ||= request.referer
     @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_url
+    redirect_to session.delete(:return_to)
   end
 
-  
+
   private
 
   def post_params
