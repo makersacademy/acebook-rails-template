1991beb8d0e2e96a9df2efba0ba63835f7db9dbe
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-06-16 18:26:00 +0100

    validate registration fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { maximum: 50 }
 
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+
+  validates :password, presence: true, length: { minimum: 8 }
+
 end
