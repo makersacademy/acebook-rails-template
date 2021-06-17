2e61e75dbe3bb9da83e7b8a2b58efdd1e9d990b9
Dewald Viljoen <79870549+Dev-ops-true@users.noreply.github.com>
2021-06-17 17:15:43 +0100

    users can login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to '/'
+    else 
+      
+      flash.now[:alert] = 'Invalid email/password combination' # Not quite right!
+      render 'new', notice: "hey"  
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to '/login'
+  end
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,11 @@
+module SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
