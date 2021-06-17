59f854494bb988b56fe89ab6415003d7a5a01672
BecaLParker <becaparker@gmail.com>
2021-06-17 14:55:02 +0100

    first validity test passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new }
+
+  it "is valid with valid attributes" do
+    subject.username = "Anything"
+    subject.email = "example@gmail.com"
+    subject.password = "password123"
+    expect(subject).to be_valid
+  end
+  it "is not valid without a username"
+
+  it "is not valid without a email"
+  it "is not valid without a password"
+  it "is not valid without a password confirmation"
 end
