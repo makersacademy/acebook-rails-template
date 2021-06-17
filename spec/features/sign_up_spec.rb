ea14b770e3ea428ad508f171de74843541f034e4
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-06-17 15:54:54 +0100

    add passwprd matching tests

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -13,5 +13,17 @@ RSpec.feature "Registration", type: :feature do
       expect(current_path).to eq('/login') #to be replaced with the path to the login page 
       expect(page).to have_content ("Log in") # to be replaced with the new log in page 
     end
+
+    scenario 'a user should enter matching passwords' do
+      visit "/users/new"
+      fill_in "Username", with: "test"
+      fill_in "Email", with: "test@example.com"
+      fill_in "Password", with: "password123"
+      fill_in "Password confirmation", with: "password12345"
+      click_button "Register"
+
+      expect(page).to have_content("Password confirmation doesn't match Password")
+    end
+
   end
 end
