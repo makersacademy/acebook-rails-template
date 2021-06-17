66ed1b4103c75efb44e2db46e91c86b6b51a773b
JP Ferreira <jpferreira@MA301.broadband>
2021-06-17 17:13:46 +0100

    Adding comment and deleting comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_185107) do
+ActiveRecord::Schema.define(version: 2021_06_17_125809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
@@ -31,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_06_16_185107) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
