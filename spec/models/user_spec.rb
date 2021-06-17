dbde64087ff34a444f8c2a86b3bafaea570b095a
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-06-17 16:40:32 +0100

    unit test email is the correct format

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,7 +31,14 @@ RSpec.describe User, type: :model do
   end
 
 
-  it "email should be correct format"
+  it "email should be correct format" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+    foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      subject.email = invalid_address
+      expect(subject).to_not be_valid
+    end
+  end 
 
   it "is not valid without a password" do 
     subject.password = nil
