c3293add63d0598704bd3f1d46c163a2c52a2df6
BecaLParker <becaparker@gmail.com>
2021-06-18 11:26:03 +0100

    updates testing to match add-post button which is new

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     visit "/posts"
-    click_link "New post"
+    click_button "add-post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
@@ -11,10 +11,10 @@ RSpec.feature "Timeline", type: :feature do
 
   scenario "Last post is on top" do
     visit "/posts"
-    click_link "New post"
+    click_button "add-post"
     fill_in "Message", with: "Message 1"
     click_button "Submit"
-    click_link "New post"
+    click_button "add-post"
     fill_in "Message", with: "Message 2"
     click_button "Submit"
     expect(page.find('p:nth-of-type(1)')).to have_content("Message 2")
