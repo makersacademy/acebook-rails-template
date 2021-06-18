14cffcc1d5401705d10e75a158849133eeee439e
BecaLParker <becaparker@gmail.com>
2021-06-18 11:18:06 +0100

    copied test from old branch

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -8,4 +8,15 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+
+  scenario "Last post is on top" do
+    visit "/posts"
+    click_link "New post"
+    fill_in "Message", with: "Message 1"
+    click_button "Submit"
+    click_link "New post"
+    fill_in "Message", with: "Message 2"
+    click_button "Submit"
+    expect(page.find('p:nth-of-type(1)')).to have_content("Message 2")
+  end
 end
