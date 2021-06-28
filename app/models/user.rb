4d0d8c4229499e137623422a052ae62e6b89f645
BecaLParker <becaparker@gmail.com>
2021-06-28 14:46:31 +0100

    allows every User object to have only one image attached to it

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+  
   validates :username, presence: true, length: { maximum: 50 }
 
   validates :email, presence: true, length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
