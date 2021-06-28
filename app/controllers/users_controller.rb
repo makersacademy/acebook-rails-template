3cbb6e04507aa7050a06c996130a483c64f354a4
BecaLParker <becaparker@gmail.com>
2021-06-28 15:15:34 +0100

    WIP profile images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
   private 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :avatar, :email, :password, :password_confirmation)
   end
 end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
+  has_one :user
   has_many :comments
 end
