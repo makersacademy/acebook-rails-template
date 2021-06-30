8b6f79ffb975aea69965c82803db8a7fce8aa619
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-06-30 16:09:57 +0100

    fix edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,12 @@ class UsersController < ApplicationController
     @users = User.all.order(updated_at: :desc)
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
   private
 
   def user_params
