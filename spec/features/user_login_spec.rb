aff36e3368649bf2717abe42c76dc748b9ac4717
BecaLParker <becaparker@gmail.com>
2021-06-30 16:17:45 +0100

    refactors by extracting to a web_helper

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -3,12 +3,7 @@ require 'rails_helper'
 RSpec.feature "Login", type: :feature do
   feature 'Login' do
     scenario 'a registered user can login' do
-      User.create(username: "login", email: "login@example.com", password: "1234", password_confirmation: "1234")
-      visit "/login"
-      fill_in "session[email]", with: "login@example.com"
-      fill_in "session[password]", with: "1234"
-      click_button "Log in"
-
+      successfullogin
       expect(current_path).to eq('/posts') 
     end
 
