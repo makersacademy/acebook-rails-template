d1b156c46fa2769b738066fd09e3094a80b2bb61
BecaLParker <becaparker@gmail.com>
2021-06-30 15:48:12 +0100

    Unregistered users fail login

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -11,5 +11,16 @@ RSpec.feature "Login", type: :feature do
 
       expect(current_path).to eq('/posts') 
     end
+
+    scenario 'an unregistered user cannot login' do
+      visit "/login"
+      fill_in "session[email]", with: "fail@example.com"
+      fill_in "session[password]", with: "FAIL"
+      click_button "Log in"
+
+      expect(current_path).to eq('/login') 
+      expect(current_path).not_to eq('/posts') 
+      expect(page).to have_content ("Invalid email/password combination.")
+    end
   end
 end
