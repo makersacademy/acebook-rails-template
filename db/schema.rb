88789ac6ca6621f026139250eea849c4eb1fc31a
JP Ferreira <jpferreira@MA301.broadband>
2021-06-30 14:15:59 +0100

    list of online users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_29_125009) do
+ActiveRecord::Schema.define(version: 2021_06_30_111229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2021_06_29_125009) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.datetime "last_seen_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
