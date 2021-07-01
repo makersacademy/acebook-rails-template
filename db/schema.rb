660f9762a71ad8263a312fa7514a5835e1c4e85d
JP Ferreira <jpferreira@MA301.broadband>
2021-07-01 12:22:51 +0100

    Styling fixes on posts and profile page

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_06_30_111229) do
-
+ActiveRecord::Schema.define(version: 2021_06_30_183226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,13 +68,13 @@ ActiveRecord::Schema.define(version: 2021_06_30_111229) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.datetime "last_seen_at"
     t.date "birthday"
     t.string "relationship_status"
     t.string "hobbies", default: "N/A"
     t.string "phone_number", default: "N/A"
     t.string "home_town"
     t.string "current_location"
-    t.datetime "last_seen_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
