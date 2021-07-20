bc72b989fbe4b228ef61a647d02f45c90a92ba27
Cynthia Fu <yinnycfu@gmail.com>
2021-07-20 15:55:33 +0100

    User model does not accept duplicated emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   validates :password, presence: true
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
