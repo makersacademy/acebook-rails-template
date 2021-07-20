8321cb2a8af76c9274a8eb8176de60bd95c92f2e
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-20 12:28:02 +0100

    adding databases and set up files for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :posts do
     get '/like', to: 'posts#like', on: :member
   end
+
+  resources :users do
+    # get '/new', to: 'users#new' 
+  end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_210240) do
+ActiveRecord::Schema.define(version: 2021_07_20_111958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2021_07_19_210240) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likes", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
