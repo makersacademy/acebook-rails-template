11fda3c05d0a5d9efa5bee3ae36ffa504485cff7
Cynthia Fu <yinnycfu@gmail.com>
2021-07-20 16:10:01 +0100

    Users table has an index on the email column which is unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+  before_save { self.email = email.downcase }
   validates :password, presence: true
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
