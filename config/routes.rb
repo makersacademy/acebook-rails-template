32617e41e7b3c7754467c12e130b32fd711630f7
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-20 12:28:02 +0100

    adding databases and set up files for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :posts do
     get '/like', to: 'posts#like', on: :member
   end
+
+  resources :users do
+    # get '/new', to: 'users#new' 
+  end 
 end
