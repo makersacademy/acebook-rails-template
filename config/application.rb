8a9ff38df909eec91525fc36789e414e8d91cc79
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-07-21 10:39:56 +0100

    Visualize logo on navbar and add favicon to images

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,12 @@ module Acebook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    Rails.application.config.assets.paths << Rails.root.join("app", "assets", "images")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+
+
