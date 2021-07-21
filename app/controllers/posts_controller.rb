45eed7ba23d8ebe5e06e63d35b141775890efc1b
Merryn <merrynhr@gmail.com>
2021-07-21 12:41:15 +0100

    added deletee

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,7 +30,15 @@ class PostsController < ApplicationController
       else
         render :edit
       end
-    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  
+  
   
   
 
