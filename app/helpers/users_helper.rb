da309adf0e458405bd8bac1570ddbbac221e6a3c
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-21 16:11:44 +0100

    adding template profile picture

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    p gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    p gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
