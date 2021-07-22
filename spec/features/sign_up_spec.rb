f3b1809eeafb9161ddf60c00cfdf1bf9d7ab07cd
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-07-22 15:12:19 +0200

    test for sign in

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,9 +1,14 @@
+require 'rails_helper'
+
 RSpec.feature "User authentication", type: :feature do
-  scenario "user can sign in" do
+  scenario "user can sign up" do
     visit "/"
-    click_link "signup"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
-    expect(page).to have_content("Hello, world!")
+    
+    fill_in "email_field", with: "test@test.com"
+    fill_in "password", with: "testing123"
+    fill_in "password_confirmation", with: "testing123"
+
+    click_button "Sign up"
+    expect(page).to have_content("Welcome, test@test.com")
   end
 end
