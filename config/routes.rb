d67415d533088decf03c3e59445b550a98d174ab
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-07-23 23:15:44 +0100

    debugging

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,7 @@ Rails.application.routes.draw do
   resources :posts do
     get '/like', to: 'posts#like', on: :member
   end
-<<<<<<< HEAD
 
   resources :users do
   end 
-=======
-  
->>>>>>> 54a2906066562116ed4928bb797435cd81a1cb0f
 end
