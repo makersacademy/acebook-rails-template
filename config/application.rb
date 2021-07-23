ee7b3f5d306091e4e94df1cb5fff78b8cc95236d
mikey-roberts <mikey_roberts@outlook.com>
2021-07-23 14:51:17 +0100

    rspec tests fully working

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative 'boot'
-require "active_storage/attached"
 require 'rails/all'
+require "active_storage/attached"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,7 +29,6 @@ RSpec.configure do |config|
   # Devise helper methods for controller specs
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
-  config.include ControllerHelpers, type: :controller
   config.include Warden::Test::Helpers
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
