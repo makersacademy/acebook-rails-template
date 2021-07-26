f449cc4ef9b85e142f33df8b0c272afbd3fd4be6
Masako Gray <masakogray@hotmail.co.uk>
2021-07-26 12:26:05 +0100

    config.active storage

--- db/schema.rb
+++ db/schema.rb
@@ -43,5 +43,14 @@ ActiveRecord::Schema.define(version: 2021_07_22_112803) do
     t.integer "likes", default: 0
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
