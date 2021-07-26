853913d665edcac5a65925a7c1236d9abd313313
mikey-roberts <mikey_roberts@outlook.com>
2021-07-26 17:09:01 +0100

    sign in to view this page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,12 @@
 class ProfileController < ApplicationController
   def index
-     
+     @current_user = current_user
+  end
+
+  
+
+  def post_params
+    params.inspect
+    params.require(:post).permit(:message, :image)
   end
 end
