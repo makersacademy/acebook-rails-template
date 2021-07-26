1eb6e1f84c7d366cfd2f8fdfcdff58da25dd6859
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-07-26 11:58:43 +0100

    More refactoring of helper methods

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-require_relative 'users_helper.rb'
+require_relative 'web_helpers/users_helper.rb'
 
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
