3ca3bca1ba39ad7dc6802b2f626f91e0591236d1
mikey-roberts <mikey_roberts@outlook.com>
2021-07-26 20:23:29 +0100

    update image if avatar attached, otherwise display default

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :image
+  has_one_attached :avatar
 
+  def avatar_thumbnail
+    avatar.variant(resize: "150x150!").processed
+  end
+
+  def avatar_profile
+    avatar.variant(resize: "400x400!").processed
+  end
 end
