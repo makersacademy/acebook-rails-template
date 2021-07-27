7e421ad41676a749349fdb9276f0e57a3967d30c
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-27 15:49:36 +0100

    linting

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
-	has_one_attached :image
-	scope :order_by_created_at, -> { order("created_at DESC") }
+	 has_one_attached :image
+	 scope :order_by_created_at, -> { order("created_at DESC") }
 
-	def increment_likes(by_value)
-		self.likes += by_value
-	end
+	 def increment_likes(by_value)
+ 		 self.likes += by_value
+ 	end
 	
 end
