6ca2b6f4db857264d47fa7a1277829838bc6dd2a
JooMin Choi <joominchoi23@gmail.com>
2021-07-27 15:14:54 +0100

    made minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(params.require(:user).permit(:username,:password))
+    # keep code in to log user in after sign up
     # session[:user_id] = @user.id
-    redirect_to '/welcome'
+    redirect_to '/posts'
   end
 end
