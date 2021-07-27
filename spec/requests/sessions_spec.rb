98b405f968f4087aceb6e377630a35b1a7d73549
Cynthia Fu <yinnycfu@gmail.com>
2021-07-27 15:04:42 +0100

    feat: sessions controller

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe "Sessions", type: :request do
   describe "GET /new" do
     it "returns http success" do
+<<<<<<< HEAD
       get "/login"
+=======
+      get "/sessions/new"
+>>>>>>> feat: sessions controller
       expect(response).to have_http_status(:success)
     end
   end
