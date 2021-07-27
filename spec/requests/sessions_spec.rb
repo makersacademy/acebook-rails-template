89aa283538471e99605574290074249c6259fa87
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-27 16:55:42 +0100

    sign in css

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -4,10 +4,14 @@ RSpec.describe "Sessions", type: :request do
   describe "GET /new" do
     it "returns http success" do
 <<<<<<< HEAD
+<<<<<<< HEAD
       get "/login"
 =======
       get "/sessions/new"
 >>>>>>> feat: sessions controller
+=======
+      get "/sessions/new"
+>>>>>>> sign in css
       expect(response).to have_http_status(:success)
     end
   end
