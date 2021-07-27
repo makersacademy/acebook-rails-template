4b7341c7a51e32e9c603fbb3be7561f8816a11a9
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-07-27 15:42:39 +0100

    Added nav bar across all view files, feature tests still passing

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -4,7 +4,7 @@ require_relative 'web_helpers/users_helper.rb'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     signup_login
-    click_link "New post"
+    click_button "New post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature "Login", type: :feature do
   scenario "User can login" do
     signup_login
-    expect(page).to have_content("Welcome You are Logged In, jooomin")
+    expect(page).to have_content("Welcome, jooomin")
   end
 end
