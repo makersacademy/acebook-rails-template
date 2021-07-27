b8df8bc642175544d6e02a21414df066917b33d7
Craig Eaton <craigea92@gmail.com>
2021-07-27 15:57:18 +0100

    Can Now Delete Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,13 @@ class CommentsController < ApplicationController
     redirect_to post_path(@posts)
   end
 
+  def destroy
+    @posts = Post.find(params[:post_id])
+    @comment = @posts.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@posts)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:body)
