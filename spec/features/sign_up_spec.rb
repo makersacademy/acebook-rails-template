ab12c24a0fabe49489acee512cf4eb0e927ed124
Fabio <alessf13@gmail.com>
2021-07-27 18:56:37 +0300

    feat: shoulda gem

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -5,57 +5,57 @@ RSpec.feature "User Sign Up", type: :feature do
   # Capybara can't pick up CSS so we can't check for unhappy paths
   scenario 'name is not submitted' do
 
-    visit '/users/new'
+    visit '/signup'
     fill_in 'user_name', with: ' '
     fill_in 'user_email', with: 'test@test.com'
     fill_in 'user_password', with: 'password123'
     fill_in 'user_password_confirmation', with: 'password123'
 
-    expect(page).to have_current_path('/users/new')
+    expect(page).to have_current_path('/signup')
   end
 
   scenario 'user email is not submitted' do
 
-    visit '/users/new'
+    visit '/signup'
     fill_in 'user_name', with: 'TestName'
     fill_in 'user_email', with: ' '
     fill_in 'user_password', with: 'password123'
     fill_in 'user_password_confirmation', with: 'password123'
 
-    expect(page).to have_current_path('/users/new')
+    expect(page).to have_current_path('/signup')
   end
 
   scenario 'user email is submitted incorrectly' do
 
-    visit '/users/new'
+    visit '/signup'
     fill_in 'user_name', with: 'TestName'
     fill_in 'user_email', with: 'failEmail.wrong'
     fill_in 'user_password', with: 'password123'
     fill_in 'user_password_confirmation', with: 'password123'
 
-    expect(page).to have_current_path('/users/new')
+    expect(page).to have_current_path('/signup')
   end
 
   scenario 'password is not submitted' do
 
-    visit '/users/new'
+    visit '/signup'
     fill_in 'user_name', with: 'TestName'
     fill_in 'user_email', with: 'test@test.com'
     fill_in 'user_password', with: ' '
     fill_in 'user_password_confirmation', with: ' '
 
-    expect(page).to have_current_path('/users/new')
+    expect(page).to have_current_path('/signup')
   end
 
   scenario 'passwords dont match' do
 
-    visit '/users/new'
+    visit '/signup'
     fill_in 'user_name', with: 'TestName'
     fill_in 'user_email', with: 'test@test.com'
     fill_in 'user_password', with: 'password123'
     fill_in 'user_password_confirmation', with: 'password321'
 
-    expect(page).to have_current_path('/users/new')
+    expect(page).to have_current_path('/signup')
   end
 
   # scenario 'Everything is done correctly' do
