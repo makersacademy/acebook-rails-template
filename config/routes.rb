a7120c339a069e87094f27f94d0ed5d828951d48
Cynthia Fu <yinnycfu@gmail.com>
2021-07-27 17:20:55 +0100

    chore: review duplicated code and linting

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-
   root 'posts#index'
   get 'pages/about'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-
-  get    'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
 
   resources :posts do
     get '/like', to: 'posts#like', on: :member
