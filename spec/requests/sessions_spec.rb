cf288fc71394b50cecf6e49d4d597f9ec9015c76
Cynthia Fu <yinnycfu@gmail.com>
2021-07-27 17:25:30 +0100

    change route

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -3,17 +3,8 @@ require 'rails_helper'
 RSpec.describe "Sessions", type: :request do
   describe "GET /new" do
     it "returns http success" do
-<<<<<<< HEAD
-<<<<<<< HEAD
       get "/login"
-=======
-      get "/sessions/new"
->>>>>>> feat: sessions controller
-=======
-      get "/sessions/new"
->>>>>>> sign in css
       expect(response).to have_http_status(:success)
     end
   end
-
 end
