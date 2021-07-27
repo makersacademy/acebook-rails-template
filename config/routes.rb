a9959d574b6b9b07642fce002eca466fbc839f5a
Cynthia Fu <yinnycfu@gmail.com>
2021-07-27 15:04:42 +0100

    feat: sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   get 'pages/about'
   root "posts#index"
 
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :posts do
     get '/like', to: 'posts#like', on: :member
   end
