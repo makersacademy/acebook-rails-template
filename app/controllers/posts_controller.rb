cadd03263ff9aefb897ef41ab336ceda0c48daa4
mikey-roberts <mikey_roberts@outlook.com>
2021-07-28 16:47:15 +0100

    pr of registration changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
 
   def index
     @post = Post.new
-      @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc)
     @comment = Comment.new
   end
 
