bb7837abe3ff960f681d65f11cd5e180e110878f
Fabio <alessf13@gmail.com>
2021-07-28 15:35:38 +0300

    test: user sign in with incorrect password passing

--- spec/features/users_login_test_spec.rb
+++ spec/features/users_login_test_spec.rb
@@ -3,17 +3,16 @@ require 'rails_helper'
 RSpec.feature "User Sign in", type: :feature do
 	 scenario 'Login in without the correct password' do
 
- 		 visit '/signup'
+ 		visit '/signup'
     fill_in 'user_name', with: 'test'
     fill_in 'user_email', with: 'test@test.com'
     fill_in 'user_password', with: 'password123'
     fill_in 'user_password_confirmation', with: 'password123'
 
- 		 visit '/login'
- 		 fill_in 'session_email', with: 'test@test.com'
- 		 fill_in 'session_password', with: 'password'
+ 		visit '/login'
+ 		fill_in 'session_email', with: 'test@test.com'
+ 		fill_in 'session_password', with: 'password'
 
- 		 expect(flash[:danger]).to match('Invalid email/password combination')
-
+		expect(page).to have_current_path('/login')
   end
 end
