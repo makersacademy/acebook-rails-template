09bb302fdf443a8639211417004e507eb80f7b3a
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-07-28 17:51:34 +0200

    profile page showing users post only(logic only)

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,11 @@ class ProfileController < ApplicationController
   def index
      @current_user = current_user
   end
+  
+  def posts
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
 
   def post_params
     params.require(:user).permit(:avatar)
