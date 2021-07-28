f1194e88b7bcd192c06362af9577aca0b0d12caa
Merryn <merrynhr@gmail.com>
2021-07-28 15:50:33 +0100

    posts may not be blank

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
 	 has_one_attached :image
 	 scope :order_by_created_at, -> { order("created_at DESC") }
+	 validates :message, presence: true
 
 	 def increment_likes(by_value)
  		 self.likes += by_value
  	end
 	
+
 end
