43da4193419f2763c9c378ffa648203addff44c7
Fabio <alessf13@gmail.com>
2021-07-28 17:13:04 +0300

    feat: index/about/new_post cannot be accessed without loggin in

--- spec/features/users_login_test_spec.rb
+++ spec/features/users_login_test_spec.rb
@@ -82,4 +82,37 @@ RSpec.feature "User Sign in", type: :feature do
  	# fill_in 'session_password', with: 'password123'
  	  expect(page).to have_current_path('/users')
   end
+
+	scenario 'Login in without the correct user_email' do
+
+	 visit '/signup'
+	 fill_in 'user_name', with: 'test'
+	 fill_in 'user_email', with: 'test@test.com'
+	 fill_in 'user_password', with: 'password123'
+	 fill_in 'user_password_confirmation', with: 'password123'
+
+	 visit '/login'
+	 fill_in 'session_email', with: 'development@test.com'
+	 fill_in 'session_password', with: 'password'
+	 click_on('Log in')
+
+	 expect(page).to have_current_path('/login')
+ end
+
+ scenario 'Successful login' do
+
+	visit '/signup'
+	fill_in 'user_name', with: 'test'
+	fill_in 'user_email', with: 'test@test.com'
+	fill_in 'user_password', with: 'password123'
+	fill_in 'user_password_confirmation', with: 'password123'
+
+	# click_on('Create my account')
+	find_button('Create my account').click
+
+	# visit '/login'
+	# fill_in 'session_email', with: 'test@test.com'
+	# fill_in 'session_password', with: 'password123'
+	expect(page).to have_current_path('/users')
+end
 end
