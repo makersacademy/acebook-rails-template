c797a4463d4e3ffef5f3f9b4b25bbb8ce6df8bb6
Fabio <alessf13@gmail.com>
2021-07-28 15:17:48 +0300

    fix: extra > deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,9 @@
 class Post < ApplicationRecord
 	 has_one_attached :image
-<<<<<<< HEAD
 	 scope :order_by_created_at, -> { order("created_at DESC") }
 
 	 def increment_likes(by_value)
  		 self.likes += by_value
  	end
 	
-=======
->>>>>>> chore: review duplicated code and linting
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,13 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> chore: review duplicated code and linting
   root 'posts#index'
   get 'pages/about'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
-
-<<<<<<< HEAD
-  get    'signup'  => 'users#new'
-  get    'login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
-=======
-  get 'sessions/new'
-  get 'pages/about'
-  root "posts#index"
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
->>>>>>> sign in css
-
-=======
->>>>>>> chore: review duplicated code and linting
   resources :posts do
     get '/like', to: 'posts#like', on: :member
   end
-
   resources :users do
   end 
 end
