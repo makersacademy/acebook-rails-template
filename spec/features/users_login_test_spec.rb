df136df757a5b5e9291b2a01d2cd53cb84a9a691
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-29 10:42:39 +0100

    linting

--- spec/features/users_login_test_spec.rb
+++ spec/features/users_login_test_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "User Sign in", type: :feature do
 	 scenario 'Login in without the correct password' do
 
- 		visit '/signup'
+ 		 visit '/signup'
     fill_in 'user_name', with: 'test'
     fill_in 'user_email', with: 'test@test.com'
     fill_in 'user_password', with: 'password123'
@@ -50,36 +50,36 @@ RSpec.feature "User Sign in", type: :feature do
  	  expect(page).to have_current_path('/users')
   end
 
-	scenario 'Login in without the correct user_email' do
+	 scenario 'Login in without the correct user_email' do
 
-	 visit '/signup'
-	 fill_in 'user_name', with: 'test'
-	 fill_in 'user_email', with: 'test@test.com'
-	 fill_in 'user_password', with: 'password123'
-	 fill_in 'user_password_confirmation', with: 'password123'
+ 	  visit '/signup'
+ 	  fill_in 'user_name', with: 'test'
+ 	  fill_in 'user_email', with: 'test@test.com'
+ 	  fill_in 'user_password', with: 'password123'
+ 	  fill_in 'user_password_confirmation', with: 'password123'
 
-	 visit '/login'
-	 fill_in 'session_email', with: 'development@test.com'
-	 fill_in 'session_password', with: 'password'
-	 click_on('Log in')
+ 	  visit '/login'
+ 	  fill_in 'session_email', with: 'development@test.com'
+ 	  fill_in 'session_password', with: 'password'
+ 	  click_on('Log in')
 
-	 expect(page).to have_current_path('/login')
- end
+ 	  expect(page).to have_current_path('/login')
+  end
 
- scenario 'Successful login' do
+  scenario 'Successful login' do
 
-	visit '/login/new'
-	fill_in 'user_name', with: 'test'
-	fill_in 'user_email', with: 'test123@test.com'
-	fill_in 'user_password', with: 'password123'
-	fill_in 'user_password_confirmation', with: 'password123'
+ 	  visit '/login/new'
+ 	  fill_in 'user_name', with: 'test'
+ 	  fill_in 'user_email', with: 'test123@test.com'
+ 	  fill_in 'user_password', with: 'password123'
+ 	  fill_in 'user_password_confirmation', with: 'password123'
 
-	click_on('Create my account')
-	# find_button('Create my account').click
+ 	  click_on('Create my account')
+ 	# find_button('Create my account').click
 
-	# visit '/login'
-	# fill_in 'session_email', with: 'test@test.com'
-	# fill_in 'session_password', with: 'password123'
-	expect(page).to have_current_path('/users')
-end
+ 	# visit '/login'
+ 	# fill_in 'session_email', with: 'test@test.com'
+ 	# fill_in 'session_password', with: 'password123'
+ 	  expect(page).to have_current_path('/users')
+  end
 end
