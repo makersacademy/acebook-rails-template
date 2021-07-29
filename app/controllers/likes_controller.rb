ff3ea30a812d7e8f3921726a9f97f44c9ededb8a
Masako Gray <masakogray@hotmail.co.uk>
2021-07-29 19:50:12 +0100

    renews page to show likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,11 @@
 class LikesController < ApplicationController
   before_action :find_post
   def create
-    @post.likes.create(user_id: current_user.id)
-    redirect_to post_path(@post)
+    unless @post.likes.create(user_id: current_user.id)
+      redirect_to post_path(@post)
+    else
+      redirect_to URI(request.referer).path
+    end
   end
   private
   def find_post
