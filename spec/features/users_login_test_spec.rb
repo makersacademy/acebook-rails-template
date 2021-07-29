8dee86f0292b0c3c21ec894f8a7be62ac6eee77a
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-29 10:40:48 +0100

    sign-in & sign-up finished

--- spec/features/users_login_test_spec.rb
+++ spec/features/users_login_test_spec.rb
@@ -35,14 +35,14 @@ RSpec.feature "User Sign in", type: :feature do
 
  scenario 'Successful login' do
 
-	visit '/signup'
+	visit '/login/new'
 	fill_in 'user_name', with: 'test'
-	fill_in 'user_email', with: 'test@test.com'
+	fill_in 'user_email', with: 'test123@test.com'
 	fill_in 'user_password', with: 'password123'
 	fill_in 'user_password_confirmation', with: 'password123'
 
-	# click_on('Create my account')
-	find_button('Create my account').click
+	click_on('Create my account')
+	# find_button('Create my account').click
 
 	# visit '/login'
 	# fill_in 'session_email', with: 'test@test.com'
