a73dc46cbc5094a5e7a0a57b5ef6a61ec0a7f086
Chuka Okoye <chukanorth@hotmail.co.uk>
2021-07-30 11:23:41 +0100

    Fixed bugs to make feature tests pass and refactored

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
-require_relative 'web_helpers'
+require_relative 'web_helpers/users_helper'
+
 
 RSpec.feature "Acebook", type: :feature do
   scenario "Can view a post" do
+    signup_login
     visit_home_create_post
+    expect(page).to have_content("Hello, world!")
   end
 
   scenario "Can delete a post" do
+    signup_login
     visit_home_create_post
     click_link "Delete"
     expect(page).to_not have_content("Hello, world!")
   end
 
   scenario "Can edit a post" do
+    signup_login
     visit_home_create_post
     click_link "Edit"
     fill_in "Message", with: "Hello, world!!"
