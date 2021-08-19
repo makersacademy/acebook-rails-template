c21ddd2707c24376c461b66d8ab090dac90e757e
Brett Karlson <brettjkarlson@gmail.com>
2021-08-19 16:52:09 +0100

    fixed non-image uploads

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
 
   has_one_attached :image
+
+  validates :image, blob: { content_type: ['image/png', 'image/jpg', 'image/jpeg'], size_range: 1..5.megabytes }
+
 end
