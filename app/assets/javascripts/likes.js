e1a1421763458488539045980855495813b8a0d0
karimabuseer <karimabuseer@gmail.com>
2021-08-19 14:44:37 +0100

    Likes update without reloading

--- app/assets/javascripts/likes.js
+++ app/assets/javascripts/likes.js
@@ -1,24 +1,22 @@
 document.addEventListener('DOMContentLoaded', (event) => {
   console.log('DOM fully loaded and parsed');
-  const button = document.querySelectorAll('.button_to')
+  const button = document.querySelectorAll('.button_to') 
   // let fn = function(e) { e.preventDefault() };
-  for ( var i = 0; i < button.length; i++) {
-    button[i].addEventListener('click', (e) => {
-    console.log('Button pressed!!!!')
-    e.preventDefault()
-    $.ajax ({ 
-      url: 'posts/:post_id/likes',
-      type: 'POST',
+ 
+  for ( let i  = 0; i < button.length; i++) {
+    button[i].addEventListener('click', (e) => { 
+      e.preventDefault();
+     let id = button[i][0].id;
+     let likes = document.getElementById(`likes${id}`);
+      $.ajax ({ 
+        url: `posts/${id}/likes`,
+        type: 'POST',
+        success: function (response) {
+          let like = $($.parseHTML(response)).filter('p')[i];
+          likes.innerHTML = like.innerHTML;
+       }
+      });
+      
     });
-    })
   }
-})
-
-  //url: post_likes_path
-
-// var buttons = document.querySelectorAll('button'),
-//     fn      = function(e) { e.preventDefault() };
-
-// for ( var i = buttons.length; i--; ) {
-//     buttons[i].addEventListener('click', fn, false);
-// }
+});
