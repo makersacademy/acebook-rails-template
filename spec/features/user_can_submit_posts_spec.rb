6265d9765aaafa964b346b2e4e6ec2c6fceca32d
William Young <wadyoung@outlook.com>
2021-08-20 17:07:33 +0100

    Blocked on image content test

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -12,10 +12,11 @@ RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and with images view them" do
     visit "/posts"
     click_link "New post"
-    fill_in "Message", with: "Hello, world!"
+    fill_in "post[message]", with: "Hello, world!"
     attach_file("post[image]", './spec/files/attachment.jpeg')
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
+    expect(page).to have_css("img[src*='./spec/files/attachment.jpeg']")
   end
 
   scenario "Gives error message if post empty" do
