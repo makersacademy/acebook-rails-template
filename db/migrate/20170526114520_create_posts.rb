84b6fff39fd3e698382ef3c12390e505de04dd98
Rebecca <Beckyrose200@aol.com>
2021-11-24 10:48:47 +0000

    Test for likes

--- db/migrate/20170526114520_create_posts.rb
+++ db/migrate/20170526114520_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
       t.string :message
 
       t.timestamps
+
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_05_26_114520) do
+ActiveRecord::Schema.define(version: 2021_11_23_160744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2017_05_26_114520) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "likes"
   end
 
 end
