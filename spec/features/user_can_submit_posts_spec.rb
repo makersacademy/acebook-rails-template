31f2cfee41145a2083370246b12b9a13f2c46f83
MarcBrosch <marc.brosch@ford.com>
2021-11-26 12:27:29 +0100

    User can like a post - Test implemented

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
   scenario "Can submit posts and view them" do
     visit "/posts"
-    click_link "New post"
+    click_link "Write a post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
@@ -11,10 +11,19 @@ RSpec.feature "Timeline", type: :feature do
 
   scenario "User can like a post" do
     visit "/posts"
-    click_link "New post"
+    click_link "Write a post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
-    click_button ("Hello, world!")
-    expect(page).to have_content("1 Like") 
+    click_button ("Likes")
+    expect(page).to have_button("1 Likes")
   end 
+
+  scenario 'Posts are displayed in descending order' do
+    visit '/posts'
+    click_link "Write a post"
+    fill_in "Message", with: "First post"
+    click_button "Submit"
+    click_button ("Likes")
+  end
+
 end
