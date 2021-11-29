bb71beb0e321030e7c2ba3e8623102a154f444ce
MarcBrosch <marc.brosch@ford.com>
2021-11-29 14:40:00 +0100

    wip user can submit a comment

--- spec/features/user_can_write_comment_spec.rb
+++ spec/features/user_can_write_comment_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "Comments", type: :feature do
-  scenario "User can write comment on a post" do
+  scenario "User can comment on a post" do
     visit "/posts"
     click_link "Write a post"
     fill_in "Message", with: "Hello, world!"
     click_button "Submit"
-    fill_in "Comment", with: "Nice comment"
-    click_button "Post Comment"
-    expect(page).to have_content("Nice comment")
+    click_link "Comment"
+    fill_in "Comment", with: "Nice one!"
+    click_button "Submit comment"
+    expect(page).to have_content("Nice one!")
   end
-end 
+end
