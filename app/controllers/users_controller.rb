1deb20ad5914432e36d0a8a173e568f09ab794eb
Rebecca <Beckyrose200@aol.com>
2021-11-30 12:06:29 +0000

    Dates displaying

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:first_name, :surname, :email, :password, :password_confirmation, :gender, :dob)
+   p params.require(:user).permit(:first_name, :surname, :email, :password, :password_confirmation, :gender, :dob)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_25_153348) do
+ActiveRecord::Schema.define(version: 2021_11_30_115933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.string "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
@@ -44,7 +52,7 @@ ActiveRecord::Schema.define(version: 2021_11_25_153348) do
     t.string "email"
     t.string "password_digest"
     t.string "gender"
-    t.string "dob"
+    t.date "dob"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
