5c200ee6b0f69d44f15529b7c03af0b7f3759099
Hannah Desmond <desmond.hannah@googlemail.com>
2022-02-08 17:22:48 +0000

    Update for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
