ffff11b29331191babb0881e87a8f602ed691273
Neto <fpottinger.marques@gmail.com>
2022-02-09 07:49:55 +0000

    debugging heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :posts
   root 'posts#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
 end
