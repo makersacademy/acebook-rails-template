1aa5122ab54026fcc3c75251705471921e2c9bb4
samir <s-malik@live.co.uk>
2022-02-10 16:37:48 +0000

    Routing Testing

--- spec/features/users_can_sign_in_spec.rb
+++ spec/features/users_can_sign_in_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature 'sign in' do
   scenario 'users get to a sign in page' do
     visit('/')
-    click_button("Sign In")
+    click_link("Sign In")
     expect(page).to have_content("Email address")
     expect(page).to have_content("Remember me")
   end
