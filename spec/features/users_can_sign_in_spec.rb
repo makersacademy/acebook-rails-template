929cdf86c73734f581d3fe2d858bf452e1089727
Hannah Desmond <desmond.hannah@googlemail.com>
2022-02-10 15:43:48 +0000

    Add feature tests for welcome page and sign in

---
+++ spec/features/users_can_sign_in_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'sign in' do
+  scenario 'users get to a sign in page' do
+    visit('/')
+    click_button("Sign In")
+    expect(page).to have_content("Email address")
+    expect(page).to have_content("Remember me")
+  end
+end
