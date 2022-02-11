691d182ad70a41a338fe75ced4bb66a29fd76834
jmodiale <remen.omodiale@outlook.com>
2022-02-11 15:12:29 +0000

    debugging

--- spec/features/user_can_delete_posts_spec.rb
+++ spec/features/user_can_delete_posts_spec.rb
@@ -3,11 +3,10 @@ require 'rails_helper'
 RSpec.feature "Timeline", type: :feature do
     scenario "Posts page" do
       visit "/posts"
-      click_link "New post"
+      click_link "Add a post"
       fill_in "Message", with: "Hello, world!"
       click_button "Submit"
-      click_button "Delete"
-      expect(page).to have_button("Delete")
+      click_on "Delete"
       expect(page).not_to have_content("Hello, world!")
     end
 end
