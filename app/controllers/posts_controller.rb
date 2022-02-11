a0a7a06fbcf904f185a454f75079d9c05c789523
Alexander Jamshidi <alexanderjamshidi@Alexanders-Air.localdomain>
2022-02-11 17:11:25 +0000

    if not signed in, users will be redirected to sign-in page. and added temporary signout link to layout view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /posts or /posts.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :posts
-
   get '/welcome/index'
   
-  
+  resources :posts
+  devise_for :users
 end
