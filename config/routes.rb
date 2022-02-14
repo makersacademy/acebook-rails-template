f0e2628eae86330c9c78e968dcb225c34e09bd92
Hannah Desmond <desmond.hannah@googlemail.com>
2022-02-14 15:12:47 +0000

    Remove duplicate show posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/welcome/index'
+  get 'posts/index'
 
   resources :posts
-  devise_for :users
 end
