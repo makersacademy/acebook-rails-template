fa803072e784726ec5767606e5cf21d8a992a958
samir <s-malik@live.co.uk>
2022-02-15 14:24:17 +0000

    Functionality to restrict CRUD to only signed in user profiles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   # GET /posts or /posts.json
   def index
     @posts = Post.all
@@ -14,7 +14,8 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    # @post = Post.new
+    @post = current_user.posts.build 
   end
 
   # GET /posts/1/edit
@@ -22,13 +23,14 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    # @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
         format.html { redirect_to post_url(@post), notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
-      else
+      else  
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
@@ -58,10 +60,10 @@ class PostsController < ApplicationController
     end
   end
 
-  # def correct_user
-  #    @post = current_user.posts.find_by(id: params[:id])
-  #    redirect_to posts_path, notice: "not permitted to edit" if @post.nil?
-  # end
+  def correct_user
+     @post = current_user.posts.find_by(id: params[:id])
+     redirect_to posts_path, notice: "Not Permitted To Edit" if @post.nil?
+  end
   private
 
   # Use callbacks to share common setup or constraints between actions.
