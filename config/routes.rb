c0e444e9ec55e61d28393a4824b65198385fb037
Raven Reynolds Sears <ravenrosereynolds@gmail.com>
2022-02-15 14:58:19 +0000

    debugging heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root 'users#sign_up'
+  root 'registrations#new'
   #HTML route          Controller_name  #method
   get '/users/sign_up', to:'registrations#new'
 
