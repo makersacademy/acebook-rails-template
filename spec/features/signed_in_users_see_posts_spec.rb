7bb7dc9021f938649d5977ee6252542c20b35243
Hannah Desmond <desmond.hannah@googlemail.com>
2022-02-15 17:13:23 +0000

    Re-routing so clicking logo sends user to home page

--- spec/features/signed_in_users_see_posts_spec.rb
+++ spec/features/signed_in_users_see_posts_spec.rb
@@ -12,4 +12,16 @@ feature 'index page for signed in users' do
     expect(page).to have_content("Posts")
     expect(page).not_to have_content("Welcome to Acebook")
   end
+  scenario 'clicking Acebook returns to posts page if signed in' do
+    visit('/')
+    click_link("Sign Up")
+    fill_in("user_email", with: "hello@hello.com")
+    fill_in("user_password", with: "123456")
+    fill_in("user_password_confirmation", with: "123456")
+    click_button("Sign up")
+    click_link('Account')
+    click_link('Acebook')
+    expect(page).to have_content("Posts")
+    expect(page).not_to have_content("Welcome to Acebook")
+  end
 end
