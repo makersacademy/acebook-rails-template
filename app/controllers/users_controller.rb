5563d0909f7ad972cd7bea1e93b7033e0e8957cc
Neto <fpottinger.marques@gmail.com>
2022-02-16 13:17:26 +0000

    debugging edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,6 @@
-# redirecting to posts index even when posting on user wall 
-
-
-
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    # @posts = @user.posts.order(created_at: :desc)
     @posts = Post.all.where(receiver_id: params[:id]).order("created_at DESC")
     @post = Post.new
   end
@@ -15,9 +10,20 @@ class UsersController < ApplicationController
     redirect_to current_user_path
   end
 
+  def edit
+    @user = User.find(params[:id])
+    # redirect_to edit_user_path(current_user)
+    # render(:devise => 'registrations/edit')
+  end
+
+  def update
+    current_user.update(user_params)
+    redirect_to edit_user_registration_path
+  end
+
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :id)
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   get 'profile/show'
   devise_for :users
 
+  devise_scope :user do
+    get '/users/edit' => 'devise/registrations#edit'
+  end
+
   resources :posts
   resources :profile
-  resources :users, only: [:create, :new, :show]
+  resources :users, only: [:create, :new, :show, :edit, :update]
   root to: 'posts#index'
   
 end
