fd45217b0ef65ddae2b78c5e40c8bc6163349513
ElliottZaki <spudnick99@hotmail.com>
2022-02-16 17:00:19 +0000

    debugging like - Feature test 3

--- spec/features/user_can_like_spec.rb
+++ spec/features/user_can_like_spec.rb
@@ -1,26 +1,39 @@
 require 'rails_helper'
 
 RSpec.feature "Like", type: :feature do
-  scenario "A post starts with 0 likes" do
-    visit "/posts"
-    click_link "Add a post"
-    fill_in "Message", with: "Hello, world!"
-    click_button "Submit"
+  # it "A post starts with 0 likes" do
+  #   visit "/posts"
+  #   click_link "Add a post"
+  #   fill_in "Message", with: "Hello, world!"
+  #   click_button "Submit"
 
-    expect(page).to have_content('0 Likes')
+  #   expect(page).to have_content('0 Likes')
+  # end
+  before(:each) do
+    u = User.new(username: "practice_user",
+             email: "practice@practice.co.uk",
+              password: "Password"
+            )
+    puts "#{u.id}"
   end
+  
+    it "A post has 1 like" do
+      visit "/posts"
+      click_link "Add a post"
+      fill_in "Message", with: "Hello, world!"
+      click_button "Submit"
+      click_button "Like"
+      expect(response).to have_http_status(302)
+      # expect(page).to have_content('0 Likes')  
+    end
 
 end
 
 
 
+# id | email | encrypted_password | reset_password_token | reset_password_sent_at | remember_created_at | created_at | updated_at | surname | name | 
 
-# subject {
-#   described_class.new(title: "Anything",
-#                       description: "Lorem ipsum",
-#                       start_date: DateTime.now,
-#                       end_date: DateTime.now + 1.week)
-# }
+
 
 
 # @post.likes.create(user_id: current_user.id)
