01d2e9d190df8b0993574267db32a57c5503f5ea
ElliottZaki <spudnick99@hotmail.com>
2022-02-16 14:39:54 +0000

    Debugging like test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 
-    belongs_to :user
+    # belongs_to :user
     has_many :likes, dependent: :destroy
     #  attr_reader :created_at, :persisted
 
--- spec/features/user_can_like_spec.rb
+++ spec/features/user_can_like_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "Like", type: :feature do
-  # scenario "A post starts with 0 likes" do
-  #   visit "/posts"
-  #   click_link "Add a post"
-  #   fill_in "Message", with: "Hello, world!"
-  #   click_button "Submit"
+  scenario "A post starts with 0 likes" do
+    visit "/posts"
+    click_link "Add a post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
 
-  #   expect(Post.first.get_likes.count).to be(0)
-  #   click_button "Like"
-  #   expect(Post.first.get_likes.count).to be(1)
-  # end
+    # Post.create.count
+    click_button "Like"
+    # expect(Post.create.count).to be(1)
+  end
 end
+
+# @post.likes.create(user_id: current_user.id)
+#    redirect_to posts_url
 
 
