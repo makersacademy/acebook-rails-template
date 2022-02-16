be28920577d9c69359b93da4df49cdf30dac2955
Neto <fpottinger.marques@gmail.com>
2022-02-16 09:07:07 +0000

    debug heroku: deleted comments from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,6 @@ class UsersController < ApplicationController
     @posts = @user.posts.order(created_at: :desc)
   end
 
-  # def index
-  #   # @users = User.all
-  #   @users = User.where('id != ?' , current_user.id).page(params[:page]).per(12)
-  # end
-
   def create
     User.create(user_params)
     redirect_to current_user_path
