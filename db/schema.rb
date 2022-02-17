3e4e25dfe2b348818ce30388ad81d0b3bd027909
Vanessa Florio <vanessaflorio1997@gmail.com>
2022-02-17 15:16:25 +0000

    Push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_15_171444) do
+ActiveRecord::Schema.define(version: 2022_02_16_173440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_02_15_171444) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,7 +41,7 @@ ActiveRecord::Schema.define(version: 2022_02_15_171444) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "surname"
     t.string "name"
-    t.string "username"
+    t.string "username", default: ""
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
