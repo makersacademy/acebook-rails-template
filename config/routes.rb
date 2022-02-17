b0ca84a14b30c0f1b03da61fd2a84751122c08db
Raven Reynolds Sears <ravenrosereynolds@gmail.com>
2022-02-17 12:45:59 +0000

    debugging heroku

--- config/routes.rb
+++ config/routes.rb
@@ -16,13 +16,9 @@ Rails.application.routes.draw do
   get '/users/sign_up', to:'registrations#new'
 
   get '/posts/new', to: 'posts#new'
-<<<<<<< HEAD
-  get '/posts/:id/edit', to: 'posts#edit'
-  resources :posts
-=======
+
   get 'posts/:id/edit', to: 'posts#edit'
   resources :posts do
     resources :likes
   end
->>>>>>> a0424141cce5e03ec2369003152bb7a5a27900d0
 end
