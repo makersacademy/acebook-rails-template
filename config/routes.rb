220518dcc4fda7ae0d7d5d9bfcd76a8638c3f976
Raven Reynolds Sears <ravenrosereynolds@gmail.com>
2022-02-17 13:37:27 +0000

    debugging heroku

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
 
   #HTML route          Controller_name  #method
   get '/users/sign_up', to:'registrations#new'
-
   get '/posts/new', to: 'posts#new'
   get '/posts/:id/edit', to: 'posts#edit'
   resources :posts
