f2806a3238824f1277cf89fdbed8c59f399ac50a
Raven Reynolds Sears <ravenrosereynolds@gmail.com>
2022-02-17 19:13:51 +0000

    debugging heroku

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Acebook
     
     config.assets.initialize_on_precompile = false
 
+    config.active_record.schema_format = :sql
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
