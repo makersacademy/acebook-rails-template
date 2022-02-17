8f1c1d05156b487f46a2565f0ff0d1815b8188af
chisambwe <c.m.kapihya@gmail.com>
2022-02-17 20:03:58 +0000

    css improvements

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all
+    @post = current_user.posts.build
   end
 
   # GET /posts/1 or /posts/1.json
@@ -41,7 +42,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: 'Post was successfully updated.' }
+        format.html { redirect_to posts_index_url, notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
