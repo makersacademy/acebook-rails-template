44b28c0d7d36ef07bc84363b65a8de2d5a95feb9
Francisco Pottinger-Marques <fpottinger.marques@gmail.com>
2022-02-17 16:31:23 +0000

    debug heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
   def update
     current_user.update(user_params)
-    redirect_to current_user_path
+    redirect_to root_path
   end
 
   private
