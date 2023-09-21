05d5c8869406663bd7b1133c4a13c1c1ac02d1aa
nayan0499 <nayangrg.uni@gmail.com>
2023-09-21 12:04:37 +0100

    Change sign up and sign in paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get "sign_up" => "devise/registrations#new" # 
+  end
+  
   # devise_for :users, :controllers => {
   #   registrations: 'registrations'
   # }
