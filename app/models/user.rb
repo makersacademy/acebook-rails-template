2349d8955bcb181a68ce15b7e0215ffbec784f26
nayan0499 <nayangrg.uni@gmail.com>
2023-09-21 11:57:21 +0100

    Add username and remove password confirmation to the sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
+  # devise_for :users, :controllers => {
+  #   registrations: 'registrations'
+  # }
 end
