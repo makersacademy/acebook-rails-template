27d6d72b5aebc0d3c1dca29ac1d9d7f0de2b42b9
nayan0499 <nayangrg.uni@gmail.com>
2023-09-21 12:16:12 +0100

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validate :email_has_com_domain
+
+  def email_has_com_domain
+    unless email =~ /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+      errors.add(:email, "must have a '.com' domain")
+    end
+  end
 end
