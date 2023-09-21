9a16ed6fd391469ec1dfbc269c65c5acbdccba26
Ani <anisingh@outlook.com>
2023-09-21 15:07:40 +0100

    Add logout button and redirect to posts once signed up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
+
+  def after_sign_in_path_for(resource)
+    posts_path # Replace with your desired path
+  end
+  
 end
