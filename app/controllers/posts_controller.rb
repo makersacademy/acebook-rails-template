cf0b9e2c439c6737683637a2c44c682cfd0bd82c
nayan0499 <nayangrg.uni@gmail.com>
2023-09-25 17:06:39 +0100

    Redirect to the previous page after posting new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,19 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
+  before_action :store_previous_path, only: [:new]
 
   def new
     @post = Post.new
+  
   end
 
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id # Set the user_id attribute
+    username = current_user.username # Get the username
+
     if @post.save
-      # Handle successful post creation
-      redirect_to posts_url
-    # else
-    #   # Handle errors
+      redirect_to session.delete(:previous_path) || posts_path
     end
   end
 
