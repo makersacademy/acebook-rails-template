181f4f06d3d279a127162788d37e7fed8fbfef74
nayan0499 <nayangrg.uni@gmail.com>
2023-09-26 12:06:13 +0100

    Obtain post correctly in user profile

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,8 +1,9 @@
 class UserProfilesController < ApplicationController
+    before_action :authenticate_user!
     def show
         @user = User.find_by(username: params[:username])
         @username = params[:username]
-        @posts = Post.where(user_id: current_user.id)
+        @posts = Post.where(user_id: @user.id).order("created_at DESC")
         if @user
             render 'user_profiles/user_profile'
         else
