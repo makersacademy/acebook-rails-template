701ca649d5bd30f4ac9c3ff590ea71d1365afd3e
nayan0499 <nayangrg.uni@gmail.com>
2023-09-26 12:05:15 +0100

    Modify profile route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   authenticated :user do
     root to: 'posts#index', as: :authenticated_root
   end
-  get '/:username', to: 'user_profiles#show', as: :user_profile
+  get '/profile/:username', to: 'user_profiles#show', as: :user_profile
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
