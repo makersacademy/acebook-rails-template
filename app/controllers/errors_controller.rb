58a749da6fede2c1a19afdd2528c96863ed93382
nayan0499 <nayangrg.uni@gmail.com>
2023-09-27 12:25:44 +0100

    Pass test

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,8 @@
 class ErrorsController < ApplicationController
   def not_found
-    @username = current_user.username
+    if user_signed_in?
+      @username = current_user.username
+    end 
     render status: 404
   end
 end
--- spec/requests/errors_spec.rb
+++ spec/requests/errors_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Errors", type: :request do
   describe "GET /not_found" do
     it "returns http success" do
       get "/errors/not_found"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(404)
     end
   end
 
