61e35455d8301c158bfd465243548de434a992dd
nayan0499 <nayangrg.uni@gmail.com>
2023-09-27 16:49:20 +0100

    Remove write your post btn

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -2,6 +2,7 @@ class UserProfilesController < ApplicationController
     before_action :authenticate_user!
     def show
         @user = User.find_by(username: params[:username])
+        @current_username = current_user.username
         @username = params[:username]
         @posts = Post.where(user_id: @user.id).order("created_at DESC")
         if @user
